ed076d77778e24152c91da94a68e55f5a9ebafb4
Arno Candel <arno.candel@gmail.com>
2014-05-09 15:19:25 -0700

    Add ConfusionMatrix classes, now with different names...

---
+++ h2o-core/src/main/water/ConfusionMatrix.java
@@ -0,0 +1,188 @@
+package water;
+
+import water.fvec.Chunk;
+import water.fvec.Frame;
+import water.fvec.TransfVec;
+import water.fvec.Vec;
+import water.util.ArrayUtils;
+import water.util.ModelUtils;
+import java.util.Arrays;
+
+/**
+ *  Compare two categorical columns, reporting a grid of co-occurrences.
+ *  <br>
+ *  The semantics follows R-approach - see R code:
+ *  <pre>
+ *  &gt; l = c("A", "B", "C")
+ *  &gt; a = factor(c("A", "B", "C"), levels=l)
+ *  &gt; b = factor(c("A", "B", "A"), levels=l)
+ *  &gt; confusionMatrix(a,b)
+ *
+ *            Reference
+ * Prediction A B C
+ *          A 1 0 0
+ *          B 0 1 0
+ *          C 1 0 0
+ *  </pre>
+  *
+ *  <p>Note: By default we report zero rows and columns.</p>
+ *
+ *  @author cliffc
+ */
+public class ConfusionMatrix {
+//  static final int API_WEAVER = 1; // This file has auto-gen'd doc & json fields
+//  static public DocGen.FieldDoc[] DOC_FIELDS; // Initialized from Auto-Gen code.
+
+//  @API(help = "", required = true, filter = Default.class)
+  public Frame actual;
+
+//  @API(help="Column of the actual results (will display vertically)", required=true, filter=actualVecSelect.class)
+  public Vec vactual;
+//  class actualVecSelect extends VecClassSelect { actualVecSelect() { super("actual"); } }
+
+//  @API(help = "", required = true, filter = Default.class)
+  public Frame predict;
+
+//  @API(help="Column of the predicted results (will display horizontally)", required=true, filter=predictVecSelect.class)
+  public Vec vpredict;
+//  class predictVecSelect extends VecClassSelect { predictVecSelect() { super("predict"); } }
+
+//  @API(help="domain of the actual response")
+  String [] actual_domain;
+//  @API(help="domain of the predicted response")
+  String [] predicted_domain;
+//  @API(help="union of domains")
+  public
+  String [] domain;
+//  @API(help="Confusion Matrix (or co-occurrence matrix)")
+  public long cm[][];
+
+//  @API(help="Mean Squared Error")
+  public double mse = Double.NaN;
+
+  private boolean classification;
+
+  private void init() throws IllegalArgumentException {
+    classification = vactual.isInt() && vpredict.isInt();
+    // Input handling
+    if( vactual==null || vpredict==null )
+      throw new IllegalArgumentException("Missing actual or predict!");
+    if (vactual.length() != vpredict.length())
+      throw new IllegalArgumentException("Both arguments must have the same length!");
+    // Handle regression kind which is producing CM 1x1 elements
+    if (!classification && vactual.isEnum())
+      throw new IllegalArgumentException("Actual vector cannot be categorical for regression scoring.");
+    if (!classification && vpredict.isEnum())
+      throw new IllegalArgumentException("Predicted vector cannot be categorical for regression scoring.");
+  }
+
+  public void execImpl() {
+    init();
+    Vec va = null,vp = null, avp = null;
+    try {
+      if (classification) {
+        // Create a new vectors - it is cheap since vector are only adaptation vectors
+        va = vactual .toEnum(); // always returns TransfVec
+        actual_domain = va.factors();
+        vp = vpredict.toEnum(); // always returns TransfVec
+        predicted_domain = vp.factors();
+        if (!Arrays.equals(actual_domain, predicted_domain)) {
+          domain = ArrayUtils.domainUnion(actual_domain, predicted_domain);
+          int[][] vamap = Model.getDomainMapping(domain, actual_domain, true);
+          va = TransfVec.compose( (TransfVec) va, vamap, domain, false ); // delete original va
+          int[][] vpmap = Model.getDomainMapping(domain, predicted_domain, true);
+          vp = TransfVec.compose( (TransfVec) vp, vpmap, domain, false ); // delete original vp
+        } else domain = actual_domain;
+        // The vectors are from different groups => align them, but properly delete it after computation
+        if (!va.group().equals(vp.group())) {
+          avp = vp;
+          vp = va.align(vp);
+        }
+        cm = new CM(domain.length).doAll(va,vp)._cm;
+      } else {
+        mse = new CM(1).doAll(vactual,vpredict).mse();
+      }
+      return;
+    } finally {       // Delete adaptation vectors
+      if (va!=null) DKV.remove(va._key);
+      if (vp!=null) DKV.remove(vp._key);
+      if (avp!=null) DKV.remove(avp._key);
+    }
+  }
+
+  // Compute the co-occurrence matrix
+  private static class CM extends MRTask<CM> {
+    /* @IN */ final int _c_len;
+    /* @OUT Classification */ long _cm[][];
+    /* @OUT Regression */ public double mse() { return _count > 0 ? _mse/_count : Double.POSITIVE_INFINITY; }
+    /* @OUT Regression Helper */ private double _mse;
+    /* @OUT Regression Helper */ private long _count;
+    CM(int c_len) { _c_len = c_len;  }
+    @Override public void map( Chunk ca, Chunk cp ) {
+      //classification
+      if (_c_len > 1) {
+        _cm = new long[_c_len+1][_c_len+1];
+        int len = Math.min(ca._len,cp._len); // handle different lenghts, but the vectors should have been rejected already
+        for( int i=0; i < len; i++ ) {
+          int a=ca.isNA0(i) ? _c_len : (int)ca.at80(i);
+          int p=cp.isNA0(i) ? _c_len : (int)cp.at80(i);
+          _cm[a][p]++;
+        }
+        if( len < ca._len )
+          for( int i=len; i < ca._len; i++ )
+            _cm[ca.isNA0(i) ? _c_len : (int)ca.at80(i)][_c_len]++;
+        if( len < cp._len )
+          for( int i=len; i < cp._len; i++ )
+            _cm[_c_len][cp.isNA0(i) ? _c_len : (int)cp.at80(i)]++;
+      } else {
+        _cm = null;
+        _mse = 0;
+        assert(ca._len == cp._len);
+        int len = ca._len;
+        for( int i=0; i < len; i++ ) {
+          if (ca.isNA0(i) || cp.isNA0(i)) continue; //TODO: Improve
+          final double a=ca.at0(i);
+          final double p=cp.at0(i);
+          _mse += (p-a)*(p-a);
+          _count++;
+        }
+      }
+    }
+
+    @Override public void reduce( CM cm ) {
+      if (_cm != null && cm._cm != null) {
+        ArrayUtils.add(_cm,cm._cm);
+      } else {
+        assert(_mse != Double.NaN && cm._mse != Double.NaN);
+        assert(_cm == null && cm._cm == null);
+        _mse += cm._mse;
+        _count += cm._count;
+      }
+    }
+  }
+
+  public boolean toHTML( StringBuilder sb ) {
+    //TODO: Re-enable
+//    if (classification) {
+//      DocGen.HTML.section(sb,"Confusion Matrix");
+//      if( cm == null ) return true;
+//      printConfusionMatrix(sb, cm, domain, true);
+//    } else{
+//      DocGen.HTML.section(sb,"Mean Squared Error");
+//      if( mse == Double.NaN ) return true;
+//      DocGen.HTML.arrayHead(sb);
+//      sb.append("<tr class='warning'><td>" + mse + "</td></tr>");
+//      DocGen.HTML.arrayTail(sb);
+//    }
+    return true;
+  }
+
+  public void toASCII( StringBuilder sb ) {
+    if (classification) {
+      if(cm == null) return;
+      ModelUtils.printConfusionMatrix(sb, cm, domain, false);
+    } else {
+      sb.append("MSE: " + mse);
+    }
+  }
+}
---
+++ h2o-core/src/main/water/ConfusionMatrix2.java
@@ -0,0 +1,248 @@
+package water;
+
+import water.util.ArrayUtils;
+import static water.util.ModelUtils.printConfusionMatrix;
+
+import java.util.Arrays;
+
+public class ConfusionMatrix2 extends Iced {
+//  static final int API_WEAVER = 1; // This file has auto-gen'd doc & json fields
+//  static public FieldDoc[] DOC_FIELDS; // Initialized from Auto-Gen code.
+//  @API(help="Confusion matrix (Actual/Predicted)")
+  public long[][] _arr; // [actual][predicted]
+//  @API(help = "Prediction error by class")
+  public final double[] _classErr;
+//  @API(help = "Prediction error")
+  public double _predErr;
+
+//  @Override public ConfusionMatrix2 clone() {
+//    ConfusionMatrix2 res = new ConfusionMatrix2(0);
+//    res._arr = _arr.clone();
+//    for( int i = 0; i < _arr.length; ++i )
+//      res._arr[i] = _arr[i].clone();
+//    return res;
+//  }
+
+  public enum ErrMetric {
+    MAXC, SUMC, TOTAL;
+
+    public double computeErr(ConfusionMatrix2 cm) {
+      double[] cerr = cm.classErr();
+      double res = 0;
+      switch( this ) {
+        case MAXC:
+          res = cerr[0];
+          for( double d : cerr )
+            if( d > res )
+              res = d;
+          break;
+        case SUMC:
+          for( double d : cerr )
+            res += d;
+          break;
+        case TOTAL:
+          res = cm.err();
+          break;
+        default:
+          throw new RuntimeException("unexpected err metric " + this);
+      }
+      return res;
+    }
+
+  }
+
+  public ConfusionMatrix2(int n) {
+    _arr = new long[n][n];
+    _classErr = classErr();
+    _predErr = err();
+  }
+
+  public ConfusionMatrix2(long[][] value) {
+    _arr = value;
+    _classErr = classErr();
+    _predErr = err();
+  }
+
+  public ConfusionMatrix2(long[][] value, int dim) {
+    _arr = new long[dim][dim];
+    for (int i=0; i<dim; ++i)
+      System.arraycopy(value[i], 0, _arr[i], 0, dim);
+    _classErr = classErr();
+    _predErr = err();
+  }
+
+  public void add(int i, int j) {
+    _arr[i][j]++;
+  }
+
+  public double[] classErr() {
+    double[] res = new double[_arr.length];
+    for( int i = 0; i < res.length; ++i )
+      res[i] = classErr(i);
+    return res;
+  }
+
+  public final int size() {
+    return _arr.length;
+  }
+
+  public void reComputeErrors(){
+    for(int i = 0; i < _arr.length; ++i)
+      _classErr[i] = classErr(i);
+    _predErr = err();
+  }
+  public final long classErrCount(int c) {
+    long s = 0;
+    for( long x : _arr[c] )
+      s += x;
+    return s - _arr[c][c];
+  }
+  public final double classErr(int c) {
+    long s = 0;
+    for( long x : _arr[c] )
+      s += x;
+    if( s == 0 )
+      return 0.0;    // Either 0 or NaN, but 0 is nicer
+    return (double) (s - _arr[c][c]) / s;
+  }
+  public long totalRows() {
+    long n = 0;
+    for (long[] a_arr : _arr)
+      for (int p = 0; p < a_arr.length; ++p)
+        n += a_arr[p];
+    return n;
+  }
+
+  public void add(ConfusionMatrix2 other) {
+    ArrayUtils.add(_arr, other._arr);
+  }
+
+  /**
+   * @return overall classification error
+   */
+  public double err() {
+    long n = totalRows();
+    long err = n;
+    for( int d = 0; d < _arr.length; ++d )
+      err -= _arr[d][d];
+    return (double) err / n;
+  }
+  public long errCount() {
+    long n = totalRows();
+    long err = n;
+    for( int d = 0; d < _arr.length; ++d )
+      err -= _arr[d][d];
+    return err;
+  }
+  /**
+   * The percentage of predictions that are correct.
+   */
+  public double accuracy() { return 1-err(); }
+  /**
+   * The percentage of negative labeled instances that were predicted as negative.
+   * @return TNR / Specificity
+   */
+  public double specificity() {
+    if(!isBinary())throw new UnsupportedOperationException("specificity is only implemented for 2 class problems.");
+    double tn = _arr[0][0];
+    double fp = _arr[0][1];
+    return tn / (tn + fp);
+  }
+  /**
+   * The percentage of positive labeled instances that were predicted as positive.
+   * @return Recall / TPR / Sensitivity
+   */
+  public double recall() {
+    if(!isBinary())throw new UnsupportedOperationException("recall is only implemented for 2 class problems.");
+    double tp = _arr[1][1];
+    double fn = _arr[1][0];
+    return tp / (tp + fn);
+  }
+  /**
+   * The percentage of positive predictions that are correct.
+   * @return Precision
+   */
+  public double precision() {
+    if(!isBinary())throw new UnsupportedOperationException("precision is only implemented for 2 class problems.");
+    double tp = _arr[1][1];
+    double fp = _arr[0][1];
+    return tp / (tp + fp);
+  }
+  /**
+   * The maximum per-class error
+   * @return max(classErr(i))
+   */
+  public double max_per_class_error() {
+    int n = nclasses();
+    if(n == 0)throw new UnsupportedOperationException("max per class error is only defined for classification problems");
+    double res = classErr(0);
+    for(int i = 1; i < n; ++i)
+      res = Math.max(res,classErr(i));
+    return res;
+  }
+
+  public final int nclasses(){return _arr == null?0:_arr.length;}
+  public final boolean isBinary(){return nclasses() == 2;}
+
+  /**
+   * Returns the F-measure which combines precision and recall. <br>
+   * C.f. end of http://en.wikipedia.org/wiki/Precision_and_recall.
+   */
+  public double F1() {
+    final double precision = precision();
+    final double recall = recall();
+    return 2. * (precision * recall) / (precision + recall);
+  }
+
+  @Override public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for( long[] r : _arr )
+      sb.append(Arrays.toString(r) + "\n");
+    return sb.toString();
+  }
+
+//  public JsonArray toJson() {
+//    JsonArray res = new JsonArray();
+//    JsonArray header = new JsonArray();
+//    header.add(new JsonPrimitive("Actual / Predicted"));
+//    for( int i = 0; i < _arr.length; ++i )
+//      header.add(new JsonPrimitive("class " + i));
+//    header.add(new JsonPrimitive("Error"));
+//    res.add(header);
+//    for( int i = 0; i < _arr.length; ++i ) {
+//      JsonArray row = new JsonArray();
+//      row.add(new JsonPrimitive("class " + i));
+//      long s = 0;
+//      for( int j = 0; j < _arr.length; ++j ) {
+//        s += _arr[i][j];
+//        row.add(new JsonPrimitive(_arr[i][j]));
+//      }
+//      double err = s - _arr[i][i];
+//      err /= s;
+//      row.add(new JsonPrimitive(err));
+//      res.add(row);
+//    }
+//    JsonArray totals = new JsonArray();
+//    totals.add(new JsonPrimitive("Totals"));
+//    long S = 0;
+//    long DS = 0;
+//    for( int i = 0; i < _arr.length; ++i ) {
+//      long s = 0;
+//      for( int j = 0; j < _arr.length; ++j )
+//        s += _arr[j][i];
+//      totals.add(new JsonPrimitive(s));
+//      S += s;
+//      DS += _arr[i][i];
+//    }
+//    double err = (S - DS) / (double) S;
+//    totals.add(new JsonPrimitive(err));
+//    res.add(totals);
+//    return res;
+//  }
+
+  public void toHTML(StringBuilder sb, String[] domain) {
+    long[][] cm = _arr;
+    printConfusionMatrix(sb, cm, domain, true);
+  }
+
+}
