e22ac3b84e6f8c512eaee0585cd8de468566cf6a
cliffclick <cliffc@acm.org>
2014-06-19 18:14:39 -0700

    Bring in Summary to H2O2

--- h2o-algos/src/main/java/hex/schemas/SummaryHandler.java
+++ h2o-algos/src/main/java/hex/schemas/SummaryHandler.java
@@ -10,12 +10,13 @@ public class SummaryHandler extends Handler<SummaryHandler,SummaryV2> {
   @Override protected int max_ver() { return Integer.MAX_VALUE; }
   // Inputs
   public Frame _fr;
+  public int _max_qbins;
 
   // Outputs
-  public Summary _summary;           // The summary
+  public Summary[] _summary;    // The summary
 
   public SummaryHandler() {}
-  public void work() { _summary = new Summary(_fr); }
+  public void work() { _summary = Summary.doFrame(_fr,_max_qbins); }
   @Override protected SummaryV2 schema(int version) { return new SummaryV2(); }
   @Override public void compute2() { throw H2O.fail(); }
 }
--- h2o-algos/src/main/java/hex/schemas/SummaryV2.java
+++ h2o-algos/src/main/java/hex/schemas/SummaryV2.java
@@ -1,5 +1,6 @@
 package hex.schemas;
 
+import hex.Summary;
 import water.*;
 import water.api.API;
 import water.api.Schema;
@@ -11,6 +12,10 @@ public class SummaryV2 extends Schema<SummaryHandler,SummaryV2> {
   // Input fields
   @API(help="Input source frame",required=true)
   public Key src;
+
+  @API(help="Max binning for quantiles")
+  public int max_qbins;
+  
 
   // Output fields
   @API(help="Columns")
@@ -25,10 +30,10 @@ public class SummaryV2 extends Schema<SummaryHandler,SummaryV2> {
     final long missing;
 
     @API(help="min")
-    final double min;
+    final double[] mins;
 
     @API(help="max")
-    final double max;
+    final double[] maxs;
 
     @API(help="mean")
     final double mean;
@@ -36,21 +41,50 @@ public class SummaryV2 extends Schema<SummaryHandler,SummaryV2> {
     @API(help="sigma")
     final double sigma;
 
+    @API(help="NAs")
+    final long nas;
+
+    @API(help="Zeros")
+    final long zeros;
+
     @API(help="datatype: {time, enum, int, real}")
     final String type;
 
     @API(help="domain; not-null for enum columns only")
     final String[] domain;
 
-    Col( String name, Vec vec ) {
+    @API(help="Histogram Counts")
+    final long[] hcnt;
+
+    @API(help="Histogram Bin Starts")
+    final double[] hstarts;
+
+    @API(help="Percentiles")
+    final double[] pct;
+
+    @API(help="Percentile Values")
+    final double[] pctile;
+
+    @API(help="Cardinality")
+    final int cardinality;
+
+    Col( String name, Vec vec, Summary sum ) {
       label=name;
       missing = vec.naCnt();
-      min = vec.min();
-      max = vec.max();
+      mins = sum._mins;
+      maxs = sum._maxs;
       mean = vec.mean();
       sigma = vec.sigma();
       type = vec.isEnum() ? "enum" : vec.isUUID() ? "uuid" : (vec.isInt() ? (vec.isTime() ? "time" : "int") : "real");
       domain = vec.domain();
+      nas   = sum._stat0._nas;
+      zeros = sum._stat0._zeros;
+      hcnt = sum._hcnt;
+      hstarts = new double[hcnt.length];
+      for( int i=0; i<hcnt.length; i++ ) hstarts[i] = sum.binValue(i);
+      pct = sum._stats._pct;
+      pctile  = sum._stats._pctile;
+      cardinality = sum._stats._cardinality;
     }
   }
 
@@ -63,6 +97,9 @@ public class SummaryV2 extends Schema<SummaryHandler,SummaryV2> {
     if( val == null ) throw new IllegalArgumentException("Missing key "+src);
     if( !val.isFrame() ) throw new IllegalArgumentException("Not a Frame "+src);
     h._fr = val.get();
+    if( max_qbins < 0 || max_qbins > 1e8 ) throw new IllegalArgumentException("max_qbins out of range");
+    if( max_qbins == 0 ) max_qbins = 1000;
+    h._max_qbins = max_qbins;
     return this;
   }
 
@@ -71,7 +108,7 @@ public class SummaryV2 extends Schema<SummaryHandler,SummaryV2> {
     columns = new Col[h._fr.numCols()];
     Vec[] vecs = h._fr.vecs();
     for( int i=0; i<columns.length; i++ )
-      columns[i] = new Col(h._fr._names[i],vecs[i]);
+      columns[i] = new Col(h._fr._names[i],vecs[i],h._summary[i]);
     return this;
   }
 
