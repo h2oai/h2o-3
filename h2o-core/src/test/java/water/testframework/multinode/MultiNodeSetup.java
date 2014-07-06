b102f9d4e86e61a1016e866a42c917339b22ea04
Tom Kraljevic <tomk@tomk.net>
2014-07-06 00:09:27 -0700

    Update comments.

--- h2o-core/src/test/java/water/testframework/multinode/MultiNodeSetup.java
+++ h2o-core/src/test/java/water/testframework/multinode/MultiNodeSetup.java
@@ -3,22 +3,17 @@ package water.testframework.multinode;
 import org.testng.annotations.BeforeSuite;
 import water.H2O;
 
-/**
- * Run cookbook examples.
- *
- * Runs all the cookbook examples.
- * Does not require any arguments.
- */
 public class MultiNodeSetup {
   /**
    * Number of total H2O nodes to start.
    *
    * Note that all nodes are started within one process by this harness.
-   * This approach is good for unit testing.  You would not want to deploy to production like this.
+   * This approach is good for unit testing.
+   * You would not want to deploy to production like this.
    *
-   * Each H2O node is wrapped in it's own classloader.  The H2O nodes communicate with each other
-   * using sockets even though they all live in the same process.  This gives very realistic test
-   * behavior.
+   * Each H2O node is wrapped in it's own classloader.
+   * The H2O nodes communicate with each other using sockets even though
+   * they all live in the same process.
    */
   int _numNodes;
 
@@ -27,10 +22,11 @@ public class MultiNodeSetup {
   }
 
   /**
-   * Main program.
+   * Start 1 leader node.
+   * Start n-1 secondary nodes.
    *
-   * No args required.  Exit code 0 means all tests passed, nonzero otherwise.
-   * Each H2O node needs two ports, n and n+1.
+   * The primary node blocks for cloud initialization and runs the test
+   * program flow inside TestNG.
    */
   @BeforeSuite(groups={"multi-node"})
   public void setupCloud() {
