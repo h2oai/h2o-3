e45e443fed485b8eae271ec3003580ac6e831945
SpencerA <spnrpa@gmail.com>
2014-07-09 20:08:17 -0700

    add IcedInt class to wate.utilr

---
+++ h2o-core/src/main/java/water/util/IcedInt.java
@@ -0,0 +1,13 @@
+package water.util;
+
+import water.Iced;
+
+public class IcedInt extends Iced {
+  public final int _val;
+  public IcedInt(int v){_val = v;}
+  @Override public boolean equals( Object o ) {
+    return o instanceof IcedInt && ((IcedInt) o)._val == _val;
+  }
+  @Override public int hashCode() { return _val; }
+  @Override public String toString() { return Integer.toString(_val); }
+}
