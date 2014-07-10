0a3b66ea867a77060a1736320be6e8368e737bf8
Arno Candel <arno.candel@gmail.com>
2014-07-10 09:30:19 -0700

    PUBDEV-2: Update C4SChunk test to encode a 32-bit number range at totally different bias and scale.

--- h2o-core/src/test/java/water/fvec/C4SChunkTest.java
+++ h2o-core/src/test/java/water/fvec/C4SChunkTest.java
@@ -38,4 +38,35 @@ public class C4SChunkTest extends TestUtil {
       AssertJUnit.assertTrue(Arrays.equals(cc._mem, cc2._mem));
     }
   }
+  @Test void test_inflate_impl2() {
+    for (int l=0; l<1; ++l) {
+      NewChunk nc = new NewChunk(null, 0);
+
+      // different bias and scale than above, but still using the full 32-bit range (~4.29 billion different integers from -8.1b to -3.8b)
+      long[] man = new long[]{-814748364700000l, -5999999700000l, -58119987600000l, -385251635300000l};
+      int[] exp = new int[]{-19, -17, -18, -19};
+
+      if (l==1) nc.addNA(); //-2147483648
+      for (int i = 0; i < man.length; ++i) nc.addNum(man[i], exp[i]);
+      nc.addNA();
+
+      Chunk cc = nc.compress();
+      AssertJUnit.assertEquals(man.length + 1 + l, cc.len());
+      AssertJUnit.assertTrue(cc instanceof C4SChunk);
+      if (l==1) AssertJUnit.assertTrue(cc.isNA0(0));
+      for (int i = 0; i < man.length; ++i)
+        AssertJUnit.assertTrue("Expected: " + man[i] * Math.pow(10, exp[i]) + ", but is " + cc.at0(l+i), Math.abs((man[i] * Math.pow(10, exp[i])) - cc.at0(l+i)) < 1e-10);
+      AssertJUnit.assertTrue(cc.isNA0(man.length + l));
+
+      Chunk cc2 = cc.inflate_impl(new NewChunk(null, 0)).compress();
+      AssertJUnit.assertEquals(man.length + 1 + l, cc.len());
+      if (l==1) AssertJUnit.assertTrue(cc2.isNA0(0));
+      for (int i = 0; i < man.length; ++i)
+      AssertJUnit.assertTrue("Expected: " + man[i] * Math.pow(10, exp[i]) + ", but is " + cc2.at0(l+i), Math.abs((man[i] * Math.pow(10, exp[i])) - cc2.at0(l+i)) < 1e-10);
+      AssertJUnit.assertTrue(cc2.isNA0(man.length + l));
+      AssertJUnit.assertTrue(cc2 instanceof C4SChunk);
+
+      AssertJUnit.assertTrue(Arrays.equals(cc._mem, cc2._mem));
+    }
+  }
 }
