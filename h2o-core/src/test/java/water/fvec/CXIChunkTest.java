35df526dd43bc6f42213889bc08aa807aed77d04
Arno Candel <arno.candel@gmail.com>
2014-07-11 12:19:39 -0700

    PUBDEV-2: Add tests for CXIChunk.

---
+++ h2o-core/src/test/java/water/fvec/CXIChunkTest.java
@@ -0,0 +1,40 @@
+package water.fvec;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+import water.TestUtil;
+
+import java.util.Arrays;
+
+public class CXIChunkTest extends TestUtil {
+  @Test void test_inflate_impl() {
+    for (int l=0; l<2; ++l) {
+      NewChunk nc = new NewChunk(null, 0);
+
+      int[] vals = new int[]{0, 0, 0, Integer.MAX_VALUE, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, Integer.MIN_VALUE+1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+              0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+      if (l==1) nc.addNA();
+      for (int v : vals) nc.addNum(v, 0);
+      nc.addNA();
+
+      Chunk cc = nc.compress();
+      AssertJUnit.assertEquals(vals.length+1+l, cc.len());
+      AssertJUnit.assertTrue(cc instanceof CXIChunk);
+      if (l==1) AssertJUnit.assertTrue(cc.isNA0(0));
+      for (int i = 0; i < vals.length; ++i) AssertJUnit.assertEquals(vals[i], cc.at80(i+l));
+      AssertJUnit.assertTrue(cc.isNA0(vals.length+l));
+
+      Chunk cc2 = cc.inflate_impl(new NewChunk(null, 0)).compress();
+      AssertJUnit.assertEquals(vals.length+1+l, cc.len());
+      AssertJUnit.assertTrue(cc2 instanceof CXIChunk);
+      if (l==1) AssertJUnit.assertTrue(cc2.isNA0(0));
+      for (int i = 0; i < vals.length; ++i) AssertJUnit.assertEquals(vals[i], cc2.at80(i+l));
+      AssertJUnit.assertTrue(cc2.isNA0(vals.length+l));
+
+      AssertJUnit.assertTrue(Arrays.equals(cc._mem, cc2._mem));
+    }
+  }
+}
