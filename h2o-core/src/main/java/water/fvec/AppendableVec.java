19197ceac14bbc6dd39a78f5d7a4c3bea202c7f1
cliffclick <cliffc@acm.org>
2014-08-05 14:17:47 -0700

    small syntax change

--- h2o-core/src/main/java/water/fvec/AppendableVec.java
+++ h2o-core/src/main/java/water/fvec/AppendableVec.java
@@ -22,7 +22,7 @@ public class AppendableVec extends Vec {
   public static final byte NUMBER = 4;
   public static final byte TIME   = 8;
   public static final byte UUID   =16;
-  public static final byte STRING    =32;
+  public static final byte STRING =32;
   byte [] _chunkTypes;
   long _naCnt;
   long _enumCnt;
@@ -57,8 +57,8 @@ public class AppendableVec extends Vec {
   }
 
   // What kind of data did we find?  NA's?  Strings-only?  Floats or Ints?
-  public boolean shouldBeEnum() {
-    // TODO: we declare column to be string/enum only if it does not have ANY numbers in it.
+  boolean shouldBeEnum() {
+    // We declare column to be string/enum only if it does not have ANY numbers in it.
     return _enumCnt > 0 && (_enumCnt + _strCnt + _naCnt) == _totalCnt;
   }
 
