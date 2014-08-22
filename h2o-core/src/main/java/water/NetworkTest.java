3840aae049d8cd363ad77009cb50ffe8de1aa9bd
Arno Candel <arno.candel@gmail.com>
2014-08-22 15:18:49 -0700

    PUB-837: Port over recent changes to NetworkTest (collective comm).

--- h2o-core/src/main/java/water/NetworkTest.java
+++ h2o-core/src/main/java/water/NetworkTest.java
@@ -1,5 +1,6 @@
 package water;
 
+import water.fvec.Vec;
 import water.util.ArrayUtils;
 import water.util.PrettyPrint;
 import water.util.Timer;
@@ -7,15 +8,27 @@ import water.util.Timer;
 import java.util.Random;
 
 
-public class NetworkTest extends H2O.H2OCountedCompleter<NetworkTest> {
+public class NetworkTest {
 //  static final int API_WEAVER=1; // This file has auto-gen'd doc & json fields
 //  static public DocGen.FieldDoc[] DOC_FIELDS; // Initialized from Auto-Gen code.
-
+//
 //  @API(help = "Message sizes", filter = Default.class, json=true)
   public int[] msg_sizes = new int[]{1,1<<10,1<<20}; //INPUT
 
 //  @API(help = "Repeats", filter = Default.class, json=true)
   public int repeats = 10; //INPUT
+
+//  @API(help = "Do collective test", filter = Default.class, json=true)
+  public boolean collective = true;
+
+//  @API(help = "Do serial test", filter = Default.class, json=true)
+  public boolean serial = true;
+
+//  @API(help = "Collective broadcast/reduce times in microseconds (for each message size)", json=true)
+  public double[] microseconds_collective; //OUTPUT
+
+//  @API(help = "Collective bandwidths in Bytes/sec (for each message size, for each node)", json=true)
+  public double[] bandwidths_collective; //OUTPUT
 
 //  @API(help = "Round-trip times in microseconds (for each message size, for each node)", json=true)
   public double[][] microseconds; //OUTPUT
@@ -26,21 +39,74 @@ public class NetworkTest extends H2O.H2OCountedCompleter<NetworkTest> {
 //  @API(help = "Nodes", json=true)
   public String[] nodes; //OUTPUT
 
-  @Override protected void compute2() {
+  public void execImpl() {
     microseconds = new double[msg_sizes.length][];
+    microseconds_collective = new double[msg_sizes.length];
+    NetworkTester nt = new NetworkTester(msg_sizes, microseconds, microseconds_collective, repeats, serial, collective);
+    nt.compute2();
+
+    // compute bandwidths from timing results
     bandwidths = new double[msg_sizes.length][];
-    for (int i=0; i<msg_sizes.length; ++i) {
-      microseconds[i] = send_recv_all(msg_sizes[i], repeats); //nanoseconds
-      ArrayUtils.div(microseconds[i], 1e3f); //microseconds
+    for (int i=0; i<bandwidths.length; ++i) {
       bandwidths[i] = new double[microseconds[i].length];
       for (int j=0; j< microseconds[i].length; ++j) {
         //send and receive the same message -> 2x
         bandwidths[i][j] = ( 2*msg_sizes[i] /*Bytes*/) / (microseconds[i][j] / 1e6 /*Seconds*/) ;
       }
     }
+
+    bandwidths_collective = new double[msg_sizes.length];
+    for (int i=0; i<bandwidths_collective.length; ++i) {
+      //broadcast and reduce the message to all nodes -> 2 x nodes
+      bandwidths_collective[i] = ( 2*H2O.CLOUD.size()*msg_sizes[i] /*Bytes*/) / (microseconds_collective[i] / 1e6 /*Seconds*/) ;
+    }
+
+    // populate node names
     nodes = new String[H2O.CLOUD.size()];
     for (int i=0; i<nodes.length; ++i)
       nodes[i] = H2O.CLOUD._memary[i]._key.toString();
+  }
+
+  // Helper class to run the actual test
+  public static class NetworkTester extends H2O.H2OCountedCompleter {
+    double[][] microseconds;
+    double[] microseconds_collective;
+    int[] msg_sizes;
+    public int repeats = 10;
+    boolean serial;
+    boolean collective;
+
+    @Override
+    public byte priority() {
+      return H2O.MIN_HI_PRIORITY;
+    }
+
+    public NetworkTester(int[] msg, double[][] res, double[] res_collective, int rep, boolean serial, boolean collective) {
+      microseconds = res;
+      microseconds_collective = res_collective;
+      msg_sizes = msg;
+      repeats = rep;
+      this.serial = serial;
+      this.collective = collective;
+    }
+    @Override
+    public void compute2() {
+      // serial comm
+      if (serial) {
+        for (int i = 0; i < microseconds.length; ++i) {
+          microseconds[i] = send_recv_all(msg_sizes[i], repeats);
+          ArrayUtils.div(microseconds[i], 1e3f); //microseconds
+        }
+      }
+      // collective comm
+      if (collective) {
+        for (int i = 0; i < microseconds_collective.length; ++i) {
+          microseconds_collective[i] = send_recv_collective(msg_sizes[i], repeats);
+        }
+        ArrayUtils.div(microseconds_collective, 1e3f); //microseconds
+      }
+      tryComplete();
+    }
   }
 
   /**
@@ -56,6 +122,10 @@ public class NetworkTest extends H2O.H2OCountedCompleter<NetworkTest> {
     }
     @Override public void compute2() {
       tryComplete();
+    }
+
+    @Override public byte priority() {
+      return H2O.MIN_HI_PRIORITY;
     }
   }
 
@@ -79,9 +149,40 @@ public class NetworkTest extends H2O.H2OCountedCompleter<NetworkTest> {
     return times;
   }
 
+
+  /**
+   * Helper class that contains a payload and has an empty map/reduce.
+   * If it is remotely executed, it will just send the payload over the wire.
+   */
+  private static class CollectiveTask extends MRTask<CollectiveTask> {
+    private final byte[] _payload; //will be sent over the wire (broadcast/reduce)
+
+    public CollectiveTask(byte[] payload){
+      _payload = payload;
+    }
+  }
+
+  /**
+   * Broadcast a message from this node to all nodes and reduce it back
+   * @param msg_size message size in bytes
+   * @return Time in nanoseconds that it took
+   */
+  private static double send_recv_collective(int msg_size, int repeats) {
+    byte[] payload = new byte[msg_size];
+    new Random().nextBytes(payload);
+    Vec v = Vec.makeConSeq(0., 1); //trivial Vec: 1 element with value 0.
+
+    Timer t = new Timer();
+    for (int l = 0; l < repeats; ++l) {
+      new CollectiveTask(payload).doAll(v); //same payload for all nodes
+    }
+    v.remove(new Futures()).blockForPending();
+    return (double) t.nanos() / repeats;
+  }
+
   public boolean toHTML(StringBuilder sb) {
     try {
-      sb.append("Origin: ").append(H2O.SELF._key);
+      sb.append("Origin: " + H2O.SELF._key);
 
       sb.append("<table cellpadding='10'>");
       sb.append("<tr>");
@@ -91,7 +192,21 @@ public class NetworkTest extends H2O.H2OCountedCompleter<NetworkTest> {
         sb.append(PrettyPrint.bytes(msg_size));
         sb.append("</th>");
       }
-      for (int n = 0; n < microseconds[0].length; ++n) {
+      sb.append("</tr>");
+
+      sb.append("<tr>");
+      sb.append("<td>");
+      sb.append("All (broadcast & reduce)");
+      sb.append("</td>");
+      for (int m = 0; m < msg_sizes.length; ++m) {
+        sb.append("<td>");
+        sb.append(PrettyPrint.usecs((long) microseconds_collective[m])).append(", ").
+                append(PrettyPrint.bytesPerSecond((long)bandwidths_collective[m]));
+        sb.append("</td>");
+      }
+      sb.append("</tr>");
+
+      for (int n = 0; n < H2O.CLOUD._memary.length; ++n) {
         sb.append("</tr>");
 
         sb.append("<tr>");
