18c42ea698710e9905f9e668325a44ba5c597995
mmalohlava <michal.malohlava@gmail.com>
2014-08-24 23:26:36 -0700

    Evil bytecode convertor transforming class file from Java7 to Java6 only in publishing mode.

---
+++ gradle/bin/j6patch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ ! -f "$1" ]; then
+cat <<EOF 
+  File name is required!"
+  Syntax is
+    $0 <class file>
+
+EOF
+exit 1
+fi
+
+FILENAME=$1
+[ $DEBUG ] && echo "Converting $FILENAME"
+printf "\x00\x00\x00\x32" | dd of=$FILENAME seek=4 bs=1 count=4 conv=notrunc 2> /dev/null
+KLAZZNAME="$(echo $1 | sed -e "s/.class$//")"
+[ $DEBUG ] && echo "Verifying class $KLAZZNAME"
+$JAVA_6_HOME/bin/javap $KLAZZNAME > /dev/null  || ( echo "Verification failed: $KLAZZNAME"; exit -1 )
+
