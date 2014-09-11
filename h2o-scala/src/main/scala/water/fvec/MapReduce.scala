a52ffb85711b388070423a3f8be8fee40c680269
cliffclick <cliffc@acm.org>
2014-09-11 21:22:40 +0200

    scala syntax haircut long fun talks with scala coding experts leads to terser map/reduce syntax

--- h2o-scala/src/main/scala/water/fvec/MapReduce.scala
+++ h2o-scala/src/main/scala/water/fvec/MapReduce.scala
@@ -3,22 +3,24 @@ package water.fvec
 import water._
 import scala.reflect.ClassTag
 
-abstract class MapReduce[MapType: ClassTag, E <: MapReduce[MapType,E]] extends Iced { self: E =>
+abstract class MapReduce[MapType: ClassTag] extends Iced {
+  type maptype=MapType
+  type self <: this.type
   // Selected numeric columns mapped into 'this'
-  def map : (MapType) => Unit
+  def map(@@ : MapType) : Unit
   // Reduce the given operand into 'this'
-  def reduce : (E) => Unit
+  def reduce(@@ : self) : Unit
   // Skip the whole row if any value is missing
   def skipNA = true
 
   // Take all columns; they must be of type MapType
-  def doAll( fr : DataFrame ) : E = {
+  def doAll( fr : DataFrame ) : self = {
     val jc = implicitly[ClassTag[MapType]].runtimeClass
     if( jc.isArray ) { // Array check: all Vecs are compatible with MapType's base type
       jc.getComponentType match {
         case q if q == classOf[Double] => 
           fr.vecs().zip(fr.names()).foreach( x => if( !x._1.isNumeric ) throw new IllegalArgumentException(x._2+"is not of type "+q))
-          new MRTask_AD().doAll(fr).outer
+          new MRTask_AD().doAll(fr).outer.asInstanceOf[self]
         case _ => ???                   // Array of String or UUID, etc
       }
     } else {
@@ -34,36 +36,41 @@ abstract class MapReduce[MapType: ClassTag, E <: MapReduce[MapType,E]] extends I
       val len = chks(0).len
       if( len == 0 ) return
       // Specialize user map to double[]
-      val map2 = outer.map.asInstanceOf[(Array[Double])=>Unit]
-      val mr2 : E = outer.clone    // Something to reduce into
-      val map22 = mr2.map.asInstanceOf[(Array[Double])=>Unit]
+      val mr2 : outer.self = outer.clone // Something to reduce into
       // Temp buffer to hold data without reallocating each row
       val row = new Array[Double](chks.length)
-      // No reduce for first map
-      var needreduce = false
+      val row2= row.asInstanceOf[MapType]
       // For all rows in Chunk
       var i = 0
       // Find first available row & map it
       while( i < len && !fill(row,chks,i) ) i += 1
-      map2(row); i += 1
+      outer.map(row2); i += 1
       // For all remaining rows, find available, map & reduce
       while( i < len ) {         // For all rows
         if( fill(row,chks,i) ) { // Fill all cols into 'row'
-          map22(row)             // Map into mr2
+          mr2.map(row2)          // Map into mr2
           outer.reduce(mr2)      // Reduce mr2 into self
         }
         i += 1
       }
     }
     // Call user reduce
-    override def reduce( mrt : MRTask_AD ) = outer.reduce(mrt.outer)
+    override def reduce( mrt : MRTask_AD ) = outer.reduce(mrt.outer.asInstanceOf[this.outer.self])
     // Fill reused temp array from Chunks.  Returns false is any value is NaN & skipNA true
     private def fill(row : Array[Double], chks : Array[Chunk], i : Int) : Boolean = {
-      var col = 0
-      while( col < chks.length ) {
-        val d = chks(col).at0(i); row(col) = d
-        if( skipNA && d.isNaN ) return false
-        col += 1
+      if( skipNA ) {
+        var col = 0
+        while( col < chks.length ) {
+          val d = chks(col).at0(i); row(col) = d
+          if( d.isNaN ) return false
+          col += 1
+        }
+      } else {
+        var col = 0
+        while( col < chks.length ) {
+          row(col) = chks(col).at0(i)
+          col += 1
+        }
       }
       true
     }
--- h2o-scala/src/test/scala/water/BasicTest.scala
+++ h2o-scala/src/test/scala/water/BasicTest.scala
@@ -4,6 +4,8 @@ import java.io.File
 import org.junit.Test
 import org.junit.BeforeClass
 import water.fvec.{DataFrame,MapReduce}
+import scala.reflect.ClassTag
+//import scala.language.reflectiveCalls
 
 class BasicTest extends TestUtil {
   @Test def basicTest() = {
@@ -15,24 +17,22 @@ class BasicTest extends TestUtil {
 
       // Linear Regression, Pass1
       // Sums & sum squares
-      class CalcSums(var X:Double =0, var Y:Double =0, var X2:Double =0, var nrows:Long=0) extends MapReduce[Array[Double],CalcSums] {
-        override def map = (row : Array[Double]) => { X = row(0); Y = row(1); X2 = X*X; nrows=1 }
-        override def reduce = (that : CalcSums) => { X += that.X ; Y += that.Y; X2 += that.X2; nrows += that.nrows }
-      }
-      val lr1 = new CalcSums().doAll(fr2)
+      val lr1 = new MapReduce[Array[Double]] { var X, Y, X2 =0.0; var nrows=0L
+        override def map(row : maptype) = { X = row(0); Y = row(1); X2 = X*X; nrows=1 }
+        override def reduce(@@ : self) = { X += @@.X ; Y += @@.Y; X2 += @@.X2; nrows += @@.nrows }
+      }.doAll(fr2)
       val meanX = lr1.X/lr1.nrows
       val meanY = lr1.Y/lr1.nrows
 
       // Linear Regression, Pass 2
       // Sum of squares of errors
-      class CalcErrs( var XXbar:Double =0, var YYbar:Double =0, var XYbar:Double =0 ) extends MapReduce[Array[Double],CalcErrs] {
-        override def map = (row : Array[Double]) => { 
+      val lr2 = new MapReduce[Array[Double]] { var XXbar, YYbar, XYbar = 0.0
+        override def map(row : maptype) = { 
           val dx = row(0)-meanX; val dy = row(1)-meanY
           XXbar = dx*dx;  YYbar = dy*dy;  XYbar = dx*dy
         }
-        override def reduce = (that : CalcErrs) => { XXbar += that.XXbar ; YYbar += that.YYbar; XYbar += that.XYbar }
-      }
-      val lr2 = new CalcErrs().doAll(fr2)
+        override def reduce(@@ : self) = { XXbar += @@.XXbar ; YYbar += @@.YYbar; XYbar += @@.XYbar }
+      }.doAll(fr2)
 
       // Compute the regression
       val beta1 = lr2.XYbar / lr2.XXbar
@@ -40,16 +40,15 @@ class BasicTest extends TestUtil {
       println(fr2._names(1)+" = "+beta1+"*"+fr2._names(0)+" + "+beta0)
 
       // Linear Regression, Pass 3
-      class CalcRegression( var ssr:Double = 0, var rss:Double = 0 ) extends MapReduce[Array[Double],CalcRegression] {
-        override def map = (row : Array[Double]) => { 
+      val lr3 = new MapReduce[Array[Double]] { var ssr, rss = 0.0
+        override def map(row : maptype) = { 
           val X = row(0); val Y = row(1)
           val fit = beta1*X + beta0
           rss = (fit-    Y)*(fit-    Y)
           ssr = (fit-meanY)*(fit-meanY)
         }
-        override def reduce = (that : CalcRegression) => { ssr += that.ssr ; rss += that.rss }
-      }
-      val lr3 = new CalcRegression().doAll(fr2)
+        override def reduce(@@ : self) = { ssr += @@.ssr ; rss += @@.rss }
+      }.doAll(fr2)
 
       // Compute goodness of fit
       val r2 = lr3.ssr / lr2.YYbar
@@ -67,30 +66,26 @@ class BasicTest extends TestUtil {
     }
   }
 
-  // test is off, until can pass around anon functions
+  // test is off because of its size
   /*@Test*/ def biggerTest() = {
+    //val fr = new DataFrame(new File("../smalldata/junit/cars_nice_header.csv"))
     val fr = new DataFrame(new File("../../datasets/UCI/UCI-large/covtype/covtype.data"))
     try {
-
+      val iters = 100
       val start = System.currentTimeMillis
-      (0 until 100) foreach( i => {
-        class CalcSums(var X:Double =0, var Y:Double =0, var X2:Double =0, var nrows:Long=0) extends MapReduce[Array[Double],CalcSums] {
-          override def map = (row : Array[Double]) => { X = row(0); Y = row(1); X2 = X*X; nrows=1 }
-          override def reduce = (that : CalcSums) => { X += that.X ; Y += that.Y; X2 += that.X2; nrows += that.nrows }
-        }
-        val lr1 = new CalcSums().doAll(fr)
+      (0 until iters) foreach( i => {
+        val lr1 = new MapReduce[Array[Double]] { var X, Y, X2 =0.0; var nrows=0L
+          override def map(row : maptype) = { X = row(0); Y = row(1); X2 = X*X; nrows=1 }
+          override def reduce(@@ : self) = { X += @@.X ; Y += @@.Y; X2 += @@.X2; nrows += @@.nrows }
+        }.doAll(fr)
         val meanX = lr1.X/lr1.nrows
         val meanY = lr1.Y/lr1.nrows
       })
       val end = System.currentTimeMillis
-      println("CalcSums iter over covtype: "+(end-start)/10)
+      println("CalcSums iter over covtype: "+(end-start)/iters)
 
     } finally {
       fr.delete()
     }
   }
-}
-
-object BasicTest extends TestUtil {
-  @BeforeClass def setup() = water.TestUtil.stall_till_cloudsize(5)
 }
