68a8ac65d9c066e8c7406ac9dc8edd578562d5eb
Arno Candel <arno.candel@gmail.com>
2014-09-16 19:54:55 -0700

    Set nodes to 5.

--- h2o-algos/src/test/java/hex/deeplearning/DeepLearningProstateTest.java
+++ h2o-algos/src/test/java/hex/deeplearning/DeepLearningProstateTest.java
@@ -18,7 +18,7 @@ import java.util.Random;
 import static hex.deeplearning.DeepLearningModel.DeepLearningParameters;
 
 public class DeepLearningProstateTest extends TestUtil {
-  @BeforeClass() public static void setup() { stall_till_cloudsize(1); }
+  @BeforeClass() public static void setup() { stall_till_cloudsize(5); }
 
   @Test public void run() throws Exception { runFraction(0.001f); }
 
