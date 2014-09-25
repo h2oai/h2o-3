c1c8b09a494261c0c1257d5ffeb10bc31fa8f432
SpencerA <spnrpa@gmail.com>
2014-09-24 17:22:18 -0700

    collpase usePackages into a single lapply

--- h2o-r/tests/Utils/runnerSetupPackage.R
+++ h2o-r/tests/Utils/runnerSetupPackage.R
@@ -12,42 +12,18 @@ function(p) {
 }
 
 # what packages did the h2o_master_DEV_test need?
-usePackage("R.utils")
-usePackage("R.oo")
-usePackage("R.methodsS3")
-usePackage("RCurl")
-usePackage("rjson")
-usePackage("statmod")
-usePackage("testthat")
-usePackage("bitops")
-usePackage("tools")
-usePackage("LiblineaR")
-usePackage("gdata")
-usePackage("caTools")
-usePackage("gplots")
-usePackage("ROCR")
-usePackage("digest")
-usePackage("penalized")
-usePackage("rgl")
-usePackage("randomForest")
-usePackage("expm")
-usePackage("Matrix")
-usePackage("glmnet")
-usePackage("survival")
-usePackage("gbm")
-usePackage("lattice")
-usePackage("RUnit")
-usePackage("plyr")
+packages <- c("R.utils", "R.oo", "R.methodsS3", "RCurl", "rjson", "statmod", "testthat", "bitops", "tools", "LiblineaR",
+              "gdata", "caTools", "ROCR", "digest", "penalized", "rgl", "randomForest", "expm", "Matrix", "glmnet",
+              "survival", "gbm", "lattice", "RUnit", "plyr")
 
+invisible(lapply(packages, usePackage))
 
 library(R.utils)
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("h2oR.R")
-
 ipPort <- get_args(commandArgs(trailingOnly = TRUE))
 
 if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
-
 failed <<- F
 tryCatch(library(h2o), error = function(e) {failed <<- T})
 if (! failed) {
