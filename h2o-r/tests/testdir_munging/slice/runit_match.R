a2cc67c0fde73bdfc987d9005dc1c91b74853e54
SpencerA <spnrpa@gmail.com>
2014-10-14 10:35:27 -0700

    add more debug prints to test (only fails in jenkins)

--- h2o-r/tests/testdir_munging/slice/runit_match.R
+++ h2o-r/tests/testdir_munging/slice/runit_match.R
@@ -8,15 +8,24 @@ test.match <- function(conn) {
 
   hex <- as.h2o(conn, iris)
 
+
+  Log.info("Printing the head of the iris data frame."
   print(hex)
 
+
+  Log.info("doing the match: hex$Species %in% c(\"setosa\", \"versicolor\")")
   sub <- hex$Species %in% c("setosa", "versicolor")
+
+  Log.info("Printing out the subset bit vec from the match call")
   print(sub)
-  
+ 
+  Log.info("performing the subsetting: hex[sub,]") 
   hh <- hex[sub,]
 
+  Log.info("print the head of the subsetted frame")
   print(hh)
   
+  Log.info("print the dim of the subsetted frame")
   print(dim(hh))
     
   testEnd()
