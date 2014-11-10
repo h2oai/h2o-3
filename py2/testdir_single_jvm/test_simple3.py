740ded23e3015558539ad26a07224c600a1d54a1
Kevin Normoyle <kevin@0xdata.com>
2014-11-10 00:48:27 -0800

    touch ray's magical endpoint routes to see what h2o can do.

---
+++ py2/testdir_single_jvm/test_simple3.py
@@ -0,0 +1,26 @@
+import unittest, sys, time
+sys.path.extend(['.','..','../..','py'])
+
+import h2o, h2o_cmd, h2o_import as h2i, h2o_browse as h2b
+
+class Basic(unittest.TestCase):
+    def tearDown(self):
+        h2o.check_sandbox_for_errors()
+
+    @classmethod
+    def setUpClass(cls):
+        h2o.init()
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_simple3(self):
+        a = h2o.nodes[0].endpoints()
+        # print h2o.dump_json(a)
+        print "There are %s endpoints" % len(a['routes'])
+        for l in a['routes']:
+            print l['url_pattern']
+
+if __name__ == '__main__':
+    h2o.unit_main()
