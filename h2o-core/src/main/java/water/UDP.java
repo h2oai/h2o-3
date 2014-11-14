3fee12f0f57eed824de82ea286b4988109dcde1d
cliffclick <cliffc@acm.org>
2014-11-13 17:32:56 -0800

    Fix unsigned byte bug

--- h2o-core/src/main/java/water/UDP.java
+++ h2o-core/src/main/java/water/UDP.java
@@ -56,7 +56,7 @@ public abstract class UDP {
   static char hex(int x) { x &= 0xf; return (char)(x+((x<10)?'0':('a'-10))); }
   String print16( AutoBuffer ab ) {
     for( int i=0; i<16; i++ ) {
-      int b = ab.get1();
+      int b = ab.get1U();
       cs[(i<<1)     ] = hex(b>>4);
       cs[(i<<1)+1   ] = hex(b   );
     }
