93457ee251360e1e77eacd16b6557a5295a34987
SpencerA <spnrpa@gmail.com>
2014-11-17 22:17:51 -0800

    allow ws after the '(' and before next token

--- h2o-core/src/main/java/water/rapids/Exec.java
+++ h2o-core/src/main/java/water/rapids/Exec.java
@@ -123,7 +123,7 @@ public class Exec extends Iced {
 
   String parseID() {
     StringBuilder sb = new StringBuilder();
-    if (peek() == '(') {_x++; return parseID(); } // eat the '('
+    if (peek() == '(') {_x++; skipWS(); return parseID(); } // eat the '(' and any ws.
     if ( isSpecial(peek())) { return sb.append((char)_ast[_x++]).toString(); } // if attached_token, then use parse_impl
     while(_x < _ast.length && _ast[_x] != ' ' && _ast[_x] != ')' && _ast[_x] != ';') {  // while not WS...
       sb.append((char)_ast[_x++]);
