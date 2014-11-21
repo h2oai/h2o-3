054b47444831147c3606f0956df6d46a4b60ac65
Kevin Normoyle <kevin@0xdata.com>
2014-11-21 14:58:47 -0800

    play with obj creation for responses

--- py2/testdir_single_jvm/test_GLM_basic_2.py
+++ py2/testdir_single_jvm/test_GLM_basic_2.py
@@ -1,6 +1,6 @@
 import unittest, time, sys, random
 sys.path.extend(['.','..','../..','py'])
-import h2o, h2o_cmd, h2o_glm, h2o_import as h2i, h2o_jobs
+import h2o, h2o_cmd, h2o_import as h2i, h2o_jobs
 from h2o_test import verboseprint, dump_json
 
 
