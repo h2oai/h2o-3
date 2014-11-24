e18cc10004f6f4941d418fbb8e217fe2e68e027d
Kevin Normoyle <kevin@0xdata.com>
2014-11-23 17:07:22 -0800

    a few more cases

--- py2/h2o_xexec.py
+++ py2/h2o_xexec.py
@@ -1,8 +1,9 @@
 
 import h2o_exec as h2e
 import re
-# from h2o_exec import xUnary, xBinary, xCall, xSequence, xColon, xAssign, xNum, xExec, xFrame, xVector
+# from h2o_exec import xUnary, xBinary, xFcn, xSequence, xColon, xAssign, xNum, xExec, xFrame, xVector
 
+#********************************************************************************
 def xNum(number):
     if isinstance(number, (list, tuple)):
         raise Exception("xexec xNum doesn't take lists or tuples %s" % number)
@@ -16,6 +17,32 @@ def xNum(number):
     except:
         return number
 
+#********************************************************************************
+# operands is a list of items or an item. Each item can be number, string, list or tuple
+# there is only one level of unpacking lists or tuples
+def unpackOperands(operands, joinSep=',', parent="none"):
+    operandList = []
+    if isinstance(operands, (list, tuple)):
+        for v in operands:
+            # can we handle any operand being a list here too? might be compact
+            # just one level of extra unpacking
+            if not isinstance(v, (list,tuple)):
+                operandList.append(xNum(v))
+            else:
+                for v2 in v:
+                    # collapse it into the one new list
+                    operandList.append(xNum(v2))
+    else:
+        operandList.append(xNum(operands))
+    
+    if len(operandList)!=0:
+        operandString = joinSep.join(map(str, operandList))
+        return "%s" % operandString
+    else:
+        raise Exception("xexec unpackOperands for %s, no operands: %s" % (parent, operands))
+
+#********************************************************************************
+# key is a string
 def xKey(key):
     if isinstance(key, (list, tuple)):
         raise Exception("xexec xKey doesn't take lists or tuples %s" % key)
@@ -28,6 +55,7 @@ def xKey(key):
     else:
         return "$%s" % key
 
+#********************************************************************************
 def xUnary(unary='_', rhs='xTemp'):
     rhs = xNum(rhs)
     return "%s %s" % (unary, rhs)
@@ -37,36 +65,27 @@ def xBinary(binary='+', operand1='xTemp', operand2='xTemp'):
     operand2 = xNum(operand2)
     return "%s %s %s" % (binary, operand1, operand2)
 
-def xCall(function='sum', *operands):
+# function is a string. operands is a list of items
+def xFcn(function='sum', *operands):
     # no checking for correct number of params
-    print "xCall %s has %s operands" % (function, len(operands))
+    print "xFcn %s has %s operands" % (function, len(operands))
 
-    for k,v in operands.iteritems():
-        if isinstance(v, (list, tuple)):
-            raise Exception("xexec xCall operand %s: %s doesn't take lists or tuples %s" % (k, v))
-        newOperands[k] = xNum(v)
-
-    if len(operands)!=0:
-        return "(%s %s)" % (function, " ".join(newOperands))
+    oprStr = unpackOperands(operands, joinSep=' ', parent='xSequence')
+    if len(oprStr)==0:
+        raise Exception("xexec xFcn, no operands: %s" % operands)
     else:
-        raise Exception("xexec xCall, no operands: %s" % operands)
+        return "(%s %s)" % (function, oprStr)
 
-# can take multiple parameters, each of which can be number, string, list or tuple
-def xSequence(*operands):
-    newOperands = []
-    for k,v in operands.iteritems():
-        # can we handle any operand being a list here too? might be compact
-        if not isinstance(v, (list,tuple)):
-            newOperands.append(xNum(v))
-        else:
-            for v2 in v:
-                # collapse it into the one new list
-                newOperands.append(xNum(v2))
-    
-    if len(newOperands)!=0:
-        return "{%s}" % (function, ";".join(newOperands))
-    else:
-        raise Exception("xexec xSequence, no operands: %s" % operands)
+# operands is a list of items
+def xSeq(*operands):
+    print "xexec xSeq operands: %s" % repr(operands)
+    return "{%s}" % unpackOperands(operands, joinSep=";", parent='xSeq')
+
+# operands is a list of items
+# 'c' can only have one operand? And it has to be a string or number
+# have this because it's so common as a function
+def xC(operand):
+    return xFcn("c", operand)
 
 # a/b can be number or string
 def xColon(a='#0', b='#0'):
@@ -80,7 +99,7 @@ def xColon(a='#0', b='#0'):
     b = xNum(b)
     return '(: %s %s)' % (a, b) 
 
-# row/col can be numbers, ranges, vectors. verify legal?
+# row/col can be numbers or strings or not specified
 def xFrame(frame='xTemp', row=None, col=None):
     if re.match('\$', frame):
         raise Exception("xExec xFrame adds '$': frame ref shouldn't start with '$' %s" % frame)
@@ -94,7 +113,7 @@ def xFrame(frame='xTemp', row=None, col=None):
     # we always add $ to a here?. Suppose could detect whether it's already there
     return '([ $%s %s %s)' % (frame, row, col)
 
-# row/col can be numbers, ranges, vectors. verify legal?
+# row/col can be numbers or strings or not speciffied
 # should Frame call this for single arg case?
 def xVector(frame='xTemp', row=None):
     if re.match('\$', frame):
