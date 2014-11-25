01493875517a03d69388d351500fe695098ae9ef
eric eckstrand <eric.eckstrand@gmail.com>
2014-11-25 12:56:21 -0800

    testthat kmeans test

---
+++ h2o-r/src/test/R/acceptance/test-kmeans.r
@@ -0,0 +1,18 @@
+context("Kmeans")
+
+conn <- h2o.init(ip=Sys.getenv("H2O_IP"), port=as.integer(Sys.getenv("H2O_PORT")))
+hex <- as.h2o(conn, iris)
+
+model <- tryCatch({
+	h2o.kmeans(data=hex, centers=1)
+}, error = function(err) { 
+	return(err)
+})
+
+if(is(model, "H2OKMeansModel")) {
+	test_that("Correct # of centers returned: ", {
+		expect_equal(5, length(model@model$clusters))
+	})
+} else {
+	test_that("Input permutation x: ", fail(message=toString(model)))
+}
