24101d3782905152d541ee8ea4c210a67193b753
Kevin Normoyle <kevin@0xdata.com>
2014-11-25 00:09:50 -0800

    maybe I have ls:ms reversed in a sequence, causing assertion error in *with_funs.py

--- py2/testdir_single_jvm/test_rapids_funs_basic2.py
+++ py2/testdir_single_jvm/test_rapids_funs_basic2.py
@@ -4,7 +4,9 @@ import h2o, h2o_browse as h2b, h2o_exec as h2e, h2o_import as h2i
 
 funsList = [
     # bad (Frame.java: 78 assert)
-    '(def anon {x} (var $x "null" $FALSE "null");;(var $x "null" $FALSE "null");;;)',
+    # '(def anon {x} (var $x "null" $FALSE "null");;(var $x "null" $FALSE "null");;;)',
+    # bad also
+    '(def anon {x} ( (var $x "null" $FALSE "null");;(var $x "null" $FALSE "null") );;;)',
     # good
     # '(def anon {x} (var $x "null" $FALSE "null");;;)',
 ]
@@ -16,13 +18,19 @@ funsList2 = [
     # bad. causes assertion
     # '(def anon {x}  ( (var ([ $x (: #0 #19) "null") "null" $FALSE "null") $x);;;)',
 
+    # bad?
+    '(def anon {x}  (var ([ $x (: #0 #19) "null") "null" $FALSE "null");;;)',
+    # does it need the extra paren? for the function def?
     '(def anon {x}  ( (var ([ $x (: #0 #19) "null") "null" $FALSE "null") );;;)',
+    '(def anon {x}  ((var ([ $x (: #0 #19) "null") "null" $FALSE "null"));;;)',
+
+    # '(def anon {x} (var $x "null" $FALSE "null");;;)',
+    # '(def anon {x} (var $x "null" $FALSE "null");;(var $x "null" $FALSE "null");;;)',
+    # '(def anon {x} (var $x "null" $FALSE "null"))',
+
     # '(def anon {x}  (var ([ $x (: #0 #19) "null") "null" $FALSE "null");;;)',
     # '(def anon {x}  (var ([ $x (: #0 #19) "null") "null" $FALSE "null") ;;;)',
-    '(def anon {x} (var $x "null" $FALSE "null");;;)',
-    '(def anon {x} (var $x "null" $FALSE "null");;(var $x "null" $FALSE "null");;;)',
     # what if you don't have ;;;
-    '(def anon {x} (var $x "null" $FALSE "null"))',
     # '(def anon {x} ( (var $x "null" $FALSE "null");;;) )',
     # '(def anon {x} ( ( (var $x "null" $FALSE "null");;;) ))',
     # '(def anon {x} (var $x "null" $FALSE "null") ;;;)',
