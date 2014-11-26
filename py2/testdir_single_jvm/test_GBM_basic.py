56fcc1d1d2eb987e05966af61a034b00f458b2fa
Kevin Normoyle <kevin@0xdata.com>
2014-11-25 21:19:17 -0800

    fix comment

--- py2/testdir_single_jvm/test_GBM_basic.py
+++ py2/testdir_single_jvm/test_GBM_basic.py
@@ -72,7 +72,6 @@ class Basic(unittest.TestCase):
         mmResult = h2o.n0.model_metrics(model=model_key, frame=parse_key, timeoutSecs=60)
         mm = OutputObj(mmResult, 'mm')
 
-        print "Skipping predict"
         prResult = h2o.n0.predict(model=model_key, frame=parse_key, timeoutSecs=60)
         pr = OutputObj(prResult['model_metrics'][0]['predictions'], 'pr')
 
