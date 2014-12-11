6383c792076c7d2dc25816821ec03df61db8ecb0
cliffclick <cliffc@acm.org>
2014-12-10 19:55:05 -0800

    lazy eval python wrapper works
    still no h2o, but lazy eval  & ref-cnt delete all work

--- h2o-py/src/main/py/frame.py
+++ h2o-py/src/main/py/frame.py
@@ -4,39 +4,42 @@ import csv
 # For now, see if we can make (small) 2-D associative arrays,
 # and overload everything.
 class Frame(object):
-  def __init__(self, file=None, vecs=None):
+  def __init__(self, str=None, file=None, vecs=None):
     if file != None:
       with open(file, 'rb') as csvfile:
         __reader__ = csv.reader(csvfile)
-        self.vecs=None
+        self._vecs=None
         for row in __reader__:
-          if self.vecs==None: 
-            self.vecs = []
+          if self._vecs==None: 
+            self._vecs = []
             for name in row:
-              self.vecs.append(Vec(name,[]))
+              self._vecs.append(Vec(str+name,[]))
           else:
             for i,data in enumerate(row):
-              self.vecs[i].append(data)
+              self._vecs[i].append(data)
     elif vecs!= None:
-      self.vecs=vecs
+      self._vecs=vecs
 
   def __str__(self):
-    return self.vecs.__repr__()
+    return self._vecs.__repr__()
+
+  def foo(self):
+    return "{"+",".join([v.foo() for v in self._vecs])+"}"
 
   def __getitem__(self,i):
     if isinstance(i,int):
-      return self.vecs[i]
+      return self._vecs[i]
     if isinstance(i,str):
-      for v in self.vecs:
-        if i==v.name:
+      for v in self._vecs:
+        if i==v._name:
           return v
       raise ValueError("Name "+i+" not in Frame")
     if isinstance(i,slice):
-      return Frame(vecs=self.vecs[i])
+      return Frame(vecs=self._vecs[i])
     raise NotImplementedError
 
   def __len__(self):
-    return len(self.vecs)
+    return len(self._vecs)
 
   def __add__(self,i):
     if len(self)==0:
@@ -44,26 +47,30 @@ class Frame(object):
     if isinstance(i,Frame):
       if len(i) != len(self):
         raise ValueError("Frame len()="+len(self)+" cannot be broadcast across len(i)="+len(i))
-      return Frame(vecs=[x+y for x,y in zip(self.vecs,i.vecs)])
+      return Frame(vecs=[x+y for x,y in zip(self._vecs,i._vecs)])
     if isinstance(i,Vec):
-      if len(i) != len(self.vecs[0]):
-        raise ValueError("Vec len()="+len(self.vecs[0])+" cannot be broadcast across len(i)="+len(i))
-      return Frame(vecs=[x+i for x in self.vecs])
+      if len(i) != len(self._vecs[0]):
+        raise ValueError("Vec len()="+len(self._vecs[0])+" cannot be broadcast across len(i)="+len(i))
+      return Frame(vecs=[x+i for x in self._vecs])
     if isinstance(i,int):
-      return Frame(vecs=[x+i for x in self.vecs])
+      return Frame(vecs=[x+i for x in self._vecs])
     raise NotImplementedError
 
   def __radd__(self,i):
     return self+i # Add is associative
 
-  def __del__(self):
-    print "del of "+str([v.name for v in self.vecs ])
 
 ###
 class Vec(object):
   def __init__(self, name, data):
-    self.name = name
-    self.data = data
+    self._name = name
+    self._data = data
+    self._len = len(data)
+
+  def eager(self):
+    if isinstance(self._data,Expr):
+      self._data=self._data.compute()
+    return self._data
 
   def append(self,data):
     __x__ = data
@@ -71,53 +78,86 @@ class Vec(object):
       __x__ = float(data)
     except ValueError:
       pass
-    self.data.append(__x__)
+    self._data.append(__x__)
     
   def __repr__(self):
-    return self.name+" "+self.data.__str__()
+    return self._name+" "+self.eager().__str__()
+
+  def foo(self):
+    return self._name+" "+str(self._data)
 
   def __getitem__(self,i):
-    return self.data[i]
+    return self._data[i]
 
   def __add__(self,i):
     if isinstance(i,Vec):
       if len(i) != len(self):
         raise ValueError("Vec len()="+len(self)+" cannot be broadcast across len(i)="+len(i))
-      return Vec(self.name+"+"+i.name,[x+y for x,y in zip(self.data,i.data)])
+      return Vec(self._name+"+"+i._name,Expr("+",self,i))
     if isinstance(i,int):
-      return Vec(self.name+"+"+str(i),[i+x for x in self.data])
+      if i==0:                  # Additive identity
+        return self
+      return Vec(self._name+"+"+str(i),Expr("+",self,i))
     raise NotImplementedError
 
   def __radd__(self,i):
     return self+i # Add is associative
 
   def __len__(self):
-    return len(self.data)
+    return len(self._data)
 
   def __del__(self):
-    print "del of "+self.name
+    if Expr==None or not isinstance(self._data,Expr):
+      print "DEL of "+self._name
+
+###
+class Expr(object):
+  def __init__(self,op,left,rite):
+    self._op = op
+    self._left = left
+    self._rite = rite
+
+  def compute(self):
+    print "WORK: ",self
+    if self._op == "+":
+      if isinstance(self._rite,Vec):
+        return [x+y for x,y in zip(self._left.eager(),self._rite.eager())]
+      if isinstance(self._rite,int):
+        return [x+self._rite for x in self._left.eager()]
+      raise NotImplementedError
+    raise NotImplementedError
+
+  def __len__(self):
+    return len(self._left) if isinstance(self._left,Vec) else len(self._rite)
+
+  def __repr__(self):
+    return str(self._left._name)+self._op+str(self._rite._name if isinstance(self._rite,Vec) else self._rite)
 
 ######################################################
 #
 #
 
-a = Frame(file="smalldata/iris/iris_wheader.csv")[0:4]
+a = Frame("a.",file="smalldata/iris/iris_wheader.csv")[0:4]
 
-print a[0].name     # Column header
+print a[0]._name    # Column header
 print a[0][2]       # column 0, row 2 value
-print a["sepal_len"][2] # Column 0, row 2 value
-print a[0]+2        # Add 2 to every element; broadcast a constant
-print a[0]+a[1]     # Add 2 columns; broadcast parallel add
-print sum(a["sepal_len"])/len(a[0])
+print a["a.sepal_len"][2] # Column 0, row 2 value
+print "EVAL a[0]+2 is: "   ,a[0]+2        # Add 2 to every element; broadcast a constant
+print "EVAL a[0]+a[1] is: ",a[0]+a[1]     # Add 2 columns; broadcast parallel add
+print "EVAL mean(a) is: ",sum(a["a.sepal_len"])/len(a[0])
 
+print "FAIL TEST"
 try:
   print a["Sepal_len"] # Error, mispelt column name
 except ValueError,e:
-  pass                          # Expected error
+  pass              # Expected error
 
-b = Frame(file="smalldata/iris/iris_wheader.csv")[0:4]
+print "READ b"
+b = Frame("b.",file="smalldata/iris/iris_wheader.csv")[0:4]
+print "EVAL c=a+b"
 c = a+b
-d = c+c+sum(a)
-e = c+a+1
-print e
-
+print "EVAL d=c+c+sum(a)"
+d = (c+c)+sum(a)
+print "EVAL c+a+1"
+e = c+(a+1)
+print "EVAL e is: ",e
