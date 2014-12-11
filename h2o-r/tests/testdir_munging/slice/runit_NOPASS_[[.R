31e6d9d0800d8e8ed2c658b16e796b9f3b59eb68
SpencerA <spnrpa@gmail.com>
2014-12-11 13:53:50 -0800

    guts of test went missing, putting them back

---
+++ h2o-r/tests/testdir_munging/slice/runit_NOPASS_[[.R
@@ -0,0 +1,27 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../h2o-runit.R')
+
+test.columndereference <- function(conn) {
+  Log.info('test column dereference')
+
+  hdf <- h2o.importFile(conn, locate('smalldata/jira/pub-180.csv'))
+  otherhdf <- h2o.importFile(conn, locate('smalldata/jira/v-11.csv'))
+
+  column <- 'colgroup2'
+
+  # get a single column out
+  expect_that( dim(hdf[['colgroup2']]), equals(c(12,1)) )
+  expect_that( dim(hdf[[column]]), equals(c(12,1)) )
+
+  # NULL if column name doesn't exist
+  expect_that( hdf[['col2group2']], equals(NULL))
+
+  # we can overwrite a column
+  hdf[['colgroup2']] <- hdf[['col2']]
+  ldf <- as.data.frame( hdf[[ column ]] )[,1]
+  expect_that(ldf, equals(c(2,4,6,11,3,4,6,11,2,4,6,11)) )
+
+  testEnd()
+}
+
+doTest("test column dereference and assignment", test.columndereference)
