2f28eb5235d5a32fdb8bab2e13b6db628561b51e
eric eckstrand <eric.eckstrand@gmail.com>
2014-12-12 16:23:46 -0800

    minor changes. still NOPASS

--- h2o-r/tests/testdir_docexamples/runit_NOPASS_Rdoc_as_factor.R
+++ h2o-r/tests/testdir_docexamples/runit_NOPASS_Rdoc_as_factor.R
@@ -5,11 +5,12 @@ test.rdocasfactor.golden <- function(H2Oserver) {
 	
 #Example from as.factor R example
 
-prosPath = system.file("extdata", "prostate.csv", package="h2o")
-prostate.hex = h2o.importFile(H2Oserver, path = prosPath)
-prostate.hex[,4]=as.factor(prostate.hex[,4])
-sum<- summary(prostate.hex[,4])
-is<- is.factor(prostate.hex[,4])
+prosPath <- system.file("extdata", "prostate.csv", package="h2o")
+prostate.hex <- h2o.importFile(H2Oserver, path = prosPath)
+prostate.hex[,4] <- as.factor(prostate.hex[,4])
+sum <- summary(prostate.hex[,4])
+is <- is.factor(prostate.hex[,4])
+print(is)
 
 Log.info("Print output from as.data.frame call")
 Log.info(paste("H2O Summary  :" ,sum))
