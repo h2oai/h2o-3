f221b9ab504bf1b506012ff8167c6545e43b5dd8
eric eckstrand <eric.eckstrand@gmail.com>
2014-12-14 17:44:18 -0800

    passing Rdoc tests

---
+++ h2o-r/tests/testdir_docexamples/runit_Rdoc_table.R
@@ -0,0 +1,21 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../h2o-runit.R')
+
+test.rdoc_table.golden <- function(H2Oserver) {
+
+prosPath <- system.file("extdata", "prostate.csv", package="h2o")
+prostate.hex <- h2o.importFile(H2Oserver, path = prosPath, key = "prostate.hex")
+summary(prostate.hex)
+
+# Counts of the ages of all patients
+head(h2o.table(prostate.hex[,3]))
+h2o.table(prostate.hex[,3])
+
+# Two-way table of ages (rows) and race (cols) of all patients
+head(h2o.table(prostate.hex[,c(3,4)]))
+h2o.table(prostate.hex[,c(3,4)])
+
+testEnd()
+}
+
+doTest("R Doc Table", test.rdoc_table.golden)
