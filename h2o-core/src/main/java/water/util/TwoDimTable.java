4aa359ca0518d74113dc28cc98124a1828b90a50
Arno Candel <arno.candel@gmail.com>
2014-12-18 12:27:25 -0800

    Modify TwoDimTable API to handle Double and String types, and serializes properly. Add JUnit tests.

--- h2o-core/src/main/java/water/util/TwoDimTable.java
+++ h2o-core/src/main/java/water/util/TwoDimTable.java
@@ -1,37 +1,71 @@
 package water.util;
 
-public class TwoDimTable {
+import water.Iced;
+
+public class TwoDimTable extends Iced {
   String description;
   String[] colNames;
   String[] colFormatStrings; //optional
-  String[] rowNames;
-  Object[][] values;
+  String[] rowHeaders;
+  String[][] strings;
+  Double[][] doubles;
 
-  /**
-   * Full two-dim table constructor
-   * @param colNames
-   * @param colFormatStrings
-   * @param rowNames
-   * @param values
-   */
-  public TwoDimTable(String description, String[] colNames, String[] colFormatStrings, String[] rowNames, Object[][] values) {
+  public TwoDimTable(String description, String[] colNames, String[] colFormatStrings, String[] rowHeaders, String[][] strings, Double[][] doubles) {
     if (description == null) throw new IllegalArgumentException("description is missing.");
     if (colNames == null) throw new IllegalArgumentException("colNames are missing.");
     if (colFormatStrings == null) throw new IllegalArgumentException("colFormatStrings are missing.");
-    if (rowNames == null) throw new IllegalArgumentException("rowNames are missing.");
-    if (values == null) throw new IllegalArgumentException("values are missing.");
-    if (values.length != rowNames.length)
-      throw new IllegalArgumentException("values must have the same length as rowNames: " + rowNames.length);
-    for (Object[] v : values)
+    if (rowHeaders == null) throw new IllegalArgumentException("rowHeaders are missing.");
+    if (strings == null) throw new IllegalArgumentException("string values are missing.");
+    if (strings.length != rowHeaders.length)
+      throw new IllegalArgumentException("string values must have the same length as rowHeaders: " + rowHeaders.length);
+    if (doubles == null) throw new IllegalArgumentException("double values are missing.");
+    if (doubles.length != rowHeaders.length)
+      throw new IllegalArgumentException("double values must have the same length as rowHeaders: " + rowHeaders.length);
+
+    for (String[] v : strings) {
+      if (v != null)
       if (v.length != colNames.length)
-        throw new IllegalArgumentException("Each entry in values must have the same length as colNames: " + colNames.length);
+        throw new IllegalArgumentException("Each entry in string values must have the same length as colNames: " + colNames.length);
+    }
+    for (Double[] v : doubles) {
+      if (v != null)
+      if (v.length != colNames.length)
+        throw new IllegalArgumentException("Each entry in string values must have the same length as colNames: " + colNames.length);
+    }
+
     if (colFormatStrings != null && colFormatStrings.length != colNames.length)
       throw new IllegalArgumentException("colFormatStrings must have the same length as colNames: " + colNames.length);
     this.description = description;
     this.colNames = colNames;
     this.colFormatStrings = colFormatStrings;
-    this.rowNames = rowNames;
-    this.values = values;
+    this.rowHeaders = rowHeaders;
+    this.strings = strings;
+    this.doubles = doubles;
+    checkConsistency();
+  }
+
+  void checkConsistency() {
+    for (int r=0; r<rowHeaders.length; ++r) {
+      for (int c=0; c<colNames.length; ++c) {
+        if (this.strings[r] != null && this.strings[r][c] != null && this.doubles[r] != null && this.doubles[r][c] != null)
+          throw new IllegalArgumentException("Cannot provide both a String and a Double at row idx " + r + " and column idx " + c + ".");
+      }
+    }
+  }
+
+  public Object get(int row, int col) {
+    return strings[row][col] != null ? strings[row][col]
+            : doubles[row][col] != null ? doubles[row][col] : null;
+  }
+
+  public void set(int row, int col, String s) {
+    strings[row][col] = s;
+    checkConsistency();
+  }
+
+  public void set(int row, int col, Double d) {
+    doubles[row][col] = d;
+    checkConsistency();
   }
 
   public String toString() {
@@ -40,16 +74,19 @@ public class TwoDimTable {
     StringBuilder sb = new StringBuilder();
 
     // First pass to figure out cell sizes
-    int maxRowNameLen = rowNames[0].length();
-    for (int r=1; r<rowNames.length; ++r) {
-      maxRowNameLen = Math.max(maxRowNameLen, rowNames[r].length());
+    int maxRowNameLen = rowHeaders[0].length();
+    for (int r=1; r<rowHeaders.length; ++r) {
+      maxRowNameLen = Math.max(maxRowNameLen, rowHeaders[r].length());
     }
     maxRowNameLen += pad;
     int[] colLen = new int[colNames.length];
     for (int c=0; c<colNames.length; ++c) {
       colLen[c] = colNames[c].length();
-      for (int r=0; r<rowNames.length; ++r) {
-        colLen[c] = Math.max(colLen[c], String.format(colFormatStrings[c], values[r][c]).length());
+      for (int r=0; r<rowHeaders.length; ++r) {
+        if (strings[r] != null && strings[r][c] != null)
+          colLen[c] = Math.max(colLen[c], String.format(colFormatStrings[c], strings[r][c]).length());
+        else if (doubles[r] != null && doubles[r][c] != null)
+          colLen[c] = Math.max(colLen[c], String.format(colFormatStrings[c], doubles[r][c]).length());
       }
       colLen[c] += pad;
     }
@@ -62,10 +99,16 @@ public class TwoDimTable {
     sb.append("\n");
 
     // Print table entries row by row
-    for (int r=0; r<rowNames.length; ++r) {
-      sb.append(String.format("%" + maxRowNameLen + "s", rowNames[r]));
-      for (int c=0; c<colNames.length; ++c)
-        sb.append(String.format("%" + colLen[c] + "s", String.format(colFormatStrings[c], values[r][c])));
+    for (int r=0; r<rowHeaders.length; ++r) {
+      sb.append(String.format("%" + maxRowNameLen + "s", rowHeaders[r]));
+      for (int c=0; c<colNames.length; ++c) {
+        if (strings[r] != null && strings[r][c] != null)
+          sb.append(String.format("%" + colLen[c] + "s", String.format(colFormatStrings[c], strings[r][c])));
+        else if (doubles[r] != null && doubles[r][c] != null)
+          sb.append(String.format("%" + colLen[c] + "s", String.format(colFormatStrings[c], doubles[r][c])));
+        else
+          sb.append(String.format("%" + colLen[c] + "s", "")); //empty field
+      }
       sb.append("\n");
     }
     return sb.toString();
--- h2o-core/src/test/java/water/TwoDimTableTest.java
+++ h2o-core/src/test/java/water/TwoDimTableTest.java
@@ -1,25 +1,169 @@
 package water;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import water.util.Log;
 import water.util.TwoDimTable;
 
-public class TwoDimTableTest {
+public class TwoDimTableTest extends TestUtil {
+  @BeforeClass() public static void setup() { stall_till_cloudsize(1); }
 
   @Test
-  public void run() {
+  public void run1() {
     TwoDimTable table = new TwoDimTable(
             "My foo bar table",
             new String[]{"DoubleValue", "S2", "My Terrible Percent Value"},
             new String[]{"%5.8e", "%s", "%5.8g %%"},
             new String[]{"First row", "R2", "Row #3", "Last row is here:"},
-            new Object[][]{
-                    new Object[]{new Double(1.123),            new String("One"),    new Float(3200034.00001f)},
-                    new Object[]{new Float(123.34),            new String("Two"),    new Float(1.0f)},
-                    new Object[]{new Double(5.123),            new String("Three"),  new Float(3234.00001f)},
-                    new Object[]{new Double(3.33420923423423), new String("FooBar"), new Double(3.40234234)}
-            });
+            new String[][]{
+                    new String[]{null,             "One",    null        },
+                    new String[]{null,             null,     null        },
+                    new String[]{null,             "Three",  null        },
+                    new String[]{null,             "FooBar", null        }
+            },
+            new Double[][]{
+                    new Double[]{1.123,            null,    3200034.00001},
+                    new Double[]{123.34,           null,    1.0          },
+                    new Double[]{null,             null,    3234.00001   },
+                    new Double[]{3.33420923423423, null,    3.40234234   }
+            }
+    );
     String ts = table.toString();
     assertTrue(ts.length() > 0);
+    Log.info(ts);
+
+    String json = new String(table.writeJSON(new AutoBuffer()).buf());
+    assertTrue(json.equals("{\"description\":\"My foo bar table\",\"colNames\":[\"DoubleValue\",\"S2\",\"My Terrible Percent Value\"],\"colFormatStrings\":[\"%5.8e\",\"%s\",\"%5.8g %%\"],\"rowHeaders\":[\"First row\",\"R2\",\"Row #3\",\"Last row is here:\"],\"strings\":[[null,\"One\",null],[null,null,null],[null,\"Three\",null],[null,\"FooBar\",null]],\"doubles\":[[1.123,null,3200034.00001],[123.34,null,1.0],[null,null,3234.00001],[3.33420923423423,null,3.40234234]]}"));
+    Log.info(json);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void run2() {
+    TwoDimTable table = new TwoDimTable(
+            "My foo bar table",
+            new String[]{"DoubleValue", "S2", "My Terrible Percent Value"},
+            new String[]{"%5.8e", "%s", "%5.8g %%"},
+            new String[]{"First row", "R2", "Row #3", "Last row is here:"},
+            new String[][]{
+                    new String[]{null,             "One",    null        },
+                    new String[]{null,             null,     null        },
+                    new String[]{null,             "Three",  "extra"     },
+                    new String[]{null,             "FooBar", null        }
+            },
+            new Double[][]{
+                    new Double[]{1.123,            null,    3200034.00001},
+                    new Double[]{123.34,           null,    1.0          },
+                    new Double[]{null,             null,    3234.00001   },
+                    new Double[]{3.33420923423423, null,    3.40234234   }
+            }
+    );
+  }
+
+  @Test
+  public void run3() {
+    TwoDimTable table = new TwoDimTable(
+            "My foo bar table",
+            new String[]{"DoubleValue", "S2", "My Terrible Percent Value"},
+            new String[]{"%f", "%s", "%f"},
+            new String[]{"First row", "R2", "Row #3", "Last row is here:"},
+            new String[][]{
+                    new String[]{null,             "One",    null        },
+                    new String[]{null,             null,     null        },
+                    new String[]{null,             "Three",  null        },
+                    new String[]{null,             "FooBar", null        }
+            },
+            new Double[][]{
+                    new Double[]{1.123,            null,    3200034.00001},
+                    new Double[]{123.34,           null,    1.0          },
+                    new Double[]{null,             null,    3234.00001   },
+                    new Double[]{3.33420923423423, null,    3.40234234   }
+            }
+    );
+    String ts = table.toString();
+    assertTrue(ts.length() > 0);
+    Log.info(ts);
+
+    String json = new String(table.writeJSON(new AutoBuffer()).buf());
+    assertTrue(json.equals("{\"description\":\"My foo bar table\",\"colNames\":[\"DoubleValue\",\"S2\",\"My Terrible Percent Value\"],\"colFormatStrings\":[\"%f\",\"%s\",\"%f\"],\"rowHeaders\":[\"First row\",\"R2\",\"Row #3\",\"Last row is here:\"],\"strings\":[[null,\"One\",null],[null,null,null],[null,\"Three\",null],[null,\"FooBar\",null]],\"doubles\":[[1.123,null,3200034.00001],[123.34,null,1.0],[null,null,3234.00001],[3.33420923423423,null,3.40234234]]}"));
+    Log.info(json);
+  }
+
+  @Test
+  public void run4() {
+    TwoDimTable table = new TwoDimTable(
+            "All numbers",
+            new String[]{"Num1", "Num2", "Num3"},
+            new String[]{"%f", "%f", "%f"},
+            new String[]{"R1", "R2", "R3", "R4"},
+            new String[4][],
+            new Double[][]{
+                    new Double[]{1.123,            3.42,    3200034.00001},
+                    new Double[]{123.34,           null,    1.0          },
+                    new Double[]{null,             null,    3234.00001   },
+                    new Double[]{3.33420923423423, 83.32,   3.40234234   }
+            }
+    );
+    String ts = table.toString();
+    assertTrue(ts.length() > 0);
+    Log.info(ts);
+
+    String json = new String(table.writeJSON(new AutoBuffer()).buf());
+    assertTrue(json.equals("{\"description\":\"All numbers\",\"colNames\":[\"Num1\",\"Num2\",\"Num3\"],\"colFormatStrings\":[\"%f\",\"%f\",\"%f\"],\"rowHeaders\":[\"R1\",\"R2\",\"R3\",\"R4\"],\"strings\":[null,null,null,null],\"doubles\":[[1.123,3.42,3200034.00001],[123.34,null,1.0],[null,null,3234.00001],[3.33420923423423,83.32,3.40234234]]}"));
+    Log.info(json);
+  }
+
+  @Test
+  public void run5() {
+    TwoDimTable table = new TwoDimTable(
+            "All strings",
+            new String[]{"S1", "S2", "S3", "S4"},
+            new String[]{"%s", "%s", "%s", "%s"},
+            new String[]{"R1", "R2", "R3", "R4"},
+            new String[][]{
+                    new String[]{"a", "b", "c", "d"},
+                    new String[]{"a", "b", "c", "d"},
+                    new String[]{"a", null, "c", "d"},
+                    new String[]{"a", "b", "c", null},
+            },
+            new Double[4][]
+    );
+    String ts = table.toString();
+    assertTrue(ts.length() > 0);
+    Log.info(ts);
+
+    String json = new String(table.writeJSON(new AutoBuffer()).buf());
+    assertTrue(json.equals("{\"description\":\"All strings\",\"colNames\":[\"S1\",\"S2\",\"S3\",\"S4\"],\"colFormatStrings\":[\"%s\",\"%s\",\"%s\",\"%s\"],\"rowHeaders\":[\"R1\",\"R2\",\"R3\",\"R4\"],\"strings\":[[\"a\",\"b\",\"c\",\"d\"],[\"a\",\"b\",\"c\",\"d\"],[\"a\",null,\"c\",\"d\"],[\"a\",\"b\",\"c\",null]],\"doubles\":[null,null,null,null]}"));
+    Log.info(json);
+  }
+
+  @Test
+  public void run6() {
+    TwoDimTable table = new TwoDimTable(
+            "All strings",
+            new String[]{"C0", "C1", "C2", "C3"},
+            new String[]{"%s", "%s", "%s", "%s"},
+            new String[]{"R0", "R1", "R2", "R3"},
+            new String[4][4],
+            new Double[4][4]
+    );
+    table.set(3, 3, "a33");
+    table.set(0, 1, "a01");
+    table.set(1, 2, 1.2);
+    table.set(0, 2, "a02");
+    table.set(3, 0, "a30");
+
+    String ts = table.toString();
+    assertTrue(ts.length() > 0);
+    Log.info(ts);
+
+    assertTrue(table.get(3, 0).equals("a30"));
+    assertTrue(table.get(1, 2).equals(1.2));
+    assertTrue(table.get(1, 3) == null);
+
+    String json = new String(table.writeJSON(new AutoBuffer()).buf());
+    assertTrue(json.equals("{\"description\":\"All strings\",\"colNames\":[\"C0\",\"C1\",\"C2\",\"C3\"],\"colFormatStrings\":[\"%s\",\"%s\",\"%s\",\"%s\"],\"rowHeaders\":[\"R0\",\"R1\",\"R2\",\"R3\"],\"strings\":[[null,\"a01\",\"a02\",null],[null,null,null,null],[null,null,null,null],[\"a30\",null,null,\"a33\"]],\"doubles\":[[null,null,null,null],[null,null,1.2,null],[null,null,null,null],[null,null,null,null]]}"));
+    Log.info(json);
+
   }
 }
