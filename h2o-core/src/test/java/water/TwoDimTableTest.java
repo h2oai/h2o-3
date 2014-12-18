4d89628474fb062bd1c88be0b89ba7944aceee96
Arno Candel <arno.candel@gmail.com>
2014-12-17 16:36:50 -0800

    Cosmetics.

--- h2o-core/src/test/java/water/TwoDimTableTest.java
+++ h2o-core/src/test/java/water/TwoDimTableTest.java
@@ -12,12 +12,13 @@ public class TwoDimTableTest {
             "My foo bar table",
             new String[]{"DoubleValue", "S2", "My Terrible Percent Value"},
             new String[]{"%5.8e", "%s", "%5.8g %%"},
-            new String[]{"RowName1", "R2", "R3", "R56"},
+            new String[]{"First row", "R2", "Row #3", "Last row is here:"},
             new Object[][]{
-                    new Object[]{new Double(1.123), new String("Two"), new Float(3200034.00001f)},
-                    new Object[]{new Float(123.34), new String("adfasdfTwo"), new Float(4)},
-                    new Object[]{new Double(1.123), new String("Two"), new Float(3200034.00001f)},
-                    new Object[]{new Double(3.33420923423423), new String("asdStsdfa"), new Double(3.4f)}});
+                    new Object[]{new Double(1.123),            new String("One"),    new Float(3200034.00001f)},
+                    new Object[]{new Float(123.34),            new String("Two"),    new Float(1.0f)},
+                    new Object[]{new Double(5.123),            new String("Three"),  new Float(3234.00001f)},
+                    new Object[]{new Double(3.33420923423423), new String("FooBar"), new Double(3.40234234)}
+            });
     String ts = table.toString();
     assertTrue(ts.length() > 0);
   }
