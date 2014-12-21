e7436d67f8411b127f57c1684b0523a1231deec9
Kevin Normoyle <kevin@0xdata.com>
2014-12-20 16:42:44 -0800

    clean up some kmeans shared routines

--- py2/h2o_kmeans.py
+++ py2/h2o_kmeans.py
@@ -41,9 +41,8 @@ class KMeansObj(OutputObj):
         rows = self.rows # [78, 5, 41, 76]
         model_category = self.model_category # Clustering
         iters = self.iters # 11.0
-        domains = self.domains # [None, None, None, None, None, None, None, None, None, None, None, None, None, None]
+        domains = self.domains 
         names = self.names 
-        # [u'STR', u'OBS', u'AGMT', u'FNDX', u'HIGD', u'DEG', u'CHK', u'AGP1', u'AGMN', u'NLV', u'LIV', u'WT', u'AGLP', u'MST']
         ncats = self.ncats # 0
         clusters = self.clusters # [ 4 lists of centers ]
         withinmse = self.withinmse
@@ -81,109 +80,87 @@ class KMeansObj(OutputObj):
                 if math.isnan(float(n)):
                     raise Exception("cluster", i, "has NaN:", n, "center:", c)
 
-        # shouldn't have any errors
-        check_sandbox_for_errors()
-
         # create a tuple for each cluster result, then sort by rows for easy comparison
         # maybe should sort by centers?
         # put a cluster index in there too, (leftmost) so we don't lose track
-        tuples = zip(range(len(clusters)), withinmse, rows, clusters)
-        self.tuplesSorted = sorted(tuples, key=itemgetter(3))
+        tuples = zip(range(len(clusters)), clusters, rows, withinmse)
+        # can we sort on the sum of the centers?
+        self.tuplesSorted = sorted(tuples, key=lambda tup: sum(tup[1]))
 
-        # undo for printing what the caller will see
-        ids, withinmse, rows, clusters = zip(*self.tuplesSorted)
         print "iters:", iters
-        print "ids:", ids
-        print "withinmse:", withinmse
-        print "rows:", rows
+        # undo for printing what the caller will see
+        ids, clusters, rows, withinmse = zip(*self.tuplesSorted)
         for i,c in enumerate(clusters):
             print "cluster id %s (2 places):" % ids[i], h2o_util.twoDecimals(c)
-        
+            print "rows_per_cluster[%s]: " % i, rows[i]
+            print "withinmse[%s]: " % i, withinmse[i]
+            print "rows[%s]:" % i, rows[i]
+
         print "KMeansObj created for:", "???"# vars(self)
 
+        # shouldn't have any errors
+        check_sandbox_for_errors()
 
-def bigCheckResults(self, kmeans, csvPathname, parseResult, predictKey, **kwargs):
-    simpleCheckKMeans(self, kmeans, **kwargs)
-    # can't use inspect on a model key? now?
-    model = kmeans['model']
-    model_key = model['_key']
-    centers = model['centers']
-    size = model["size"]
-    cluster_variances = model["within_cluster_variances"]
-    error = model["total_within_SS"]
-    iterations = model["iterations"]
-    standardized = model["standardized"]
-    max_iter = model["max_iter"]
-    kmeansResult = kmeans
 
+# This is all messed up now...really want it to just do predict and compare histogram, and also do the compare results to expected
+# will have to fix all this (and don't overlap with simpleCheck above)
+def bigCheckResults(kmeansObj, kmeans, csvPathname, parseResult, predictKey, **kwargs):
     predictResult = h2o_nodes.nodes[0].generate_predictions(data_key=parseResult['destination_key'], model_key=model_key, destination_key=predictKey)
     summaryResult = h2o_nodes.nodes[0].summary_page(key=predictKey, timeoutSecs=120)
     hcnt = summaryResult['summaries'][0]['hcnt'] # histogram
     rows_per_cluster = hcnt
+
     # FIX! does the cluster order/naming match, compared to cluster variances
     sqr_error_per_cluster = cluster_variances
     
-    tupleResultList = []
-    print "\nerror: ", error
-
     if (len(centers)!=len(rows_per_cluster) or len(centers)!=len(sqr_error_per_cluster)):
         raise Exception("centers, rows_per_cluster, sqr_error_per_cluster should all be same length %s, %s, %s" % \
             (len(centers), len(rows_per_cluster), len(sqr_error_per_cluster)))
             
     print "Did iterations: %s  given max_iter: %s" % (iterations, max_iter)
-    for i,c in enumerate(centers):
-        print "\ncenters[%s]: " % i, [round(c,2) for c in centers[i]]
-        print "rows_per_cluster[%s]: " % i, rows_per_cluster[i]
-        print "sqr_error_per_cluster[%s]: " % i, sqr_error_per_cluster[i]
-        print "size[%s]:" % i, size[i]
-        tupleResultList.append( (centers[i], rows_per_cluster[i], sqr_error_per_cluster[i]) )
-
-    return (centers, tupleResultList)
+    # shouldn't have to return a tuplesList from here any more
 
 
-# list of tuples: center, rows, sqr_error
-# expected = [ # tupleResultList is returned by bigCheckResults like this
-#       ([-2.2824436059344264, -0.9572469619836067], 61, 71.04484889371177),
-#       ([0.04072444664179102, 1.738305108029851], 67, 118.83608173427331),
-#       ([2.7300104405999996, -1.16148755108], 50, 68.67496427685141)
-# ]
-# delta is a tuple of multipliers against the tupleResult for abs delta
-# allowedDelta = (0.01, 0.1, 0.01)
-def compareResultsToExpected(self, tupleResultList, expected=None, allowedDelta=None, allowError=False, allowRowError=False, trial=0):
-    # sort the tuple list by center for the comparison. (this will be visible to the caller?)
-    from operator import itemgetter
-    tupleResultList.sort(key=itemgetter(0))
+def compareResultsToExpected(tupleResultList, expected=None, allowedDelta=None, allowError=False, allowRowError=False):
+    # the expected/tupleResultlist should be sorted already by center sum, but just in case...
+    tupleResultList.sort(key=lambda tup: sum(tup[1]))
 
     if expected is not None:
         # sort expected, just in case, for the comparison
-        expected.sort(key=itemgetter(0))
-        print "\nTrial #%d Expected:" % trial
+        expected.sort(key=lambda tup: sum(tup[1]))
+        print "\nExpected:"
         for e in expected:
             print e
 
     # now compare to expected, with some delta allowed
-    print "\nTrial #%d Actual:" % trial
+    print "\nActual:"
     for t in tupleResultList:
         print t, "," # so can cut and paste and put results in an expected = [..] list
 
     if expected is not None and not allowError: # allowedDelta must exist if expected exists
-        for i, (expCenter, expRows, expError)  in enumerate(expected):
-            (actCenter, actRows, actError) = tupleResultList[i]
+        for i, (expCid, expCenter, expRows, expError)  in enumerate(expected):
+            (actCid, actCenter, actRows, actError) = tupleResultList[i]
 
             for (a,b) in zip(expCenter, actCenter): # compare list of floats
                 absAllowedDelta = abs(allowedDelta[0] * a)
-                self.assertAlmostEqual(a, b, delta=absAllowedDelta,
-                    msg="Trial %d Center value expected: %s actual: %s delta > %s" % (trial, a, b, absAllowedDelta))
+                absAllowedDelta = max(absAllowedDelta, allowedDelta[0]) # comparing to 0?
+                h2o_util.assertApproxEqual(a, b, tol=absAllowedDelta,
+                    msg="Center value expected: %s actual: %s delta > %s" % (a, b, absAllowedDelta))
 
-            if not allowRowError: # allow error in row count? 
+            if not allowRowError and expRows: # allow error in row count? 
                 absAllowedDelta = abs(allowedDelta[1] * expRows)
-                self.assertAlmostEqual(expRows, actRows, delta=absAllowedDelta,
-                    msg="Trial %d Rows expected: %s actual: %s delta > %s" % (trial, expRows, actRows, absAllowedDelta))
+                absAllowedDelta = max(absAllowedDelta, allowedDelta[1]) # comparing to 0?
+                h2o_util.assertApproxEqual(expRows, actRows, tol=absAllowedDelta,
+                    msg="Rows expected: %s actual: %s delta > %s" % (expRows, actRows, absAllowedDelta))
 
-            # fix, we don't compare the actual error # (what is it?)
+            if not allowRowError and expError: # allow error in row count? 
+                absAllowedDelta = abs(allowedDelta[2] * expError)
+                absAllowedDelta = max(absAllowedDelta, allowedDelta[2]) # comparing to 0?
+                h2o_util.assertApproxEqual(expRows, actRows, tol=absAllowedDelta,
+                    msg="Error expected: %s actual: %s delta > %s" % (expError, actError, absAllowedDelta))
 
 # just print info on the distribution
-def showClusterDistribution(self, tupleResultList, expected=None, allowedDelta=None, allowError=False, trial=0):
+def showClusterDistribution(tupleResultList, expected=None, allowedDelta=None, allowError=False, trial=0):
     # sort the tuple list by center for the comparison. (this will be visible to the caller?)
     from operator import itemgetter
     if expected is not None:
@@ -193,12 +170,12 @@ def showClusterDistribution(self, tupleResultList, expected=None, allowedDelta=N
         totalRows = 0
         totalError = 0
         print "\nExpected distribution, rows and error:"
-        for i, (expCenter, expRows, expError)  in enumerate(expected):
+        for i, (expCid, expCenter, expRows, expError)  in enumerate(expected):
             totalRows += expRows
             totalError += expError
         # now go thru again and print percentages
         print "totalRows:", totalRows, "totalError:", totalError
-        for i, (expCenter, expRows, expError)  in enumerate(expected):
+        for i, (expCid, expCenter, expRows, expError)  in enumerate(expected):
             print expCenter, "pctRows: %0.2f" % (expRows/(totalRows+0.0)), "pctError: %0.2f" % (expError/(totalError+0.0))
 
     if tupleResultList is not None:
@@ -206,12 +183,12 @@ def showClusterDistribution(self, tupleResultList, expected=None, allowedDelta=N
         totalRows = 0
         totalError = 0
         print "\nActual distribution, rows and error:"
-        for i, (actCenter, actRows, actError)  in enumerate(tupleResultList):
+        for i, (actCid, actCenter, actRows, actError)  in enumerate(tupleResultList):
             totalRows += actRows
             totalError += actError
         # now go thru again and print percentages
         print "totalRows:", totalRows, "totalError:", totalError
-        for i, (actCenter, actRows, actError)  in enumerate(tupleResultList):
+        for i, (actCid, actCenter, actRows, actError)  in enumerate(tupleResultList):
             print actCenter, "pctRows: %0.2f" % (actRows/(totalRows+0.0)), "pctError: %0.2f" % (actError/(totalError+0.0))
 
 
