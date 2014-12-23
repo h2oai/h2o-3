d9e4fc9a84314983a2b0395822e7b2bd1f285212
Brandon Hill <brandon@0xdata.com>
2014-12-22 19:54:39 -0800

    Test should only be run as a part of runit_large tests.  New name ensures that.

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_mnist_manyCols_large.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_mnist_manyCols_large.R
@@ -6,8 +6,18 @@ if(!"R.utils" %in% rownames(installed.packages())) install.packages("R.utils")
 require(R.utils)
 
 test.mnist.manyCols <- function(conn) {
+   fPath = tryCatch({
+      locate("bigdata/laptop/mnist/train.csv.gz")
+    }, warning= function(w) {
+      print("File bigdata/laptop/mnist/train.csv.gz could not be found.  Please run ./gradlew syncBigdataLaptop (or gradlew.bat syncBigdataLaptop for Windows) to retrieve the file.")
+    }, error= function(e) {
+      print("File bigdata/laptop/mnist/train.csv.gz could not be found.  Please run ./gradlew syncBigdataLaptop (or gradlew.bat syncBigdataLaptop for Windows) to retrieve the file.")
+    }, finally = {
+      testEnd()
+    })
+
   Log.info("Importing mnist train data...\n")
-  train.hex <- h2o.uploadFile(conn, locate("bigdata/laptop/mnist/train.csv.gz"), "train.hex")
+  train.hex <- h2o.uploadFile(conn, fPath, "train.hex")
   Log.info("Check that tail works...")
   tail(train.hex)
   tail_ <- tail(train.hex)
