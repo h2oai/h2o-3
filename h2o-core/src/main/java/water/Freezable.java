6d9be187099f38264272eabf7ca250849c100908
mmalohlava <michal.malohlava@gmail.com>
2014-12-28 21:39:35 +0100

    Fix compile time error.
    ECJ complained about clone method collision because of type erasure.
    Clone method signature was changed to expected one.

--- h2o-core/src/main/java/water/Freezable.java
+++ h2o-core/src/main/java/water/Freezable.java
@@ -46,10 +46,9 @@ public interface Freezable extends Cloneable {
    *  Useful as an array index.
    *  @return Small integer, unique per-type */
   int frozenType();
-  /** Clone, without the annoying exception.
-   *  @param <T> Type of returned object
+  /** Make clone public, but without the annoying exception.
    *  @return Returns this object cloned. */
-  public <T extends Freezable> T clone();
+  public Object clone();
   /** Implementation of the {@link Iced} serialization protocol, only called by
    *  auto-genned code.  Not intended to be called by user code.  Override only
    *  for custom Iced serializers.
