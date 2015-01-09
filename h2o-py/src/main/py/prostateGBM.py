c6a89cea64d97e26649a6eb61c63d1acb15bbead
cliffclick <cliffc@acm.org>
2015-01-09 12:21:15 -0800

    test/train split from python
    Handle slice of assignment; remove some dead vars
    switch 'df.drop' for 'del df[]' pythonic
    support runif, bulk <,>=

--- h2o-py/src/main/py/prostateGBM.py
+++ h2o-py/src/main/py/prostateGBM.py
@@ -13,21 +13,26 @@ df = H2OFrame(remote_fname="smalldata/logreg/prostate.csv")
 print df.describe()
 
 # Remove ID from training frame
-train = df.drop("ID")
+del df['ID']
 
 # For VOL & GLEASON, a zero really means "missing"
-vol = train['VOL']
+vol = df['VOL']
 vol[vol==0] = None
-gle = train['GLEASON']
+gle = df['GLEASON']
 gle[gle==0] = None
 
 # Convert CAPSULE to a logical factor
-train['CAPSULE'] = train['CAPSULE'].asfactor()
+df['CAPSULE'] = df['CAPSULE'].asfactor()
+
+r = vol.runif()
+train = df[r< 0.8]
+test  = df[r>=0.8]
 
 # See that the data is ready
 print train.describe()
+print test .describe()
 
 # Run GBM
-gbm = H2OGBM(dataset=train,x="CAPSULE",ntrees=50,learn_rate=0.1)
+gbm = H2OGBM(dataset=train,x="CAPSULE",validation_dataset=test,ntrees=50,max_depth=5,learn_rate=0.1)
 print gbm._model
 print gbm.metrics()
