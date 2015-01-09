194778cd4e7f74594dfe1a389b65a42d340ea136
anqi <anqi@0xdata.com>
2015-01-08 17:10:58 -0800

    Add golden tests for standardized and non-standardized training data in kmeans model building.

--- h2o-r/tests/testdir_golden/runit_km2_2_golden.R
+++ h2o-r/tests/testdir_golden/runit_km2_2_golden.R
@@ -2,20 +2,27 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source('../h2o-runit.R')
 
 # Compare within-cluster sum of squared error
-test.kmvanilla.golden <- function(H2Oserver) {
+test.kmstand.golden <- function(H2Oserver) {
   # Import data: 
   Log.info("Importing iris.csv data...") 
   irisH2O <- h2o.uploadFile(H2Oserver, locate("smalldata/iris/iris2.csv"), key = "irisH2O")
   irisR <- read.csv(locate("smalldata/iris/iris2.csv"), header = TRUE)
   startIdx <- sort(sample(1:nrow(irisR), 3))
   
-  Log.info("Initial cluster centers:"); print(irisR[startIdx,1:4])
-  fitR <- kmeans(irisR[,1:4], centers = irisR[startIdx,1:4], iter.max = 1000, algorithm = "Lloyd")
-  fitH2O <- h2o.kmeans(irisH2O[,1:4], init = irisH2O[startIdx,1:4], standardize = FALSE)
+  # H2O automatically standardizes data (de-mean and scale so standard deviation is one)
+  irisScale = scale(irisR[,1:4], center = TRUE, scale = TRUE)
+  Log.info("Initial cluster centers:"); print(irisScale[startIdx,])
+  fitR <- kmeans(irisScale, centers = irisScale[startIdx,], iter.max = 1000, algorithm = "Lloyd")
+  fitH2O <- h2o.kmeans(irisH2O[,1:4], init = irisH2O[startIdx,1:4], standardize = TRUE)
   
   Log.info("R Final Clusters:"); print(fitR$centers)
-  Log.info("H2O Final Clusters:"); print(fitH2O@model$centers)
-  expect_equivalent(fitH2O@model$centers, fitR$centers)
+  Log.info("H2O Final Clusters (de-standardized):"); print(fitH2O@model$centers)
+  
+  # De-standardize R final clusters for comparison with H2O
+  avg <- apply(irisR[,1:4], 2, mean)
+  std <- apply(irisR[,1:4], 2, sd)
+  fitR_centstd <- sweep(sweep(fitR$centers, 2, std, '*'), 2, avg, "+")
+  expect_equivalent(fitH2O@model$centers, fitR_centstd)
   
   wmseR <- sort.int(fitR$withinss/fitR$size)
   wmseH2O <- sort.int(fitH2O@model$withinmse)
@@ -39,4 +46,4 @@ test.kmvanilla.golden <- function(H2Oserver) {
   testEnd()
 }
 
-doTest("KMeans Test: Golden Kmeans - Iris without Standardization", test.kmvanilla.golden)
+doTest("KMeans Test: Golden Kmeans - Iris with Standardization", test.kmstand.golden)
