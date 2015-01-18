df222484f4bd4a48b7e1ca896b0e0c89bcf534b2
Tom Kraljevic <tomk@tomk.net>
2015-01-17 18:24:13 -0800

    Add various documentation links to build page.

--- make-dist.sh
+++ make-dist.sh
@@ -12,11 +12,12 @@ set -x
 TOPDIR=$(cd `dirname $0` && pwd)
 IMAGEDIR=${TOPDIR}/h2o-dist/tmp/h2o-dev-${PROJECT_VERSION}
 
-# Create target dir, which is uploaded to s3.
-cd $TOPDIR
+# Remove any previously created build directories.
 rm -fr target
-mkdir target
 rm -fr h2o-dist/tmp
+
+# Create image dir, which contains what is in the zip file.
+cd $TOPDIR
 mkdir -p $IMAGEDIR
 cp build/h2o.jar $IMAGEDIR
 mkdir $IMAGEDIR/R
@@ -24,8 +25,23 @@ cp h2o-r/R/src/contrib/h2o_${PROJECT_VERSION}.tar.gz $IMAGEDIR/R
 
 cd $IMAGEDIR/..
 zip -r h2o-dev-${PROJECT_VERSION}.zip h2o-dev-${PROJECT_VERSION}
-mv h2o-dev-${PROJECT_VERSION}.zip ${TOPDIR}/target
+
+# Create target dir, which is uploaded to s3.
 cd $TOPDIR
+mkdir target
+
+# Add zip file to target.
+mv $IMAGEDIR/../h2o-dev-${PROJECT_VERSION}.zip ${TOPDIR}/target
+
+# Add documentation to target.
+mkdir target/docs-website
+mkdir target/docs-website/h2o-r
+mkdir target/docs-website/h2o-core
+mkdir target/docs-website/h2o-algos
+cp -rp build/docs/REST target/docs-website
+cp -p h2o-r/R/h2o_package.pdf target/docs-website/h2o-r
+cp -rp h2o-core/build/docs/javadoc target/docs-website/h2o-core
+cp -rp h2o-algos/build/docs/javadoc target/docs-website/h2o-algos
 
 # Create index file.
 cat h2o-dist/index.html | sed -e "s/SUBST_PROJECT_VERSION/${PROJECT_VERSION}/g" | sed -e "s/SUBST_LAST_COMMIT_HASH/${LAST_COMMIT_HASH}/g" > target/index.html
