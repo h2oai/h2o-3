f597d4958ff2200f68e2cead31f3a184bfcaa5f2
Arno Candel <arno.candel@gmail.com>
2015-01-19 18:52:30 -0800

    Make binomial response "0" and "1" by default - allows testing of toEnum() conversion and back, etc.

--- h2o-core/src/main/java/water/fvec/FrameCreator.java
+++ h2o-core/src/main/java/water/fvec/FrameCreator.java
@@ -47,7 +47,7 @@ public class FrameCreator extends H2O.H2OCountedCompleter {
         _domain[0] = _createFrame.response_factors == 1 ? null : new String[_createFrame.response_factors];
         if (_domain[0] != null) {
           for (int i = 0; i < _domain[0].length; ++i) {
-            _domain[0][i] = "resp." + i;
+            _domain[0][i] = new Integer(i).toString();
           }
         }
       }
