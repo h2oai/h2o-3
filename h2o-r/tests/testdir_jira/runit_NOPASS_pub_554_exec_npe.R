874cf2e189f793d93b0df12d6026619f560c2d29
Sebastian Vidrio <sebastian@0xdata.com>
2015-01-22 18:34:42 -0800

    testdir_jira Runit updates

--- h2o-r/tests/testdir_jira/runit_NOPASS_pub_554_exec_npe.R
+++ h2o-r/tests/testdir_jira/runit_NOPASS_pub_554_exec_npe.R
@@ -6,8 +6,8 @@ test.ifce<- function(conn) {
   r.hex <- as.h2o(conn, iris)
   #ifelse(1, r.hex, r.hex + 1)[1,2]
   r.hex[2+4,-4] + 5
-  h2o.exec(ifelse(1, r.hex, (r.hex + 1))[1,2])
-  h2o.exec(r.hex[2+4,-4] + 5)
+  ifelse(1, r.hex, (r.hex + 1))[1,2])
+  r.hex[2+4,-4] + 5)
 
   testEnd()
 }
