3219c2afaa72eaaecca5ec108d25bb3820e567c0
SpencerA <spnrpa@gmail.com>
2015-01-26 13:04:27 -0800

    PEP8 stuff

--- h2o-py/src/main/py/h2o/model/h2o_km_builder.py
+++ h2o-py/src/main/py/h2o/model/h2o_km_builder.py
@@ -4,6 +4,7 @@ A KMeans model builder.
 
 from h2o_model_builder import H2OModelBuilder
 
+
 class H2OKMeansBuilder(H2OModelBuilder):
     """
     Build a new KMeans model.
@@ -23,17 +24,19 @@ class H2OKMeansBuilder(H2OModelBuilder):
     """
     
     def __init__(self, x=None, k=5, training_frame=None, key=None, max_iterations=1000,
-                 standardize=True, init=("Furthest","Random","PlusPlus"), seed=None):
+                 standardize=True, init=("Furthest", "Random", "PlusPlus"), seed=None):
         """
         Instantiate a KMeansBuilder.
         :param x: Columns to use for clustering (may be indices or strings).
         :param k: Number of clusters (must be between 1 and 1e7 inclusive).
         :param training_frame: An object of type H2OFrame.
         :param key: The output name of the model.
-        :param max_iterations: The maximum number of iterations allowed (must be between 0 and 1e6 inclusive).
+        :param max_iterations: The maximum number of iterations allowed
+                               (must be between 0 and 1e6 inclusive).
         :param standardize: Whether data should be standardized before clustering.
-        :param init: How to select initial set of cluster points. Random for random initialization, Furthest 
-                     for initialization at furthest point from successive centers, and PlusPlus for k-means++.
+        :param init: How to select initial set of cluster points. Random for random
+                     initialization, Furthest for initialization at furthest point from
+                     successive centers, and PlusPlus for k-means++.
         :param seed: A random seed.
         :return: A new KMeansBuilder.
         """
