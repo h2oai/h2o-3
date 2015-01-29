8fc9684bc9ce3bf3065c2f645f478dd30f25a346
Cliff Click <cliffc@acm.org>
2015-01-29 13:50:42 -0800

    Update WorkFlowTest
    Frame.toString handles NAs better

--- h2o-algos/src/test/java/hex/example/WorkFlowTest.java
+++ h2o-algos/src/test/java/hex/example/WorkFlowTest.java
@@ -3,6 +3,8 @@ package hex.example;
 import hex.splitframe.ShuffleSplitFrame;
 import hex.tree.gbm.GBM;
 import hex.tree.gbm.GBMModel;
+import hex.glm.GLM;
+import hex.glm.GLMModel;
 import java.io.File;
 import java.util.Arrays;
 import org.junit.BeforeClass;
@@ -13,12 +15,12 @@ import water.fvec.*;
 import water.parser.ParseDataset;
 import org.joda.time.*;
 
-@Ignore("Test DS end-to-end workflow; not intended as a junit yet")
+//@Ignore("Test DS end-to-end workflow; not intended as a junit yet")
 public class WorkFlowTest extends TestUtil {
   @BeforeClass() public static void setup() { stall_till_cloudsize(1); }
 
   // Test DS end-to-end workflow on a small dataset
-  @Test 
+  @Test
   public void testWorkFlowSmall() {
     String[] files = { "bigdata/laptop/citibike-nyc/2013-10.csv" };
     testWorkFlow(files);
@@ -34,12 +36,12 @@ public class WorkFlowTest extends TestUtil {
       "bigdata/laptop/citibike-nyc/2013-10.csv",
       "bigdata/laptop/citibike-nyc/2013-11.csv",
       "bigdata/laptop/citibike-nyc/2013-12.csv",
-      "bigdata/laptop/citibike-nyc/2013-01.csv",
-      "bigdata/laptop/citibike-nyc/2013-02.csv",
-      "bigdata/laptop/citibike-nyc/2013-03.csv",
-      "bigdata/laptop/citibike-nyc/2013-04.csv",
-      "bigdata/laptop/citibike-nyc/2013-05.csv",
-      "bigdata/laptop/citibike-nyc/2013-06.csv",
+      "bigdata/laptop/citibike-nyc/2014-01.csv",
+      "bigdata/laptop/citibike-nyc/2014-02.csv",
+      "bigdata/laptop/citibike-nyc/2014-03.csv",
+      "bigdata/laptop/citibike-nyc/2014-04.csv",
+      "bigdata/laptop/citibike-nyc/2014-05.csv",
+      "bigdata/laptop/citibike-nyc/2014-06.csv",
       "bigdata/laptop/citibike-nyc/2014-07.csv",
       "bigdata/laptop/citibike-nyc/2014-08.csv" };
     testWorkFlow(files);
@@ -54,77 +56,113 @@ public class WorkFlowTest extends TestUtil {
   // If files are missing, silently fail - as the files are big and this is not
   // yet a junit test
   private void testWorkFlow(String[] files) {
+    try {
+      Scope.enter();
 
-    // 1- Load datasets
-    Frame data = load_files("data.hex",files);
-    if( data == null ) return;
+      // 1- Load datasets
+      Frame data = load_files("data.hex",files);
+      if( data == null ) return;
+  
+  
+      // 2- light data munging
+  
+      // Drop station id, as it's redundant with the name
+      data.remove("start station id").remove();
+      data.remove(  "end station id").remove();
 
+      // Drop bike id, as it should not add any improvement to predictions
+      // CNC - currently raising r2 from .66 to .70, so leaving in
+      data.remove("bikeid").remove();
 
-    // 2- light data munging
+      // Drop long/lat as the station name is already a unique identifier
+      // CNC - currently raising r2 from .62 to .70, so leaving in
+      data.remove("start station latitude" ).remove();
+      data.remove(  "end station latitude" ).remove();
+      data.remove("start station longitude").remove();
+      data.remove(  "end station longitude").remove();
+  
+      // Convert stop & start times to: Month, Weekday, Hour
+      Vec startime = data.remove("starttime");
+      data.add(new TimeSplit().doIt(startime,"s"));
+      startime.remove();
+  
+      Vec stoptime = data.remove("stoptime");
+      data.add(new TimeSplit().doIt(stoptime,"e"));
+      stoptime.remove();
 
-    // Drop station id, as it's redundant with the name
-    data.remove("start station id").remove();
-    data.remove(  "end station id").remove();
+      // Split into train, test and holdout sets
+      Key[] keys = new Key[]{Key.make("train.hex"),Key.make("test.hex"),Key.make("hold.hex")};
+      double[] ratios = new double[]{0.6,0.3,0.1};
+      Frame[] frs = ShuffleSplitFrame.shuffleSplitFrame(data,keys,ratios,1234567689L);
+      Frame train = frs[0];
+      Frame test  = frs[1];
+      Frame hold  = frs[2];
+      data.remove();
+  
+      System.out.println(train);
+      System.out.println(test);
+  
+  
+      // 3- build model on train; using test as validation
+      //// Gradiant Boosting Method
+      //GBMModel.GBMParameters gbm_parms = new GBMModel.GBMParameters();
+      //// base Model.Parameters
+      //gbm_parms._train = train._key;
+      //gbm_parms._valid = test ._key;
+      //gbm_parms._score_each_iteration = false; // default is false
+      //// SupervisedModel.Parameters
+      //gbm_parms._response_column = "tripduration";
+      //gbm_parms._convert_to_enum = false; // regression
+      //
+      //// SharedTreeModel.Parameters
+      //gbm_parms._ntrees = 200;        // default is 50
+      //gbm_parms._max_depth = 7;       // default
+      //gbm_parms._min_rows = 10;       // default
+      //gbm_parms._nbins = 20;          // default
+      //
+      //// GBMModel.Parameters
+      //gbm_parms._loss = GBMModel.GBMParameters.Family.AUTO; // default
+      //gbm_parms._learn_rate = 0.1f;   // default
+      //
+      //Job<GBMModel> job = new GBM(gbm_parms).trainModel();
+      //GBMModel gbm = job.get();
+      //job.remove();
+      //
+      //System.out.println(Arrays.toString(gbm._output._mse_train));
+      //System.out.println(Arrays.toString(gbm._output._mse_valid));
+  
+      // Score on holdout set & report
+      
 
-    // Convert stop & start times to: Month, Weekday, Hour
-    Vec startime = data.remove("starttime");
-    data.add(new TimeSplit().doIt(startime,"s"));
-    startime.remove();
-
-    Vec stoptime = data.remove("stoptime");
-    data.add(new TimeSplit().doIt(stoptime,"e"));
-    stoptime.remove();
-
-    // Split into train, test and holdout sets
-    Key[] keys = new Key[]{Key.make("train.hex"),Key.make("test.hex"),Key.make("hold.hex")};
-    double[] ratios = new double[]{0.6,0.3,0.1};
-    Frame[] frs = ShuffleSplitFrame.shuffleSplitFrame(data,keys,ratios,1234567689L);
-    Frame train = frs[0];
-    Frame test  = frs[1];
-    Frame hold  = frs[2];
-    data.remove();
-
-    System.out.println(train);
-    System.out.println(test);
-
-    // 3- build model on train; using test as validation
-    GBMModel.GBMParameters gbm_parms = new GBMModel.GBMParameters();
-    // base Model.Parameters
-    gbm_parms._train = train._key;
-    gbm_parms._valid = test ._key;
-    gbm_parms._score_each_iteration = false; // default is false
-    // SupervisedModel.Parameters
-    gbm_parms._response_column = "tripduration";
-    gbm_parms._convert_to_enum = false; // regression
-
-    // SharedTreeModel.Parameters
-    gbm_parms._ntrees = 200;        // default is 50
-    gbm_parms._max_depth = 7;       // default
-    gbm_parms._min_rows = 10;       // default
-    gbm_parms._nbins = 20;          // default
-
-    // GBMModel.Parameters
-    gbm_parms._loss = GBMModel.GBMParameters.Family.AUTO; // default
-    gbm_parms._learn_rate = 0.1f;   // default
-
-    Job<GBMModel> job = new GBM(gbm_parms).trainModel();
-    GBMModel gbm = job.get();
-    job.remove();
-
-    System.out.println(Arrays.toString(gbm._output._mse_train));
-    System.out.println(Arrays.toString(gbm._output._mse_valid));
-
-    // Score on holdout set & report
-    
-
-
-
-    // Cleanup
-    gbm.remove();
-
-    train.remove();
-    test .remove();
-    hold .remove();
+      // Build a GLM model also
+      GLMModel.GLMParameters glm_parms = new GLMModel.GLMParameters(GLMModel.GLMParameters.Family.gaussian);
+      // base Model.Parameters
+      glm_parms._train = train._key;
+      glm_parms._valid = test ._key;
+      glm_parms._score_each_iteration = false; // default is false
+      // SupervisedModel.Parameters
+      glm_parms._response_column = "tripduration";
+      glm_parms._convert_to_enum = false; // regression
+  
+      // GLMModel.Parameters
+      glm_parms._use_all_factor_levels = true;
+  
+      Job<GLMModel> job = new GLM(glm_parms).trainModel();
+      GLMModel glm = job.get();
+      job.remove();
+  
+      System.out.println(glm._output._coefficients_table);
+  
+      // Cleanup
+      //gbm.remove();
+      glm.remove();
+  
+      train.remove();
+      test .remove();
+      hold .remove();
+    } finally {
+      Scope.exit();
+    }
   }
 
   class TimeSplit extends MRTask<TimeSplit> {
@@ -161,7 +199,10 @@ public class WorkFlowTest extends TestUtil {
       Key keys[] = new Key[fnames.length];
       for( int i=0; i<fnames.length; i++ ) {
         File f = new File(fnames[i]);
-        if( !f.exists() ) return null;
+        if( !f.exists() ) {
+          System.out.println("File "+fnames[i]+" missing, aborting test");
+          return null;
+        }
         keys[i] = NFSFileVec.make(f)._key;
       }
       return ParseDataset.parse(Key.make(hex),keys);
