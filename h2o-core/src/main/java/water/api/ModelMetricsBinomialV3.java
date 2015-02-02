a7c557b536dacc08a357f010c2faa8014b8b5e93
Arno Candel <arno.candel@gmail.com>
2015-02-02 13:19:52 -0800

    Remove uneeded createImpl() methods.

--- h2o-core/src/main/java/water/api/ModelMetricsBinomialV3.java
+++ h2o-core/src/main/java/water/api/ModelMetricsBinomialV3.java
@@ -1,7 +1,6 @@
 package water.api;
 
 import hex.ModelMetricsBinomial;
-import water.util.PojoUtils;
 
 public class ModelMetricsBinomialV3 extends ModelMetricsBase<ModelMetricsBinomial, ModelMetricsBinomialV3> {
   @API(help="The Mean Squared Error of the prediction for this scoring run.", direction=API.Direction.OUTPUT)
@@ -12,16 +11,6 @@ public class ModelMetricsBinomialV3 extends ModelMetricsBase<ModelMetricsBinomia
 
   @API(help="The ConfusionMatrix object for this scoring run.", direction=API.Direction.OUTPUT)
   public ConfusionMatrixBase cm;
-
-  @Override public ModelMetricsBinomial createImpl() {
-    return new ModelMetricsBinomial(this.model.createImpl().get(), this.frame.createImpl().get());
-  }
-
-  @Override
-  public ModelMetricsBinomial fillImpl(ModelMetricsBinomial m) {
-    PojoUtils.copyProperties(m, this, PojoUtils.FieldNaming.CONSISTENT, new String[]{"mse", "auc", "cm"});
-    return m;
-  }
 
   @Override public ModelMetricsBinomialV3 fillFromImpl(ModelMetricsBinomial modelMetrics) {
     super.fillFromImpl(modelMetrics);
