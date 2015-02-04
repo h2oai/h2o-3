0bd0ec7147cbe9d6588aeff3f835ad7b7eca2194
Raymond Peck <rpeck@rpeck.com>
2015-02-04 12:37:48 -0800

    Add GLM-specific binomial ModelMetrics.

--- h2o-core/src/main/java/hex/ModelMetricsBinomialGLM.java
+++ h2o-core/src/main/java/hex/ModelMetricsBinomialGLM.java
@@ -1,4 +1,14 @@
 package hex;
 
+import water.fvec.Frame;
+
 public class ModelMetricsBinomialGLM extends ModelMetricsBinomial {
+
+  public ModelMetricsBinomialGLM(Model model, Frame frame) {
+    super(model, frame);
+  }
+
+  public ModelMetricsBinomialGLM(Model model, Frame frame, AUCData aucdata, double sigma, double mse) {
+    super(model, frame, aucdata, sigma, mse);
+  }
 }
---
+++ h2o-core/src/main/java/water/api/ModelMetricsBinomialGLMV3.java
@@ -0,0 +1,8 @@
+package water.api;
+
+import hex.ModelMetricsBinomialGLM;
+
+public class ModelMetricsBinomialGLMV3 extends ModelMetricsBinomialBaseV3<ModelMetricsBinomialGLM, ModelMetricsBinomialGLMV3> {
+
+
+}
