8835d40311c22af61d50967c720a34372b97f26f
Raymond Peck <rpeck@rpeck.com>
2015-02-13 13:44:40 -0800

    PUBDEV-391: preparing for /SplitFrame to get unhacked by adding a failing test.  Commented out for now.

--- py/testdir_multi_jvm/test_rest_api.py
+++ py/testdir_multi_jvm/test_rest_api.py
@@ -164,6 +164,19 @@ def validate_frame_exists(frame_name, frames=None):
     return frames_dict[frame_name]
 
 
+def validate_job_exists(job_name, jobs=None):
+    '''
+    Validate that a given job key is found in the jobs list.
+    '''
+    if jobs is None:
+        result = a_node.jobs()
+        jobs = result['jobs']
+
+    jobs_dict = list_to_dict(jobs, 'key/name')
+    assert job_name in jobs_dict, "FAIL: Failed to find " + job_name + " in jobs list: " + repr(jobs_dict.keys())
+    return jobs_dict[job_name]
+
+
 def validate_actual_parameters(input_parameters, actual_parameters, training_frame, validation_frame):
     '''
     Validate that the returned parameters list for a model build contains all the values we passed in as input.
@@ -659,6 +672,7 @@ bigger = a_node.frames(key='bigger')['frames'][0]
 smaller = a_node.frames(key='smaller')['frames'][0]
 assert bigger['rows'] == 304, 'FAIL: 80/20 SplitFrame yielded the wrong number of rows.  Expected: 304; got: ' + bigger['rows']
 assert smaller['rows'] == 76, 'FAIL: 80/20 SplitFrame yielded the wrong number of rows.  Expected: 76; got: ' + smaller['rows']
+# TODO: validate_job_exists(splits['key']['name'])
 
 if verbose: print 'Testing SplitFrame with generated destKeys. . .'
 splits = a_node.split_frame(dataset='prostate_binomial', ratios=[0.5])
@@ -670,6 +684,7 @@ first = a_node.frames(key=splits['destKeys'][0]['name'])['frames'][0]
 second = a_node.frames(key=splits['destKeys'][1]['name'])['frames'][0]
 assert first['rows'] == 190, 'FAIL: 50/50 SplitFrame yielded the wrong number of rows.  Expected: 190; got: ' + first['rows']
 assert second['rows'] == 190, 'FAIL: 50/50 SplitFrame yielded the wrong number of rows.  Expected: 190; got: ' + second['rows']
+# TODO: validate_job_exists(splits['key']['name'])
 
 
 ####################################################################################################
