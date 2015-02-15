dbf4673ed0a1182609b6c04d66c5d646cc22dab0
Arno Candel <arno.candel@gmail.com>
2015-02-14 16:19:09 -0800

    Add debugging output.

--- h2o-algos/src/test/java/hex/deeplearning/DeepLearningProstateTest.java
+++ h2o-algos/src/test/java/hex/deeplearning/DeepLearningProstateTest.java
@@ -154,6 +154,7 @@ public class DeepLearningProstateTest extends TestUtil {
                                       } finally {
                                         dl.remove();
                                       }
+                                      Log.info("Trained for " + model1.epoch_counter + " epochs.");
                                       assert( ((p._train_samples_per_iteration <= 0 || p._train_samples_per_iteration >= frame.numRows()) && model1.epoch_counter > epochs)
                                               || Math.abs(model1.epoch_counter - epochs)/epochs < 0.20 );
 
