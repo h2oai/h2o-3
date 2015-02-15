481bc8759a48b6883e635430e953c18267be27a5
Tom Kraljevic <tomk@tomk.net>
2015-02-15 12:57:13 -0800

    Minor refactoring.

--- h2o-core/src/main/java/water/persist/PersistManager.java
+++ h2o-core/src/main/java/water/persist/PersistManager.java
@@ -1,7 +1,9 @@
 package water.persist;
 
+import water.H2O;
 import water.Key;
 import water.Value;
+import water.util.Log;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,29 +30,49 @@ public class PersistManager {
     deleteCount = new AtomicLong();
     loadCount = new AtomicLong();
 
+    if (iceRoot == null) {
+      Log.err("ice_root must be specified.  Exiting.");
+      H2O.exit(1);
+    }
+
     Persist ice = null;
-    URI uri = iceRoot;
-    if( uri != null ) { // Otherwise class loaded for reflection
-      boolean windowsPath = uri.toString().matches("^[a-zA-Z]:.*");
+    boolean windowsPath = iceRoot.toString().matches("^[a-zA-Z]:.*");
 
-      if ( windowsPath ) {
-        ice = new PersistFS(new File(uri.toString()));
-      }
-      else if ((uri.getScheme() == null) || Schemes.FILE.equals(uri.getScheme())) {
-        ice = new PersistFS(new File(uri.getPath()));
-      }
-      else if( Schemes.HDFS.equals(uri.getScheme()) ) {
-        ice = new PersistHdfs(uri);
-      }
+    if (windowsPath) {
+      ice = new PersistFS(new File(iceRoot.toString()));
+    }
+    else if ((iceRoot.getScheme() == null) || Schemes.FILE.equals(iceRoot.getScheme())) {
+      ice = new PersistFS(new File(iceRoot.getPath()));
+    }
+    else if( Schemes.HDFS.equals(iceRoot.getScheme()) ) {
+      Log.err("HDFS ice_root not yet supported.  Exiting.");
+      H2O.exit(1);
 
-      I[Value.ICE ] = ice;
-      I[Value.NFS ] = new PersistNFS();
-      try {
-        I[Value.HDFS] = new PersistHdfs();
-        I[Value.S3  ] = new PersistS3();
-      } catch( NoClassDefFoundError e ) {
-        // Not linked against HDFS or S3, so not available in this build
-      }
+// I am not sure anyone actually ever does this.
+// H2O on Hadoop launches use local disk for ice root.
+// This has a chance to work, but turn if off until it gets tested.
+//
+//      try {
+//        Class klass = Class.forName("water.persist.PersistHdfs");
+//        java.lang.reflect.Constructor constructor = klass.getConstructor(new Class[]{URI.class});
+//        ice = (Persist) constructor.newInstance(iceRoot);
+//      } catch (Exception e) {
+//        Log.err("Could not initialize HDFS");
+//        throw new RuntimeException(e);
+//      }
+    }
+
+    I[Value.ICE ] = ice;
+    I[Value.NFS ] = new PersistNFS();
+    try {
+      I[Value.HDFS] = new PersistHdfs();
+    } catch( NoClassDefFoundError ignore ) {
+      // Not linked against HDFS, so not available in this build
+    }
+    try {
+      I[Value.S3  ] = new PersistS3();
+    } catch( NoClassDefFoundError ignore ) {
+      // Not linked against S3, so not available in this build
     }
   }
 
