79139c74baa349839669e0446cea5e223960a4d5
Kevin Normoyle <kevin@0xdata.com>
2015-02-15 20:44:32 -0800

    this test was taking 60s. Get rid of the 2 sec sleeps, and downsize the rows used by 10x

--- h2o-r/tests/testdir_munging/exec/runit_pub-770_sleep.R
+++ h2o-r/tests/testdir_munging/exec/runit_pub-770_sleep.R
@@ -13,25 +13,25 @@ test.cbind <- function(conn) {
 
     # seemed to get different errors (cbind) if I pasted the stuff below in "paragraphs" into R
     # adding delays to test
-    Sys.sleep(2)
-    df <- data.frame(matrix(1:300000, nrow=300000, ncol=150))
-    sample.IDs <- 1:60000
+    # Sys.sleep(2)
+    df <- data.frame(matrix(1:30000, nrow=30000, ncol=150))
+    sample.IDs <- 1:6000
     index <- data.frame(ifelse(df[,1] %in% sample.IDs,1,0))
     colnames(index) <- c("index")
 
-    Sys.sleep(2)
+    # Sys.sleep(2)
     df.hex <- as.h2o(conn, df, key="df")
     index.h2o <- as.h2o(conn, index, key="index.h2o")
 
-    Sys.sleep(2)
+    # Sys.sleep(2)
     df.hex <- h2o.cbind(df.hex,index.h2o)
     summary(df.hex[,"index"])
 
-    Sys.sleep(2)
+    # Sys.sleep(2)
     df.train <- h2o.assign(df.hex[df.hex$index==1,],"df.train")
     df.test <- h2o.assign(df.hex[df.hex$index==0,],"df.test")
 
-    Sys.sleep(2)
+    # Sys.sleep(2)
     df.hex[,"index"] <- index.h2o[,"index"]
     summary(df.hex[,"index"])
     df.train <- h2o.assign(df.hex[df.hex$index==1,],"df.train")
