847770f336344f2130b162c4f2ecc3f10e4d5bc0
Kevin Normoyle <kevin@0xdata.com>
2015-02-24 21:13:45 -0800

    repeatedly causes bad AST (missing key on frame slice?)

---
+++ h2o-r/tests/testdir_algos/deeplearning/runit_NOPASS_deeplearning_iris_getModel.R
@@ -0,0 +1,16 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../h2o-runit.R')
+
+check.deeplearning_basic <- function(conn) {
+  iris.hex <- h2o.uploadFile(conn, locate("smalldata/iris/iris.csv"), "iris.hex")
+  hh <- h2o.deeplearning(x=c(1,2,3,4),y=5,training_frame=iris.hex)
+  print(hh)
+
+  print(predict(hh, iris.hex))
+  m <- h2o.getModel(conn, hh@key)
+  print(predict(m, iris.hex))
+  testEnd()
+}
+
+doTest("Deep Learning Test: Iris", check.deeplearning_basic)
+
