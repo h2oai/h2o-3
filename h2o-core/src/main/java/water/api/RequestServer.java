6b81df0b7fa2379073b214c4cacfbdfcf3d3fe68
Tom Kraljevic <tomk@tomk.net>
2015-02-25 03:41:51 -0800

    Added NodePersistentStorage updates for Prithvi.

--- h2o-core/src/main/java/water/api/RequestServer.java
+++ h2o-core/src/main/java/water/api/RequestServer.java
@@ -5,6 +5,7 @@ import water.*;
 import water.exceptions.H2OAbstractRuntimeException;
 import water.exceptions.H2ONotFoundArgumentException;
 import water.fvec.Frame;
+import water.init.NodePersistentStorage;
 import water.nbhm.NonBlockingHashMap;
 import water.parser.ParseSetupHandler;
 import water.util.GetLogsFromNode;
@@ -17,6 +18,7 @@ import java.net.MalformedURLException;
 import java.net.ServerSocket;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
@@ -533,14 +535,24 @@ public class RequestServer extends NanoHTTPD {
 
     alwaysLogRequest(uri, method, parms);
 
-    // Handle any URLs that bypass the route approach.  This is stuff that has abnormal non-JSON response payloads.
-    if (uri.endsWith("/Logs/download")) {
-      maybeLogRequest(method, uri, "", parms);
-      return downloadLogs();
-    }
-
     // Load resources, or dispatch on handled requests
     try {
+      // Handle any URLs that bypass the route approach.  This is stuff that has abnormal non-JSON response payloads.
+      if (method.equals("GET") && uri.endsWith("/Logs/download")) {
+        maybeLogRequest(method, uri, "", parms);
+        return downloadLogs();
+      }
+      if (method.equals("GET")) {
+        Pattern p2 = Pattern.compile(".*/NodePersistentStorage.bin/([^/]+)/([^/]+)");
+        Matcher m2 = p2.matcher(uri);
+        boolean b2 = m2.matches();
+        if (b2) {
+          String categoryName = m2.group(1);
+          String keyName = m2.group(2);
+          return downloadNps(categoryName, keyName);
+        }
+      }
+
       // Find handler for url
       Route route = lookup(method, versioned_path);
 
@@ -862,6 +874,21 @@ public class RequestServer extends NanoHTTPD {
     NanoHTTPD.Response res = new Response(NanoHTTPD.HTTP_OK,NanoHTTPD.MIME_DEFAULT_BINARY, new ByteArrayInputStream(finalZipByteArray));
     res.addHeader("Content-Length", Long.toString(finalZipByteArray.length));
     res.addHeader("Content-Disposition", "attachment; filename="+outputFileStem + ".zip");
+    return res;
+  }
+
+
+  // ---------------------------------------------------------------------
+  // Download NPS support
+  // ---------------------------------------------------------------------
+
+  private Response downloadNps(String categoryName, String keyName) {
+    NodePersistentStorage nps = H2O.getNPS();
+    AtomicLong length = new AtomicLong();
+    InputStream is = nps.get(categoryName, keyName, length);
+    NanoHTTPD.Response res = new Response(NanoHTTPD.HTTP_OK, NanoHTTPD.MIME_DEFAULT_BINARY, is);
+    res.addHeader("Content-Length", Long.toString(length.get()));
+    res.addHeader("Content-Disposition", "attachment; filename="+keyName + ".flow");
     return res;
   }
 }
