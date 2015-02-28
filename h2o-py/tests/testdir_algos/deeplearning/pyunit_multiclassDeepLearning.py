f0675f7a5639e1e2322bae6b7b9b2080a530efd1
Tom Kraljevic <tomk@tomk.net>
2015-02-28 14:27:49 -0800

    Use locate function so import finds the file.

--- h2o-py/tests/testdir_algos/deeplearning/pyunit_multiclassDeepLearning.py
+++ h2o-py/tests/testdir_algos/deeplearning/pyunit_multiclassDeepLearning.py
@@ -7,7 +7,7 @@ def deeplearning_multi(ip, port):
 
     print("Test checks if Deep Learning works fine with a multiclass training and test dataset")
 
-    prostate = h2o.import_frame("smalldata/logreg/prostate.csv")
+    prostate = h2o.import_frame(h2o.locate("smalldata/logreg/prostate.csv"))
 
     hh = h2o.deeplearning(x             = prostate[0:2],
                           y             = prostate[4],
