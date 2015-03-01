5374f5812093f475d953f2de2ba4f831236cf473
Tom Kraljevic <tomk@tomk.net>
2015-03-01 14:03:57 -0800

    Fix some hadoop documentation.

--- h2o-hadoop/src/main/java/water/hadoop/h2odriver.java
+++ h2o-hadoop/src/main/java/water/hadoop/h2odriver.java
@@ -386,8 +386,7 @@ public class h2odriver extends Configured implements Tool {
     System.err.printf(
             "\n" +
                     "Usage: h2odriver\n" +
-                    "          -libjars <.../h2o.jar>\n" +
-                    "          [other generic Hadoop ToolRunner options]\n" +
+                    "          [generic Hadoop ToolRunner options]\n" +
                     "          [-h | -help]\n" +
                     "          [-jobname <name of job in jobtracker (defaults to: 'H2O_nnnnn')>]\n" +
                     "              (Note nnnnn is chosen randomly to produce a unique name)\n" +
@@ -441,9 +440,8 @@ public class h2odriver extends Configured implements Tool {
                     "          o  All mappers must start before the H2O cloud is considered up.\n" +
                     "\n" +
                     "Examples:\n" +
-                    "          hadoop jar h2odriver_HHH.jar water.hadoop.h2odriver -jt <yourjobtracker>:<yourport> -libjars h2o.jar -mapperXmx 1g -nodes 1 -output hdfsOutputDir\n" +
-                    "          hadoop jar h2odriver_HHH.jar water.hadoop.h2odriver -jt <yourjobtracker>:<yourport> -libjars h2o.jar -mapperXmx 1g -nodes 1 -notify notify.txt -disown -output hdfsOutputDir\n" +
-                    "          (Choose the proper h2odriver (_HHH) for your version of hadoop.\n" +
+                    "          hadoop jar h2odriver.jar -nodes 1 -mapperXmx 1g -output hdfsOutputDir\n" +
+                    "          hadoop jar h2odriver.jar -nodes 1 -mapperXmx 1g -notify notify.txt -disown -output hdfsOutputDir\n" +
                     "\n" +
                     "Exit value:\n" +
                     "          0 means the cluster exited successfully with an orderly Shutdown.\n" +
