e5e111faed8542cdf0962539c55b9a19e9fe64f2
Sebastian Vidrio <sebastian@0xdata.com>
2015-03-06 10:19:38 -0800

    deeplearning iris basic pyunit

---
+++ h2o-py/tests/testdir_algos/deeplearning/pyunit_iris_basicDeepLearning.py
@@ -0,0 +1,14 @@
+import sys, os
+sys.path.insert(1,"../../../")
+import h2o
+
+def deeplearning_basic(ip, port):
+    h2o.init(ip, port)
+
+    iris_hex = h2o.import_frame(path=h2o.locate("smalldata/iris/iris.csv"))
+    hh = h2o.deeplearning(x=iris_hex[:3],
+                          y=iris_hex[4])
+    hh.show()
+
+if __name__ == '__main__':
+    h2o.run_test(sys.argv, deeplearning_basic)
