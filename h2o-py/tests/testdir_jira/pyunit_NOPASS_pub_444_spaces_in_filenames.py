304e4b9eb0c61866be6e20196b2d0e51c6448dbd
eric eckstrand <eric.eckstrand@gmail.com>
2015-03-05 16:23:24 -0800

    move to NOPASS until ./gradlew syncSmalldata can handle filenames with spaces.

--- h2o-py/tests/testdir_jira/pyunit_NOPASS_pub_444_spaces_in_filenames.py
+++ h2o-py/tests/testdir_jira/pyunit_NOPASS_pub_444_spaces_in_filenames.py
@@ -8,6 +8,7 @@ def pub_444_spaces_in_filenames(ip,port):
 
     train_data = h2o.import_frame(path=h2o.locate("smalldata/jira/pub 444.csv"))
     train_data.show()
+    train_data.describe()
 
     gbm = h2o.gbm(x=train_data[1:], y=train_data["response"], ntrees=1, loss="bernoulli")
     gbm.show()
