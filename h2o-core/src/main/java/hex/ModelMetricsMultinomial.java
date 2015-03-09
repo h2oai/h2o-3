31bbc079e19e7a84bea8eed6c7e7c1c8672be47d
Cliff Click <cliffc@acm.org>
2015-03-09 13:44:50 -0700

    Remove broken HitRatio code
    Expensive, but results never used.  Looks like it got zero'd every row
    also, so no results even accumulated.  Needs a real test before turning
    back on.

--- h2o-core/src/main/java/hex/ModelMetricsMultinomial.java
+++ h2o-core/src/main/java/hex/ModelMetricsMultinomial.java
@@ -4,20 +4,10 @@ import water.exceptions.H2OIllegalArgumentException;
 import water.fvec.Frame;
 import water.util.ArrayUtils;
 
-import java.util.Arrays;
-import java.util.Random;
-
-import static water.util.ModelUtils.getPredictions;
-
 public class ModelMetricsMultinomial extends ModelMetricsSupervised {
   public final float[] _hit_ratios;         // Hit ratios
   public final ConfusionMatrix _cm;
 
-  public ModelMetricsMultinomial(Model model, Frame frame) {
-    super(model, frame);
-    _cm = null;
-    _hit_ratios = null;
-  }
   public ModelMetricsMultinomial(Model model, Frame frame, ConfusionMatrix cm, float[] hr, double sigma, double mse) {
     super(model, frame, sigma, mse);
     _cm = cm;
@@ -41,69 +31,65 @@ public class ModelMetricsMultinomial extends ModelMetricsSupervised {
     return (ModelMetricsMultinomial) mm;
   }
 
-  /**
-   * Update hit counts for given set of actual label and predicted labels
-   * This is to be called for each predicted row
-   * @param hits Array of length K, counting the number of hits (entries will be incremented)
-   * @param actual_label 1 actual label
-   * @param pred_labels K predicted labels
-   */
-  static void updateHits(long[] hits, int actual_label, int[] pred_labels) {
-    assert(hits != null);
-    for (long h : hits) assert(h >= 0);
-    assert(pred_labels != null);
-    assert(actual_label >= 0);
-    assert(hits.length == pred_labels.length);
-
-    // find the first occurrence of the actual label and increment all counters from there on
-    // do nothing if no hit
-    for (int k = 0; k < pred_labels.length; ++k) {
-      if (pred_labels[k] == actual_label) {
-        while (k < pred_labels.length) hits[k++]++;
-      }
-    }
-  }
-
   public static class MetricBuilderMultinomial extends MetricBuilderSupervised {
     long[/*nclasses*/][/*nclasses*/] _cm;
-    long[/*K*/] _hits;            //the number of hits, length: K
-    private int _K;               // TODO: Let user set K and seed
-    private long _seed = 12345;
+    long[/*K*/] _hits;            // the number of hits for hitratio, length: K
+    private int _K;               // TODO: Let user set K
 
-    public MetricBuilderMultinomial( String[] domain ) {
-      super(domain);
-      _cm = new long[_nclasses][_nclasses];
+    public MetricBuilderMultinomial( int nclasses, String[] domain ) {
+      super(nclasses,domain);
+      _cm = new long[domain.length][domain.length];
       _K = Math.min(10,_nclasses-1);
+      _hits = new long[_K];
     }
 
     // Passed a float[] sized nclasses+1; ds[0] must be a prediction.  ds[1...nclasses-1] must be a class
     // distribution;
     @Override public float[] perRow( float ds[], float [] yact, Model m ) {
       if( Float.isNaN(yact[0]) ) return ds; // No errors if   actual   is missing
-      if( Float.isNaN(ds[0])) return ds; // No errors if prediction is missing
+      if( Float.isNaN(ds  [0]) ) return ds; // No errors if prediction is missing
       final int iact = (int)yact[0];
 
       // Compute error
       float sum = 0;          // Check for sane class distribution
       for( int i=1; i<ds.length; i++ ) { assert 0 <= ds[i] && ds[i] <= 1; sum += ds[i]; }
       assert Math.abs(sum-1.0f) < 1e-6;
-      float err = 1.0f-ds[iact+1];  // Error: distance from predicting ycls as 1.0
-      _sumsqe += err*err;       // Squared error
+      float err = iact+1 < ds.length ? 1.0f-ds[iact+1] : 1.0f;  // Error: distance from predicting ycls as 1.0
+      _sumsqe += err*err;           // Squared error
       assert !Double.isNaN(_sumsqe);
-
-      // Compute hit ratio
-      // TODO: Skip rows with predicted class = NA
-      _hits = new long[_K];
-      Arrays.fill(_hits, 0);
-      final double[] tieBreaker = new double [] {
-              new Random(_seed).nextDouble(), new Random(_seed+1).nextDouble(),
-              new Random(_seed+2).nextDouble(), new Random(_seed+3).nextDouble() };
-      final int[] pred_labels = getPredictions(_K, ds, tieBreaker);
-      if(iact < ds.length-1) updateHits(_hits, iact, pred_labels);
 
       // Plain Olde Confusion Matrix
       _cm[iact][(int)ds[0]]++; // actual v. predicted
       _count++;
+
+      // Compute hit ratio
+      if( _K > 0 && iact < ds.length-1) {
+        if( iact == (int)ds[0] ) _hits[0]++; // Top prediction is correct?
+        else {                  // Else need to find how far down the correct guy is
+          float p = ds[(int)ds[0]]; // Prediction value which failed
+          int tie=0;
+          for( int k=1; k<_K; k++ ) {
+            // Find largest prediction less than 'p', or for ties, the tie'th
+            int best = 0;
+            int tiebreak=0;
+            for( int i=1; i<ds.length; i++ ) {
+              if( ds[i] <= p ) {
+                if( best==0 || ds[i] > ds[best] || 
+                    (ds[i] == ds[best] && tie < tiebreak) )
+                  best = i;
+              }
+            }              
+            if( ds[best] < p ) {
+              p = ds[best]; tie=0;
+            } else {
+              assert ds[best]==p;
+              tie++;
+            }
+            if( best==iact+1 ) { _hits[k]++; break; }
+          }
+        }
+      }
+
       return ds;                // Flow coding
     }
 
