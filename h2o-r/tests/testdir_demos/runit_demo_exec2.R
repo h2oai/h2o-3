1df793be2ff1480bdde3b483598576ba0bea8750
Sebastian Vidrio <sebastian@0xdata.com>
2015-03-13 00:10:19 -0700

    got all the runits sorted out, had to change one to NOPASS

--- h2o-r/tests/testdir_demos/runit_demo_exec2.R
+++ h2o-r/tests/testdir_demos/runit_demo_exec2.R
@@ -11,23 +11,30 @@ test.exec2.demo <- function(conn) {
   prosPath <- system.file("extdata", "prostate.csv", package="h2o")
   Log.info(paste("Importing", prosPath))
   prostate.hex <- h2o.importFile(conn, path = prosPath, key = "prostate.hex")
-  
+
   Log.info("Print out summary, head, and tail")
   print(summary(prostate.hex))
   print(head(prostate.hex))
   print(tail(prostate.hex))
-  
+
+  Log.info("Convert CAPSULE column from numeric to factor variable")
+  prostate.hex$CAPSULE <- as.factor(prostate.hex$CAPSULE)
+  print(prostate.hex)
+  expect_true(is.factor(prostate.hex$RACE))
+  print(summary(prostate.hex))
+
+
   Log.info("Convert RACE column from numeric to factor variable")
   prostate.hex$RACE <- as.factor(prostate.hex$RACE)
   print(prostate.hex)
   expect_true(is.factor(prostate.hex$RACE))
   print(summary(prostate.hex))
-  
+
   Log.info("Display count of AGE column levels")
   Log.info("Note: Currently only working on a single integer or factor column")
   age.count <- h2o.table(prostate.hex$AGE)
   print(head(age.count))
-  
+
   Log.info("Run GLM2 on random sample of 50 observations")
   Log.info("y = CAPSULE, x = AGE, RACE, PSA, VOL, GLEASON")
   prostate.samp <- prostate.hex[sample(1:nrow(prostate.hex), 50),]
@@ -37,11 +44,11 @@ test.exec2.demo <- function(conn) {
   # glm(CAPSULE ~ AGE + PSA + VOL + GLEASON, family = binomial(), data = prostate.samp.df)
   prostate.glm <- h2o.glm(x = c("AGE", "RACE", "PSA", "VOL", "GLEASON"), y = "CAPSULE", training_frame = prostate.samp, family = "binomial")
   print(prostate.glm)
-  
+
   Log.info("Get quantiles of PSA column")
   prostate.qs <- quantile(prostate.hex$PSA)
   print(prostate.qs)
-  
+
   Log.info("Extract outliers based on 5% and 95% quantile cutoffs")
   # Note: Right now, assignment must be done manually with h2o.assign!
   PSA.outliers <- prostate.hex[prostate.hex$PSA <= prostate.qs[2] | prostate.hex$PSA >= prostate.qs[10],]
@@ -51,13 +58,13 @@ test.exec2.demo <- function(conn) {
   print(paste("Number of outliers:", nrow(PSA.outliers)))
   print(head(PSA.outliers))
   print(tail(PSA.outliers))
-  
+
   Log.info("Trim outliers from dataset")
   prostate.trim <- prostate.hex[prostate.hex$PSA > prostate.qs[2] & prostate.hex$PSA < prostate.qs[10],]
   # prostate.trim = prostate.hex[!PSA.outliers.ind,]
   prostate.trim <- h2o.assign(prostate.trim, "prostate.trim")
   print(paste("Number of rows in trimmed dataset:", nrow(prostate.trim)))
-  
+
   Log.info("Construct test and train sets using runif")
   s <- h2o.runif(prostate.hex)
   prostate.train <- prostate.hex[s <= 0.8,]
@@ -65,7 +72,7 @@ test.exec2.demo <- function(conn) {
   prostate.test <- prostate.hex[s > 0.8,]
   prostate.test <- h2o.assign(prostate.test, "prostate.test")
   expect_that(nrow(prostate.train) + nrow(prostate.test), equals(nrow(prostate.hex)))
-  
+
   Log.info("For large datasets, need to sample using h2o.runif")
   s2 <- h2o.runif(prostate.hex)
   prostate.train2 <- prostate.hex[s2 <= 0.8,]
@@ -73,28 +80,28 @@ test.exec2.demo <- function(conn) {
   prostate.test2 <- prostate.hex[s2 > 0.8,]
   prostate.test2 <- h2o.assign(prostate.test2, "prostate.test2")
   expect_that(nrow(prostate.train2) + nrow(prostate.test2), equals(nrow(prostate.hex)))
-  
+
   myY <- "CAPSULE"; myX = setdiff(colnames(prostate.train), c(myY, "ID"))
   Log.info(paste("Run GBM with y = CAPSULE, x =", paste(myX, collapse = ",")))
   prostate.gbm <- h2o.gbm(x = myX, y = myY, training_frame = prostate.train, validation_frame = prostate.test, loss = "bernoulli")
   print(prostate.gbm)
-  
+
   Log.info("Generate GBM predictions on test set")
   prostate.pred <- predict(prostate.gbm, prostate.test)
   summary(prostate.pred)
   head(prostate.pred)
   tail(prostate.pred)
-  
+
   Log.info("Create new boolean column based on 25% quantile cutoff")
   prostate.hex[,10] <- prostate.hex$PSA <= prostate.qs["25%"]
   head(prostate.hex)
   expect_that(ncol(prostate.hex), equals(10))
   # prostate.hex[,11] = prostate.hex$PSA >= prostate.qs["75%"]
-  
+
   Log.info("Run GLM2 with y = new boolean column, x = AGE, RACE, VOL, GLEASON")
   prostate.glm.lin <- h2o.glm(y = 10, x = c("AGE", "RACE", "VOL", "GLEASON"), training_frame = prostate.hex, family = "binomial")
   print(prostate.glm.lin)
-  
+
   testEnd()
 }
 
--- h2o-r/tests/testdir_demos/runit_demo_h2o_gbm.R
+++ h2o-r/tests/testdir_demos/runit_demo_h2o_gbm.R
@@ -11,30 +11,31 @@ test.h2o.gbm <- function(conn) {
   prosPath <- system.file("extdata", "prostate.csv", package="h2o")
   Log.info(paste("Uploading", prosPath))
   prostate.hex <- h2o.uploadFile(conn, path = prosPath, key = "prostate.hex")
-  
+
   Log.info("Print out summary of prostate.csv")
+  prostate.hex$CAPSULE <- as.factor(prostate.hex$CAPSULE)
   print(summary(prostate.hex))
-  
+
   myX <- setdiff(colnames(prostate.hex), "CAPSULE")
   Log.info(paste("Run GBM with y = CAPSULE, x =", paste(myX, collapse=",")))
   prostate.gbm <- h2o.gbm(x = setdiff(colnames(prostate.hex), "CAPSULE"), y = "CAPSULE", training_frame = prostate.hex, ntrees = 10, max_depth = 5, learn_rate = 0.1, loss = "bernoulli")
   print(prostate.gbm)
-  
+
   Log.info("Run GBM with y = CAPSULE, x = AGE, RACE, PSA, VOL, GLEASON")
   prostate.gbm2 <- h2o.gbm(x = c("AGE", "RACE", "PSA", "VOL", "GLEASON"), y = "CAPSULE", training_frame = prostate.hex, ntrees = 10, max_depth = 8, min_rows = 10, learn_rate = 0.2, loss = "bernoulli")
   print(prostate.gbm2)
-  
+
   irisPath <- system.file("extdata", "iris.csv", package="h2o")
   Log.info(paste("Uploading", irisPath))
   iris.hex <- h2o.uploadFile(conn, path = irisPath, key = "iris.hex")
-  
+
   Log.info("Print out summary of iris.csv")
   print(summary(iris.hex))
-  
+
   Log.info("Run GBM with y = column 5, x = columns 1:4")
   iris.gbm <- h2o.gbm(x = 1:4, y = 5, training_frame = iris.hex, loss = "multinomial")
   print(iris.gbm)
-  
+
   testEnd()
 }
 
