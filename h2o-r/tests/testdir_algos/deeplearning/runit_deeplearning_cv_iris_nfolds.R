a81296e2e0ea96cd45ca1d09b466a36e0226f281
Arno Candel <arno.candel@gmail.com>
2015-03-12 20:02:59 -0700

    Adapt RUnits for DL for removal of do_classification, and specify the loss function.

--- h2o-r/tests/testdir_algos/deeplearning/runit_deeplearning_cv_iris_nfolds.R
+++ h2o-r/tests/testdir_algos/deeplearning/runit_deeplearning_cv_iris_nfolds.R
@@ -4,6 +4,7 @@ source('../../h2o-runit.R')
 test.deeplearning.nfolds <- function(conn) {
   iris.hex <- h2o.uploadFile(conn, locate("smalldata/iris/iris.csv"), key="iris.hex")
   iris.hex[,5] <- as.factor(iris.hex[,5])
+  iris.hex <- h2o.assign(iris.hex, "iris_factor.hex")
   print(summary(iris.hex))
   iris.nfolds <- h2o.deeplearning.cv(x = 1:4, y = 5, training_frame = iris.hex, nfolds = 3, loss = "CrossEntropy")
   print(iris.nfolds)
