8cb32b2ead28e88e68e3c324cd60e5009dd5427f
Arno Candel <arno.candel@gmail.com>
2015-03-12 23:54:57 -0700

    Code cleanup.

--- h2o-algos/src/main/java/hex/deeplearning/DeepLearning.java
+++ h2o-algos/src/main/java/hex/deeplearning/DeepLearning.java
@@ -93,21 +93,6 @@ public class DeepLearning extends SupervisedModelBuilder<DeepLearningModel,DeepL
 
     Key self() { return _key; }
 
-//  /**
-//   * Report the relative progress of building a Deep Learning model (measured by how many epochs are done)
-//   * @return floating point number between 0 and 1
-//   */
-//  @Override public float progress(){
-//    if(UKV.get(dest()) == null)return 0;
-//    DeepLearningModel m = UKV.get(dest());
-//    if (m != null && m.model_info()!=null ) {
-//      final float p = (float) Math.min(1, (m.epoch_counter / m.model_info().get_params().epochs));
-//      return cv_progress(p);
-//    }
-//    return 0;
-//  }
-
-
     // the following parameters can be modified when restarting from a checkpoint
     transient final String [] cp_modifiable = new String[] {
             "_expert_mode",
@@ -487,29 +472,5 @@ public class DeepLearning extends SupervisedModelBuilder<DeepLearningModel,DeepL
     private float rowFraction(Frame train, DeepLearningModel.DeepLearningParameters p, DeepLearningModel m) {
       return computeRowUsageFraction(train.numRows(), m.actual_train_samples_per_iteration, p._replicate_training_data);
     }
-
-//  /**
-//   * Cross-Validate a DeepLearning model by building new models on N train/test holdout splits
-//   * @param splits Frames containing train/test splits
-//   * @param cv_preds Array of Frames to store the predictions for each cross-validation run
-//   * @param offsets Array to store the offsets of starting row indices for each cross-validation run
-//   * @param i Which fold of cross-validation to perform
-//   */
-//  @Override public void crossValidate(Frame[] splits, Frame[] cv_preds, long[] offsets, int i) {
-//    // Train a clone with slightly modified parameters (to account for cross-validation)
-//    final DeepLearning cv = (DeepLearning) this.clone();
-//    cv.genericCrossValidation(splits, offsets, i);
-//    cv_preds[i] = ((DeepLearningModel) UKV.get(cv.dest())).score(cv.validation);
-//    new TAtomic<DeepLearningModel>() {
-//      @Override public DeepLearningModel atomic(DeepLearningModel m) {
-//        if (!keep_cross_validation_splits && /*paranoid*/cv.dest().toString().contains("xval")) {
-//          m.get_params().source = null;
-//          m.get_params().validation=null;
-//          m.get_params().response=null;
-//        }
-//        return m;
-//      }
-//    }.invoke(cv.dest());
-//  }
   }
 }
