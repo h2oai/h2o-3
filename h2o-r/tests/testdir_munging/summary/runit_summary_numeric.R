742d4cf5171accd165eda2caa04f65b301360624
Sebastian Vidrio <sebastian@0xdata.com>
2015-03-18 11:21:56 -0700

    changes to summary numeric

--- h2o-r/tests/testdir_munging/summary/runit_summary_numeric.R
+++ h2o-r/tests/testdir_munging/summary/runit_summary_numeric.R
@@ -1,10 +1,23 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source('../../h2o-runit.R')
 
+summary_to_numeric <- function(s) {
+  res <- list()
+  for(p in s) {
+    p_name <- strsplit(p, ':')[[1]][1]
+    res[p_name] <- strsplit(p, ':')[[1]][2]
+  }
+
+  res
+}
+
+
+
+
 test.summary.numeric <- function(conn) {
   Log.info("Importing USArrests.csv data...\n")
   # arrests.hex <- h2o.importFile(conn, locate("smalldata/pca_test/USArrests.csv", schema = "local"), "arrests.hex")
-  arrests.hex = as.h2o(conn, USArrests, key = "arrests.hex")
+  arrests.hex <- as.h2o(conn, USArrests, key = "arrests.hex")
 
   Log.info("Check that summary works...")
   summary(arrests.hex)
@@ -23,7 +36,26 @@ test.summary.numeric <- function(conn) {
   Log.info("summary(tail(arrests.hex))\n")
 
   print(summary(tail(arrests.hex)))
-  expect_that(summary(tail(arrests.hex)), equals(summary_2))
+
+  Log.info("Convert summaries to numeric lists")
+  summary_to_numeric <- function(s) {
+    lapply(s, function(x) {
+      val <- strsplit(x, ":")[[1]][2]
+      as.numeric(val)
+    })
+  }
+
+  Log.info("Compare R and H2O, accounting for binning error")
+  valR <- unlist(summary_to_numeric(summary_2))
+  valH2O <- unlist(summary_to_numeric(summary(tail(arrests.hex))))
+  for (i in 1:6)
+    expect_equal(valH2O, valR, 1)
+  for (i in 7:12)
+    expect_equal(valH2O, valR, 7)
+  for (i in 13:18)
+    expect_equal(valH2O, valR, 7)
+  for (i in 19:24)
+    expect_equal(valH2O, valR, 2.5)
 
   testEnd()
 }
