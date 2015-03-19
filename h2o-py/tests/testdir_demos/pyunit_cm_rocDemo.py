a32ed0f183b65c87b93319c79ce1315799f4f8d3
eric eckstrand <eric.eckstrand@gmail.com>
2015-03-19 09:32:22 -0700

    demo pyunit change

--- h2o-py/tests/testdir_demos/pyunit_cm_rocDemo.py
+++ h2o-py/tests/testdir_demos/pyunit_cm_rocDemo.py
@@ -7,7 +7,7 @@
 import sys
 sys.path.insert(1, "../../")
 import h2o
-import pylab as pl
+#import pylab as pl
 
 def demo_cm_roc(ip,port):
     # Connect to a pre-existing cluster
@@ -65,33 +65,33 @@ def demo_cm_roc(ip,port):
     print('GBM Precision: {0}'.format(gbm_perf.precision()))
     print('GBM Accuracy: {0}'.format(gbm_perf.accuracy()))
     print('GBM AUC: {0}'.format(gbm_perf.auc()))
-    gbm_tpr = [rc[1] if rc[1] != '' else 0.0 for rc in gbm_perf.recall()]
-    gbm_fpr = [1.0 - sp[1] if sp[1] != '' else 1.0 for sp in gbm_perf.specificity()]
-    pl.clf()
-    pl.plot(gbm_fpr, gbm_tpr, label='GBM ROC Curve (AUC = %0.2f)' % gbm_perf.auc())
-    pl.plot([0, 1], [0, 1], 'k--')
-    pl.xlim([0.0, 1.0])
-    pl.ylim([0.0, 1.0])
-    pl.xlabel('False Positive Rate')
-    pl.ylabel('True Positive Rate')
-    pl.title('GBM ROC curve')
-    pl.legend(loc="lower right")
+    #gbm_tpr = [rc[1] if rc[1] != '' else 0.0 for rc in gbm_perf.recall()]
+    #gbm_fpr = [1.0 - sp[1] if sp[1] != '' else 1.0 for sp in gbm_perf.specificity()]
+    #pl.clf()
+    #pl.plot(gbm_fpr, gbm_tpr, label='GBM ROC Curve (AUC = %0.2f)' % gbm_perf.auc())
+    #pl.plot([0, 1], [0, 1], 'k--')
+    #pl.xlim([0.0, 1.0])
+    #pl.ylim([0.0, 1.0])
+    #pl.xlabel('False Positive Rate')
+    #pl.ylabel('True Positive Rate')
+    #pl.title('GBM ROC curve')
+    #pl.legend(loc="lower right")
     #pl.savefig('gbm_roc.png')
 
     print('GLM Precision: {0}'.format(glm_perf.precision()))
     print('GLM Accuracy: {0}'.format(glm_perf.accuracy()))
     print('GLM AUC: {0}'.format(glm_perf.auc()))
-    glm_tpr = [rc[1] if rc[1] != '' else 0.0 for rc in glm_perf.recall()]
-    glm_fpr = [1.0 - sp[1] if sp[1] != '' else 1.0 for sp in glm_perf.specificity()]
-    pl.clf()
-    pl.plot(glm_fpr, glm_tpr, label='GLM ROC Curve (AUC = %0.2f)' % glm_perf.auc())
-    pl.plot([0, 1], [0, 1], 'k--')
-    pl.xlim([0.0, 1.0])
-    pl.ylim([0.0, 1.0])
-    pl.xlabel('False Positive Rate')
-    pl.ylabel('True Positive Rate')
-    pl.title('GLM ROC curve')
-    pl.legend(loc="lower right")
+    #glm_tpr = [rc[1] if rc[1] != '' else 0.0 for rc in glm_perf.recall()]
+    #glm_fpr = [1.0 - sp[1] if sp[1] != '' else 1.0 for sp in glm_perf.specificity()]
+    #pl.clf()
+    #pl.plot(glm_fpr, glm_tpr, label='GLM ROC Curve (AUC = %0.2f)' % glm_perf.auc())
+    #pl.plot([0, 1], [0, 1], 'k--')
+    #pl.xlim([0.0, 1.0])
+    #pl.ylim([0.0, 1.0])
+    #pl.xlabel('False Positive Rate')
+    #pl.ylabel('True Positive Rate')
+    #pl.title('GLM ROC curve')
+    #pl.legend(loc="lower right")
     #pl.savefig('glm_roc.png')
 
 if __name__ == "__main__":
