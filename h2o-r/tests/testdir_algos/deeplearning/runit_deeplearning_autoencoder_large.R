720e4fd03cabb6f654afc5115a2e60a04c040a99
Sebastian Vidrio <sebastian@0xdata.com>
2015-03-23 20:17:05 -0700

    passing Runits: imbalanced tests no longer tests xval as well

---
+++ h2o-r/tests/testdir_algos/deeplearning/runit_deeplearning_autoencoder_large.R
@@ -0,0 +1,71 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../h2o-runit.R')
+
+check.deeplearning_autoencoder <- function(conn) {
+     Log.info("Deep Learning Autoencoder MNIST)")
+
+     train_hex = h2o.uploadFile(conn, locate("bigdata/laptop/mnist/train.csv.gz"))
+     test_hex = h2o.uploadFile(conn, locate("bigdata/laptop/mnist/test.csv.gz"))
+
+     predictors = c(1:784)
+     resp = 785
+     nfeatures = 20 #number of features (smallest hidden layer)
+
+     # split data into two parts (first part for unsupervised training, second part for supervised training)
+     sid <- h2o.runif(train_hex, 1234)
+     # split <- h2o.splitFrame(train_hex, 0.5)
+
+
+     # first part of the data, without labels for unsupervised learning (DL auto-encoder)
+     train_unsupervised <- train_hex[sid>=0.5,-resp]
+     summary(train_unsupervised)
+
+     # second part of the data, with labels for supervised learning (drf)
+     train_supervised <- train_hex[sid<0.5,-resp]
+     summary(train_supervised)
+     train_supervised_labels <- train_hex[sid<0.5, resp]
+     summary(train_supervised_labels)
+
+     # third part of the data: test set
+     test <- test_hex[,-resp]
+     summary(test)
+     test_labels <- test_hex[,resp]
+     summary(test_labels)
+
+     # train autoencoder on train_unsupervised
+     ae_model <- h2o.deeplearning(x=predictors,
+                                  y=42, #ignored (pick any non-constant predictor)
+                                  training_frame=train_unsupervised,
+                                  activation="Tanh",
+                                  autoencoder=T,
+                                  hidden=c(nfeatures),
+                                  epochs=1,
+                                  reproducible=T,#slow - turn off for real problems
+                                  seed=1234)
+
+     # convert train_supervised with autoencoder model to lower-dimensional space
+     train_supervised_features <- h2o.deepfeatures(ae_model, train_supervised, layer=1)
+     summary(train_supervised_features)
+
+     expect_equal(ncol(train_supervised_features), nfeatures, "Dimensionality of reconstruction is wrong!")
+
+     myX = c(1:nfeatures)
+     myY = nfeatures+1
+     # Now train DRF on extracted feature space, first need to add response back
+     train_supervised_drf <- h2o.cbind(train_supervised_features, as.factor(train_supervised_labels))
+     drf_model <- h2o.randomForest(training_frame=train_supervised_drf, x=myX, y=myY, ntrees=10, seed=1234)
+
+     # Now test the DRF model on the test set (first need to process into the reduced feature space)
+     test_features <- h2o.deepfeatures(ae_model, test, layer=1)
+     test_features <- h2o.cbind(test_features,test_hex[,resp])
+     cm <- h2o.confusionMatrix(drf_model, test_features)
+     print(cm)
+
+     print(cm[length(cm),])
+     expect_equal(cm[11, "Error"], 0.1056) #10% test set error
+
+     testEnd()
+}
+
+doTest("Deep Learning AutoEncoder MNIST", check.deeplearning_autoencoder)
+
---
+++ h2o-r/tests/testdir_algos/randomforest/runit_RF_imbalanced.R
@@ -0,0 +1,36 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../h2o-runit.R')
+
+test.rf.imbalanced <- function(conn) {
+  covtype <- h2o.uploadFile(conn, locate("smalldata/covtype/covtype.20k.data"))
+  covtype[,55] <- as.factor(covtype[,55])
+
+  hh_imbalanced<-h2o.randomForest(x=1:54,y=55,ntrees=50,training_frame=covtype,
+                                 balance_classes=F)
+  hh_imbalanced_metrics <- h2o.performance(hh_imbalanced)
+  print(hh_imbalanced)
+  hh_balanced<-h2o.randomForest(x=1:54,y=55,ntrees=50,training_frame=covtype,
+                               balance_classes=T)
+  print(hh_balanced)
+  hh_balanced_metrics <- h2o.performance(hh_balanced)
+
+  #compare error for class 6 (difficult minority)
+  #confusion_matrix element at position A,P for N classes is at: model$confusion[P*(N+1)-(N-A+1)]
+  #Here, A=6 P=8, N=7 -> need element 8*(7+1)-(7-6+1) = 62
+
+  class_6_err_imbalanced <- hh_imbalanced_metrics@metrics$cm$prediction_error_by_class[6]
+  class_6_err_balanced <- hh_balanced_metrics@metrics$cm$prediction_error_by_class[6]
+
+
+  print("class_6_err_imbalanced")
+  print(class_6_err_imbalanced)
+  print("")
+  print("class_6_err_balanced")
+  print(class_6_err_balanced)
+
+ expect_true(class_6_err_imbalanced >= 0.9*class_6_err_balanced, "balance_classes makes it at least 10% worse!")
+
+  testEnd()
+}
+
+doTest("rf imbalanced", test.rf.imbalanced)
