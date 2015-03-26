d459d1a7fb405f8a1f7b466caae99281feae370c
Neeraja Madabhushi <neeraja@h2o.ai>
2015-03-25 17:42:28 -0700

    fixed init param

--- h2o-test-integ/tests/hdfs-bigdata/runit_186KRows_3.2KCols_large.R
+++ h2o-test-integ/tests/hdfs-bigdata/runit_186KRows_3.2KCols_large.R
@@ -23,13 +23,13 @@ if (!running_inside_hexdata) {
 }
 
 heading("BEGIN TEST")
-conn <- h2o.init(ip=myIP, port=myPort,startH2O = FALSE)
+conn <- h2o.init(ip=myIP, port=myPort, startH2O = FALSE)
 
 #----------------------------------------------------------------------
 # Parameters for the test.
 #----------------------------------------------------------------------
 
-data.hex <- h2o.importFile(conn, "/mnt/0xcustomer-datasets/c25/df_h2o.csv", header = T)
+data.hex <- h2o.uploadFile(conn, "/mnt/0xcustomer-datasets/c25/df_h2o.csv", header = T)
 
 colNames = {}
 for(col in names(data.hex)) {
