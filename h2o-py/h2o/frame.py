2ad0d1567d454eef7011e60a025d72a01596f0a2
eric eckstrand <eric.eckstrand@gmail.com>
2015-03-26 17:37:01 -0700

    simplified H2OFrame._simple_frames_op

--- h2o-py/h2o/frame.py
+++ h2o-py/h2o/frame.py
@@ -396,7 +396,7 @@ class H2OFrame:
     """
     return len(self._vecs)
 
-  def _simple_frames_bin_op(self, data, op):
+  def _simple_frames_bin_op(self, data, op, r=False):
     if len(self) == 0: return self
     if isinstance(data, (H2OVec, H2OFrame)): self._len_check(data)
 
@@ -423,49 +423,8 @@ class H2OFrame:
       arg2 = "\"" + data + "\""
 
     else: raise NotImplementedError
-    expr = "(= !{} (".format(tmp_key) + op + " %{} {}))".format(key1,arg2)
-
-    h2o.rapids(expr)
-    # Remove h2o temp frames
-    h2o.remove(key1)
-    if key2: h2o.remove(key2)
-    # Construct H2OFrame result
-    j = h2o.frame(tmp_key)
-    fr = j['frames'][0]
-    rows = fr['rows']
-    veckeys = fr['vec_keys']
-    cols = fr['columns']
-    colnames = [col['label'] for col in cols]
-    return H2OFrame(vecs=H2OVec.new_vecs(zip(colnames, veckeys), rows))
-
-  def _simple_frames_bin_rop(self, data, op):
-    if len(self) == 0: return self
-    if isinstance(data, (H2OVec, H2OFrame)): self._len_check(data)
-
-    # Construct rapids expression
-    tmp_key = H2OFrame.py_tmp_key()
-    key1 = self.send_frame()
-    key2 = None
-    if isinstance(data, H2OFrame):
-      key2 = data.send_frame()
-      arg2 = "%" + str(key2)
-
-    elif isinstance(data, H2OVec):
-      tmp_frame = H2OFrame(vecs=[data])
-      key2 = tmp_frame.send_frame()
-      arg2 = "%" + str(key2)
-
-    elif isinstance(data, Expr):
-      raise NotImplementedError
-
-    elif isinstance(data, (int, float)):
-      arg2 = "#" + str(data)
-
-    elif isinstance(data, str):
-      arg2 = "\"" + data + "\""
-
-    else : raise NotImplementedError
-    expr = "(= !{} (".format(tmp_key) + op + " {} %{}))".format(arg2,key1)
+    expr = "(= !{} (".format(tmp_key) + op + " %{} {}))".format(key1,arg2) if not r else \
+      "(= !{} (".format(tmp_key) + op + " {} %{}))".format(arg2,key1)
 
     h2o.rapids(expr)
     # Remove h2o temp frames
@@ -497,12 +456,12 @@ class H2OFrame:
 
   # rops
   def __radd__(self, i): return self.__add__(i)
-  def __rsub__(self, i): return self._simple_frames_bin_rop(i,"-")
+  def __rsub__(self, i): return self._simple_frames_bin_op(i,"-",True)
   def __rand__(self, i): return self.__and__(i)
   def __ror__ (self, i): return self.__or__ (i)
-  def __rdiv__(self, i): return self._simple_frames_bin_rop(i,"/")
+  def __rdiv__(self, i): return self._simple_frames_bin_op(i,"/",True)
   def __rmul__(self, i): return self.__mul__(i)
-  def __rpow__(self, i): return self._simple_frames_bin_rop(i,"^")
+  def __rpow__(self, i): return self._simple_frames_bin_op(i,"^",True)
 
   @staticmethod
   def py_tmp_key():
