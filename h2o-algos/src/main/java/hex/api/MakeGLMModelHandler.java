80b24f2368f91eb66b0ff1dff4cd53ff058ad5ab
Tomas Nykodym <tomas.nykodym@gmail.com>
2015-04-02 10:18:07 -0700

    Fixed latest merge, no thresholds in GLMModel constructor..

--- h2o-algos/src/main/java/hex/api/MakeGLMModelHandler.java
+++ h2o-algos/src/main/java/hex/api/MakeGLMModelHandler.java
@@ -28,7 +28,7 @@ public class MakeGLMModelHandler extends Handler {
     // beta has new coefficients in proper order
     System.out.println("coefs:");
     System.out.println(coefs);
-    GLMModel m = new GLMModel(args.dest,model._parms,new GLMOutput(model._output._names,model._output._domains, names, beta,.5f,model._output._binomial), model.dinfo(), Double.NaN, Double.NaN, -1, null);
+    GLMModel m = new GLMModel(args.dest,model._parms,new GLMOutput(model._output._names,model._output._domains, names, beta,.5f,model._output._binomial), model.dinfo(), Double.NaN, Double.NaN, -1);
     DKV.put(m._key, m);
     GLMModelV2 res = new GLMModelV2();
     res.fillFromImpl(m);
