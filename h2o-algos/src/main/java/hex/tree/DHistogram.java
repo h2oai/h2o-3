c6c1ac6c4964de6641a29f2094346a13080a3546
Arno Candel <arno.candel@gmail.com>
2015-04-02 19:23:43 -0700

    HEXDEV-241: Fix debug printout of histogram bin.

--- h2o-algos/src/main/java/hex/tree/DHistogram.java
+++ h2o-algos/src/main/java/hex/tree/DHistogram.java
@@ -219,7 +219,7 @@ public abstract class DHistogram<TDH extends DHistogram> extends Iced {
     sb.append(_name).append(":").append(_min).append("-").append(_maxEx).append(" step="+(1/_step)+" nbins="+nbins()+" isInt="+_isInt);
     if( _bins != null ) {
       for( int b=0; b<_bins.length; b++ ) {
-        sb.append(String.format("\ncnt=%d, [%f - %f], mean/var=", _bins[b],_min2,_maxIn));
+        sb.append(String.format("\ncnt=%d, [%f - %f], mean/var=", _bins[b],_min+b/_step,_min+(b+1)/_step));
         sb.append(String.format("%6.2f/%6.2f,", mean(b), var(b)));
       }
       sb.append('\n');
