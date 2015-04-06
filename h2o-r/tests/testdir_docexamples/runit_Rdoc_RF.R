84262ce840bbf62574b30d3bcc1aff0699db4ff7
Kevin Normoyle <kevin@0xdata.com>
2015-04-05 17:58:04 -0700

    runit_NOPASS_Rdoc_RF.R seems to pass now with param changes

--- h2o-r/tests/testdir_docexamples/runit_Rdoc_RF.R
+++ h2o-r/tests/testdir_docexamples/runit_Rdoc_RF.R
@@ -5,7 +5,7 @@ test.rdocRF.golden <- function(H2Oserver) {
 	
 irisPath <- system.file("extdata", "iris.csv", package="h2o")
 iris.hex <- h2o.importFile(H2Oserver, path = irisPath, key = "iris.hex")
-h2o.randomForest(y = 5, x = c(2,3,4), training_frame = iris.hex, ntree = 50, depth = 100, type = "BigData")
+h2o.randomForest(y = 5, x = c(2,3,4), training_frame = iris.hex, ntrees = 50, max_depth = 100)
 
 testEnd()
 }
