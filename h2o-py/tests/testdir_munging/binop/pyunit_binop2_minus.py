ca0743c259a398f7096f1149bda152dff3d7e039
eric eckstrand <eric.eckstrand@gmail.com>
2015-04-07 09:52:36 -0700

    updated pyunits

--- h2o-py/tests/testdir_munging/binop/pyunit_binop2_minus.py
+++ h2o-py/tests/testdir_munging/binop/pyunit_binop2_minus.py
@@ -6,7 +6,8 @@ def binop_minus(ip,port):
     # Connect to h2o
     h2o.init(ip,port)
 
-    iris = h2o.import_frame(path=h2o.locate("smalldata/iris/iris_wheader.csv"))
+
+    iris = h2o.import_frame(path=h2o.locate("smalldata/iris/iris_wheader_65_rows.csv"))
     rows, cols = iris.dim()
     iris.show()
 
@@ -15,48 +16,44 @@ def binop_minus(ip,port):
     # LHS: scaler, RHS: H2OFrame
     res = 2 - iris
     res_rows, res_cols = res.dim()
+    res = h2o.as_list(res)
     assert res_rows == rows and res_cols == cols, "dimension mismatch"
-    for x, y in zip([sum([res[r,c] for r in range(rows)]) for c in range(cols-1)], [-576.5, -158.1, -263.8, 120.2]):
-        assert abs(x.eager() - y) < 1e-1,  "expected same values"
+    for x, y in zip([sum([res[r][c] for r in range(rows)]) for c in range(cols-1)], [-209.9, -82.6, -6.9, 97.8]):
+        assert abs((x - y)) < 1e-1,  "expected same values"
 
     # LHS: scaler, RHS: H2OVec
     res = 2 - iris[1]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - -158.1) < 1e-1, "expected same values"
+    res = h2o.as_list(res)
+    assert abs(sum([res[i][0] for i in range(rows)]) - -82.6) < 1e-1, "expected same values"
 
     # LHS: scaler, RHS: Expr
     res = 2 - iris[0]
-    res2 = 1.1 - res[133]
-    assert abs(res2.eager() - 5.4) < 1e-1, "expected same values"
+    res2 = 1.1 - res[21]
+    res2 = h2o.as_list(res2)
+    assert abs(res2 - 4.2) < 1e-1, "expected same values"
 
     ###################################################################
 
     # LHS: Expr, RHS: H2OFrame
-    try:
-        res = 1.2 - iris[2]
-        res2 = res[133] - iris
-        res2.show()
-        assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 - iris[2]
+    res2 = res[21] - iris
+    res_rows, res_cols = res2.dim()
+    assert res_rows == rows and res_cols == cols, "dimension mismatch"
 
     # LHS: Expr, RHS: H2OVec
-    try:
-        res = 1.2 - iris[2]
-        res2 = res[133] - iris[1]
-        res2.show()
-        #assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 - iris[2]
+    res2 = res[21] - iris[1]
+    res2.show()
 
     # LHS: Expr, RHS: Expr
     res = 1.1 - iris[2]
-    res2 = res[133] - res[10]
-    assert abs(res2.eager() - -3.6) < 1e-1, "expected same values"
+    res2 = res[21] - res[10]
+    assert abs(h2o.as_list(res2) - 0) < 1e-1, "expected same values"
 
     # LHS: Expr, RHS: scaler
     res = 2 - iris[0]
-    res2 = res[133] - 3
-    assert abs(res2.eager() - -7.3) < 1e-1, "expected same values"
+    res2 = res[21] - 3
+    assert abs(h2o.as_list(res2) - -6.1) < 1e-1, "expected same values"
 
     ###################################################################
 
@@ -69,27 +66,23 @@ def binop_minus(ip,port):
         pass
 
     # LHS: H2OVec, RHS: H2OVec
-    res = iris[0] - iris[1]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 418.4) < 1e-1, "expected same values"
+    res = h2o.as_list(iris[0] - iris[1])
+    assert abs(sum([res[i][0] for i in range(rows)]) - 127.3) < 1e-1, "expected same values"
 
-    res = iris[2] - iris[1]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 105.7) < 1e-1, "expected same values"
+    res = h2o.as_list(iris[2] - iris[1])
+    assert abs(sum([res[i][0] for i in range(rows)]) - -75.7) < 1e-1, "expected same values"
 
-    res = iris[0] - iris[1] * iris[2] - iris[3]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - -977.21) < 1e-2, "expected same values"
+    res = h2o.as_list(iris[0] - iris[1] * iris[2] - iris[3])
+    assert abs(sum([res[i][0] for i in range(rows)]) - -122.15) < 1e-2, "expected same values"
 
     # LHS: H2OVec, RHS: Expr
-    try:
-        res = 1.2 - iris[2]
-        res2 = iris[1] - res[133]
-        res2.show()
-        #assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 - iris[2]
+    res2 = iris[1] - res[21]
+    res2.show()
 
     # LHS: H2OVec, RHS: scaler
-    res = iris[0] - 2
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 576.5) < 1e-2, "expected different column sum"
+    res = h2o.as_list(iris[0] - 2)
+    assert abs(sum([res[i][0] for i in range(rows)]) - 209.9) < 1e-2, "expected different column sum"
 
     ###################################################################
 
@@ -118,22 +111,20 @@ def binop_minus(ip,port):
         pass
 
     # LHS: H2OFrame, RHS: Expr
-    try:
-        res = 1.2 - iris[2]
-        res2 = iris - res[133]
-        res2.show()
-        assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 - iris[2]
+    res2 = iris - res[21]
+    res2.show()
+
 
     # LHS: H2OFrame, RHS: scaler
     res = iris - 2
     res_rows, res_cols = res.dim()
+    res = h2o.as_list(res)
     assert res_rows == rows and res_cols == cols, "dimension mismatch"
-    for x, y in zip([sum([res[r,c] for r in range(rows)]) for c in range(cols-1)], [576.5, 158.1, 263.8, -120.2]):
-        assert abs(x.eager() - y) < 1e-1,  "expected same values"
+    for x, y in zip([sum([res[r][c] for r in range(rows)]) for c in range(cols-1)], [209.9, 82.6, 6.9, -97.8]):
+        assert abs(x - y) < 1e-1,  "expected same values"
 
-    ###################################################################
+###################################################################
 
 if __name__ == "__main__":
     h2o.run_test(sys.argv, binop_minus)
--- h2o-py/tests/testdir_munging/binop/pyunit_binop2_plus.py
+++ h2o-py/tests/testdir_munging/binop/pyunit_binop2_plus.py
@@ -6,7 +6,8 @@ def binop_plus(ip,port):
     # Connect to h2o
     h2o.init(ip,port)
 
-    iris = h2o.import_frame(path=h2o.locate("smalldata/iris/iris_wheader.csv"))
+
+    iris = h2o.import_frame(path=h2o.locate("smalldata/iris/iris_wheader_65_rows.csv"))
     rows, cols = iris.dim()
     iris.show()
 
@@ -15,48 +16,42 @@ def binop_plus(ip,port):
     # LHS: scaler, RHS: H2OFrame
     res = 2 + iris
     res_rows, res_cols = res.dim()
+    res = h2o.as_list(res)
     assert res_rows == rows and res_cols == cols, "dimension mismatch"
-    for x, y in zip([sum([res[r,c] for r in range(rows)]) for c in range(cols-1)], [1176.5, 758.1, 863.8, 479.8]):
-        assert abs(x.eager() - y) < 1e-1,  "expected same values"
+    for x, y in zip([sum([res[r][c] for r in range(rows)]) for c in range(cols-1)], [469.9, 342.6, 266.9, 162.2]):
+        assert abs(x - y) < 1e-1,  "expected same values"
 
     # LHS: scaler, RHS: H2OVec
-    res = 2 + iris[1]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 758.1) < 1e-1, "expected same values"
+    res = h2o.as_list(2 + iris[1])
+    assert abs(sum([res[i][0] for i in range(rows)]) - 342.6) < 1e-1, "expected same values"
 
     # LHS: scaler, RHS: Expr
     res = 2 + iris[0]
-    res2 = 1.1 + res[133]
-    assert abs(res2.eager() - 9.4) < 1e-1, "expected same values"
+    res2 = h2o.as_list(1.1 + res[21])
+    assert abs(res2 - 8.2) < 1e-1, "expected same values"
 
     ###################################################################
 
     # LHS: Expr, RHS: H2OFrame
-    try:
-        res = 1.2 + iris[2]
-        res2 = res[133] + iris
-        res2.show()
-        assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 + iris[2]
+    res2 = res[21] + iris
+    res2.show()
+
 
     # LHS: Expr, RHS: H2OVec
-    try:
-        res = 1.2 + iris[2]
-        res2 = res[133] + iris[1]
-        res2.show()
-        #assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 + iris[2]
+    res2 = res[21] + iris[1]
+    res2.show()
 
     # LHS: Expr, RHS: Expr
     res = 1.1 + iris[2]
-    res2 = res[133] + res[10]
-    assert abs(res2.eager() - 8.8) < 1e-1, "expected same values"
+    res2 = h2o.as_list(res[21] + res[10])
+    assert abs(res2 - 5.2) < 1e-1, "expected same values"
 
     # LHS: Expr, RHS: scaler
     res = 2 + iris[0]
-    res2 = res[133] + 3
-    assert abs(res2.eager() - 11.3) < 1e-1, "expected same values"
+    res2 = h2o.as_list(res[21] + 3)
+    assert abs(res2 - 10.1) < 1e-1, "expected same values"
 
     ###################################################################
 
@@ -69,24 +64,20 @@ def binop_plus(ip,port):
         pass
 
     # LHS: H2OVec, RHS: H2OVec
-    res = iris[0] + iris[1]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 1334.6) < 1e-1, "expected same values"
+    res = h2o.as_list(iris[0] + iris[1])
+    assert abs(sum([res[i][0] for i in range(rows)]) - 552.5) < 1e-1, "expected same values"
 
-    res = iris[2] + iris[1]
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 1021.9) < 1e-1, "expected same values"
+    res = h2o.as_list(iris[2] + iris[1])
+    assert abs(sum([res[i][0] for i in range(rows)]) - 349.5) < 1e-1, "expected same values"
 
     # LHS: H2OVec, RHS: Expr
-    try:
-        res = 1.2 + iris[2]
-        res2 = iris[1] + res[133]
-        res2.show()
-        #assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 + iris[2]
+    res2 = iris[1] + res[21]
+    res2.show()
 
     # LHS: H2OVec, RHS: scaler
-    res = iris[0] + 2
-    assert abs(sum([res[i] for i in range(rows)]).eager() - 1176.5) < 1e-2, "expected different column sum"
+    res = h2o.as_list(iris[0] + 2)
+    assert abs(sum([res[i][0] for i in range(rows)]) - 469.9) < 1e-2, "expected different column sum"
 
     ###################################################################
 
@@ -115,20 +106,17 @@ def binop_plus(ip,port):
         pass
 
     # LHS: H2OFrame, RHS: Expr
-    try:
-        res = 1.2 + iris[2]
-        res2 = iris + res[133]
-        res2.show()
-        assert False, "expected error. objects with different dimensions not supported."
-    except EnvironmentError:
-        pass
+    res = 1.2 + iris[2]
+    res2 = iris + res[21]
+    res2.show()
 
     # LHS: H2OFrame, RHS: scaler
     res = iris + 2
     res_rows, res_cols = res.dim()
+    res = h2o.as_list(res)
     assert res_rows == rows and res_cols == cols, "dimension mismatch"
-    for x, y in zip([sum([res[r,c] for r in range(rows)]) for c in range(cols-1)], [1176.5, 758.1, 863.8, 479.8]):
-        assert abs(x.eager() - y) < 1e-1,  "expected same values"
+    for x, y in zip([sum([res[r][c] for r in range(rows)]) for c in range(cols-1)], [469.9, 342.6, 266.9, 162.2]):
+        assert abs(x - y) < 1e-1,  "expected same values"
 
     ###################################################################
 
