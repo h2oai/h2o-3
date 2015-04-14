4481fd627feebae378c3193c2c804fb6aadae850
eric eckstrand <eric.eckstrand@gmail.com>
2015-04-14 11:30:21 -0700

    updated test so that it doesn't fail intermittently.

--- h2o-py/tests/testdir_jira/pyunit_pub_444_spaces_in_filenames.py
+++ h2o-py/tests/testdir_jira/pyunit_pub_444_spaces_in_filenames.py
@@ -11,25 +11,24 @@ def pub_444_spaces_in_filenames(ip,port):
     f1 = open(h2o.locate("smalldata/jira/") + "foo .csv", "w")
     f1.write("response, predictor\n")
     for i in range(10):
-        f1.write("1, a\n");
-        f1.write("0, b\n");
-        f1.write("1, a\n" if random.randint(0,1) else "0, b\n");
-        f1.write("1, a\n" if random.randint(0,1) else "0, b\n");
+        f1.write("1, a\n")
+        f1.write("0, b\n")
+        f1.write("1, a\n" if random.randint(0,1) else "0, b\n")
     f1.close()
 
     f2 = open(h2o.locate("smalldata/jira/") + "b a r .csv", "w")
     f2.write("response, predictor\n")
     for i in range(10):
-        f1.write("1, a\n");
-        f1.write("0, b\n");
-        f2.write("1, a\n" if random.randint(0,1) else "0, b\n");
+        f2.write("1, a\n")
+        f2.write("0, b\n")
+        f2.write("1, a\n" if random.randint(0,1) else "0, b\n")
     f2.close()
 
     f3 = open(h2o.locate("smalldata/jira/") + " ba z.csv", "w")
     for i in range(10):
-        f1.write("1, a\n");
-        f1.write("0, b\n");
-        f3.write("1, a\n" if random.randint(0,1) else "0, b\n");
+        f3.write("1, a\n")
+        f3.write("0, b\n")
+        f3.write("1, a\n" if random.randint(0,1) else "0, b\n")
     f3.close()
 
     train_data = h2o.upload_file(path=h2o.locate("smalldata/jira/foo .csv"))
