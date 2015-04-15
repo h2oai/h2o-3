12779e32781a52ea2dae122db59e1a466372c6db
Arno Candel <arno.candel@gmail.com>
2015-04-14 21:13:39 -0700

    HEXDEV-150: Fix Runit for K-Means.

--- h2o-r/tests/testdir_algos/kmeans/runit_kmeans_parameters.R
+++ h2o-r/tests/testdir_algos/kmeans/runit_kmeans_parameters.R
@@ -30,7 +30,8 @@ check.verify.parameters.slot <- function(conn) {
     Log.info("check that we can replace the old model, and the destination_key parameter is mapped from Key<Model> to character properly")
 
     iris.km.cpy <- do.call("h2o.kmeans", parameters_unmunged)
-    expect_equal(length(h2o.ls()[,1]), 3)
+    print(h2o.ls()[,1])
+    expect_equal(length(h2o.ls()[,1]), 5)
     testEnd()
 }
 
