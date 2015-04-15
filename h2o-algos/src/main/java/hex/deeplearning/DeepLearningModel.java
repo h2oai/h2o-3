7c0bce3068ebe2dd893f5beca2891f2a22416362
Arno Candel <arno.candel@gmail.com>
2015-04-14 21:14:14 -0700

    Fix logic in model metrics description for DL, only needed when actually downsampling.

--- h2o-algos/src/main/java/hex/deeplearning/DeepLearningModel.java
+++ h2o-algos/src/main/java/hex/deeplearning/DeepLearningModel.java
@@ -1695,7 +1695,7 @@ public class DeepLearningModel extends SupervisedModel<DeepLearningModel,DeepLea
           err.training_MSE = mm1._MSE;
           err.training_R2 = mm1.r2();
           _output._training_metrics = mm1;
-          if (get_params()._score_training_samples != 0 && get_params()._score_training_samples != ftrain.numRows()) {
+          if (get_params()._score_training_samples != 0 && get_params()._score_training_samples < ftrain.numRows()) {
             _output._training_metrics._description = "Metrics reported on " + ftrain.numRows() + " training set samples";
           }
 
