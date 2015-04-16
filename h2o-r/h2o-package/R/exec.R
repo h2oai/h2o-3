0db9a3716ecf573ef4b3c71ec1116cc8b27e62c6
SpencerA <spnrpa@gmail.com>
2015-04-15 20:08:20 -0700

    force gc more frequently

--- h2o-r/h2o-package/R/exec.R
+++ h2o-r/h2o-package/R/exec.R
@@ -211,7 +211,7 @@ function(conn, ast) {
     ret <- res$scalar
     if (ret == "NaN") ret <- NA_real_
   }
-
+  gc()
   ret
 }
 
@@ -224,7 +224,7 @@ function(conn, ast, key=.key.make(conn, "rapids"), linkToGC=FALSE) {
   # Process the results
   res <- .h2o.__remoteSend(conn, .h2o.__RAPIDS, ast=ast, method = "POST")
   if (!is.null(res$error)) stop(paste0("Error From H2O: ", res$error), call.=FALSE)
-
+  gc()
   h2o.getFrame(key, conn, linkToGC=linkToGC)
 }
 
@@ -239,6 +239,7 @@ function(conn, ast, key, finalizers) {
 
   res <- h2o.getFrame(key, conn, linkToGC=FALSE)
   res@finalizers <- finalizers
+  gc()
   res
 }
 
@@ -279,6 +280,7 @@ function(x, scalarAsFrame = TRUE) {
 #' Convenient to have a POST function method.
 .h2o.post.function<-
 function(fun.ast) {
+  gc()
   expr <- .fun.visitor(fun.ast)
   .h2o.__remoteSend(h2o.getConnection(), .h2o.__RAPIDS, fun=expr$ast, method = "POST")
 }
