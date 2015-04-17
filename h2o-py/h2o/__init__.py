6108234dce07c14dbb021e9190f5726ca1371e28
seba-1511 <seba-1511@hotmail.com>
2015-04-16 20:19:11 -0700

    Changed back the python SUBST_PROJECT_VERSION

--- h2o-py/h2o/__init__.py
+++ h2o-py/h2o/__init__.py
@@ -195,7 +195,7 @@ Metrics
 * See more in the chapter on Metrics
 
 """
-__version__ = "0.3.0.99999"
+__version__ = "SUBST_PROJECT_VERSION"
 from h2o import *
 from model import *
 from frame import H2OFrame
