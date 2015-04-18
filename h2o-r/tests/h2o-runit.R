55f97d0417bf2ff82144aee416889b7d495188bd
Sebastian Vidrio <sebastian@0xdata.com>
2015-04-17 20:35:58 -0700

    testing changes

--- h2o-r/tests/h2o-runit.R
+++ h2o-r/tests/h2o-runit.R
@@ -118,7 +118,7 @@ function(ROOT.PATH) {
 #'
 src.utils<-
 function(ROOT.PATH) {
-  to_src <- c("/h2oR.R", "/setupR.R", "/pcaR.R", "/glmR.R", "/gbmR.R", "/kmeansR.R", "/naivebayesR.R", "/utilsR.R")
+  to_src <- c("/h2oR.R", "/setupR.R", "/pcaR.R", "/deeplearningR.R", "/glmR.R", "/gbmR.R", "/kmeansR.R", "/naivebayesR.R", "/utilsR.R")
   invisible(lapply(to_src,function(x){source(paste(ROOT.PATH, x, sep = ""))}))
 }
 
