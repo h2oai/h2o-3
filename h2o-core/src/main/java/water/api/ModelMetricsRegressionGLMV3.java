fc49857f14b5f73bf9f37e570974577ac0150c56
Tomas Nykodym <tomas.nykodym@gmail.com>
2015-04-21 11:01:22 -0700

    GLM metrics schema fix.

--- h2o-core/src/main/java/water/api/ModelMetricsRegressionGLMV3.java
+++ h2o-core/src/main/java/water/api/ModelMetricsRegressionGLMV3.java
@@ -8,23 +8,22 @@ import water.api.API.Direction;
  */
 public class ModelMetricsRegressionGLMV3 extends ModelMetricsRegressionV3<ModelMetricsRegressionGLM, ModelMetricsRegressionGLMV3> {
 
-    @API(help="residual deviance",direction = Direction.OUTPUT)
-    public double residual_deviance;
+  @API(help = "residual deviance", direction = Direction.OUTPUT)
+  public double residual_deviance;
 
-    @API(help="null deviance",direction = Direction.OUTPUT)
-    public double null_deviance;
+  @API(help = "null deviance", direction = Direction.OUTPUT)
+  public double null_deviance;
 
-    @API(help="AIC",direction = Direction.OUTPUT)
-    public double AIC;
+  @API(help = "AIC", direction = Direction.OUTPUT)
+  public double AIC;
 
-    @Override
-    public ModelMetricsRegressionGLMV3 fillFromImpl(ModelMetricsRegressionGLM modelMetrics) {
-      super.fillFromImpl(modelMetrics);
-      this.AIC = modelMetrics._AIC;
-      this.residual_deviance = modelMetrics._resDev;
-      this.null_deviance = modelMetrics._nullDev;
-      return this;
-    }
-
+  @Override
+  public ModelMetricsRegressionGLMV3 fillFromImpl(ModelMetricsRegressionGLM modelMetrics) {
+    super.fillFromImpl(modelMetrics);
+    this.AIC = modelMetrics._AIC;
+    this.residual_deviance = modelMetrics._resDev;
+    this.null_deviance = modelMetrics._nullDev;
+    return this;
   }
+
 }
