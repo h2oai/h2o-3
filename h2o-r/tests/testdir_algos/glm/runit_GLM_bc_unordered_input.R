54f46ec4d89f52e7d9dc7b194bf6050c0c481746
eric eckstrand <eric.eckstrand@gmail.com>
2015-04-22 16:11:01 -0700

    added logic to glm-bc runits so that they won't fail locally when using h2o-server-customer-data

--- h2o-r/tests/testdir_algos/glm/runit_GLM_bc_unordered_input.R
+++ h2o-r/tests/testdir_algos/glm/runit_GLM_bc_unordered_input.R
@@ -6,42 +6,47 @@ source('../../h2o-runit.R')
 
 test <- function(conn) {
   ## Import data
-  h2oData <- h2o.importFile(conn, locate("/mnt/0xcustomer-datasets/c27/data.csv"))
-  bc <- h2o.importFile(conn, locate("/mnt/0xcustomer-datasets/c27/constraints_indices.csv"))
-  bc <- bc[1:(nrow(bc)-1),] # remove intercept
-  bc <- as.data.frame(bc)
+  if (!file.exists("/mnt/0xcustomer-datasets/c27/data.csv")) {
+      Log.info("h2o-only data")
+      testEnd()
+  } else {
+    h2oData <- h2o.importFile(conn, "/mnt/0xcustomer-datasets/c27/data.csv")
+    bc <- h2o.importFile(conn, "/mnt/0xcustomer-datasets/c27/constraints_indices.csv")
+    bc <- bc[1:(nrow(bc)-1),] # remove intercept
+    bc <- as.data.frame(bc)
 
-  ## Set Parameters
-  indVars <-  as.character(bc[1:nrow(bc), "names"])
-  depVars <- "C3"
-  totRealProb <- 0.002912744
-  lambda <- 0
-  alpha <- 0
-  family_type <- "binomial"
+    ## Set Parameters
+    indVars <-  as.character(bc[1:nrow(bc), "names"])
+    depVars <- "C3"
+    totRealProb <- 0.002912744
+    lambda <- 0
+    alpha <- 0
+    family_type <- "binomial"
 
-  ## Take subset of data
-  Log.info("Subset dataset to only predictor and response variables...")
-  h2oData <- h2oData[,c(indVars, depVars)]
+    ## Take subset of data
+    Log.info("Subset dataset to only predictor and response variables...")
+    h2oData <- h2oData[,c(indVars, depVars)]
 
-  ## Run GLM
-  run_glm <- function(data, beta_constraints){
-    h2o.glm(x = indVars, y = depVars, training_frame = data, family = family_type, prior = totRealProb,
-            use_all_factor_levels = T, lambda = lambda, alpha = alpha, beta_constraints = beta_constraints)
+    ## Run GLM
+    run_glm <- function(data, beta_constraints){
+      h2o.glm(x = indVars, y = depVars, training_frame = data, family = family_type, prior = totRealProb,
+              use_all_factor_levels = T, lambda = lambda, alpha = alpha, beta_constraints = beta_constraints)
+    }
+
+    Log.info("Run GLM with original data and original constraints.")
+    a <- run_glm(data = h2oData, beta_constraints = bc)
+
+    Log.info("Run GLM with reordered data and original constraints.")
+    b <- run_glm(data = h2oData, beta_constraints = bc)
+
+    Log.info("Run GLM with reordered data and reordered beta constraints ")
+    bc2 <- rbind(bc[6:nrow(bc),], bc[1:5,])
+    c <- run_glm(data = h2oData, beta_constraints = bc2)
+
+    checkEqualsNumeric(a@model$coefficients_table$coefficients, b@model$coefficients_table$coefficients)
+    checkEqualsNumeric(b@model$coefficients_table$coefficients, c@model$coefficients_table$coefficients)
+    testEnd()
   }
-
-  Log.info("Run GLM with original data and original constraints.")
-  a <- run_glm(data = h2oData, beta_constraints = bc)
-
-  Log.info("Run GLM with reordered data and original constraints.")
-  b <- run_glm(data = h2oData, beta_constraints = bc)
-
-  Log.info("Run GLM with reordered data and reordered beta constraints ")
-  bc2 <- rbind(bc[6:nrow(bc),], bc[1:5,])
-  c <- run_glm(data = h2oData, beta_constraints = bc2)
-
-  checkEqualsNumeric(a@model$coefficients_table$coefficients, b@model$coefficients_table$coefficients)
-  checkEqualsNumeric(b@model$coefficients_table$coefficients, c@model$coefficients_table$coefficients)
-  testEnd()
 }
 
 doTest("GLM Test: Beta Constraints with Priors", test)
