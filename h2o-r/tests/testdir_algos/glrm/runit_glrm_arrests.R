7d397a6524f8e068e51955057f6d5549fb19036b
anqi <anqi@h2o.ai>
2015-04-22 12:49:49 -0700

    Forgot to change path to h2o-runit.R

--- h2o-r/tests/testdir_algos/glrm/runit_glrm_arrests.R
+++ h2o-r/tests/testdir_algos/glrm/runit_glrm_arrests.R
@@ -1,5 +1,5 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
-source('../h2o-runit.R')
+source('../../h2o-runit.R')
 
 test.glrmvanilla.golden <- function(conn) {
   Log.info("Importing arrests.csv data...") 
