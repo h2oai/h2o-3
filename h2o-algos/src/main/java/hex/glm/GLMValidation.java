cdc4a3b85a7573808cf9a3c1aa6d118a7ed4fff7
Tomas Nykodym <tomas.nykodym@gmail.com>
2015-04-24 15:34:48 -0700

    PUBDEV-922 - fixed glm scoring.

--- h2o-algos/src/main/java/hex/glm/GLMValidation.java
+++ h2o-algos/src/main/java/hex/glm/GLMValidation.java
@@ -44,9 +44,9 @@ public class GLMValidation extends MetricBuilderBinomial<GLMValidation> {
   @Override public double[] perRow(double ds[], float[] yact, Model m) {
     _metricBuilder.perRow(ds,yact,m);
     if(_glm._family == Family.binomial)
-      add2(ds[2],yact[0]);
+      add2(yact[0],ds[2]);
     else
-      add2(ds[0],yact[0]);
+      add2(yact[0],ds[0]);
     return ds;
   }
 
