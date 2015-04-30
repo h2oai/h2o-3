0d92c7cdf3d54915f5cb0681e873c64f8a731343
Cliff Click <cliffc@acm.org>
2015-04-30 11:33:15 -0700

    Count tree leaves correctly
    simplify a bunch of logic around dead node handling

--- h2o-algos/src/main/java/hex/tree/TreeStats.java
+++ h2o-algos/src/main/java/hex/tree/TreeStats.java
@@ -15,21 +15,17 @@ public class TreeStats extends Iced {
   transient long _sum_depth = 0;
   transient long _sum_leaves = 0;
   public boolean isValid() { return _min_depth <= _max_depth; }
-  public void updateBy(DTree[] ktrees) {
-    if( ktrees==null ) return;
-    for( int i=0; i<ktrees.length; i++ ) {
-      DTree tree = ktrees[i];
-      if( tree == null ) continue;
-      if( _min_depth > tree._depth ) _min_depth = tree._depth;
-      if( _max_depth < tree._depth ) _max_depth = tree._depth;
-      if( _min_leaves > tree._leaves) _min_leaves = tree._leaves;
-      if( _max_leaves < tree._leaves) _max_leaves = tree._leaves;
-      _sum_depth += tree._depth;
-      _sum_leaves += tree._leaves;
-      _num_trees++;
-      _mean_depth = ((float) _sum_depth / _num_trees);
-      _mean_leaves = ((float) _sum_leaves / _num_trees);
-    }
+  public void updateBy(DTree tree) {
+    if( tree == null ) return;
+    if( _min_depth > tree._depth ) _min_depth = tree._depth;
+    if( _max_depth < tree._depth ) _max_depth = tree._depth;
+    if( _min_leaves > tree._leaves) _min_leaves = tree._leaves;
+    if( _max_leaves < tree._leaves) _max_leaves = tree._leaves;
+    _sum_depth += tree._depth;
+    _sum_leaves += tree._leaves;
+    _num_trees++;
+    _mean_depth = ((float) _sum_depth / _num_trees);
+    _mean_leaves = ((float) _sum_leaves / _num_trees);
   }
 
   public void setNumTrees(int i) { _num_trees = i; }
