32adcdeb14749092f5aadf3d32e765ec9ff73e17
SpencerA <spnrpa@gmail.com>
2015-05-02 14:47:15 -0700

    uncomment necessary install lines!

--- h2o-r/demos/kaggle/otto/beating_the_benchmark.R
+++ h2o-r/demos/kaggle/otto/beating_the_benchmark.R
@@ -11,21 +11,21 @@
 ######################################################################
 
 # The following two commands remove any previously installed H2O packages for R.
-#if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
-#if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
-#
-## Next, we download packages that H2O depends on.
-#if (! ("methods" %in% rownames(installed.packages()))) { install.packages("methods") }
-#if (! ("statmod" %in% rownames(installed.packages()))) { install.packages("statmod") }
-#if (! ("stats" %in% rownames(installed.packages()))) { install.packages("stats") }
-#if (! ("graphics" %in% rownames(installed.packages()))) { install.packages("graphics") }
-#if (! ("RCurl" %in% rownames(installed.packages()))) { install.packages("RCurl") }
-#if (! ("rjson" %in% rownames(installed.packages()))) { install.packages("rjson") }
-#if (! ("tools" %in% rownames(installed.packages()))) { install.packages("tools") }
-#if (! ("utils" %in% rownames(installed.packages()))) { install.packages("utils") }
-#
-## Now we download, install and initialize the H2O package for R.
-#install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o-dev/master/1112/R")))
+if ("package:h2o" %in% search()) { detach("package:h2o", unload=TRUE) }
+if ("h2o" %in% rownames(installed.packages())) { remove.packages("h2o") }
+
+# Next, we download packages that H2O depends on.
+if (! ("methods" %in% rownames(installed.packages()))) { install.packages("methods") }
+if (! ("statmod" %in% rownames(installed.packages()))) { install.packages("statmod") }
+if (! ("stats" %in% rownames(installed.packages()))) { install.packages("stats") }
+if (! ("graphics" %in% rownames(installed.packages()))) { install.packages("graphics") }
+if (! ("RCurl" %in% rownames(installed.packages()))) { install.packages("RCurl") }
+if (! ("rjson" %in% rownames(installed.packages()))) { install.packages("rjson") }
+if (! ("tools" %in% rownames(installed.packages()))) { install.packages("tools") }
+if (! ("utils" %in% rownames(installed.packages()))) { install.packages("utils") }
+
+# Now we download, install and initialize the H2O package for R.
+install.packages("h2o", type="source", repos=(c("http://h2o-release.s3.amazonaws.com/h2o-dev/master/1112/R")))
 
 
 ######################################################################
