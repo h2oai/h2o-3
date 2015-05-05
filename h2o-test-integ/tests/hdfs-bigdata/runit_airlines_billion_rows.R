dc4c5e54dfaaea64123fb484d758a4ac4dd4230d
Neeraja Madabhushi <neeraja@h2o.ai>
2015-05-04 19:18:53 -0700

    Added more distributions and split frame

--- h2o-test-integ/tests/hdfs-bigdata/runit_airlines_billion_rows.R
+++ h2o-test-integ/tests/hdfs-bigdata/runit_airlines_billion_rows.R
@@ -28,6 +28,7 @@ hdfs_data_file = "/datasets/airlinesbillion.csv"
 heading("Testing single file importHDFS")
 url <- sprintf("hdfs://%s%s", hdfs_name_node, hdfs_data_file)
 data.hex <- h2o.importFile(conn, url)
+data1.hex <- data.hex
 
 n <- nrow(data.hex)
 print(n)
@@ -35,18 +36,26 @@ if (n != 1166952590) {
     stop("nrows is wrong")
 }
 
-## Chose which col as response
+#Constructing validation and train sets by sampling (20/80)
+#creating a column as tall as airlines(nrow(air))
+s <- h2o.runif(air)    # Useful when number of rows too large for R to handle
+data.train <- data.hex[s <= 0.8,]
+data.valid <- data.hex[s > 0.8,]
+
 ## Response = Distance 
 myY = "C19"
 myX = setdiff(names(data.hex), myY)
 ## Build GLM Model and compare AUC with h2o1
-data_admm.glm <- h2o.glm(x = myX, y = myY, training_frame = data.hex, family = "gaussian", solver = "IRLSM")
-data_lbfgs.glm <- h2o.glm(x = myX, y = myY, training_frame = data.hex, family = "gaussian", solver = "L_BFGS")
+data_irlsm.glm <- h2o.glm(x = myX, y = myY, training_frame = data.train, validation_frame=data.valid, family = "gaussian", solver = "IRLSM")
+data_lbfgs.glm <- h2o.glm(x = myX, y = myY, training_frame = data.train, validation_frame=data.valid, family = "gaussian", solver = "L_BFGS")
 
 ## Chose which col as response
 ## Response = IsDepDelayed
 myY = "C31"
-myX = setdiff(names(data.hex), myY)
-data.gbm <- h2o.gbm(x = myX, y = myY, training_frame = data.hex, distribution = "AUTO")
+myX = setdiff(names(data1.hex), myY)
+data1.gbm <- h2o.gbm(x = myX, y = myY, training_frame = data.train, validation_frame=data.valid, distribution = "AUTO")
+data2.gbm <- h2o.gbm(x = myX, y = myY, training_frame = data.train, validation_frame=data.valid, distribution = "gaussian")
+data3.gbm <- h2o.gbm(x = myX, y = myY, training_frame = data.train, validation_frame=data.valid, distribution = "bernoulli")
+data4.gbm <- h2o.gbm(x = myX, y = myY, training_frame = data.train, validation_frame=data.valid, distribution = "multinomial
 
 PASS_BANNER()
