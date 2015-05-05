a555acb4ee10ad6d86dbb11b58da3e13cfab6226
Arno Candel <arno.candel@gmail.com>
2015-05-05 00:06:09 -0700

    PUBDEV-987: Proper fix.

--- h2o-algos/src/main/java/hex/deeplearning/DeepLearningModel.java
+++ h2o-algos/src/main/java/hex/deeplearning/DeepLearningModel.java
@@ -2007,7 +2007,7 @@ public class DeepLearningModel extends SupervisedModel<DeepLearningModel,DeepLea
     Vec[] vecs = adaptFrm.anyVec().makeZeros(features);
 
     Scope.enter();
-    adaptTestForTrain(_output._names, null /*don't skip response*/, _output._domains, frame, _parms.missingColumnsType(), true);
+    adaptTestForTrain(_output._names, null /*don't skip response*/, _output._domains, adaptFrm, _parms.missingColumnsType(), true);
     for (int j=0; j<features; ++j) {
       adaptFrm.add("DF.L"+(layer+1)+".C" + (j+1), vecs[j]);
     }
