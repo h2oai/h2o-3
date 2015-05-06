990eecd5420886b8504d89b5fc93f112ca0a8b59
Neeraja Madabhushi <neeraja@h2o.ai>
2015-05-06 16:20:33 -0700

    Narrowing issue

--- h2o-test-integ/tests/hdfs-bigdata/runit_376KRows_6KCols.R
+++ h2o-test-integ/tests/hdfs-bigdata/runit_376KRows_6KCols.R
@@ -17,11 +17,6 @@ library(h2o)
 
 running_inside_hexdata = file.exists("/mnt/0xcustomer-datasets/c28")
 
-if (!running_inside_hexdata) {
-    # hdp2.2 cluster
-    stop("0xdata internal test and data.")
-}
-
 heading("BEGIN TEST")
 conn <- h2o.init(ip=myIP, port=myPort, startH2O = FALSE)
 h2o.removeAll()
@@ -30,7 +25,7 @@ h2o.ls(conn)
 #----------------------------------------------------------------------
 # Parameters for the test.
 #----------------------------------------------------------------------
-data.hex <- h2o.importFile(conn, "/mnt/0xcustomer-datasets/c28/mr_output.tsv.sorted.gz", key="lob_mr_data)
+data.hex <- h2o.importFile(conn, "/mnt/0xcustomer-datasets/c28/mr_output.tsv.sorted.gz")
 
 dim(data.hex)
 
@@ -38,11 +33,10 @@ s = h2o.runif(data.hex)
 train = data.hex[s <= 0.8,]
 valid = data.hex[s > 0.8,]
 
-model.glm <- h2o.glm(x = 3:(ncol(train)), y = 2,
-                     training_frame = train, validation_frame=valid, family = "binomial", solver = "L_BFGS")
+#model.glm <- h2o.glm(x = 3:(ncol(train)), y = 2, training_frame = train, validation_frame=valid, family = "binomial", solver = "L_BFGS")
 
-pred = predict(model.glm, valid)
-perf <- h2o.performance(model.glm, valid)
+#pred = predict(model.glm, valid)
+#perf <- h2o.performance(model.glm, valid)
 
 model.gbm <- h2o.gbm(x = 3:(ncol(train)), y = 2, training_frame = train, validation_frame=valid, ntrees=10, max_depth=5) 
 pred = predict(model.gbm, valid)
