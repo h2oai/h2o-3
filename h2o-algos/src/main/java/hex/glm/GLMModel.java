db53ab4d3ba0d1ced91ecdca20b4a9b7353bc9b9
Arno Candel <arno.candel@gmail.com>
2015-05-11 11:27:41 -0700

    Cosmetics in GLM POJO.

--- h2o-algos/src/main/java/hex/glm/GLMModel.java
+++ h2o-algos/src/main/java/hex/glm/GLMModel.java
@@ -861,11 +861,12 @@ public class GLMModel extends SupervisedModel<GLMModel,GLMModel.GLMParameters,GL
     body.ip("double mu = hex.genmodel.GenModel.GLM_").p(_parms._link.toString()).p("Inv(eta");
 //    if( _parms._link == hex.glm.GLMModel.GLMParameters.Link.tweedie ) body.p(",").p(_parms._tweedie_link_power);
     body.p(");").nl();
-    body.ip("preds[0] = mu;").nl();
-    if( _parms._family == Family.binomial ) { // threshold for prediction
-      body.ip("preds[0] = mu > ").p(_output._threshold).p(" ? 1 : 0);").nl();
+    if( _parms._family == Family.binomial ) {
+      body.ip("preds[0] = mu > ").p(_output._threshold).p(" ? 1 : 0); // threshold given by ROC").nl();
       body.ip("preds[1] = 1.0 - mu; // class 0").nl();
       body.ip("preds[2] =       mu; // class 1").nl();
+    } else {
+      body.ip("preds[0] = mu;").nl();
     }
   }
 
