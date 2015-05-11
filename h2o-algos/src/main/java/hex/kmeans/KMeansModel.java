54387a67aca6c1df4f71827a434c3f842dd4e749
Arno Candel <arno.candel@gmail.com>
2015-05-10 22:55:03 -0700

    Always show K-Means POJO model, shouldn't be too big.

--- h2o-algos/src/main/java/hex/kmeans/KMeansModel.java
+++ h2o-algos/src/main/java/hex/kmeans/KMeansModel.java
@@ -80,4 +80,9 @@ public class KMeansModel extends ClusteringModel<KMeansModel,KMeansModel.KMeansP
       bodySb.ip("preds[0] = KMeans_closest(CENTERS,data,DOMAINS,null,null);").nl(); // at function level
     }
   }
+
+  @Override
+  protected boolean toJavaCheckTooBig() {
+    return false;
+  }
 }
