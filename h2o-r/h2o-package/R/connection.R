50fbf2499ccd8daccc6bf9ae41303b4f6de6cf34
SpencerA <spnrpa@gmail.com>
2015-05-14 00:56:02 -0700

    hail mary curl

--- h2o-r/h2o-package/R/connection.R
+++ h2o-r/h2o-package/R/connection.R
@@ -128,6 +128,9 @@ h2o.init <- function(ip = "127.0.0.1", port = 54321, startH2O = TRUE, forceDL = 
         print(rv$curlError)
         print(rv$httpStatusCode)
         print(rv$curlErrorMessage)
+
+        #try a hail mary curl
+        print(system("curl 'http://localhost:54321"))
         stop("H2O failed to start, stopping execution.")
       }
     } else
