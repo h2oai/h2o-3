76567a62b472d4f6dcfd05269befa492a869fff4
Arno Candel <arno.candel@gmail.com>
2015-05-20 14:43:40 -0700

    PUBDEV-1169 - Make binomial DRF twice as fast by building only 1 tree and using 1-p for the other class' probability. Same as for GBM.
    Note: We the 2x in speedup, we can now afford more trees, which should lead to higher accuracy for the same overall model building cost.

--- h2o-algos/src/test/java/hex/tree/drf/DRFTest.java
+++ h2o-algos/src/test/java/hex/tree/drf/DRFTest.java
@@ -102,9 +102,9 @@ public class DRFTest extends TestUtil {
             1,
             20,
             a(a(3,   0, 0,  0,  0),
-              a(2, 177, 1,  4,  0),
+              a(2, 178, 1,  3,  0),
               a(0,   1, 1,  0,  0),
-              a(0,   2, 2, 69,  1),
+              a(0,   3, 2, 68,  1),
               a(0,   0, 0,  3, 87)),
             s("3", "4", "5", "6", "8"));
   }
@@ -428,6 +428,9 @@ public class DRFTest extends TestUtil {
       test = parse_test_file(fnametrain);
       res = model.score(test);
 
+      // Build a POJO, validate same results
+      Assert.assertTrue(model.testJavaScoring(test,res,1e-15));
+
       if (classification && expCM != null) {
         Assert.assertTrue("Expected: " + Arrays.deepToString(expCM) + ", Got: " + Arrays.deepToString(mm.cm()._cm),
                 Arrays.deepEquals(mm.cm()._cm, expCM));
@@ -443,9 +446,6 @@ public class DRFTest extends TestUtil {
       }
 
       hex.ModelMetrics.getFromDKV(model, test);
-
-      // Build a POJO, validate same results
-      Assert.assertTrue(model.testJavaScoring(test,res,1e-15));
 
     } finally {
       if (frTrain!=null) frTrain.remove();
@@ -569,7 +569,7 @@ public class DRFTest extends TestUtil {
       Log.info("trial: " + i + " -> MSE: " + mses[i]);
     }
     for (int i=0; i<mses.length; ++i) {
-      assertEquals(0.20430759039450014, mses[i], 1e-4); //check for the same result on 1 nodes and 5 nodes
+      assertEquals(0.2093151515479678, mses[i], 1e-4); //check for the same result on 1 nodes and 5 nodes
     }
   }
 
