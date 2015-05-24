c5add83b41e3741d24e1c35a51c373c932bf91fb
Cliff Click <cliffc@acm.org>
2015-05-23 23:04:38 -0700

    first prim op executes

--- h2o-core/src/main/java/water/currents/ASTExec.java
+++ h2o-core/src/main/java/water/currents/ASTExec.java
@@ -11,14 +11,15 @@ class ASTExec extends AST {
   // apply the function.
   @Override Val exec( Env env ) {
     try( Env.StackHelp stk = env.stk()) {
-        for( Val val : _vals )
-          stk.push(val.exec(env)); // Args are evaled and pushed on stack in order
-        Val fun = env.peek(-_vals.length);
+        Val fun = _vals[0].exec(env);
         if( !fun.isFun() )
           throw new Exec.IllegalASTException("Expected a function but found "+fun.getClass());
-        // Validate stack depth: called primitive function pulls _vals.length
-        //return ((ValFun)fun)._ast.apply(env,_vals.length);
-        throw H2O.unimpl();
+        int oldsp = env.sp();
+        for( int i=1; i<_vals.length; i++ )
+          stk.push(_vals[i].exec(env)); // Args are evaled and pushed on stack in order
+        Val res = ((ValFun)fun)._ast.exec(env);
+        assert oldsp==env.sp();
+        return res;
       }
   }
 }
--- h2o-core/src/main/java/water/currents/ASTPrim.java
+++ h2o-core/src/main/java/water/currents/ASTPrim.java
@@ -1,15 +1,51 @@
 package water.currents;
 
 import water.H2O;
+import water.MRTask;
+import water.fvec.Vec;
+import water.fvec.Chunk;
+import water.fvec.NewChunk;
 
 abstract class ASTPrim extends AST {
   ASTPrim() { super(null); }
 }
 
-class ASTPlus extends ASTPrim {
+abstract class ASTBinOp extends ASTPrim {
+  @Override Val exec( Env env ) {
+    try( Env.StackHelp stk = env.stk()) {
+        Val rite = stk.pop();
+        Val left = stk.pop();
+
+        switch( left.type() ) {
+        case Env.NUM: throw H2O.unimpl();
+        case Env.STR: throw H2O.unimpl();
+
+        case Env.VEC: 
+          Vec vec = ((ValVec)left)._vec;
+          switch( rite.type() ) {
+          case Env.NUM:
+            final double drt = ((ValNum)rite)._d;
+            return new ValVec(new MRTask() {
+                @Override public void map( Chunk clf, Chunk crt, NewChunk cres ) {
+                  for( int i=0; i<clf._len; i++ )
+                    cres.addNum(op(clf.atd(i),crt.atd(i)));
+                }
+              }.doAll(1,vec).outputFrame().vec(0));
+          case Env.STR: throw H2O.unimpl();
+          case Env.VEC: throw H2O.unimpl();
+          default: throw H2O.fail();
+          }
+          
+        default: throw H2O.fail();
+        }
+      }
+  }
+  abstract protected double op( double left, double rite );
+}
+
+// 
+class ASTPlus extends ASTBinOp {
   static { init(new ASTPlus()); }
   @Override public String toString() { return "+"; }
-  @Override Val exec( Env env ) {
-    throw H2O.unimpl();
-  }
+  @Override protected double op( double left, double rite ) { return left+rite; }
 }
