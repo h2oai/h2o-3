761b6ef5d75d7327d446e0b23e1fe74bc509b6a3
Arno Candel <arno.candel@gmail.com>
2015-05-24 15:01:21 -0700

    Add the seed back to GBM imbalanced test (was set to 0 by default before, now explicit).

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_imbalanced.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_imbalanced.R
@@ -5,8 +5,8 @@ test.gbm.imbalanced <- function(conn) {
   covtype <- h2o.uploadFile(conn, locate("smalldata/covtype/covtype.20k.data"))
   covtype[,55] <- as.factor(covtype[,55])
 
-  hh_imbalanced<-h2o.gbm(x=c(1:54),y=55,ntrees=10,min_rows=5,learn_rate=0.2,training_frame=covtype,distribution="multinomial",balance_classes=F)
-  hh_balanced  <-h2o.gbm(x=c(1:54),y=55,ntrees=10,min_rows=5,learn_rate=0.2,training_frame=covtype,distribution="multinomial",balance_classes=T)
+  hh_imbalanced<-h2o.gbm(x=c(1:54),y=55,ntrees=10,min_rows=5,learn_rate=0.2,training_frame=covtype,distribution="multinomial",balance_classes=F, seed=0)
+  hh_balanced  <-h2o.gbm(x=c(1:54),y=55,ntrees=10,min_rows=5,learn_rate=0.2,training_frame=covtype,distribution="multinomial",balance_classes=T, seed=0)
   hh_imbalanced_metrics <- h2o.performance(hh_imbalanced)
   hh_balanced_metrics   <- h2o.performance(hh_balanced  )
 
