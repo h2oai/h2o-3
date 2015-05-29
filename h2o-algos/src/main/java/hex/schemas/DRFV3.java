84fce9cfab37a6c4fd73a22e9258685bc2fb124e
Arno Candel <arno.candel@gmail.com>
2015-05-29 14:36:52 -0700

    PUBDEV-1169: Add back an option to build double trees for binomial DRF: _binomial_double_trees.

--- h2o-algos/src/main/java/hex/schemas/DRFV3.java
+++ h2o-algos/src/main/java/hex/schemas/DRFV3.java
@@ -10,7 +10,8 @@ public class DRFV3 extends SharedTreeV3<DRF,DRFV3, DRFV3.DRFParametersV3> {
     static public String[] own_fields = new String[] {
         "mtries",
         "sample_rate",
-        "build_tree_one_node"
+        "build_tree_one_node",
+        "binomial_double_trees"
     };
 
     // Input fields
@@ -22,5 +23,8 @@ public class DRFV3 extends SharedTreeV3<DRF,DRFV3, DRFV3.DRFParametersV3> {
 
     @API(help="Run on one node only; no network overhead but fewer cpus used.  Suitable for small datasets.")
     public boolean build_tree_one_node;
+
+    @API(help="For binary classification: Build 2x as many trees (one per class) - can lead to higher accuracy")
+    public boolean binomial_double_trees;
   }
 }
--- h2o-algos/src/main/java/hex/tree/drf/DRF.java
+++ h2o-algos/src/main/java/hex/tree/drf/DRF.java
@@ -231,7 +231,7 @@ public class DRF extends SharedTree<hex.tree.drf.DRFModel, hex.tree.drf.DRFModel
           // The Boolean Optimization
           // This optimization assumes the 2nd tree of a 2-class system is the
           // inverse of the first (and that the same columns were picked)
-          if( k==1 && _nclass==2 ) continue;
+          if( k==1 && _nclass==2 && !_parms._binomial_double_trees) continue;
           ktrees[k] = new DRFTree(fr, _ncols, (char)_parms._nbins, (char)_parms._nbins_cats, (char)_nclass, _parms._min_rows, mtrys, rseed);
           boolean isBinom = isClassifier();
           new DRFUndecidedNode(ktrees[k], -1, DHistogram.initialHist(fr, _ncols, adj_nbins, _parms._nbins_cats, hcs[k][0], isBinom)); // The "root" node
@@ -490,11 +490,11 @@ public class DRF extends SharedTree<hex.tree.drf.DRFModel, hex.tree.drf.DRFModel
   // turns the results into a probability distribution.
   @Override protected double score1( Chunk chks[], double fs[/*nclass*/], int row ) {
     double sum = 0;
-    if (_nclass > 2) { //multinomial
+    if (_nclass > 2 || (_nclass == 2 && _parms._binomial_double_trees) ) { //multinomial or binomial with 1 tree per class
       for (int k = 0; k < _nclass; k++)
         sum += (fs[k+1] = chk_tree(chks, k).atd(row));
     }
-    else if (_nclass==2) { //binomial optimization
+    else if (_nclass==2 && !_parms._binomial_double_trees) { //binomial optimization
       fs[1] = chk_tree(chks, 0).atd(row);
       assert(fs[1] >= 0 && fs[1] <= 1);
       fs[2] = 1. - fs[1];
