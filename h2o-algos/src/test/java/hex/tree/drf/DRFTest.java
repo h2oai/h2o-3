c651cb9bcdaf6eddf8b26d21a17ec66649f29cde
Arno Candel <arno.candel@gmail.com>
2015-05-29 17:50:04 -0700

    Fix expected test results after changing the default values.

--- h2o-algos/src/test/java/hex/tree/drf/DRFTest.java
+++ h2o-algos/src/test/java/hex/tree/drf/DRFTest.java
@@ -574,7 +574,7 @@ public class DRFTest extends TestUtil {
       Log.info("trial: " + i + " -> MSE: " + mses[i]);
     }
     for (int i=0; i<mses.length; ++i) {
-      assertEquals(0.208323733363106, mses[i], 1e-4); //check for the same result on 1 nodes and 5 nodes
+      assertEquals(0.2087662734069947, mses[i], 1e-4); //check for the same result on 1 nodes and 5 nodes
     }
   }
 
