b95e094c45630eca07d2f99e5a27ac9409603e28
Brandon Hill <brandon@0xdata.com>
2015-06-08 12:44:35 -0700

    Addresses PUBDEV-1361.  This was setting the timezone but never changing it back.  Occasionally this would give an unexpected date result in other tests if run on the same H2O server after this test.

--- h2o-r/tests/testdir_docexamples/runit_Rdoc_set_time_zone.R
+++ h2o-r/tests/testdir_docexamples/runit_Rdoc_set_time_zone.R
@@ -2,7 +2,9 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source('../h2o-runit.R')
 
 test.rdoc_settimezone.golden <- function(H2Oserver) {
-  h2o.setTimezone("Etc/UTC", H2OServer)
+  origTZ = h2o.getTimezone(H2Oserver)
+  h2o.setTimezone("Etc/UTC", H2Oserver)
+  h2o.setTimezone(as.character(origTZ[1,1]), H2Oserver)
   testEnd()
 }
 
