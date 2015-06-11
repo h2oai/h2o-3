74e7f6de084c69bdb54093d19fdb089401359ca2
Tomas Nykodym <tomas.nykodym@gmail.com>
2015-06-11 13:44:45 -0700

    GLM: updated null-model constant term fitting when running with offset

--- h2o-algos/src/main/java/hex/glm/GLM.java
+++ h2o-algos/src/main/java/hex/glm/GLM.java
@@ -441,7 +441,7 @@ public class GLM extends ModelBuilder<GLMModel,GLMParameters,GLMOutput> {
         _likelihood = glmIterationTask._likelihood;
         _ymu = glmIterationTask._beta[0];
         double ymu = glmIterationTask._xy[0]/glmIterationTask._gram.get(0,0);
-        if(++_iter < 50 && Math.abs(ymu - glmIterationTask._beta[0]) > _parms._beta_epsilon) {
+        if(++_iter < 100 && Math.abs(ymu - glmIterationTask._beta[0]) > _parms._beta_epsilon) {
           InitTsk.this.addToPendingCount(1);
           new GLMTask.GLMIterationTask(GLM.this._key,_nullDinfo,0,_parms,false,new double[]{ymu},0,_rowFilter, new NullModelIteration(_nullDinfo)).asyncExec(_nullDinfo._adaptedFrame);
         } else {
@@ -469,7 +469,7 @@ public class GLM extends ModelBuilder<GLMModel,GLMParameters,GLMOutput> {
           if(_dinfo._offset && _parms._intercept) {
             InitTsk.this.addToPendingCount(1);
             DataInfo dinfo = _dinfo.filterExpandedColumns(new int[]{});
-            new GLMIterationTask(GLM.this._key,dinfo,0,_parms,false,new double[]{0},0,_rowFilter, new NullModelIteration(dinfo)).asyncExec(dinfo._adaptedFrame);
+            new GLMIterationTask(GLM.this._key,dinfo,0,_parms,false,new double[]{_response.mean() - _offset.mean()},0,_rowFilter, new NullModelIteration(dinfo)).asyncExec(dinfo._adaptedFrame);
           } else
             computeGradients();
         }
