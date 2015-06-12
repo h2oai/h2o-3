f813260a665908609a3d4c4f9bee95dedca2151a
Amy Wang <amy@0xdata.com>
2015-06-12 12:44:38 -0700

    change VI test to take out redundant cals

--- h2o-r/tests/testdir_demos/runit_demo_VI_all_algos.R
+++ h2o-r/tests/testdir_demos/runit_demo_VI_all_algos.R
@@ -29,43 +29,42 @@ colnames(data.hex)
 myX <- 1:20
 myY <- "y"
 
+#--------------------------------------------------
 # Run GBM with variable importance
 my.gbm <- h2o.gbm(x = myX, y = myY, distribution = "bernoulli", training_frame = data.hex,
                   ntrees =100, max_depth = 2, learn_rate = 0.01)
 
 # Access Variable Importance from the built model
-gbm.VI <- h2o.varimp(my.gbm)
 print("Variable importance from GBM")
+gbm.VI <- h2o.varimp(my.gbm)
 print(gbm.VI)
 
-par(mfrow=c(2,2))
+
 # Plot variable importance from GBM
-barplot(t(gbm.VI[1]),las=2,main="VI from GBM")
+par(mfrow=c(2,2))
+barplot(gbm.VI$scaled_importance, names.arg = gbm.VI$variable, las=2,main="VI from GBM")
 
 #--------------------------------------------------
 # Run random Forest with variable importance
 my.rf <- h2o.randomForest(x=myX,y=myY,training_frame=data.hex,ntrees=100)
 
 # Access Variable Importance from the built model
-rf.VI <- h2o.varimp(my.rf)
 print("Variable importance from Random Forest")
+rf.VI <- h2o.varimp(my.rf)
 print(rf.VI)
 
-rf.VI <- rf.VI[order(rf.VI[1,],decreasing=T)]
-# RF variable importance Without normalization, i.e scale =T
+# RF variable importance Without normalization, i.e scale = F
 print("Variable importance from Random Forest without normalization")
-print(t(rf.VI[1,]))
-print(h2o.varimp(my.rf)[1,])
+print(t(rf.VI[,1:2]))
 
 # RF variable importance With normalization, i.e scale =T (divide mean decrease accuracy by standard deviation)
-norm_rf.VI <- h2o.varimp(my.rf)[1,]/h2o.varimp(my.rf)[2,]
-# Sort in decreasing order
-nrf.VI <- norm_rf.VI[order(norm_rf.VI[1,],decreasing=T)]
+norm_rf.VI <- rf.VI$relative_importance/max(rf.VI$relative_importance)
 print("Variable importance from Random Forest with normalization")
-print(t(nrf.VI[1,]))
+print(t(rf.VI[,c(1,3)]))
+checkEqualsNumeric(norm_rf.VI, rf.VI$scaled_importance)
 
 # Plot variable importance from Random Forest
-barplot(t(nrf.VI[1,]),beside=T,names.arg=row.names(t(nrf.VI[1,])),las=2,main="VI from RF")
+barplot(rf.VI$scaled_importance,beside=T,names.arg=rf.VI$variable,las=2,main="VI from RF")
 
 #--------------------------------------------------
 # Run GLM with variable importance, lambda search and using all factor levels
@@ -73,33 +72,30 @@ my.glm <- h2o.glm(x=myX, y=myY, training_frame=data.hex, family="binomial",stand
   lambda_search=T)
 
 # Select the best model picked by glm
-best_model <- my.glm@best_model
+# best_model <- my.glm@best_model
 
 # Get the normalized coefficients of the best model
-n_coeff <- abs(my.glm@models[[best_model]]@model$normalized_coefficients)
-
-# Access Variable Importance by removing the intercept term
-VI <- abs(n_coeff[-length(n_coeff)])
-
-glm.VI <- VI[order(VI,decreasing=T)]
+#n_coeff <- abs(my.glm@models[[best_model]]@model$normalized_coefficients)
+glm.VI <- my.glm@model$standardized_coefficients_magnitude
 print("Variable importance from GLM")
 print(glm.VI)
 
 # Plot variable importance from glm
-barplot(glm.VI[1:20],las=2,main="VI from GLM")
+barplot(glm.VI[1:20,]$coefficients, names.arg = glm.VI[1:20,]$names, las=2,main="VI from GLM")
 
 #--------------------------------------------------
 # Run deeplearning with variable importance
-my.dl <- h2o.deeplearning(x=myX,y=myY,data=data.hex,classification=T,activation="Tanh",hidden=c(10,10,10),epochs=12,variable_importances=T)
+my.dl <- h2o.deeplearning(x = myX, y = myY, training_frame = data.hex,
+                          activation = "Tanh", hidden = c(10,10,10),
+                          epochs = 12, variable_importances = T)
 
 # Access Variable Importance from the built model
-dl.VI <- my.dl@model$varimp
 print("Variable importance from Deep Learning")
+dl.VI <- my.dl@model$variable_importances
 print(dl.VI)
 
 # Plot variable importance from deeplearing
-barplot(t(dl.VI[1:20]),las=2,main="VI from Deep Learning")
-
+barplot(dl.VI$scaled_importance[1:20], names.arg = dl.VI$variable[1:20], las=2,main="VI from Deep Learning")
 
 testEnd()
 }
