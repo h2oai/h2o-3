1936ec538c17f75a22d599aa01ca85c083cfafe0
eric eckstrand <eric.eckstrand@gmail.com>
2015-06-16 11:04:31 -0700

    fix runit

--- h2o-r/tests/testdir_jira/runit_NOPASS_pub_1432_scale.R
+++ h2o-r/tests/testdir_jira/runit_NOPASS_pub_1432_scale.R
@@ -3,12 +3,17 @@ source('../h2o-runit.R')
 
 test <- function(conn) {
   foo <- as.h2o(iris)
-  scale(iris[,1:4],center=c(4,3,2,1), scale=c(1,1,1,1))
-  scale(iris[,1:4],center=c(4,3,2,1), scale=T)
-  scale(iris[,1:4],center=T, scale=c(1,1,1,1))
-  scale(iris[,1:4],center=c(4,3,2,1), scale=F)
-  scale(iris[,1:4],center=F, scale=c(1,1,1,1))
-
+  print(foo)
+  frame <- scale(foo[,1:4],center=c(4,3,2,1), scale=c(1,1,1,1))
+  print(frame)
+  frame <- scale(foo[,1:4],center=c(4,3,2,1), scale=T)
+  print(frame)
+  frame <- scale(foo[,1:4],center=T, scale=c(1,1,1,1))
+  print(frame)
+  frame <- scale(foo[,1:4],center=c(4,3,2,1), scale=F)
+  print(frame)
+  frame <- scale(foo[,1:4],center=F, scale=c(1,1,1,1))
+  print(frame)
   testEnd()
 }
 
