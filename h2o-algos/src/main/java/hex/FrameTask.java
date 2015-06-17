fcda60d9ec8df230b0372e43f8912ff4527ec6d4
Arno Candel <arno.candel@gmail.com>
2015-06-16 22:47:46 -0700

    PUBDEV-677: Fix AUC for non-integer row weights. PUBDEV-676: WIP towards row weights for DL (FrameTask).

--- h2o-algos/src/main/java/hex/FrameTask.java
+++ h2o-algos/src/main/java/hex/FrameTask.java
@@ -92,79 +92,75 @@ public abstract class FrameTask<T extends FrameTask<T>> extends MRTask<T>{
 
     Random skip_rng = null; //random generator for skipping rows
 
+    final boolean nontrivial_weights = _dinfo._weights && !_dinfo._adaptedFrame.vecs()[_dinfo.weightChunkId()].isConst();
+    final double global_weight_sum = nontrivial_weights ? _dinfo._adaptedFrame.vecs()[_dinfo.weightChunkId()].mean() * _dinfo._adaptedFrame.numRows() : 0;
+
+    DataInfo.Row row = _dinfo.newDenseRow();
+    double[] weight_map = null;
+    double relative_chunk_weight = 1;
+    //TODO: store node-local helper arrays in _dinfo -> avoid re-allocation and construction
+    if (nontrivial_weights) {
+      weight_map = new double[nrows];
+      double weight_sum = 0;
+      for (int i=0;i<nrows;++i) {
+        row = _dinfo.extractDenseRow(chunks, i, row);
+        weight_sum+=row.weight;
+        weight_map[i]=weight_sum;
+        assert(i == 0 || weight_map[i] > weight_map[i-1]);
+      }
+      if (weight_sum > 0) {
+        ArrayUtils.div(weight_map, weight_sum); //normalize to 0...1
+        relative_chunk_weight = global_weight_sum / _dinfo._adaptedFrame.anyVec().nChunks() / weight_sum;
+      }
+      else return; //nothing to do here - all rows have 0 weight
+    }
+
     //Example:
     // _useFraction = 0.8 -> 1 repeat with fraction = 0.8
     // _useFraction = 1.0 -> 1 repeat with fraction = 1.0
     // _useFraction = 1.1 -> 2 repeats with fraction = 0.55
     // _useFraction = 2.1 -> 3 repeats with fraction = 0.7
     // _useFraction = 3.0 -> 3 repeats with fraction = 1.0
-    final int repeats = (int)Math.ceil(_useFraction);
-    final float fraction = _useFraction / repeats;
-
-    // have non-trivial ?
-    final boolean nontrivial_weights = _dinfo._weights && !_dinfo._adaptedFrame.vecs()[_dinfo.weightChunkId()].isConst();
+    final int repeats = (int)Math.ceil(_useFraction * relative_chunk_weight);
+    final float fraction = (float)(_useFraction * relative_chunk_weight) / repeats;
+    assert(fraction <= 1.0);
 
     if (fraction < 1.0 || nontrivial_weights) {
       skip_rng = RandomUtils.getRNG(_seed+offset);
     }
-    long[] shuf_map = null;
-    if (_shuffle && !nontrivial_weights) { //we are already shuffling when using nontrivial_weights
-      shuf_map = new long[nrows];
-      for (int i=0;i<nrows;++i)
-        shuf_map[i] = i;
-      ArrayUtils.shuffleArray(shuf_map, new Random(_seed+offset).nextLong());
-    }
-    DataInfo.Row row = _dinfo.newDenseRow();
-    float[] weight_map = null;
-    float weight_sum;
-    //TODO: store node-local helper arrays in _dinfo -> avoid re-allocation and construction
-    if (nontrivial_weights) {
-      weight_map = new float[nrows];
-      weight_sum = 0;
-      for (int i=0;i<nrows;++i) {
-        row = _dinfo.extractDenseRow(chunks, i, row);
-        weight_sum+=row.weight;
-        weight_map[i]=weight_sum;
-      }
-      if (weight_sum > 0)
-        ArrayUtils.div(weight_map, weight_sum); //normalize to 0...1
-      else return; //nothing to do here - all rows have 0 weight
-    }
 
     double num_processed_rows = 0;
-    for(int rrr = 0; rrr < repeats; ++rrr) {
-      OUTER:
-      for(int rr = 0; rr < nrows; ++rr){
-        int r = 0; //index of local training row in this chunk to process
+    for(int rep = 0; rep < repeats; ++rep) {
+      for(int row_idx = 0; row_idx < nrows; ++row_idx){
+        int r = _shuffle ? -1 : 0;
 
         // only train with a given number of training samples (fraction*nrows)
-        if (skip_rng != null && skip_rng.nextFloat() > fraction) {
+        if (skip_rng != null && skip_rng.nextDouble() > fraction) {
           // we need to pick at least one row per map pass
-          if (rrr==repeats-1 && rr==nrows-1 && num_processed_rows == 0) {
-            r = -1; //marker
+          if (rep==repeats-1 && row_idx==nrows-1 && num_processed_rows == 0) {
+            r = -1; //do random sampling
           } else {
-            continue; //toss of coin
+            continue;
           }
         }
 
-        if (nontrivial_weights) {
+        if (nontrivial_weights) { // && row_idx % 2 == 0) { //every second row is totally random
           // importance sampling based on inverse of cumulative distribution
-          float key = skip_rng.nextFloat();
+          double key = skip_rng.nextDouble();
           r = Arrays.binarySearch(weight_map, 0, nrows, key);
 //          Log.info(Arrays.toString(weight_map));
 //          Log.info("key: " + key + " idx: " + (r >= 0 ? r : (-r-1)));
           if (r<0) r=-r-1;
         } else if (r == -1){
-          // uniformly pick one row
-          r = skip_rng.nextInt(nrows);
-        } else if (r != -1){
-          r = shuf_map != null ? (int) shuf_map[rr] : rr;
+          r = skip_rng.nextInt(nrows); //random sampling (with replacement)
+        } else {
+          r = row_idx; //linear scan - slightly faster
         }
         assert(r >= 0 && r<=nrows);
 
         row = _dinfo.extractDenseRow(chunks, r, row);
         if(!row.bad) {
-          long seed = offset + rrr * nrows + r;
+          long seed = offset + rep * nrows + r;
           if (outputs != null && outputs.length > 0)
             processRow(seed++, row, outputs);
           else
