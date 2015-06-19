5c9bfa7d72107baff323e255930e0b461498f744
Arno Candel <arno.candel@gmail.com>
2015-06-19 16:33:38 -0700

    Fix PUBDEV-1468: Disallow constant response.

--- h2o-core/src/main/java/hex/ModelBuilder.java
+++ h2o-core/src/main/java/hex/ModelBuilder.java
@@ -347,8 +347,11 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
 
     if(isSupervised()) {
 
-      if(_response != null)
-        _nclass = _response.isEnum()?_response.cardinality():1;
+      if(_response != null) {
+        _nclass = _response.isEnum() ? _response.cardinality() : 1;
+        if (_response.isConst())
+          error("_response","Response cannot be constant.");
+      }
       if (! _parms._balance_classes)
         hide("_max_after_balance_size", "Balance classes is false, hide max_after_balance_size");
       if( _parms._max_after_balance_size <= 0.0 )
