d80bae00e0d737d4e655753c6788102bb2b89a65
mmalohlava <michal.malohlava@gmail.com>
2015-06-24 13:37:48 -0700

    Fix Java bindings generator to generate code under project's location.
     - Idea was complaining about source located outside the module
    h2o-bindings.
    
     - This patch forces generator to put sources under
    `h2o-bindings/build/src-gen/java/main`
    
     - The generator script was moved under project's `bin` directory.
    
     - The bindings project was renamed to `h2o-java-rest-bindings`
    
     - h2o-java-rest-bindings dependency was moved to adhoc configuratio to
       avoid publishing of dependency in generated POM for maven central
    
     - h2o-java-rest-bindings project was appended to a list of published
       projects
    
     - the fix appends generated jar with bindings to distribution zip file

--- make-dist.sh
+++ make-dist.sh
@@ -23,6 +23,9 @@ function make_zip_common {
 
   cp h2o-py/dist/*whl $IMAGEDIR/python
 
+  mkdir -p $IMAGEDIR/bindings/java
+  cp h2o-java-rest-bindings/build/libs/h2o-java-rest-bindings-*.jar $IMAGEDIR/bindings/java
+
   cd $IMAGEDIR/..
   zip -r ${PROJECT_BASE}.zip ${PROJECT_BASE}
   cd $TOPDIR
@@ -112,7 +115,7 @@ cd ..
 
 # Add Java bindings Jar to target.
 mkdir -p target/bindings/java
-cp -p h2o-bindings/build/libs/*.jar target/bindings/java
+cp -p h2o-java-rest-bindings/build/libs/*.jar target/bindings/java
 
 # Add Maven repo to target.
 mkdir target/maven
