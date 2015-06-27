9bf2c947582cecd7a98af3464a8cf06b74eb5ed3
Nishant Kalonia <nishant@h2o.ai>
2015-06-27 13:24:09 -0700

    Turns out there's already a parameter for run.py to change the phantom.js timeout time...

--- scripts/run.py
+++ scripts/run.py
@@ -612,8 +612,6 @@ class Test:
             cmd = ["phantomjs",
                    self.test_name,
                    self.ip + ":" + str(self.port), str(g_phantomjs_to)]
-            if (g_include_jacoco):
-                cmd.append("10800") # Give the task additional time before timing out
         else:
             print("")
             print("ERROR: Test runner failure with test: " + self.test_name)
