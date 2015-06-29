692a2d0525d0127e7dbecec5791733d49e84c1ed
Arno Candel <arno.candel@gmail.com>
2015-06-29 12:41:52 -0700

    Mark the GBM weights test NOPASS again - needs real data (Random data leads to chaotic changes).

--- h2o-py/tests/testdir_algos/gbm/pyunit_NOPASS_weightsGBM.py
+++ h2o-py/tests/testdir_algos/gbm/pyunit_NOPASS_weightsGBM.py
@@ -9,12 +9,12 @@ def weights_check(ip,port):
     h2o.init(ip,port)
 
     def check_same(data1, data2, min_rows_scale):
-        gbm1_regression = h2o.gbm(x=data1[2:20], y=data1[1], min_rows=5, ntrees=5, max_depth=3)
-        gbm2_regression = h2o.gbm(x=data2[2:21], y=data2[1], min_rows=5*min_rows_scale, weights_column="weights", ntrees=5, max_depth=3)
-        gbm1_binomial = h2o.gbm(x=data1[1:20], y=data1[0], min_rows=5, distribution="bernoulli", ntrees=5, max_depth=3)
-        gbm2_binomial = h2o.gbm(x=data2[1:21], y=data2[0], weights_column="weights", min_rows=5*min_rows_scale, distribution="bernoulli", ntrees=5, max_depth=3)
-        gbm1_multinomial = h2o.gbm(x=data1[1:20], y=data1[0], min_rows=5, distribution="multinomial", ntrees=5, max_depth=3)
-        gbm2_multinomial = h2o.gbm(x=data2[1:21], y=data2[0], weights_column="weights", min_rows=5*min_rows_scale, distribution="multinomial", ntrees=5, max_depth=3)
+        gbm1_regression = h2o.gbm(x=data1[2:20], y=data1[1], min_rows=5, ntrees=5, max_depth=5)
+        gbm2_regression = h2o.gbm(x=data2[2:21], y=data2[1], min_rows=5*min_rows_scale, weights_column="weights", ntrees=5, max_depth=5)
+        gbm1_binomial = h2o.gbm(x=data1[1:20], y=data1[0], min_rows=5, distribution="bernoulli", ntrees=5, max_depth=5)
+        gbm2_binomial = h2o.gbm(x=data2[1:21], y=data2[0], weights_column="weights", min_rows=5*min_rows_scale, distribution="bernoulli", ntrees=5, max_depth=5)
+        gbm1_multinomial = h2o.gbm(x=data1[1:20], y=data1[0], min_rows=5, distribution="multinomial", ntrees=5, max_depth=2)
+        gbm2_multinomial = h2o.gbm(x=data2[1:21], y=data2[0], weights_column="weights", min_rows=5*min_rows_scale, distribution="multinomial", ntrees=5, max_depth=5)
 
         assert abs(gbm1_regression.mse() - gbm2_regression.mse()) < 1e-6, "Expected mse's to be the same, but got {0}, " \
                                                                           "and {1}".format(gbm1_regression.mse(),
@@ -28,11 +28,11 @@ def weights_check(ip,port):
     h2o_data = h2o.H2OFrame(python_obj=data)
 
     # uniform weights same as no weights
-    weight = random.uniform(.1,2)
+    weight = random.uniform(.1,5)
     uniform_weights = [[weight] for r in range(100)]
     h2o_uniform_weights = h2o.H2OFrame(python_obj=uniform_weights)
     h2o_uniform_weights.setNames(["weights"])
-    h2o_data_uniform_weights = h2o.cbind(h2o_data, h2o_uniform_weights)
+    h2o_data_uniform_weights = h2o_data.cbind(h2o_uniform_weights)
 
     print "Checking that using uniform weights is equivalent to no weights:"
     print
@@ -42,7 +42,7 @@ def weights_check(ip,port):
     zero_weights = [[0] if random.randint(0,1) else [1] for r in range(100)]
     h2o_zero_weights = h2o.H2OFrame(python_obj=zero_weights)
     h2o_zero_weights.setNames(["weights"])
-    h2o_data_zero_weights = h2o.cbind(h2o_data, h2o_zero_weights)
+    h2o_data_zero_weights = h2o_data.cbind(h2o_zero_weights)
     h2o_data_zeros_removed = h2o_data[h2o_zero_weights["weights"] == 1]
 
     print "Checking that using some zero weights is equivalent to removing those observations:"
@@ -53,7 +53,7 @@ def weights_check(ip,port):
     doubled_weights = [[1] if random.randint(0,1) else [2] for r in range(100)]
     h2o_doubled_weights = h2o.H2OFrame(python_obj=doubled_weights)
     h2o_doubled_weights.setNames(["weights"])
-    h2o_data_doubled_weights = h2o.cbind(h2o_data, h2o_doubled_weights)
+    h2o_data_doubled_weights = h2o_data.cbind(h2o_doubled_weights)
 
     doubled_data = copy.deepcopy(data)
     for d, w in zip(data,doubled_weights):
