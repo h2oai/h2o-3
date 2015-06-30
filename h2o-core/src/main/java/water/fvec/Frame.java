739861cba6f6276c1a5b89ca4ea29476d7369d05
SpencerA <spnrpa@gmail.com>
2015-06-30 12:48:29 -0700

    cosmetic

--- h2o-core/src/main/java/water/fvec/Frame.java
+++ h2o-core/src/main/java/water/fvec/Frame.java
@@ -1153,7 +1153,7 @@ public class Frame extends Lockable<Frame> {
             Chunk chk = chks[j];
             if( chk.isNA(i) )                   nchks[j].addNA();
             else if( chk instanceof C16Chunk )  nchks[j].addUUID(chk, i);
-            else if(chk instanceof CStrChunk)   nchks[j].addStr((chk.atStr(new ValueString(), i)));
+            else if( chk instanceof CStrChunk)  nchks[j].addStr((chk.atStr(new ValueString(), i)));
             else if( chk.hasFloat() )           nchks[j].addNum(chk.atd(i));
             else                                nchks[j].addNum(chk.at8(i),0);
           }
