779108ecf1e3621ea9b1d0472660219fe302dca7
Raymond Peck <rpeck@rpeck.com>
2015-07-02 15:01:37 -0700

    PUBDEV-1448: ModelBuilder handlers need to return the proper schema, not Schema.

--- h2o-algos/src/main/java/hex/api/GBMBuilderHandler.java
+++ h2o-algos/src/main/java/hex/api/GBMBuilderHandler.java
@@ -3,12 +3,11 @@ package hex.api;
 import hex.schemas.GBMV3;
 import hex.tree.gbm.GBM;
 import water.api.ModelBuilderHandler;
-import water.api.Schema;
 
 public class GBMBuilderHandler extends ModelBuilderHandler<GBM, GBMV3, GBMV3.GBMParametersV3> {
   /** Required so that Handler.handle() gets the correct schema types. */
   @SuppressWarnings("unused") // called through reflection by RequestServer
-  public Schema train(int version, GBMV3 builderSchema) {
+  public GBMV3 train(int version, GBMV3 builderSchema) {
     return super.do_train(version, builderSchema);
   }
 
