960da058eac276662ab5770ecc387b8ca26f5680
mmalohlava <michal.malohlava@gmail.com>
2015-07-04 00:04:08 +0200

    [PUBDEV-1586] Rename grid search schemas to experimental version 99

---
+++ h2o-algos/src/main/java/hex/schemas/DRFGridSearchV99.java
@@ -0,0 +1,13 @@
+package hex.schemas;
+
+import hex.GridSearchSchema;
+import hex.tree.drf.DRFGrid;
+import hex.tree.drf.DRFModel;
+
+/**
+ * End-point for DRF grid search.
+ *
+ * @see hex.GridSearchSchema
+ */
+public class DRFGridSearchV99 extends GridSearchSchema<DRFGrid, DRFGridSearchV99, DRFModel.DRFParameters, DRFV3.DRFParametersV3> {
+}
