cdfbdbefc16867bf9e8a172f6774425598e8521c
eric eckstrand <eric.eckstrand@gmail.com>
2015-07-03 11:11:42 -0700

    a pyunit and an runit that checks that the dimensions of a dataset are the same, regards of import/upload method. PUBDEV-1609

---
+++ h2o-r/tests/testdir_misc/runit_upload_import.R
@@ -0,0 +1,21 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../h2o-runit.R')
+
+upload.import <- function(conn) {
+    uploaded_frame <- h2o.uploadFile(locate("bigdata/laptop/mnist/train.csv.gz"))
+    imported_frame <- h2o.importFile(locate("bigdata/laptop/mnist/train.csv.gz"))
+
+    rows_u <- nrow(uploaded_frame)
+    rows_i <- nrow(imported_frame)
+    cols_u <- ncol(uploaded_frame)
+    cols_i <- ncol(imported_frame)
+
+    print(paste0("rows upload: ", rows_u, ", rows import: ", rows_i))
+    print(paste0("cols upload: ", cols_u, ", cols import: ", cols_i))
+    expect_equal(rows_u, rows_i, info="Expected same number of rows regardless of method.")
+    expect_equal(cols_u, cols_i, info="Expected same number of cols regardless of method.")
+
+    testEnd()
+}
+
+doTest("Test runif", upload.import)
