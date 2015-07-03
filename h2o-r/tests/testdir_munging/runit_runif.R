744f604145df8c34fb7e58deaca849244fa77d97
eric eckstrand <eric.eckstrand@gmail.com>
2015-07-03 11:51:52 -0700

    more minor runit fixes

--- h2o-r/tests/testdir_munging/runit_runif.R
+++ h2o-r/tests/testdir_munging/runit_runif.R
@@ -2,10 +2,10 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source('../h2o-runit.R')
 
 test.runif <- function(conn) {
-    uploaded_frame <- h2o.uploadFile(locate("bigdata/laptop/mnist/train.csv.gz"))
+    uploaded_frame <- h2o.uploadFile(conn, locate("bigdata/laptop/mnist/train.csv.gz"))
     r_u <- h2o.runif(uploaded_frame, seed=1234)
 
-    imported_frame <- h2o.importFile(locate("bigdata/laptop/mnist/train.csv.gz"))
+    imported_frame <- h2o.importFile(conn, locate("bigdata/laptop/mnist/train.csv.gz"))
     r_i <- h2o.runif(imported_frame, seed=1234)
 
     print(paste0("This demonstrates that seeding runif on identical frames with different chunk distributions ",
