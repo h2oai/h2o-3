6da3e4da5f41b8f014f94f640c09695473cd9eb3
eric eckstrand <eric.eckstrand@gmail.com>
2015-07-06 10:18:23 -0700

    fix and move pyunit to PASS

--- h2o-py/tests/testdir_misc/pyunit_headers.py
+++ h2o-py/tests/testdir_misc/pyunit_headers.py
@@ -7,7 +7,8 @@ def headers(ip,port):
     h2o.init(ip,port)
 
     headers = h2o.import_frame(h2o.locate("smalldata/airlines/allyears2k_headers_only.csv"))
-    headers_and = h2o.import_frame(h2o.locate("smalldata/airlines/allyears2k.zip"), col_names=headers)
+    headers_and = h2o.import_frame(h2o.locate("smalldata/airlines/allyears2k.zip"))
+    headers_and.setNames(headers.names())
     print headers.names()
     print headers_and.names()
     assert headers.names() == headers_and.names(), "Expected the same column names but got {0} and {1}". \
