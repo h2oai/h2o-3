69b563ce119f8466627b1a7d60465d1853f556f7
Arno Candel <arno.candel@gmail.com>
2015-07-07 13:53:13 -0700

    HEXDEV-214: GBM offset gamma test passes.

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_offset_gamma.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_offset_gamma.R
@@ -25,7 +25,7 @@ test <- function(h) {
 
 	hh = h2o.gbm(x = 1:3,y = "Claims",distribution ="gamma",ntrees = 600,max_depth = 1,min_rows = 1,learn_rate = .1,offset_column = "offset",training_frame = hdf) 
 	ph = as.data.frame(h2o.predict(hh,newdata = hdf)) 
-	expect.equal(fit2$initF - hh@model$init_f )
+	expect_equal(fit2$initF, hh@model$init_f)
 
 	expect_equal(mean(pr), mean(ph[,1]),tolerance=1e-5 )
 	expect_equal(min(pr), min(ph[,1]) ,tolerance=1e-5)
