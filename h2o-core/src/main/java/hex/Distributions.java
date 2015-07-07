df38ec39e202c0934e028c838819d7d36a1f1d65
Arno Candel <arno.candel@gmail.com>
2015-07-06 17:49:43 -0700

    HEXDEV-214: Add Gamma distribution definition.

--- h2o-core/src/main/java/hex/Distributions.java
+++ h2o-core/src/main/java/hex/Distributions.java
@@ -14,13 +14,19 @@ public class Distributions {
 
   public static class Bernoulli {
     public static double deviance(double w, double y, double f) { return w*(y*f - Math.log(1+Math.exp(f))); }
-    public static double gradient(double y, double f) { return y-invLink(f); }
-    public static double invLink(double f) { return 1./(1.+Math.exp(-f)); }
+    public static double gradient(double y, double f) { return y-linkInv(f); }
+    public static double linkInv(double f) { return 1./(1.+Math.exp(-f)); } //logit
   }
 
   public static class Poisson {
     public static double deviance(double w, double y, double f) { return w*(y*f - invLink(f)); }
     public static double gradient(double y, double f) { return y-invLink(f); }
     public static double invLink(double f) { return Math.max(1e-19,Math.min(1e19,Math.exp(f))); }
+  }
+
+  public static class Gamma {
+    public static double deviance(double w, double y, double f) { return w*(y*Math.exp(-f)+f); }
+    public static double gradient(double y, double f) { return y*Math.exp(-f)-1; }
+    public static double linkInv(double f) { return -1/f; }
   }
 }
