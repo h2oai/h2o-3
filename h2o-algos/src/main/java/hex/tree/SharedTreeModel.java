0f38e2f0c732095b005dc75cbb0b5b6c04c0b031
Arno Candel <arno.candel@gmail.com>
2015-07-10 13:01:14 -0700

    Fix a potential race condition in tree validation scoring.

--- h2o-algos/src/main/java/hex/tree/SharedTreeModel.java
+++ h2o-algos/src/main/java/hex/tree/SharedTreeModel.java
@@ -118,7 +118,7 @@ public abstract class SharedTreeModel<M extends SharedTreeModel<M,P,O>, P extend
       _ntrees++;
       // 1-based for errors; _scored_train[0] is for zero trees, not 1 tree
       _scored_train = ArrayUtils.copyAndFillOf(_scored_train, _ntrees+1, new ScoreKeeper());
-      _scored_valid = _validation_metrics != null ? ArrayUtils.copyAndFillOf(_scored_valid, _ntrees+1, new ScoreKeeper()) : null;
+      _scored_valid = _scored_valid != null ? ArrayUtils.copyAndFillOf(_scored_valid, _ntrees+1, new ScoreKeeper()) : null;
       _training_time_ms = ArrayUtils.copyAndFillOf(_training_time_ms, _ntrees+1, System.currentTimeMillis());
       fs.blockForPending();
     }
