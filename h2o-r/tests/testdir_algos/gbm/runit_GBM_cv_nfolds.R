e65c4e6040742302ec3a6a55019903a834ef9c21
Arno Candel <arno.candel@gmail.com>
2015-07-10 19:39:41 -0700

    PUBDEV-1451: Add RUnits for cross-val.

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_cv_nfolds.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_cv_nfolds.R
@@ -6,8 +6,9 @@ test.GBM.nfolds <- function(conn) {
   print(summary(prostate.hex))
   prostate.hex[,2] <- as.factor(prostate.hex[,2])
   prostate.nfolds <- h2o.gbm.cv(y = 2, x = 3:9, training_frame = prostate.hex, nfolds = 5, distribution = "bernoulli")
-  # prostate.nfolds <- h2o.crossValidate(model.type = 'gbm', params = list(y = 2, x = 3:9, training_frame = prostate.hex), nfolds = 5)
   print(prostate.nfolds)
+  prostate.nfolds.real <- h2o.gbm(y = 2, x = 3:9, training_frame = prostate.hex, nfolds = 5, distribution = "bernoulli")
+  print(prostate.nfolds.real)
 
   # Can't specify both nfolds >= 2 and validation = H2OParsedData at once
   expect_error(h2o.gbm.cv(y = 2, x = 3:9, training_frame = prostate.hex, nfolds = 5, validation_frame = prostate.hex, distribution = "bernoulli"))
