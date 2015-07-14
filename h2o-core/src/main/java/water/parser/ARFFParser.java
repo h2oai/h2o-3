7b00abe229368d27ffd8a2269211fa7bf28aed51
Brandon Hill <brandon@0xdata.com>
2015-07-13 20:30:03 -0700

    Updates the ARFF parser to to be more Java6 friendly.  Some loops were not being JIT'ed, and way too much time was spent in getting the default character set.  The code has been broken up into a few methods, and the default character set is no longer set.

--- h2o-core/src/main/java/water/parser/ARFFParser.java
+++ h2o-core/src/main/java/water/parser/ARFFParser.java
@@ -1,6 +1,5 @@
 package water.parser;
 
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 
 import water.exceptions.H2OParseSetupException;
@@ -26,28 +25,10 @@ class ARFFParser extends CsvParser {
 
     // header section
     ArrayList<String> header = new ArrayList<>();
-    while (offset < bits.length) {
-      int lineStart = offset;
-      while (offset < bits.length && !CsvParser.isEOL(bits[offset])) ++offset;
-      int lineEnd = offset;
-      ++offset;
-      // For Windoze, skip a trailing LF after CR
-      if ((offset < bits.length) && (bits[offset] == CsvParser.CHAR_LF)) ++offset;
-      if (bits[lineStart] == '#') continue; // Ignore      comment lines
-      if (bits[lineStart] == '%') continue; // Ignore ARFF comment lines
-      if (lineEnd > lineStart) {
-        String str = new String(bits, lineStart, lineEnd - lineStart, Charset.defaultCharset()).trim();
-        if (str.equalsIgnoreCase("@DATA")) {
-          if (!CsvParser.isEOL(bits[offset])) {
-            haveData = true; //more than just the header
-          }
-          break;
-        }
-        String[] tok = determineTokens(str, CHAR_SPACE, singleQuotes);
-        if (tok.length > 0 && tok[0].equalsIgnoreCase("@RELATION")) continue; // Ignore name of dataset
-        if (!str.isEmpty()) header.add(str);
-      }
-    }
+    offset = readArffHeader(offset, header, bits, singleQuotes);
+    if (offset < bits.length && !CsvParser.isEOL(bits[offset]))
+      haveData = true; //more than just the header
+
     if (header.size() == 0)
       throw new H2OParseSetupException("No data!");
     headerlines = header.toArray(headerlines);
@@ -59,57 +40,7 @@ class ARFFParser extends CsvParser {
     labels = new String[ncols];
     domains = new String[ncols][];
     ctypes = new byte[ncols];
-    for (int i=0; i<ncols; ++i) {
-      data[i] = headerlines[i].split("\\s+");
-      if (!data[i][0].equalsIgnoreCase("@ATTRIBUTE")) {
-        throw new H2OParseSetupException("Expected line to start with @ATTRIBUTE.");
-      } else {
-        if (data[i].length < 3 ) {
-          throw new H2OParseSetupException("Expected @ATTRIBUTE to be followed by <attribute-name> <datatype>");
-        }
-        labels[i] = data[i][1];
-        String type = data[i][2];
-        domains[i] = null;
-        if (type.equalsIgnoreCase("NUMERIC") || type.equalsIgnoreCase("REAL") || type.equalsIgnoreCase("INTEGER") || type.equalsIgnoreCase("INT")) {
-          ctypes[i] = Vec.T_NUM;
-          continue;
-        }
-        else if (type.equalsIgnoreCase("DATE") || type.equalsIgnoreCase("TIME")) {
-          ctypes[i] = Vec.T_TIME;
-          continue;
-        }
-        else if (type.equalsIgnoreCase("ENUM")) {
-          ctypes[i] = Vec.T_ENUM;
-          continue;
-        }
-        else if (type.equalsIgnoreCase("STRING")) {
-          ctypes[i] = Vec.T_STR;
-          continue;
-        }
-        else if (type.equalsIgnoreCase("UUID")) { //extension of ARFF
-          ctypes[i] = Vec.T_UUID;
-          continue;
-        }
-        else if (type.equalsIgnoreCase("RELATIONAL")) {
-          throw new UnsupportedOperationException("Relational ARFF format is not supported.");
-        }
-        else if (type.startsWith("{") && data[i][data[i].length-1].endsWith("}")) {
-          StringBuilder builder = new StringBuilder();
-          for(int j = 2; j < data[i].length; j++) {
-            builder.append(data[i][j]);
-          }
-          domains[i] = builder.toString().replaceAll("[{}]", "").split(",");
-          if (domains[i][0].length() > 0) {
-            // case of {A,B,C} (valid list of factors)
-            ctypes[i] = Vec.T_ENUM;
-            continue;
-          }
-        }
-
-        // only get here if data is invalid ARFF
-        throw new H2OParseSetupException("Unexpected line.");
-      }
-    }
+    processArffHeader(ncols, headerlines, data, labels, domains, ctypes);
 
     // data section (for preview)
     if (haveData) {
@@ -168,4 +99,85 @@ class ARFFParser extends CsvParser {
     return new ParseSetup(ParserType.ARFF, sep, singleQuotes, ParseSetup.NO_HEADER, ncols, labels, ctypes, domains, naStrings, data);
   }
 
+  private static int readArffHeader(int offset, ArrayList<String> header, byte[] bits, boolean singleQuotes) {
+    while (offset < bits.length) {
+      int lineStart = offset;
+      while (offset < bits.length && !CsvParser.isEOL(bits[offset])) ++offset;
+      int lineEnd = offset;
+      ++offset;
+      // For Windoze, skip a trailing LF after CR
+      if ((offset < bits.length) && (bits[offset] == CsvParser.CHAR_LF)) ++offset;
+      if (bits[lineStart] == '#') continue; // Ignore      comment lines
+      if (bits[lineStart] == '%') continue; // Ignore ARFF comment lines
+      if (lineEnd > lineStart) {
+        if (bits[lineStart] == '@' &&
+                (bits[lineStart+1] == 'D' || bits[lineStart+1] =='d' ) &&
+                (bits[lineStart+2] == 'A' || bits[lineStart+2] =='a' ) &&
+                (bits[lineStart+3] == 'T' || bits[lineStart+3] =='t' ) &&
+                (bits[lineStart+4] == 'A' || bits[lineStart+4] =='a' )){
+          break;
+        }
+        String str = new String(bits, lineStart, lineEnd - lineStart).trim();
+        String[] tok = determineTokens(str, CHAR_SPACE, singleQuotes);
+        if (tok.length > 0 && tok[0].equalsIgnoreCase("@RELATION")) continue; // Ignore name of dataset
+        if (!str.isEmpty()) header.add(str);
+      }
+    }
+    return offset;
+  }
+
+  private static void processArffHeader(int ncols, String[] headerlines, String[][] data, String[] labels, String[][] domains, byte[] ctypes) {
+    for (int i=0; i<ncols; ++i) {
+      data[i] = headerlines[i].split("\\s+");
+      if (!data[i][0].equalsIgnoreCase("@ATTRIBUTE")) {
+        throw new H2OParseSetupException("Expected line to start with @ATTRIBUTE.");
+      } else {
+        if (data[i].length < 3 ) {
+          throw new H2OParseSetupException("Expected @ATTRIBUTE to be followed by <attribute-name> <datatype>");
+        }
+        labels[i] = data[i][1];
+        String type = data[i][2];
+        domains[i] = null;
+        if (type.equalsIgnoreCase("NUMERIC") || type.equalsIgnoreCase("REAL") || type.equalsIgnoreCase("INTEGER") || type.equalsIgnoreCase("INT")) {
+          ctypes[i] = Vec.T_NUM;
+          continue;
+        }
+        else if (type.equalsIgnoreCase("DATE") || type.equalsIgnoreCase("TIME")) {
+          ctypes[i] = Vec.T_TIME;
+          continue;
+        }
+        else if (type.equalsIgnoreCase("ENUM")) {
+          ctypes[i] = Vec.T_ENUM;
+          continue;
+        }
+        else if (type.equalsIgnoreCase("STRING")) {
+          ctypes[i] = Vec.T_STR;
+          continue;
+        }
+        else if (type.equalsIgnoreCase("UUID")) { //extension of ARFF
+          ctypes[i] = Vec.T_UUID;
+          continue;
+        }
+        else if (type.equalsIgnoreCase("RELATIONAL")) {
+          throw new UnsupportedOperationException("Relational ARFF format is not supported.");
+        }
+        else if (type.startsWith("{") && data[i][data[i].length-1].endsWith("}")) {
+          StringBuilder builder = new StringBuilder();
+          for(int j = 2; j < data[i].length; j++) {
+            builder.append(data[i][j]);
+          }
+          domains[i] = builder.toString().replaceAll("[{}]", "").split(",");
+          if (domains[i][0].length() > 0) {
+            // case of {A,B,C} (valid list of factors)
+            ctypes[i] = Vec.T_ENUM;
+            continue;
+          }
+        }
+
+        // only get here if data is invalid ARFF
+        throw new H2OParseSetupException("Unexpected line.");
+      }
+    }
+
+  }
 }
