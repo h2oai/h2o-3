ae9974a45b8ebbdda322bce44bc69d756bb4b9c6
eric eckstrand <eric.eckstrand@gmail.com>
2015-07-14 15:54:40 -0700

    add gbm cross-validation junit for checking that consecutive models built with the same parameters are equal.

--- h2o-algos/src/test/java/hex/tree/gbm/GBMTest.java
+++ h2o-algos/src/test/java/hex/tree/gbm/GBMTest.java
@@ -1134,8 +1134,6 @@ public class GBMTest extends TestUtil {
       GBMModel.GBMParameters parms = new GBMModel.GBMParameters();
       parms._train = tfr._key;
       parms._response_column = "response";
-      parms._weights_column = "weight";
-      parms._seed = 0xdecaf;
       parms._min_rows = 1;
       parms._max_depth = 2;
       parms._nfolds = (int) tfr.numRows();
@@ -1182,8 +1180,6 @@ public class GBMTest extends TestUtil {
       GBMModel.GBMParameters parms = new GBMModel.GBMParameters();
       parms._train = tfr._key;
       parms._response_column = "response";
-      parms._weights_column = "weight";
-      parms._seed = 0xdecaf;
       parms._min_rows = 1;
       parms._max_depth = 2;
       parms._ntrees = 3;
@@ -1235,8 +1231,6 @@ public class GBMTest extends TestUtil {
       parms._train = tfr._key;
       parms._valid = vfr._key;
       parms._response_column = "response";
-      parms._weights_column = "weight";
-      parms._seed = 0xdecaf;
       parms._min_rows = 1;
       parms._max_depth = 2;
       parms._nfolds = 3;
@@ -1256,6 +1250,55 @@ public class GBMTest extends TestUtil {
       if (tfr != null) tfr.remove();
       if (vfr != null) vfr.remove();
       if (gbm != null) gbm.delete();
+      Scope.exit();
+    }
+  }
+
+  @Test
+  public void testNfoldsConsecutiveModelsSame() {
+    Frame tfr = null;
+    Vec old = null;
+    GBMModel gbm1 = null;
+    GBMModel gbm2 = null;
+
+    Scope.enter();
+    try {
+      tfr = parse_test_file("smalldata/junit/cars_20mpg.csv");
+      tfr.remove("name").remove(); // Remove unique id
+      tfr.remove("economy").remove();
+      old = tfr.remove("economy_20mpg");
+      tfr.add("economy_20mpg", old.toEnum()); // response to last column
+      DKV.put(tfr);
+
+      GBMModel.GBMParameters parms = new GBMModel.GBMParameters();
+      parms._train = tfr._key;
+      parms._response_column = "economy_20mpg";
+      parms._min_rows = 1;
+      parms._max_depth = 2;
+      parms._nfolds = 3;
+      parms._ntrees = 3;
+      parms._learn_rate = 1e-3f;
+
+      GBM job1 = new GBM(parms);
+      gbm1 = job1.trainModel().get();
+
+      GBM job2 = new GBM(parms);
+      gbm2 = job2.trainModel().get();
+
+      ModelMetricsBinomial mm1 = (ModelMetricsBinomial)gbm1._output._validation_metrics;
+      ModelMetricsBinomial mm2 = (ModelMetricsBinomial)gbm2._output._validation_metrics;
+      assertEquals(mm1.auc()._auc, mm2.auc()._auc, 1e-12);
+      assertEquals(mm1.mse(), mm2.mse(), 1e-12);
+      assertEquals(mm1.r2(), mm2.r2(), 1e-12);
+      assertEquals(mm1.logloss(), mm2.logloss(), 1e-12);
+
+      job1.remove();
+      job2.remove();
+    } finally {
+      if (tfr != null) tfr.remove();
+      if (old != null) old.remove();
+      if (gbm1 != null) gbm1.delete();
+      if (gbm2 != null) gbm2.delete();
       Scope.exit();
     }
   }
