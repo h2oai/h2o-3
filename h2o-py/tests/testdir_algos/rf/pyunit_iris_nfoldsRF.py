bd52f4d7d29c4d59cf04f2b39d1a57f15223fc6a
Arno Candel <arno.candel@gmail.com>
2015-07-16 22:10:37 -0700

    PUBDEV-1451: Modify tests - it is now possible to specify validation and cross-validation at the same time.

--- h2o-py/tests/testdir_algos/rf/pyunit_iris_nfoldsRF.py
+++ h2o-py/tests/testdir_algos/rf/pyunit_iris_nfoldsRF.py
@@ -11,12 +11,12 @@ def iris_nfolds(ip,port):
     model = h2o.random_forest(y=iris[4], x=iris[0:4], ntrees=50, nfolds=5)
     model.show()
   
-    # Can't specify both nfolds >= 2 and validation = H2OParsedData at once
+    # Can specify both nfolds >= 2 and validation = H2OParsedData at once
     try:
         h2o.random_forest(y=iris[4], x=iris[0:4], validation_y=iris[4], validation_x=iris[0:4], ntrees=50, nfolds=5)
-        assert False, "expected an error"
-    except EnvironmentError:
         assert True
+    except EnvironmentError:
+        assert False, "expected an error"
 
 if __name__ == "__main__":
   h2o.run_test(sys.argv, iris_nfolds)
