9cbf53910e5c4965e2769cead57cd8e332c1dcdd
Luan Nguyen <luan.nguyen@infonam.com>
2015-07-20 21:06:09 +0700

    Update new GLM, DRF tests and clean up code + Clean up GLM specifics from OptionsGroupParam. This class can be reused cross algorithms + Clean up GLM Basic test file: using generic libraries for now + Add new nfolds param for GLM tests + Add beta_constraints param in + Add dataset directory param for DRF tests + Update new GLM and DRF test csv files

--- h2o-testng/src/test/java/h2o/testng/utils/OptionsGroupParam.java
+++ h2o-testng/src/test/java/h2o/testng/utils/OptionsGroupParam.java
@@ -1,8 +1,5 @@
 package h2o.testng.utils;
 
-import hex.glm.GLMModel.GLMParameters.Family;
-import hex.glm.GLMModel.GLMParameters.Solver;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,36 +23,5 @@ public class OptionsGroupParam {
 			}
 		}
 		return null;
-	}
-	
-	public static Object getValue(int paramType, String[] value, List<String> tcHeaders) {
-
-		return OptionsGroupParam.optionParams[paramType].getValue(value,tcHeaders);
-	}
-
-	
-	public final static OptionsGroupParam[] optionParams = new OptionsGroupParam[] {
-		new OptionsGroupParam(
-				new String[] {"gaussian", "binomial", "poisson", "gamma"},
-				new Object[] {Family.gaussian, Family.binomial, Family.poisson, Family.gamma}),
-		new OptionsGroupParam(
-				new String[] {"auto", "irlsm", "lbfgs"},
-				new Object[] {Solver.AUTO, Solver.IRLSM, Solver.L_BFGS}),
-	};
-
-	public enum ParamType {
-		FAMILY(0), SOLVER(1);
-		
-		private int value;
-		
-		private ParamType(int value) {
-			
-			this.value = value;
-		}
-		
-		public int getValue() {
-			
-			return value;
-		}
 	}
 }
