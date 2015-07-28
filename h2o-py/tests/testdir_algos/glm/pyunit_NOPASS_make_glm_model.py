5c86e0c597363325f71ddf17a99dafbcecdcfde0
eric eckstrand <eric.eckstrand@gmail.com>
2015-07-28 13:57:46 -0700

    minor change to NOPASS pyunit

--- h2o-py/tests/testdir_algos/glm/pyunit_NOPASS_make_glm_model.py
+++ h2o-py/tests/testdir_algos/glm/pyunit_NOPASS_make_glm_model.py
@@ -8,12 +8,11 @@ def pyunit_make_glm_model(ip,port):
     model = h2o.glm(x=pros[["AGE","DPROS","DCAPS","PSA","VOL","GLEASON"]], y=pros["CAPSULE"], family="gaussian", alpha=[0])
     new_betas = {"AGE":0.5, "DPROS":0.5, "DCAPS":0.5, "PSA":0.5, "VOL":0.5, "GLEASON":0.5}
 
-    new_glm = model.make_glm_model(new_betas)
-
     names = '['
     for n in new_betas.keys(): names += "\""+n+"\","
     names = names[0:len(names)-1]+"]"
     betas = '['
+    
     for b in new_betas.values(): betas += str(b)+","
     betas = betas[0:len(betas)-1]+"]"
     res = h2o.H2OConnection.post_json("MakeGLMModel",model=model._id,names=names,beta=betas)
