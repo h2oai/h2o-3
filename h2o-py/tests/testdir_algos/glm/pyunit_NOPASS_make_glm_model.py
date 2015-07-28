44faa7f15801b9218db6dfa84cde85baa56afb62
eric eckstrand <eric.eckstrand@gmail.com>
2015-07-28 14:43:31 -0700

    add python client h2o.assign pyunit. PUBDEV-1324.

--- h2o-py/tests/testdir_algos/glm/pyunit_NOPASS_make_glm_model.py
+++ h2o-py/tests/testdir_algos/glm/pyunit_NOPASS_make_glm_model.py
@@ -12,7 +12,7 @@ def pyunit_make_glm_model(ip,port):
     for n in new_betas.keys(): names += "\""+n+"\","
     names = names[0:len(names)-1]+"]"
     betas = '['
-    
+
     for b in new_betas.values(): betas += str(b)+","
     betas = betas[0:len(betas)-1]+"]"
     res = h2o.H2OConnection.post_json("MakeGLMModel",model=model._id,names=names,beta=betas)
