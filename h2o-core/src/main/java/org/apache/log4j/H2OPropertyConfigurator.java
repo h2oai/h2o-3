7928dcb0813f221bd047ed8a7ab7c679e9488435
Tom Kraljevic <tomk@tomk.net>
2015-07-28 16:08:18 -0700

    Add comment.

--- h2o-core/src/main/java/org/apache/log4j/H2OPropertyConfigurator.java
+++ h2o-core/src/main/java/org/apache/log4j/H2OPropertyConfigurator.java
@@ -4,6 +4,13 @@ import org.apache.log4j.spi.LoggerRepository;
 
 import java.util.Properties;
 
+/**
+ * Append to an existing live log4j configuration rather than to create a new one
+ * with a new complete properties file.
+ *
+ * This is used by embedded environments like Sparkling Water that don't want to
+ * blindly clobber the parent logger configuration.
+ */
 public class H2OPropertyConfigurator extends PropertyConfigurator {
   @Override
   public
