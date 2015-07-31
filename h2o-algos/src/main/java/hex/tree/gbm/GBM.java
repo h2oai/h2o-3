d85a0bea1160221fdb5be6c3c1a2784fbc8c46a3
Arno Candel <arno.candel@gmail.com>
2015-07-31 14:19:20 -0700

    PUBDEV-1674: Fix a major bug in GBM Regression: was casting the response to a long instead of double... (was ok for classification).

--- h2o-algos/src/main/java/hex/tree/gbm/GBM.java
+++ h2o-algos/src/main/java/hex/tree/gbm/GBM.java
@@ -259,7 +259,7 @@ public class GBM extends SharedTree<GBMModel,GBMModel.GBMParameters,GBMModel.GBM
         for( int row = 0; row < wk._len; row++) {
           if( ys.isNA(row) ) continue;
           double f = tr.atd(row) + offset.atd(row);
-          double y = ys.at8(row);
+          double y = ys.atd(row);
           if( _parms._distribution == Distribution.Family.multinomial ) {
             double weight = hasWeightCol() ? chk_weight(chks).atd(row) : 1;
             double sum = score1(chks, weight,0.0 /*offset not used for multiclass*/,fs,row);
