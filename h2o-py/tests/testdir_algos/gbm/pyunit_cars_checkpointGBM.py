f82f07617a4557f26315a28d5a5f8a8a4b0e5066
eric eckstrand <eric.eckstrand@gmail.com>
2015-08-03 14:01:44 -0700

    add some gbm/rf checkpointing tests. add more debug information to intermittent checkpointing tests

--- h2o-py/tests/testdir_algos/gbm/pyunit_cars_checkpointGBM.py
+++ h2o-py/tests/testdir_algos/gbm/pyunit_cars_checkpointGBM.py
@@ -1,4 +1,4 @@
-import sys
+import sys, shutil
 sys.path.insert(1, "../../../")
 import h2o
 import random
@@ -9,6 +9,10 @@ def cars_checkpoint(ip,port):
     s = cars.runif()
     train = cars[s > .2]
     valid = cars[s <= .2]
+    print "\n*** Description (chunk distribution, etc) of training frame:"
+    train.describe()
+    print "\n*** Description (chunk distribution, etc) of validation frame:"
+    valid.describe()
 
     # choose the type model-building exercise (multinomial classification or regression). 0:regression, 1:binomial,
     # 2:multinomial
@@ -19,59 +23,108 @@ def cars_checkpoint(ip,port):
     if problem == 1   :
         response_col = "economy_20mpg"
         distribution = "bernoulli"
-        cars[response_col] = cars[response_col].asfactor()
+        train[response_col] = train[response_col].asfactor()
+        valid[response_col] = valid[response_col].asfactor()
     elif problem == 2 :
         response_col = "cylinders"
         distribution = "multinomial"
-        cars[response_col] = cars[response_col].asfactor()
+        train[response_col] = train[response_col].asfactor()
+        valid[response_col] = valid[response_col].asfactor()
     else              :
         response_col = "economy"
         distribution = "gaussian"
 
-    print "Distribution: {0}".format(distribution)
-    print "Response column: {0}".format(response_col)
+    print "\n*** Distribution: {0}".format(distribution)
+    print "\n*** Response column: {0}".format(response_col)
 
     # build first model
-    ntrees1 = random.sample(range(5,21),1)[0]
+    ntrees1 = 5
     max_depth1 = random.sample(range(2,6),1)[0]
     min_rows1 = random.sample(range(10,16),1)[0]
-    print "ntrees model 1: {0}".format(ntrees1)
-    print "max_depth model 1: {0}".format(max_depth1)
-    print "min_rows model 1: {0}".format(min_rows1)
-    model1 = h2o.gbm(x=train[predictors],y=train[response_col],ntrees=ntrees1,max_depth=max_depth1, min_rows=min_rows1,
-                     distribution=distribution,validation_x=valid[predictors],validation_y=valid[response_col])
+    print "\n*** Building model 1 with the following parameters:"
+    print "*** ntrees model 1: {0}".format(ntrees1)
+    print "*** max_depth model 1: {0}".format(max_depth1)
+    print "*** min_rows model 1: {0}".format(min_rows1)
+    model1 = h2o.gbm(x=train[predictors],
+                     y=train[response_col],
+                     ntrees=ntrees1,
+                     max_depth=max_depth1,
+                     min_rows=min_rows1,
+                     score_each_iteration=True,
+                     distribution=distribution,
+                     validation_x=valid[predictors],
+                     validation_y=valid[response_col])
 
-    # save the model, remove all keys, then load the model
+    # save the model, then load the model
     model_path = h2o.save_model(model1, name="delete_model", force=True)
     restored_model = h2o.load_model(model_path)
+    shutil.rmtree("delete_model")
 
     # continue building the model
-    ntrees2 = ntrees1 + random.sample(range(5,21),1)[0]
+    ntrees2 = ntrees1 + 5
     max_depth2 = max_depth1
     min_rows2 = min_rows1
-    print "ntrees model 2: {0}".format(ntrees2)
-    print "max_depth model 2: {0}".format(max_depth2)
-    print "min_rows model 2: {0}".format(min_rows2)
-    model2 = h2o.gbm(x=train[predictors],y=train[response_col],ntrees=ntrees2,max_depth=max_depth2, min_rows=min_rows2,
-                     distribution=distribution,checkpoint=restored_model._id,validation_x=valid[predictors],
-                     validation_y=valid[response_col])
+    print "\n*** Continuing to build model 1 (now called model 2) with the following parameters:"
+    print "*** ntrees model 2: {0}".format(ntrees2)
+    print "*** max_depth model 2: {0}".format(max_depth2)
+    print "*** min_rows model 2: {0}".format(min_rows2)
+    model2 = h2o.gbm(x=train[predictors],
+                     y=train[response_col],
+                     ntrees=ntrees2,
+                     max_depth=max_depth2,
+                     min_rows=min_rows2,
+                     distribution=distribution,
+                     score_each_iteration=True,
+                     validation_x=valid[predictors],
+                     validation_y=valid[response_col],
+                     checkpoint=restored_model._id)
 
     # continue building the model, but with different number of trees
     ntrees3 = ntrees2 + 50
     max_depth3 = max_depth1
     min_rows3 = min_rows1
-    print "ntrees model 3: {0}".format(ntrees3)
-    print "max_depth model 3: {0}".format(max_depth3)
-    print "min_rows model 3: {0}".format(min_rows3)
-    model3 = h2o.gbm(x=train[predictors],y=train[response_col],ntrees=ntrees3,max_depth=max_depth3, min_rows=min_rows3,
-                     distribution=distribution,checkpoint=restored_model._id,validation_x=valid[predictors],
-                     validation_y=valid[response_col])
+    print "\n*** Continuing to build model 1 (now called model 3) with the following parameters:"
+    print "*** ntrees model 3: {0}".format(ntrees3)
+    print "*** max_depth model 3: {0}".format(max_depth3)
+    print "*** min_rows model 3: {0}".format(min_rows3)
+    model3 = h2o.gbm(x=train[predictors],
+                     y=train[response_col],
+                     ntrees=ntrees3,
+                     max_depth=max_depth3,
+                     min_rows=min_rows3,
+                     distribution=distribution,
+                     score_each_iteration=True,
+                     validation_x=valid[predictors],
+                     validation_y=valid[response_col],
+                     checkpoint=restored_model._id)
 
     # build the equivalent of model 2 in one shot
-    model4 = h2o.gbm(x=train[predictors],y=train[response_col],ntrees=ntrees2,max_depth=max_depth2,
-                     min_rows=min_rows2,validation_frame=valid,distribution=distribution,validation_x=valid[predictors],
+    print "\n*** Building the equivalent of model 2 (called model 4) in one shot:"
+    model4 = h2o.gbm(x=train[predictors],
+                     y=train[response_col],
+                     ntrees=ntrees2,
+                     max_depth=max_depth2,
+                     min_rows=min_rows2,
+                     distribution=distribution,
+                     score_each_iteration=True,
+                     validation_x=valid[predictors],
                      validation_y=valid[response_col])
 
+    print "\n*** Model Summary for model 2:"
+    print model2.summary()
+    print "\n*** Model Summary for model 3:"
+    print model3.summary()
+    print "\n*** Model Summary for model 4:"
+    print model4.summary()
+
+    print "\n*** Score History for model 2:"
+    print model2.score_history()
+    print "\n*** Score History for model 3:"
+    print model3.score_history()
+    print "\n*** Score History for model 4:"
+    print model4.score_history()
+
+    # checks
     if problem == 0:
         assert isinstance(model2,type(model4))
         assert model2.mse(valid=True)==model4.mse(valid=True), "Expected Model 2 MSE: {0} to be the same as Model 4 MSE: {1}".format(model2.mse(valid=True), model4.mse(valid=True))
