516dd33029827d82084c2572c8edc661c903f13b
eric eckstrand <eric.eckstrand@gmail.com>
2015-08-02 19:57:13 -0700

    fix checkpointing pyunits

--- h2o-py/tests/testdir_algos/rf/pyunit_cars_checkpointRF.py
+++ h2o-py/tests/testdir_algos/rf/pyunit_cars_checkpointRF.py
@@ -42,7 +42,7 @@ def cars_checkpoint(ip,port):
     model_path = h2o.save_model(model1, name="delete_model", force=True)
     restored_model = h2o.load_model(model_path)
 
-    # continue building the model with the same max_depth and min_rows
+    # continue building the model
     ntrees2 = ntrees1 + random.sample(range(5,21),1)[0]
     max_depth2 = max_depth1
     min_rows2 = min_rows1
@@ -53,10 +53,10 @@ def cars_checkpoint(ip,port):
                                min_rows=min_rows2,checkpoint=restored_model._id,validation_x=valid[predictors],
                                validation_y=valid[response_col],seed=2345)
 
-    # continue building the model, but with different max_depth and min_rows (ensemble)
-    ntrees3 = ntrees2
-    max_depth3 = max_depth2 + random.sample(range(3,6),1)[0]
-    min_rows3 = min_rows2 + random.sample(range(5,10),1)[0]
+    # continue building the model, but with different number of trees
+    ntrees3 = ntrees2 + 50
+    max_depth3 = max_depth2
+    min_rows3 = min_rows2
     print "ntrees model 3: {0}".format(ntrees3)
     print "max_depth model 3: {0}".format(max_depth3)
     print "min_rows model 3: {0}".format(min_rows3)
@@ -71,39 +71,27 @@ def cars_checkpoint(ip,port):
 
     if problem == 0:
         assert isinstance(model2,type(model4))
-        print "model2.mse", model2.mse, "model4.mse", model4.mse
-        print "model3.mse", model2.mse, "model4.mse", model4.mse
-        assert model2.mse(valid=True)==model4.mse(valid=True)
-        assert model3.mse(valid=True)!=model4.mse(valid=True)
+        assert model2.mse(valid=True)==model4.mse(valid=True), "Expected Model 2 MSE: {0} to be the same as Model 4 MSE: {1}".format(model2.mse(valid=True), model4.mse(valid=True))
+        assert model3.mse(valid=True)!=model4.mse(valid=True), "Expected Model 3 MSE: {0} to be different from Model 4 MSE: {1}".format(model3.mse(valid=True), model4.mse(valid=True))
 
     elif problem == 1:
         assert isinstance(model2,type(model4))
-        print "model2.auc", model2.auc, "model4.auc", model4.auc
-        print "model3.auc", model2.auc, "model4.auc", model4.auc
-        assert model2.auc(valid=True)==model4.auc(valid=True)
-        assert model3.auc(valid=True)!=model4.auc(valid=True)
+        assert model2.auc(valid=True)==model4.auc(valid=True), "Expected Model 2 AUC: {0} to be the same as Model 4 AUC: {1}".format(model2.auc(valid=True), model4.auc(valid=True))
+        assert model3.auc(valid=True)!=model4.auc(valid=True), "Expected Model 3 AUC: {0} to be different from Model 4 AUC: {1}".format(model3.auc(valid=True), model4.auc(valid=True))
 
-        print "model2.logloss", model2.logloss, "model4.logloss", model4.logloss
-        print "model3.logloss", model2.logloss, "model4.logloss", model4.logloss
-        assert model2.logloss(valid=True)==model4.logloss(valid=True)
-        assert model3.logloss(valid=True)!=model4.logloss(valid=True)
+        assert model2.logloss(valid=True)==model4.logloss(valid=True), "Expected Model 2 Log Loss: {0} to be the same as Model 4 Log Loss: {1}".format(model2.logloss(valid=True), model4.logloss(valid=True))
+        assert model3.logloss(valid=True)!=model4.logloss(valid=True), "Expected Model 2 Log Loss: {0} to be different from as Model 4 Log Loss: {1}".format(model2.logloss(valid=True), model4.logloss(valid=True))
 
-        print "model2.giniCoef", model2.giniCoef, "model4.giniCoef", model4.giniCoef
-        print "model3.giniCoef", model2.giniCoef, "model4.giniCoef", model4.giniCoef
-        assert model2.giniCoef(valid=True)==model4.giniCoef(valid=True)
-        assert model3.giniCoef(valid=True)!=model4.giniCoef(valid=True)
+        assert model2.giniCoef(valid=True)==model4.giniCoef(valid=True), "Expected Model 2 Gini Coef {0} to be the same as Model 4 Gini Coef: {1}".format(model2.giniCoef(valid=True), model4.giniCoef(valid=True))
+        assert model3.giniCoef(valid=True)!=model4.giniCoef(valid=True), "Expected Model 2 Gini Coef: {0} to be the same as Model 4 Gini Coef: {1}".format(model2.giniCoef(valid=True), model4.giniCoef(valid=True))
 
     else:
         assert isinstance(model2,type(model4))
-        assert model2.mse(valid=True)==model4.mse(valid=True)
-        assert model3.mse(valid=True)!=model4.mse(valid=True)
-        print "model2.mse", model2.mse, "model4.mse", model4.mse
-        print "model3.mse", model2.mse, "model4.mse", model4.mse
+        assert model2.mse(valid=True)==model4.mse(valid=True), "Expected Model 2 MSE: {0} to be the same as Model 4 MSE: {1}".format(model2.mse(valid=True), model4.mse(valid=True))
+        assert model3.mse(valid=True)!=model4.mse(valid=True), "Expected Model 3 MSE: {0} to be different from Model 4 MSE: {1}".format(model3.mse(valid=True), model4.mse(valid=True))
 
-        assert model2.r2(valid=True)==model4.r2(valid=True)
-        assert model3.r2(valid=True)!=model4.r2(valid=True)
-        print "model2.r2", model2.r2, "model4.r2", model4.r2
-        print "model3.r2", model2.r2, "model4.r2", model4.r2
+        assert model2.r2(valid=True)==model4.r2(valid=True), "Expected Model 2 R2: {0} to be the same as Model 4 R2: {1}".format(model2.r2(valid=True), model4.r2(valid=True))
+        assert model3.r2(valid=True)!=model4.r2(valid=True), "Expected Model 3 R2: {0} to be different from Model 4 R2: {1}".format(model3.r2(valid=True), model4.r2(valid=True))
 
 if __name__ == "__main__":
     h2o.run_test(sys.argv, cars_checkpoint)
