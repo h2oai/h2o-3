4bac076bdb93aadb30b6adab822da195938b03fd
Cliff Click <cliffc@acm.org>
2015-08-03 11:15:28 -0700

    bug with cbind not making vecs compatible before joining

--- h2o-core/src/main/java/water/currents/ASTColSlice.java
+++ h2o-core/src/main/java/water/currents/ASTColSlice.java
@@ -132,7 +132,7 @@ class ASTCBind extends ASTPrim {
     for( int i=1; i<asts.length; i++ ) {
       switch( vals[i].type() ) {
       case Val.FRM:  
-        fr.add(vals[i].getFrame());
+        fr.add(fr.makeCompatible(vals[i].getFrame()));
         break;
       case Val.FUN:  throw H2O.unimpl();
       case Val.STR:  throw H2O.unimpl();
