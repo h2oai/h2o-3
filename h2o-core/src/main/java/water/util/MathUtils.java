d13f8d0d814282706e8356bba1b0e00019cd69c4
Arno Candel <arno.candel@gmail.com>
2015-08-08 00:35:14 -0700

    WIP towards adding 1D FFT.

--- h2o-core/src/main/java/water/util/MathUtils.java
+++ h2o-core/src/main/java/water/util/MathUtils.java
@@ -1,5 +1,15 @@
 package water.util;
 
+import edu.emory.mathcs.jtransforms.fft.DoubleFFT_1D;
+import edu.emory.mathcs.utils.ConcurrencyUtils;
+import hex.Transformer;
+import water.*;
+import water.exceptions.H2OIllegalArgumentException;
+import water.fvec.Chunk;
+import water.fvec.Frame;
+import water.fvec.NewChunk;
+import water.fvec.Vec;
+
 import java.util.Arrays;
 
 public class MathUtils {
@@ -220,4 +230,85 @@ public class MathUtils {
     return d < 0?-1:1;
   }
 
+  public static class FFT {
+    public class FFTTransformer extends Transformer<FFTTransformer> {
+
+      /** Input dataset to transform */
+      public Frame dataset;
+      public Key<Frame> destination_frame;
+
+      public FFTTransformer() { this(Key.<FFTTransformer>make()); }
+      public FFTTransformer(Key<FFTTransformer> dest) { this(dest, "FFT job"); }
+      public FFTTransformer(Key<FFTTransformer> dest, String desc) {
+        super(dest, desc);
+      }
+
+      @Override
+      protected FFTTransformer execImpl() {
+        return this;
+      }
+    }
+
+    private static void initCheck(Frame input) {
+      ConcurrencyUtils.setNumberOfThreads(1);
+      for (Vec v : input.vecs()) {
+        if (v.naCnt() > 0)
+          throw new H2OIllegalArgumentException("FFT can not be computed on rows with missing values");
+        if (!v.isNumeric())
+          throw new H2OIllegalArgumentException("FFT can only be computed on numeric columns");
+      }
+    }
+
+    /**
+     * Compute the real-valued 1D Fourier transform for each row in the given Frame, and return a new Frame
+     * @param input Frame containing numeric columns with data samples
+     * @return Frame containing real-valued 1D FFT of each row (same dimensionality)
+     */
+    public static Frame transform1D(Frame input) {
+      initCheck(input);
+      return new MRTask() {
+        @Override
+        public void map(Chunk[] cs, NewChunk[] ncs) {
+          int N = cs.length;
+          double[] a = new double[N];
+          for (int row=0; row<cs[0]._len; ++row) {
+            // fill 1D array
+            for (int i = 0; i < N; ++i)
+              a[i] = cs[i].atd(row);
+            // compute FFT for each row
+            new DoubleFFT_1D(N).realForward(a);
+            // write result to NewChunk
+            for (int i = 0; i < N; ++i)
+              ncs[i].addNum(a[i]);
+          }
+        }
+      }.doAll(input.numCols(), input).outputFrame();
+    }
+
+    /**
+     * Compute the real-valued 1D inverse Fourier transform for each row in the given Frame, and return a new Frame
+     * @param input Frame containing numeric columns with data samples
+     * @return Frame containing real-valued 1D inverse FFT of each row (same dimensionality)
+     */
+    public static Frame inverseTransform1D(Frame input) {
+      initCheck(input);
+      return new MRTask() {
+        @Override
+        public void map(Chunk[] cs, NewChunk[] ncs) {
+          int N = cs.length;
+          double[] a = new double[N];
+          for (int row=0; row<cs[0]._len; ++row) {
+            // fill 1D array
+            for (int i = 0; i < N; ++i)
+              a[i] = cs[i].atd(row);
+            // compute inverse FFT for each row
+            new DoubleFFT_1D(N).realInverse(a, true);
+            // write result to NewChunk
+            for (int i = 0; i < N; ++i)
+              ncs[i].addNum(a[i]);
+          }
+        }
+      }.doAll(input.numCols(), input).outputFrame();
+    }
+  }
 }
---
+++ h2o-core/src/test/java/water/FFTTest.java
@@ -0,0 +1,52 @@
+package water;
+
+import hex.CreateFrame;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import water.fvec.Frame;
+import water.util.*;
+
+public class FFTTest extends TestUtil {
+  @BeforeClass()
+  public static void setup() {
+    stall_till_cloudsize(1);
+  }
+
+  @Test
+  public void run() {
+    Frame frame = null;
+    Frame frameFFT = null;
+    Frame frameRec = null;
+    try {
+      CreateFrame cf = new CreateFrame();
+      cf.rows = 1000;
+      cf.cols = 1000;
+      cf.categorical_fraction = 0.0;
+      cf.integer_fraction = 0;
+      cf.binary_fraction = 0;
+      cf.missing_fraction = 0;
+      cf.factors = 0;
+      cf.seed = 1234;
+      cf.execImpl();
+      cf.get();
+      frame = DKV.getGet(cf.dest());
+      long now = System.currentTimeMillis();
+      frameFFT = MathUtils.FFT.transform1D(frame);
+      Log.info("Computed 1D DFT of " + cf.rows + " rows of size " + cf.cols + " in" + PrettyPrint.msecs(System.currentTimeMillis() - now, true));
+
+      now = System.currentTimeMillis();
+      frameRec = MathUtils.FFT.inverseTransform1D(frameFFT);
+      Log.info("Computed inverse 1D DFT of " + cf.rows + " rows of size " + cf.cols + " in" + PrettyPrint.msecs(System.currentTimeMillis() - now, true));
+
+      for (int i=0; i<frame.vecs().length; ++i)
+        TestUtil.assertVecEquals(frame.vecs()[i], frameRec.vecs()[i], 1e-5);
+      Log.info("Identity test passed: FFT^-1(FFT(frame)) == frame");
+    } finally {
+      if (frame!=null) frame.delete();
+      if (frameFFT!=null) frameFFT.delete();
+      if (frameRec!=null) frameRec.delete();
+    }
+  }
+
+}
