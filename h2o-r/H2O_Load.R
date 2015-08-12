e6ad4360721f2d8f6ac858f193f34d4cbac1b27e
anqi <anqi@h2o.ai>
2015-08-12 16:45:03 -0700

    Should load grid.R as well

--- h2o-r/H2O_Load.R
+++ h2o-r/H2O_Load.R
@@ -11,8 +11,9 @@ function() {
   warning("MAY NOT WORK ON YOUR SYSTEM -- **TRY TO CHANGE `ROOT.PATH`!**")
   to_src <- c("classes.R", "connection.R", "constants.R", "logging.R", "communication.R", 
               "kvstore.R", "exec.R", "ops.R", "frame.R", "ast.R", "astfun.R", "import.R", 
-              "parse.R", "export.R", "models.R", "edicts.R", "glm.R", "glrm.R", "pca.R", "kmeans.R", 
-              "gbm.R", "deeplearning.R", "naivebayes.R", "randomForest.R", "svd.R", "locate.R")
+              "parse.R", "export.R", "models.R", "edicts.R", "glm.R", "glrm.R", "grid.R",
+              "pca.R", "kmeans.R", "gbm.R", "deeplearning.R", "naivebayes.R", "randomForest.R", 
+              "svd.R", "locate.R")
   require(rjson); require(RCurl)
   invisible(lapply(to_src,function(x){source(paste(FULL.PATH, x, sep = ""))}))
 }
