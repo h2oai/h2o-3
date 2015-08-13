9e2e14c1ce782e0016cb632e9130ce291826b97e
mmalohlava <michal.malohlava@gmail.com>
2015-08-12 19:15:49 -0700

    Add rest end-points for glrm,svd,pca,naive bayes algorithms.

--- h2o-core/src/main/java/hex/grid/GridSearch.java
+++ h2o-core/src/main/java/hex/grid/GridSearch.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import hex.Model;
 import hex.ModelBuilder;
 import hex.ModelParametersBuilderFactory;
-import hex.grid.HyperSpaceWalker.CartezianWalker;
+import hex.grid.HyperSpaceWalker.CartesianWalker;
 import water.DKV;
 import water.H2O;
 import water.Job;
@@ -281,9 +281,9 @@ public final class GridSearch<MP extends Model.Parameters> extends Job<Grid> {
       final ModelFactory<MP> modelFactory,
       final ModelParametersBuilderFactory<MP> paramsBuilderFactory) {
     // Create a walker to traverse hyper space of model parameters
-    CartezianWalker<MP>
+    CartesianWalker<MP>
         hyperSpaceWalker =
-        new CartezianWalker<>(params, hyperParams, paramsBuilderFactory);
+        new CartesianWalker<>(params, hyperParams, paramsBuilderFactory);
 
     return startGridSearch(destKey, modelFactory, hyperSpaceWalker);
   }
