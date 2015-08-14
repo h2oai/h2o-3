9d189cd75e490c620351d0a9ff0d2c8335bb3389
Arno Candel <arno.candel@gmail.com>
2015-08-14 02:13:17 -0700

    Fix bad test - offset only works for DL regression.

--- h2o-py/tests/testdir_algos/deeplearning/pyunit_offsets_and_distributionsDeeplearning.py
+++ h2o-py/tests/testdir_algos/deeplearning/pyunit_offsets_and_distributionsDeeplearning.py
@@ -16,10 +16,10 @@ def offsets_and_distributions(ip,port):
     insurance = h2o.import_frame(h2o.locate("smalldata/glm_test/insurance.csv"))
     insurance["offset"] = insurance["Holders"].log()
 
-    # bernoulli
-    dl = h2o.deeplearning(x=cars[2:8], y=cars["economy_20mpg"], distribution="bernoulli", offset_column="x1",
-                           training_frame=cars)
-    predictions = dl.predict(cars)
+    # bernoulli - offset not supported
+    #dl = h2o.deeplearning(x=cars[2:8], y=cars["economy_20mpg"], distribution="bernoulli", offset_column="x1",
+    #                       training_frame=cars)
+    #predictions = dl.predict(cars)
 
     # gamma
     dl = h2o.deeplearning(x=insurance[0:3], y=insurance["Claims"], distribution="gamma", offset_column="offset",
