22e08082b9442747ea9139ff05a98413a8bbe4a2
mmalohlava <michal.malohlava@gmail.com>
2015-08-15 18:10:25 -0700

    Enable more gridable parameters for DRF/GBM.

--- h2o-algos/src/main/java/hex/schemas/SharedTreeV3.java
+++ h2o-algos/src/main/java/hex/schemas/SharedTreeV3.java
@@ -2,8 +2,8 @@ package hex.schemas;
 
 import hex.tree.SharedTree;
 import hex.tree.SharedTreeModel.SharedTreeParameters;
-import water.api.*;
-import water.api.FrameV3.ColSpecifierV3;
+import water.api.API;
+import water.api.ModelParametersSchema;
 
 public class SharedTreeV3<B extends SharedTree, S extends SharedTreeV3<B,S,P>, P extends SharedTreeV3.SharedTreeParametersV3> extends ModelBuilderSchema<B,S,P> {
 
@@ -13,7 +13,7 @@ public class SharedTreeV3<B extends SharedTree, S extends SharedTreeV3<B,S,P>, P
      * For imbalanced data, balance training data class counts via
      * over/under-sampling. This can result in improved predictive accuracy.
      */
-    @API(help = "Balance training data class counts via over/under-sampling (for imbalanced data).", level = API.Level.secondary, direction = API.Direction.INOUT)
+    @API(help = "Balance training data class counts via over/under-sampling (for imbalanced data).", level = API.Level.secondary, direction = API.Direction.INOUT, gridable = true)
     public boolean balance_classes;
 
     /**
@@ -21,14 +21,14 @@ public class SharedTreeV3<B extends SharedTree, S extends SharedTreeV3<B,S,P>, P
      * Only when balance_classes is enabled.
      * If not specified, they will be automatically computed to obtain class balance during training.
      */
-    @API(help = "Desired over/under-sampling ratios per class (in lexicographic order). If not specified, sampling factors will be automatically computed to obtain class balance during training. Requires balance_classes.", level = API.Level.expert, direction = API.Direction.INOUT)
+    @API(help = "Desired over/under-sampling ratios per class (in lexicographic order). If not specified, sampling factors will be automatically computed to obtain class balance during training. Requires balance_classes.", level = API.Level.expert, direction = API.Direction.INOUT, gridable = true)
     public float[] class_sampling_factors;
 
     /**
      * When classes are balanced, limit the resulting dataset size to the
      * specified multiple of the original dataset size.
      */
-    @API(help = "Maximum relative size of the training data after balancing class counts (can be less than 1.0). Requires balance_classes.", /* dmin=1e-3, */ level = API.Level.expert, direction = API.Direction.INOUT)
+    @API(help = "Maximum relative size of the training data after balancing class counts (can be less than 1.0). Requires balance_classes.", /* dmin=1e-3, */ level = API.Level.expert, direction = API.Direction.INOUT, gridable = true)
     public float max_after_balance_size;
 
     /** For classification models, the maximum size (in terms of classes) of
@@ -40,7 +40,7 @@ public class SharedTreeV3<B extends SharedTree, S extends SharedTreeV3<B,S,P>, P
     /**
      * The maximum number (top K) of predictions to use for hit ratio computation (for multi-class only, 0 to disable)
      */
-    @API(help = "Max. number (top K) of predictions to use for hit ratio computation (for multi-class only, 0 to disable)", level = API.Level.secondary, direction=API.Direction.INOUT)
+    @API(help = "Max. number (top K) of predictions to use for hit ratio computation (for multi-class only, 0 to disable)", level = API.Level.secondary, direction=API.Direction.INOUT, gridable = true)
     public int max_hit_ratio_k;
 
     @API(help="Number of trees.", gridable = true)
@@ -61,10 +61,10 @@ public class SharedTreeV3<B extends SharedTree, S extends SharedTreeV3<B,S,P>, P
     @API(help="For categorical columns (enum), build a histogram of this many bins, then split at the best point. Higher values can lead to more overfitting.", gridable = true)
     public int nbins_cats;
 
-    @API(help="Stop making trees when the R^2 metric equals or exceeds this", level = API.Level.secondary)
+    @API(help="Stop making trees when the R^2 metric equals or exceeds this", level = API.Level.secondary, gridable = true)
     public double r2_stopping;
 
-    @API(help = "Seed for pseudo random number generator (if applicable)")
+    @API(help = "Seed for pseudo random number generator (if applicable)", gridable = true)
     public long seed;
 
     @API(help="Run on one node only; no network overhead but fewer cpus used.  Suitable for small datasets.", level = API.Level.secondary)
