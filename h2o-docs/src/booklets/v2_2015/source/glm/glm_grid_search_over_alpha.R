5c47d9529e189ce1776cb32b048807946c63854f
Tom Kraljevic <tomk@tomk.net>
2015-08-15 20:17:29 -0700

    Example works now.

--- h2o-docs/src/booklets/v2_2015/source/glm/glm_grid_search_over_alpha.R
+++ h2o-docs/src/booklets/v2_2015/source/glm/glm_grid_search_over_alpha.R
@@ -7,4 +7,7 @@ alpha_opts = c(0, 0.25, 0.5, 0.75, 1.0)
 hyper_parameters = list(alpha = alpha_opts)
 grid <- h2o.grid("glm", grid_id="glm_grid_test", hyper_params = hyper_parameters,
                  y = "CAPSULE", x = c("AGE", "RACE", "PSA", "GLEASON"), training_frame = h2o_df, family = "binomial")
-grid_models <- lapply(grid@model_ids, function(mid) { model = h2o.getModel(mid) })
+grid_models <- lapply(grid@model_ids, function(model_id) { model = h2o.getModel(model_id) })
+for (i in 1:length(grid_models)) {
+    print(sprintf("regularization: %-50s  auc: %f", grid_models[[i]]@model$model_summary$regularization, h2o.auc(grid_models[[i]])))
+}
