5aa61ef8c2e2952828439a31d6ed9cdf0420d724
mmalohlava <michal.malohlava@gmail.com>
2015-08-19 11:54:04 -0700

    Fix test API.

--- h2o-py/tests/testdir_algos/deeplearning/pyunit_offsets_and_distributionsDeeplearning.py
+++ h2o-py/tests/testdir_algos/deeplearning/pyunit_offsets_and_distributionsDeeplearning.py
@@ -5,7 +5,7 @@ import h2o
 def offsets_and_distributions(ip,port):
 
     # cars
-    cars = h2o.upload_frame(h2o.locate("smalldata/junit/cars_20mpg.csv"))
+    cars = h2o.upload_file(h2o.locate("smalldata/junit/cars_20mpg.csv"))
     cars = cars[cars["economy_20mpg"].isna() == 0]
     cars["economy_20mpg"] = cars["economy_20mpg"].asfactor()
     offset = h2o.H2OFrame(python_obj=[[.5] for x in range(398)])
