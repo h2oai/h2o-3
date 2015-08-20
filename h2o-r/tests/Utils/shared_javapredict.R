4a87a2f4c79bc1c500870824ae248016711f62d4
Tom Kraljevic <tomk@tomk.net>
2015-08-19 20:30:33 -0700

    Added GLM javapredict test.

---
+++ h2o-r/tests/Utils/shared_javapredict.R
@@ -0,0 +1,116 @@
+
+doJavapredictTest <- function(model, training_frame, test_file, test_frame, ...) {
+  heading("Uploading train data to H2O")
+
+  heading(paste("Creating", model, "model in H2O"))
+  if (model == "glm") {
+    model <- h2o.glm(training_frame = training_frame, ...)
+    print(model)
+  } else if (model == "gbm") {
+    model <- h2o.gbm(training_frame = training_frame, ...)
+    print(model)
+  } else if (model == "randomForest") {
+    model <- h2o.randomForest(training_frame = training_frame, ...)
+    print(model)
+  } else {
+    stop(paste("Unknown model type", model))
+  }
+  
+  heading("Downloading Java prediction model code from H2O")
+  model_key <- model@model_id
+  tmpdir_name <- sprintf("%s/results/tmp_model_%s", TEST_ROOT_DIR, as.character(Sys.getpid()))
+  cmd <- sprintf("rm -fr %s", tmpdir_name)
+  safeSystem(cmd)
+  cmd <- sprintf("mkdir -p %s", tmpdir_name)
+  safeSystem(cmd)
+  h2o.download_pojo(model, tmpdir_name)
+
+  heading("Uploading test data to H2O")
+
+  heading("Predicting in H2O")
+  pred <- h2o.predict(model, test_frame)
+  summary(pred)
+  head(pred)
+  prediction1 <- as.data.frame(pred)
+  cmd <- sprintf(   "%s/out_h2o.csv", tmpdir_name)
+  write.csv(prediction1, cmd, quote=FALSE, row.names=FALSE)
+
+  heading("Setting up for Java POJO")
+  test_with_response <- read.csv(test_file, header=T)
+  test_without_response <- test_with_response[,x]
+  if(is.null(ncol(test_without_response))) {
+    test_without_response <- data.frame(test_without_response)
+    colnames(test_without_response) <- x
+  }
+  write.csv(test_without_response, file = sprintf("%s/in.csv", tmpdir_name), row.names=F, quote=F)
+  cmd <- sprintf("curl http://%s:%s/3/h2o-genmodel.jar > %s/h2o-genmodel.jar", myIP, myPort, tmpdir_name)
+  safeSystem(cmd)
+  cmd <- sprintf("cp PredictCSV.java %s", tmpdir_name)
+  safeSystem(cmd)
+  cmd <- sprintf("javac -cp %s/h2o-genmodel.jar -J-Xmx4g -J-XX:MaxPermSize=256m %s/PredictCSV.java %s/%s.java", tmpdir_name, tmpdir_name, tmpdir_name, model_key)
+  safeSystem(cmd)
+
+  heading("Predicting with Java POJO")
+  cmd <- sprintf("java -ea -cp %s/h2o-genmodel.jar:%s -Xmx4g -XX:MaxPermSize=256m -XX:ReservedCodeCacheSize=256m PredictCSV --header --model %s --input %s/in.csv --output %s/out_pojo.csv", tmpdir_name, tmpdir_name, model_key, tmpdir_name, tmpdir_name)
+  safeSystem(cmd)
+
+  heading("Comparing predictions between H2O and Java POJO")
+  prediction2 <- read.csv(sprintf("%s/out_pojo.csv", tmpdir_name), header=T)
+  if (nrow(prediction1) != nrow(prediction2)) {
+    warning("Prediction mismatch")
+    print(paste("Rows from H2O", nrow(prediction1)))
+    print(paste("Rows from Java POJO", nrow(prediction2)))
+    stop("Number of rows mismatch")
+  }
+
+  tolerance = 1e-8
+  match <- all.equal(prediction1, prediction2, tolerance = tolerance)
+  if (class(match) != "logical") {
+    match <- FALSE
+  }
+  
+  if (! match) {
+    for (i in 1:nrow(prediction1)) {
+      rowmatch <- all.equal(prediction1[i,], prediction2[i,], tolerance = tolerance)
+      if (class(rowmatch) != "logical") {
+        rowmatch <- FALSE
+      }
+      if (! rowmatch) {
+        print("----------------------------------------------------------------------")
+        print("")
+        print(paste("Prediction mismatch on data row", i, "of test file", test))
+        print("")
+        print(      "(Note: That is the 1-based data row number, not the file line number.")
+        print(      "       If you have a header row, then the file line number is off by one.)")
+        print("")
+        print("----------------------------------------------------------------------")
+        print("")
+        print("Data from failing row")
+        print("")
+        print(test_without_response[i,])
+        print("")
+        print("----------------------------------------------------------------------")
+        print("")
+        print("Prediction from H2O")
+        print("")
+        print(prediction1[i,])
+        print("")
+        print("----------------------------------------------------------------------")
+        print("")
+        print("Prediction from Java POJO")
+        print("")
+        print(prediction2[i,])
+        print("")
+        print("----------------------------------------------------------------------")
+        print("")
+        stop("Prediction mismatch")
+      }
+    }
+
+    stop("Paranoid; should not reach here")
+  }
+
+  heading("Cleaning up tmp files")
+  cmd <- sprintf("rm -fr %s", tmpdir_name)
+  safeSystem(cmd)
+}
---
+++ h2o-r/tests/testdir_javapredict/runit_NOPASS_GLM_javapredict_prostate.R
@@ -0,0 +1,38 @@
+#----------------------------------------------------------------------
+# Purpose:  This test exercises the GLM model downloaded as java code.
+#
+# Notes:    Assumes unix environment.
+#           curl, javac, java must be installed.
+#           java must be at least 1.6.
+#----------------------------------------------------------------------
+
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+# setwd("/Users/tomk/0xdata/ws/h2o-3/h2o-r/tests/testdir_javapredict")
+TEST_ROOT_DIR <- ".."
+source("../h2o-runit.R")
+source("../Utils/shared_javapredict.R")
+
+#----------------------------------------------------------------------
+# Parameters for the test.
+#----------------------------------------------------------------------
+
+training_file          <- h2o:::.h2o.locate("smalldata/prostate/prostate.csv")
+training_frame         <- h2o.importFile(training_file)
+training_frame$CAPSULE <- as.factor(training_frame$CAPSULE)
+
+test_file              <- h2o:::.h2o.locate("smalldata/prostate/prostate.csv")
+test_frame             <- h2o.importFile(test_file)
+test_frame$CAPSULE     <- as.factor(test_frame$CAPSULE)
+
+y <- "CAPSULE"
+x <- c("AGE","RACE","DPROS","DCAPS","PSA","VOL","GLEASON")
+
+#----------------------------------------------------------------------
+# Run the test
+#----------------------------------------------------------------------
+doJavapredictTest(model = "glm", training_frame = training_frame, test_file = test_file, test_frame = test_frame,
+                  y = y,
+                  x = x,
+                  family = "binomial")
+
+PASS()
