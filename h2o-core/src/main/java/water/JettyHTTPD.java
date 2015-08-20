a65a116875ca17eaf5b3535135f152781b51a40f
Nishant Kalonia <nishant@h2o.ai>
2015-08-20 13:14:16 -0700

    PUBDEV-1431: /3/DownloadDataset.bin is now a registered handler in JettyHTTPD.java. A dummy handler was also created in RequestServer.java for API reasons, but it should never be called.

--- h2o-core/src/main/java/water/JettyHTTPD.java
+++ h2o-core/src/main/java/water/JettyHTTPD.java
@@ -25,6 +25,7 @@ import water.api.H2OErrorV3;
 import water.exceptions.H2OAbstractRuntimeException;
 import water.exceptions.H2OFailException;
 import water.fvec.UploadFileVec;
+import water.fvec.Frame;
 import water.init.NodePersistentStorage;
 import water.util.FileUtils;
 import water.util.HttpResponseStatus;
@@ -212,6 +213,7 @@ public class JettyHTTPD {
     context.addServlet(H2oNpsBinServlet.class,   "/3/NodePersistentStorage.bin/*");
     context.addServlet(H2oPostFileServlet.class, "/3/PostFile.bin");
     context.addServlet(H2oPostFileServlet.class, "/3/PostFile");
+    context.addServlet(H2oDatasetServlet.class,   "/3/DownloadDataset.bin");   
     context.addServlet(H2oDefaultServlet.class,  "/");
 
     Handler[] handlers = {gh, rhh, eh1, context};
@@ -491,6 +493,51 @@ public class JettyHTTPD {
     response.setHeader("X-h2o-build-project-version", H2O.ABV.projectVersion());
     response.setHeader("X-h2o-rest-api-version-max", Integer.toString(water.api.RequestServer.H2O_REST_API_VERSION));
     response.setHeader("X-h2o-cluster-id", Long.toString(H2O.CLUSTER_ID));
+  }
+
+  public static class H2oDatasetServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws IOException, ServletException {
+      String uri = getDecodedUri(request);
+      try {
+        boolean use_hex = false;
+        String f_name = request.getParameter("frame_id");
+        String hex_string = request.getParameter("hex_string");
+        if (f_name == null) {
+          throw new RuntimeException("Cannot find value for parameter \'frame_id\'");
+        }
+        if (hex_string != null && hex_string.toLowerCase().equals("true")) {
+          use_hex = true;
+        }
+        
+        Frame dataset = DKV.getGet(f_name);
+        // TODO: Find a way to determing the hex_string parameter. It should not always be false
+        InputStream is = dataset.toCSV(true, use_hex);
+        response.setContentType("application/octet-stream");
+        // Clean up the file name
+        int x = f_name.length()-1;
+        boolean dot=false;
+        for( ; x >= 0; x-- )
+          if( !Character.isLetterOrDigit(f_name.charAt(x)) && f_name.charAt(x)!='_' )
+            if( f_name.charAt(x)=='.' && !dot ) dot=true;
+            else break;
+        String suggested_fname = f_name.substring(x+1).replace(".hex", ".csv");
+        if( !suggested_fname.endsWith(".csv") )
+          suggested_fname = suggested_fname+".csv";
+        f_name = suggested_fname;
+        response.addHeader("Content-Disposition", "attachment; filename=" + f_name);
+        setResponseStatus(response, HttpServletResponse.SC_OK);
+        OutputStream os = response.getOutputStream();
+        water.util.FileUtils.copyStream(is, os, 2048);
+      }
+      catch (Exception e) {
+        sendErrorResponse(response, e, uri);
+      }
+      finally {
+        logRequest("GET", request, response);
+      }
+    }
   }
 
   @SuppressWarnings("serial")
