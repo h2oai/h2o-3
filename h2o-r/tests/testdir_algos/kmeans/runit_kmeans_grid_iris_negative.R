1437d692b626be9cb7fe9f8d4b64ab6d278c5537
eric eckstrand <eric.eckstrand@gmail.com>
2015-08-21 12:02:26 -0700

    kmeans grid negative runit.

---
+++ h2o-r/tests/testdir_algos/kmeans/runit_kmeans_grid_iris_negative.R
@@ -0,0 +1,47 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../h2o-runit.R')
+
+check.kmeans.grid.cars.negative <- function(conn) {
+  iris <- h2o.uploadFile(locate("smalldata/iris/iris.csv"))
+
+  ## Invalid kmeans parameters
+  grid_space <- list()
+  grid_space$max_iterations <- c(1,2,-20)
+  grid_space$init = c('Random','PlusPlus','Foo')
+  Log.info(lapply(names(grid_space), function(n) paste0("The provided ",n," search space: ", grid_space[n])))
+
+  expected_grid_space <- list()
+  expected_grid_space$max_iterations <- c(1,2)
+  expected_grid_space$init = c('Random','PlusPlus')
+  Log.info(lapply(names(grid_space), function(n) paste0("The expected ",n," search space: ", expected_grid_space[n])))
+
+  Log.info("Constructing the grid of kmeans models with some invalid kmeans parameters...")
+  cars_kmeans_grid <- h2o.grid("kmeans", grid_id="kmeans_grid_iris_test", x=1:4, k=3, training_frame=iris, hyper_params=grid_space)
+  print(cars_kmeans_grid)
+
+  Log.info("Performing various checks of the constructed grid...")
+  Log.info("Check cardinality of grid, that is, the correct number of models have been created...")
+  expect_equal(length(cars_kmeans_grid@model_ids), 4)
+
+  Log.info("Check that the hyper_params that were passed to grid, were used to construct the models...")
+  # Get models
+  grid_models <- lapply(cars_kmeans_grid@model_ids, function(mid) { model = h2o.getModel(mid) })
+  # Check expected number of models
+  expect_equal(length(grid_models), 4)
+  # Check parameters coverage
+  for ( name in names(grid_space) ) { expect_model_param(grid_models, name, expected_grid_space[[name]]) }
+
+  # TODO: Check error messages for cases with invalid kmeans parameters
+
+  ## Non-gridable parameter passed as grid parameter
+  grid_space <- list()
+  grid_space$init <- iris[c(2,70,148),1:4]
+
+  Log.info(paste0("Constructing the grid of kmeans models with non-gridable parameter build_tree_one_node"))
+  expect_error(cars_kmeans_grid <- h2o.grid("kmeans", grid_id="kmeans_grid_iris_test", x=1:4, k=3, training_frame=iris, hyper_params=grid_space))
+
+  testEnd()
+}
+
+doTest("K-Means Grid Search using bad parameters", check.kmeans.grid.cars.negative)
+
