302edfb53a2edea81c406c13cd8eb2931accfca1
ledell <erin@h2o.ai>
2015-08-29 01:37:37 -0700

    DL booklet mnist code added

--- h2o-docs/src/booklets/v2_2015/source/deeplearning/deeplearning_examplerun.R
+++ h2o-docs/src/booklets/v2_2015/source/deeplearning/deeplearning_examplerun.R
@@ -1,13 +1,13 @@
-# Load the data and prepare for modeling
-airlines.hex <- h2o.uploadFile(h2o_server, path = "allyears2k_headers.csv", header = TRUE, sep = ",", destination_frame = "airlines.hex")
+# We first encode the response column as categorical for multinomial classification
+train[,785] <- as.factor(train[,785])
 
-# Generate random numbers and create training, validation, testing splits
-r <- h2o.runif(airlines.hex)
-air_train.hex <- airlines.hex[r  < 0.6,]
-air_valid.hex <- airlines.hex[(r >= 0.6) & (r < 0.9),]
-air_test.hex  <- airlines.hex[r  >= 0.9,]
-
-myX <- c("DayofMonth", "DayOfWeek")
-
-# Now, train the Deep Learning model:
-air.model <- h2o.deeplearning(y = "IsDepDelayed", x = myX, distribution="bernoulli", training_frame = air_train.hex, validation_frame = air_valid.hex, activation = "Maxout", hidden = c(100,100)))
+# Train a Deep Learning model and validate on a test set
+mnist_model <- h2o.deeplearning(x = 1:784, y = 785, 
+                               training_frame = train,
+                               distribution = "multinomial",
+                               activation = "RectifierWithDropout", 
+                               hidden = c(200,200,200), 
+                               input_dropout_ratio = 0.2, 
+                               l1 = 1e-5, 
+                               validation_frame = test, 
+                               epochs = 10)
