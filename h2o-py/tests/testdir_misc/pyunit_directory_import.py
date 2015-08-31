e0f5fca93bf866be64518c8d32c881054f3d8063
eric eckstrand <eric.eckstrand@gmail.com>
2015-08-30 18:21:46 -0700

    os.path.isdir will return false for urls like hdfs://172.16.2.186/datasets/foo/. This poses a problem when trying to import a directory of files (specified by a url). add pyunit for testing importing a directory of files.

---
+++ h2o-py/tests/testdir_misc/pyunit_directory_import.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.insert(1, "../../")
+import h2o, tests
+
+def directory_import(ip,port):
+
+    running_inside_h2o = tests.is_running_internal_to_h2o()
+
+    if running_inside_h2o:
+        hdfs_name_node = tests.get_h2o_internal_hdfs_name_node()
+        url1 = "hdfs://{0}{1}".format(hdfs_name_node, "/datasets/iris/identical_iris_files/iris1.csv")
+        url2 = "hdfs://{0}{1}".format(hdfs_name_node, "/datasets/iris/identical_iris_files/")
+        print "Importing HDFS file {0} and directory {1}".format(url1, url2)
+        frm_one = h2o.import_file(url1)
+        frm_all = h2o.import_file(url2)
+
+        r1, c1 = frm_one.dim
+        ra, ca = frm_all.dim
+
+        assert r1*3 == ra, "Expected 3 times the rows, but got {0} and {1}".format(r1,ra)
+        assert c1 == ca, "Expected same number of cols, but got {0} and {1}".format(c1,ca)
+
+    small1 = h2o.locate("smalldata/jira/identical_files/iris1.csv")
+    small2 = small1.split("iris1.csv")[0]
+    print "Importing smalldata file {0} and directory {1}".format(small1, small2)
+    frm_one = h2o.import_file(small1)
+    frm_all = h2o.import_file(small2)
+
+    r1, c1 = frm_one.dim
+    ra, ca = frm_all.dim
+
+    assert r1*3 == ra, "Expected 3 times the rows, but got {0} and {1}".format(r1,ra)
+    assert c1 == ca, "Expected same number of cols, but got {0} and {1}".format(c1,ca)
+
+if __name__ == "__main__":
+    tests.run_test(sys.argv, directory_import)
