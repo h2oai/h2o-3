47c5e4accd001c4b607ada8df9eafbf3ec8a8e6f
eric eckstrand <eric.eckstrand@gmail.com>
2015-08-30 18:52:39 -0700

    missed one pyunit in bulk remove of ip,port args.

--- h2o-py/tests/testdir_misc/pyunit_user.py
+++ h2o-py/tests/testdir_misc/pyunit_user.py
@@ -4,8 +4,6 @@ import h2o, tests
 
 
 def user():
-    # Connect to a pre-existing cluster
-    h2o.init(ip=ip, port=port)
 
     a = h2o.import_file(path=h2o.locate("smalldata/iris/iris_wheader.csv"))[0:4]
     a.head()
