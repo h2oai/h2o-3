ef0d1e7c97e399df1e8b0c4409f960ee27e8632c
ledell <erin@h2o.ai>
2015-09-05 01:50:29 -0700

    Modified DL checkpoint demo in booklet

--- h2o-docs/src/booklets/v2_2015/source/deeplearning/deeplearning_checkpoint.R
+++ h2o-docs/src/booklets/v2_2015/source/deeplearning/deeplearning_checkpoint.R
@@ -1,5 +1,5 @@
-# TO DO: Check this code
-
+# Re-start the training process on a saved DL model
+# using the `checkpoint` argument
 model_chkp <- h2o.deeplearning(x = 1:784, y = 785, 
                                training_frame = train, 
                                checkpoint = model_grid@model_ids[[1]],
