dcb75cfd7b4c418738c862fc1b8379577c97a11a
Arno Candel <arno.candel@gmail.com>
2015-09-04 20:30:02 -0700

    Fix merge.

--- h2o-algos/src/main/java/hex/deeplearning/DeepLearningModelInfo.java
+++ h2o-algos/src/main/java/hex/deeplearning/DeepLearningModelInfo.java
@@ -119,6 +119,7 @@ public class DeepLearningModelInfo extends Iced {
   public double[] rms_weight;
   public double[] mean_a;
 
+  private volatile boolean bogus = false;
   private volatile boolean unstable = false;
   public boolean unstable() { return unstable; }
   public void set_unstable() {
@@ -654,10 +655,10 @@ public class DeepLearningModelInfo extends Iced {
       // This can happen with Rectifier units when L1/L2/max_w2 are all set to 0, especially when using more than 1 hidden layer.
       final double thresh = 1e10;
       final double bthresh = 1000;
-      unstable |= Math.abs(mean_bias[y]) > bthresh || isNaN(mean_bias[y])
-              || rms_bias[y] > bthresh || isNaN(rms_bias[y])
-              || Math.abs(mean_weight[y]) > thresh || isNaN(mean_weight[y])
+      unstable |= isNaN(mean_bias[y]) || isNaN(rms_bias[y])
+              || mean_weight[y] > thresh || isNaN(mean_weight[y])
               || rms_weight[y] > thresh || isNaN(rms_weight[y]);
+      bogus |= mean_bias[y] > bthresh || rms_bias[y] > bthresh;
     }
   }
 
