51a6d583086f6216601b6e67e9ddc6263d2478af
Cliff Click <cliffc@acm.org>
2015-09-10 15:17:51 -0700

    merge build error

--- h2o-core/src/main/java/water/H2O.java
+++ h2o-core/src/main/java/water/H2O.java
@@ -19,9 +19,7 @@ import water.util.Log;
 import water.util.OSUtils;
 import water.util.PrettyPrint;
 
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.io.*;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.lang.reflect.Field;
