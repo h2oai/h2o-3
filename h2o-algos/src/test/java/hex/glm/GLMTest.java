b74872ca4fb573dfedbf653cc5daf28581c07d9f
Cliff Click <cliffc@acm.org>
2015-09-10 17:59:41 -0700

    tone down coordinate descent test
    limit lambda search to 5 lambdas

--- h2o-algos/src/test/java/hex/glm/GLMTest.java
+++ h2o-algos/src/test/java/hex/glm/GLMTest.java
@@ -1106,6 +1106,7 @@ public class GLMTest  extends TestUtil {
       params._standardize = false;
       params._solver = Solver.COORDINATE_DESCENT_NAIVE;
       params._lambda_search = true;
+      params._nlambdas = 5;
       job = new GLM(Key.make("airlines_cat_nostd"), "Airlines with auto-expanded categorical variables, no standardization", params);
       model1 = job.trainModel().get();
       double [] beta = model1.beta();
