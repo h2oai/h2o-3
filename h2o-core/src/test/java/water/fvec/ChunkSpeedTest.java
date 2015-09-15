db6c960619e133f4050bbc6ae028f6eb5bd41275
Arno Candel <arno.candel@gmail.com>
2015-09-14 22:17:46 -0700

    Add benchmark of new bulk reader.

--- h2o-core/src/test/java/water/fvec/ChunkSpeedTest.java
+++ h2o-core/src/test/java/water/fvec/ChunkSpeedTest.java
@@ -19,8 +19,8 @@ public class ChunkSpeedTest extends TestUtil {
     for (int j=0; j<cols; ++j) {
       for (int i = 0; i < rows; ++i) {
 //        switch (j%1) { //just do 1 byte chunks
-//        switch (j%2) { //just do 1/2 byte chunks
-        switch (j%3) { // do all 3 chunk types
+        switch (j%2) { //just do 1/2 byte chunks
+//        switch (j%3) { // do all 3 chunk types
           case 0:
             raw[j][i] = i % 200; //C1NChunk - 1 byte integer
             break;
@@ -83,6 +83,31 @@ public class ChunkSpeedTest extends TestUtil {
       Log.info("");
     }
 
+    // bulk chunks
+    {
+      long start = 0;
+      double sum = 0;
+      double[] bulk = new double[rows]; //allocate only once
+      for (int r = 0; r < rep; ++r) {
+        if (r==rep/10)
+          start = System.currentTimeMillis();
+        for (int j=0; j<cols; ++j) {
+          chunks[j].toDoubleArray(bulk);
+          for (int i = 0; i < rows; ++i) {
+            sum += bulk[i];
+          }
+        }
+      }
+      long done = System.currentTimeMillis();
+      Log.info("Sum: " + sum);
+      long siz = 0;
+      for (int j=0; j<cols; ++j) {
+        siz += chunks[j].byteSize();
+      }
+      Log.info("Data size: " + PrettyPrint.bytes(siz));
+      Log.info("Time to access via bulk reader: " + PrettyPrint.msecs(done - start, true));
+      Log.info("");
+    }
   }
 
 }
