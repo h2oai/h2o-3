0bac9015377bcf1620bd40dd03c6d47d5b4b3ecf
anqi <anqi@h2o.ai>
2015-09-15 18:39:57 -0700

    Add Pyunits for random missing data in GLRM

--- h2o-r/tests/testdir_algos/glrm/runit_glrm_missing_prostate.R
+++ h2o-r/tests/testdir_algos/glrm/runit_glrm_missing_prostate.R
@@ -23,7 +23,7 @@ test.glrm.prostate_miss <- function(conn) {
     print(summary(prostate.miss))
     
     Log.info(paste("H2O GLRM with ", 100 * f, "% missing entries:\n", sep = ""))
-    prostate.glrm <- h2o.glrm(training_frame = prostate.miss, validation_frame = prostate.full, ignore_const_cols = FALSE, k = 8, init = "SVD", 
+    prostate.glrm <- h2o.glrm(training_frame = prostate.miss, validation_frame = prostate.full, ignore_const_cols = FALSE, k = 8, init = "SVD", loss = "Quadratic",
                               max_iterations = 2000, gamma_x = 0.5, gamma_y = 0.5, regularization_x = "L1", regularization_y = "L1", min_step_size = 1e-6, seed = SEED)
     
     # Check imputed data and error metrics
