4d3270c0627c58da31e5c23649f2bdd2b7264685
anqi <anqi@h2o.ai>
2015-09-15 17:06:54 -0700

    Fix a file path error

--- h2o-r/tests/testdir_jira/runit_pubdev_1692.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_1692.R
@@ -1,5 +1,5 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
-source('../../h2o-runit.R')
+source('../h2o-runit.R')
 
 test.pubdev.1692 <- function(conn) {
   Log.info("Importing glrm_test/msq.csv data...")
