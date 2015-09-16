da384a8ace3da0bca8c2218b4636abbd522456ed
Arno Candel <arno.candel@gmail.com>
2015-09-16 13:38:22 -0700

    PUBDEV-2091: Disallow the response to be the same as fold/offset/weights.

--- h2o-core/src/main/java/hex/ModelBuilder.java
+++ h2o-core/src/main/java/hex/ModelBuilder.java
@@ -666,6 +666,12 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
         if (isSupervised())
           error("_response_column", "Response column '" + _parms._response_column + "' not found in the training frame");
       } else {
+        if(_response == _offset)
+          error("_response", "Response must be different from offset_column");
+        if(_response == _weights)
+          error("_response", "Response must be different from weights_column");
+        if(_response == _fold)
+          error("_response", "Response must be different from fold_column");
         _train.add(_parms._response_column, _response);
         ++res;
       }
