40caff83a572c236de2324db714fbb69f5116271
mmalohlava <michal.malohlava@gmail.com>
2015-09-20 11:13:41 -0700

    [PUBDEV-1850] Fix a cherry-picked test.
    The test function was missing connection object.

--- h2o-r/tests/testdir_jira/runit_pubdev_1850.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_1850.R
@@ -2,7 +2,7 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source('../h2o-runit.R')
 
 # Tests H2O's ability to read in numbers from the bit64 package
-test.pubdev_1850 <- function() {
+test.pubdev_1850 <- function(localH2O) {
 
 foo <- h2o.importFile(locate("smalldata/jira/hexdev_29.csv"), na.strings=list(NULL, c("fish","xyz"), NULL)) 
 expect_equal(sum(is.na(foo)), 2)
