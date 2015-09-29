ccb721098469998712c8a4ec5fa7acc5a6d62dbc
Arno Candel <arno.candel@gmail.com>
2015-09-28 22:14:08 -0700

    PUBDEV-1847: Fix super-picky golden JUnit tests for GBM after recent code refactoring.

--- h2o-algos/src/test/java/hex/tree/gbm/GBMTest.java
+++ h2o-algos/src/test/java/hex/tree/gbm/GBMTest.java
@@ -632,7 +632,7 @@ public class GBMTest extends TestUtil {
     }
     Scope.exit();
     for( double mse : mses )
-      assertEquals(0.21926955145068244, mse, 1e-8); //check for the same result on 1 nodes and 5 nodes (will only work with enough chunks)
+      assertEquals(0.21979375165014595, mse, 1e-8); //check for the same result on 1 nodes and 5 nodes (will only work with enough chunks)
   }
 
   @Test public void testReprodubilityAirlineSingleNode() {
@@ -691,7 +691,7 @@ public class GBMTest extends TestUtil {
     }
     Scope.exit();
     for( double mse : mses )
-      assertEquals(0.21926955145068244, mse, 1e-8); //check for the same result on 1 nodes and 5 nodes (will only work with enough chunks)
+      assertEquals(0.21979375165014595, mse, 1e-8); //check for the same result on 1 nodes and 5 nodes (will only work with enough chunks)
   }
 
   // HEXDEV-223
