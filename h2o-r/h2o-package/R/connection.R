5d64a68e6409b138b8ed66f577bdadecafefb380
eric eckstrand <eric.eckstrand@gmail.com>
2015-10-02 14:48:06 -0700

    R CMD check Jenkins job will set these environment variables, so that when h2o.init() is called within an example, we connect to the right h2o, and not some existing h2o on localhost:54321

--- h2o-r/h2o-package/R/connection.R
+++ h2o-r/h2o-package/R/connection.R
@@ -101,6 +101,11 @@ h2o.init <- function(ip = "127.0.0.1", port = 54321, startH2O = TRUE, forceDL = 
     strict_version_check = FALSE
   }
 
+  doc_ip <- Sys.getenv("H2O_R_CMD_CHECK_DOC_EXAMPLES_IP")
+  doc_ip <- Sys.getenv("H2O_R_CMD_CHECK_DOC_EXAMPLES_PORT")
+  if (nchar(doc_ip)) ip = doc_ip
+  if (nchar(doc_port)) port = doc_port
+
   warnNthreads <- FALSE
   tmpConn <- new("H2OConnection", ip = ip, port = port)
   if (!h2o.clusterIsUp(tmpConn)) {
