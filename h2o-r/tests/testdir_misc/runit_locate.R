a62b3f6b6737818abf62e3bdbbab08b0bf39d898
eric eckstrand <eric.eckstrand@gmail.com>
2015-10-05 19:12:37 -0700

    add another check to runit

--- h2o-r/tests/testdir_misc/runit_locate.R
+++ h2o-r/tests/testdir_misc/runit_locate.R
@@ -5,6 +5,10 @@ test <- function() {
 
     e <- tryCatch(locate("this_file_does_not_exist.csv"), error = function(x) x)
     expect_true("Could not find the dataset bucket: this_file_does_not_exist.csv" == e[[1]])
+
+    setwd("/")
+    e <- tryCatch(locate("this_file_does_not_exist.csv"), error = function(x) x)
+    expect_true("evaluation nested too deeply: infinite recursion / options(expressions=)?" == e[[1]])
 }
 
 doTest("test", test)
