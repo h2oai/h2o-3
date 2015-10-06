08341aafcb45e8c6e6a0b0a0aff74715cb1b05d9
mmalohlava <michal.malohlava@gmail.com>
2015-10-05 18:09:23 -0700

    [PUBDEV-2075] Another collision in parameters checksum

--- h2o-r/tests/testdir_jira/runit_pubdev_2075.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_2075.R
@@ -11,20 +11,18 @@ test.pubdev.2075 <- function(conn){
   predictors <- c("displacement","power","weight","acceleration","year")
   response_col <- "economy_20mpg"
   grid_space <- list()
-  grid_space$ntrees <- c(3, 5)
-  grid_space$max_depth <- 2:3
-  grid_space$nbins <- c(8, 7, 9)
-  grid_space$nbins_cats <- c(551, 478, 171)
-  grid_space$sample_rate <- c(0.346999, 0.645949, 0.497151)
+  grid_space$nbins <- c(8, 7)
+  grid_space$nbins_cats <- c(551, 478)
   train[,response_col] <- as.factor(train[,response_col])
   valid[,response_col] <- as.factor(valid[,response_col])
   cars_drf_grid <- h2o.grid("randomForest", grid_id="drf_grid_cars_test", x=predictors, y=response_col,
+                            ntrees=3, max_depth=2, sample_rate=0.346999,
                            training_frame=train, validation_frame=valid, hyper_params=grid_space)
 
-  Log.info("Expect 108 models to be constructed")
+  Log.info("Expect 4 models to be constructed")
   num_models <- length(cars_drf_grid@model_ids)
   Log.info(paste0(num_models," models were constructed"))
-  expect_equal(108, num_models)
+  expect_equal(4, num_models)
 
   
 }
