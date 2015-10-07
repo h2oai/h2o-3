7d4c8a818c641d0c6d720936837263169d33d940
Tomas Nykodym <tomas@h2o.ai>
2015-10-07 15:20:44 -0700

    Fixed Vec.preWriting to not to use anonymous inner task which cause the whole Vec header to be passed over the wire..

--- h2o-core/src/main/java/water/fvec/Vec.java
+++ h2o-core/src/main/java/water/fvec/Vec.java
@@ -663,6 +663,12 @@ public class Vec extends Keyed<Vec> {
   @Override protected long checksum_impl() { return rollupStats()._checksum;}
 
 
+  private static class SetMutating extends TAtomic<RollupStats> {
+    @Override protected RollupStats atomic(RollupStats rs) {
+      return rs != null && rs.isMutating() ? null : RollupStats.makeMutating();
+    }
+  }
+
   /** Begin writing into this Vec.  Immediately clears all the rollup stats
    *  ({@link #min}, {@link #max}, {@link #mean}, etc) since such values are
    *  not meaningful while the Vec is being actively modified.  Can be called
@@ -677,11 +683,7 @@ public class Vec extends Keyed<Vec> {
       if( rs.isMutating() ) return; // Vector already locked against rollups
     }
     // Set rollups to "vector isMutating" atomically.
-    new TAtomic<RollupStats>() {
-      @Override protected RollupStats atomic(RollupStats rs) {
-        return rs != null && rs.isMutating() ? null : RollupStats.makeMutating();
-      }
-    }.invoke(rskey);
+    new SetMutating().invoke(rskey);
   }
 
   /** Stop writing into this Vec.  Rollup stats will again (lazily) be
