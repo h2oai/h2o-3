5629ad001b9e130e6fe3bac158b699404410eda5
eric eckstrand <eric.eckstrand@gmail.com>
2015-10-06 18:03:15 -0700

    add runit for all solver/family combos. move glm random attack pyunit to pass.

---
+++ h2o-r/tests/testdir_algos/glm/runit_GLM_solvers.R
@@ -0,0 +1,34 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source('../../h2o-runit.R')
+
+test.GLM.solvers <- function() {
+#  Log.info("Importing prostate.csv data...\n")
+#  prostate.hex = h2o.uploadFile(locate("smalldata/logreg/prostate.csv"), "prostate.hex")
+#  prostate.sum = summary(prostate.hex)
+#  print(prostate.sum)
+#
+#  prostate.data = read.csv(locate("smalldata/logreg/prostate.csv"), header = TRUE)
+#  prostate.data = na.omit(prostate.data)
+
+  training_data <- h2o.importFile(locate("smalldata/junit/cars_20mpg.csv"))
+  predictors <- c("displacement","power","weight","acceleration","year")
+
+  for (solver in c("AUTO", "IRLSM", "L_BFGS", "COORDINATE_DESCENT_NAIVE", "COORDINATE_DESCENT")) {
+    print(paste0("Solver = ",solver))
+    for (family in c("binomial", "gaussian", "poisson", "tweedie", "gamma")) {
+        if        (family == 'binomial') { response_col <- "economy_20mpg"
+        } else if (family == 'gaussian') { response_col <- "economy"
+        } else                           { response_col <- "cylinders" }
+        print(paste0("Family = ",family))
+
+        if (family == 'binomial') { training_data[,response_col] <- as.factor(training_data[,response_col])
+        } else {                    training_data[,response_col] <- as.numeric(training_data[,response_col]) }
+
+        model <- h2o.glm(x=predictors, y=response_col, training_frame=training_data, family=family, alpha=0,
+                         lambda=1e-5, solver=solver)
+    }
+  }
+}
+
+doTest("GLM Solvers", test.GLM.solvers)
+
