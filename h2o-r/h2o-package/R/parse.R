c630e40f5ba577e912aaf44d3c7f7fb10f1693dd
eric eckstrand <eric.eckstrand@gmail.com>
2015-10-08 15:41:42 -0700

    add ability to set col.types with a named list to R interface. add respective runit tests

--- h2o-r/h2o-package/R/parse.R
+++ h2o-r/h2o-package/R/parse.R
@@ -62,7 +62,10 @@ h2o.parseRaw <- function(data, destination_frame = "", header=NA, sep = "", col.
 h2o.parseSetup <- function(data, destination_frame = "", header=NA, sep = "", col.names=NULL, col.types=NULL, na.strings=NULL, parse_type=NULL) {
 
   # quick sanity checking
-  chk.Frame(data)
+  if (class(data) == "Frame") data <- list(data)
+
+  for (d in data) chk.Frame(d)
+
   .key.validate(destination_frame)
   if(!(is.na(header) || is.logical(header))) stop("`header` cannot be of class ", class(header))
   if(!is.character(sep) || length(sep) != 1L || is.na(sep)) stop("`sep` must a character string")
@@ -72,34 +75,48 @@ h2o.parseSetup <- function(data, destination_frame = "", header=NA, sep = "", co
   parseSetup.params <- list()
 
   # Prep srcs: must be of the form [src1,src2,src3,...]
-  parseSetup.params$source_frames = .collapse.char(attr(data, "id"))
+  parseSetup.params$source_frames <- .collapse.char(sapply(data, function (d) attr(d, "id")))
+
+  parseSetup <- .h2o.__remoteSend(.h2o.__PARSE_SETUP, method = "POST", .params = parseSetup.params)
 
   # set field sep
-  # if( nchar(sep) > 0 ) parseSetup.params$separator <- .asc(sep)
-  if( nzchar(sep) ) parseSetup.params$separator <- .asc(sep)
+  if( nzchar(sep) ) parseSetup$separator <- .asc(sep)
 
   # check the header
-  if( is.na(header) && is.null(col.names) ) parseSetup.params$check_header <-  0
-  else if( !isTRUE(header) )                parseSetup.params$check_header <- -1
-  else                                      parseSetup.params$check_header <-  1
+  if( is.na(header) && is.null(col.names) ) parseSetup$check_header <-  0
+  else if( !isTRUE(header) )                parseSetup$check_header <- -1
+  else                                      parseSetup$check_header <-  1
 
   # set the column names
-  if( !is.null(col.names) ) {
-    parseSetup.params$column_names <- .collapse.char(if(is.Frame(col.names)) colnames(col.names) else col.names)
+  if(!is.null(col.names)) parseSetup$column_names <- if(is.Frame(col.names)) colnames(col.names) else col.names
+
+  # set col.types
+  if( !is.null(col.types) ) {
+    if (typeof(col.types) == "character") {
+        parseSetup$column_types <- col.types
+    } else if ((typeof(col.types) == "list")) {
+        nms <- names(col.types)
+        if (is.null(nms)) stop("col.types must be named list")
+        if (is.null(col.names)) stop("if col.types is a named list, then col.names must be specified")
+        if (length(setdiff(nms, col.names)) > 0) stop("names specified in col.types must be a subset of col.names")
+        if (length(col.names) != parseSetup$number_columns) stop("length of col.names must equal to the number of columns in dataset")
+        for (n in nms) {
+            i <- 1
+            for (cn in parseSetup$column_names) {
+                if (n == cn) break
+                i <- i + 1
+            }
+            parseSetup$column_types[[i]] <- col.types[[n]]
+        }
+    } else { stop("col.types must be a character vector or a named list") }
   }
 
-  # check the types
-  if( !is.null(col.types) )  parseSetup.params$column_types <- .collapse.char(col.types)
-
   # check the na.strings
-  if( !is.null(na.strings) ) parseSetup.params$na_strings <- .collapse.array(na.strings)
+  if( !is.null(na.strings) ) parseSetup$na_strings <- .collapse.array(na.strings)
 
   # check the parse_type
   # currently valid types are ARFF, XLS, CSV, SVMLight
-  if( !is.null(parse_type) ) parseSetup.params$parse_type <- parse_type
-
-  # pass through ParseSetup
-  parseSetup <- .h2o.__remoteSend(.h2o.__PARSE_SETUP, method = "POST", .params = parseSetup.params)
+  if( !is.null(parse_type) ) parseSetup$parse_type <- parse_type
 
   # make a name only if there was no destination_frame ( i.e. !nzchar("") == TRUE )
   if( !nzchar(destination_frame) ) destination_frame <- .key.make(parseSetup$destination_frame)
@@ -138,3 +155,4 @@ h2o.parseSetup <- function(data, destination_frame = "", header=NA, sep = "", co
 
 # ASCII lookup on sep
 .asc <- function(c) strtoi(charToRaw(c),16L)
+
