f02af8ae5dedfb9ece3b2e77cfccfee5c9de344c
eric eckstrand <eric.eckstrand@gmail.com>
2015-10-09 16:40:34 -0700

    use gzipped file instead

--- h2o-py/tests/testdir_javapredict/pyunit_NOPASS_javapredict_2x100000GLM.py
+++ h2o-py/tests/testdir_javapredict/pyunit_NOPASS_javapredict_2x100000GLM.py
@@ -9,7 +9,7 @@ def javapredict_2x100000():
     print "Parameter list:"
     for k,v in zip(params.keys(), params.values()): print "{0}, {1}".format(k,v)
 
-    train = h2o.import_file(tests.locate("smalldata/jira/2x100000_real.csv"))
+    train = h2o.import_file(tests.locate("smalldata/jira/2x100000_real.csv.gz"))
     test = train
     x = range(1,train.ncol)
     y = 0
