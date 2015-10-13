799621cbe7e3c5ec4675002a6d7a688e2aa5662e
eric eckstrand <eric.eckstrand@gmail.com>
2015-10-13 10:05:14 -0700

    add GBM, DRF, DL large pojo pyunits

---
+++ h2o-py/tests/testdir_javapredict/pyunit_javapredict_DRF_xlarge.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.insert(1, "../../")
+import h2o, tests
+
+def javapredict_drf_xlarge():
+
+    hdfs_name_node = tests.hadoop_namenode()
+    hdfs_file_name = "/datasets/z_repro.csv"
+    url = "hdfs://{0}{1}".format(hdfs_name_node, hdfs_file_name)
+
+    params = {'ntrees':20, 'max_depth':35, 'min_rows':1} # 739MB pojo
+    print "Parameter list:"
+    for k,v in zip(params.keys(), params.values()): print "{0}, {1}".format(k,v)
+
+    train =  h2o.import_file(url)
+    test = train[range(0,10),:]
+    x = range(1,train.ncol)
+    y = 0
+
+    tests.javapredict("random_forest", "numeric", train, test, x, y, **params)
+
+if __name__ == "__main__":
+    tests.run_test(sys.argv, javapredict_drf_xlarge)
