b7cee486ca366bfcbe5eb27a03c0640a040d3de5
Hank Roark <hank@h2oai.com>
2015-10-16 11:09:02 -0700

    python booklet work

--- h2o-docs/src/booklets/v2_2015/source/python/ipython_input.py
+++ h2o-docs/src/booklets/v2_2015/source/python/ipython_input.py
@@ -16,3 +16,23 @@ df = h2o.H2OFrame({'A': [1, 2, 3],
                    'B': ['a', 'b', 'c'],
                    'C': [0.1, 0.2, 0.3]})
 df
+
+# To create an H2OFrame object from a dict with specified column types:
+df2 = h2o.H2OFrame({'A': [1, 2, 3],
+                    'B': ['a', 'a', 'b'],
+                    'C': ['hello', 'all', 'world'],
+                    'D': ['12/1/2015 00:21:25', '12/2/2015 01:21:25', '12/3/2015 02:21:25']},
+                   column_types=['numeric', 'enum', 'string', 'time'])
+
+df2
+
+df2.types
+
+import numpy as np
+df = h2o.H2OFrame(np.random.randn(100,4).tolist(), columns=list('ABCD'))
+df.head()
+df.tail(5)
+
+df.columns
+
+df.describe()
