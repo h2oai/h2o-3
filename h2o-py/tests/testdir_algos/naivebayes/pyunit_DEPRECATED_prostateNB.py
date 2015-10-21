4d79e046ed560e9eaad080ccfb2cbb441a6f5548
ludi <ludi317@gmail.com>
2015-10-21 15:49:11 -0700

    update naive bayes tests

---
+++ h2o-py/tests/testdir_algos/naivebayes/pyunit_DEPRECATED_prostateNB.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.insert(1,"../../../")
+import h2o
+from tests import pyunit_utils
+
+
+
+
+def nb_prostate():
+    
+
+    print "Importing prostate.csv data..."
+    prostate = h2o.upload_file(pyunit_utils.locate("smalldata/logreg/prostate.csv"))
+
+    print "Converting CAPSULE, RACE, DCAPS, and DPROS to categorical"
+    prostate['CAPSULE'] = prostate['CAPSULE'].asfactor()
+    prostate['RACE'] = prostate['CAPSULE'].asfactor()
+    prostate['DCAPS'] = prostate['DCAPS'].asfactor()
+    prostate['DPROS'] = prostate['DPROS'].asfactor()
+
+    print "Compare with Naive Bayes when x = 3:9, y = 2"
+    prostate_nb = h2o.naive_bayes(x=prostate[2:9], y=prostate[1], laplace = 0)
+    prostate_nb.show()
+
+    print "Predict on training data"
+    prostate_pred = prostate_nb.predict(prostate)
+    prostate_pred.head()
+
+
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(nb_prostate)
+else:
+    nb_prostate()
--- h2o-py/tests/testdir_algos/naivebayes/pyunit_init_errNB.py
+++ h2o-py/tests/testdir_algos/naivebayes/pyunit_init_errNB.py
@@ -7,36 +7,37 @@ from tests import pyunit_utils
 
 
 def nb_init_err():
-    
 
-    print "Importing iris_wheader.csv data...\n"
-    iris = h2o.upload_file(pyunit_utils.locate("smalldata/iris/iris_wheader.csv"))
-    iris.describe
 
-    print "Laplace smoothing parameter is negative"
-    try:
-        h2o.naive_bayes(x=iris[0:4], y=iris[4], laplace=-1)
-        assert False, "Expected naive bayes algo to fail on negative laplace training parameter"
-    except:
-        pass
+  print "Importing iris_wheader.csv data...\n"
+  iris = h2o.upload_file(pyunit_utils.locate("smalldata/iris/iris_wheader.csv"))
+  iris.describe
 
-    print "Minimum standard deviation is zero"
-    try:
-        h2o.naive_bayes(x=iris[0:4], y=iris[4], min_sdev=0)
-        assert False, "Expected naive bayes algo to fail on min_sdev = 0"
-    except:
-        pass
+  print "Laplace smoothing parameter is negative"
+  from h2o.estimators.naive_bayes import H2ONaiveBayesEstimator
+  try:
+    H2ONaiveBayesEstimator(laplace=-1).train(x=range(4), y=4, training_frame=iris)
+    assert False, "Expected naive bayes algo to fail on negative laplace training parameter"
+  except:
+    pass
 
-    print "Response column is not categorical"
-    try:
-        h2o.naive_bayes(x=iris[0:3], y=iris[3], min_sdev=0)
-        assert False, "Expected naive bayes algo to fail on response not categorical"
-    except:
-        pass
+  print "Minimum standard deviation is zero"
+  try:
+    H2ONaiveBayesEstimator().train(x=range(4), y=4, training_frame=iris, min_sdev=0)
+    assert False, "Expected naive bayes algo to fail on min_sdev = 0"
+  except:
+    pass
+
+  print "Response column is not categorical"
+  try:
+    H2ONaiveBayesEstimator().train(x=range(3), y=3, min_sdev=0)
+    assert False, "Expected naive bayes algo to fail on response not categorical"
+  except:
+    pass
 
 
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(nb_init_err)
+  pyunit_utils.standalone_test(nb_init_err)
 else:
-    nb_init_err()
+  nb_init_err()
