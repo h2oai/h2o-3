89a2525f5a8720891d9be0f312572f07eb9714db
SpencerA <spnrpa@gmail.com>
2015-10-22 13:39:31 -0700

    loading_key -> representation_name

--- h2o-py/tests/testdir_algos/glrm/pyunit_nnmf_glrm.py
+++ h2o-py/tests/testdir_algos/glrm/pyunit_nnmf_glrm.py
@@ -29,21 +29,21 @@ def glrm_nnmf():
   fit_y = glrm_h2o._model_json['output']['archetypes'].cell_values
   fit_y_np = [[float(s) for s in list(row)[1:]] for row in fit_y]
   fit_y_np = np.array(fit_y_np)
-  # fit_x = h2o.get_frame(glrm_h2o._model_json['output']['loading_key']['name'])
-  # fit_x_np = np.array(h2o.as_list(fit_x))
+  fit_x = h2o.get_frame(glrm_h2o._model_json['output']['representation_name'])
+  fit_x_np = np.array(h2o.as_list(fit_x))
   assert np.all(fit_y_np >= 0), "Y must contain only non-negative elements"
-  # assert np.all(fit_x_np >= 0), "X must contain only non-negative elements"
+  assert np.all(fit_x_np >= 0), "X must contain only non-negative elements"
 
   print "Check final objective function value"
-  # fit_xy = np.dot(fit_x_np, fit_y_np)
+  fit_xy = np.dot(fit_x_np, fit_y_np)
   glrm_obj = glrm_h2o._model_json['output']['objective']
-  # sse = np.sum(np.square(train.__sub__(fit_xy)))
-  # assert abs(glrm_obj - sse) < 1e-6, "Final objective was " + str(glrm_obj) + " but should equal " + str(sse)
+  sse = np.sum(np.square(train.__sub__(fit_xy)))
+  assert abs(glrm_obj - sse) < 1e-6, "Final objective was " + str(glrm_obj) + " but should equal " + str(sse)
 
   print "Impute XY and check error metrics"
   pred_h2o = glrm_h2o.predict(train_h2o)
   pred_np = np.array(h2o.as_list(pred_h2o))
-  # assert np.allclose(pred_np, fit_xy), "Imputation for numerics with quadratic loss should equal XY product"
+  assert np.allclose(pred_np, fit_xy), "Imputation for numerics with quadratic loss should equal XY product"
   glrm_numerr = glrm_h2o._model_json['output']['training_metrics']._metric_json['numerr']
   glrm_caterr = glrm_h2o._model_json['output']['training_metrics']._metric_json['caterr']
   assert abs(glrm_numerr - glrm_obj) < 1e-3, "Numeric error was " + str(glrm_numerr) + " but should equal final objective " + str(glrm_obj)
