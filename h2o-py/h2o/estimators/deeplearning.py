557d650aa13937eb12dc11aa5a45c28fc7cb235a
SpencerA <spnrpa@gmail.com>
2015-10-25 13:21:20 -0700

    merge cliffc_jenkins into spencer_jenkins

--- h2o-py/h2o/estimators/deeplearning.py
+++ h2o-py/h2o/estimators/deeplearning.py
@@ -674,7 +674,7 @@ class H2OAutoEncoderEstimator(H2ODeepLearningEstimator):
     >>> import h2o as ml
     >>> from h2o.estimators.deeplearning import H2OAutoEncoderEstimator
     >>> ml.init()
-    >>> rows=[[1,2,3,4,0],[2,1,2,4,1],[2,1,4,2,1],[0,1,2,34,1],[2,3,4,1,0]]*50
+    >>> rows=[[1,2,3,4,0]*50,[2,1,2,4,1]*50,[2,1,4,2,1]*50,[0,1,2,34,1]*50,[2,3,4,1,0]*50]
     >>> fr = ml.H2OFrame(rows)
     >>> fr[4] = fr[4].asfactor()
     >>> model = H2OAutoEncoderEstimator()
