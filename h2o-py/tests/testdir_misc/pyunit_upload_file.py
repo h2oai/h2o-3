09779bb751c032c007f31b2408bfe7196a30c46f
ludi <ludi@h2o.ai>
2015-10-28 12:59:39 -0700

    correct .fromPython.fromPython

--- h2o-py/tests/testdir_misc/pyunit_upload_file.py
+++ h2o-py/tests/testdir_misc/pyunit_upload_file.py
@@ -17,32 +17,32 @@ def upload_file():
 
 
     # using lists []
-    py_list_to_h2o = H2OFrame.fromPython.fromPython(zip(*[[0, 1, 2, 3, 4]]))
+    py_list_to_h2o = H2OFrame.fromPython(zip(*[[0, 1, 2, 3, 4]]))
 
     print py_list_to_h2o.describe()
 
-    py_list_to_h2o_2 = H2OFrame.fromPython.fromPython(zip(*[[0, 1, 2, 3], [5, 6, "hi", "dog"]]))
+    py_list_to_h2o_2 = H2OFrame.fromPython(zip(*[[0, 1, 2, 3], [5, 6, "hi", "dog"]]))
 
     print py_list_to_h2o_2.describe()
 
 
     # using tuples ()
-    py_tuple_to_h2o = H2OFrame.fromPython.fromPython(zip(*[(0, 1, 2, 3, 4)]))
+    py_tuple_to_h2o = H2OFrame.fromPython(zip(*[(0, 1, 2, 3, 4)]))
 
     print py_tuple_to_h2o.describe()
 
-    py_tuple_to_h2o_2 = H2OFrame.fromPython.fromPython(zip(*((0, 1, 2, 3), (5, 6, "hi", "dog"))))
+    py_tuple_to_h2o_2 = H2OFrame.fromPython(zip(*((0, 1, 2, 3), (5, 6, "hi", "dog"))))
 
     print py_tuple_to_h2o_2.describe()
 
 
     # using dicts {}
-    py_dict_to_h2o = H2OFrame.fromPython.fromPython({"column1": [5, 4, 3, 2, 1],
+    py_dict_to_h2o = H2OFrame.fromPython({"column1": [5, 4, 3, 2, 1],
                                           "column2": (1, 2, 3, 4, 5)})
 
     py_dict_to_h2o.describe()
 
-    py_dict_to_h2o_2 = H2OFrame.fromPython.fromPython({"colA": ["bilbo", "baggins"], "colB": ["meow"]})
+    py_dict_to_h2o_2 = H2OFrame.fromPython({"colA": ["bilbo", "baggins"], "colB": ["meow"]})
 
     print py_dict_to_h2o_2.describe()
 
