61345f00fd44bdd4a2d6c544cf3ec5efa39faee9
Tomas Nykodym <tomas@h2o.ai>
2015-10-29 12:16:11 -0700

    GLM multinomial fix. Do not do line search first go around for a covariate in coordinate descent

--- h2o-algos/src/main/java/hex/glm/GLM.java
+++ h2o-algos/src/main/java/hex/glm/GLM.java
@@ -1722,7 +1722,7 @@ public class GLM extends ModelBuilder<GLMModel,GLMParameters,GLMOutput> {
           if(_c == 0)_oldObj = _taskInfo._objVal;
           _taskInfo._beta = _taskInfo._beta_multinomial[_c];
           GLMSingleLambdaTsk.this.addToPendingCount(1);
-          solve(true);
+          solve(false);
         }
       }).asyncExec(_dinfo._adaptedFrame);
     }
