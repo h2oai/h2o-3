614eb3ae62b09aa1c92474a9dfd6b36ef8e9ad0a
Navdeep Gill <mr.navdeepgill@gmail.com>
2015-10-29 12:57:31 -0700

    Added more exception tests to runit_separator_test.R

--- h2o-r/tests/testdir_parser/runit_separator_test.R
+++ h2o-r/tests/testdir_parser/runit_separator_test.R
@@ -10,29 +10,55 @@ test.separator <- function(){
   path.caret = "smalldata/parser/caret.csv"
   path.asterisk = "smalldata/parser/asterisk.asv"
   
-  #Tab delimited
+  #Tab delimited with argument
   tab.test = h2o.importFile(path = normalizePath(locate(path.tab)), destination_frame = "tab.hex",sep = "\t")
   expect_that(dim(tab.test), equals(c(3,3)))
-  
-  #Pipe delimited
+  #Tab delimited with no argument
+  tab.test.noarg = h2o.importFile(path = normalizePath(locate(path.tab)), destination_frame = "tab.hex",sep = "")
+  expect_that(dim(tab.test.noarg), equals(c(3,3)))
+  #--------------------------------------------------------------------------------------------------------------------
+  #Pipe delimited with argument
   pipe.test = h2o.importFile(path = normalizePath(locate(path.pipe)), destination_frame = "pipe.hex",sep = "|")
   expect_that(dim(pipe.test), equals(c(3,3)))
-  
-  #Hive delimited
+  #Pipe delimited with no argument
+  pipe.test.noarg = h2o.importFile(path = normalizePath(locate(path.pipe)), destination_frame = "pipe.hex",sep = "")
+  expect_that(dim(pipe.test.noarg), equals(c(3,3)))
+  #--------------------------------------------------------------------------------------------------------------------
+  #Hive delimited with argument
   hive.test = h2o.importFile(path = normalizePath(locate(path.hive)), destination_frame = "hive.hex",sep = "\001")
   expect_that(dim(hive.test), equals(c(3,3)))
-  
-  #Semi colon delimited
+  #Hive delimited wih no argument
+  hive.test.noarg = h2o.importFile(path = normalizePath(locate(path.hive)), destination_frame = "hive.hex",sep = "")
+  expect_that(dim(hive.test.noarg), equals(c(3,3)))
+  #--------------------------------------------------------------------------------------------------------------------
+  #Semi colon delimited with argument
   semi.test = h2o.importFile(path = normalizePath(locate(path.semi)), destination_frame = "semi.hex",sep = ";")
   expect_that(dim(semi.test), equals(c(3,3)))
-  
-  #Caret delimited
+  #Semi colon delimited with no argument
+  semi.test.noarg = h2o.importFile(path = normalizePath(locate(path.semi)), destination_frame = "semi.hex",sep = "")
+  expect_that(dim(semi.test.noarg), equals(c(3,3)))
+  #--------------------------------------------------------------------------------------------------------------------
+  #Caret delimited with argument
   caret.test = h2o.importFile(path = normalizePath(locate(path.caret)), destination_frame = "caret.hex",sep = "^")
   expect_that(dim(caret.test), equals(c(3,3)))
   
-  #Asterisk delimited
+  #Below gives error and does not parse correctly:
+  #Post to JIRA
+  #Caret delimited with no argument
+  #caret.test.noarg = h2o.importFile(path = normalizePath(locate(path.caret)), destination_frame = "caret.hex",sep = "")
+  #expect_that(dim(caret.test.noarg), equals(c(3,3)))
+  #print(caret.test.noarg)
+  #--------------------------------------------------------------------------------------------------------------------
+  #Asterisk delimited with argument
   asterisk.test = h2o.importFile(path = normalizePath(locate(path.asterisk)), destination_frame = "asterisk.hex",sep = "*")
   expect_that(dim(asterisk.test), equals(c(3,3)))
+  
+  #Below gives error and does not parse correctly:
+  #Post to JIRA
+  #Asterisk delimited with no argument
+  #asterisk.test.noarg = h2o.importFile(path = normalizePath(locate(path.asterisk)), destination_frame = "asterisk.hex",sep = "")
+  #expect_that(dim(asterisk.test.noarg), equals(c(3,3)))
+  #print(asterisk.test.noarg)
 }
 
 doTest("Separator Test", test.separator)
