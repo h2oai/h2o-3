858e4b950816324ba4afe466d458e1b1909b679a
kyanga2 <kevinyang222@gmail.com>
2015-10-30 15:41:12 -0700

    generating dummies for python ports

---
+++ h2o-docs/src/booklets/v2_2015/source/GLM_Vignette_code_examples/glm_model_output.py
@@ -0,0 +1,16 @@
+library(h2o)
+h2o.init()
+path = system.file("extdata", "prostate.csv", package = "h2o")
+h2o_df = h2o.importFile(path)
+h2o_df$CAPSULE = as.factor(h2o_df$CAPSULE)
+rand_vec <- h2o.runif(h2o_df, seed = 1234)
+train <- h2o_df[rand_vec <= 0.8,]
+valid <- h2o_df[rand_vec > 0.8,]
+binomial.fit = h2o.glm(y = "CAPSULE", x = c("AGE", "RACE", "PSA", "GLEASON"), training_frame = train, validation_frame = valid, family = "binomial")
+print(binomial.fit)
+
+binomial.fit@model$coefficients
+
+binomial.fit@model$coefficients_table
+
+binomial.fit@model$standardized_coefficient_magnitudes
---
+++ h2o-docs/src/booklets/v2_2015/source/GLM_Vignette_code_examples/glm_recalculate_predict.py
@@ -0,0 +1,3 @@
+pred = binomial_fit.predict(h2o_df)
+
+#redefine our threshold for predictions to be 0.3 rather than 0.5
