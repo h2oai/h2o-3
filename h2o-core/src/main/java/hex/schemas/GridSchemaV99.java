068a910c6a8621040ad183ad5075f00d90372c18
mmalohlava <michal.malohlava@gmail.com>
2015-10-31 10:54:50 -0700

    Grid REST API does not return model keys which were already deleted

--- h2o-core/src/main/java/hex/schemas/GridSchemaV99.java
+++ h2o-core/src/main/java/hex/schemas/GridSchemaV99.java
@@ -1,7 +1,11 @@
 package hex.schemas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import hex.Model;
 import hex.grid.Grid;
+import water.DKV;
 import water.Key;
 import water.api.API;
 import water.api.KeyV3;
@@ -49,10 +53,21 @@ public class GridSchemaV99 extends Schema<Grid, GridSchemaV99> {
 
   @Override
   public GridSchemaV99 fillFromImpl(Grid grid) {
-    Key<Model>[] models = grid.getModelKeys();
-    KeyV3.ModelKeyV3[] modelIds = new KeyV3.ModelKeyV3[models.length];
+    Key<Model>[] gridModelKeys = grid.getModelKeys();
+    // Return only keys which are referencing to existing objects in DKV
+    // However, here is still implicit race, since we are sending
+    // keys to client, but referenced models can be deleted in meantime
+    // Hence, client has to be responsible for handling this situation
+    // - call getModel and check for null model
+    List<Key> modelKeys = new ArrayList<>(gridModelKeys.length); // pre-allocate
+    for (Key k : gridModelKeys) {
+      if (k != null && DKV.get(k) != null) {
+        modelKeys.add(k);
+      }
+    }
+    KeyV3.ModelKeyV3[] modelIds = new KeyV3.ModelKeyV3[modelKeys.size()];
     for (int i = 0; i < modelIds.length; i++) {
-      modelIds[i] = new KeyV3.ModelKeyV3(models[i]);
+      modelIds[i] = new KeyV3.ModelKeyV3(modelKeys.get(i));
     }
     grid_id = new KeyV3.GridKeyV3(grid._key);
     model_ids = modelIds;
@@ -70,8 +85,8 @@ public class GridSchemaV99 extends Schema<Grid, GridSchemaV99> {
     for (int i = 0; i < modelParameters.length; i++) {
       if (modelParameters[i] != null) {
         result[i] =
-                (ModelParametersSchema) Schema.schema(Schema.getLatestVersion(), modelParameters[i])
-                        .fillFromImpl(modelParameters[i]);
+            (ModelParametersSchema) Schema.schema(Schema.getLatestVersion(), modelParameters[i])
+                .fillFromImpl(modelParameters[i]);
       } else {
         result[i] = null;
       }
