67a7283a10cf16f8dcb0055de4584c3b1ba38faa
ludi <ludi@h2o.ai>
2015-11-05 12:15:38 -0800

    Fix erratic sd behavior (nan/0/small num) for constant columns in groupby

--- h2o-core/src/main/java/water/rapids/ASTGroup.java
+++ h2o-core/src/main/java/water/rapids/ASTGroup.java
@@ -53,13 +53,21 @@ class ASTGroup extends ASTPrim {
     var() {
       @Override void op( double[] d0s, double d1 ) { d0s[0]+=d1*d1; d0s[1]+=d1; }
       @Override void atomic_op( double[] d0s, double[] d1s ) { ArrayUtils.add(d0s,d1s); }
-      @Override double postPass( double ds[], long n) { return (ds[0] - ds[1]*ds[1]/n)/(n-1); }
+      @Override double postPass( double ds[], long n) { 
+        double numerator = ds[0] - ds[1]*ds[1]/n;
+        if (Math.abs(numerator) < 1e-5) numerator = 0;
+        return numerator/(n-1); 
+      }
       @Override double[] initVal(int ignored) { return new double[2]; /* 0 -> sum_squares; 1 -> sum*/}
     },
     sdev() {
       @Override void op( double[] d0s, double d1 ) { d0s[0]+=d1*d1; d0s[1]+=d1; }
       @Override void atomic_op( double[] d0s, double[] d1s ) { ArrayUtils.add(d0s,d1s); }
-      @Override double postPass( double ds[], long n) { return Math.sqrt((ds[0] - ds[1]*ds[1]/n)/(n-1)); }
+      @Override double postPass( double ds[], long n) { 
+        double numerator = ds[0] - ds[1]*ds[1]/n;
+        if (Math.abs(numerator) < 1e-5) numerator = 0;
+        return Math.sqrt(numerator/(n-1)); 
+      }
       @Override double[] initVal(int ignored) { return new double[2]; /* 0 -> sum_squares; 1 -> sum*/}
     },
     min() { 
