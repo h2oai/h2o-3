602e5f5435ca312864c7635c4fac917a5dcfeb84
SpencerA <spnrpa@gmail.com>
2015-11-05 20:40:37 -0800

    varimp() no longer returns None, fix test

--- h2o-py/tests/testdir_misc/pyunit_varimp.py
+++ h2o-py/tests/testdir_misc/pyunit_varimp.py
@@ -14,9 +14,6 @@ def varimp_test():
     # Run GBM
     my_gbm = h2o.gbm(y=train["class"], x=train[1:4], ntrees=50, learn_rate=0.1, distribution="multinomial")
 
-    should_be_none = my_gbm.varimp()
-    assert should_be_none is None, "expected varimp to return None, but returned {0}".format(should_be_none)
-
     should_be_list = my_gbm.varimp()
     assert len(should_be_list) == 3, "expected varimp list to contain 3 entries, but it has " \
                                      "{0}".format(len(should_be_list))
