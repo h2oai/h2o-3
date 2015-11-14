4267621f18a5e2396561fac8843cd3d2538d8c5d
Cliff Click <cliffc@acm.org>
2015-11-13 18:30:06 -0800

    Raise priority of ChunkSummary and RollupStats
    Match GUI_PRIORITY if launched from a non-FJ thread.
    Fix bug in AAA_PreCloudLock that the URLs presented now always only ever
    returned an error, and did not exercise the expected functionality
    Add test to watch gui priorities vs some FJ

--- h2o-core/src/main/java/water/H2O.java
+++ h2o-core/src/main/java/water/H2O.java
@@ -995,9 +995,16 @@ final public class H2O {
   static int getWrkQueueSize  (int i) { return FJPS[i]==null ? -1 : FJPS[i].getQueuedSubmissionCount();}
   static int getWrkThrPoolSize(int i) { return FJPS[i]==null ? -1 : FJPS[i].getPoolSize();             }
 
+  // For testing purposes (verifying API work exceeds grunt model-build work)
+  // capture the class of any submitted job lower than this priority;
+  static public int LOW_PRIORITY_API_WORK;
+  static public String LOW_PRIORITY_API_WORK_CLASS;
+
   // Submit to the correct priority queue
   public static <T extends H2OCountedCompleter> T submitTask( T task ) {
     int priority = task.priority();
+    if( priority < LOW_PRIORITY_API_WORK )
+      LOW_PRIORITY_API_WORK_CLASS = task.getClass().toString();
     assert MIN_PRIORITY <= priority && priority <= MAX_PRIORITY:"priority " + priority + " is out of range, expected range is < " + MIN_PRIORITY + "," + MAX_PRIORITY + ">";
     if( FJPS[priority]==null )
       synchronized( H2O.class ) { if( FJPS[priority] == null ) FJPS[priority] = new PrioritizedForkJoinPool(priority,-1); }
