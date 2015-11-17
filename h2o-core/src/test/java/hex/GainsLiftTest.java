a81e827c9dc18a84aa4d844bb49a90b17945e151
Arno Candel <arno.candel@gmail.com>
2015-11-16 21:32:52 -0800

    PUBDEV-2118: Add more JUnits for Gains/Lift.

--- h2o-core/src/test/java/hex/GainsLiftTest.java
+++ h2o-core/src/test/java/hex/GainsLiftTest.java
@@ -1,5 +1,6 @@
 package hex;
 
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import water.TestUtil;
@@ -11,14 +12,13 @@ import java.util.Random;
 public class GainsLiftTest extends TestUtil {
   @BeforeClass public static void stall() { stall_till_cloudsize(1); }
 
-  @Test public void run() {
+  @Test public void good() {
     int len = 100000;
     double[] p = new double[len];
     long[] a = new long[len];
-    Random rng = new Random();
+    Random rng = new Random(0xDECAF);
     for (int i=0; i<len; ++i) {
       a[i] = rng.nextDouble() > 0.8 ? 1 : 0;
-//      p[i] = rng.nextDouble();
       p[i] = a[i] == 0 ? 0.5*rng.nextDouble() : 0.5 + rng.nextDouble() * 0.5;
     }
     Vec actual = Vec.makeVec(a, new String[]{"N","Y"}, Vec.newKey());
@@ -27,9 +27,58 @@ public class GainsLiftTest extends TestUtil {
     GainsLift gl = new GainsLift(predict, actual);
     gl.exec();
     Log.info(gl);
+    for (int i=0;i<2;++i)
+      Assert.assertTrue(gl.response_rates[i] > 0.9);
+    for (int i=2;i<gl.response_rates.length;++i)
+      Assert.assertTrue(gl.response_rates[i] < 0.1);
 
     actual.remove();
     predict.remove();
   }
 
+  @Test public void bad() {
+    int len = 100000;
+    double[] p = new double[len];
+    long[] a = new long[len];
+    Random rng = new Random(0xDECAF);
+    for (int i=0; i<len; ++i) {
+      a[i] = rng.nextDouble() > 0.8 ? 1 : 0;
+      p[i] = a[i] == 0 ? 0.5 + 0.5*rng.nextDouble() : 0.5*rng.nextDouble();
+    }
+    Vec actual = Vec.makeVec(a, new String[]{"N","Y"}, Vec.newKey());
+    Vec predict = Vec.makeVec(p, Vec.newKey());
+
+    GainsLift gl = new GainsLift(predict, actual);
+    gl.exec();
+    Log.info(gl);
+    for (int i=gl.response_rates.length-2;i<gl.response_rates.length;++i)
+      Assert.assertTrue(gl.response_rates[i] > 0.9);
+    for (int i=0;i<gl.response_rates.length-2;++i)
+      Assert.assertTrue(gl.response_rates[i] < 0.1);
+
+    actual.remove();
+    predict.remove();
+  }
+
+  @Test public void random() {
+    int len = 100000;
+    double[] p = new double[len];
+    long[] a = new long[len];
+    Random rng = new Random(0xDECAF);
+    for (int i=0; i<len; ++i) {
+      a[i] = rng.nextDouble() > 0.8 ? 1 : 0;
+      p[i] = rng.nextDouble();
+    }
+    Vec actual = Vec.makeVec(a, new String[]{"N","Y"}, Vec.newKey());
+    Vec predict = Vec.makeVec(p, Vec.newKey());
+
+    GainsLift gl = new GainsLift(predict, actual);
+    gl.exec();
+    Log.info(gl);
+    for (int i=0;i<gl.response_rates.length;++i)
+      Assert.assertTrue(gl.response_rates[i] > 0.19 && gl.response_rates[i] < 0.21);
+
+    actual.remove();
+    predict.remove();
+  }
 }
