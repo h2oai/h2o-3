3ebd7589a739a1aa016f7b051d065518992560b6
Mark Chan <mkchan@gmail.com>
2015-12-09 15:10:13 -0800

    split test train

--- h2o-py/test_hadoop/pyunit_hadoop.py
+++ h2o-py/test_hadoop/pyunit_hadoop.py
@@ -12,9 +12,9 @@ def test_hadoop():
     '''
     hdfs_name_node = os.getenv("NAME_NODE")
     h2o_data = h2o.import_file("hdfs://" + hdfs_name_node + "/datasets/100k.csv")
-
+    train,test = h2o_data.split_frame(ratios=[0.1])
     h2o_glm = H2OGeneralizedLinearEstimator(family="binomial", alpha=0.5, Lambda=0.01)
-    h2o_glm.train(x=range(1, 10), y=0, training_frame=h2o_data) # dont need to train on all features
+    h2o_glm.train(x=range(1, 10), y=0, training_frame=train) # dont need to train on all features
 
     hdfs_model_path = os.getenv("MODEL_PATH")
     h2o.save_model(h2o_glm, "hdfs://" + hdfs_model_path)
