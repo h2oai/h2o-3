b212ff5dbe099183e763e4d92cb30124df4c80bc
Mark Chan <mkchan@gmail.com>
2015-12-09 11:09:39 -0800

    dont train on all features

--- h2o-py/test_hadoop/pyunit_hadoop.py
+++ h2o-py/test_hadoop/pyunit_hadoop.py
@@ -16,7 +16,7 @@ def test_hadoop():
     h2o_data.summary()
 
     h2o_glm = H2OGeneralizedLinearEstimator(family="binomial", alpha=0.5, Lambda=0.01)
-    h2o_glm.train(x=range(1, h2o_data.ncol), y=0, training_frame=h2o_data)
+    h2o_glm.train(x=range(1, 10), y=0, training_frame=h2o_data) # dont need to train on all features
 
     hdfs_model_path = os.getenv("MODEL_PATH")
     h2o.save_model(h2o_glm, "hdfs://" + hdfs_model_path)
