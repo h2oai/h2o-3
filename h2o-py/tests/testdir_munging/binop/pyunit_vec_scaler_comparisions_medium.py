4340e35ea039e017ad551c67b6a10b4048e728f2
SpencerA <spnrpa@gmail.com>
2015-12-08 18:48:39 -0800

    print something on assert error

--- h2o-py/tests/testdir_munging/binop/pyunit_vec_scaler_comparisions_medium.py
+++ h2o-py/tests/testdir_munging/binop/pyunit_vec_scaler_comparisions_medium.py
@@ -48,9 +48,9 @@ def vec_scaler_comparisons():
     # ==
     row_sum = 0
     for level in levels[0]:
-       r, c = air[str(level) == air["Origin"]].dim
+       r, c = air[level == air["Origin"]].dim
        row_sum += r
-    assert row_sum == rows, "expected equal number of rows"
+    assert row_sum == rows, "expected equal number of rows: " + str(row_sum) + "!=" + str(rows)
 
     # ==, !=
     jan = air[1 == air["Month"]]
