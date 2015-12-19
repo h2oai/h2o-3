b5d61b867c9d01c1902ab91df778967e98790c8a
Arno Candel <arno.candel@gmail.com>
2015-12-18 19:53:53 -0800

    Make fold_column internally categorical to avoid having it to be continuous.

--- h2o-core/src/main/java/hex/ModelBuilder.java
+++ h2o-core/src/main/java/hex/ModelBuilder.java
@@ -11,10 +11,7 @@ import water.fvec.Chunk;
 import water.fvec.RebalanceDataSet;
 import water.fvec.Frame;
 import water.fvec.Vec;
-import water.util.FrameUtils;
-import water.util.Log;
-import water.util.MRUtils;
-import water.util.ReflectionUtils;
+import water.util.*;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.ParameterizedType;
@@ -246,8 +243,13 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
   // Work for each requested fold
   private int nFoldWork() {
     if( _parms._fold_column == null ) return _parms._nfolds;
-    Vec fc = train().vec(_parms._fold_column);
-    return ((int)fc.max()-(int)fc.min()) + 1;
+    Vec f = train().vec(_parms._fold_column);
+    if(!f.isInt() && !f.isCategorical())
+      error("_fold_column","Invalid fold column '" + _parms._fold_column  + "', fold must be integer or categorical");
+    Vec fc = VecUtils.toCategoricalVec(f);
+    int N = fc.domain().length;
+    if (fc != f) fc.remove();
+    return N;
   }
   // Temp zero'd vector, same size as train()
   private Vec zTmp() { return train().anyVec().makeZero(); }
@@ -292,8 +294,9 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
     // TODO: Implement better splitting algo (with Strata if response is categorical), e.g. http://www.lexjansen.com/scsug/2009/Liang_Xie2.pdf
     final Integer N = nFoldWork();
     final Vec foldAssignment;
+    final Vec foldCol = origTrainFrame.vec(_parms._fold_column);
     if (_parms._fold_column != null) {
-      foldAssignment = origTrainFrame.vec(_parms._fold_column);
+      foldAssignment = VecUtils.toCategoricalVec(foldCol);
     } else {
       final long seed = _parms.nFoldSeed();
       Log.info("Creating " + N + " cross-validation splits with random number seed: " + seed);
@@ -364,7 +367,7 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
     }
 
     // clean up memory (mostly small helper vectors and Frame headers)
-    if (_parms._fold_column == null) foldAssignment.remove();
+    if (foldAssignment != foldCol || _parms._fold_column == null) foldAssignment.remove();
     if (origWeightsName == null) origWeight.remove();
 
     // adapt main Job's progress bar to build N+1 models
@@ -657,8 +660,8 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
       if(f == null)
         error("_fold_column","Fold column '" + _parms._fold_column  + "' not found in the training frame");
       else {
-        if(!f.isInt())
-          error("_fold_column","Invalid fold column '" + _parms._fold_column  + "', fold must be integer");
+        if(!f.isInt() && !f.isCategorical())
+          error("_fold_column","Invalid fold column '" + _parms._fold_column  + "', fold must be integer or categorical");
         if(f.min() < 0)
           error("_fold_column","Invalid fold column '" + _parms._fold_column  + "', fold must be non-negative");
         if(f.isConst())
