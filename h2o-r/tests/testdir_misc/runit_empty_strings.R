345ce7fbe20dcb6fcf842a8f9f7aaedb8bd38d06
ludi <ludi@h2o.ai>
2015-12-22 14:00:03 -0800

    Ensure Py and R handle empty strings the same way

---
+++ h2o-r/tests/testdir_misc/runit_empty_strings.R
@@ -0,0 +1,16 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../scripts/h2o-r-test-setup.R")
+
+
+#analogous to pyunit_empty_strings.py
+test.empty.strings <- function() {
+  d <- as.data.frame(list(e=rep("",2),c=rep("",2), f=rep("",2)))
+  h <- as.h2o(d)
+  
+  expect_equal(sum(is.na(h)),0)
+  expect_equal(sum(h==""),nrow(h) * ncol(h))
+  
+  
+}
+
+doTest("Testing Empty Strings", test.empty.strings)
