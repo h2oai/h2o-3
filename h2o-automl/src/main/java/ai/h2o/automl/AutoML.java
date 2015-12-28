392533ab1701c6cdfc06bef72ee48f75dd95623a
SpencerA <spnrpa@gmail.com>
2015-12-28 11:36:34 -0800

    add some tests reorganize some

--- h2o-automl/src/main/java/ai/h2o/automl/AutoML.java
+++ h2o-automl/src/main/java/ai/h2o/automl/AutoML.java
@@ -3,7 +3,10 @@ package ai.h2o.automl;
 import ai.h2o.automl.strategies.initial.InitModel;
 import hex.Model;
 import hex.ModelBuilder;
+import water.*;
 import water.fvec.Frame;
+
+import java.util.Arrays;
 
 /**
  * Initial draft of AutoML
@@ -15,7 +18,7 @@ import water.fvec.Frame;
 public final class AutoML {
   private final Frame _fr;               // all learning on this frame
   private final int _response;           // response column, -1 for no response column
-  private final String _loss;            // overarching loss to minimize (not within algo loss, but without)
+  private final String _loss;            // overarching loss to minimize (meta loss)
   private final long _maxTime;           // maximum amount of time allotted to automl
   private final double _minAcc;          // minimum accuracy to achieve
   private final boolean _ensemble;       // allow ensembles?
@@ -24,10 +27,11 @@ public final class AutoML {
 
   private boolean _isClassification;
 
-  enum models { GBM, RF, GLM, GLRM, DL }
+  enum models { RF, GBM, GLM, GLRM, DL, KMEANS }  // consider EnumSet
 
   // https://0xdata.atlassian.net/browse/STEAM-52  --more interesting user options
-  public AutoML(Frame fr, int response, String loss, long maxTime, double minAccuracy, boolean ensemble, String[] modelExclude, boolean allowMutations) {
+  public AutoML(Frame fr, int response, String loss, long maxTime, double minAccuracy,
+                boolean ensemble, String[] modelExclude, boolean allowMutations) {
     _fr=fr;
     _response=response;
     _loss=loss;
@@ -62,12 +66,61 @@ public final class AutoML {
     ModelBuilder initModel = selectInitial(fm);
     initModel._parms._ignored_columns = fm.ignoredCols();
 
-    Model m = (Model)initModel.trainModel().get();
-
+    Model m = build(initModel); // need to track this...
+    System.out.println("bink");
     // gather more data? build more models? start applying transforms? what next ...?
+  }
+
+  public void delete() {
+    for(Model m: models()) m.delete();
+    DKV.remove(MODELLIST);
   }
 
   private ModelBuilder selectInitial(FrameMeta fm) {  // may use _isClassification so not static method
     return InitModel.initRF(fm._fr, fm.response()._name);
+  }
+
+
+  // track models built by automl
+  public static final Key<Model> MODELLIST = Key.make(" AutoMLModelList ", (byte) 0, (byte) 2 /*built-in key*/, false);  // public for the test
+  static class ModelList extends Keyed {
+    Key<Model>[] _models;
+    ModelList() { super(MODELLIST); _models = new Key[0]; }
+    private ModelList(Key<Model>[] models) { super(MODELLIST); _models = models; }
+    @Override protected long checksum_impl() { throw H2O.fail("ModelList checksum does not exist by definition"); }
+  }
+
+  static Model[] models() {
+    final Value val = DKV.get(MODELLIST);
+    if( val==null ) return new Model[0];
+    ModelList ml = val.get();
+    Model[] models = new Model[ml._models.length];
+    int j=0;
+    for( int i=0; i<ml._models.length; i++ ) {
+      final Value model = DKV.get(ml._models[i]);
+      if( model != null ) models[j++] = model.get();
+    }
+    if( j==models.length ) return models; // All jobs still exist
+    models = Arrays.copyOf(models, j);     // Shrink out removed
+    Key keys[] = new Key[j];
+    for( int i=0; i<j; i++ ) keys[i] = models[i]._key;
+    // One-shot throw-away attempt at remove dead jobs from the jobs list
+    DKV.DputIfMatch(MODELLIST,new Value(MODELLIST,new ModelList(keys)),val,new Futures());
+    return models;
+  }
+
+  static Model build(ModelBuilder mb) {
+    Model m = (Model)mb.trainModel().get(); // need to track this...
+    final Key modelKey  = m._key;
+    new TAtomic<ModelList>() {
+      @Override public ModelList atomic(ModelList old) {
+        if( old == null ) old = new ModelList();
+        Key[] models = old._models;
+        old._models = Arrays.copyOf(models, models.length + 1);
+        old._models[models.length] = modelKey;
+        return old;
+      }
+    }.invoke(MODELLIST);
+    return m;
   }
 }
