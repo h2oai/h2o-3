92c23007a610e13e13f2f5123e07debfc68419bc
SpencerA <spnrpa@gmail.com>
2015-12-28 13:22:43 -0800

    flesh out test

--- h2o-automl/src/test/java/ai/h2o/automl/SpecialNATest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/SpecialNATest.java
@@ -27,7 +27,51 @@ public class SpecialNATest {
 
     for(int i=0;i<50;++i)
       specialInts.add(i);
+  }
 
-    System.out.println();
+  @Test public void strsTest() {
+    ColMeta.SpecialNA specialStrs = new ColMeta.SpecialNA(ColMeta.SpecialNA.STR);
+    Assert.that(specialStrs.typeToString().equals("String"), "type was " + specialStrs.typeToString() + "; expected String");
+    specialStrs.add("hello");
+
+    // error to add int
+    try {
+      specialStrs.add(5);
+    } catch( AssertionError e) {
+      Assert.that(e.getMessage().equals("expected String; type is int"), "expected assertion");
+    }
+
+    // error to add double
+    try {
+      specialStrs.add(5.34289);
+    } catch( AssertionError e) {
+      Assert.that(e.getMessage().equals("expected String; type is double"), "expected assertion");
+    }
+
+    for(int i=0;i<50;++i)
+      specialStrs.add("_"+i);
+  }
+
+  @Test public void dblsTest() {
+    ColMeta.SpecialNA specialDbls = new ColMeta.SpecialNA(ColMeta.SpecialNA.DBL);
+    Assert.that(specialDbls.typeToString().equals("double"), "type was " + specialDbls.typeToString() + "; expected double");
+    specialDbls.add(3.14159);
+
+    // error to add String
+    try {
+      specialDbls.add("hello");
+    } catch( AssertionError e) {
+      Assert.that(e.getMessage().equals("expected double; type is String"), "expected assertion");
+    }
+
+    // error to add int
+    try {
+      specialDbls.add(5);
+    } catch( AssertionError e) {
+      Assert.that(e.getMessage().equals("expected double; type is int"), "expected assertion");
+    }
+
+    for(int i=0;i<50;++i)
+      specialDbls.add(i+.34324);
   }
 }
