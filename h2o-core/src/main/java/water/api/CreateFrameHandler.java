a1ce73b35e20f734c23b5070218ff0848f8b151f
Cliff Click <cliffc@acm.org>
2016-01-04 15:03:35 -0800

    CreateFrame returns a Job not a CreateFrame

--- h2o-core/src/main/java/water/api/CreateFrameHandler.java
+++ h2o-core/src/main/java/water/api/CreateFrameHandler.java
@@ -2,17 +2,12 @@ package water.api;
 
 import hex.CreateFrame;
 import water.Job;
-import water.fvec.Frame;
 
 public class CreateFrameHandler extends Handler {
 
-  public CreateFrameV3 run(int version, CreateFrameV3 cf) {
+  public JobV3 run(int version, CreateFrameV3 cf) {
     CreateFrame cfr = new CreateFrame(cf.dest.key());
     cf.fillImpl(cfr);
-    Job<Frame> job = cfr.execImpl(); //non-blocking -> caller has to check Job progress
-    CreateFrameV3 cfv3 = (CreateFrameV3)Schema.schema(version, CreateFrame.class).fillFromImpl(cfr);
-    cfv3.key  = new KeyV3.JobKeyV3  (job._key   );
-    cfv3.dest = new KeyV3.FrameKeyV3(job._result);
-    return cfv3;
+    return (JobV3)Schema.schema(version, Job.class).fillFromImpl(cfr.execImpl());
   }
 }
