141757e225b75398b240f298bd9e9bcd6cb0b0a5
eric eckstrand <eric.eckstrand@gmail.com>
2016-01-07 18:23:38 -0800

    wip (AST) feature test harness: some clean up. no longer have parameter schemas for each feature. assume values are provided in parameter string for all parameters. let h2o/r package to checks on parameters. handle cut and match operations.

--- h2o-r/h2o-test-package/R/feature.R
+++ h2o-r/h2o-test-package/R/feature.R
@@ -1,19 +1,3 @@
-#h2oTest.loadFeatureTestCase<-
-#function(testCaseId) {
-#    testCase <- new("FeatureTestCase")
-#    h2oTest.logInfo(slotNames(testCase))
-#    print(testCase@featureTestCasesCSV)
-#    featureTestCasesCSVPath <- h2oTest.locate(testCase@featureTestCasesCSV)
-#    print(featureTestCasesCSVPath)
-#    if (file.exists(featureTestCasesCSVPath)) {
-#        featureTestCasesCSVTable <- read.table(featureTestCasesCSVPath,header=TRUE,sep="|")
-#    } else {
-#        h2oTest.fail(paste0("Couldn't find featureTestCases.csv which should be located in: ",featureTestCasesCSVPath))
-#    }
-#    print(featureTestCasesCSVTable)
-#}
-
-
 #'
 #' --------------- execute a given test case ---------------
 #'
@@ -22,20 +6,12 @@ function(testCase) {
 
     dataSets <- .loadDataSets(testCase@dataSets)
 
-    h2oArgSchemaType <- .getArgSchemaType(testCase@feature,FALSE)
-    print("The h2o arg schema type")
-    print(h2oArgSchemaType)
-
-    h2oArgSchema  <- .getArgSchema(h2oArgSchemaType)
-    print("The h2o parameter schema")
-    print(h2oArgSchema)
-
-    h2oFeatureParamsList <- .makeFeatureParamsList(testCase@featureParams, h2oArgSchema)
+    h2oFeatureParamsList <- .makeH2OFeatureParamsList(testCase@featureParams)
     print("The h2o feature params list")
     print(h2oFeatureParamsList)
 
     h2oEnv <- new.env() # new environment for do.call, that has the h2o args defined
-    argsH2O <- .makeArgList(h2oArgSchema, h2oFeatureParamsList, dataSets, h2oEnv, FALSE)
+    argsH2O <- .makeArgList(h2oFeatureParamsList, dataSets, testCase@feature, h2oEnv, FALSE)
     print("checking argsH2O")
     print(argsH2O)
 
@@ -45,26 +21,18 @@ function(testCase) {
     print("checking h2o res")
     print(h2oRes)
 
-    h2oReturnClass <- .validateH2OReturnClass(h2oRes, testCase@feature)
+    h2oReturnClass <- class(h2oRes)
     print("class of h2o res")
     print(h2oReturnClass)
 
     if (testCase@validationMethod == "R") {
 
-        rArgSchemaType <- .getArgSchemaType(testCase@feature,TRUE)
-        print("The R arg schema type")
-        print(rArgSchemaType)
-
-        rArgSchema  <- .getArgSchema(rArgSchemaType)
-        print("The R arg schema")
-        print(rArgSchema)
-
-        rFeatureParamsList <- .makeFeatureParamsList(testCase@featureParams, rArgSchema)
+        rFeatureParamsList <- .translateH2OFeatureParamsListToR(h2oFeatureParamsList, testCase@feature)
         print("The r feature params list")
         print(rFeatureParamsList)
 
         rEnv <- new.env()
-        argsR <- .makeArgList(rArgSchema, rFeatureParamsList, dataSets, rEnv, TRUE)
+        argsR <- .makeArgList(rFeatureParamsList, dataSets, testCase@feature, rEnv, TRUE)
         print("checking argsR")
         print(argsR)
 
@@ -115,210 +83,105 @@ function(dataSets) {
 
 
 #'
-#' --------------- return the argument schema type for the given feature ---------------
+#' --------------- parse the semi-colon-seperated feature parameters string into a list of params ---------------
 #'
-.getArgSchemaType<-
-function(feature, r) {
-    if (feature %in% c("asFactor", "cosine", "all")) {
-        return("df1")
-    } else if (feature %in% c("and")) {
-        return("df2")
-    } else if (feature %in% c("cbind", "table")) {
-        return("df-1")
-    } else if (feature %in% c("colNames")) {
-        return("df1l1c1")
-    } else if (feature %in% c("slice")) {
-        return("df1i1i1")
-    } else if (feature %in% c("histogram")) {
-        return("df1c1")
-    } else if (feature %in% c("impute")) {
-        return("df1i1c1c1i1l1")
-    } else if (feature %in% c("quantile")) {
-        if (!r) {
-            return("df1n1c1n1")
-        } else {
-            return("dn1n1")
-        }
-    }
-}
+# TODO: doesn't allow empty parameters in parameter string. all parameters need to be specified (in order).
+# allow optional parameters.
+.makeH2OFeatureParamsList<-
+function(featureParamsString) {
+    featureParamsList <- as.list(strsplit(featureParamsString,";")[[1]])
+    print("strList: ")
+    print(featureParamsList)
 
-
-#'
-#' --------------- return the argument schema for the given schema type ---------------
-#'
-.getArgSchema<-
-function(schemaType) {
-    if        (schemaType == "df1")           { return(list(list(type="dataset",   cardinality=1,  dtype="frame")))
-    } else if (schemaType == "df2")           { return(list(list(type="dataset",   cardinality=2,  dtype="frame")))
-    } else if (schemaType == "df-1")          { return(list(list(type="dataset",   cardinality=-1, dtype="frame")))
-    } else if (schemaType == "df1l1c1")       { return(list(list(type="dataset",   cardinality=1,  dtype="frame"),
-                                                            list(type="logical",   cardinality=1),
-                                                            list(type="character", cardinality=1)))
-    } else if (schemaType == "df1i1i1")       { return(list(list(type="dataset",   cardinality=1,  dtype="frame"),
-                                                            list(type="integer",   cardinality=1),
-                                                            list(type="integer",   cardinality=1)))
-    } else if (schemaType == "df1c1")         { return(list(list(type="dataset",   cardinality=1,  dtype="frame"),
-                                                            list(type="character", cardinality=1)))
-    } else if (schemaType == "df1i1c1c1i1l1") { return(list(list(type="dataset",   cardinality=1,  dtype="frame"),
-                                                            list(type="integer",   cardinality=1),
-                                                            list(type="character", cardinality=1),
-                                                            list(type="character", cardinality=1),
-                                                            list(type="integer",   cardinality=1),
-                                                            list(type="logical",   cardinality=1)))
-    } else if (schemaType == "df1n1c1n1")     { return(list(list(type="dataset",   cardinality=1,  dtype="frame"),
-                                                            list(type="numeric",   cardinality=1),
-                                                            list(type="character", cardinality=1),
-                                                            list(type="numeric",   cardinality=1)))
-    } else if (schemaType == "dn1n1")         { return(list(list(type="dataset",   cardinality=1,  dtype="numeric"),
-                                                            list(type="numeric",   cardinality=1)))
-    }
-}
-
-
-#'
-#' --------------- parse the semi-colon-seperated feature parameters string into a list of params, which have been
-#'                 casted to their correct type ---------------
-#'
-.makeFeatureParamsList<-
-function(featureParamsString, pSchema) {
-    # get all of the non dataset parameter schemas
-    newPSchema <- list()
-    c <- 1
-    for(p in pSchema) {
-        if (p$type != "dataset") {
-            newPSchema[[c]] <- p
-            c <- c + 1
-        }
-    }
-
-    featureParamsList <- list()
-
-    # make a list of all the parameters in their string format
-    numArgs <- length(newPSchema) #TODO: this needs to take into account non dataset parameters with cardinality > 1
-    if (numArgs == 0) { return(featureParamsList) }
-    print(paste0("numArgs: ",numArgs))
-
-    strList <- as.list(strsplit(featureParamsString,";")[[1]])
-    print(paste0("strList: ",strList))
-
-    numStrs <- length(strList)
-    print(paste0("numStrs: ",numStrs))
-
-    if (numStrs != 0) { for (i in 1:numStrs) { featureParamsList[[i]] <- strList[[i]] } }
-    if (numArgs > numStrs) { featureParamsList[[numArgs]] <- "" }
-    print(paste0("featureParamsList: ",featureParamsList))
-
-    # cast the non-empty parameters to their proper type
-    for (i in 1:numArgs) {
-        # TODO: doesn't allow empty parameters in parameter string. all parameters need to be specified (in order).
-        # allow optional parameters.
-        if (featureParamsList[[i]] == "") { stop("All parameters must be specified!")}
-        # parameters in the the parameter string should be valid R expressions
-        p <- eval(parse(text=featureParamsList[[i]]))
-        if (is.null(p))                                 { featureParamsList[i]   <- list(NULL)
-        } else if (newPSchema[[i]]$type == "character") { featureParamsList[[i]] <- as.character(p)
-        } else if (newPSchema[[i]]$type == "logical")   { featureParamsList[[i]] <- as.logical(p)
-        } else if (newPSchema[[i]]$type == "integer")   { featureParamsList[[i]] <- as.integer(p)
-        } else if (newPSchema[[i]]$type == "numeric")   { featureParamsList[[i]] <- as.numeric(p)
-        }
+    if (length(featureParamsList) == 0) { return(featureParamsList) }
+    # evaluate the individual parameter strings
+    for (i in 1:length(featureParamsList)) {
+        p <- eval(parse(text=featureParamsList[[i]])) # parameters in the the parameter string should be valid R expressions
+        if (is.null(p)) { featureParamsList[i]   <- list(NULL)
+        } else {          featureParamsList[[i]] <- p }
     }
     return(featureParamsList)
 }
 
 
 #'
-#' --------------- get args for subsequent do.call ---------------
+#' --------------- translate the list of h2o parameters into the proper R parameters ---------------
 #'
+.translateH2OFeatureParamsListToR<-
+function(h2oFeatureParamsList, feature) {
+    if (feature == "quantile") {
+        return(h2oFeatureParamsList[1])
+    } else {
+        return(h2oFeatureParamsList)
+    }
+}
 
+
+#'
+#' --------------- return the data sets type for the given h2o or r feature ---------------
+#'
+.getDataSetsType<-
+function(feature, r) {
+    if (feature %in% c("asFactor", "cosine", "all", "and", "cbind", "table", "colNames", "slice", "histogram",
+                       "impute")) {
+        return("H2OFrameOrdata.frame")
+    } else if (feature %in% c("quantile", "cut", "match")) {
+        if (!r) { return("H2OFrameOrdata.frame")
+        } else  { return("numeric")
+        }
+    }
+}
+
+#'
+#' --------------- simple protocol. put the data sets (in the order given) on the arg list first. next, put the
+#' --------------- parameters (in the order given) on the arg list.
+#'
 # modifies env
 .makeArgList<-
-function(pSchema, featureParamsList, dataSets, env, r=FALSE) {
+function(featureParamsList, dataSets, feature, env, r) {
     parameterNameSpace <- LETTERS
     symbolCount <- 0
-    dataSetCount <- 0
-    featureParamCount <- 0
 
-    argList <- list()
-    for (p in pSchema) {
-        if (p$type == "dataset") { # parameter is one or more data sets
-            if (p$cardinality == -1) { # -1 means get all of the data sets in dataSets
-                numDataSets <- length(dataSets)
-            } else {
-                numDataSets <- p$cardinality
+    dataSetsType <- .getDataSetsType(feature, r)
+
+    for (d in dataSets) {
+        if (dataSetsType == "H2OFrameOrdata.frame") {
+            if (r) { d <- as.data.frame(h2o.getFrame(d@key))
+            } else { d <- h2o.getFrame(d@key)
             }
-            for(i in 1:numDataSets) {
-                if (p$dtype == "frame") {
-                    if (r) { d <- as.data.frame(h2o.getFrame(dataSets[[dataSetCount+1]]@key))
-                    } else { d <- h2o.getFrame(dataSets[[dataSetCount+1]]@key)
-                    }
-                } else if (p$dtype == "numeric") { # we only take the first column of the dataset source file
-                    d <- as.data.frame(h2o.getFrame(dataSets[[dataSetCount+1]]@key)[,1])[,1]
-                    print("dataset dtype numeric")
-                    print(d)
-                }
-                assign(parameterNameSpace[symbolCount+1], d, envir=env)
-                dataSetCount <- dataSetCount + 1
-                symbolCount <- symbolCount + 1
-            }
-        } else { # parameter is a non-dataset parameter
-            assign(parameterNameSpace[symbolCount+1], featureParamsList[[featureParamCount+1]], envir=env)
-            featureParamCount <- featureParamCount + 1
-            symbolCount <- symbolCount + 1
+        } else if (dataSetsType == "numeric") { # we only take the first column of the dataset source file
+            d <- as.data.frame(h2o.getFrame(d@key)[,1])[,1]
         }
+        assign(parameterNameSpace[symbolCount+1], d, envir=env)
+        symbolCount <- symbolCount + 1
+    }
+
+    for (f in featureParamsList) {
+        assign(parameterNameSpace[symbolCount+1], f, envir=env)
+        symbolCount <- symbolCount + 1
     }
     return(lapply(LETTERS[1:symbolCount], function(s) { as.name(s) }))
 }
 
 
 #'
-#' --------------- validate the return class of the h2o feature ---------------
+#' --------------- ensure the h2o and r results are comparable ---------------
 #'
-.validateH2OReturnClass<-
-function(h2oRes, feature) {
-    returnedClass <- class(h2oRes)
-    allowableH2OReturnClasses <- .getAllowableH2OReturnClasses(feature)
-    if (!(returnedClass %in% allowableH2OReturnClasses)) {
-        stop(paste0("Returned class was: ", returnedClass, ", but expected: ", paste(allowableH2OReturnClasses,
-                    collapse=", ")))
-    }
-    return(returnedClass)
-}
-
-.getAllowableH2OReturnClasses<-
-function(feature) {
-    if (feature == "cosine" || feature == "slice") {
-        return(c("H2OFrame", "factor", "numeric", "character"))
-    } else if (feature == "and") {
-        return(c("H2OFrame", "numeric"))
-    } else if (feature == "all") {
-        return(c("logical"))
-    } else if (feature == "asFactor" || feature == "cbind" || feature == "impute" || feature == "table") {
-        return(c("H2OFrame"))
-    } else if (feature == "colNames") {
-        return(c("character"))
-    } else if (feature == "histogram") {
-        return(c("histogram"))
-    } else if (feature == "quantile") {
-        return(c("numeric"))
-    } else {
-        stop("Haven't decided what allowable return type for this feature is.")
-    }
-}
-
 .validateRAndH2OResultComparability<-
 function(h2oReturnClass, rReturnClass) {
     if        (h2oReturnClass == "H2OFrame"  && rReturnClass == "data.frame") {
     } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "matrix")     {
-    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "table")     {
+    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "table")      {
     } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "factor")     {
-    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "numeric")     {
-    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "character")     {
+    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "numeric")    {
+    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "character")  {
+    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "integer")    {
     } else if (h2oReturnClass == "factor"    && rReturnClass == "factor")     {
     } else if (h2oReturnClass == "numeric"   && (rReturnClass %in% c("numeric", "integer", "double")))   {
     } else if (h2oReturnClass == "character" && rReturnClass == "character")  {
     } else if (h2oReturnClass == "logical"   && rReturnClass == "logical")    {
     } else {
-        stop("H2O's and R's results cannot be compared because they are incompatible classes.")
+        stop("H2O's and R's results cannot be compared because they are not compatible classes.")
     }
 }
 
@@ -331,7 +194,8 @@ function(h2oRes, rRes, h2oReturnClass, rReturnClass) {
     if        (h2oReturnClass == "H2OFrame"  && rReturnClass == "data.frame") { .compare2Frames(h2oRes, rRes)
     } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "matrix")     { .compareFrameAndMatrix(h2oRes, rRes)
     } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "table")      { .compareFrameAndTable(h2oRes, rRes)
-    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "factor")     { .compareFrameAndFactor(h2oRes, rRes)
+    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "factor")     { .compareFrameAndBase(h2oRes, rRes)
+    } else if (h2oReturnClass == "H2OFrame"  && rReturnClass == "integer")    { .compareFrameAndBase(h2oRes, rRes)
     } else if (h2oReturnClass == "factor"    && rReturnClass == "factor")     { .compare2Bases(h2oRes, rRes)
     } else if (h2oReturnClass == "numeric"   && is(rReturnClass,"numeric"))   { .compare2Bases(h2oRes, rRes)
     } else if (h2oReturnClass == "character" && rReturnClass == "character")  { .compare2Bases(h2oRes, rRes)
@@ -374,7 +238,7 @@ function(h2oRes, rRes) {
     }
 }
 
-.compareFrameAndFactor<-
+.compareFrameAndBase<-
 function(h2oRes, rRes) {
     numH2OCols <- ncol(h2oRes)
     if (!(numH2OCols == 1)) {
@@ -421,6 +285,8 @@ function(op) {
     } else if (op == "impute")    { return("h2o.impute")
     } else if (op == "table")     { return("h2o.table")
     } else if (op == "quantile")  { return("h2o.quantile")
+    } else if (op == "cut")       { return("cut")
+    } else if (op == "match")     { return("h2o.match")
     }
 }
 
@@ -435,5 +301,23 @@ function(op) {
     } else if (op == "slice")     { return("[")
     } else if (op == "table")     { return("table")
     } else if (op == "quantile")  { return("quantile")
+    } else if (op == "cut")       { return("cut")
+    } else if (op == "match")     { return("match")
     }
 }
+
+
+#h2oTest.loadFeatureTestCase<-
+#function(testCaseId) {
+#    testCase <- new("FeatureTestCase")
+#    h2oTest.logInfo(slotNames(testCase))
+#    print(testCase@featureTestCasesCSV)
+#    featureTestCasesCSVPath <- h2oTest.locate(testCase@featureTestCasesCSV)
+#    print(featureTestCasesCSVPath)
+#    if (file.exists(featureTestCasesCSVPath)) {
+#        featureTestCasesCSVTable <- read.table(featureTestCasesCSVPath,header=TRUE,sep="|")
+#    } else {
+#        h2oTest.fail(paste0("Couldn't find featureTestCases.csv which should be located in: ",featureTestCasesCSVPath))
+#    }
+#    print(featureTestCasesCSVTable)
+#}
