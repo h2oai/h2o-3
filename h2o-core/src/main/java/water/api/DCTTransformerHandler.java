3b1f1a5f199ee4ce01373891a5f19458172324e3
Cliff Click <cliffc@acm.org>
2016-01-07 18:36:48 -0800

    DCT REST call expects a JobV3

--- h2o-core/src/main/java/water/api/DCTTransformerHandler.java
+++ h2o-core/src/main/java/water/api/DCTTransformerHandler.java
@@ -1,11 +1,12 @@
 package water.api;
 
+import water.Job;
 import water.util.DCTTransformer;
 
 public class DCTTransformerHandler extends Handler {
 
-  public DCTTransformerV3 run(int version, DCTTransformerV3 sf) {
+  public JobV3 run(int version, DCTTransformerV3 sf) {
     DCTTransformer fft = sf.createAndFillImpl();
-    return (DCTTransformerV3) Schema.schema(version, DCTTransformer.class).fillFromImpl(fft.exec());
+    return (JobV3) Schema.schema(version, Job.class).fillFromImpl(fft.exec());
   }
 }
