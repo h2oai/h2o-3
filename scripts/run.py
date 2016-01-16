385e39c7fe0e7fb3a52312261c6353cee1f0a6ee
Nishant Kalonia <nishant@h2o.ai>
2016-01-15 21:18:23 -0800

    Fixed another indentation error

--- scripts/run.py
+++ scripts/run.py
@@ -1546,9 +1546,9 @@ class TestRunner:
             if len(self.tests_running) > 0:
                 for test in self.tests_running:
                     if (test.is_completed()):
-                    self.tests_running.remove(test)
-                    self._report_test_result(test, nopass)
-                    return (test.get_ip(), test.get_port())
+                        self.tests_running.remove(test)
+                        self._report_test_result(test, nopass)
+                        return (test.get_ip(), test.get_port())
             else if len(self.suspicious_clouds) == 0:
                 self._log('WAITING FOR ONE TEST TO COMPLETE, BUT THERE ARE NO RUNNING TESTS. EXITING...')
                 sys.exit(1)
