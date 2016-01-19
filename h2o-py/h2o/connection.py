4905783d53f85275ab71d124ea22df22d82090c8
Nishant Kalonia <nishant@h2o.ai>
2016-01-19 12:49:25 -0800

    Added a "force_connect" parameter to H2O.init() in h2o.py. When set to True, h2o will attempt to connect to the specified cluster regardless of the cluster's health (A warning message will still be displayed if the cluster is unhealthy). This force_connect parameter is now enabled whenever a test is run through run.py with the "--jacoco" flag enabled.

--- h2o-py/h2o/connection.py
+++ h2o-py/h2o/connection.py
@@ -42,7 +42,7 @@ class H2OConnection(object):
   __ENCODING_ERROR__ = "replace"
 
   def __init__(self, ip, port, start_h2o, enable_assertions, license, nthreads, max_mem_size, min_mem_size, ice_root,
-               strict_version_check, proxy, https, insecure, username, password, max_mem_size_GB, min_mem_size_GB):
+               strict_version_check, proxy, https, insecure, username, password, force_connect, max_mem_size_GB, min_mem_size_GB):
     """
     Instantiate the package handle to the H2O cluster.
     :param ip: An IP address, default is "localhost"
@@ -60,7 +60,8 @@ class H2OConnection(object):
     :param https: Set this to True to use https instead of http.
     :param insecure: Set this to True to disable SSL certificate checking.
     :param username: Username to login with.
-    :param password: Password to login with. 
+    :param password: Password to login with.
+    :param force_connect: Set this to True to attempt to establish a connection even if the cluster reports as unhealthy
     :param max_mem_size_GB: DEPRECATED. Use max_mem_size.
     :param min_mem_size_GB: DEPRECATED. Use min_mem_size.
     :return: None
@@ -88,6 +89,7 @@ class H2OConnection(object):
     self._insecure = insecure
     self._username = username
     self._password = password
+    self.force_connect = force_connect
     self._session_id = None
     self._rest_version = __H2O_REST_API_VERSION__
     self._child = getattr(__H2OCONN__, "_child") if hasattr(__H2OCONN__, "_child") else None
@@ -229,7 +231,10 @@ class H2OConnection(object):
       try:
         cld = H2OConnection.get_json(url_suffix="Cloud")
         if not cld['cloud_healthy']:
-          raise ValueError("Cluster reports unhealthy status", cld)
+          if __H2OCONN__.force_connect:
+            print("WARNING: Cluster reports unhealthy status")
+          else:
+            raise ValueError("Cluster reports unhealthy status", cld)
         if cld['cloud_size'] >= size and cld['consensus']:
           if print_dots: print(" Connection successful!")
           return cld
--- h2o-py/h2o/h2o.py
+++ h2o-py/h2o/h2o.py
@@ -668,7 +668,7 @@ def cluster_status():
 def init(ip="localhost", port=54321, start_h2o=True, enable_assertions=True,
          license=None, nthreads=-1, max_mem_size=None, min_mem_size=None, ice_root=None, 
          strict_version_check=True, proxy=None, https=False, insecure=False, username=None, 
-         password=None, max_mem_size_GB=None, min_mem_size_GB=None):
+         password=None, force_connect=False, max_mem_size_GB=None, min_mem_size_GB=None):
   """Initiate an H2O connection to the specified ip and port.
 
   Parameters
@@ -707,6 +707,8 @@ def init(ip="localhost", port=54321, start_h2o=True, enable_assertions=True,
     Username to login with.
   password : str
     Password to login with.
+  force_connect : bool
+    When set to True, a connection to the cluster will attempt to be established regardless of the its health.
   max_mem_size_GB: DEPRECATED
     Use max_mem_size instead.
   min_mem_size_GB: DEPRECATED
@@ -727,7 +729,7 @@ def init(ip="localhost", port=54321, start_h2o=True, enable_assertions=True,
   H2OConnection(ip=ip, port=port,start_h2o=start_h2o,enable_assertions=enable_assertions,license=license,
                 nthreads=nthreads,max_mem_size=max_mem_size,min_mem_size=min_mem_size,ice_root=ice_root,
                 strict_version_check=strict_version_check,proxy=proxy,https=https,insecure=insecure,username=username,
-                password=password,max_mem_size_GB=max_mem_size_GB,min_mem_size_GB=min_mem_size_GB)
+                password=password,force_connect=force_connect,max_mem_size_GB=max_mem_size_GB,min_mem_size_GB=min_mem_size_GB)
   return None
 
 
