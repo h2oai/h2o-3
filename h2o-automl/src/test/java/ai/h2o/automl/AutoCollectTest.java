b8809ee19b44e5bc5f1d90ddc80a62e487be37eb
Navdeep Gill <navdeep@h2o.ai>
2016-01-28 11:40:48 -0800

    Bring back testConnection() and make paths relative to repo.

--- h2o-automl/src/test/java/ai/h2o/automl/AutoCollectTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/AutoCollectTest.java
@@ -3,23 +3,26 @@ package ai.h2o.automl;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class AutoCollectTest extends TestUtil { // requires mysql-connector-java-3.1.14-bin.jar in -cp
   @BeforeClass public static void setup() { stall_till_cloudsize(1); }
-//  @Test public void testConnection() {
-//    ResultSet rs = AutoCollect.query("SHOW TABLES;");
-//    try {
-//      while(rs.next()) {
-//        System.out.println(rs.getString(1));
-//      }
-//    } catch (SQLException e) {
-//      e.printStackTrace();
-//    } finally {
-//      try {
-//        rs.close();
-//      } catch(SQLException ex) {}
-//    }
-//    System.out.println();
-//  }
+  @Test public void testConnection() {
+    ResultSet rs = AutoCollect.query("SHOW TABLES;");
+    try {
+      while(rs.next()) {
+        System.out.println(rs.getString(1));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        rs.close();
+      } catch(SQLException ex) {}
+    }
+    System.out.println();
+  }
 //
 //  @Test public void testHasDataset() {
 //    AutoCollect ac = new AutoCollect(3600,"");
@@ -38,8 +41,9 @@ public class AutoCollectTest extends TestUtil { // requires mysql-connector-java
 //    }
 //  }
 
-  @Test public void testSmalldataMetaCollect() {
-    AutoCollect ac = new AutoCollect(120, find_test_file_static("/0xdata/steam-automl/smalldata/meta").getAbsolutePath());
-    ac.start();
-  }
+
+//  @Test public void testSmalldataMetaCollect() {
+//    AutoCollect ac = new AutoCollect(120, find_test_file_static("smalldata/meta").getAbsolutePath());
+//    ac.start();
+//  }
 }
