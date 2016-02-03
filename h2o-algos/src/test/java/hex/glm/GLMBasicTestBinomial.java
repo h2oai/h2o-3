ffc146a1a7811114232f99ad6c564619cbf6e9de
Tomas Nykodym <tomas@h2o.ai>
2016-02-02 18:34:13 -0800

    Fixed sign in my previous test updated.

--- h2o-algos/src/test/java/hex/glm/GLMBasicTestBinomial.java
+++ h2o-algos/src/test/java/hex/glm/GLMBasicTestBinomial.java
@@ -438,7 +438,7 @@ public class GLMBasicTestBinomial extends TestUtil {
         // compare validation res dev matches R
         // sum(binomial()$dev.resids(y=test$CAPSULE,mu=p,wt=1))
         // [1]80.92923
-        assertTrue(80.92923 >= GLMTest.residualDevianceTest(model) + 1e-2);
+        assertTrue(80.92923 >= GLMTest.residualDevianceTest(model) - 1e-2);
 //      compare validation null dev against R
 //      sum(binomial()$dev.resids(y=test$CAPSULE,mu=.5,wt=1))
 //      [1] 124.7665
