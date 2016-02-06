11f81dd8e240dc7f5fd5e9e630a388cbed91a54b
Arno Candel <arno.candel@gmail.com>
2016-02-06 09:22:51 -0800

    Add quantile to DL GradientCheck JUnit.

--- h2o-algos/src/main/java/hex/deeplearning/DeepLearningModel.java
+++ h2o-algos/src/main/java/hex/deeplearning/DeepLearningModel.java
@@ -788,7 +788,7 @@ public class DeepLearningModel extends Model<DeepLearningModel,DeepLearningModel
     double loss = 0;
     Neurons[] neurons = DeepLearningTask.makeNeuronsForTraining(model_info());
     //for absolute error, gradient -1/1 matches the derivative of abs(x) without correction term
-    final double prefactor = _parms._distribution == Distribution.Family.laplace ? 1 : 0.5;
+    final double prefactor = _parms._distribution == Distribution.Family.laplace || _parms._distribution == Distribution.Family.quantile ? 1 : 0.5;
     for (DataInfo.Row myRow : myRows) {
       if (myRow == null) continue;
       long seed = -1; //ignored
