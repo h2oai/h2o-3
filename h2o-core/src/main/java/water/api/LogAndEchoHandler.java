fbe2d2500d6623986cdd15ea1671a82a3466ab7b
SpencerA <spnrpa@gmail.com>
2016-02-12 17:43:24 -0800

    make log and echo perform on all nodes

--- h2o-core/src/main/java/water/api/LogAndEchoHandler.java
+++ h2o-core/src/main/java/water/api/LogAndEchoHandler.java
@@ -1,8 +1,12 @@
 package water.api;
 
+import water.MRTask;
 import water.util.Log;
 
 public class LogAndEchoHandler extends Handler {
   @SuppressWarnings("unused") // called through reflection by RequestServer
-  public LogAndEchoV3 echo(int version, LogAndEchoV3 u) { Log.info(u.message); return u; }
+  public LogAndEchoV3 echo(int version, final LogAndEchoV3 u) {
+    new MRTask() { @Override public void setupLocal() { Log.info(u.message); } }.doAllNodes();
+    return u;
+  }
 }
