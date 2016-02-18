d07cee18e69bb11a7572f197bc404d479e0c6f7b
SpencerA <spnrpa@gmail.com>
2016-02-18 13:09:35 -0800

    run log and echo mrtask at gui lvl priority

--- h2o-core/src/main/java/water/api/LogAndEchoHandler.java
+++ h2o-core/src/main/java/water/api/LogAndEchoHandler.java
@@ -1,12 +1,13 @@
 package water.api;
 
+import water.H2O;
 import water.MRTask;
 import water.util.Log;
 
 public class LogAndEchoHandler extends Handler {
   @SuppressWarnings("unused") // called through reflection by RequestServer
   public LogAndEchoV3 echo(int version, final LogAndEchoV3 u) {
-    new MRTask() { @Override public void setupLocal() { Log.info(u.message); } }.doAllNodes();
+    new MRTask(H2O.MIN_HI_PRIORITY) { @Override public void setupLocal() { Log.info(u.message); } }.doAllNodes();
     return u;
   }
 }
