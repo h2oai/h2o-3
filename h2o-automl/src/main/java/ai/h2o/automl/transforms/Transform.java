42156264e215799812d6f4f2e8659356684be5b0
SpencerA <spnrpa@gmail.com>
2016-02-28 21:18:11 -0800

    update gradle props for new h2o version start building a transform interface that will:   take column meta and column as input (plus historical Transformation info)   select a Transformation for the Vec (includes "drop/ignore")

--- h2o-automl/src/main/java/ai/h2o/automl/transforms/Transform.java
+++ h2o-automl/src/main/java/ai/h2o/automl/transforms/Transform.java
@@ -1,18 +1,27 @@
 package ai.h2o.automl.transforms;
 
+import org.reflections.Reflections;
+import water.rapids.ASTUniOp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Stupid class just to get the proverbial ball rolling.
  * Eventually, will want to javassist (i.e. fuse) transforms over multiple vecs
  * (rather than 1 at a time, which is the current limitation of this class).
  *
- * A pile of heuristics (rules) and models drive the choice of transforms here.
+ * A pile of heuristics (rules) and models drive the choice of transforms by the column to
+ * be transformed. The set of transforms (in the String[] ops) are the names of the AST
+ * operations
  *
  *
  *
  * Some heuristics:
  *    Allowing users to do a log(), or log1p() quickly would be nice.
  *    Trying it behind their back might be useful. I've started to rattle off
- *        "if your numbers span more than an order of magnitude,
+ *        "if your numbers span more than an order of magnitude,x
  *         you might try a log transform"
  *    quite often. Reasonable enough heuristic. Heuristics are good for DSiaB.
  *
@@ -21,5 +30,19 @@ package ai.h2o.automl.transforms;
  *   for that, so we might want to understand the difference and be willing to guess.
  */
 public class Transform {
+  public static final String[] uniOps;
+  static {
+    List<String> ops = new ArrayList<>();
 
+    Reflections reflections = new Reflections("water.rapids");
+
+    Set<Class<? extends ASTUniOp>> uniOpClasses =
+            reflections.getSubTypesOf(water.rapids.ASTUniOp.class);
+
+    for(Class c: uniOpClasses)
+      ops.add(c.getSimpleName().substring("AST".length()));
+
+    uniOps = ops.toArray(new String[ops.size()]);
+  }
+  public static void printOps() { for(String op: uniOps) System.out.println(op); }
 }
