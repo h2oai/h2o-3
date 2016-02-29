dbc697d4f46eb1612e5b2a9db60497d283a228b3
Navdeep Gill <navdeep@h2o.ai>
2016-02-29 14:43:57 -0800

    Remove experiment.R. Not needed anymore.

--- scripts/InternalMetrics/experiment.R
+++
@@ -1,4 +0,0 @@
-master_jobname_plot = ggplot(master_jobname, aes(x=JobName, y=AvgDuration, fill = JobName)) + geom_bar(stat="identity") 
-master_jobname_plot = master_jobname_plot + ggtitle(paste("Average Duration per Jenkins Job in Master on ", master_jobname$Date)) 
-master_jobname_plot = master_jobname_plot + theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) + labs(x="Jenkins Job",y="Average Duration") 
-ggsave(master_jobname_plot,file = "plotsMySQL/master_jobname.pdf",height = 15,width = 15)
