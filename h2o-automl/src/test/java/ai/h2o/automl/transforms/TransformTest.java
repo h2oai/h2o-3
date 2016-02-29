ee10963e12a4f9931a7cec2909e9f1257b4b53ef
SpencerA <spnrpa@gmail.com>
2016-02-28 21:18:11 -0800

    update gradle props for new h2o version start building a transform interface that will:   take column meta and column as input (plus historical Transformation info)   select a Transformation for the Vec (includes "drop/ignore")

---
+++ h2o-automl/src/test/java/ai/h2o/automl/transforms/TransformTest.java
@@ -0,0 +1,16 @@
+package ai.h2o.automl.transforms;
+
+import ai.h2o.automl.TestUtil;
+import org.junit.Test;
+
+public class TransformTest extends TestUtil {
+
+//  @BeforeClass
+//  public static void setup() { stall_till_cloudsize(1); }
+
+  @Test public void testTransformReflection() {
+
+    Transform.printOps();
+
+  }
+}
