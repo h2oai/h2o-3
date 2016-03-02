6a0c764a3ecc89275aaae8b1e18b884216adccc8
Nishant Kalonia <nishant@h2o.ai>
2016-03-01 17:55:34 -0800

    Minor bug fixes

--- scripts/run.py
+++ scripts/run.py
@@ -838,7 +838,7 @@ class Test:
 
     def _pytest_cmd(self, test_name, ip, port, on_hadoop, hadoop_namenode):
       if g_pycoverage:
-        pyver = "coverage" if g_py3 else "coverage-3.5"
+        pyver = "coverage-3.5" if g_py3 else "coverage"
         cmd = [pyver,"run", "-a", g_py_test_setup, "--usecloud", ip + ":" + str(port), "--resultsDir", g_output_dir,
                "--testName", test_name]
       else:
@@ -2063,14 +2063,8 @@ def parse_args(argv):
                 usage()
             g_base_port = int(argv[i])
         elif s == "--py3":
-            i += 1
-            if i >= len(argv):
-                usage()
             g_py3 = True
         elif s == "--coverage":
-            i += 1
-            if i >= len(argv):
-              usage()
             g_pycoverage = True
         elif (s == "--numclouds"):
             i += 1
