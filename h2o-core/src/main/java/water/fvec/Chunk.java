b72d2cb44aa8e57a70ca5a58947236f365e169dc
Tomas Nykodym <tomas.nykodym@gmail.com>
2016-03-04 13:39:29 -0800

    Removed extra type argument from chunk as it was causing problems with scala and not buying us much.

--- h2o-core/src/main/java/water/fvec/Chunk.java
+++ h2o-core/src/main/java/water/fvec/Chunk.java
@@ -108,7 +108,7 @@ public void map( Chunk[] chks ) {                  // Map over a set of same-num
 }}</pre>
  */
 
-public abstract class Chunk<D extends Chunk> extends Iced<D> {
+public abstract class Chunk extends Iced<Chunk> {
   public Chunk() {}
   private Chunk(byte [] bytes) {_mem = bytes;initFromBytes();}
 
@@ -615,10 +615,10 @@ public abstract class Chunk<D extends Chunk> extends Iced<D> {
   public final byte [] asBytes(){return _mem;}
 
   @Override
-  public final D reloadFromBytes(byte [] ary){
+  public final Chunk reloadFromBytes(byte [] ary){
     _mem = ary;
     initFromBytes();
-    return (D)this;
+    return this;
   }
 
   protected abstract void initFromBytes();
