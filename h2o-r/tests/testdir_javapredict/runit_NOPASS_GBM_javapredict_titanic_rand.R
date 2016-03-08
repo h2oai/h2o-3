72f155e3766e00fc133cc6ede7adce5920fb6d1a
eric eckstrand <eric.eckstrand@gmail.com>
2016-03-08 14:06:41 -0800

    update and move GBM pojo runit to PASS

--- h2o-r/tests/testdir_javapredict/runit_NOPASS_GBM_javapredict_titanic_rand.R
+++ h2o-r/tests/testdir_javapredict/runit_NOPASS_GBM_javapredict_titanic_rand.R
@@ -9,50 +9,25 @@ source("../../scripts/h2o-r-test-setup.R")
 #           java must be at least 1.6.
 #----------------------------------------------------------------------
 
-options(echo=FALSE)
-TEST_ROOT_DIR <- ".."
+test.gbm.javapredict.titanic.rand <-
+function() {
 
+    training_file <- locate("smalldata/gbm_test/titanic.csv")
+    test_file <- locate("smalldata/gbm_test/titanic.csv")
+    training_frame <- h2o.importFile(training_file)
+    test_frame <- h2o.importFile(test_file)
 
+    params                 <- list()
+    params$ntree           <- sample(1000, 1)
+    params$max_depth       <- sample(5, 1)
+    params$min_rows        <- sample(10, 1)
+    params$learn_rate      <- sample(c(0.001, 0.002, 0.01, 0.02, 0.1, 0.2), 1)
+    params$balance_classes <- sample(c(T, F), 1)
+    params$x               <- c("pclass","name","sex","age","fare")
+    params$y               <- "survived"
+    params$training_frame  <- training_frame
 
+    doJavapredictTest("gbm",test_file,test_frame,params)
+}
 
-#----------------------------------------------------------------------
-# Parameters for the test.
-#----------------------------------------------------------------------
-
-# The purpose of this test is to:
-# - verify handling of NAs in generated model which has to be consistent with  interpreted version
-# - verify correct escaping of strings which are part of domain: eg. "The name" -> \"The name \"
-
-heading("Choose random parameters")
-
-n.trees <- sample(1000, 1)
-print(paste("n.trees", n.trees))
-
-interaction.depth <- sample(5, 1)
-print(paste("interaction.depth", interaction.depth))
-
-n.minobsinnode <- sample(10, 1)
-print(paste("n.minobsinnode", n.minobsinnode))
-
-shrinkage <- sample(c(0.001, 0.002, 0.01, 0.02, 0.1, 0.2), 1)
-print(paste("shrinkage", shrinkage))
-
-train <- locate("smalldata/gbm_test/titanic.csv")
-print(paste("train", train))
-
-test <- locate("smalldata/gbm_test/titanic.csv")
-print(paste("test", test))
-
-x = c("pclass","name","sex","age","sibsp","parch","ticket","fare","cabin","embarked","boat","body","home.dest")
-print("x")
-print(x)
-
-y = "survived"
-print(paste("y", y))
-
-balance_classes <- sample(c(T, F), 1)
-
-#----------------------------------------------------------------------
-# Run the test
-#----------------------------------------------------------------------
-source('../Utils/shared_javapredict_GBM.R')
+doTest("GBM pojo test", test.gbm.javapredict.titanic.rand)
