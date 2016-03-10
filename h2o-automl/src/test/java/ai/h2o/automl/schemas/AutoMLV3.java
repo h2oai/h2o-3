fa7d7ddce35f4e9c7268d46be4a235e1a5f86122
SpencerA <spnrpa@gmail.com>
2016-03-09 16:01:20 -0800

    change type to ModelKeyV3

--- h2o-automl/src/test/java/ai/h2o/automl/schemas/AutoMLV3.java
+++ h2o-automl/src/test/java/ai/h2o/automl/schemas/AutoMLV3.java
@@ -12,5 +12,5 @@ public class AutoMLV3 extends Schema {
   @API(help="Allow AutoML to build ensembles",direction=API.Direction.INPUT)         public boolean ensemble;
   @API(help="Prevent AutoML from trying these models",direction=API.Direction.INPUT) public String[] exclude;
   @API(help="Try frame transformations",direction=API.Direction.INPUT)               public boolean tryMutations;
-  @API(help="The AutoML result",direction=API.Direction.OUTPUT)                      public KeyV3 result;
+  @API(help="The AutoML result",direction=API.Direction.OUTPUT)                      public KeyV3.ModelKeyV3 result;
 }
