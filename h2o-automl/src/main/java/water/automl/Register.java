829c819865a2523d36480d45f43f218eaed73b39
SpencerA <spnrpa@gmail.com>
2016-03-10 16:28:59 -0800

    add second endpoint for refreshing automl view

--- h2o-automl/src/main/java/water/automl/Register.java
+++ h2o-automl/src/main/java/water/automl/Register.java
@@ -5,6 +5,7 @@ import water.api.AbstractRegister;
 
 public class Register extends AbstractRegister{
   @Override public void register(String relativeResourcePath) throws ClassNotFoundException {
-    H2O.registerPOST("/99/AutoML", AutoMLHandler.class, "automl", "automatically build models");
+    H2O.registerPOST("/99/AutoMLBuilder", AutoMLBuilderHandler.class, "automl", "automatically build models");
+    H2O.registerGET("/99/AutoML/(?<automl_id>.*)/", AutoMLHandler.class,"refersh", "refresh the model key");
   }
 }
