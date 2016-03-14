8633250c99350ef63328300cb2a65aa588e8aeb6
SpencerA <spnrpa@gmail.com>
2016-03-13 23:35:17 -0700

    add json schema generator

---
+++ h2o-automl/src/main/java/water/automl/AutoMLJSONSChemaV3.java
@@ -0,0 +1,11 @@
+package water.automl;
+
+
+import water.Iced;
+import water.api.API;
+import water.api.KeyV3;
+import water.api.RequestSchema;
+
+public class AutoMLJSONSChemaV3 extends RequestSchema<Iced,AutoMLJSONSChemaV3> {
+  @API(help="json schema", direction=API.Direction.OUTPUT) public String json_schema;
+}
--- h2o-automl/src/main/java/water/automl/Register.java
+++ h2o-automl/src/main/java/water/automl/Register.java
@@ -7,5 +7,6 @@ public class Register extends AbstractRegister{
   @Override public void register(String relativeResourcePath) throws ClassNotFoundException {
     H2O.registerPOST("/3/AutoMLBuilder", AutoMLBuilderHandler.class, "automl", "automatically build models");
     H2O.registerGET("/3/AutoML/(?<automl_id>.*)", AutoMLHandler.class,"refresh", "refresh the model key");
+    H2O.registerGET("/3/AutoMLJSONSchemaHandler", AutoMLJSONSchemaHandler.class,"getJSONSchema", "Get the json schema for the AutoML input fields.");
   }
 }
