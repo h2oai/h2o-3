7bb8c0a870c9ee3f9df265568ea305913d4d3fad
SpencerA <spnrpa@gmail.com>
2016-03-16 16:37:11 -0700

    correctly offset the interactions when extracting rows

--- h2o-algos/src/test/java/hex/DataInfoTest.java
+++ h2o-algos/src/test/java/hex/DataInfoTest.java
@@ -4,7 +4,9 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import water.Key;
+import water.MRTask;
 import water.TestUtil;
+import water.fvec.Chunk;
 import water.fvec.Frame;
 import water.fvec.InteractionWrappedVec;
 import water.fvec.Vec;
@@ -14,6 +16,7 @@ import water.fvec.Vec;
 // skipMissing = TRUE/FALSE
 // useAllLevels = TRUE/FALSE
 // limit enums
+// (dont) standardize predictor columns
 
 // data info tests with interactions
 public class DataInfoTest extends TestUtil {
@@ -37,7 +40,7 @@ public class DataInfoTest extends TestUtil {
               false,       // weight
               false,       // offset
               false,       // fold
-              DataInfo.InteractionPair.generatePairwiseInteractions(8, 16, 2)  // interactions
+              DataInfo.InteractionPair.generatePairwiseInteractionsFromList(8, 16, 2)  // interactions
       );
       dinfo.dropInteractions();
       dinfo.remove();
@@ -50,7 +53,7 @@ public class DataInfoTest extends TestUtil {
   @Test public void testAirlines2() {
     Frame fr = parse_test_file(Key.make("a.hex"), "smalldata/airlines/allyears2k_headers.zip");
     try {
-      Frame interactions = DataInfo.makeInteractions(fr,false,DataInfo.InteractionPair.generatePairwiseInteractions(8,16,2),true,true);
+      Frame interactions = DataInfo.makeInteractions(fr,false,DataInfo.InteractionPair.generatePairwiseInteractionsFromList(8, 16, 2),true,true);
       int len=0;
       for(Vec v: interactions.vecs()) len += ((InteractionWrappedVec)v).expandedLength();
       interactions.delete();
@@ -89,7 +92,7 @@ public class DataInfoTest extends TestUtil {
               false,       // weight
               false,       // offset
               false,       // fold
-              DataInfo.InteractionPair.generatePairwiseInteractions(8, 16, 2)  // interactions
+              DataInfo.InteractionPair.generatePairwiseInteractionsFromList(8, 16, 2)  // interactions
       );
       System.out.println(dinfo__withInteractions.fullN());
       Assert.assertTrue(dinfo__withInteractions.fullN() == dinfo__noInteractions.fullN() + len);
@@ -104,7 +107,7 @@ public class DataInfoTest extends TestUtil {
   @Test public void testAirlines3() {
     Frame fr = parse_test_file(Key.make("a.hex"), "smalldata/airlines/allyears2k_headers.zip");
     try {
-      Frame interactions = DataInfo.makeInteractions(fr,false,DataInfo.InteractionPair.generatePairwiseInteractions(8,16,2),false,true);
+      Frame interactions = DataInfo.makeInteractions(fr,false,DataInfo.InteractionPair.generatePairwiseInteractionsFromList(8, 16, 2),false,true);
       int len=0;
       for(Vec v: interactions.vecs()) len += ((InteractionWrappedVec)v).expandedLength();
       interactions.delete();
@@ -141,13 +144,57 @@ public class DataInfoTest extends TestUtil {
               false,       // weight
               false,       // offset
               false,       // fold
-              DataInfo.InteractionPair.generatePairwiseInteractions(8, 16, 2)  // interactions
+              DataInfo.InteractionPair.generatePairwiseInteractionsFromList(8, 16, 2)  // interactions
       );
       System.out.println(dinfo__withInteractions.fullN());
       Assert.assertTrue(dinfo__withInteractions.fullN() == dinfo__noInteractions.fullN() + len);
       dinfo__withInteractions.dropInteractions();
       dinfo__noInteractions.remove();
       dinfo__withInteractions.remove();
+    } finally {
+      fr.delete();
+    }
+  }
+
+
+
+
+  @Test public void testIris1() {  // test that getting sparseRows and denseRows produce the same results
+    Frame fr = parse_test_file(Key.make("a.hex"), "smalldata/iris/iris_wheader.csv");
+    fr.swap(1,4);
+    DataInfo.InteractionPair[] ips = DataInfo.InteractionPair.generatePairwiseInteractionsFromList(0, 1);
+
+    try {
+      final DataInfo di = new DataInfo(
+              fr.clone(),  // train
+              null,        // valid
+              1,           // num responses
+              true,        // use all factor levels
+              DataInfo.TransformType.NONE,  // predictor transform
+              DataInfo.TransformType.NONE,  // response  transform
+              true,        // skip missing
+              false,       // impute missing
+              false,       // missing bucket
+              false,       // weight
+              false,       // offset
+              false,       // fold
+              ips          // interactions
+      );
+
+      new MRTask() {
+        @Override public void map(Chunk[] cs) {
+          DataInfo.Row[] sparseRows = di.extractSparseRows(cs);
+          for(int i=0;i<cs[0]._len;++i) {
+            DataInfo.Row r=di.newDenseRow();
+            di.extractDenseRow(cs,i,r);
+            for(int j=0;j<di.fullN();++j)
+              if( r.get(j) != sparseRows[i].get(j) )
+                throw new RuntimeException("Row mismatch on row " + i);
+          }
+        }
+      }.doAll(di._adaptedFrame);
+      di.dropInteractions();
+      di.remove();
     } finally {
       fr.delete();
     }
