56b74f7c312147dbcc24f9f35cd0239356f24e17
Arno Candel <arno.candel@gmail.com>
2016-03-18 17:04:57 -0700

    PUBDEV-2620: Populate each individual model's start/end/runtime in ms. Was 0. Note: Checkpointing might not restart the clock, so the total runtime can be the difference between the last end time and the first start time.

--- h2o-core/src/main/java/hex/Model.java
+++ h2o-core/src/main/java/hex/Model.java
@@ -325,6 +325,14 @@ public abstract class Model<M extends Model<M,P,O>, P extends Model.Parameters, 
     public Key _cross_validation_predictions[];
     public Key _cross_validation_holdout_predictions_frame_id;
 
+    // Model-specific start/end/run times
+    // Each individual model's start/end/run time is reported here, not the total time to build N+1 cross-validation models, or all grid models
+    public long _start_time;
+    public long _end_time;
+    public long _run_time;
+    protected void startClock() { _start_time = System.currentTimeMillis(); }
+    protected void stopClock()  { _end_time   = System.currentTimeMillis(); _run_time = _end_time - _start_time; }
+
     public Output(){this(false,false,false);}
     public Output(boolean hasWeights, boolean hasOffset, boolean hasFold) {
       _hasWeights = hasWeights;
@@ -519,6 +527,7 @@ public abstract class Model<M extends Model<M,P,O>, P extends Model.Parameters, 
     super(selfKey);
     _parms  = parms ;  assert parms  != null;
     _output = output;  // Output won't be set if we're assert output != null;
+    _output.startClock();
   }
 
   /**
