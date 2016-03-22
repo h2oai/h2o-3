a0ef523d63dc2fb7783578ae8651ce23590a5a73
Arno Candel <arno.candel@gmail.com>
2016-03-22 16:50:19 -0700

    PUBDEV-2774: Add more tests to RUnit - test modulo assignment and user-given fold_column.

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_cv_fold_assignment.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_cv_fold_assignment.R
@@ -16,6 +16,17 @@ test.GBM.nfolds.fold.assignment <- function() {
       B=mean((df[fold==(i-1),4] - h2o.predict(cv_models[[i]], df[fold==(i-1),]))^2)
       expect_true(abs(A-B)<1e-6)
     }
+
+    ## test that user-given fold column is respected
+    df$fold <- as.h2o(sample(1:5,nrow(df),TRUE))
+    m <- h2o.gbm(1:3,4,df,fold_column="fold",keep_cross_validation_fold_assignment=TRUE,keep_cross_validation_predictions = TRUE)
+    fold <- h2o.cross_validation_fold_assignment(m)
+    expect_true(any(fold!=df$fold)==0)
+
+    ## test that modulo fold column is actually modulo
+    m <- h2o.gbm(1:3,4,df,nfolds=5,fold_assignment="Modulo",keep_cross_validation_fold_assignment=TRUE,keep_cross_validation_predictions = TRUE)
+    fold <- h2o.cross_validation_fold_assignment(m)
+    for (i in 1:5) expect_true(any(fold[(1:30)*5-(5-i),]!=(i-1))==0)
 }
 
 doTest("GBM Cross-Validation Fold Assignment Test: Prostate", test.GBM.nfolds.fold.assignment)
