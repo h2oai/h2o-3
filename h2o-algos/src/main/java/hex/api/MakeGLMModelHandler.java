c3ab338ef0aca280cdb47114e8573a1e8bebfd6c
SpencerA <spnrpa@gmail.com>
2016-03-31 03:56:50 -0700

    add one hot expander (that handles interactions as well)

--- h2o-algos/src/main/java/hex/api/MakeGLMModelHandler.java
+++ h2o-algos/src/main/java/hex/api/MakeGLMModelHandler.java
@@ -4,12 +4,20 @@ import hex.DataInfo;
 import hex.DataInfo.TransformType;
 import hex.glm.GLMModel;
 import hex.glm.GLMModel.GLMOutput;
+import hex.schemas.DataInfoFrameV3;
 import hex.schemas.GLMModelV3;
 import hex.schemas.MakeGLMModelV3;
 import water.DKV;
 import water.Key;
+import water.MRTask;
 import water.api.Handler;
+import water.api.KeyV3;
+import water.fvec.Chunk;
+import water.fvec.Frame;
+import water.fvec.NewChunk;
+import water.fvec.Vec;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -38,4 +46,30 @@ public class MakeGLMModelHandler extends Handler {
     return res;
   }
 
+  // instead of adding a new endpoint, just put this stupid test functionality here
+ /** Get the expanded (interactions + offsets) dataset. Careful printing! Test only
+  */
+
+  public DataInfoFrameV3 getDataInfoFrame(int version, DataInfoFrameV3 args) {
+    Frame fr = DKV.getGet(args.frame.key());
+    if( null==fr ) throw new IllegalArgumentException("no frame found");
+    args.result = new KeyV3.FrameKeyV3(HoTDAAWWG(fr,args.interactions,args.useAll,args.standardize)._key);
+    return args;
+  }
+
+  public Frame HoTDAAWWG(Frame fr, String[] interactions, boolean useAll, boolean standardize) {
+    final DataInfo dinfo = new DataInfo(fr,null,1,useAll,standardize?TransformType.STANDARDIZE:TransformType.NONE,TransformType.NONE,true,false,false,false,false,false,interactions);
+    byte[] types = new byte[dinfo.fullN()];
+    Arrays.fill(types, Vec.T_NUM);
+    return new MRTask() {
+      @Override public void map(Chunk[] cs, NewChunk ncs[]) {
+        DataInfo.Row r = dinfo.newDenseRow();
+        for(int i=0;i<cs[0]._len;++i) {
+          r=dinfo.extractDenseRow(cs,i,r);
+          for(int n=0;n<ncs.length;++n)
+            ncs[n].addNum(r.get(n));
+        }
+      }
+    }.doAll(types,dinfo._adaptedFrame.vecs()).outputFrame(Key.make(), dinfo.coefNames(), null);
+  }
 }
---
+++ h2o-algos/src/main/java/hex/schemas/DataInfoFrameV3.java
@@ -0,0 +1,14 @@
+package hex.schemas;
+
+import water.Iced;
+import water.api.API;
+import water.api.KeyV3;
+import water.api.Schema;
+
+public class DataInfoFrameV3 extends Schema<Iced,DataInfoFrameV3> {
+  @API(help="input frame")                                  public KeyV3.FrameKeyV3 frame;
+  @API(help="interactions")                                 public String[] interactions;
+  @API(help="use all factor levels")                        public boolean useAll;
+  @API(help="standardize")                                  public boolean standardize;
+  @API(help="output frame", direction=API.Direction.OUTPUT) public KeyV3.FrameKeyV3 result;
+}
