f1239ef3e136fd443b587d3ce59bc6cf6da8b065
ludi <ludi@h2o.ai>
2016-04-07 15:49:07 -0700

    Uppercase file names

---
+++ h2o-core/src/main/java/water/api/ImportSQLTableHandler.java
@@ -0,0 +1,20 @@
+package water.api;
+
+
+import water.jdbc.SQLManager;
+
+/**
+ * Import Sql Table into H2OFrame
+ */
+
+public class ImportSQLTableHandler extends Handler {
+
+  @SuppressWarnings("unused") // called through reflection by RequestServer
+  public JobV3 importSQLTable(int version, ImportSQLTableV99 importSqlTable) {
+     return SQLManager.importSqlTable(importSqlTable.database_sys, importSqlTable.host, importSqlTable.port, 
+            importSqlTable.database, importSqlTable.table, importSqlTable.username, importSqlTable.password, 
+             importSqlTable.optimize);
+    
+  }
+
+}
---
+++ h2o-core/src/main/java/water/api/ImportSQLTableV99.java
@@ -0,0 +1,33 @@
+package water.api;
+
+
+import water.Iced;
+
+public class ImportSQLTableV99 extends RequestSchema<Iced,ImportSQLTableV99> {
+  
+  //Input fields
+  @API(help="database_sys", required = true)
+  String database_sys;
+
+  @API(help="host", required = true)
+  String host;
+  
+  @API(help="port", required = true)
+  String port;
+  
+  @API(help="database", required = true)
+  String database;
+  
+  @API(help="table", required = true)
+  String table;
+  
+  @API(help="username", required = true)
+  String username;
+  
+  @API(help="password", required = true)
+  String password;
+
+  @API(help="optimize")
+  boolean optimize;
+  
+}
