2e57bc5eec47c4da383dc10482b08da6b474cc82
SpencerA <spnrpa@gmail.com>
2016-04-08 04:09:46 -0700

    finish expr builder, add test to show expr builder api in action

--- h2o-automl/src/test/java/ai/h2o/automl/transforms/TransformTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/transforms/TransformTest.java
@@ -1,15 +1,42 @@
 package ai.h2o.automl.transforms;
 
+import ai.h2o.automl.TestUtil;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import water.Key;
+import water.fvec.Frame;
 
-public class TransformTest {
+public class TransformTest extends TestUtil {
 
-//  @BeforeClass
-//  public static void setup() { stall_till_cloudsize(1); }
+  @BeforeClass public static void setup() { stall_till_cloudsize(1); }
 
   @Test public void testTransformReflection() {
+//    Transform.printOps();
+  }
 
-    Transform.printOps();
+  @Test public void testExpr() {
+    Frame fr=null;
+    try {
+      fr = parse_test_file(Key.make("a.hex"), "smalldata/iris.csv");
+
+      Expr x = Expr.unOp("log", fr.vec(0));
+      System.out.println(x.toRapids());
+      x.bop("-", x, 1);
+      System.out.println(x.toRapids());
+
+      x.bop("+", fr.vec(0), x);
+      System.out.println(x.toRapids());
+
+      x.bop("*", fr.vec(2), x);
+      System.out.println(x.toRapids());
+
+      x.uop("sqrt");
+      System.out.println(x.toRapids());
+
+
+    } finally {
+      if( null!=fr ) fr.delete();
+    }
 
   }
 }
