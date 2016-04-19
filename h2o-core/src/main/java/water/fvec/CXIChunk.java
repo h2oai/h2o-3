2c7622286ad1659c76184b4560477d7a358846b8
Arno Candel <arno.candel@gmail.com>
2016-04-19 15:42:34 -0700

    PUBDEV-2852: Fix getDoubles for sparse NA chunk, led to GBM AssertionError.

--- h2o-core/src/main/java/water/fvec/CXIChunk.java
+++ h2o-core/src/main/java/water/fvec/CXIChunk.java
@@ -140,7 +140,7 @@ public class CXIChunk extends Chunk {
           break;
       }
     } else throw H2O.unimpl();
-    return sparseLenZero();
+    return isSparseNA() ? sparseLenNA() : sparseLenZero();
   }
 
   @Override public boolean isSparseZero() {return true;}
