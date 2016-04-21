084fc8313527623518c6a1312ee0617c9d7ea909
wendycwong <wendycwong@yahoo.com>
2016-04-21 12:49:11 -0700

    PUBDEV-1843: grid testing.  Subtask 6,7,8.  Fixed bug that sometimes may not duplicate parameter specification in both hyper-parameters and model parameters.

--- h2o-py/tests/testdir_dynamic_tests/testdir_algos/glm/pyunit_glm_gridsearch_over_all_params_large.py
+++ h2o-py/tests/testdir_dynamic_tests/testdir_algos/glm/pyunit_glm_gridsearch_over_all_params_large.py
@@ -498,6 +498,8 @@ class Test_glm_grid_search:
 
         error_number = np.random.random_integers(0, 3, 1)   # randomly choose an error
 
+        print("Test 2 error_number is {0}".format(error_number[0]))
+
         error_hyper_params = \
             pyunit_utils.insert_error_grid_search(self.hyper_params, self.gridable_parameters, self.gridable_types,
                                                   error_number[0])
@@ -554,6 +556,8 @@ class Test_glm_grid_search:
         print("test3_duplicated_parameter_specification for GLM " + self.family)
 
         error_number = np.random.random_integers(0, 2, 1)   # randomly choose an error
+
+        print("Test 3 error_number is {0}".format(error_number[0]))
 
         params_dict, error_hyper_params = \
             pyunit_utils.generate_redundant_parameters(self.hyper_params, self.gridable_parameters,
