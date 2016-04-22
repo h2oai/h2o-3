929829a950c06be7b86fc841af977d38ec94329c
Tomas Nykodym <tomas.nykodym@gmail.com>
2016-04-22 15:06:41 -0700

    python test update.

--- h2o-py/tests/testdir_algos/glm/pyunit_glm_regularization_path.py
+++ h2o-py/tests/testdir_algos/glm/pyunit_glm_regularization_path.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(1,"../../../")
 import h2o
+from builtins import range
 from tests import pyunit_utils
 from h2o.estimators.glm import H2OGeneralizedLinearEstimator as glm
 
