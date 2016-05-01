9d0da55c5619e94eaf4dc747f5dbadee286373b8
SpencerA <spnrpa@gmail.com>
2016-05-01 16:49:46 -0700

    update special NA scanner, uniq scanner (for IDs)

--- h2o-automl/src/main/java/ai/h2o/automl/ColMeta.java
+++ h2o-automl/src/main/java/ai/h2o/automl/ColMeta.java
@@ -3,14 +3,18 @@ package ai.h2o.automl;
 
 import ai.h2o.automl.autocollect.AutoCollect;
 import ai.h2o.automl.guessers.ColNameScanner;
+import hex.quantile.Quantile;
+import hex.quantile.QuantileModel;
 import hex.tree.DHistogram;
-import water.Iced;
-import water.fvec.InteractionWrappedVec;
-import water.fvec.TransformWrappedVec;
-import water.fvec.Vec;
+import org.apache.commons.lang.ArrayUtils;
+import water.*;
+import water.fvec.*;
+import water.nbhm.NonBlockingHashMapLong;
+import water.util.Log;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 
 /** Column Meta Data
  *
@@ -31,6 +35,8 @@ public class ColMeta extends Iced {
   public double _percentNA;     // fraction of NAs in the column
   public double _variance;      // variance of the column, pulled from the vec
   public double _sigma;         // pulled from vec rollups
+
+  public String _ignoredReason; // was this ignored by user, or by automl
 
   /**
    * Meta data collected on the first pass over this column.
@@ -95,24 +101,93 @@ public class ColMeta extends Iced {
     _v = v;
     _name = colname;
     _ignored=ignored;
+    if(_ignored) _ignoredReason = "Ignored by Input";
     _idx = idx;
     _nameType=ColNameScanner.IGNORED;
     if( !_ignored || response ) {
       _nameType = ColNameScanner.scan(_name,v);
       _response = response;
-      _specialNAs = new SpecialNA(
-              _v.isNumeric()
-                      ? (_v.isInt() ? SpecialNA.INT : SpecialNA.DBL)
-                      : SpecialNA.STR
-      );
+
+      if( !(v instanceof TransformWrappedVec || v instanceof InteractionWrappedVec) && !_response ) {
+
+        if( _nameType==ColNameScanner.ID ) {
+          assert _v.isInt(): "ColNameScanner declared this column as ID but column not integral!";
+
+          // need to gather up some info on number of uniques in this column
+          // if the number of uniques is ~nrow then want to ignore this column
+          // for smaller datasets, it's ok to MR collect the grand set of uniques to a
+          // single node... but as the number of rows grows (think 100M) then this becomes
+          // prohibitive. As a rule of thumb, cap out number of possible uniques to 1M
+          // after which we must try something else (heuristic based on unqies/chk)
+
+          // Assume that ignorable ID columns are >= 0.
+          // Using the pigeonhole principle, we can exclude any cases where the RANGE of
+          // values is less than the the number of rows.
+          long nrow;
+          if( (v.min() >= 0) &&  (v.max() - v.min() >= (nrow=(v.length() - v.naCnt()))) ) {
+            if( nrow > 5e6 )  // for > 5M rows, use a different strategy
+              _ignored = nrow-(new UniqTask().doAll(v))._size <= 10;
+            else
+              _ignored = new UniqTaskPerChk().doAll(v)._fracUniq >= 0.75;
+          }
+        }
+        if( _ignored ) {
+          _ignoredReason = "AutoML ignoring ID column for too many uniques";
+          Log.info("AutoML ignoring " + _name + " column (Reason: ID + too many uniques)");
+          return;
+        }
+        _specialNAs = new SpecialNA(
+                _v.isNumeric()
+                        ? (_v.isInt() ? SpecialNA.INT : SpecialNA.DBL)
+                        : SpecialNA.STR
+        ).scan(v,this);
+      }
       _percentNA = (double) v.naCnt() / (double) v.length();
       if (!(v instanceof TransformWrappedVec) && !(v instanceof InteractionWrappedVec)) {
-        _ignored = v.isConst() || v.isString() || v.isBad() || v.isUUID(); // auto ignore from the outset
+        _ignored = null!=(ignoreReason(_v)); // auto ignore from the outset
+        if( _ignored ) {
+          _ignoredReason = "AutoML ignoring ID column for " + ignoreReason(v);
+          Log.info("AutoML ignoring " + _name + " column (Reason: "+_ignoredReason + ")");
+          return;
+        }
         _sigma = v.sigma();
         _variance = _sigma * _sigma;
         _vif = -1;
       }
     }
+  }
+
+  private static String ignoreReason(Vec v) {
+    if( v.isBad() ) return "is BAD";
+    if( v.isConst()) return "is constant";
+    if( v.isString()) return "is String";
+    if( v.isUUID()) return "is UUID";
+    return null;
+  }
+
+  private static class UniqTask extends MRTask<UniqTask> {
+    NonBlockingHashMapLong<String> _hm;
+    int _size;
+    @Override public void setupLocal() { _hm=new NonBlockingHashMapLong<>(); }
+    @Override public void map(Chunk c) {
+      for(int i=0;i<c._len;++i) _hm.putIfAbsent(c.at8(i),"");
+    }
+    @Override public void reduce(UniqTask t) {
+      if( _hm!=t._hm ) _hm.putAll(t._hm);
+      t._hm=null;
+    }
+    @Override public void postGlobal() { _size = _hm.size(); }
+  }
+
+  private static class UniqTaskPerChk extends MRTask<UniqTaskPerChk> {
+    private double _fracUniq;
+    @Override public void map(Chunk c) {
+      NonBlockingHashMapLong<String> hm = new NonBlockingHashMapLong<>();
+      for(int i=0;i<c._len;++i) hm.putIfAbsent(c.at8(i),"");
+      _fracUniq = (double)hm.size()/(double)c._len;
+    }
+    @Override public void reduce(UniqTaskPerChk t) { _fracUniq+=t._fracUniq; }
+    @Override public void postGlobal() { _fracUniq /= (double)_fr.anyVec().nChunks(); }
   }
 
   public ColMeta(Vec v, String colname, int idx, boolean response) {
@@ -171,6 +246,8 @@ public class ColMeta extends Iced {
     double[] _dbls;
     String[] _strs;
 
+    private transient HashSet<Double> _nas;
+
     byte _type;
     int _idx;
     public static final byte INT=0;
@@ -179,6 +256,28 @@ public class ColMeta extends Iced {
 
     String typeToString() {
       return _type==INT ? "int" : (_type==DBL ? "double" : "String");
+    }
+
+    @Override public String toString() {
+      if( _type==INT ) return arrToString(_ints);
+      if( _type==DBL ) return arrToString(_dbls);
+      return ArrayUtils.toString(_strs);
+    }
+
+    private String arrToString(int[] a) {
+      StringBuilder s = new StringBuilder("{");
+      for(int i=0;i<_idx;++i)
+        if( i!=_idx-1) s.append(a[i]).append(",");
+        else s.append(a[i]);
+      return s.append("}").toString();
+    }
+
+    private String arrToString(double[] a) {
+      StringBuilder s = new StringBuilder("{");
+      for(int i=0;i<=_idx;++i)
+        if( i!=_idx) s.append(a[i]).append(",");
+        else s.append(a[i]);
+      return s.append("}").toString();
     }
 
     SpecialNA(byte type) {
@@ -215,5 +314,76 @@ public class ColMeta extends Iced {
         _strs[_idx++]=val;
       }
     }
+
+    public boolean isNA(double d) {
+      if( null==_nas ) setupLocal();
+      return _nas.contains(d);
+    }
+
+    private void setupLocal() {
+      if( _type==STR ) throw H2O.unimpl();
+      _nas=new HashSet<>();
+      for(int i=0;i<_idx;++i)
+        _nas.add(_type==INT?_ints[i]:_dbls[i]);
+    }
+
+    SpecialNA scan(Vec v, ColMeta cm) {
+      if( _type!=STR ) {
+        double median;
+        cm._median = median = median(v, QuantileModel.CombineMethod.INTERPOLATE);
+        double mean = v.mean();
+        double skew = Math.abs(mean-median);
+        if( Math.abs(skew) > 700 ) {  // strong skew => maybe odd NA encoding as large/small value
+          Log.info("Found a strong skew due to possible NA encoded as integer. Median=" + median + "; mean=" + mean);
+          Log.info("Checking if NAs encoded as -99999 or 99999 or some variant.");
+          int l = v.pctiles().length-1;
+          int min = (int)v.pctiles()[0];
+          int max = (int)v.pctiles()[l];
+          if( min==-9999 || min==-99999 || min==-999999 ) add(min);
+          else if( max==9999 || max==99999 || max==999999 ) add(max);
+        }
+      }
+      if( 0!=_idx ) {  // added some special NAs
+        long start = System.currentTimeMillis();
+        new ReplaceSpecialNATask(this).doAll(v);
+        Log.info("Finished substitution in " + (System.currentTimeMillis() - start)/1000. +  "seconds");
+      }
+      return this;
+    }
+
+    private static class ReplaceSpecialNATask extends MRTask<ReplaceSpecialNATask> {
+      private final SpecialNA _nas;
+      ReplaceSpecialNATask(SpecialNA nas) {
+        _nas=nas;
+        Log.info("Found some special NAs: " + _nas.toString());
+      }
+      @Override public void setupLocal() { _nas.setupLocal(); }
+      @Override public void map(Chunk c) {
+        for(int i=0;i<c._len;++i)
+          if( _nas.isNA(c.atd(i)) ) c.setNA(i);
+      }
+    }
+  }
+
+  public static double median(Frame fr, QuantileModel.CombineMethod combine_method) {
+    if( fr.numCols() !=1 || !fr.anyVec().isNumeric() )
+      throw new IllegalArgumentException("median only works on a single numeric column");
+    // Frame needs a Key for Quantile, might not have one from rapids
+    Key tk=null;
+    if( fr._key == null ) { DKV.put(tk = Key.make(), fr = new Frame(tk, fr.names(), fr.vecs())); }
+    // Quantiles to get the median
+    QuantileModel.QuantileParameters parms = new QuantileModel.QuantileParameters();
+    parms._probs = new double[]{0.5};
+    parms._train = fr._key;
+    parms._combine_method = combine_method;
+    QuantileModel q = new Quantile(parms).trainModel().get();
+    double median = q._output._quantiles[0][0];
+    q.delete();
+    if( tk!=null ) { DKV.remove(tk); }
+    return median;
+  }
+
+  static double median(Vec v, QuantileModel.CombineMethod combine_method) {
+    return median(new Frame(v),combine_method);
   }
 }
