15a03d4bd61ca84726a6e313126454074053c5a3
ludi <ludi@h2o.ai>
2016-05-05 09:42:41 -0700

    PUBDEV-2891 Single row of strings are enums, not numeric

--- h2o-py/tests/testdir_jira/pyunit_pubdev_2891.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_2891.py
@@ -8,13 +8,10 @@ import pandas as pd
 def pubdev_2891():
 
     #check the dimension of the frame
-    python_obj = [["a", "b","c","asdfasdf"]]
+    python_obj = ["a", "b","c","asdfasdf"]
     the_frame_1 = h2o.H2OFrame(python_obj)
+    print(the_frame_1)
     pyunit_utils.check_dims_values(python_obj, the_frame_1, rows=1, cols=4)
-
-    python_obj_2 = [["a", "b","c"],["2", "b","c"],["t", "b","c"]]
-    the_frame_2 = h2o.H2OFrame(python_obj_2)
-    pyunit_utils.check_dims_values(python_obj, the_frame_2, rows=3, cols=3)
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(pubdev_2891)
