f5ff2b67e01216edbe541e70ab08f2f4284bf755
Arno Candel <arno.candel@gmail.com>
2016-05-17 14:57:06 -0700

    Rename AggregatorV3 -> AggregatorV99.

--- h2o-algos/src/main/java/hex/schemas/AggregatorModelV99.java
+++ h2o-algos/src/main/java/hex/schemas/AggregatorModelV99.java
@@ -6,15 +6,15 @@ import water.api.KeyV3;
 import water.api.ModelOutputSchema;
 import water.api.ModelSchema;
 
-public class AggregatorModelV3 extends ModelSchema<AggregatorModel, AggregatorModelV3, AggregatorModel.AggregatorParameters, AggregatorV3.AggregatorParametersV3, AggregatorModel.AggregatorOutput, AggregatorModelV3.AggregatorModelOutputV3> {
-  public static final class AggregatorModelOutputV3 extends ModelOutputSchema<AggregatorModel.AggregatorOutput, AggregatorModelOutputV3> {
+public class AggregatorModelV99 extends ModelSchema<AggregatorModel, AggregatorModelV99, AggregatorModel.AggregatorParameters, AggregatorV99.AggregatorParametersV99, AggregatorModel.AggregatorOutput, AggregatorModelV99.AggregatorModelOutputV99> {
+  public static final class AggregatorModelOutputV99 extends ModelOutputSchema<AggregatorModel.AggregatorOutput, AggregatorModelOutputV99> {
     @API(help = "Aggregated Frame of Exemplars")
     public KeyV3.FrameKeyV3 output_frame;
   }
 
   // TODO: I think we can implement the following two in ModelSchema, using reflection on the type parameters.
-  public AggregatorV3.AggregatorParametersV3 createParametersSchema() { return new AggregatorV3.AggregatorParametersV3(); }
-  public AggregatorModelOutputV3 createOutputSchema() { return new AggregatorModelOutputV3(); }
+  public AggregatorV99.AggregatorParametersV99 createParametersSchema() { return new AggregatorV99.AggregatorParametersV99(); }
+  public AggregatorModelOutputV99 createOutputSchema() { return new AggregatorModelOutputV99(); }
 
   // Version&Schema-specific filling into the impl
   @Override public AggregatorModel createImpl() {
