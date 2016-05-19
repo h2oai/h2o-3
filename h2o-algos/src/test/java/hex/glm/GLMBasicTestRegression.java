e2d60c67866d720da5880922d215d382732e0c44
Tomas Nykodym <tomas.nykodym@gmail.com>
2016-05-19 15:36:14 -0700

    Fix in glm test - use vec make copy instead of toNumnericVec which behaves weird.

--- h2o-algos/src/test/java/hex/glm/GLMBasicTestRegression.java
+++ h2o-algos/src/test/java/hex/glm/GLMBasicTestRegression.java
@@ -73,11 +73,11 @@ public class GLMBasicTestRegression extends TestUtil {
     _prostateTrain = parse_test_file("smalldata/glm_test/prostate_cat_train.csv");
     _airlines = parse_test_file("smalldata/airlines/AirlinesTrain.csv.zip");
     Vec v = _airlines.remove("IsDepDelayed");
-    _airlines.add("IsDepDelayed",v.toNumericVec());
+    _airlines.add("IsDepDelayed",v.makeCopy(null));
     v.remove();
     _airlinesMM = parse_test_file(Key.make("AirlinesMM"), "smalldata/airlines/AirlinesTrainMM.csv.zip");
     v = _airlinesMM.remove("IsDepDelayed");
-    _airlinesMM.add("IsDepDelayed",v.toNumericVec());
+    _airlinesMM.add("IsDepDelayed",v.makeCopy(null));
     v.remove();
   }
 
