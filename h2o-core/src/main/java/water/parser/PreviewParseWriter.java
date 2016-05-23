89f6f17b7198051a7c58be9bab66db71f2270199
Cliff Click <cliffc@acm.org>
2016-05-23 12:43:36 -0700

    Tweak parser heuristic; looked busted
    Failed to handle a file with a few numbers and majority of a single
    popular categorical string - column came out as numeric, with most
    values mis-parsed because of the single string so column was >99%
    missing

--- h2o-core/src/main/java/water/parser/PreviewParseWriter.java
+++ h2o-core/src/main/java/water/parser/PreviewParseWriter.java
@@ -139,23 +139,14 @@ public class PreviewParseWriter extends Iced implements ParseWriter {
         continue;
       }
 
-      // All same string, but not obvious NA, declare categorical
-      if (_domains[i].size() == 1
-              && !_domains[i].containsKey("NA")
-              && !_domains[i].containsKey("na")
-              && !_domains[i].containsKey("Na")
-              &&  _nstrings[i] >= nonemptyLines) {
-        types[i] = Vec.T_CAT;
-        continue;
-      }
-
-      // All same string (NA), so just guess numeric
-      if (_domains[i].size() == 1
-          && (_domains[i].containsKey("NA")
-          || !_domains[i].containsKey("na")
-          || !_domains[i].containsKey("Na")
-          ||  _nstrings[i] >= nonemptyLines)) {
-        types[i] = Vec.T_NUM;
+      // All same string or empty?
+      if( _domains[i].size() == 1  ) {
+        // Obvious NA, or few instances of the single string, declare numeric
+        // else categorical
+        types[i] = (_domains[i].containsKey("NA") ||
+                    _domains[i].containsKey("na") ||
+                    _domains[i].containsKey("Na") ||
+                    _nstrings[i] < nonemptyLines) ? Vec.T_NUM : Vec.T_CAT;
         continue;
       }
 
