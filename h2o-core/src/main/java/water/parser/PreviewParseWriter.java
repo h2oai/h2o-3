7fc0705f77f201359aea79562d25a4448790213d
cliffclick <cliffc@acm.org>
2016-05-23 16:53:16 -0700

    Another N/A flavor

--- h2o-core/src/main/java/water/parser/PreviewParseWriter.java
+++ h2o-core/src/main/java/water/parser/PreviewParseWriter.java
@@ -146,6 +146,7 @@ public class PreviewParseWriter extends Iced implements ParseWriter {
         types[i] = (_domains[i].containsKey("NA") ||
                     _domains[i].containsKey("na") ||
                     _domains[i].containsKey("Na") ||
+                    _domains[i].containsKey("N/A") ||
                     _nstrings[i] < _nnums[i]+_nzeros[i]) ? Vec.T_NUM : Vec.T_CAT;
         continue;
       }
