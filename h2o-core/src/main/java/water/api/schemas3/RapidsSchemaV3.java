80f3b2b8ade61146f6c31121ed6da7f80745228b
Pasha Stetsenko <pasha@h2o.ai>
2016-06-22 14:56:22 -0700

    Mark field id in RapidsSchemaV3 as deprecated -- should always be null

--- h2o-core/src/main/java/water/api/schemas3/RapidsSchemaV3.java
+++ h2o-core/src/main/java/water/api/schemas3/RapidsSchemaV3.java
@@ -10,6 +10,12 @@ public class RapidsSchemaV3<I extends Iced, S extends RapidsSchemaV3<I, S>> exte
   @API(help="A Rapids AST expression", direction=API.Direction.INPUT, required=true)
   public String ast;
 
+<<<<<<< 11c9bc6ca4a8d60eee8dea3bf03d145011e126e8
+=======
+  @API(help="[DEPRECATED] Key name to assign Frame results", direction=API.Direction.INPUT)
+  public String id;
+
+>>>>>>> Mark field id in RapidsSchemaV3 as deprecated -- should always be null
   @API(help="Session key", direction=API.Direction.INPUT)
   public String session_id;
 
