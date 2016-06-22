9bc22341ec237631e6b82a8da3d6022297ef1d3e
Pasha Stetsenko <pasha@h2o.ai>
2016-06-22 15:58:07 -0700

    Make .ast parameter in RapidsSchemaV3 required, and call the api function simply 'rapids' (instead of _rapids_exec)

--- h2o-core/src/main/java/water/api/schemas3/RapidsSchemaV3.java
+++ h2o-core/src/main/java/water/api/schemas3/RapidsSchemaV3.java
@@ -7,13 +7,13 @@ import water.api.API;
  */
 public class RapidsSchemaV3<I extends Iced, S extends RapidsSchemaV3<I, S>> extends SchemaV3<I, S> {
 
-  @API(help="An Abstract Syntax Tree.", direction=API.Direction.INPUT)
+  @API(help="A Rapids AST expression", direction=API.Direction.INPUT, required=true)
   public String ast;
-
-  @API(help="[DEPRECATED] Key name to assign Frame results", direction=API.Direction.INPUT)
-  public String id;
 
   @API(help="Session key", direction=API.Direction.INPUT)
   public String session_id;
+
+  @API(help="[DEPRECATED] Key name to assign Frame results", direction=API.Direction.INPUT)
+  public String id;
 
 }
