7dfd671eb68b8fd4962b1d2a77db355b19e40008
angela0xdata <angela@h2o.ai>
2016-07-07 08:43:02 -0700

    PUBDEV-3015: Added gradient_epsilon and objective_epsilon to GLM booklet
    Added the gradient_epsilon and objective_epsilon options to the
    Parameters section in the GLM booklet. Also removed not in R and Python
    code samples that said the options would be available in a future
    release.

--- h2o-docs/src/booklets/v2_2015/source/GLM_Vignette_code_examples/glm_stopping_criteria.py
+++ h2o-docs/src/booklets/v2_2015/source/GLM_Vignette_code_examples/glm_stopping_criteria.py
@@ -4,7 +4,6 @@ h2o.init()
 h2o_df = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/airlines/allyears2k_headers.zip")
 
 #stops the model when we reach 10 active predictors
-#Objective epsilon and gradient epsilon stopping criteria will be added in a future release.
 model = H2OGeneralizedLinearEstimator(family = "binomial", lambda_search = True, max_active_predictors = 10)
 model.train(y = "IsDepDelayed", x = ["Year", "Origin"], training_frame = h2o_df)
 print(model)
