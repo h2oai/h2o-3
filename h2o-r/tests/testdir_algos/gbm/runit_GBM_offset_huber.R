feb799e46b38c5ea1e3b49809e504886dd3b45f2
Arno Candel <arno.candel@gmail.com>
2016-07-07 10:54:12 -0700

    PUBDEV-3050: Fix RUnit with new golden results for Huber regression after using the alpha-quantile.

--- h2o-r/tests/testdir_algos/gbm/runit_GBM_offset_huber.R
+++ h2o-r/tests/testdir_algos/gbm/runit_GBM_offset_huber.R
@@ -12,16 +12,16 @@ test <- function(h) {
   df = data.frame(Insurance,offset)
   hdf = as.h2o(df,destination_frame = "hdf")
 
-  hh = h2o.gbm(x = 1:3,y = "Claims",distribution ="huber",ntrees = 600,max_depth = 1,min_rows = 1,learn_rate = .1,offset_column = "offset",training_frame = hdf,min_split_improvement=0)
+  hh = h2o.gbm(x = 1:3,y = "Claims",distribution ="huber",ntrees = 600,max_depth = 1,min_rows = 1,learn_rate = .1,offset_column = "offset",training_frame = hdf,min_split_improvement=0,huber_alpha=0.9)
   ph = as.data.frame(h2o.predict(hh,newdata = hdf))
-  hh@model$init_f
-  mean(ph[,1])
-  min(ph[,1])
-  max(ph[,1])
+  print(hh@model$init_f)
+  print(mean(ph[,1]))
+  print(min(ph[,1]))
+  print(max(ph[,1]))
 
   expect_equal(17.37666, hh@model$init_f, tolerance=1e-4)
-  expect_equal(25.5357, mean(ph[,1]),tolerance=1e-4 )
-  expect_equal(0.6066683, min(ph[,1]) ,tolerance=1e-3)
-  expect_equal(60.99921, max(ph[,1]) ,tolerance=1e-3)
+  expect_equal(41.38122, mean(ph[,1]),tolerance=1e-4 )
+  expect_equal(-28.01114, min(ph[,1]) ,tolerance=1e-3)
+  expect_equal(145.0559, max(ph[,1]) ,tolerance=1e-3)
 }
 doTest("GBM offset Test: GBM w/ offset for gamma distribution", test)
