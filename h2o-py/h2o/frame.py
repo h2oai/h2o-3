d54c073291718d6096bcd70f806766f17123f054
Pasha Stetsenko <pasha@h2o.ai>
2016-07-15 10:32:30 -0700

    A different attempt at eager-izing the frame

--- h2o-py/h2o/frame.py
+++ h2o-py/h2o/frame.py
@@ -1454,9 +1454,13 @@ class H2OFrame(object):
 
     if values is None: values = "_"
     if group_by_frame is None: group_by_frame = "_"
+
+    # This code below is needed to ensure the frame (self) exists on the server. Without it, self._ex._cache.fill()
+    # fails with an assertion that ._id is None.
+    # This code should be removed / reworked once we have a more consistent strategy of dealing with frames.
     if self._ex._cache._id is None:
       self._ex._cache._id = "%s_%s" % (_py_tmp_key(), h2o.connection().session_id)
-      self._ex._eager_frame()
+      self._ex._eval_driver(True)
 
     if by is not None or group_by_frame is not "_":
       res = H2OFrame._expr(expr=ExprNode("h2o.impute", self, column, method, combine_method, by, group_by_frame, values))._frame()
