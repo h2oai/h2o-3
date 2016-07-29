35e21bcf2d30fd53cdb15d2533c62de83d6ab654
Arno Candel <arno.candel@gmail.com>
2016-07-28 18:44:38 -0700

    Overlap CPU and GPU work.

--- h2o-algos/src/main/java/hex/deepwater/DeepWaterImageIterator.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWaterImageIterator.java
@@ -1,5 +1,7 @@
 package hex.deepwater;
 
+import water.Futures;
+import water.H2O;
 import static water.gpu.util.img2pixels;
 
 import java.io.IOException;
@@ -16,53 +18,94 @@ public class DeepWaterImageIterator {
     start_index = 0;
     this.width = width;
     this.height = height;
-    data = new float[batch_size * width * height * 3];
-    label = new float[batch_size];
-    file = new String[batch_size];
+    data = new float[2][];
+    data[0] = new float[batch_size * width * height * 3];
+    data[1] = new float[batch_size * width * height * 3];
+    label = new float[2][];
+    label[0] = new float[batch_size];
+    label[1] = new float[batch_size];
+    file = new String[2][];
+    file[0] = new String[batch_size];
+    file[1] = new String[batch_size];
   }
 
-  public boolean Next() throws IOException {
+  //Helper for image conversion
+  //TODO: add cropping, distortion, rotation, etc.
+  public static class Conversion {
+    int width;
+    int height;
+    int channels;
+  }
+
+  class ImageConverter extends H2O.H2OCountedCompleter<ImageConverter> {
+    String _file;
+    float _label;
+    Conversion _conv;
+    float[] _destData;
+    float[] _destLabel;
+    String[] _destFile;
+    int _index;
+    public ImageConverter(int index, String file, float label, Conversion conv, float[] destData, float[] destLabel, String[] destFile) {
+      _index=index;
+      _file=file;
+      _label=label;
+      _conv=conv;
+      _destData=destData;
+      _destLabel=destLabel;
+      _destFile=destFile;
+    }
+
+    @Override
+    public void compute2() {
+      _destFile[_index] = _file;
+      _destLabel[_index] = _label;
+      try {
+        float[] tmp = img2pixels(_file, _conv.width, _conv.height); //TODO: directly fill _destData
+        final int len = tmp.length;
+        assert(len==_conv.width*_conv.height*_conv.channels);
+        final int start=_index*len;
+        for (int j = 0; j < len; j++)
+          _destData[start+j] = tmp[j];
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      tryComplete();
+    }
+  }
+
+  public boolean Next(Futures fs) throws IOException {
     if (start_index < val_num) {
-      if (start_index + batch_size > val_num) {
+      if (start_index + batch_size > val_num)
         start_index = val_num - batch_size;
-      }
-
-      for (int i = start_index; i < start_index + batch_size; i++) {
-        file[i - start_index] = img_lst.get(i);
-        label[i - start_index] = label_lst.get(i);
-        float[] tmp = img2pixels(img_lst.get(i), width, height);
-        for (int j = 0; j < width * height * 3; j++) {
-          data[(i - start_index) * width * height * 3 + j] = tmp[j];
-        }
-      }
-
+      // Multi-Threaded data preparation
+      Conversion conv = new Conversion();
+      conv.height=this.height;
+      conv.width=this.width;
+      conv.channels=3;
+      for (int i = 0; i < batch_size; i++)
+        fs.add(H2O.submitTask(new ImageConverter(i,img_lst.get(start_index+i),label_lst.get(start_index+i),conv,data[which],label[which],file[which])));
+      fs.blockForPending();
+      flip();
       start_index = start_index + batch_size;
-
       return true;
     } else {
       return false;
     }
   }
 
-  public String[] getFiles() {
-    return file;
-  }
+  public void flip() { assert(which==0 || which==1); which^=1; }
+  public String[] getFiles() { return file[which^1]; }
+  public float[] getData() { return data[which^1]; }
+  public float[] getLabel() { return label[which^1]; }
 
-  public float[] getData() {
-    return data;
-  }
-
-  public float[] getLabel() {
-    return label;
-  }
-
+  private int which;
   private int val_num;
   private int start_index;
   private int batch_size;
   private int width, height;
-  private float[] data;
-  private float[] label;
-  private String[] file;
+  private float[][] data;
+  private float[][] label;
+  private String[][] file;
   private ArrayList<String> img_lst;
   private ArrayList<Float> label_lst;
 }
