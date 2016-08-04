3fa48a78cd3e9e0c9d118657bce9c52297d64fb7
Arno Candel <arno.candel@gmail.com>
2016-08-04 13:00:43 -0700

    Fix typo.

--- h2o-algos/src/main/java/hex/deepwater/DeepWaterModelInfo.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWaterModelInfo.java
@@ -120,7 +120,7 @@ final public class DeepWaterModelInfo extends Iced {
       final String networkParms = parameters._network_parameters_file;
       if (networkParms != null && !networkParms.isEmpty()) {
         Log.info("Loading the model parameters file: " + networkParms);
-        _imageTrain.loadModel(networkParms);
+        _imageTrain.loadParam(networkParms);
       }
     } catch(Throwable t) {
       Log.err("Unable to initialize the native Deep Learning backend: " + t.getMessage());
