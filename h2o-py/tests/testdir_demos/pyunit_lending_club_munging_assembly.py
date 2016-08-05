d2daaa0c8fe4001e760436f8dc52897cd63ca617
Pasha Stetsenko <pasha@h2o.ai>
2016-08-04 18:31:21 -0700

    h2o.py -- provide typechecking for all top level h2o functions. Format docstrings. Fix several incorrect tests
    Summary:
    This is a first step toward making python module more robust by not accepting any arguments of wrong types from the users.
    Ref T5
    
    Test Plan: ./gradlew smokeTest + Jenkins
    
    Reviewers: mmalohlava, arno
    
    Maniphest Tasks: T5
    
    Differential Revision: https://test-4btqyg52txp2.phacility.com/D7

--- h2o-py/tests/testdir_demos/pyunit_lending_club_munging_assembly.py
+++ h2o-py/tests/testdir_demos/pyunit_lending_club_munging_assembly.py
@@ -16,7 +16,8 @@ def lending_club_munging_assembly():
   # lending-club munging assembly
   print("Import and Parse data")
 
-  types = {"int_rate":"String", "revol_util":"String", "emp_length":"String", "earliest_cr_line":"String", "issue_d":"String", "last_credit_pull_d":"Factor"}
+  types = {"int_rate": "string", "revol_util": "string", "emp_length": "string", "earliest_cr_line": "string",
+           "issue_d": "string", "last_credit_pull_d": "factor"}
 
   data = h2o.import_file(path=small_test, col_types=types)
   data[["int_rate","revol_util","emp_length"]].show()
