7ebdc924afa87eb746f0e0ba17f75957a05d303b
Pasha Stetsenko <pasha@h2o.ai>
2016-08-12 15:02:10 -0700

    In typechecks, create base MagicType for all special-cased type checking

--- h2o-py/h2o/utils/typechecks.py
+++ h2o-py/h2o/utils/typechecks.py
@@ -14,7 +14,7 @@ import tokenize
 from h2o.utils.compatibility import *  # NOQA
 from h2o.exceptions import H2OTypeError, H2OValueError
 
-__all__ = ("U", "I", "numeric",
+__all__ = ("U", "I", "numeric", "h2oframe", "pandas_dataframe", "numpy_ndarray",
            "assert_is_type", "assert_matches", "assert_satisfies", "test_type")
 
 
@@ -24,10 +24,12 @@ if PY2:
     _str_type = (str, _native_unicode)
     _int_type = (int, _native_long)
     _num_type = (int, _native_long, float)
+    _primitive_type = (str, int, float, bool, _native_unicode, _native_long)
 else:
     _str_type = str
     _int_type = int
     _num_type = (int, float)
+    _primitive_type = (str, int, float, bool, bytes)
 
 
 
@@ -39,14 +41,25 @@ def test_type(var, *args):
     This function is similar to :func:`assert_is_type`, however instead of raising an error when the variable does not
     match the provided type, it merely returns False.
     """
-    return _check_type(var, args)
+    return _check_type(var, U(*args))
 
 
 #-----------------------------------------------------------------------------------------------------------------------
 # Special types
 #-----------------------------------------------------------------------------------------------------------------------
 
-class U(object):
+class MagicType(object):
+    """Abstract "special" type."""
+
+    def check(self, var):
+        """Return True if the variable matches this type, and False otherwise."""
+
+    def __str__(self):
+        """Return string representing the name of this type."""
+        return "<%s>" % self.__class__.__name__
+
+
+class U(MagicType):
     """
     Union of types.
 
@@ -54,20 +67,27 @@ class U(object):
 
     This is a helper class that can be used in situations where unions are otherwise hard or impossible to declare.
     For example, one doesn't need a union type to in the test ``assert_is_type(x, int, str, None)``, but if we want to
-    have unions as dictionary keys, one has to use this class: ``assert_is_type(d, {str: U(int, bool, None)})``.
+    have unions as dictionary keys we can say: ``assert_is_type(d, {str: U(int, bool, None)})``.
     """
 
     def __init__(self, *types):
         """Create an object representing the union of ``*types``."""
         self._types = types
 
-    def __iter__(self):
-        """Iterate through the union's constituent types."""
-        for t in self._types:
-            yield t
+    def check(self, var):
+        """Return True if the variable matches this type, and False otherwise."""
+        return any(_check_type(var, tt) for tt in self._types)
+
+    def __str__(self):
+        res = [_get_type_name(tt) for tt in self._types]
+        if len(res) == 2 and "None" in res:
+            res.remove("None")
+            return "?" + res[0]
+        else:
+            return "|".join(res)
 
 
-class I(object):
+class I(MagicType):
     """
     Intersection of types.
 
@@ -80,14 +100,70 @@ class I(object):
         """Create an intersection of types."""
         self._types = types
 
-    def __iter__(self):
-        """Iterate through the intersection's constituent types."""
-        for t in self._types:
-            yield t
+    def check(self, var):
+        """Return True if the variable matches this type, and False otherwise."""
+        return all(_check_type(var, tt) for tt in self._types)
+
+    def __str__(self):
+        return "&".join(_get_type_name(tt) for tt in self._types)
 
 
-# We export this type, for convenience
+class _LazyClass(MagicType):
+    """
+    Helper class for lazy (on-demand) loading of some external classes.
+
+    The purpose of this class is to provide a way of testing against classes that either cannot be loaded immediately,
+    or may not even be present on user's system. This class must be taught how to load each class of interest, and
+    therefore should not be exposed to the user code. Instead use "singletons" ``h2oframe``, ``pandas_dataframe``,
+    ``numpy_ndarray`` (and perhaps others in future). Usage::
+
+        from h2o.utils.typechecks import assert_is_type, h2oframe, pandas_dataframe, numpy_ndarray
+        assert_is_type(fr, h2oframe, pandas_dataframe, numpy_ndarray)
+    """
+
+    def __init__(self, name):
+        assert name in {"H2OFrame", "pandas.DataFrame", "numpy.ndarray"}
+        self._name = name
+        # Initially this is None, but will contain the class object once the class is loaded. If the class cannot be
+        # loaded, this will be set to False.
+        self._class = None
+
+    def check(self, var):
+        """Return True if the variable matches this type, and False otherwise."""
+        if self._class is None: self._init()
+        return self._class and isinstance(var, self._class)
+
+    def _init(self):
+        if self._name == "H2OFrame":
+            from h2o import H2OFrame
+            self._class = H2OFrame
+        elif self._name == "pandas.DataFrame":
+            try:
+                # noinspection PyUnresolvedReferences
+                from pandas import DataFrame
+                self._class = DataFrame
+            except ImportError:
+                self._class = False
+        elif self._name == "numpy.ndarray":
+            try:
+                # noinspection PyUnresolvedReferences
+                from numpy import ndarray
+                self._class = ndarray
+            except ImportError:
+                self._class = False
+
+    def __str__(self):
+        return self._name
+
+
+
+
 numeric = U(int, float)
+"""Number, either integer or real."""
+
+h2oframe = _LazyClass("H2OFrame")
+pandas_dataframe = _LazyClass("pandas.DataFrame")
+numpy_ndarray = _LazyClass("numpy.ndarray")
 
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -152,12 +228,17 @@ def assert_is_type(var, *types, **kwargs):
     :raises H2OTypeError: if the argument is not of the desired type.
     """
     assert types, "The list of expected types was not provided"
-    if _check_type(var, types): return
+    if len(types) == 1:
+        if _check_type(var, types[0]): return
+        etn = _get_type_name(types[0])
+    else:
+        union_type = U(*types)
+        if _check_type(var, union_type): return
+        etn = str(union_type)
     assert set(kwargs).issubset({"message", "skip_frames"}), "Unexpected keyword arguments: %r" % kwargs
     vname = _retrieve_assert_arguments()[0]
     message = kwargs.get("message", None)
     skip_frames = kwargs.get("skip_frames", 1)
-    etn = _get_type_name(types)
     vtn = _get_type_name([type(var)])
     raise H2OTypeError(var_name=vname, var_value=var, var_type_name=vtn, exp_type_name=etn, message=message,
                        skip_frames=skip_frames)
@@ -266,91 +347,76 @@ def _retrieve_assert_arguments():
             return "arg",
 
 
-def _check_type(var, types):
+def _check_type(var, vtype):
     """
-    Return True if the variable is of one of the specified types, and False otherwise.
+    Return True if the variable is of the specified type, and False otherwise.
 
     :param var: variable to check
-    :param types: iterable of types
+    :param vtype: expected variable's type
     """
-    for tt in types:
-        if tt is None:
-            if var is None: return True
-        elif tt is str:
-            if isinstance(var, _str_type): return True
-        elif tt is int:
-            if isinstance(var, _int_type): return True
-        elif tt is numeric:
-            if isinstance(var, _num_type): return True
-        elif isinstance(tt, _str_type) or isinstance(tt, _int_type):
-            if var == tt: return True
-        elif isinstance(tt, U):
-            if _check_type(var, tt): return True
-        elif isinstance(tt, I):
-            if all(_check_type(var, [tttt]) for tttt in tt): return True
-        elif isinstance(tt, type):
-            if isinstance(var, tt): return True
-        elif isinstance(tt, list):
-            if isinstance(var, list) and all(_check_type(item, tt) for item in var): return True
-        elif isinstance(tt, set):
-            if isinstance(var, set) and all(_check_type(item, tt) for item in var): return True
-        elif isinstance(tt, tuple):
-            if isinstance(var, tuple) and len(tt) == len(var) and \
-               all(_check_type(var[i], [tt[i]]) for i in range(len(tt))): return True
-        elif isinstance(tt, dict):
-            ttkv = viewitems(tt)
-            if isinstance(var, dict) and all(_check_type(kv, ttkv) for kv in viewitems(var)): return True
-        else:
-            raise RuntimeError("Ivalid type %r in _check_type()" % tt)
-    return False
+    if vtype is None:
+        return var is None
+    if isinstance(vtype, _primitive_type):
+        return var == vtype
+    if vtype is str:
+        return isinstance(var, _str_type)
+    if vtype is int:
+        return isinstance(var, _int_type)
+    if vtype is numeric:
+        return isinstance(var, _num_type)
+    if isinstance(vtype, MagicType):
+        return vtype.check(var)
+    if isinstance(vtype, type):
+        return isinstance(var, vtype)
+    if isinstance(vtype, list):
+        elem_type = U(*vtype)
+        return isinstance(var, list) and all(_check_type(item, elem_type) for item in var)
+    if isinstance(vtype, set):
+        elem_type = U(*vtype)
+        return isinstance(var, set) and all(_check_type(item, elem_type) for item in var)
+    if isinstance(vtype, tuple):
+        return (isinstance(var, tuple) and len(vtype) == len(var) and
+                all(_check_type(var[i], vtype[i]) for i in range(len(vtype))))
+    if isinstance(vtype, dict):
+        ttkv = U(*viewitems(vtype))
+        return isinstance(var, dict) and all(_check_type(kv, ttkv) for kv in viewitems(var))
+    raise RuntimeError("Ivalid type %r in _check_type()" % vtype)
 
 
-def _get_type_name(types):
+def _get_type_name(vtype):
     """
     Return the name of the provided type(s).
 
-        _get_type_name([int]) == "integer"
-        _get_type_name([str]) == "string"
-        _get_type_name([tuple]) == "tuple"
-        _get_type_name([Exception]) == "Exception"
-        _get_type_name((int, float, bool)) == "integer|float|bool"
-        _get_type_name((H2OFrame, None)) == "?H2OFrame"
+        _get_type_name(int) == "integer"
+        _get_type_name(str) == "string"
+        _get_type_name(tuple) == "tuple"
+        _get_type_name(Exception) == "Exception"
+        _get_type_name(U(int, float, bool)) == "integer|float|bool"
+        _get_type_name(U(H2OFrame, None)) == "?H2OFrame"
     """
-    maybe_type = False
-    res = []
-    for tt in types:
-        if tt is None:
-            maybe_type = True
-        elif tt is str:
-            res.append("string")
-        elif tt is int:
-            res.append("integer")
-        elif tt is numeric:
-            res.append("numeric")
-        elif test_type(tt, str):
-            res.append('"%s"' % repr(tt)[1:-1])
-        elif test_type(tt, int):
-            res.append(str(tt))
-        elif isinstance(tt, U):
-            res.append(H2OTypeError._get_type_name(tt))
-        elif isinstance(tt, I):
-            res.append("&".join(H2OTypeError._get_type_name([tttt]) for tttt in tt))
-        elif isinstance(tt, type):
-            res.append(tt.__name__)
-        elif isinstance(tt, list):
-            res.append("list(%s)" % H2OTypeError._get_type_name(tt))
-        elif isinstance(tt, set):
-            res.append("set(%s)" % H2OTypeError._get_type_name(tt))
-        elif isinstance(tt, tuple):
-            res.append("(%s)" % ", ".join(H2OTypeError._get_type_name([item]) for item in tt))
-        elif isinstance(tt, dict):
-            res.append("dict(%s)" % ", ".join(
-                "%s: %s" % (H2OTypeError._get_type_name([tk]), H2OTypeError._get_type_name([tv]))
-                for tk, tv in tt.items()
-            ))
-        else:
-            raise RuntimeError("Unexpected `tt`: %r" % tt)
-    if maybe_type:
-        if not res: return "None"
-        res[0] = "?" + res[0]
-    return "|".join(res)
+    if vtype is None:
+        return "None"
+    if vtype is str:
+        return "string"
+    if vtype is int:
+        return "integer"
+    if vtype is numeric:
+        return "numeric"
+    if test_type(vtype, str):
+        return '"%s"' % repr(vtype)[1:-1]
+    if test_type(vtype, int):
+        return str(vtype)
+    if isinstance(vtype, MagicType):
+        return str(vtype)
+    if isinstance(vtype, type):
+        return vtype.__name__
+    if isinstance(vtype, list):
+        return "list(%s)" % _get_type_name(U(*vtype))
+    if isinstance(vtype, set):
+        return "set(%s)" % _get_type_name(U(*vtype))
+    if isinstance(vtype, tuple):
+        return "(%s)" % ", ".join(_get_type_name(item) for item in vtype)
+    if isinstance(vtype, dict):
+        return "dict(%s)" % ", ".join("%s: %s" % (_get_type_name(tk), _get_type_name(tv))
+                                      for tk, tv in viewitems(vtype))
+    raise RuntimeError("Unexpected `vtype`: %r" % vtype)
