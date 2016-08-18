b081f7238fb3c43dda04943dc28ab41cb64e20a9
Pasha Stetsenko <pasha@h2o.ai>
2016-08-18 10:41:08 -0700

    Remove some cruft from h2o.py; and delete the tox.ini file -- we should have a single configuration file, otherwise different tools get confused about which one to use...

--- h2o-py/h2o/h2o.py
+++ h2o-py/h2o/h2o.py
@@ -15,7 +15,7 @@ from h2o.backend import H2OConnection
 from h2o.backend import H2OLocalServer
 from h2o.exceptions import H2OConnectionError, H2OValueError
 from h2o.utils.shared_utils import deprecated, gen_header, is_list_of_lists, py_tmp_key, quoted, urlopen
-from h2o.utils.typechecks import U, assert_is_type, assert_satisfies, is_type, numeric
+from h2o.utils.typechecks import I, U, assert_is_type, assert_satisfies, is_type, numeric
 from .estimators.deeplearning import H2OAutoEncoderEstimator
 from .estimators.deeplearning import H2ODeepLearningEstimator
 from .estimators.estimator_base import H2OEstimator
@@ -33,7 +33,8 @@ from .model.model_base import ModelBase
 from .transforms.decomposition import H2OPCA
 from .transforms.decomposition import H2OSVD
 from .utils.debugging import *  # NOQA
-from .utils.compatibility import PY3, *  # NOQA
+from .utils.compatibility import *  # NOQA
+from .utils.compatibility import PY3
 warnings.simplefilter("always", DeprecationWarning)
 
 
@@ -207,8 +208,8 @@ def lazy_import(path):
 def _import(path):
     assert_is_type(path, str)
     j = api("GET /3/ImportFiles", data={"path": path})
-    if j['fails']: raise ValueError("ImportFiles of " + path + " failed on " + str(j['fails']))
-    return j['destination_frames']
+    if j["fails"]: raise ValueError("ImportFiles of " + path + " failed on " + str(j["fails"]))
+    return j["destination_frames"]
 
 
 def upload_file(path, destination_frame="", header=0, sep="", col_names=None, col_types=None,
@@ -439,7 +440,7 @@ def parse_setup(raw_frames, destination_frame="", header=0, separator="", column
     assert_is_type(raw_frames, str, [str])
     assert_is_type(destination_frame, None, str)
     assert_is_type(header, -1, 0, 1)
-    assert_is_type(separator, None, str)
+    assert_is_type(separator, None, I(str, lambda s: len(s) == 1))
     assert_is_type(column_names, [str], None)
     assert_is_type(column_types, [coltype], {str: coltype}, None)
     assert_is_type(na_strings, [natype], {str: natype}, None)
@@ -448,22 +449,15 @@ def parse_setup(raw_frames, destination_frame="", header=0, separator="", column
     if is_type(raw_frames, str): raw_frames = [raw_frames]
 
     # temporary dictionary just to pass the following information to the parser: header, separator
-    kwargs = {}
-    # set header
-    if header != (-1, 0, 1):
-        if header not in (-1, 0, 1): raise ValueError("header should be -1, 0, or 1")
-        kwargs["check_header"] = header
+    kwargs = {
+        "check_header": header,
+        "separator": ord(separator),
+        "source_frames": [quoted(id) for id in raw_frames],
+    }
 
-    # set separator
-    if separator:
-        if not is_type(separator, str) or len(separator) != 1:
-            raise ValueError("separator should be a single character string; got %r" % separator)
-        kwargs["separator"] = ord(separator)
-
-    kwargs["source_frames"] = [quoted(id) for id in raw_frames]
     j = api("POST /3/ParseSetup", data=kwargs)
     if "warnings" in j and j["warnings"]:
-        for w in j['warnings']:
+        for w in j["warnings"]:
             warnings.warn(w)
     # TODO: really should be url encoding...
     # TODO: clean up all this
@@ -609,16 +603,16 @@ def get_grid(grid_id):
     """
     assert_is_type(grid_id, str)
     grid_json = api("GET /99/Grids/%s" % grid_id)
-    models = [get_model(key['name']) for key in grid_json['model_ids']]
+    models = [get_model(key["name"]) for key in grid_json["model_ids"]]
     # get first model returned in list of models from grid search to get model class (binomial, multinomial, etc)
-    first_model_json = api("GET /3/Models/%s" % grid_json['model_ids'][0]['name'])['models'][0]
+    first_model_json = api("GET /3/Models/%s" % grid_json["model_ids"][0]["name"])["models"][0]
     gs = H2OGridSearch(None, {}, grid_id)
     gs._resolve_grid(grid_id, grid_json, first_model_json)
     gs.models = models
     hyper_params = {param: set() for param in gs.hyper_names}
     for param in gs.hyper_names:
         for model in models:
-            hyper_params[param].add(model.full_parameters[param]['actual_value'][0])
+            hyper_params[param].add(model.full_parameters[param]["actual_value"][0])
     hyper_params = {str(param): list(vals) for param, vals in hyper_params.items()}
     gs.hyper_params = hyper_params
     gs.model = model.__class__()
@@ -684,14 +678,12 @@ def remove(x):
         elif isinstance(xi, H2OEstimator):
             api("DELETE /3/DKV/%s" % xi.model_id)
             xi._id = None
-        elif is_type(xi, str):
+        else:
             # string may be a Frame key name part of a rapids session... need to call rm thru rapids here
             try:
                 rapids("(rm {})".format(xi))
             except:
                 api("DELETE /3/DKV/%s" % xi)
-        else:
-            raise ValueError('input to h2o.remove must one of: H2OFrame, H2OEstimator, or string')
 
 
 def remove_all():
@@ -763,7 +755,7 @@ def download_pojo(model, path="", get_jar=True):
     else:
         filepath = os.path.join(path, pojoname + ".java")
         print("Filepath: {}".format(filepath))
-        with open(filepath, 'wb') as f:
+        with open(filepath, "wb") as f:
             f.write(java.encode("utf-8"))
     if get_jar and path != "":
         url = h2oconn.make_url("h2o-genmodel.jar")
@@ -786,7 +778,7 @@ def download_csv(data, filename):
     assert_is_type(data, H2OFrame)
     assert_is_type(filename, str)
     url = h2oconn.make_url("DownloadDataset", 3) + "?frame_id={}&hex_string=false".format(data.frame_id)
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         f.write(urlopen()(url).read())
 
 
@@ -812,14 +804,14 @@ def download_all_logs(dirname=".", filename=None):
         else:
             headers = response.headers.headers
         for h in headers:
-            if 'filename=' in h:
+            if "filename=" in h:
                 filename = h.split("filename=")[1].strip()
                 break
     path = os.path.join(dirname, filename)
     response = opener(url).read()
 
     print("Writing H2O logs to " + path)
-    with open(path, 'wb') as f:
+    with open(path, "wb") as f:
         f.write(response)
     return path
 
@@ -856,7 +848,7 @@ def load_model(path):
     """
     assert_is_type(path, str)
     res = api("POST /99/Models.bin/%s" % "", data={"dir": path})
-    return get_model(res['models'][0]['model_id']['name'])
+    return get_model(res["models"][0]["model_id"]["name"])
 
 
 
