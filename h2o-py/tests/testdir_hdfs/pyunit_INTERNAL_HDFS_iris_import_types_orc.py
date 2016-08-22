bdce0b858732d294d96687d0ef232caf8d0e4b85
wendycwong <wendycwong@yahoo.com>
2016-08-22 14:41:27 -0700

    PUBDEV-1612: orc parser.  Added airline_05 test to HDFS.
    PUBDEV-1612: orc parser.  Remove all other tests but focused on the one that failed.
    
    PUBDEV-1612: orc parser.  Fixed bug in test HDFS_import_folder_airline_05_orc_large.py
    
    PUBDEV-1612: orc parser.  Fixed another bug in test pyunit_INTERNAL_HDFS_import_folder_airline_05_orc_large.py
    
    PUBDEV-1612: orc parser.  Added the last two failing tests in HDFS.
    
    PUBDEV-1612: orc parser.  The h2o.import_file takes different arguments under HDFS.  Put this into our test infrastructure for orc parser.
    
    PUBDEV-1612: orc parser.  Added HDFS tests back.  Two more bad tests to debug for tomorrow.
    
    PUBDEV-162: orc parser.  Reduce gradle.properties setting for Orc.
    
    PUBDEV-1612: orc parser.  Revert Hadoop and Orc Hive Exec versions back to what Michal has.  Cannot go any lower to accomodate J6.
    
    PUBDEV-1612: orc parser.  Changed to lower HDP version for gradle.properties.
    
    PUBDEV-1612: orc parser.  Added more printouts to test_HDFS.
    
    PUBDEV-1612: orc parser.  Fixed bug in test_HDFS.
    
    PUBDEV-1612: orc parser.  Revert Hive/Orc module versions.  Cannot find a way to make orc work in Java 6.  Michal is going to skip tests for Orc under Java6.
    
    PUBDEV-1612: orc parser.  Added more printings to aid debugging.
    
    PUBDEV-1612: orc parser.  Added more printing to test harness.
    
    PUBDEV-1612: orc parser.  Fixed bug in HDFS test: looking for the wrong warning string.
    
    PUBDEV-1612: orc parser.  Copy test from pyunit to HDFS.
    
    PUBDEV-1612: orc parser.  Fixed bug in dataset path.
    
    PUBDEV-1612: orc parser.  Fixed test harness bug.
    
    PUBDEV-1612: orc parser.  Done translating and debugging all Pyunit tests to HDFS!  Yeah.
    
    PUBDEV-1612: orc parser.  Implemented Michal method to detect early version of hive-exec.
    
    PUBDEV-1612: orc parser.  Added provisions in test_hdfs to not fail test but skip it if the hive-exec version is too old.
    
    PUBDEV-1612: orc parser.  Fixed bugs in hive-exec version checking.
    
    PUBDEV-1612: orc parser.  Completed adding all hive-exec version checks to all hdfs ORC parser tests.
    
    PUBDEV-1612: orc parser.  Removed h2o frame built to test hive-exec version.  Some hadoop versions do not like this.
    
    PUBDEV-1612: orc parser.  Changed cannary file to be orc_split_elim_orc which contains decimal and will throw an error for earlier hive-exec versions.
    
    PUBDEV-1612: orc parser.  Add hive-exec version check inside orcparser.java
    
    PUBDEV-1612: orc parser.  Remove the hive check in java side.
    
    PUBDEV-1612: orc parser.  Added tests to figure out multi-file parsing behavior when the data files are same/different types (orc/csv) generated from same/different datasets.
    
    PUBDEV-1612: orc parser. Removed change to original hdfs test files by mistake.
    
    PUBDEV-1612: orc parser.  Change except Exception, e to except Exception as err per suggestion by Michal.

--- h2o-py/tests/testdir_hdfs/pyunit_INTERNAL_HDFS_iris_import_types_orc.py
+++ h2o-py/tests/testdir_hdfs/pyunit_INTERNAL_HDFS_iris_import_types_orc.py
@@ -18,22 +18,28 @@ def hdfs_orc_parser():
     if hadoop_namenode_is_accessible:
         hdfs_name_node = pyunit_utils.hadoop_namenode()
 
-        numElements2Compare = 100
-        tol_time = 200
-        tol_numeric = 1e-5
+        if pyunit_utils.cannaryHDFSTest(hdfs_name_node, "/datasets/orc_parser/orc/orc_split_elim.orc"):
+            print("Your hive-exec version is too old.  Orc parser test {0} is "
+                  "skipped.".format("pyunit_INTERNAL_HDFS_iris_import_types_orc.py"))
+            pass
+        else:
 
-        hdfs_orc_file = "/datasets/orc_parser/orc/iris.orc"
-        url_orc = "hdfs://{0}{1}".format(hdfs_name_node, hdfs_orc_file)
-        hdfs_csv_file = "/datasets/orc_parser/csv/iris.csv"
-        url_csv = "hdfs://{0}{1}".format(hdfs_name_node, hdfs_csv_file)
+            numElements2Compare = 100
+            tol_time = 200
+            tol_numeric = 1e-5
 
-        h2oframe_csv = h2o.import_file(url_csv)
-        data_types = ['real', 'real', 'real', 'real', 'enum']
-        h2oframe_orc = h2o.import_file(url_orc, col_types = data_types)
+            hdfs_orc_file = "/datasets/orc_parser/orc/iris.orc"
+            url_orc = "hdfs://{0}{1}".format(hdfs_name_node, hdfs_orc_file)
+            hdfs_csv_file = "/datasets/orc_parser/csv/iris.csv"
+            url_csv = "hdfs://{0}{1}".format(hdfs_name_node, hdfs_csv_file)
 
-        # compare the two frames
-        assert pyunit_utils.compare_frames(h2oframe_orc, h2oframe_csv, numElements2Compare, tol_time, tol_numeric,
-                                           True), "H2O frame parsed from orc and csv files are different!"
+            h2oframe_csv = h2o.import_file(url_csv)
+            data_types = ['real', 'real', 'real', 'real', 'enum']
+            h2oframe_orc = h2o.import_file(url_orc, col_types = data_types)
+
+            # compare the two frames
+            assert pyunit_utils.compare_frames(h2oframe_orc, h2oframe_csv, numElements2Compare, tol_time, tol_numeric,
+                                               True), "H2O frame parsed from orc and csv files are different!"
     else:
         raise EnvironmentError
 
