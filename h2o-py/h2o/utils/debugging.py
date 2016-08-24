e7faf6bb49f83c82c0173cfd3c856838c5f90e3a
Pasha Stetsenko <pasha@h2o.ai>
2016-08-24 14:06:47 -0700

    Make h2o.init() understand max_mem_size/min_mem_size parameters of the form '10g' or '123M'

--- h2o-py/h2o/utils/debugging.py
+++ h2o-py/h2o/utils/debugging.py
@@ -195,7 +195,7 @@ sys.excepthook = _except_hook
 
 def _handle_soft_error(exc_type, exc_value, exc_tb):
     colorama.init()
-    err(Fore.LIGHTRED_EX + exc_type.__name__ + ": " + str(exc_value) + Style.RESET_ALL)
+    err((Fore.LIGHTRED_EX + "%s: %s" + Style.RESET_ALL) % (exc_type.__name__, exc_value))
 
     # Convert to the list of frames
     tb = exc_tb
