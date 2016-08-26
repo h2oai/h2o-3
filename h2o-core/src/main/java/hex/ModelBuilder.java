16a2b3cbf523229746b2d42c32c58601aa415fde
Arno Candel <arno.candel@gmail.com>
2016-08-26 08:36:46 -0700

    Fix order in CV (join main model only after the cv models are done scoring as they depend on temporary validation frames (they just add a weight column)).

--- h2o-core/src/main/java/hex/ModelBuilder.java
+++ h2o-core/src/main/java/hex/ModelBuilder.java
@@ -264,15 +264,16 @@ abstract public class ModelBuilder<M extends Model<M,P,O>, P extends Model.Param
       // Step 4: Run all the CV models and launch the main model
       H2O.H2OCountedCompleter mainMB = cv_buildModels(N, cvModelBuilders);
 
-      // wait for completion of the main model
-      if (mainMB!=null) mainMB.join();
-
       // Step 5: Score the CV models
       ModelMetrics.MetricBuilder mbs[] = cv_scoreCVModels(N, weights, cvModelBuilders);
+
+      // wait for completion of the main model
+      if (mainMB!=null) mainMB.join();
 
       // Step 6: Combine cross-validation scores; compute main model x-val
       // scores; compute gains/lifts
       cv_mainModelScores(N, mbs, cvModelBuilders);
+
 
       // Step 7: Clean up potentially created temp frames
       for (ModelBuilder mb : cvModelBuilders)
