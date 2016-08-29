45abc7e26793ddcdb2ad2ed057bd1d2e2cfd67da
SpencerA <spnrpa@gmail.com>
2016-08-29 02:48:18 -0700

    add getter for the classpool used by weaver use the iced_clazz's ClassLoader (instead of whichever loader loaded Weaver) * this allows for classes loaded in a variety of contexts to be used by H2O

--- h2o-core/src/main/java/water/Weaver.java
+++ h2o-core/src/main/java/water/Weaver.java
@@ -54,6 +54,8 @@ public class Weaver {
     } catch( NotFoundException nfe ) { throw new RuntimeException(nfe); }
   }
 
+  public static ClassPool getPool() { return _pool; }
+
   public static <T extends Freezable> Icer<T> genDelegate( int id, Class<T> clazz ) {
     Exception e2;
     try {
@@ -110,8 +112,8 @@ public class Weaver {
     CtClass icer_cc = _pool.getOrNull(icer_name); // Full Name Lookup of Icer
     if( icer_cc != null ) {
       synchronized( iced_clazz ) {
-        if( !icer_cc.isFrozen() ) icer_cc.toClass(Weaver.class.getClassLoader()); // Load class (but does not link & init)
-        return Class.forName(icer_name); // Found a pre-cooked Icer implementation
+        if( !icer_cc.isFrozen() )   icer_cc.toClass(iced_clazz.getClassLoader()); // Load class (but does not link & init)
+        return Class.forName(icer_name,true,iced_clazz.getClassLoader()); // Found a pre-cooked Icer implementation
       }
     }
 
@@ -139,8 +141,8 @@ public class Weaver {
       icer_cc = _pool.getOrNull(icer_name); // Retry under lock
       if( icer_cc != null ) return Class.forName(icer_name); // Found a pre-cooked Icer implementation
       icer_cc = genIcerClass(id,iced_cc,iced_clazz,icer_name,super_id,super_icer_cc,super_has_jfields);
-      icer_cc.toClass(Weaver.class.getClassLoader());               // Load class (but does not link & init)
-      return Class.forName(icer_name); // Initialize class now, before subclasses
+      icer_cc.toClass(iced_clazz.getClassLoader());               // Load class (but does not link & init)
+      return Class.forName(icer_name,true, iced_clazz.getClassLoader()); // Initialize class now, before subclasses
     }
   }
 
@@ -491,3 +493,4 @@ public class Weaver {
   }
 
 }
+
