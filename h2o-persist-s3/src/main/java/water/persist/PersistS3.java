6c0fdd743c29b699410916d9eaa18bc08fa6827b
mmalohlava <michal.malohlava@gmail.com>
2016-08-31 20:33:03 -0700

    [PUBDEV-3321] Allow arbitrary S3 connection end point.
    The fix:
      - exposes system property "sys.ai.h2o.persist.s3.endPoint" which can override default
      S3 connection end point. For example, `java -Dsys.ai.h2o.persist.s3.endPoint="https://localhost:9000" -jar h2o.jar`

--- h2o-persist-s3/src/main/java/water/persist/PersistS3.java
+++ h2o-persist-s3/src/main/java/water/persist/PersistS3.java
@@ -39,7 +39,7 @@ public final class PersistS3 extends Persist {
           try {
             H2OAWSCredentialsProviderChain c = new H2OAWSCredentialsProviderChain();
             ClientConfiguration cc = s3ClientCfg();
-            _s3 = new AmazonS3Client(c, cc);
+            _s3 = configureClient(new AmazonS3Client(c, cc));
           } catch( Throwable e ) {
             e.printStackTrace();
             StringBuilder msg = new StringBuilder();
@@ -299,6 +299,9 @@ public final class PersistS3 extends Persist {
   public final static String S3_MAX_HTTP_CONNECTIONS_PROP = SYSTEM_PROP_PREFIX + "persist.s3.maxHttpConnections";
   /** S3 force HTTP traffic */
   public final static String S3_FORCE_HTTP = SYSTEM_PROP_PREFIX + "persist.s3.force.http";
+  /** S3 end-point, for example: "https://localhost:9000 */
+  public final static String S3_END_POINT = SYSTEM_PROP_PREFIX + "persist.s3.endPoint";
+
 
   static ClientConfiguration s3ClientCfg() {
     ClientConfiguration cfg = new ClientConfiguration();
@@ -310,6 +313,13 @@ public final class PersistS3 extends Persist {
     if (prop.containsKey(S3_FORCE_HTTP)) cfg.setProtocol(Protocol.HTTP);
 
     return cfg;
+  }
+
+  static  AmazonS3Client configureClient(AmazonS3Client s3Client) {
+    if (System.getProperty(S3_END_POINT) != null) {
+      s3Client.setEndpoint(System.getProperty(S3_END_POINT));
+    }
+    return s3Client;
   }
 
   @Override public void delete(Value v) {
