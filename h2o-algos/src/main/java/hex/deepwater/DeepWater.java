e53c13c0fe2dce3cbd772d97aac772fcee44165c
Arno Candel <arno.candel@gmail.com>
2016-09-07 21:53:17 -0700

    Revert back to correct behavior of best model deletion.

--- h2o-algos/src/main/java/hex/deepwater/DeepWater.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWater.java
@@ -262,7 +262,7 @@ public class DeepWater extends ModelBuilder<DeepWaterModel,DeepWaterParameters,D
           model.unlock(_job);
           if (model.actual_best_model_key != null) {
             assert (model.actual_best_model_key != model._key);
-            ((DeepWaterModel)model.actual_best_model_key.get()).delete();
+            DKV.remove(model.actual_best_model_key); //don't call model.delete() as many things are shared with the main model
           }
         }
       }
