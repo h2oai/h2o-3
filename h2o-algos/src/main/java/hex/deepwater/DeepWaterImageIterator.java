6e74691d86a6aeb10e7954d3fbdbf4e181bac268
Arno Candel <arno.candel@gmail.com>
2016-09-20 18:15:33 -0700

    Add support for training on general H2O Frames (numeric/enum), with automatic standardization, NA imputation, one-hot encoding, etc.

--- h2o-algos/src/main/java/hex/deepwater/DeepWaterImageIterator.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWaterImageIterator.java
@@ -8,28 +8,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 
-public class DeepWaterImageIterator {
+public class DeepWaterImageIterator extends DeepWaterIterator {
 
   public DeepWaterImageIterator(ArrayList<String> img_lst, ArrayList<Float> lable_lst, float[] meanData, int batch_size, int width, int height, int channels, boolean cache) throws IOException {
+    super(batch_size, width*height*channels, cache);
     _img_lst = img_lst;
     _label_lst = lable_lst;
     _meanData = meanData;
-    _batch_size = batch_size;
-    _val_num = img_lst.size();
-    _start_index = 0;
+    _num_obs = img_lst.size();
     _width = width;
     _height = height;
     _channels = channels;
-    _data = new float[2][];
-    _data[0] = new float[batch_size * width * height * channels];
-    _data[1] = new float[batch_size * width * height * channels];
-    _label = new float[2][];
-    _label[0] = new float[batch_size];
-    _label[1] = new float[batch_size];
     _file = new String[2][];
     _file[0] = new String[batch_size];
     _file[1] = new String[batch_size];
-    _cache = cache;
   }
 
   public static class Dimensions extends Iced<Dimensions> implements Comparable<Dimensions> {
@@ -65,7 +57,6 @@ public class DeepWaterImageIterator {
     float[] _destData;
     float[] _meanData;
     float[] _destLabel;
-    String[] _destFile;
     int _index;
     boolean _cache;
     public ImageConverter(int index, String file, float label, Conversion conv, float[] destData, float[] meanData, float[] destLabel, String[] destFile, boolean cache) {
@@ -76,19 +67,17 @@ public class DeepWaterImageIterator {
       _destData=destData;
       _meanData=meanData;
       _destLabel=destLabel;
-      _destFile=destFile;
       _cache = cache;
     }
 
     @Override
     public void compute2() {
-      _destFile[_index] = _file;
       _destLabel[_index] = _label;
       try {
         final int start=_index*_conv.len();
         Key imgKey = Key.make(_file + DeepWaterModel.CACHE_MARKER);
         boolean status = false;
-        if (_cache) {
+        if (_cache) { //try to get the data from cache first
           IcedImage icedIm = DKV.getGet(imgKey);
           if (icedIm != null && icedIm._dim.compareTo(_conv._dim)==0) {
             // place the cached image into the right minibatch slot
@@ -112,16 +101,16 @@ public class DeepWaterImageIterator {
   }
 
   public boolean Next(Futures fs) throws IOException {
-    if (_start_index < _val_num) {
-      if (_start_index + _batch_size > _val_num)
-        _start_index = _val_num - _batch_size;
+    if (_start_index < _num_obs) {
+      if (_start_index + _batch_size > _num_obs)
+        _start_index = _num_obs - _batch_size;
       // Multi-Threaded data preparation
       Conversion conv = new Conversion();
       conv._dim._height=this._height;
       conv._dim._width=this._width;
       conv._dim._channels=this._channels;
       for (int i = 0; i < _batch_size; i++)
-        fs.add(H2O.submitTask(new ImageConverter(i, _img_lst.get(_start_index +i), _label_lst ==null?Float.NaN: _label_lst.get(_start_index +i),conv, _data[_which], _meanData, _label[_which], _file[_which], _cache)));
+        fs.add(H2O.submitTask(new ImageConverter(i, _img_lst.get(_start_index +i), _label_lst ==null?Float.NaN: _label_lst.get(_start_index +i),conv, _data[which()], _meanData, _label[which()], _file[which()], _cache)));
       fs.blockForPending();
       flip();
       _start_index = _start_index + _batch_size;
@@ -131,21 +120,13 @@ public class DeepWaterImageIterator {
     }
   }
 
-  public void flip() { assert(_which ==0 || _which ==1); _which ^=1; }
-  public String[] getFiles() { return _file[_which ^1]; }
-  public float[] getData() { return _data[_which ^1]; }
-  public float[] getLabel() { return _label[_which ^1]; }
+  public String[] getFiles() { return _file[which() ^1]; }
 
-  private int _which; //0 or 1
-  final private int _val_num;
+  final private int _num_obs;
   private int _start_index;
-  final private int _batch_size;
   final private int _width, _height, _channels;
-  final private float[][] _data;
   final private float[] _meanData; //mean image
-  final private float[][] _label;
   final private String[][] _file;
   final private ArrayList<String> _img_lst;
   final private ArrayList<Float> _label_lst;
-  private final boolean _cache;
 }
