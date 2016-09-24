69aada263dcca5eb8c8807ec3bca1cfdd1acf757
Arno Candel <arno.candel@gmail.com>
2016-09-24 11:45:41 -0700

    Cleanup loading or user-defined JSONs.

--- h2o-py/tests/testdir_algos/deepwater/pyunit_custom_lenet_mnist_deepwater.py
+++ h2o-py/tests/testdir_algos/deepwater/pyunit_custom_lenet_mnist_deepwater.py
@@ -13,13 +13,11 @@ def lenet(num_classes):
     # first conv
     conv1 = mx.symbol.Convolution(data=data, kernel=(5,5), num_filter=20)
     tanh1 = mx.symbol.Activation(data=conv1, act_type="tanh")
-    pool1 = mx.symbol.Pooling(data=tanh1, pool_type="max",
-                              kernel=(2,2), stride=(2,2))
+    pool1 = mx.symbol.Pooling(data=tanh1, pool_type="max", kernel=(2,2), stride=(2,2))
     # second conv
     conv2 = mx.symbol.Convolution(data=pool1, kernel=(5,5), num_filter=50)
     tanh2 = mx.symbol.Activation(data=conv2, act_type="tanh")
-    pool2 = mx.symbol.Pooling(data=tanh2, pool_type="max",
-                              kernel=(2,2), stride=(2,2))
+    pool2 = mx.symbol.Pooling(data=tanh2, pool_type="max", kernel=(2,2), stride=(2,2))
     # first fullc
     flatten = mx.symbol.Flatten(data=pool2)
     fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=500)
@@ -50,13 +48,13 @@ def deepwater_custom_lenet_mnist():
   model = H2ODeepWaterEstimator(epochs=50, rate=1e-3, mini_batch_size=32,
                                 network='user', 
 				network_definition_file="/tmp/symbol_lenet-py.json", 
-				image_shape=[28,28], channels=1,
-                                score_interval=0, train_samples_per_iteration=1000,
-                                gpu=True)
+				#image_shape=[28,28], channels=1,
+                                score_interval=0, train_samples_per_iteration=1000)
+                                
   model.train(x=[0],y=resp, training_frame=train, validation_frame=test)
   model.show()
-  error = model.model_performance(train=True).mean_per_class_error()
-  assert error < 0.1, "mean classification error is too high : " + str(error)
+  error = model.model_performance(valid=True).mean_per_class_error()
+  assert error < 0.1, "mean classification error on validation set is too high : " + str(error)
 
 if __name__ == "__main__":
   pyunit_utils.standalone_test(deepwater_custom_lenet_mnist)
