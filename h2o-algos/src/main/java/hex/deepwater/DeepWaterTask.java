7a1512f091789073bcbdb62520e4c7a9c2cb9a78
Arno Candel <arno.candel@gmail.com>
2016-09-24 11:19:52 -0700

    Set learning rate and momentum.

--- h2o-algos/src/main/java/hex/deepwater/DeepWaterTask.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWaterTask.java
@@ -161,6 +161,11 @@ public class DeepWaterTask extends FrameTask<DeepWaterTask> {
 
 //        if(!_localmodel.get_params()._quiet_mode)
 //          Log.info("Trained " + n + " samples. Training on " + Arrays.toString(((DeepWaterImageIterator)iter).getFiles()));
+        float rate = _localmodel.get_params().rate((double) n);
+        _localmodel.backend.setParameter("learning_rate", rate);
+        float momentum = _localmodel.get_params().momentum((double) n);
+        _localmodel.backend.setParameter("momentum", momentum);
+        //fork off GPU work, but let the iterator.Next() wait on completion before swapping again
 
         _localmodel._backend.setParameter(_localmodel._model, "learning_rate", _localmodel.get_params().learningRate((double) n));
         _localmodel._backend.setParameter(_localmodel._model, "momentum", _localmodel.get_params().momentum((double) n));
