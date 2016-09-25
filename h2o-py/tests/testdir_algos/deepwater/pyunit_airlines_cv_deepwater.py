1a52c26bdffa00310cd7b130c995c93fc5497998
Arno Candel <arno.candel@gmail.com>
2016-09-25 11:45:17 -0700

    cosmetics.

--- h2o-py/tests/testdir_algos/deepwater/pyunit_airlines_cv_deepwater.py
+++ h2o-py/tests/testdir_algos/deepwater/pyunit_airlines_cv_deepwater.py
@@ -16,12 +16,20 @@ def cv_airlines():
   predictors = ["Year","Month","DayofMonth","DayOfWeek","CRSDepTime","CRSArrTime","UniqueCarrier","FlightNum"]
   response_col = "IsDepDelayed"
 
-  dl = H2ODeepWaterEstimator(nfolds=3, hidden=[200,200], activation="Rectifier",  # network (fully-connected)
-	 		     hidden_dropout_ratios=[0.1,0.1], input_dropout_ratio=0.0,  # regularization
-			     rate=5e-3, rate_annealing=1e-6,  # learning rate
-			     momentum_start=0.9, momentum_stable=0.99, momentum_ramp=1e7,  # momentum
-		             epochs=100, stopping_rounds=4, train_samples_per_iteration=30000,  # early stopping
-			     mini_batch_size=32, score_duty_cycle=0.25, score_interval=1)  # score often for early stopping
+  dl = H2ODeepWaterEstimator(# cross-validation
+			     nfolds=3,                                                          
+			     # network (fully-connected)
+			     hidden=[200,200], activation="Rectifier",                          
+			     # regularization
+	 		     hidden_dropout_ratios=[0.1,0.1], input_dropout_ratio=0.0,          
+	 		     # learning rate
+			     rate=5e-3, rate_annealing=1e-6,                                    
+			     # momentum
+			     momentum_start=0.9, momentum_stable=0.99, momentum_ramp=1e7,       
+		             # early stopping
+		             epochs=100, stopping_rounds=4, train_samples_per_iteration=30000,  
+			     # score often for early stopping
+			     mini_batch_size=32, score_duty_cycle=0.25, score_interval=1)       
 
   dl.train(x=predictors, y=response_col, training_frame=df)
   print(dl.show())
