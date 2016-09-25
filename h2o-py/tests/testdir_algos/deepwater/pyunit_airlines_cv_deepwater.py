cc827d30e853ed461a66c561da7985e8b6d31cce
Arno Candel <arno.candel@gmail.com>
2016-09-25 11:40:59 -0700

    Use early stopping.

--- h2o-py/tests/testdir_algos/deepwater/pyunit_airlines_cv_deepwater.py
+++ h2o-py/tests/testdir_algos/deepwater/pyunit_airlines_cv_deepwater.py
@@ -18,10 +18,10 @@ def cv_airlines():
 
   dl = H2ODeepWaterEstimator(nfolds=3, hidden=[200,200], activation="Rectifier",  # network (fully-connected)
 	 		     hidden_dropout_ratios=[0.1,0.1], input_dropout_ratio=0.0,  # regularization
-		             epochs=10, train_samples_per_iteration=100000,  # auto-tuning
 			     rate=5e-3, rate_annealing=1e-6,  # learning rate
 			     momentum_start=0.9, momentum_stable=0.99, momentum_ramp=1e7,  # momentum
-			     mini_batch_size=32, score_duty_cycle=0.2)  # scoring
+		             epochs=100, stopping_rounds=4, train_samples_per_iteration=30000,  # early stopping
+			     mini_batch_size=32, score_duty_cycle=0.25, score_interval=1)  # score often for early stopping
 
   dl.train(x=predictors, y=response_col, training_frame=df)
   print(dl.show())
