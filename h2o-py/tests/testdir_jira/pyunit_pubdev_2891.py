c26fcc02c132710955043d1b7ba229f5939866fd
Pasha Stetsenko <pasha@h2o.ai>
2016-09-28 15:00:39 -0700

    [PUBDEV-3412] Fix list -> frame constructor to match Panda's behavior (#247)
    Note: this is a breaking change! However after some discussion in #api-updates, it was decided that the old behavior is too counterintuitive, and thus the change is warranted despite any potential problems it might bring.
    
    * [PUBDEV-3412] make h2o.H2OFrame([1, 2, 3]) create a frame with 3 rows instead of 3 columns
    * Fixing pyunit tests that assumed that H2OFrame(list) should expand into a row

--- h2o-py/tests/testdir_jira/pyunit_pubdev_2891.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_2891.py
@@ -1,27 +1,25 @@
-import sys
-sys.path.insert(1,"../../")
 import h2o
 from tests import pyunit_utils
 
 def pubdev_2891():
 
     #check header is respected
-    names = ["a", "b","c","d"]
-    python_obj = [names,[1,1,1,1]]
-    the_frame_1 = h2o.H2OFrame.from_python(python_obj,header=1)
+    names = ["a", "b", "c", "d"]
+    python_obj = [names, [1, 1, 1, 1]]
+    the_frame_1 = h2o.H2OFrame.from_python(python_obj, header=1)
     print(the_frame_1)
     assert the_frame_1.col_names == names
 
     #check the dimension of the frame
-    python_obj = ["a", "b","c","asdfasdf"]
+    python_obj = ["a", "b", "c", "asdfasdf"]
     the_frame_1 = h2o.H2OFrame(python_obj)
     print(the_frame_1)
-    pyunit_utils.check_dims_values(python_obj, the_frame_1, rows=1, cols=4)
+    pyunit_utils.check_dims_values(python_obj, the_frame_1, rows=4, cols=1)
 
     the_frame_1 = h2o.H2OFrame.from_python(python_obj, header=1)
     print(the_frame_1)
-    assert the_frame_1.names == python_obj
-    assert the_frame_1.nrow == 0
+    assert the_frame_1.names == ["a"]
+    assert the_frame_1.nrows == 3
 
 
 if __name__ == "__main__":
