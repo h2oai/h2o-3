571bc82fb900b69bef543de3a46031e1a89dc8fa
Vlad Patryshev <vpatryshev@gmail.com>
2016-09-28 18:21:29 -0700

    Wrote a set of unittests for ArrayUtils Fixed ArrayUtils (remove() was broken) Added more remove() versions, for a bunch of primitives Changed one of Frame.remove()s so it uses ArrayUtils.remove() (others to follow) Addedd a test for this Frame.remove() Blocked a couple of NPEs in SVMLightFVecParseWriter (they were happening) in Frame.get() added a better exception message for the case index is out of bounds (happens in a sparse example, will have to investigate it carefully) merged with latest master

--- h2o-core/src/test/java/water/TestUtil.java
+++ h2o-core/src/test/java/water/TestUtil.java
@@ -25,6 +25,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 @Ignore("Support for tests, but no actual tests here")
@@ -159,7 +160,7 @@ public class TestUtil extends Iced {
   /** Hunt for test files in likely places.  Null if cannot find.
    *  @param fname Test filename
    *  @return      Found file or null */
-  protected static File find_test_file_static(String fname) {
+  public static File find_test_file_static(String fname) {
     // When run from eclipse, the working directory is different.
     // Try pointing at another likely place
     File file = new File(fname);
@@ -207,27 +208,45 @@ public class TestUtil extends Iced {
     return find_test_file_static(fname);
   }
 
+  private static void checkFileEntry(String name, File file) {
+    assertNotNull("File not found: " + name, file);
+    assertTrue("File should exist: " + name, file.exists());
+  }
+
+  private static void checkFile(String name, File file) {
+    checkFileEntry(name, file);
+    assertTrue("Expected a readable file: " + name, file.canRead());
+  }
+
+  private static File[] checkFolder(String name, File folder) {
+    checkFileEntry(name, folder);
+    assertTrue("Expected a folder: " + name, folder.isDirectory());
+    File[] files = folder.listFiles();
+    assertNotNull("No files found in " + folder, files);
+    return files;
+  }
+
   /** Find & parse a CSV file.  NPE if file not found.
    *  @param fname Test filename
    *  @return      Frame or NPE */
   public static Frame parse_test_file( String fname ) { return parse_test_file(Key.make(),fname); }
   public static Frame parse_test_file( Key outputKey, String fname) {
     File f = find_test_file_static(fname);
-    assert f != null && f.exists():" file not found: " + fname;
+    checkFile(fname, f);
     NFSFileVec nfs = NFSFileVec.make(f);
     return ParseDataset.parse(outputKey, nfs._key);
   }
 
   protected Frame parse_test_file( Key outputKey, String fname , boolean guessSetup) {
     File f = find_test_file(fname);
-    assert f != null && f.exists():" file not found: " + fname;
+    checkFile(fname, f);
     NFSFileVec nfs = NFSFileVec.make(f);
     return ParseDataset.parse(outputKey, new Key[]{nfs._key}, true, ParseSetup.guessSetup(new Key[]{nfs._key},false,1));
   }
 
   protected Frame parse_test_file( String fname, String na_string, int check_header, byte[] column_types ) {
     File f = find_test_file_static(fname);
-    assert f != null && f.exists():" file not found: " + fname;
+    checkFile(fname, f);
     NFSFileVec nfs = NFSFileVec.make(f);
 
     Key[] res = {nfs._key};
@@ -262,9 +281,7 @@ public class TestUtil extends Iced {
    *  @return      Frame or NPE */
   protected Frame parse_test_folder( String fname ) {
     File folder = find_test_file(fname);
-    assert folder.isDirectory();
-    File[] files = folder.listFiles();
-    if (files != null) {
+    File[] files = checkFolder(fname, folder);
       Arrays.sort(files);
       ArrayList<Key> keys = new ArrayList<>();
       for( File f : files )
@@ -273,7 +290,6 @@ public class TestUtil extends Iced {
       Key[] res = new Key[keys.size()];
       keys.toArray(res);
       return ParseDataset.parse(Key.make(), res);
-    } else return null;
   }
 
 
@@ -286,8 +302,7 @@ public class TestUtil extends Iced {
    */
   protected static Frame parse_test_folder( String fname, String na_string, int check_header, byte[] column_types ) {
     File folder = find_test_file_static(fname);
-    assert folder != null && folder.isDirectory():"Folder "+fname+" is not a directory: folder="+folder;
-    File[] files = folder.listFiles();
+    File[] files = checkFolder(fname, folder);
     Arrays.sort(files);
     ArrayList<Key> keys = new ArrayList<>();
     for( File f : files )
@@ -408,8 +423,8 @@ public class TestUtil extends Iced {
   }
 
   public static boolean[] checkProjection(Frame expected, Frame actual, double threshold, boolean[] flipped) {
-    assert expected.numCols() == actual.numCols();
-    assert expected.numCols() == flipped.length;
+    assertEquals("Number of columns", expected.numCols(), actual.numCols());
+    assertEquals("Number of columns in flipped", expected.numCols(), flipped.length);
     int nfeat = (int) expected.numRows();
     int ncomp = expected.numCols();
 
