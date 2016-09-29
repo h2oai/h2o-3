4a6829eb680a61d0ef903db84b3c993d099f5e19
Pasha Stetsenko <pasha@h2o.ai>
2016-09-29 15:18:49 -0700

    Added a test case for passing a pandas dataframe directly into H2OFrame's constructor

--- h2o-py/tests/testdir_misc/pyunit_frame_from_pandas.py
+++ h2o-py/tests/testdir_misc/pyunit_frame_from_pandas.py
@@ -10,11 +10,14 @@ def test_pandas_to_h2oframe():
     pddf = pd.DataFrame({"one": [4, 6, 1], "two": ["a", "b", "cde"], "three": [0, None, 1]})
     h2odf1 = h2o.H2OFrame.from_python(pddf)
     h2odf2 = h2o.H2OFrame.from_python(pddf, column_names=["A", "B", "C"])
+    h2odf3 = h2o.H2OFrame(pddf)
 
     assert h2odf1.shape == pddf.shape
     assert h2odf1.columns == list(pddf.columns)
     assert h2odf2.shape == pddf.shape
     assert h2odf2.columns == ["A", "B", "C"]
+    assert h2odf3.shape == pddf.shape
+    assert h2odf3.columns == list(pddf.columns)
 
 
 if __name__ == "__main__":
