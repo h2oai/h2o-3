d3c37603630eeb793a09fff93db837ad0acc5df9
Pasha Stetsenko <pasha@h2o.ai>
2016-10-05 13:52:00 -0700

    Move ModelMojo class (formerly MojoStreamWriter) into a separate file, and same for all the descendants. The purpose is to reduce the complexity of the Model classImprove documentation for the class.

---
+++ h2o-algos/src/main/java/hex/tree/SharedTreeMojo.java
@@ -0,0 +1,45 @@
+package hex.tree;
+
+import hex.ModelMojo;
+import water.DKV;
+import water.Key;
+import water.Value;
+import water.exceptions.H2OKeyNotFoundArgumentException;
+
+import java.io.IOException;
+
+/**
+ * Shared Mojo definition file for DRF and GBM models.
+ */
+public class SharedTreeMojo<M extends SharedTreeModel<M, P, O>,
+                            P extends SharedTreeModel.SharedTreeParameters,
+                            O extends SharedTreeModel.SharedTreeOutput> extends ModelMojo<M, P, O> {
+
+  public SharedTreeMojo(M model) {
+    super(model);
+  }
+
+  @Override
+  protected void writeExtraModelInfo() throws IOException {
+    writeln("n_trees = " + model._output._ntrees);
+  }
+
+  @Override
+  protected void writeModelData() throws IOException {
+    assert model._output._treeKeys.length == model._output._ntrees;
+    int nclasses = model._output.nclasses();
+    int ntreesPerClass = model.binomialOpt() && nclasses == 2 ? 1 : nclasses;
+    for (int i = 0; i < model._output._ntrees; i++) {
+      for (int j = 0; j < ntreesPerClass; j++) {
+        Key<CompressedTree> key = model._output._treeKeys[i][j];
+        Value ctVal = key != null ? DKV.get(key) : null;
+        if (ctVal == null)
+          throw new H2OKeyNotFoundArgumentException("CompressedTree " + key + " not found");
+        CompressedTree ct = ctVal.get();
+        assert ct._nclass == nclasses;
+        // assume ct._seed is useless and need not be persisted
+        writeBinaryFile(String.format("trees/t%02d_%03d.bin", j, i), ct._bits);
+      }
+    }
+  }
+}
---
+++ h2o-algos/src/main/java/hex/tree/drf/DrfModelMojo.java
@@ -0,0 +1,19 @@
+package hex.tree.drf;
+
+import hex.tree.SharedTreeMojo;
+
+import java.io.IOException;
+
+/**
+ * Mojo definition for DRF model.
+ */
+public class DrfModelMojo extends SharedTreeMojo<DRFModel, DRFModel.DRFParameters, DRFModel.DRFOutput> {
+
+  public DrfModelMojo(DRFModel model) { super(model); }
+
+  @Override
+  protected void writeExtraModelInfo() throws IOException {
+    super.writeExtraModelInfo();
+    writeln("binomial_double_trees = " + model._parms._binomial_double_trees);
+  }
+}
