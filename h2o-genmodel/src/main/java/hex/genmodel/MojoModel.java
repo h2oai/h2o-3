d052e3d0dfd86a99d558823b370b5797b15d29be
Pasha Stetsenko <pasha@h2o.ai>
2016-10-05 14:01:03 -0700

    'Gradient Boosting Machine' is the new name for GBM models

--- h2o-genmodel/src/main/java/hex/genmodel/MojoModel.java
+++ h2o-genmodel/src/main/java/hex/genmodel/MojoModel.java
@@ -63,6 +63,7 @@ abstract public class MojoModel extends GenModel {
             case "Distributed Random Forest":
                 return new DrfModel(mojoReader, info, columns, domains);
             case "Gradient Boosting Method":
+            case "Gradient Boosting Machine":
                 return new GbmModel(mojoReader, info, columns, domains);
             default:
                 throw new IOException("Unsupported algorithm " + algo + " for raw models.");
