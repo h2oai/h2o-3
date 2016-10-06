435339c6f034cbe8c55fdc18d060824351ea24e6
Nick Karpov <nick@nickkarpov.com>
2016-10-06 15:41:34 -0700

    ability to add cookies to requests made by r/py clients (#286)
    * adding ability to add cookies to requests made by r/py clients
    
    * proper type check & update doc string
    
    * can pass single str or list of

--- h2o-r/h2o-package/R/classes.R
+++ h2o-r/h2o-package/R/classes.R
@@ -60,6 +60,7 @@ setRefClass("H2OConnectionMutableState",
 #' @slot username Username to login with.
 #' @slot password Password to login with.
 #' @slot cluster_id Cluster to login to. Used for Steam connections
+#' @slot cookies Cookies to add to request
 #' @slot mutable An \code{H2OConnectionMutableState} object to hold the mutable state for the H2O connection.
 #' @aliases H2OConnection
 #' @export
@@ -68,6 +69,7 @@ setClass("H2OConnection",
                         https="logical", insecure="logical",
                         username="character", password="character",
                         cluster_id="numeric",
+                        cookies="character",
                         mutable="H2OConnectionMutableState"),
          prototype(ip           = NA_character_,
                    port         = NA_integer_,
@@ -77,6 +79,7 @@ setClass("H2OConnection",
                    username     = NA_character_,
                    password     = NA_character_,
                    cluster_id   = NA_integer_,
+                   cookies      = NA_character_,
                    mutable      = new("H2OConnectionMutableState")))
 
 setClassUnion("H2OConnectionOrNULL", c("H2OConnection", "NULL"))
