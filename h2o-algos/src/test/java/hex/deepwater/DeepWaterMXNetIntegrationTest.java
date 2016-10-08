ea25cc2ea686e282c4eafbee8e7560bc527a2f7d
Arno Candel <arno.candel@gmail.com>
2016-10-07 18:46:21 -0700

    Cleanup detection of availability of Deep Water backends.

--- h2o-algos/src/test/java/hex/deepwater/DeepWaterMXNetIntegrationTest.java
+++ h2o-algos/src/test/java/hex/deepwater/DeepWaterMXNetIntegrationTest.java
@@ -21,11 +21,11 @@ public class DeepWaterMXNetIntegrationTest extends DeepWaterAbstractIntegrationT
 
   @Before
   public void setUp() throws Exception {
-    backend = createDeepWaterBackend(DeepWaterParameters.Backend.mxnet.toString());
+    Assume.assumeTrue(DeepWater.haveBackend(DeepWaterParameters.Backend.mxnet));
   }
   @BeforeClass
   static public void _preconditionDeepWater() { // NOTE: the `_` force execution of this check after setup
-    Assume.assumeTrue(createDeepWaterBackend(DeepWaterParameters.Backend.mxnet.toString())!=null);
+    Assume.assumeTrue(backend != null);
   }
 
   // This test has nothing to do with H2O - Pure integration test of deepwater/backends/mxnet
