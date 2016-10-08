d2d7d5d497bf0a84dc767909d8fe35bddbbc6105
Pasha Stetsenko <pasha@h2o.ai>
2016-10-07 18:11:05 -0700

    Read archetypes from GlrmMojo

--- h2o-genmodel/src/main/java/hex/genmodel/algos/glrm/GlrmMojoReader.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/glrm/GlrmMojoReader.java
@@ -3,6 +3,7 @@ package hex.genmodel.algos.glrm;
 import hex.genmodel.ModelMojoReader;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 /**
  */
@@ -10,8 +11,20 @@ public class GlrmMojoReader extends ModelMojoReader<GlrmModel> {
 
   @Override
   protected void readModelData() throws IOException {
-    _model._regx = readkv("regularization_x");
-    _model._gammax = readkv("gamma_x");
+    _model._regx = GlrmRegularizer.valueOf((String) readkv("regularizationX"));
+    _model._gammax = readkv("gammaX");
+    _model._ncolY = readkv("ncolY");
+    _model._nrowY = readkv("nrowY");
+
+    // archetypes
+    _model._archetypes = new double[_model._nrowY][];
+    ByteBuffer bb = ByteBuffer.wrap(readblob("archetypes"));
+    for (int i = 0; i < _model._nrowY; i++) {
+      double[] row = new double[_model._ncolY];
+      _model._archetypes[i] = row;
+      for (int j = 0; j < _model._ncolY; j++)
+        row[j] = bb.getDouble();
+    }
   }
 
   @Override
