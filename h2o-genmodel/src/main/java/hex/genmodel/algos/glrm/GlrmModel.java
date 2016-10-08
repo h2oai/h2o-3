4cfaf4376159101f2895204fe2b5ffc3d6a21f12
Pasha Stetsenko <pasha@h2o.ai>
2016-10-07 18:11:05 -0700

    Read archetypes from GlrmMojo

--- h2o-genmodel/src/main/java/hex/genmodel/algos/glrm/GlrmModel.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/glrm/GlrmModel.java
@@ -6,8 +6,14 @@ import hex.genmodel.MojoModel;
 /**
  */
 public class GlrmModel extends MojoModel {
+  public int _ncolY;
+  public int _nrowY;
+  public double[][] _archetypes;
   public GlrmRegularizer _regx;
   public double _gammax;
+  // We don't really care about regularization of Y since it is not used during scoring
+
+
 
   protected GlrmModel(String[] columns, String[][] domains) {
     super(columns, domains);
