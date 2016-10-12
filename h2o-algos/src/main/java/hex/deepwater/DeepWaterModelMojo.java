841aa933e89aa7ed30c40c760d81323a0e7aed55
Arno Candel <arno.candel@gmail.com>
2016-10-12 00:56:11 -0700

    WIP towards loading a pretrained model with a mean image file.

--- h2o-algos/src/main/java/hex/deepwater/DeepWaterModelMojo.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWaterModelMojo.java
@@ -2,6 +2,8 @@ package hex.deepwater;
 
 import hex.ModelMojo;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -31,20 +33,24 @@ public class DeepWaterModelMojo extends ModelMojo<DeepWaterModel, DeepWaterParam
     writeln("channels = " + _model_info._channels);
     writeln("nums = " + _output._nums);
     writeln("cats = " + _output._cats);
-    writeln("catOffsets = " + Arrays.toString(_output._catOffsets));
-    writeln("normMul = " + Arrays.toString(_output._normMul));
-    writeln("normSub = " + Arrays.toString(_output._normSub));
-    writeln("useAllFactorLevels = " + (_output._useAllFactorLevels ? "true" : "false"));
+    writeln("cat_offsets = " + Arrays.toString(_output._catOffsets));
+    writeln("norm_mul = " + Arrays.toString(_output._normMul));
+    writeln("norm_sub = " + Arrays.toString(_output._normSub));
+    writeln("use_all_factor_levels = " + (_output._useAllFactorLevels ? "true" : "false"));
   }
 
   @Override
   protected void writeModelData() throws IOException {
-    writeBinaryFile("model.network", _model_info._network);
-    writeBinaryFile("model.params", _model_info._modelparams);
+    writeBinaryFile("model_network", _model_info._network);
+    writeBinaryFile("model_params", _model_info._modelparams);
     if (_parms._problem_type == DeepWaterParameters.ProblemType.image_classification) {
       String meanImage = _parms._mean_image_file;
       if (meanImage!=null) {
-//        writeBinaryFile("meandata", null); //TODO FIXME
+        byte[] data = new byte[(int)new File(meanImage).length()];
+        FileInputStream is = new FileInputStream(meanImage);
+        is.read(data);
+        is.close();
+        writeBinaryFile("mean_image_file", data);
       }
     }
   }
