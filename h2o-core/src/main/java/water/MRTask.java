136a92c750676534f32a75e60550722613b4268c
Pasha Stetsenko <pasha@h2o.ai>
2016-10-14 02:28:45 -0700

    Fix a bug in MRTask: some of the map() functions were not called; + micro-optimization: don't compute same thing over and over again

--- h2o-core/src/main/java/water/MRTask.java
+++ h2o-core/src/main/java/water/MRTask.java
@@ -252,7 +252,8 @@ public abstract class MRTask<T extends MRTask<T>> extends DTask<T> implements Fo
    *  <strong>local</strong> Chunks.  All map variants are called, but only one
    *  is expected to be overridden. */
   public void map( Chunk c0, Chunk c1 ) { }
-  //public void map( Chunk c0, Chunk c1, NewChunk nc) { }
+  public void map( Chunk c0, Chunk c1, NewChunk nc) { }
+  public void map( Chunk c0, NewChunk nc0, NewChunk nc1) { }
   //public void map( Chunk c0, Chunk c1, NewChunk nc1, NewChunk nc2 ) { }
 
   /** Override with your map implementation.  This overload is given three
@@ -624,23 +625,25 @@ public abstract class MRTask<T extends MRTask<T>> extends DTask<T> implements Fo
         // Call all the various map() calls that apply
         if(_profile!=null)
           _profile._userstart = System.currentTimeMillis();
-        if( _fr.vecs().length == 1 ) map(bvs[0]);
-        if( _fr.vecs().length == 2 ) map(bvs[0], bvs[1]);
-        if( _fr.vecs().length == 3 ) map(bvs[0], bvs[1], bvs[2]);
-        if( true                  )  map(bvs );
-        if( _output_types != null && _output_types.length == 1 ) { // convenience versions for cases with single output.
-          if( appendableChunks == null ) throw H2O.fail(); // Silence IdeaJ warnings
-          if( _fr.vecs().length == 1 ) map(bvs[0], appendableChunks[0]);
-          if( _fr.vecs().length == 2 ) map(bvs[0], bvs[1],appendableChunks[0]);
-          //if( _fr.vecs().length == 3 ) map(bvs[0], bvs[1], bvs[2],appendableChunks[0]);
-          //if( true                  )  map(bvs,    appendableChunks[0]);
+        int fr_vecs_length = _fr.vecs().length;
+        int outputs_length = _output_types == null? 0 : _output_types.length;
+        if (fr_vecs_length == 1) map(bvs[0]);
+        else if(fr_vecs_length == 2) map(bvs[0], bvs[1]);
+        else if(fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2]);
+        map(bvs);
+        if (outputs_length == 1) {  // convenience versions for cases with single output.
+          assert appendableChunks != null;
+          if (fr_vecs_length == 1) map(bvs[0], appendableChunks[0]);
+          else if (fr_vecs_length == 2) map(bvs[0], bvs[1], appendableChunks[0]);
+          //else if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2], appendableChunks[0]);
+          map(bvs, appendableChunks[0]);
         }
-        if( _output_types != null && _output_types.length == 2) { // convenience versions for cases with 2 outputs (e.g split).
-          if( appendableChunks == null ) throw H2O.fail(); // Silence IdeaJ warnings
-          if( _fr.vecs().length == 1 ) map(bvs[0], appendableChunks[0],appendableChunks[1]);
+        if (outputs_length == 2) {  // convenience versions for cases with 2 outputs (e.g split).
+          assert appendableChunks != null;
+          if (fr_vecs_length == 1) map(bvs[0], appendableChunks[0], appendableChunks[1]);
           //if( _fr.vecs().length == 2 ) map(bvs[0], bvs[1],appendableChunks[0],appendableChunks[1]);
           //if( _fr.vecs().length == 3 ) map(bvs[0], bvs[1], bvs[2],appendableChunks[0],appendableChunks[1]);
-          if( true                  )  map(bvs,    appendableChunks[0],appendableChunks[1]);
+          map(bvs, appendableChunks[0], appendableChunks[1]);
         }
         map(bvs,appendableChunks);
         _res = self();          // Save results since called map() at least once!
