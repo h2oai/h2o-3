1436c3e0b6a7e66c435ff8007d8a5db12b081203
Pasha Stetsenko <pasha@h2o.ai>
2016-10-14 13:04:46 -0700

    Add else-s to MRTask to achieve minor performance benefit

--- h2o-core/src/main/java/water/MRTask.java
+++ h2o-core/src/main/java/water/MRTask.java
@@ -625,29 +625,31 @@ public abstract class MRTask<T extends MRTask<T>> extends DTask<T> implements Fo
         // Call all the various map() calls that apply
         if(_profile!=null)
           _profile._userstart = System.currentTimeMillis();
+
         int fr_vecs_length = _fr.vecs().length;
         int outputs_length = _output_types == null? 0 : _output_types.length;
         if (outputs_length == 0) {
           if (fr_vecs_length == 1) map(bvs[0]);
-          if (fr_vecs_length == 2) map(bvs[0], bvs[1]);
-          if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2]);
+          else if (fr_vecs_length == 2) map(bvs[0], bvs[1]);
+          else if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2]);
           map(bvs);
         }
-        if (outputs_length == 1) {  // convenience versions for cases with single output.
+        else if (outputs_length == 1) {  // convenience versions for cases with single output.
           assert appendableChunks != null;
           if (fr_vecs_length == 1) map(bvs[0], appendableChunks[0]);
-          if (fr_vecs_length == 2) map(bvs[0], bvs[1], appendableChunks[0]);
-          // if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2], appendableChunks[0]);
+          else if (fr_vecs_length == 2) map(bvs[0], bvs[1], appendableChunks[0]);
+          // else if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2], appendableChunks[0]);
           map(bvs, appendableChunks[0]);
         }
-        if (outputs_length == 2) {  // convenience versions for cases with 2 outputs (e.g split).
+        else if (outputs_length == 2) {  // convenience versions for cases with 2 outputs (e.g split).
           assert appendableChunks != null;
           if (fr_vecs_length == 1) map(bvs[0], appendableChunks[0], appendableChunks[1]);
-          // if (fr_vecs_length == 2) map(bvs[0], bvs[1], appendableChunks[0], appendableChunks[1]);
-          // if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2], appendableChunks[0], appendableChunks[1]);
+          // else if (fr_vecs_length == 2) map(bvs[0], bvs[1], appendableChunks[0], appendableChunks[1]);
+          // else if (fr_vecs_length == 3) map(bvs[0], bvs[1], bvs[2], appendableChunks[0], appendableChunks[1]);
           map(bvs, appendableChunks[0], appendableChunks[1]);
         }
-        map(bvs,appendableChunks);
+        map(bvs, appendableChunks);
+
         _res = self();          // Save results since called map() at least once!
         // Further D/K/V put any new vec results.
         if(_profile!=null)
