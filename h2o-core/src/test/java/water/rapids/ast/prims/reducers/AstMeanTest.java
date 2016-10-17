604eb67ff6e8be6051fb2ed7d83da0bfdb32cceb
Pasha Stetsenko <pasha@h2o.ai>
2016-10-16 22:48:22 -0700

    Add one more test case for AstMeanTest: when all columns in the frame are time, and computes rowwise means, then the resulting column is also time

--- h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstMeanTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/reducers/AstMeanTest.java
@@ -1,6 +1,5 @@
 package water.rapids.ast.prims.reducers;
 
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import water.DKV;
@@ -11,6 +10,9 @@ import water.fvec.Vec;
 import water.rapids.Rapids;
 import water.rapids.Val;
 import water.rapids.vals.ValFrame;
+
+import static org.junit.Assert.*;
+
 
 /**
  * Test the AstMean.java class
@@ -40,28 +42,28 @@ public class AstMeanTest extends TestUtil {
       fr1 = new Frame(key1, names1, vecs1);
       DKV.put(key1, fr1);
       Val val1 = Rapids.exec("(mean " + key1 + " 0 0)");
-      Assert.assertTrue(val1 instanceof ValFrame);
+      assertTrue(val1 instanceof ValFrame);
       res1 = val1.getFrame();
-      Assert.assertArrayEquals(names1, res1.names());
-      Assert.assertArrayEquals(types1, res1.types());
+      assertArrayEquals(names1, res1.names());
+      assertArrayEquals(types1, res1.types());
       assertRowFrameEquals(new double[]{0.0, 4.0, 0.6, Double.NaN, 10000030.0, Double.NaN}, res1);
 
       // Test mean when NaNs are ignored (na_rm = True)
       Val val2 = Rapids.exec("(mean " + key1 + " 1 0)");
-      Assert.assertTrue(val2 instanceof ValFrame);
+      assertTrue(val2 instanceof ValFrame);
       res2 = val2.getFrame();
-      Assert.assertArrayEquals(names1, res2.names());
-      Assert.assertArrayEquals(types1, res2.types());
+      assertArrayEquals(names1, res2.names());
+      assertArrayEquals(types1, res2.types());
       assertRowFrameEquals(new double[]{0.0, 4.0, 0.6, 2.0, 10000030.0, Double.NaN}, res2);
 
       // Apply mean to an empty frame
       fr2 = new Frame(key2);
       DKV.put(key2, fr2);
       Val val3 = Rapids.exec("(mean " + key2 + " 0 0)");
-      Assert.assertTrue(val3 instanceof ValFrame);
+      assertTrue(val3 instanceof ValFrame);
       res3 = val3.getFrame();
-      Assert.assertEquals(res3.numCols(), 0);
-      Assert.assertEquals(res3.numRows(), 0);
+      assertEquals(res3.numCols(), 0);
+      assertEquals(res3.numRows(), 0);
 
     } finally {
       if (fr1 != null) fr1.delete();
@@ -86,63 +88,81 @@ public class AstMeanTest extends TestUtil {
     Vec vd3 = TestUtil.dvec(1, 2, Double.NaN, 3, Double.NaN);
     Vec vs1 = TestUtil.svec("a", "b", "c", "d", "e");
     Vec vt1 = TestUtil.tvec(10000000, 10000020, 10000030, 10000040, 10000060);
+    Vec vt2 = TestUtil.tvec(20000000, 20000020, 20000030, 20000040, 20000060);
 
-    Frame fr1 = null, fr2 = null;
-    Frame rs1 = null, rs2 = null, rs3 = null;
+    Frame fr1 = null, fr2 = null,             fr4 = null;
+    Frame rs1 = null, rs2 = null, rs3 = null, rs4 = null;
     try {
       // Test with na_rm = False and no vs1
       Key<Frame> key1 = Key.make();
-      fr1 = new Frame(key1, new String[]{"i1", "d1", "d2", "d3"}, new Vec[]{vi1, vd1, vd2, vd3});
+      fr1 = new Frame(key1, ar("i1", "d1", "d2", "d3"), new Vec[]{vi1, vd1, vd2, vd3});
       DKV.put(key1, fr1);
       Val val1 = Rapids.exec("(mean " + key1 + " 0 1)");
-      Assert.assertTrue(val1 instanceof ValFrame);
+      assertTrue(val1 instanceof ValFrame);
       rs1 = val1.getFrame();
-      assertColFrameEquals(new double[]{1.7/4, 2.9/4, Double.NaN, 10.3/4, Double.NaN}, rs1);
-      Assert.assertEquals("mean", rs1.name(0));
+      assertColFrameEquals(ard(1.7/4, 2.9/4, Double.NaN, 10.3/4, Double.NaN), rs1);
+      assertEquals("mean", rs1.name(0));
 
       // Test with na_rm = False and vs1 present
       Key<Frame> key2 = Key.make();
-      fr2 = new Frame(key2, new String[]{"i1", "d1", "d2", "d3", "s1"}, new Vec[]{vi1, vd1, vd2, vd3, vs1});
+      fr2 = new Frame(key2, ar("i1", "d1", "d2", "d3", "s1"), new Vec[]{vi1, vd1, vd2, vd3, vs1});
       DKV.put(key2, fr2);
       Val val2 = Rapids.exec("(mean " + key2 + " 0 1)");
-      Assert.assertTrue(val2 instanceof ValFrame);
+      assertTrue(val2 instanceof ValFrame);
       rs2 = val2.getFrame();
-      assertColFrameEquals(new double[]{Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN}, rs2);
+      assertColFrameEquals(ard(Double.NaN, Double.NaN, Double.NaN, Double.NaN, Double.NaN), rs2);
 
       // Test na_rm = true
       Val val3 = Rapids.exec("(mean " + key2 + " 1 1)");
-      Assert.assertTrue(val3 instanceof ValFrame);
+      assertTrue(val3 instanceof ValFrame);
       rs3 = val3.getFrame();
-      assertColFrameEquals(new double[]{1.7/4, 2.9/4, 4.1/3, 10.3/4, 10.0/3}, rs3);
+      assertEquals("Unexpected column name", "mean", rs3.name(0));
+      assertEquals("Unexpected column type", Vec.T_NUM, rs3.types()[0]);
+      assertColFrameEquals(ard(1.7/4, 2.9/4, 4.1/3, 10.3/4, 10.0/3), rs3);
+
+      // Test frame containing multiple time columns
+      Key<Frame> key4 = Key.make();
+      fr4 = new Frame(key4, ar("t1", "s", "t2"), aro(vt1, vs1, vt2));
+      DKV.put(key4, fr4);
+      Val val4 = Rapids.exec("(mean " + key4 + " 1 1)");
+      assertTrue(val4 instanceof ValFrame);
+      rs4 = val4.getFrame();
+      assertEquals("Unexpected column name", "mean", rs4.name(0));
+      assertEquals("Unexpected column type", Vec.T_TIME, rs4.types()[0]);
+      assertColFrameEquals(ard(15000000, 15000020, 15000030, 15000040, 15000060), rs4);
+
 
     } finally {
       if (fr1 != null) fr1.delete();
       if (fr2 != null) fr2.delete();
+      if (fr4 != null) fr4.delete();
       if (rs1 != null) rs1.delete();
       if (rs2 != null) rs2.delete();
       if (rs3 != null) rs3.delete();
+      if (rs4 != null) rs4.delete();
       vi1.remove();
       vd1.remove();
       vd2.remove();
       vd3.remove();
       vs1.remove();
       vt1.remove();
+      vt2.remove();
     }
   }
 
   public static void assertRowFrameEquals(double[] expected, Frame actual) {
-    Assert.assertEquals(1, actual.numRows());
-    Assert.assertEquals(expected.length, actual.numCols());
+    assertEquals(1, actual.numRows());
+    assertEquals(expected.length, actual.numCols());
     for (int i = 0; i < expected.length; i++) {
-      Assert.assertEquals("Wrong average in column " + actual.name(i), expected[i], actual.vec(i).at(0), 1e-8);
+      assertEquals("Wrong average in column " + actual.name(i), expected[i], actual.vec(i).at(0), 1e-8);
     }
   }
 
   public static void assertColFrameEquals(double[] expected, Frame actual) {
-    Assert.assertEquals(1, actual.numCols());
-    Assert.assertEquals(expected.length, actual.numRows());
+    assertEquals(1, actual.numCols());
+    assertEquals(expected.length, actual.numRows());
     for (int i = 0; i < expected.length; i++) {
-      Assert.assertEquals("Wrong average in row " + i, expected[i], actual.vec(0).at(i), 1e-8);
+      assertEquals("Wrong average in row " + i, expected[i], actual.vec(0).at(i), 1e-8);
     }
   }
 
