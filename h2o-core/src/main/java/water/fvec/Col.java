8d74832a55250b7d52740f12610fdc79e6e28d92
Vlad Patryshev <vpatryshev@gmail.com>
2016-10-17 15:29:23 -0700

    TestUtil: enabled java asserts Col: removed unreferenced classes, made them anonymous. Made sure data type code is passed to Vec before it's being filled. Still a hack.

--- h2o-core/src/main/java/water/fvec/Col.java
+++ h2o-core/src/main/java/water/fvec/Col.java
@@ -36,15 +36,21 @@ public class Col {
   }
 
   public static abstract class Factory<T> implements Serializable {
+    protected Factory(byte typeCode) {
+      this.typeCode = typeCode;
+    }
+
     public abstract TypedChunk<T> newChunk(final Chunk c);
     public abstract TypedVector<T> newColumn(Vec vec);
-
+    public final byte typeCode;
     public TypedVector<T> newColumn(long len, final Function<Long, T> f) throws IOException {
       return newColumn(makeVec(len, f));
     }
 
     protected Vec makeVec(long len, final Function<Long, T> f) throws IOException {
       final Vec vec0 = Vec.makeZero(len);
+      vec0._type = typeCode;
+
       return new MRTask() {
         @Override
         public void map(Chunk[] cs) {
@@ -60,7 +66,6 @@ public class Col {
     }
 
   }
-
 
   public abstract static class TypedVector<T> implements Column<T>, VectorHolder {
     protected Vec vec;
@@ -93,9 +98,7 @@ public class Col {
 
   //-------------------------------------------------------------
 
-  public static final Factory<Double> Doubles = new Factory<Double>() {
-
-    @Override public TypedVector<Double> newColumn(Vec vec) { return new OfDoubles(vec); }
+  public static final Factory<Double> Doubles = new Factory<Double>(Vec.T_NUM) {
 
     @Override public TypedChunk<Double> newChunk(final Chunk c) {
       return new TypedChunk<Double>(c) {
@@ -106,22 +109,23 @@ public class Col {
         public void set(int idx, double value) { c.set(idx, value); }
       };
     }
-  };
 
-  public static class OfDoubles extends TypedVector<Double> {
-    public OfDoubles(Vec vec) { super(vec, Vec.T_NUM); }
-    @Override public Double get(long idx) { return vec.at(idx); }
-    @Override public void set(long idx, Double value) {
-      if (value == null) vec.setNA(idx); else vec.set(idx, value);
-    }
-    public void set(long idx, double value) { vec.set(idx, value); }
-  }
+    @Override public TypedVector<Double> newColumn(final Vec vec) {
+      return new TypedVector<Double>(vec, typeCode) {
+        @Override public Double get(long idx) { return vec.at(idx); }
+
+        @Override public void set(long idx, Double value) {
+          if (value == null) vec.setNA(idx); else vec.set(idx, value);
+        }
+
+        public void set(long idx, double value) { vec.set(idx, value); }
+      }; }
+
+  };
 
   //-------------------------------------------------------------
 
-  public static final Factory<String> Strings = new Factory<String>() {
-
-    @Override public TypedVector<String> newColumn(Vec vec) { return new OfStrings(vec); }
+  public static final Factory<String> Strings = new Factory<String>(Vec.T_STR) {
 
     @Override public TypedChunk<String> newChunk(final Chunk c) {
       return new TypedChunk<String>(c) {
@@ -129,24 +133,27 @@ public class Col {
         @Override void set(int idx, String value) { c.set(idx, value); }
       };
     }
-  };
 
-  public static class OfStrings extends TypedVector<String> {
-    public OfStrings(Vec vec) { super(vec, Vec.T_STR); }
+    @Override public TypedVector<String> newColumn(final Vec vec) {
+      return new TypedVector<String>(vec, typeCode) {
+        @Override
+        public String get(long idx) { return asString(vec.atStr(new BufferedString(), idx)); }
 
-    @Override
-    public String get(long idx) { return asString(vec.atStr(new BufferedString(), idx)); }
-
-    @Override
-    public void set(long idx, String value) {
-      vec.set(idx, value);
+        @Override
+        public void set(long idx, String value) {
+          vec.set(idx, value);
+        }
+      };
     }
-  }
+
+  };
 
   //-------------------------------------------------------------
 
   static class EnumFactory extends Factory<Integer> {
-    @Override public TypedVector<Integer> newColumn(Vec vec) { return new OfEnums(vec); }
+    protected EnumFactory() {
+      super(Vec.T_CAT);
+    }
 
     @Override public TypedChunk<Integer> newChunk(final Chunk c) {
       return new TypedChunk<Integer>(c) {
@@ -163,42 +170,39 @@ public class Col {
       vec.setDomain(domain);
       return newColumn(vec);
     }
+
+    @Override public TypedVector<Integer> newColumn(final Vec vec) {
+      return new TypedVector<Integer>(vec, typeCode) {
+        private final String[] domain = vec.domain();
+        {
+          assert domain != null && domain.length > 0 : "Need a domain for enums";
+        }
+
+        @Override
+        public Integer get(long idx) { return isNA(idx) ? null : (int) vec.at8(idx); }
+
+        @Override
+        public String getString(long idx) {
+          Integer i = get(idx);
+          boolean noname = (i == null || domain == null || i < 0 || i >= domain.length);
+          return noname ? "" + i : domain[i];
+        }
+
+        @Override
+        public void set(long idx, Integer value) {
+          if (value == null) vec.setNA(idx); else vec.set(idx, value);
+        }
+
+        public void set(long idx, int value) { vec.set(idx, value); }
+      };
+    }
   }
 
   public static final EnumFactory Enums = new EnumFactory();
 
-  public static class OfEnums extends TypedVector<Integer> {
-    private final String[] domain;
-
-    public OfEnums(Vec vec) {
-      super(vec, Vec.T_CAT);
-      this.domain = vec.domain();
-      assert domain != null && domain.length > 0 : "Need a domain for enums";
-    }
-
-    @Override
-    public Integer get(long idx) { return isNA(idx) ? null : (int) vec.at8(idx); }
-
-    @Override
-    public String getString(long idx) {
-      Integer i = get(idx);
-      boolean noname = (i == null || domain == null || i < 0 || i >= domain.length);
-      return noname ? "" + i : domain[i];
-    }
-
-    @Override
-    public void set(long idx, Integer value) {
-      if (value == null) vec.setNA(idx); else vec.set(idx, value);
-    }
-
-    public void set(long idx, int value) { vec.set(idx, value); }
-  }
-
   //-------------------------------------------------------------
 
-  public static final Factory<UUID> UUIDs = new Factory<UUID>() {
-
-    @Override public TypedVector<UUID> newColumn(Vec vec) { return new OfUUID(vec); }
+  public static final Factory<UUID> UUIDs = new Factory<UUID>(Vec.T_UUID) {
 
     @Override public TypedChunk<UUID> newChunk(final Chunk c) {
       return new TypedChunk<UUID>(c) {
@@ -206,22 +210,19 @@ public class Col {
         @Override void set(int idx, UUID value) { c.set(idx, value); }
       };
     }
+
+    @Override public TypedVector<UUID> newColumn(final Vec vec) {
+      return new TypedVector<UUID>(vec, typeCode) {
+        @Override public UUID get(long idx) { return isNA(idx) ? null : new UUID(vec.at16h(idx), vec.at16l(idx)); }
+        @Override public String getString(long idx) { return PrettyPrint.uuid(get(idx)); }
+        @Override public void set(long idx, UUID value) { vec.set(idx, value); }
+      };
+    }
   };
-
-  public static class OfUUID extends TypedVector<UUID> {
-
-    OfUUID(Vec vec) { super(vec, Vec.T_UUID); }
-
-    @Override public UUID get(long idx) { return isNA(idx) ? null : new UUID(vec.at16h(idx), vec.at16l(idx)); }
-    @Override public String getString(long idx) { return PrettyPrint.uuid(get(idx)); }
-    @Override public void set(long idx, UUID value) { vec.set(idx, value); }
-  }
 
   //-------------------------------------------------------------
   
-  public static final Factory<Date> Dates = new Factory<Date>() {
-
-    @Override public TypedVector<Date> newColumn(Vec vec) { return new OfDates(vec); }
+  public static final Factory<Date> Dates = new Factory<Date>(Vec.T_TIME) {
 
     @Override public TypedChunk<Date> newChunk(final Chunk c) {
       return new TypedChunk<Date>(c) {
@@ -231,15 +232,18 @@ public class Col {
         }
       };
     }
-  };
 
-  public static class OfDates extends TypedVector<Date> {
-    OfDates(Vec vec) { super(vec, Vec.T_TIME); }
-    @Override public Date get(long idx) { return isNA(idx) ? null : new Date(vec.at8(idx)); }
-    @Override public void set(long idx, Date value) {
-      if (value == null) vec.setNA(idx); else vec.set(idx, value.getTime());
+    @Override public TypedVector<Date> newColumn(final Vec vec) {
+      return new TypedVector<Date>(vec, typeCode) {
+        @Override public Date get(long idx) { return isNA(idx) ? null : new Date(vec.at8(idx)); }
+
+        @Override public void set(long idx, Date value) {
+          if (value == null) vec.setNA(idx); else vec.set(idx, value.getTime());
+        }
+      };
     }
-  }
+
+  };
 
   static String asString(Object x) { return x == null ? null : x.toString(); }
 }
