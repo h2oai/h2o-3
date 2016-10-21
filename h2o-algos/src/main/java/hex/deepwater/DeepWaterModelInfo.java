7988854a7352d2037bedea25e2b7169cc45755b0
Arno Candel <arno.candel@gmail.com>
2016-10-20 19:33:43 -0700

    Add exception to MOJO backend instantiation (#386)
    * Update to deepwater API 1.0.1 - Thx Michal!
    
    * Add exception message if backend can't be loaded.

--- h2o-algos/src/main/java/hex/deepwater/DeepWaterModelInfo.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWaterModelInfo.java
@@ -196,7 +196,8 @@ final public class DeepWaterModelInfo extends Iced {
   private void setupNativeBackend() {
     try {
       _backend = createDeepWaterBackend(parameters._backend.toString());
-      if (_backend == null) throw new IllegalArgumentException("No backend found. Cannot build a Deep Water model.");
+      if (_backend == null)
+        throw new RuntimeException("Couldn't instantiate native backend " + parameters._backend.toString() + ". Cannot build a Deep Water model.");
       ImageDataSet imageDataSet = getImageDataSet();
       RuntimeOptions opts = getRuntimeOptions();
       BackendParams bparms = getBackendParams();
@@ -304,8 +305,9 @@ final public class DeepWaterModelInfo extends Iced {
       return;
     }
     if (_backend ==null) {
-      _backend = createDeepWaterBackend(get_params()._backend.toString()); // new ImageTrain(_width, _height, _channels, _deviceID, (int)parameters.getOrMakeRealSeed(), _gpu);
-      if (_backend == null) throw new IllegalArgumentException("No backend found. Cannot build a Deep Water model.");
+      _backend = createDeepWaterBackend(get_params()._backend.toString());
+      if (_backend == null)
+        throw new RuntimeException("Couldn't instantiate native backend " + get_params()._backend.toString() + ". Cannot build a Deep Water model.");
     }
 
     if (network==null) network = _network;
