573d713d77e33c65189a23d61165e1aedd0f40e3
Vlad Patryshev <vpatryshev@gmail.com>
2016-11-05 18:46:45 -0700

    Got rid of dead code
    Have to implement multicolumn result frame
    Have to add primitive types

--- h2o-core/src/main/java/water/udf/ChunkFactory.java
+++ h2o-core/src/main/java/water/udf/ChunkFactory.java
@@ -5,6 +5,7 @@ import water.fvec.Chunk;
 /**
  * This factory creates a TypedChunk; there's a variety of data sources,
  * can be a materialized Chunk, or a function.
+ * Have to keep a byte with type code, since, well, it's H2O.
  */
 public interface ChunkFactory<DataType> extends Function<Chunk, DataChunk<DataType>> {
   byte typeCode();
