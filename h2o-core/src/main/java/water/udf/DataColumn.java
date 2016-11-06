45fb647f45529cf571f0fe8d900beb965faee549
Vlad Patryshev <vpatryshev@gmail.com>
2016-11-05 18:35:58 -0700

    A bunch of renamings and refactorings
    More refactorings needed
    Have to implement multicolumn result frame
    Have to add primitive types

---
+++ h2o-core/src/main/java/water/udf/DataColumn.java
@@ -0,0 +1,59 @@
+package water.udf;
+
+import water.fvec.Vec;
+
+/**
+ * A Column based on actual data in a Vec (hence implementing Vec.Holder)
+ */
+public abstract class DataColumn<T> implements Column<T>, Vec.Holder {
+  protected Vec vec;
+  public final byte type;
+  private ChunkFactory<T> chunkFactory;
+
+  public abstract T get(long idx);
+
+  public abstract void set(long idx, T value);
+
+  @Override
+  public T apply(Long idx) {
+    return get(idx);
+  }
+
+  @Override
+  public T apply(long idx) {
+    return get(idx);
+  }
+
+  @Override
+  public int rowLayout() {
+    return vec._rowLayout;
+  }
+
+  @Override
+  public long size() {
+    return vec.length();
+  }
+
+  @Override
+  public TypedChunk<T> chunkAt(int i) {
+    return chunkFactory.apply(vec.chunkForChunkIdx(i));
+  }
+
+  protected DataColumn(Vec vec, byte type, ChunkFactory<T> factory) {
+    this.vec = vec;
+    this.type = type;
+    this.chunkFactory = factory;
+  }
+
+  public boolean isNA(long idx) {
+    return vec.isNA(idx);
+  }
+
+  public String getString(long idx) {
+    return isNA(idx) ? "(N/A)" : String.valueOf(apply(idx));
+  }
+
+  public Vec vec() {
+    return vec;
+  }
+}
--- h2o-core/src/main/java/water/udf/Unfoldable.java
+++ h2o-core/src/main/java/water/udf/Unfoldable.java
@@ -5,6 +5,4 @@ import java.util.List;
 /**
  * Takes a value of type X, produces a multitude of values of type Y
  */
-public interface Unfoldable<X, Y> {
-  List<Y> apply(X x);
-}
+public interface Unfoldable<X, Y> extends Function<X, List<Y>> {}
