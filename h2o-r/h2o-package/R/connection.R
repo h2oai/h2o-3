903107c77229a03c023e095dcf70a49343a54b6c
Navdeep Gill <navdeep@h2o.ai>
2016-11-08 14:47:09 -0800

    Decrease timeout to 5 sec for remoteSend()

--- h2o-r/h2o-package/R/connection.R
+++ h2o-r/h2o-package/R/connection.R
@@ -402,7 +402,7 @@ h2o.clusterStatus <- function() {
   try(.h2o.__remoteSend("InitID", method = "DELETE"), TRUE)
 }
 
-.Last <- function() { if ( .isConnected() ) try(.h2o.__remoteSend("InitID", method = "DELETE"), TRUE)}
+.Last <- function() { if ( .isConnected() ) try(.h2o.__remoteSend("InitID", method = "DELETE",timeout=5), TRUE)}
 
 .h2o.startJar <- function(nthreads = -1, max_memory = NULL, min_memory = NULL, enable_assertions = TRUE, forceDL = FALSE, license = NULL, ice_root, stdout) {
   command <- .h2o.checkJava()
