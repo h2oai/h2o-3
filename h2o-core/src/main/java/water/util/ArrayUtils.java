48932cd5e9849b90674fdd1e9fb476d8a76e2656
Arno Candel <arno.candel@gmail.com>
2016-11-17 01:49:58 +0100

    Allow more classes (1000 -> 2^20), but won't compute the CM if too many.

--- h2o-core/src/main/java/water/util/ArrayUtils.java
+++ h2o-core/src/main/java/water/util/ArrayUtils.java
@@ -222,6 +222,7 @@ public class ArrayUtils {
     return a;
   }
   public static double[][] add(double[][] a, double[][] b) {
+    if (a == null) return b;
     for(int i = 0; i < a.length; i++ ) a[i] = add(a[i], b[i]);
     return a;
   }
