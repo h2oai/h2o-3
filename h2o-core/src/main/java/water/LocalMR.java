c6f6600c978e89b482a55592e9c25ac27304709d
Tomas Nykodym <tomas.nykodym@gmail.com>
2016-11-17 17:00:18 -0800

    Updated LocalMR.   - no longer does computation in non-leaf nodes   - uses fork instead of submit   - tries to reuse MrFun if possible (i.e. previous task completed and has a useable copy)

--- h2o-core/src/main/java/water/LocalMR.java
+++ h2o-core/src/main/java/water/LocalMR.java
@@ -3,38 +3,21 @@ package water;
 import jsr166y.CountedCompleter;
 
 import java.util.concurrent.CancellationException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by tomas on 11/5/16.
  *
  * Generic Local MRTask utility. Will launch requested number of tasks (on local node!), organized in a binary tree fashion.
- * Tasks are launched via H2O.submit task rather than fork (assuming different usage pattern than MRT - use fewer longer running tasks -> push into global FJQ).
- *
- * User provides a MrFun function object with map/reduce/makeCopy functions.
- *
- * Here is a sample tree and reduce pairs for LocalMR creating 8 tasks
- *          4
- *        /  \
- *      /     \
- *     2      6
- *    / \    / \
- *   1   3  5  7
- *   |
- *   0
- *
- *   Reduce will be called for pairs (1,0), (2,1), (2,3), (4,2), (6,5), (6,7), (4,6)
  *
  */
 public class LocalMR<T extends MrFun<T>> extends H2O.H2OCountedCompleter<LocalMR> {
   private int _lo;
   private int _hi;
-  final MrFun _mrFun;
+  MrFun _mrFun;
   volatile Throwable _t;
   protected volatile boolean  _cancelled;
   private LocalMR<T> _root;
 
-  public LocalMR(MrFun mrt){this(mrt,H2O.NUMCPUS);}
   public LocalMR(MrFun mrt, int nthreads){this(mrt,nthreads,null);}
   public LocalMR(MrFun mrt, H2O.H2OCountedCompleter cc){this(mrt,H2O.NUMCPUS,cc);}
   public LocalMR(MrFun mrt, int nthreads, H2O.H2OCountedCompleter cc){
@@ -44,20 +27,12 @@ public class LocalMR<T extends MrFun<T>> extends H2O.H2OCountedCompleter<LocalMR
     _mrFun = mrt;
     _lo = 0;
     _hi = nthreads;
+    _prevTsk = null;
   }
-  public LocalMR(MrFun mrt, int nthreads, H2O.H2OCountedCompleter cc, byte priority) {
-    super(cc,priority);
-    if(nthreads <= 0) throw new IllegalArgumentException("nthreads must be positive");
-    _root = this;
-    _mrFun = mrt;
-    _lo = 0;
-    _hi = nthreads;
-  }
-
-  private LocalMR(LocalMR src, int lo, int hi) {
+  private LocalMR(LocalMR src, LocalMR prevTsk,int lo, int hi) {
     super(src);
     _root = src._root;
-    _mrFun = src._mrFun.makeCopy();
+    _prevTsk = prevTsk;
     _lo = lo;
     _hi = hi;
     _cancelled = src._cancelled;
@@ -65,32 +40,40 @@ public class LocalMR<T extends MrFun<T>> extends H2O.H2OCountedCompleter<LocalMR
 
   private LocalMR<T> _left;
   private LocalMR<T> _rite;
+  private final LocalMR<T> _prevTsk; // right child will attempt to share MrFun with " left sibling" if the whole left subtree is done
 
-  volatile boolean completed;
 
+  volatile boolean completed; // this task and all it's children completed
+  volatile boolean started; // this task and all it's children completed
   public boolean isCancelRequested(){return _root._cancelled;}
 
   private int mid(){ return _lo + ((_hi - _lo) >> 1);}
 
   @Override
   public final void compute2() {
+    started = true;
     if (!_root._cancelled) {
       try {
         int mid = mid();
         assert _hi > _lo;
-        int pending = 0;
+        int pending = 1;
         if (_hi - _lo >= 2) {
-          _left = new LocalMR(this, _lo, mid);
-          pending++;
-          if ((mid + 1) < _hi) {
-            _rite = new LocalMR(this, mid + 1, _hi);
+          _left = new LocalMR(this, _prevTsk, _lo, mid);
+          if (mid < _hi) {
+            _rite = new LocalMR(this, _left, mid, _hi);
             pending++;
           }
           addToPendingCount(pending);
-          H2O.submitTask(_left);
-          if(_rite != null) H2O.submitTask(_rite);
+          if(_rite != null) _rite.fork();
+          _left.fork();
+        } else {
+
+          if(_prevTsk != null && _prevTsk.completed){
+            _mrFun = _prevTsk._mrFun;
+            _prevTsk._mrFun = null;
+          } else _mrFun = _root._mrFun.makeCopy();
+          _mrFun.map(mid);
         }
-        _mrFun.map(mid);
       } catch (Throwable t) {
         t.printStackTrace();
         if (_root._t == null) {
@@ -99,7 +82,6 @@ public class LocalMR<T extends MrFun<T>> extends H2O.H2OCountedCompleter<LocalMR
         }
       }
     }
-    completed = true;
     tryComplete();
   }
 
@@ -112,16 +94,19 @@ public class LocalMR<T extends MrFun<T>> extends H2O.H2OCountedCompleter<LocalMR
     }
     if(_root._cancelled) return;
     assert cc == this || cc == _left || cc == _rite;
-    if (_left != null) {
+    if (_left != null && _left._mrFun != null && _mrFun != _left._mrFun) {
       assert _left.completed;
-      _mrFun.reduce(_left._mrFun);
-      _left = null;
+      if(_mrFun == null)_mrFun = _left._mrFun;
+      else _mrFun.reduce(_left._mrFun);
     }
-    if (_rite != null) {
+    if (_rite != null && _mrFun != _rite._mrFun) {
       assert _rite.completed;
-      _mrFun.reduce(_rite._mrFun);
-      _rite = null;
+      if(_mrFun == null)_mrFun = _rite._mrFun;
+      else _mrFun.reduce(_rite._mrFun);
     }
+    _left = null;
+    _rite = null;
+    completed = true;
   }
 
 }
