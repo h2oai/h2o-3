35199619ad7d87263894b1088a5405496d9fab1a
Tomas Nykodym <tomas.nykodym@gmail.com>
2016-11-17 17:01:12 -0800

    Added quick allocation path for small arrays of doubles.

--- h2o-core/src/main/java/water/MemoryManager.java
+++ h2o-core/src/main/java/water/MemoryManager.java
@@ -255,7 +255,12 @@ abstract public class MemoryManager {
   public static int    [] malloc4 (int size) { return (int    [])malloc(size,size*4L, 4,null,0); }
   public static long   [] malloc8 (int size) { return (long   [])malloc(size,size*8L, 8,null,0); }
   public static float  [] malloc4f(int size) { return (float  [])malloc(size,size*4L, 5,null,0); }
-  public static double [] malloc8d(int size) { return (double [])malloc(size,size*8L, 9,null,0); }
+  public static double [] malloc8d(int size) {
+    if(size < 32) try { // fast path for small arrays (e.g. histograms in gbm)
+      return new double [size];
+    } catch (OutOfMemoryError oom){/* fall through */}
+    return (double [])malloc(size,size*8L, 9,null,0);
+  }
   public static double [][] malloc8d(int m, int n) {
     double [][] res = new double[m][];
     for(int i = 0; i < m; ++i)
