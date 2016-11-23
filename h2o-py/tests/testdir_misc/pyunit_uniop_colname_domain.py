bb358f592461a7a4a1032885e4c60e3bd7a5e48a
Navdeep Gill <navdeep@h2o.ai>
2016-11-22 18:41:29 -0800

    Modify colname when uniop is applied. This is only applied during assignment and not in place.

---
+++ h2o-py/tests/testdir_misc/pyunit_uniop_colname_domain.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import sys
+sys.path.insert(1,"../../")
+import h2o
+from tests import pyunit_utils
+
+#Test to see if you apply H2O uniop methods to an H2OFrame it will not overwrite the original column headers & domains
+# & also update colnames of new frame with `op(colName)` where `op` is a uniop operator.
+
+def pyunit_colname_uniop():
+    dataframe = {'A': [1,0,3,4], 'B': [5,6,-6, -1], 'C':[-4, -6, -7, 8]}
+    frame = h2o.H2OFrame(dataframe)
+    frame_asin = frame.asin()
+
+    #Check colnames of original frame remain untouched.
+    assert frame.names == [u'A', u'C', u'B'], "Expected original colnames to remain the same after uniop operation"
+    #Check new colnames for modified frame are of the convention `op(colname)`
+    assert ["asin(%s)" % (name) for name in frame.names] == frame_asin.names,"Expected equal col names after uniop operation"
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(pyunit_colname_uniop)
+else:
+    pyunit_colname_uniop()
