24e1c2ed2a4ff8b240e861f5f2f0942cc6db8def
Arno Candel <arno.candel@gmail.com>
2016-11-22 19:16:01 -0800

    Revert "Hotfix LocalMR test."
    This reverts commit 5785b972dec4f770d05a21cad8ea8cddd82593a4.

--- h2o-core/src/test/java/water/LocalMRTest.java
+++ h2o-core/src/test/java/water/LocalMRTest.java
@@ -22,13 +22,10 @@ public class LocalMRTest extends TestUtil {
     public int [] _val;
     public MrFunTest1(int exId){_exId = exId;}
     public void map(int id){
-      if(_val == null)_val = new int[]{id};
-      else _val = ArrayUtils.append(_val,id);
-
+      _val = new int[]{id};
     }
     public void reduce(MrFunTest1 other){
-      if(_val == null) _val = other._val;
-      else _val = ArrayUtils.sortedMerge(_val,other._val);
+      _val = ArrayUtils.sortedMerge(_val,other._val);
     }
   }
 
