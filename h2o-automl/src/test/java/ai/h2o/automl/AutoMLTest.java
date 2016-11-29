9cdd4388a5de07f2b3e69be3bc7b05c13fd57e7c
Nidhi Mehta <nidhimehta@Nbook.local>
2016-11-29 09:35:45 -0700

    added additional frame metadata calculations

--- h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
@@ -5,6 +5,8 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import water.Key;
 import water.api.schemas3.ImportFilesV3;
+//import org.apache.commons.logging.Log;
+
 import water.fvec.Frame;
 
 public class AutoMLTest extends TestUtil {
@@ -48,19 +50,31 @@ public class AutoMLTest extends TestUtil {
 //  }
 
   @Test public void SanTanderTest() {
-    Frame fr=null;
+    //Frame fr=null;
     AutoML aml=null;
     try {
       // was: makeAutoML(Key<AutoML> key, String datasetPath, String[] relationPaths, String responseName, String loss, long maxTime, double minAccuracy, boolean ensemble, algo[] excludeAlgos, boolean tryMutations )
       AutoMLBuildSpec autoMLBuildSpec = new AutoMLBuildSpec();
       autoMLBuildSpec.input_spec.training_path = new ImportFilesV3.ImportFiles();
-      autoMLBuildSpec.input_spec.training_path.path = "smalldata/santander/train.csv.zip";
-      autoMLBuildSpec.input_spec.response_column = "TARGET";
-      autoMLBuildSpec.build_control.loss = "MSE";
-      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(3600);
+      //autoMLBuildSpec.input_spec.training_path.path = "smalldata/santander/train.csv.zip";
+      //autoMLBuildSpec.input_spec.response_column = "TARGET";
+      //autoMLBuildSpec.build_control.loss = "MSE";
+      //autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(3600);
+
+      //autoMLBuildSpec.input_spec.training_path.path = "/Users/nidhimehta/Desktop/frame_meta_testFiles/dum_2num.csv";
+      //autoMLBuildSpec.input_spec.response_column = "class";
+      autoMLBuildSpec.input_spec.training_path.path = "smalldata/allyears2k_headers.zip";
+      autoMLBuildSpec.input_spec.response_column = "IsDepDelayed";
+      autoMLBuildSpec.build_control.loss = "AUTO";
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(300);
 
       aml = AutoML.makeAutoML(Key.<AutoML>make(), autoMLBuildSpec);
       aml.learn();
+
+      aml = AutoML.startAutoML(autoMLBuildSpec);
+      //Assert.assertTrue(aml.frameMetadata._cols[0]._skew == 0.31071214388181395);
+      System.out.print(aml.frameMetadata._cols[0]._skew);
+
     } finally {
       // cleanup
       if(aml!=null) aml.delete();
