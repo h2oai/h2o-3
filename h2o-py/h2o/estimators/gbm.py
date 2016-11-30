59a65fd8769523fa8dbd7701d9cd310680ce36d5
Vlad Patryshev <vpatryshev@gmail.com>
2016-11-30 11:36:11 -0800

    merges from master

--- h2o-py/h2o/estimators/gbm.py
+++ h2o-py/h2o/estimators/gbm.py
@@ -256,7 +256,9 @@ class H2OGradientBoostingEstimator(H2OEstimator):
 
     @property
     def max_confusion_matrix_size(self):
-        """int: [Deprecated] Maximum size (# classes) for confusion matrices to be printed in the Logs (Default: 20)"""
+        """
+        int: [Deprecated] Maximum size (# classes) for confusion matrices to be printed in the Logs (Default: 20)
+        """
         return self._parms.get("max_confusion_matrix_size")
 
     @max_confusion_matrix_size.setter
