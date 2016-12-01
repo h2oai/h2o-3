8b8f39f4b3495ae690b024fd1942adb57d612bbc
Tomas Nykodym <tomas.nykodym@gmail.com>
2016-11-30 17:02:28 -0800

    PUBDEV-3752 glm hot fix. fix progress update.

--- h2o-algos/src/main/java/hex/glm/GLM.java
+++ h2o-algos/src/main/java/hex/glm/GLM.java
@@ -741,7 +741,7 @@ public class GLM extends ModelBuilder<GLMModel,GLMParameters,GLMOutput> {
         _state._u = l1Solver._u;
         _state.updateState(beta,gslvr.getGradient(beta));
       } else {
-        if(_state._iter == 0)
+        if(!_parms._lambda_search && _state._iter == 0)
           updateProgress(false);
         Result r = lbfgs.solve(gslvr, beta, _state.ginfo(), new ProgressMonitor() {
           @Override
