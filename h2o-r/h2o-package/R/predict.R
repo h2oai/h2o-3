079047e9009f37226c1bcd72a61c2e27d9c59c98
Magnus Stensmo <mstensmo@gmail.com>
2016-12-07 11:43:06 -0800

    optional classpath argument

--- h2o-r/h2o-package/R/predict.R
+++ h2o-r/h2o-package/R/predict.R
@@ -1,4 +1,21 @@
-library("jsonlite")
+#'
+#' H2O Prediction from R
+#'
+#' Provides the method h2o.predict with which you can predict a MOJO or POJO Jar model
+#' from R.
+#'
+#' @param model  String with file name of MOJO or POJO Jar
+#' @param json  JSON String with inputs to model
+#' @param classpath  (Optional) Extra items for the class path of where to look for Java classes, e.g., h2o-genmodel.jar
+#' @return Returns an object with the prediction result
+#' @importFrom jsonlite fromJSON
+#' @examples
+#' \donttest{
+#' library(h2o)
+#' h2o.predict('~/GBM_model_python_1473313897851_6.zip', '{"C7":1}')
+#' h2o.predict('~/GBM_model_python_1473313897851_6.zip', '{"C7":1}', c(".", "lib"))
+#' }
+#' @export
 
 h2o.predict_java <- "java"
 h2o.predict_javaopts <- "-Xmx4g"
@@ -9,7 +26,12 @@ if (h2o.predict_iswindows) { h2o.predict_separator <- ";" } else { h2o.predict_s
 h2o.predict_classpath <- paste(h2o.predict_javapath, sep="", collapse=h2o.predict_separator)
 h2o.predict_javaargs <- paste(" ", h2o.predict_javaopts, " -cp ", h2o.predict_classpath, " water.util.H2OPredictor", sep="")
 
-h2o.predict <- function(model, json) {
+h2o.predict <- function(model, json, classpath) {
+	if (!missing(classpath)) {
+	   h2o.predict_javapath <- c( classpath, h2o.predict_javapath )
+	   h2o.predict_classpath <- paste(h2o.predict_javapath, sep="", collapse=h2o.predict_separator)
+	   h2o.predict_javaargs <- paste(" ", h2o.predict_javaopts, " -cp ", h2o.predict_classpath, " water.util.H2OPredictor", sep="")
+        }
 	if (h2o.predict_iswindows) { jsonq <- paste('"', json, '"', sep="") } else { jsonq <- paste("'", json, "'", sep="") } 
 	args <- paste(h2o.predict_javaargs, model, jsonq, sep=" ")
 	res <- system2(h2o.predict_java, args, stdout=TRUE, stderr=TRUE)
