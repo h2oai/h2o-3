4c419e1171d927b7d92e2674aceebb9caaf1b800
Vlad Patryshev <vpatryshev@gmail.com>
2016-12-11 14:37:14 -0800

    Works with DlData

---
+++ h2o-algos/src/main/java/hex/DataContainer.java
@@ -0,0 +1,13 @@
+package hex;
+
+import water.Key;
+import water.Keyed;
+
+/**
+ * Replaces DataInfo, which mixes together everying,
+ * Created by vpatryshev on 12/11/16.
+ * 
+ */
+public class DataContainer<X> {
+  public DataContainer() {}
+}
