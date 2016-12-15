913b909609ec2d894cc62d9153328522402f269e
Navdeep Gill <navdeep@h2o.ai>
2016-12-15 11:43:51 -0800

    :fire: [HOTFIX]: Store MOJO in tmp directory then remove directory on exit

--- h2o-r/tests/testdir_misc/runit_mojo.R
+++ h2o-r/tests/testdir_misc/runit_mojo.R
@@ -2,9 +2,13 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
 
 test.mojo <- function() {
+	dir <- sandbox() #Set up tmp directory to write MOJO
 	iris.hex <- h2o.uploadFile(locate("smalldata/iris/iris.csv"), "iris.hex")
 	hh <- h2o.gbm(x=c(1,2,3,4),y=5,training_frame=iris.hex)
-	h2o.download_mojo(hh) #check mojo
-	h2o.download_mojo(hh,get_genmodel_jar=TRUE) #Check genmodel.jar
+	h2o.download_mojo(hh,path=dir) #check mojo
+	h2o.download_mojo(hh,path=dir,get_genmodel_jar=TRUE) #Check genmodel.jar
+
+	#Delete tmp directory
+	on.exit(unlink(dir,recursive=TRUE))
 }
 doTest("Test MOJO", test.mojo)
