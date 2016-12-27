99c635c00e9a366db093be99ce8cd33e7279c094
Navdeep Gill <navdeep@h2o.ai>
2016-12-27 15:51:31 -0800

    Modify runit for pojo/mojo

--- h2o-r/tests/testdir_misc/runit_mojo.R
+++ h2o-r/tests/testdir_misc/runit_mojo.R
@@ -2,9 +2,29 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
 
 test.mojo <- function() {
+	#Set up tmp directory to write MOJO
+	dir <- dir.create(file.path(sandbox(),"mojos"))
+	#Set up path as dir.create() just sets up the directory and returns a logical based on if if failed or not
+	path <- file.path(sandbox(),"mojos")
+
+	#Build GBM model
 	iris.hex <- h2o.uploadFile(locate("smalldata/iris/iris.csv"), "iris.hex")
 	hh <- h2o.gbm(x=c(1,2,3,4),y=5,training_frame=iris.hex)
-	h2o.download_mojo(hh) #check mojo
-	h2o.download_mojo(hh,get_genmodel_jar=TRUE) #Check genmodel.jar
+
+	#Download MOJO
+	print(sprintf("Dowloading MOJO..."))
+	start_time <- Sys.time()
+	mojo_file <- h2o.download_mojo(hh,path=path) #check mojo
+	end_time <- Sys.time()
+	print(sprintf("MOJO file is %s bytes", object.size(mojo_file)))
+	#Just a check for MOJO size (PUBDEV 3819)
+	expect(object.size(mojo_file) > 1, "MOJO file should be greater than 1 byte!")
+	print(sprintf("Time taken to dowloand MOJO: %s",end_time - start_time))
+
+	#Download genmodel
+	h2o.download_mojo(hh,path=path,get_genmodel_jar=TRUE) #Check genmodel.jar
+
+	#Delete tmp directory
+	on.exit(unlink(path,recursive=TRUE))
 }
 doTest("Test MOJO", test.mojo)
--- h2o-r/tests/testdir_misc/runit_pojo.R
+++ h2o-r/tests/testdir_misc/runit_pojo.R
@@ -1,12 +1,28 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
-#----------------------------------------------------------------------
-# Purpose:  This test is a sanity check for a pojo download
-#----------------------------------------------------------------------
+
 test.pojo <- function() {
+	#Set up tmp directory to write POJO
+	dir <- dir.create(file.path(sandbox(),"pojos"))
+	#Set up path as dir.create() just sets up the directory and returns a logical based on if if failed or not
+	path <- file.path(sandbox(),"pojos")
+
+	#Build GBM model
 	iris.hex <- h2o.uploadFile(locate("smalldata/iris/iris.csv"), "iris.hex")
 	hh <- h2o.gbm(x=c(1,2,3,4),y=5,training_frame=iris.hex)
-	h2o.download_pojo(hh) #check pojo
-	h2o.download_pojo(hh,get_jar=TRUE) #Check genmodel.jar
+
+	#Download POJO
+	print(sprintf("Dowloading POJO..."))
+	start_time <- Sys.time()
+	pojo_file <- h2o.download_pojo(hh,path=path) #check pojo
+	end_time <- Sys.time()
+	print(sprintf("POJO file is %s bytes", object.size(pojo_file)))
+	print(sprintf("Time taken to dowloand POJO: %s",end_time - start_time))
+
+	#Download genmodel
+	h2o.download_pojo(hh,path=path,get_jar=TRUE) #Check genmodel.jar
+
+	#Delete tmp directory
+	on.exit(unlink(path,recursive=TRUE))
 }
 doTest("Test POJO", test.pojo)
