44aa12b4970c0f6f1c277f879a91871ad64343f4
Pasha Stetsenko <pasha@h2o.ai>
2016-12-29 16:11:53 -0800

    Move exceptions definitions from StdlibFunction into Function

--- h2o-core/src/main/java/ai/h2o/cascade/core/Function.java
+++ h2o-core/src/main/java/ai/h2o/cascade/core/Function.java
@@ -1,6 +1,7 @@
 package ai.h2o.cascade.core;
 
 import ai.h2o.cascade.vals.Val;
+import ai.h2o.cascade.Cascade;
 
 /**
  */
@@ -8,5 +9,63 @@ public abstract class Function {
   public Scope scope;
 
   public abstract Val apply0(Val[] args);
+
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Exceptions
+  //--------------------------------------------------------------------------------------------------------------------
+
+  /**
+   * Exception to be raised within the body of a function, indicating that the
+   * type of the {@code index}'th argument is incorrect.
+   *
+   * <p> If the type error refers to overall shape of all arguments, then
+   * {@code index} will be -1.
+   *
+   * <p> This exception will be caught within {@code AstApply} and converted
+   * into a {@link Cascade.TypeError}, translating the argument's {@code index}
+   * into its location within the cascade expression being executed.
+   */
+  public static class TypeError extends IllegalArgumentException {
+    public int index;
+
+    public TypeError(int i, String message) {
+      super(message);
+      index = i;
+    }
+  }
+
+
+  /**
+   * Exception to be raised within the body of a function, indicating that the
+   * value of the {@code i}'th argument is incorrect.
+   *
+   * <p> This exception will be caught within {@code AstApply} and converted
+   * into a {@link Cascade.ValueError}, translating the argument's
+   * {@code index} into its location within the cascade expression being
+   * executed.
+   */
+  public static class ValueError extends IllegalArgumentException {
+    public int index;
+
+    public ValueError(int i, String message) {
+      super(message);
+      index = i;
+    }
+  }
+
+
+  /**
+   * Exception to be raised within the body of a function, indicating an error
+   * condition that is not related to any argument in particular.
+   *
+   * <p> This exception will be caught within {@code AstApply} and converted
+   * into a {@link Cascade.RuntimeError}.
+   */
+  public static class RuntimeError extends RuntimeException {
+    public RuntimeError(String message) {
+      super(message);
+    }
+  }
 
 }
--- h2o-core/src/main/java/ai/h2o/cascade/stdlib/StdlibFunction.java
+++ h2o-core/src/main/java/ai/h2o/cascade/stdlib/StdlibFunction.java
@@ -22,65 +22,6 @@ public abstract class StdlibFunction extends Function {
   }
 
 
-  //--------------------------------------------------------------------------------------------------------------------
-  // Exceptions
-  //--------------------------------------------------------------------------------------------------------------------
-
-  /**
-   * Exception to be raised within the body of an stdlib function, indicating
-   * that the type of the {@code index}'th argument is incorrect.
-   *
-   * <p>If the type error refers to overall shape of all arguments, then
-   * {@code index} will be -1.
-   *
-   * <p>This exception will be caught within {@code AstApply} and converted
-   * into a {@link ai.h2o.cascade.Cascade.TypeError}, translating the
-   * argument's {@code index} into its location within the cascade expression
-   * being executed.
-   */
-  public static class TypeError extends IllegalArgumentException {
-    public int index;
-
-    public TypeError(int i, String message) {
-      super(message);
-      index = i;
-    }
-  }
-
-
-  /**
-   * Exception to be raised within the body of an stdlib function, indicating
-   * that the value of the {@code i}'th argument is incorrect.
-   *
-   * <p>This exception will be caught within {@code AstApply} and converted
-   * into a {@link ai.h2o.cascade.Cascade.ValueError}, translating the
-   * argument's {@code index} into its location within the cascade expression
-   * being executed.
-   */
-  public static class ValueError extends IllegalArgumentException {
-    public int index;
-
-    public ValueError(int i, String message) {
-      super(message);
-      index = i;
-    }
-  }
-
-
-  /**
-   * Exception to be raised within the body of an stdlib function, indicating
-   * an error condition that is not related to any argument in particular.
-   *
-   * <p>This exception will be caught within {@code AstApply} and converted
-   * into a {@link ai.h2o.cascade.Cascade.RuntimeError}.
-   */
-  public static class RuntimeError extends RuntimeException {
-    public RuntimeError(String message) {
-      super(message);
-    }
-  }
-
-
 
   //--------------------------------------------------------------------------------------------------------------------
   // Helpers for apply0()
