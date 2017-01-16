4bbaf840b3b41ce3521fcaf85e0653a2db2a375b
Danilo Bassi <danilobassi@Danilos-MacBook-Pro.local>
2017-01-16 15:35:35 -0800

    basic backend test (class initialization)

--- h2o-genmodel/src/main/java/hex/genmodel/algos/deepwater/DeepwaterBackend.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/deepwater/DeepwaterBackend.java
@@ -6,7 +6,7 @@ import deepwater.backends.BackendTrain;
 import deepwater.backends.RuntimeOptions;
 import deepwater.datasets.ImageDataSet;
 
-public class  DeepwaterCaffeBackend implements BackendTrain {
+public class DeepwaterBackend implements BackendTrain {
   // TODO
   @Override
   public void delete(BackendModel m) {
@@ -17,9 +17,7 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
   public BackendModel buildNet(ImageDataSet dataset, RuntimeOptions opts, BackendParams bparms, int num_classes, String name /*ignored for now*/) {
     if (name.equals("MLP")) {
       // TODO: add non-MLP Models such as lenet, inception_bn, etc.
-      // Basic testing
-      System.out.println("Caffe backend: building new MLP model");
-      return new DeepwaterCaffeModel(
+      return new DeepwaterModel(
               num_classes,
               ((Integer) bparms.get("mini_batch_size")).intValue(),
               (int[]) bparms.get("hidden"),
@@ -34,8 +32,6 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
   // graph (model definition) only
   @Override
   public void saveModel(BackendModel m, String model_path) {
-    // Basic testing
-    System.out.println("Caffe backend: building new MLP model");
 
   }
 
@@ -43,8 +39,6 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
   // full state of everything but the graph to continue training
   @Override
   public void loadParam(BackendModel m, String param_path) {
-    // Basic testing
-    System.out.println("Caffe backend: building new MLP model");
 
   }
 
@@ -52,8 +46,6 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
   // full state of everything but the graph to continue training
   @Override
   public void saveParam(BackendModel m, String param_path) {
-    // Basic testing
-    System.out.println("Caffe backend: building new MLP model");
 
   }
 
@@ -70,21 +62,18 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
   // DONE
   @Override
   public void setParameter(BackendModel m, String name, float value) {
-    // Basic testing
-    System.out.println("Caffe backend: building new MLP model");
     if (name.equals("learning_rate"))
-      ((DeepwaterCaffeModel)m)._learning_rate = value;
+      ((DeepwaterCaffeModel) m)._learning_rate = value;
     else if (name.equals("momentum"))
-      ((DeepwaterCaffeModel)m)._momentum = value;
+      ((DeepwaterCaffeModel) m)._momentum = value;
   }
 
   // TODO
   // given a mini-batch worth of data and labels, train
   @Override
   public float[]/*ignored*/ train(BackendModel m, float[/*mini_batch * input_neurons*/] data, float[/*mini_batch*/] label) {
-    // Basic testing
-    System.out.println("Caffe backend: building new MLP model");
     // implement training here
+
 
     return null; //return value is always ignored
   }
@@ -98,13 +87,11 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
   // return predictions (num_classes logits (softmax outputs) x mini_batch)
   @Override
   public float[/*mini_batch * num_classes*/] predict(BackendModel m, float[/*mini_batch * input_neurons*/] data) {
-    // Basic testing
-    System.out.println("Caffe backend: building new MLP model");
-    DeepwaterCaffeModel cm = ((DeepwaterCaffeModel)m);
-    return new float[cm.mini_batch_size*cm.num_classes];
+    DeepwaterModel cm = ((DeepwaterModel) m);
+    return new float[cm.mini_batch_size * cm.num_classes];
   }
 
-  private static class DeepwaterCaffeModel implements BackendModel {
+  private static class DeepwaterModel implements BackendModel {
     public DeepwaterCaffeModel(int num_classes, int mini_batch_size, int[] hidden, String[] activations, double input_dropout_ratio, double[] hidden_dropout_ratios) {
       this.num_classes = num_classes;
       this.mini_batch_size = mini_batch_size;
@@ -125,4 +112,43 @@ public class  DeepwaterCaffeBackend implements BackendTrain {
     float _learning_rate;
     float _momentum;
   }
+
+  /**
+   * DeepwaterLayer is the basic node used for backend implementations
+   * Layer: array of units (neurons) sharing parameters (activation, etc.)
+   */
+
+  private static class DeepwaterLayer {
+    public DeepwaterLayer(String name, String type, int[] shape, int width, String[] activation, double dropout_ratio, String type_spec) {
+      this.name = name;
+      this.type = type;
+      this.width = width;
+      this.activation = activation;
+      this.dropout_ratio = dropout_ratio;
+      this.type_spec = type_spec;
+    }
+    // Fixed parameters
+    String name;              // Layer name that describe this node (should be unique)
+    String type;              // Layer type: input (buffer), full/sparse connected, convolution, etc.
+    int [] shape;             // Dimensions of the layer (may be used used for input/output layers)
+    int width;                // Layer width: number of units (neurons), output vector size
+    String activation;        // Activation function for the layer (one per layer)
+    double dropout_ratio;     // Dropout factor for the layer
+    String type_spec;         // String for type structure specification (if needed), Json
+    int [] next_layer;        // Adjacency list, for linear topology it's just next layer
+    // Variable parameters
+    LayerLearning learning_pars;  // Learning parameters for layer (adjustable)
+    Key[] weights;
+    Key[] biases;
+  }
+
+
+
+
+
+
+
+
+
+
 }
