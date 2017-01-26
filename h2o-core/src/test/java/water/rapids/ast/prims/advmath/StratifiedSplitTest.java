38417b27372e216cc0a4945407e3e17b612fb6fc
Mark Chan <mkchan@gmail.com>
2017-01-25 21:54:44 -0800

    test improvements

--- h2o-core/src/test/java/water/rapids/ast/prims/advmath/StratifiedSplitTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/advmath/StratifiedSplitTest.java
@@ -35,21 +35,16 @@ public class StratifiedSplitTest extends TestUtil{
         fanimal._key = Key.make();
         DKV.put(f);
         DKV.put(fanimal);
-        Val res1 = Rapids.exec("(h2o.random_stratified_split (cols_py " + f._key + " 0) 0.3333333 222)"); //
+        Val res1 = Rapids.exec("(h2o.random_stratified_split (cols_py " + f._key + " 0) 0.3333333 123)"); //
         fr1 = res1.getFrame();
-        //Assert.assertEquals(fr1.vec(0).at8(0),1);  // minority class should be in the test split
-        //Assert.assertEquals(fr1.vec(0).at8(11),0);  // minority class should be in the train split
-        for (int i = 0; i < fr1.anyVec().length(); i++) {
-            System.out.println(fr1.anyVec().at8(i));
-        }
-        System.out.println("\n");
+        Assert.assertEquals(fr1.vec(0).at8(0),1);  // minority class should be in the test split
+        Assert.assertEquals(fr1.vec(0).at8(11),0);  // minority class should be in the train split
+        Assert.assertEquals(fr1.vec(0).mean(),1.0/3.0,1e-5);  // minority class should be in the train split
         //test categorical
-        Val res2 = Rapids.exec("(h2o.random_stratified_split (cols_py " + fanimal._key + " 0) 0.3333333 222)"); //
+        Val res2 = Rapids.exec("(h2o.random_stratified_split (cols_py " + fanimal._key + " 0) 0.3333333 123)"); //
         fr2 = res2.getFrame();
-        for (int i = 0; i < fr2.anyVec().length(); i++) {
-            System.out.println(fr2.anyVec().at8(i));
-        }
-        //Assert.assertEquals(fr2.vec(0).at8(0),1);  // minority class should be in the test split
-        //Assert.assertEquals(fr2.vec(0).at8(11),0);  // minority class should be in the test split
+        Assert.assertEquals(fr2.vec(0).at8(0),1);  // minority class should be in the test split
+        Assert.assertEquals(fr2.vec(0).at8(11),0);  // minority class should be in the test split
+        Assert.assertEquals(fr2.vec(0).mean(),1.0/3.0,1e-5);  // minority class should be in the test split
     }
 }
