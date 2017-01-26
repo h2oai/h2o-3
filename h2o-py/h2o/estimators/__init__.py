70ac7acb3499e27acc9dcc5a4ab71b9cce89b969
Arno Candel <arno.candel@gmail.com>
2017-01-26 01:27:56 -0800

    Add R/Py bindings for XGBoost.

--- h2o-py/h2o/estimators/__init__.py
+++ h2o-py/h2o/estimators/__init__.py
@@ -19,11 +19,12 @@ from .random_forest import H2ORandomForestEstimator
 from .stackedensemble import H2OStackedEnsembleEstimator
 from .svd import H2OSingularValueDecompositionEstimator
 from .word2vec import H2OWord2vecEstimator
+from .xgboost import H2OXGBoostEstimator
 
 __all__ = (
     "H2OAggregatorEstimator", "H2OAutoEncoderEstimator", "H2ODeepLearningEstimator", "H2ODeepWaterEstimator",
     "H2OEstimator", "H2OGradientBoostingEstimator", "H2OGeneralizedLinearEstimator", "H2OGeneralizedLowRankEstimator",
     "H2OKMeansEstimator", "H2ONaiveBayesEstimator", "H2OPrincipalComponentAnalysisEstimator",
     "H2ORandomForestEstimator", "H2OStackedEnsembleEstimator", "H2OSingularValueDecompositionEstimator",
-    "H2OWord2vecEstimator"
+    "H2OWord2vecEstimator", "H2OXGBoostEstimator"
 )
