56f7ee30c8d8d0bba3dfd097b486375616a2b4ad
Tomas Nykodym <tomas.nykodym@gmail.com>
2017-01-26 14:04:56 -0800

    PUBDEV-3966 hot fix , do not store training frame in data info in glm models, vecs can be deleted in the meantime and can hit NPE.

--- h2o-algos/src/test/java/hex/DataInfoTestAdapt.java
+++ h2o-algos/src/test/java/hex/DataInfoTestAdapt.java
@@ -46,10 +46,8 @@ public class DataInfoTestAdapt extends TestUtil {
       GLMModel.GLMParameters parms = new GLMModel.GLMParameters();
       parms._response_column = "petal_wid";
       Model.adaptTestForTrain(frSplits[1],null,null,dinfo._adaptedFrame.names(),dinfo._adaptedFrame.domains(),parms,true,false,interactions,null,null, false);
-
-      scoreInfo = dinfo.scoringInfo(frSplits[1]);
+      scoreInfo = dinfo.scoringInfo(dinfo._adaptedFrame._names,frSplits[1]);
       checkFrame(scoreInfo,expandSplits[1]);
-
     } finally {
       cleanup(fr,expanded);
       cleanup(frSplits);
@@ -94,7 +92,7 @@ public class DataInfoTestAdapt extends TestUtil {
       parms._response_column = "IsDepDelayed";
       Model.adaptTestForTrain(frSplits[1],null,null,dinfo._adaptedFrame.names(),dinfo._adaptedFrame.domains(),parms,true,false,interactions,null,null, false);
 
-      scoreInfo = dinfo.scoringInfo(frSplits[1]);
+      scoreInfo = dinfo.scoringInfo(dinfo._adaptedFrame._names,frSplits[1]);
       checkFrame(scoreInfo,expandSplits[1], skipMissing);
 
     } finally {
