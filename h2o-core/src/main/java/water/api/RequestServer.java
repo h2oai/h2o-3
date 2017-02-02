9ae31216658167b30692985257239b3c6dad052d
spennihana <spnrpa@gmail.com>
2017-02-02 10:23:21 -0800

    Expose a way to add custom http logging filters (#691)
    * expose a way to add custom http logging filters
    - for easier testing, use Mockito (new dependency) to mock HttpServlet params
    
    * specify 2.6.2 version of mockito
    
    * avoid static blocks

--- h2o-core/src/main/java/water/api/RequestServer.java
+++ h2o-core/src/main/java/water/api/RequestServer.java
@@ -80,6 +80,11 @@ public class RequestServer extends HttpServlet {
   public static ArrayList<Route> routes() { return routesList; }
   public static Route lookupRoute(RequestUri uri) { return routesTree.lookup(uri, null); }
 
+  private static HttpLogFilter[] _filters=new HttpLogFilter[]{defaultFilter()};
+  public static void setFilters(HttpLogFilter... filters) {
+    _filters=filters;
+  }
+
   /**
    * Some HTTP response status codes
    */
@@ -474,25 +479,44 @@ public class RequestServer extends HttpServlet {
    * Log the request (unless it's an overly common one).
    */
   private static void maybeLogRequest(RequestUri uri, Properties header, Properties parms) {
+    for(HttpLogFilter f: _filters)
+      if( f.filter(uri,header,parms) ) return; // do not log anything if filtered
     String url = uri.getUrl();
-    if (url.endsWith(".css") ||
-        url.endsWith(".js") ||
-        url.endsWith(".png") ||
-        url.endsWith(".ico")) return;
-
-    String[] path = uri.getPath();
-    if (path[2].equals("Cloud") ||
-        path[2].equals("Jobs") && uri.isGetMethod() ||
-        path[2].equals("Log") ||
-        path[2].equals("Progress") ||
-        path[2].equals("Typeahead") ||
-        path[2].equals("WaterMeterCpuTicks")) return;
-
     Log.info(uri + ", parms: " + parms);
     GAUtils.logRequest(url, header);
   }
 
+  /**
+   * Create a new HttpLogFilter.
+   *
+   * Implement this interface to create new filters used by maybeLogRequest
+   */
+  public interface HttpLogFilter {
+    boolean filter(RequestUri uri, Properties header, Properties parms);
+  }
 
+  /**
+   * Provide the default filters for H2O's HTTP logging.
+   * @return an array of HttpLogFilter instances
+   */
+  public static HttpLogFilter defaultFilter() {
+    return new HttpLogFilter() { // this is much prettier with 1.8 lambdas
+      @Override public boolean filter(RequestUri uri, Properties header, Properties parms) {
+        String url = uri.getUrl();
+        if (url.endsWith(".css") ||
+          url.endsWith(".js")  ||
+          url.endsWith(".png") ||
+          url.endsWith(".ico")) return true;
+        String[] path = uri.getPath();
+        return path[2].equals("Cloud") ||
+          path[2].equals("Jobs") && uri.isGetMethod() ||
+          path[2].equals("Log") ||
+          path[2].equals("Progress") ||
+          path[2].equals("Typeahead") ||
+          path[2].equals("WaterMeterCpuTicks");
+      }
+    };
+  }
 
   //------ Lookup tree for Routes --------------------------------------------------------------------------------------
 
