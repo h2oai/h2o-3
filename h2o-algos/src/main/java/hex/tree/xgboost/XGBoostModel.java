12b2d8f440061cf2c35984fa5b0ac7f1e4dc1c84
Arno Candel <arno.candel@gmail.com>
2017-02-05 23:14:38 -0800

    Towards faster XGBoost bulk scoring in Model.score(Frame f).
    Includes:
      - Only use grow_gpu if env var CUDA_PATH is set.
      - Fix multinomial bulk scoring for XGBoost.
      - Make the REST endpoint use the new bulk scoring method.
      - Publish the scored frame in DKV.

--- h2o-algos/src/main/java/hex/tree/xgboost/XGBoostModel.java
+++ h2o-algos/src/main/java/hex/tree/xgboost/XGBoostModel.java
@@ -1,19 +1,23 @@
 package hex.tree.xgboost;
 
 import hex.*;
+import hex.genmodel.GenModel;
 import hex.genmodel.algos.xgboost.XGBoostMojoModel;
 import hex.genmodel.utils.DistributionFamily;
 import ml.dmlc.xgboost4j.java.Booster;
 import ml.dmlc.xgboost4j.java.DMatrix;
 import ml.dmlc.xgboost4j.java.XGBoostError;
 import water.*;
+import water.fvec.Chunk;
 import water.fvec.Frame;
+import water.fvec.NewChunk;
 import water.fvec.Vec;
 import water.util.Log;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static hex.tree.xgboost.XGBoost.convertFrametoDMatrix;
 import static hex.tree.xgboost.XGBoost.makeDataInfo;
 
 public class XGBoostModel extends Model<XGBoostModel, XGBoostModel.XGBoostParameters, XGBoostOutput> {
@@ -191,7 +195,8 @@ public class XGBoostModel extends Model<XGBoostModel, XGBoostModel.XGBoostParame
       params.put("one_drop", p._one_drop ? "1" : "0");
       params.put("skip_drop", p._skip_drop);
     }
-    params.put("updater", "grow_gpu");
+    if (System.getenv("CUDA_PATH") != null)
+      params.put("updater", "grow_gpu");
     if (p._min_child_weight!=0) {
       Log.info("Using user-provided parameter min_child_weight instead of min_rows.");
       params.put("min_child_weight", p._min_child_weight);
@@ -251,16 +256,16 @@ public class XGBoostModel extends Model<XGBoostModel, XGBoostModel.XGBoostParame
   // For multinomial, we also need to transpose the data - which is slow
   private ModelMetrics makeMetrics(Booster booster, DMatrix data) throws XGBoostError {
     ModelMetrics[] mm = new ModelMetrics[1];
-    makePreds(booster, data, mm).remove();
+    makePreds(booster, data, mm, Key.<Frame>make()).remove();
     return mm[0];
   }
 
-  private Frame makePreds(Booster booster, DMatrix data, ModelMetrics[] mm) throws XGBoostError {
+  private Frame makePreds(Booster booster, DMatrix data, ModelMetrics[] mm, Key<Frame> destinationKey) throws XGBoostError {
     Frame predFrame;
-    float[][] preds = booster.predict(data);
+    final float[][] preds = booster.predict(data);
     Vec resp = Vec.makeVec(data.getLabel(), Vec.newKey());
     float[] weights = data.getWeight();
-    if (_output.nclasses()<=2) {
+    if (_output.nclasses()==1) {
       double[] dpreds = new double[preds.length];
       for (int j = 0; j < dpreds.length; ++j)
         dpreds[j] = preds[j][0];
@@ -268,28 +273,57 @@ public class XGBoostModel extends Model<XGBoostModel, XGBoostModel.XGBoostParame
         for (int j = 0; j < dpreds.length; ++j)
           assert weights[j] == 1.0;
       Vec pred = Vec.makeVec(dpreds, Vec.newKey());
-      if (_output.nclasses() == 1) {
-        mm[0] = ModelMetricsRegression.make(pred, resp, DistributionFamily.gaussian);
-      } else {
-        mm[0] = ModelMetricsBinomial.make(pred, resp);
-      }
-      predFrame = new Frame(Key.<Frame>make(), new Vec[]{pred}, true);
+      mm[0] = ModelMetricsRegression.make(pred, resp, DistributionFamily.gaussian);
+      predFrame = new Frame(destinationKey, new Vec[]{pred}, true);
     }
-    else {
-      // ugly: need to transpose the data to put it into a Frame to score -> could be sped up
-      double[][] dpreds = new double[_output.nclasses()][preds.length];
-      for (int i = 0; i < dpreds.length; ++i) {
-        for (int j = 0; j < dpreds[i].length; ++j) {
-          dpreds[i][j] = preds[j][i];
+    else if (_output.nclasses()==2) {
+      double[] dpreds = new double[preds.length];
+      for (int j = 0; j < dpreds.length; ++j)
+        dpreds[j] = preds[j][0];
+      if (weights.length>0)
+        for (int j = 0; j < dpreds.length; ++j)
+          assert weights[j] == 1.0;
+      Vec p1 = Vec.makeVec(dpreds, Vec.newKey());
+      Vec p0 = p1.makeCon(0);
+      Vec label = p1.makeCon(0., Vec.T_CAT);
+      new MRTask() {
+        public void map(Chunk l, Chunk p0, Chunk p1) {
+          for (int i=0; i<l._len; ++i) {
+            double p = p1.atd(i);
+            p0.set(i, 1. - p);
+            double[] row = new double[]{0, 1-p, p};
+            l.set(i, hex.genmodel.GenModel.getPrediction(row, _output._priorClassDist, null, defaultThreshold()));
+          }
         }
-      }
-      Vec[] pred = new Vec[_output.nclasses()];
-      for (int i = 0; i < pred.length; ++i) {
-        pred[i] = Vec.makeVec(dpreds[i], Vec.newKey());
-      }
-      predFrame = new Frame(Key.<Frame>make(),pred,true);
+      }.doAll(label,p0,p1);
+      mm[0] = ModelMetricsBinomial.make(p1, resp);
+      label.setDomain(new String[]{"N","Y"}); // ignored
+      predFrame = new Frame(destinationKey, new Vec[]{label,p0,p1}, true);
+    }
+
+    else {
+      String[] names = makeScoringNames();
+      String[][] domains = new String[names.length][];
+      domains[0] = _output.classNames();
+      Frame input = new Frame(resp); //has the right size
+      predFrame = new MRTask() {
+        public void map(Chunk[] chk, NewChunk[] nc) {
+          for (int i=0; i<chk[0]._len; ++i) {
+            double[] row = new double[nc.length];
+            for (int j=1;j<row.length;++j) {
+              double val = preds[i][j-1];
+              nc[j].addNum(val);
+              row[j] = val;
+            }
+            nc[0].addNum(hex.genmodel.GenModel.getPrediction(row, _output._priorClassDist, null, defaultThreshold()));
+          }
+        }
+      }.doAll(_output.nclasses()+1, Vec.T_NUM, input).outputFrame(destinationKey, names, domains);
+
+      Frame pp = new Frame(predFrame);
+      pp.remove(0);
       Scope.enter();
-      mm[0] = ModelMetricsMultinomial.make(predFrame, resp, resp.toCategoricalVec().domain());
+      mm[0] = ModelMetricsMultinomial.make(pp, resp, resp.toCategoricalVec().domain());
       Scope.exit();
     }
     resp.remove();
@@ -357,4 +391,39 @@ public class XGBoostModel extends Model<XGBoostModel, XGBoostModel.XGBoostParame
       model_info()._dataInfoKey.get().remove(fs);
     return super.remove_impl(fs);
   }
+
+  @Override
+  public Frame score(Frame fr, String destination_key, Job j, boolean computeMetrics) throws IllegalArgumentException {
+    Frame adaptFr = new Frame(fr);
+    computeMetrics = computeMetrics && (!isSupervised() || (adaptFr.vec(_output.responseName()) != null && !adaptFr.vec(_output.responseName()).isBad()));
+    String[] msg = adaptTestForTrain(adaptFr,true, computeMetrics);   // Adapt
+    try {
+      DMatrix trainMat = convertFrametoDMatrix( model_info()._dataInfoKey, adaptFr,
+          _parms._response_column, _parms._weights_column, _parms._fold_column, null, _output._sparse);
+      ModelMetrics[] mm = new ModelMetrics[1];
+      Frame preds = makePreds(model_info()._booster, trainMat, mm, Key.<Frame>make(destination_key));
+      DKV.put(preds);
+      return preds;
+    } catch (XGBoostError xgBoostError) {
+      xgBoostError.printStackTrace();
+    }
+    return null;
+  }
+
+//  @Override
+//  protected ModelMetrics.MetricBuilder scoreMetrics(Frame adaptFrm) {
+//    Frame adaptFr = new Frame(adaptFrm);
+//    boolean computeMetrics = (!isSupervised() || (adaptFr.vec(_output.responseName()) != null && !adaptFr.vec(_output.responseName()).isBad()));
+//    String[] msg = adaptTestForTrain(adaptFr,true, computeMetrics);   // Adapt
+//    try {
+//      DMatrix trainMat = convertFrametoDMatrix( model_info()._dataInfoKey, adaptFr,
+//          _parms._response_column, _parms._weights_column, _parms._fold_column, null, _output._sparse);
+//      ModelMetrics[] mm = new ModelMetrics[1];
+//      Frame preds = makePreds(model_info()._booster, trainMat, mm);
+//      return mm[0];
+//    } catch (XGBoostError xgBoostError) {
+//      xgBoostError.printStackTrace();
+//    }
+//    return null;
+//  }
 }
