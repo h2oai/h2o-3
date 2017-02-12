391c37be71fd50253ecf8d9b2e890a4eefb139b3
Arno Candel <arno.candel@gmail.com>
2017-02-11 22:25:33 -0800

    Fix golden result after mini-batch size was reduced from 32 to 16.

--- h2o-algos/src/test/java/hex/deepwater/DeepWaterAbstractIntegrationTest.java
+++ h2o-algos/src/test/java/hex/deepwater/DeepWaterAbstractIntegrationTest.java
@@ -93,7 +93,7 @@ public abstract class DeepWaterAbstractIntegrationTest extends TestUtil {
   }
 
   @Test public void trainSamplesPerIteration0() { trainSamplesPerIteration(0,3); }
-  @Test public void trainSamplesPerIteration_auto() { trainSamplesPerIteration(-2,1); }
+  @Test public void trainSamplesPerIteration_auto() { trainSamplesPerIteration(-2,2); }
   @Test public void trainSamplesPerIteration_neg1() { trainSamplesPerIteration(-1,3); }
   @Test public void trainSamplesPerIteration_32() { trainSamplesPerIteration(32,26); }
   @Test public void trainSamplesPerIteration_1000() { trainSamplesPerIteration(1000,1); }
