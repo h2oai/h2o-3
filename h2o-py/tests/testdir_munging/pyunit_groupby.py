b8cf37f8637e99a6d68b911480de1f2a85846659
wendycwong <wendycwong@yahoo.com>
2017-02-14 15:45:29 -0800

    PUBDEV-3914: groupby-docs.  Added documentation for GroupBy.  Found one bug in get_frame and Pasha fixed it. (#781)
    Fixed one bug in frame.py for Pasha for type checking.
    
    PUBDEV-3914: add groupby docs.  Incorporated Pasha code review comments.  Thanks, Pasha.
    
    PUBDEV-3914: add groupby docs.  Added pyunit test to test and verify all group_by results.
    
    PUBDEV-3914: groupby-docs.  Added documentation for GroupBy.  Fix pyunit_groupby_allOPs.py for python 3.

--- h2o-py/tests/testdir_munging/pyunit_groupby.py
+++ h2o-py/tests/testdir_munging/pyunit_groupby.py
@@ -4,23 +4,13 @@ sys.path.insert(1,"../../")
 import h2o
 from tests import pyunit_utils
 
-
-
-import pandas as pd
-import numpy as np
-
 def group_by():
     # Connect to a pre-existing cluster
-    
 
     h2o_iris = h2o.import_file(path=pyunit_utils.locate("smalldata/iris/iris_wheader.csv"))
-    pd_iris = pd.read_csv(pyunit_utils.locate("smalldata/iris/iris_wheader.csv"))
-
-    na_handling = ["rm","all"]
-    col_names = h2o_iris.col_names[0:4]
+    na_handling = ["rm","ignore","all"]
 
     print("Running smoke test")
-
     # smoke test
     for na in na_handling:
       grouped = h2o_iris.group_by("class")
@@ -34,7 +24,7 @@ def group_by():
         .ss(   na=na) \
         .sum(  na=na)
       print(grouped.get_frame())
-
+      print(grouped.get_frame())    # call get_frame() again to ensure that Pasha bug fix works.
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(group_by)
