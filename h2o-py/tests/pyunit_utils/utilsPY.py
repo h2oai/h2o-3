2d0a4af51b36c66d385a1a98ff3016ed069c6f15
Navdeep Gill <navdeep@h2o.ai>
2017-02-15 11:32:42 -0800

    Revert "PUBDEV-3938: orc timestamp offset.  Force python to use local time when comparing orc and csv time data.  Added api test for 4 more commands that were left out. (#772)"
    This reverts commit d015c133d296339048d3a8d2b0e94c855a713848.

--- h2o-py/tests/pyunit_utils/utilsPY.py
+++ h2o-py/tests/pyunit_utils/utilsPY.py
@@ -2628,7 +2628,7 @@ def compareOneStringColumn(frame1, frame2, col_ind, rows, numElements):
     else:
         numElements = rows
 
-    for ele_ind in range(min(rows, numElements)):
+    for ele_ind in range(numElements):
         row_ind = row_indices[ele_ind]
 
         val1 = frame1[row_ind, col_ind]
@@ -2651,14 +2651,15 @@ def compareOneNumericColumn(frame1, frame2, col_ind, rows, tolerance, numElement
     :return: None.  Will throw exceptions if comparison failed.
     """
 
-    row_indices = list(range(rows))
+    row_indices = []
     if numElements > 0:
-        random.shuffle(row_indices)
+        row_indices = random.sample(xrange(rows), numElements)
     else:
-        numElements = rows
+        numElements = rows  # Compare all elements
+        row_indices = list(range(rows))
 
 
-    for ele_ind in range(min(rows, numElements)):
+    for ele_ind in range(numElements):
         row_ind = row_indices[ele_ind]
 
         val1 = frame1[row_ind, col_ind]
@@ -2674,6 +2675,8 @@ def compareOneNumericColumn(frame1, frame2, col_ind, rows, tolerance, numElement
         else:   # something is wrong, one frame got a missing value while the other is fine.
             assert 1 == 2,  "failed frame values check! frame1 value {0}, frame2 value {1} at row {2}, " \
                             "column {3}".format(val1, val2, row_ind, col_ind)
+
+import warnings
 
 def expect_warnings(filewithpath, warn_phrase="warn", warn_string_of_interest="warn", number_of_times=1, in_hdfs=False):
     """
