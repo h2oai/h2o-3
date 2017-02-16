071116d22141e2103db8fb2e4af8909df628b272
Raymond Peck <rpeck@rpeck.com>
2017-02-15 16:17:46 -0800

    AUTOML-121: Put the leaderboard into the AutoML object.

---
+++ h2o-automl/src/main/java/water/automl/api/schemas3/LeaderboardV99.java
@@ -0,0 +1,35 @@
+package water.automl.api.schemas3;
+
+import ai.h2o.automl.Leaderboard;
+import water.api.API;
+import water.api.Schema;
+import water.api.schemas3.KeyV3;
+
+public class LeaderboardV99 extends Schema<Leaderboard, LeaderboardV99> {
+  /**
+   * Identifier for models that should be grouped together in the leaderboard
+   * (e.g., "airlines" and "iris").
+   */
+  @API(help="Identifier for models that should be grouped together in the leaderboard", direction=API.Direction.INOUT)
+  public final String project = "<default>";
+
+  /**
+   * List of models for this leaderboard, sorted by metric so that the best is first
+   * according to the standard metric for the given model type.
+   */
+  @API(help="List of models for this leaderboard, sorted by metric so that the best is first", direction=API.Direction.OUTPUT)
+  public KeyV3.ModelKeyV3[] models;
+
+  /**
+   * Metric used to sort this leaderboard.
+   */
+  @API(help="Metric used to sort this leaderboard", direction=API.Direction.INOUT)
+  public String metric;
+
+  /**
+   * Metric direction used in the sort.
+   */
+  @API(help="Metric direction used in the sort", direction=API.Direction.INOUT)
+  public boolean sortDecreasing;
+}
+
