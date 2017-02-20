573fb375463e84da71f947def79846c7946fafd2
Vlad Patryshev <vpatryshev@gmail.com>
2017-02-20 15:24:29 -0800

    Improved error message regarding the continuity of time in DeepLearningCheckpointReporting (#812)

--- h2o-algos/src/test/java/hex/deeplearning/DeepLearningCheckpointReporting.java
+++ h2o-algos/src/test/java/hex/deeplearning/DeepLearningCheckpointReporting.java
@@ -99,7 +99,9 @@ public class DeepLearningCheckpointReporting extends TestUtil {
           durationBefore = durationBefore.substring(0, durationBefore.length()-4);
           String durationAfter = (String)table.get((int)(p._epochs+1),1);
           durationAfter = durationAfter.substring(0, durationAfter.length()-4);
-          Assert.assertTrue("Duration must be smooth", Double.parseDouble(durationAfter) - Double.parseDouble(durationBefore) < sleepTime+1);
+          double diff = Double.parseDouble(durationAfter) - Double.parseDouble(durationBefore);
+          
+          Assert.assertTrue("Duration must be smooth; actual " + diff + ", expected at most " + sleepTime + " (before=" + durationBefore + ", after=" + durationAfter + ")", diff < sleepTime+1);
 
           // Check that time stamp does see the sleep
           String timeStampBefore = (String)table.get((int)(p._epochs),0);
