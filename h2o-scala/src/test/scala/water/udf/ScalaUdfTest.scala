ef5092e1fcbdf3d94ad2dbb8c06d89cbca9b86e6
Vlad Patryshev <vpatryshev@gmail.com>
2017-02-20 19:08:56 -0800

    Moved some stuff from TestUtils to FileUtils Gave the methods proper names So a bunch of tests have those minor changes Tests only

--- h2o-scala/src/test/scala/water/udf/ScalaUdfTest.scala
+++ h2o-scala/src/test/scala/water/udf/ScalaUdfTest.scala
@@ -1,8 +1,8 @@
 package water.udf
 
 import java.io.File
-import java.lang
 import java.util.{Date, GregorianCalendar}
+import java.{lang, util}
 
 import org.junit.{Assert, BeforeClass, Test}
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll}
@@ -10,10 +10,7 @@ import water.TestUtil._
 import water.udf.MoreColumns._
 import water.udf.fp.PureFunctions._
 import water.udf.fp.{Function, Functions, PureFunctions}
-<<<<<<< 828476d76243ffb16f0556d83601983f197dfb43
-=======
 import water.util.FileUtils._
->>>>>>> Moved some stuff from TestUtils to FileUtils
 import water.{Test0, TestUtil}
 
 import scala.collection.JavaConverters._
@@ -294,18 +291,18 @@ class ScalaUdfTest extends Test0 with BeforeAndAfter with BeforeAndAfterAll {
 
     assert(ss.size == split.size())
     
-    val frame = new UnfoldingFrame[String, DataColumn[String]](Strings, split.size, split, 11)
+    val frame: UnfoldingFrame[String] = new UnfoldingFrame[String](Strings, split.size, split, 11)
     
-    val matrix: MatrixFrame[DataColumn[String]] = frame.materialize
+    val columns: util.List[DataColumn[lang.String]] = frame.materialize
 
     {
       for {i <- ss.indices} {
-        val fromColumns = (0 until 10) map (matrix.column(_).get(i))
+        val fromColumns = (0 until 10) map (columns.get(_).get(i))
         val actual = fromColumns filter (null !=) mkString " "
         assert(ss(i).replaceAll(",", " ").trim == actual)
       }
     }
-    assert(matrix.column(5).isCompatibleWith(source), "Need until align the result")
+    assert(columns.get(5).isCompatibleWith(source), "Need until align the result")
   }
 
   @Test def testSomethingElse(): Unit = {
