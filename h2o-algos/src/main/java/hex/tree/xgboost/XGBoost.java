8309ca1e753fd8efe516b9a4334c95afeecc6dee
Arno Candel <arno.candel@gmail.com>
2017-02-24 19:13:57 -0800

    Faster CSC sparse DMatrix creation.
    More work towards getting CSC to work - looks like another bug in DMatrix?

--- h2o-algos/src/main/java/hex/tree/xgboost/XGBoost.java
+++ h2o-algos/src/main/java/hex/tree/xgboost/XGBoost.java
@@ -13,6 +13,7 @@ import water.Job;
 import water.Key;
 import water.exceptions.H2OIllegalArgumentException;
 import water.exceptions.H2OModelBuilderIllegalArgumentException;
+import water.fvec.Chunk;
 import water.fvec.Frame;
 import water.fvec.Vec;
 import water.util.ArrayUtils;
@@ -23,8 +24,10 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import static hex.tree.SharedTree.createModelSummaryTable;
 import static hex.tree.SharedTree.createScoringHistoryTable;
@@ -91,66 +94,131 @@ public class XGBoost extends ModelBuilder<XGBoostModel,XGBoostModel.XGBoostParam
       // 1 0 2 0
       // 4 0 0 3
       // 3 1 2 0
+      boolean csc = false; //di._cats == 0;
 
-      // CSC:
+      // truly sparse matrix - no categoricals
+      // collect all nonzeros column by column (in parallel), then stitch together into final data structures
+      if (csc) {
+
+        // CSC:
 //    long[] colHeaders = new long[] {0,        3,  4,     6,    7}; //offsets
 //    float[] data = new float[]     {1f,4f,3f, 1f, 2f,2f, 3f};      //non-zeros down each column
 //    int[] rowIndex = new int[]     {0,1,2,    2,  0, 2,  1};       //row index for each non-zero
 
-      // CSR:
+        class SparseItem {
+          int pos;
+          double val;
+        }
+        int nCols = di._nums;
+
+        List<SparseItem>[] col = new List[nCols]; //TODO: use more efficient storage (no GC)
+        // allocate
+        for (int i=0;i<nCols;++i) {
+          col[i] = new ArrayList<>(Math.min(nRows, 10000));
+        }
+
+        // collect non-zeros
+        int nzCount=0;
+        for (int i=0;i<nCols;++i) { //TODO: parallelize over columns
+          Vec v = f.vec(i);
+          for (int c=0;c<v.nChunks();++c) {
+            Chunk ck = v.chunkForChunkIdx(c);
+            int[] nnz = new int[ck.sparseLenZero()];
+            int nnzCount = ck.nonzeros(nnz);
+            for (int k=0;k<nnzCount;++k) {
+              SparseItem item = new SparseItem();
+              int localIdx = nnz[k];
+              item.pos = (int)ck.start() + localIdx;
+              // both 0 and NA are omitted in the sparse DMatrix
+              if (w != null && w.at(item.pos) == 0) continue;
+              if (ck.isNA(localIdx)) continue;
+              item.val = ck.atd(localIdx);
+              col[i].add(item);
+              nzCount++;
+            }
+          }
+        }
+        long[] colHeaders = new long[nCols + 1];
+        float[] data = new float[nzCount];
+        int[] rowIndex = new int[nzCount];
+        // fill data for DMatrix
+        for (int i=0;i<nCols;++i) { //TODO: parallelize over columns
+          List sparseCol = col[i];
+          colHeaders[i] = nz;
+          for (int j=0;j<sparseCol.size();++j) {
+            SparseItem si = (SparseItem)sparseCol.get(j);
+            rowIndex[nz] = si.pos;
+            data[nz] = (float)si.val;
+            assert(si.val != 0);
+            assert(!Double.isNaN(si.val));
+            assert(w == null || w.at(si.pos) != 0);
+            nz++;
+          }
+        }
+        colHeaders[nCols] = nz;
+        data = Arrays.copyOf(data, nz);
+        rowIndex = Arrays.copyOf(rowIndex, nz);
+        actualRows = countUnique(rowIndex);
+        trainMat = new DMatrix(colHeaders, rowIndex, data, DMatrix.SparseType.CSC, actualRows);
+        assert trainMat.rowNum() == actualRows;
+      } else {
+
+        // CSR:
 //    long[] rowHeaders = new long[] {0,      2,      4,         7}; //offsets
 //    float[] data = new float[]     {1f,2f,  4f,3f,  3f,1f,2f};     //non-zeros across each row
 //    int[] colIndex = new int[]     {0, 2,   0, 3,   0, 1, 2};      //col index for each non-zero
 
-      long[] rowHeaders = new long[nRows + 1];
-      int initial_size = 1 << 20;
-      float[] data = new float[initial_size];
-      int[] colIndex = new int[initial_size];
+        long[] rowHeaders = new long[nRows + 1];
+        int initial_size = 1 << 20;
+        float[] data = new float[initial_size];
+        int[] colIndex = new int[initial_size];
 
-      // extract predictors
-      rowHeaders[0] = 0;
-      for (int i = 0; i < nRows; ++i) {
-        if (w != null && w.at(i) == 0) continue;
-        int nzstart = nz;
-        // enlarge final data arrays by 2x if needed
-        while (data.length < nz + di._cats + di._nums) {
-          Log.info("Enlarging sparse data structure from " + data.length + " bytes to " + (data.length << 1) + " bytes.");
-          data = Arrays.copyOf(data, data.length << 1);
-          colIndex = Arrays.copyOf(colIndex, colIndex.length << 1);
-        }
-        for (int j = 0; j < di._cats; ++j) {
-          if (!vecs[j].isNA(i)) {
-            data[nz] = 1; //one-hot encoding
-            colIndex[nz] = di.getCategoricalId(j, vecs[j].at8(i));
-            nz++;
-          } else {
-            // NA == 0 for sparse -> no need to fill
+        // extract predictors
+        rowHeaders[0] = 0;
+        for (int i = 0; i < nRows; ++i) {
+          if (w != null && w.at(i) == 0) continue;
+          int nzstart = nz;
+          // enlarge final data arrays by 2x if needed
+          while (data.length < nz + di._cats + di._nums) {
+            Log.info("Enlarging sparse data structure from " + data.length + " bytes to " + (data.length << 1) + " bytes.");
+            data = Arrays.copyOf(data, data.length << 1);
+            colIndex = Arrays.copyOf(colIndex, colIndex.length << 1);
+          }
+          for (int j = 0; j < di._cats; ++j) {
+            if (!vecs[j].isNA(i)) {
+              data[nz] = 1; //one-hot encoding
+              colIndex[nz] = di.getCategoricalId(j, vecs[j].at8(i));
+              nz++;
+            } else {
+              // NA == 0 for sparse -> no need to fill
 //            data[nz] = 1; //one-hot encoding
 //            colIndex[nz] = di.getCategoricalId(j, Double.NaN); //Fill NA bucket
 //            nz++;
+            }
           }
-        }
-        for (int j = 0; j < di._nums; ++j) {
-          float val = (float) vecs[di._cats + j].at(i);
-          if (!Float.isNaN(val) && val != 0) {
-            data[nz] = val;
-            colIndex[nz] = di._catOffsets[di._catOffsets.length - 1] + j;
+          for (int j = 0; j < di._nums; ++j) {
+            float val = (float) vecs[di._cats + j].at(i);
+            if (!Float.isNaN(val) && val != 0) {
+              data[nz] = val;
+              colIndex[nz] = di._catOffsets[di._catOffsets.length - 1] + j;
+              nz++;
+            }
+          }
+          if (nz == nzstart) {
+            // for the corner case where there are no categorical values, and all numerical values are 0, we need to
+            // assign a 0 value to any one column to have a consistent number of rows between the predictors and the special vecs (weight/response/etc.)
+            data[nz] = 0;
+            colIndex[nz] = 0;
             nz++;
           }
+          rowHeaders[++actualRows] = nz;
         }
-        if (nz == nzstart) {
-          // for the corner case where there are no categorical values, and all numerical values are 0, we need to
-          // assign a 0 value to any one column to have a consistent number of rows between the predictors and the special vecs (weight/response/etc.)
-          data[nz] = 0;
-          colIndex[nz] = 0;
-          nz++;
-        }
-        rowHeaders[++actualRows] = nz;
+        data = Arrays.copyOf(data, nz);
+        colIndex = Arrays.copyOf(colIndex, nz);
+        rowHeaders = Arrays.copyOf(rowHeaders, actualRows + 1);
+        trainMat = new DMatrix(rowHeaders, colIndex, data, DMatrix.SparseType.CSR, di.fullN());
+        assert trainMat.rowNum() == actualRows;
       }
-      data = Arrays.copyOf(data, nz);
-      colIndex = Arrays.copyOf(colIndex, nz);
-      rowHeaders = Arrays.copyOf(rowHeaders, actualRows + 1);
-      trainMat = new DMatrix(rowHeaders, colIndex, data, DMatrix.SparseType.CSR, 0);
     } else {
       Log.info("Treating matrix as dense.");
 
@@ -184,6 +252,7 @@ public class XGBoost extends ModelBuilder<XGBoostModel,XGBoostModel.XGBoostParam
       }
       data = Arrays.copyOf(data, actualRows*cols);
       trainMat = new DMatrix(data, actualRows, cols, Float.NaN);
+      assert trainMat.rowNum() == actualRows;
     }
 
     // extract weight vector
@@ -213,7 +282,6 @@ public class XGBoost extends ModelBuilder<XGBoostModel,XGBoostModel.XGBoostParam
     if (w!=null)
       trainMat.setWeight(weights);
 //    trainMat.setGroup(null); //fold //FIXME - only needed if CV is internally done in XGBoost
-    assert trainMat.rowNum() == actualRows;
     return trainMat;
   }
 
@@ -548,5 +616,20 @@ public class XGBoost extends ModelBuilder<XGBoostModel,XGBoostModel.XGBoostParam
     } catch (XGBoostError xgBoostError) {
       return false;
     }
+  }
+
+  public static int countUnique(int[] unsortedArray) {
+    if (unsortedArray.length == 0) {
+      return 0;
+    }
+    int[] array = Arrays.copyOf(unsortedArray, unsortedArray.length);
+    Arrays.sort(array);
+    int count = 1;
+    for (int i = 0; i < array.length - 1; i++) {
+      if (array[i] != array[i + 1]) {
+        count++;
+      }
+    }
+    return count;
   }
 }
