a596fa7a40e54c4d492a0899842e7fa18e3e5859
Tomas Nykodym <tomas.nykodym@gmail.com>
2017-03-02 19:36:18 -0800

    Fixed R-tests. p-values are now computed only for the non-standardized coefficients.

--- h2o-r/tests/testdir_algos/glm/runit_GLM_gram.R
+++ h2o-r/tests/testdir_algos/glm/runit_GLM_gram.R
@@ -15,11 +15,12 @@ test.GLM.nonnegative <- function() {
   h2o.rm(h2o.getId(gfr))
   ginvDiag = diag(solve(gm))
   ginvDiag = ginvDiag[c(length(ginvDiag),1:length(ginvDiag)-1)]
-  beta = m@model$coefficients_table[,6]
+  beta = m@model$coefficients_table[,2]
   names(beta) <- m@model$coefficients_table[,1]
   zvalues = beta/sqrt(ginvDiag)
   zvalues_h2o = m@model$coefficients_table[,4]
-  cbind(zvalues,zvalues_h2o)
+  print(as.data.frame(m@model$coefficients_table))
+  print(cbind(zvalues,zvalues_h2o))
   if(max(abs(zvalues-zvalues_h2o)) > 1e-4) fail("z-scores do not match")
   #test 2 compare non-weighted non-standardized matrix to t(M) %*% M
   dfr = as.data.frame(df)
