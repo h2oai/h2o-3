96fd0ed54c3d8c87aceb9e653ae00d20bc014e42
Arno Candel <arno.candel@gmail.com>
2017-03-07 18:50:28 -0800

    Fix a typo that prevented the right math for deciding whether to cache images or not.

--- h2o-algos/src/main/java/hex/deepwater/DeepWater.java
+++ h2o-algos/src/main/java/hex/deepwater/DeepWater.java
@@ -315,14 +315,14 @@ public class DeepWater extends ModelBuilder<DeepWaterModel,DeepWaterParameters,D
 
         // decide whether to cache
         long bytes;
-        if (_parms._problem_type == DeepWaterParameters.ProblemType.image) {
+        if (mp._problem_type == DeepWaterParameters.ProblemType.image) {
           bytes = train.numRows() * model.model_info()._width * model.model_info()._height * model.model_info()._channels * 4;
         } else {
           bytes = train.byteSize();
         }
         cache = mp._cache_data;
         if (cache) {
-          if (bytes < H2O.CLOUD.free_mem() / 4) {
+          if (bytes < H2O.CLOUD.free_mem() / 2) {
             Log.info("Automatically enabling data caching, expecting to require " + PrettyPrint.bytes(bytes) + ".");
           } else {
             Log.info("Automatically disabling data caching, since it would require too much space: " + PrettyPrint.bytes(bytes) + ".");
