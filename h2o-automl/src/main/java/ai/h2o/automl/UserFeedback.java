b4203ee3aad289e2d603ce44e79e3ca5242d6b73
Raymond Peck <rpeck@rpeck.com>
2017-03-07 17:56:32 -0800

    AUTOML-128: initial user feedback code.  Currently logs useful stuff from AutoML.java.

---
+++ h2o-automl/src/main/java/ai/h2o/automl/UserFeedback.java
@@ -0,0 +1,68 @@
+package ai.h2o.automl;
+
+import water.DKV;
+import water.Key;
+import water.Keyed;
+import water.util.TwoDimTable;
+
+import static water.Key.make;
+
+public class UserFeedback extends Keyed<UserFeedback> {
+  private UserFeedbackEvent[] feedbackEvents; // wish we had IcedArrayList(). . .
+
+  public UserFeedback(Key<AutoML> runKey) {
+    this._key = make(idForRun(runKey));
+
+    UserFeedback old = DKV.getGet(this._key);
+
+    if (null == old) {
+      feedbackEvents = new UserFeedbackEvent[0];
+      DKV.put(this);
+    }
+  }
+
+  private UserFeedback() {
+  }
+
+  public static String idForRun(Key<AutoML> runKey) { return "AutoML_Feedback_" + runKey.toString(); }
+
+  public void addEvent(UserFeedbackEvent.Level level, UserFeedbackEvent.Stage stage, String message) {
+    addEvent(new UserFeedbackEvent(level, stage, message));
+  }
+
+  public void addEvent(UserFeedbackEvent event) {
+    UserFeedbackEvent[] oldEvents = feedbackEvents;
+    feedbackEvents = new UserFeedbackEvent[feedbackEvents.length + 1];
+    System.arraycopy(oldEvents, 0, feedbackEvents, 0, oldEvents.length);
+    feedbackEvents[oldEvents.length] = event;
+  }
+
+  /**
+   * Delete everything in the DKV that this points to.  We currently need to be able to call this after deleteWithChildren().
+   */
+  public void delete() {
+    feedbackEvents = new UserFeedbackEvent[0];
+    remove();
+  }
+
+  public TwoDimTable toTwoDimTable() {
+    return toTwoDimTable("User Feedback");
+  }
+
+  public TwoDimTable toTwoDimTable(String tableHeader) {
+    TwoDimTable table = UserFeedbackEvent.makeTwoDimTable(tableHeader, feedbackEvents.length);
+
+    for (int i = 0; i < feedbackEvents.length; i++)
+      feedbackEvents[i].addTwoDimTableRow(table, i);
+    return table;
+  }
+
+  public String toString(String tableHeader) {
+    return this.toTwoDimTable(tableHeader).toString();
+  }
+
+  @Override
+  public String toString() {
+    return this.toTwoDimTable().toString();
+  }
+}
