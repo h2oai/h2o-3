ca170e8531319bbd68712acd362fe61b910712d4
Raymond Peck <rpeck@rpeck.com>
2017-03-10 09:51:21 -0800

    AUTOML-135: Change /3/AutoML* to /99/AutoML

--- h2o-automl/src/main/java/water/automl/Register.java
+++ h2o-automl/src/main/java/water/automl/Register.java
@@ -9,16 +9,12 @@ import water.util.Log;
 
 public class Register extends AbstractRegister{
   @Override public void register(String relativeResourcePath) throws ClassNotFoundException {
-    // H2O.register("POST /3/AutoMLBuilder", AutoMLBuilderHandler.class, "POST", "automl", "automatically build models");
-    // H2O.register("GET /3/AutoML/{automl_id}", AutoMLHandler.class,"refresh", "GET", "refresh the model key");
-    // H2O.register("GET /3/AutoMLJSONSchemaHandler", AutoMLJSONSchemaHandler.class, "GET", "getJSONSchema", "Get the json schema for the AutoML input fields.");
-
     RequestServer.registerEndpoint("automl_build",
-            "POST /3/AutoMLBuilder", AutoMLBuilderHandler.class, "build",
+            "POST /99/AutoMLBuilder", AutoMLBuilderHandler.class, "build",
             "Start an AutoML build process.");
 
     RequestServer.registerEndpoint("fetch",
-            "GET /3/AutoML/{automl_id}", AutoMLHandler.class, "fetch",
+            "GET /99/AutoML/{automl_id}", AutoMLHandler.class, "fetch",
             "Fetch the specified AutoML object.");
 
     RequestServer.registerEndpoint("list",
