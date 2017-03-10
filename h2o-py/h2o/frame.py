a01b32a0c35b2f1f814eb29e61775300a13590a0
Vlad Patryshev <vpatryshev@gmail.com>
2017-03-10 12:22:30 -0800

    Test for factoring operations. (#886)
    * Test for factoring operations.
    Also, frame.asfactor() fixed, so
    a) more than one column is factored and the result type set
    b) column type checked before passing to the server
    
    * frame.asfactor allows 'enum' type (the server side just ignores these columns)
    
    * frame.asfactor allows 'enum' type (the server side just ignores these columns)
    
    * More cultured column comparison

--- h2o-py/h2o/frame.py
+++ h2o-py/h2o/frame.py
@@ -2080,9 +2080,17 @@ class H2OFrame(object):
 
         :returns: new H2OFrame with columns of the "enum" type.
         """
+        
+        for colname in self.names:
+            t = self.types[colname]
+            if t not in {"int", "string", "enum"}: raise H2OValueError("Only 'int' or 'string' are allowed for asfactor(), got %s:%s " % (colname, t))
+
         fr = H2OFrame._expr(expr=ExprNode("as.factor", self), cache=self._ex._cache)
         if fr._ex._cache.types_valid():
-            fr._ex._cache.types = {list(fr._ex._cache.types)[0]: "enum"}
+          fr._ex._cache.types = {name: "enum" for name in self.types}
+        else:
+          raise H2OTypeError("Types are not available in result")
+        
         return fr
 
 
