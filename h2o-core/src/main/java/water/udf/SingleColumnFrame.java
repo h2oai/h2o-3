a308c6b9bd226673906dd96108b22d4bf84c16ea
Vlad Patryshev <vpatryshev@gmail.com>
2017-03-15 21:35:05 -0700

    One more layout trouble fixed

--- h2o-core/src/main/java/water/udf/SingleColumnFrame.java
+++ h2o-core/src/main/java/water/udf/SingleColumnFrame.java
@@ -8,9 +8,9 @@ import water.udf.fp.Function;
 import water.udf.specialized.EnumColumn;
 import water.udf.specialized.Enums;
 
-import static water.udf.DataColumns.*;
-
 import java.io.IOException;
+
+import static water.udf.DataColumns.BaseFactory;
 
 /**
  * Single column frame that knows its data type and can materialize
@@ -38,13 +38,15 @@ public class SingleColumnFrame<DataType, ColumnType extends DataColumn<DataType>
     this.function = function;
   }
 
-  public static
-  <DataType, ColumnType extends DataColumn<DataType>>
-  SingleColumnFrame<DataType, ColumnType> forColumn(
+  public static <DataType, ColumnType extends DataColumn<DataType>>
+    SingleColumnFrame<DataType, ColumnType> forColumn(
       final BaseFactory<DataType, ColumnType> factory,
       final Column<DataType> column) {
     return new SingleColumnFrame<DataType, ColumnType>(factory, column.size(), column) {
-      @Override protected Vec buildZeroVec() { return factory.buildZeroVec(column); }
+      @Override
+      protected Vec buildZeroVec() {
+        return factory.buildZeroVec(column);
+      }
     };
   }
 
@@ -57,12 +59,15 @@ public class SingleColumnFrame<DataType, ColumnType extends DataColumn<DataType>
     }
   }
 
-  protected Vec buildZeroVec() { return factory.buildZeroVec(length); }
+  protected Vec buildZeroVec() {
+    return factory.buildZeroVec(length);
+  }
 
   protected Vec makeVec() throws IOException {
     final Vec vec0 = buildZeroVec();
     MRTask task = new MRTask() {
-      @Override public void map(Chunk[] cs) {
+      @Override
+      public void map(Chunk[] cs) {
         for (Chunk c : cs) {
           DataChunk<DataType> tc = factory.apply(c);
           for (int r = 0; r < c._len; r++) {
