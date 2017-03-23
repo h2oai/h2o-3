b95599af7e8dea31f5f2755859e8f84388809df7
Navdeep Gill <navdeep@h2o.ai>
2017-03-23 16:40:00 -0700

    Change connection -> connection_url as its not a proper field for ImportSQLTableV99

--- h2o-py/h2o/h2o.py
+++ h2o-py/h2o/h2o.py
@@ -442,7 +442,7 @@ def import_sql_table(connection_url, table, username, password, columns=None, op
     assert_is_type(password, str)
     assert_is_type(columns, [str], None)
     assert_is_type(optimize, bool)
-    p = {"connection": connection_url, "table": table, "username": username, "password": password, "optimize": optimize}
+    p = {"connection_url": connection_url, "table": table, "username": username, "password": password, "optimize": optimize}
     if columns:
         p["columns"] = ", ".join(columns)
     j = H2OJob(api("POST /99/ImportSQLTable", data=p), "Import SQL Table").poll()
