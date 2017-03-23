e27f8bb35a750b558e9c2ff359602791fcd37317
wendycwong <wendycwong@yahoo.com>
2017-03-23 15:49:17 -0700

    PUBDEV-4177-java-orc-timezone: add condition to only run test ParseTestORCCSV.java only if timezone is America/Los_Angeles. Since our Jenkins machines are all set to America/Los_Angeles times, the test will be run.  It will be skipped for machines set to strange time zones. (#951)

--- h2o-parsers/h2o-orc-parser/src/test/java/water/parser/ParseTestORCCSV.java
+++ h2o-parsers/h2o-orc-parser/src/test/java/water/parser/ParseTestORCCSV.java
@@ -1,6 +1,7 @@
 package water.parser;
 
 
+import org.joda.time.DateTimeZone;
 import org.junit.Assume;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,50 +12,56 @@ import water.fvec.Frame;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Test suite for orc parser.
- *
- * This test will attempt to parse a bunch of files (orc and csv).  We compare the frames of these files and make
- * sure that they are equivalent.
- *
- * -- Requested by Tomas N.
- *
- */
+	* Test suite for orc parser.
+	* <p>
+	* This test will attempt to parse a bunch of files (orc and csv).  We compare the frames of these files and make
+	* sure that they are equivalent.
+	* <p>
+	* -- Requested by Tomas N.
+	*/
 public class ParseTestORCCSV extends TestUtil {
 
-    private String[] csvFiles = {"smalldata/parser/orc/orc2csv/testTimeStamp.csv",
-            "smalldata/parser/orc/orc2csv/testDate1900.csv",
-            "smalldata/parser/orc/orc2csv/testDate2038.csv"};
+		private String[] csvFiles = {"smalldata/parser/orc/orc2csv/testTimeStamp.csv",
+										"smalldata/parser/orc/orc2csv/testDate1900.csv",
+										"smalldata/parser/orc/orc2csv/testDate2038.csv"};
 
-    private String[] orcFiles = {"smalldata/parser/orc/testTimeStamp.orc",
-            "smalldata/parser/orc/TestOrcFile.testDate1900.orc",
-            "smalldata/parser/orc/TestOrcFile.testDate2038.orc"};
+		private String[] orcFiles = {"smalldata/parser/orc/testTimeStamp.orc",
+										"smalldata/parser/orc/TestOrcFile.testDate1900.orc",
+										"smalldata/parser/orc/TestOrcFile.testDate2038.orc"};
 
-    @BeforeClass
-    static public void _preconditionJavaVersion() { // NOTE: the `_` force execution of this check after setup
-        // Does not run test on Java6 since we are running on Hadoop lib
-        Assume.assumeTrue("Java6 is not supported", !System.getProperty("java.version",
-                "NA").startsWith("1.6"));
-    }
+		@BeforeClass
+		static public void _preconditionJavaVersion() { // NOTE: the `_` force execution of this check after setup
+				// Does not run test on Java6 since we are running on Hadoop lib
+				Assume.assumeTrue("Java6 is not supported", !System.getProperty("java.version",
+												"NA").startsWith("1.6"));
+		}
 
-    @BeforeClass
-    static public void setup() { TestUtil.stall_till_cloudsize(5); }
+		@BeforeClass
+		static public void setup() {
+				TestUtil.stall_till_cloudsize(5);
+		}
 
-    @Test
-    public void testParseOrcCsvFiles() {
-        Scope.enter();
-        try {
-            for (int f_index = 0; f_index < csvFiles.length; f_index++) {
+		@Test
+		public void testParseOrcCsvFiles() {
+				Scope.enter();
+				DateTimeZone currTimeZn = ParseTime.getTimezone();
+				if (currTimeZn.getID().matches("America/Los_Angeles")) {
 
-                Frame csv_frame = parse_test_file(csvFiles[f_index], "\\N", 0, null);
-                Frame orc_frame = parse_test_file(orcFiles[f_index], null, 0, null);
+						try {
+								for (int f_index = 0; f_index < csvFiles.length; f_index++) {
 
-                Scope.track(csv_frame);
-                Scope.track(orc_frame);
+										Frame csv_frame = parse_test_file(csvFiles[f_index], "\\N", 0, null);
+										Frame orc_frame = parse_test_file(orcFiles[f_index], null, 0, null);
 
-                assertTrue(TestUtil.isBitIdentical(orc_frame, csv_frame));  // both frames should equal
-            }
-        } finally {
-            Scope.exit();
-        }
-    }
+										Scope.track(csv_frame);
+										Scope.track(orc_frame);
+
+										assertTrue("Fails: " + csvFiles[f_index] + " != " + orcFiles[f_index],
+																		TestUtil.isBitIdentical(orc_frame, csv_frame));  // both frames should equal
+								}
+						} finally {
+								Scope.exit();
+						}
+				}
+		}
 }
