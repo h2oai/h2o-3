df57bbb0f927dc425dff45775fa96d3537612a88
wendycwong <wendycwong@yahoo.com>
2017-04-28 10:36:34 -0700

    PUBDEV-4283: performance.  Remove parsing 1000C dataset.  Run out of memory.

--- h2o-py/tests/testdir_hdfs/pyunit_INTERNAL_HDFS_import_bigCat_hdp2p2.py
+++ h2o-py/tests/testdir_hdfs/pyunit_INTERNAL_HDFS_import_bigCat_hdp2p2.py
@@ -22,10 +22,8 @@ def hdfs_import_bigCat():
         hdfs_name_node = pyunit_utils.hadoop_namenode()
         allFiles = ["/datasets/bigCatFiles/tenThousandCat10C.csv", "/datasets/bigCatFiles/hundredThousandCat10C.csv",
                     "/datasets/bigCatFiles/oneMillionCat10C.csv", "/datasets/bigCatFiles/tenThousandCat100C.csv",
-                    "/datasets/bigCatFiles/hundredThousandCat100C.csv", "/datasets/bigCatFiles/oneMillionCat100C.csv",
-                    "/datasets/bigCatFiles/tenThousandCat1000C.csv",
-                    "/datasets/bigCatFiles/hundredThousandCat1000C.csv", "/datasets/bigCatFiles/oneMillionCat1000C.csv"]
-        reps = [10, 10, 10, 100, 100, 100, 1000, 1000, 1000]
+                    "/datasets/bigCatFiles/hundredThousandCat100C.csv", "/datasets/bigCatFiles/oneMillionCat100C.csv"]
+        reps = [10, 10, 10, 100, 100, 100]
 
         for ind in range(0,len(allFiles)):  # run tests for 3 different sizes per Tomas request
             print("*******  Parsing file {0} ********".format(allFiles[ind]))
