e7502030e4215bca67920c3e195ce8e816886836
Navdeep Gill <navdeep@h2o.ai>
2017-05-01 14:51:20 -0700

    Update description based on feedback

--- h2o-py/h2o/estimators/gbm.py
+++ h2o-py/h2o/estimators/gbm.py
@@ -664,7 +664,7 @@ class H2OGradientBoostingEstimator(H2OEstimator):
     @property
     def sample_rate_per_class(self):
         """
-        A list/vector of the fraction of rows to sample for each tree, per class (from 0.0 to 1.0)
+        A list of row sample rates per class (relative fraction for each class, from 0.0 to 1.0), for each tree
 
         Type: ``List[float]``.
         """
