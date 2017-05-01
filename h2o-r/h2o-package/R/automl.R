e8152c241be917cd65a153d1347b5bca5a25ddc7
Navdeep Gill <navdeep@h2o.ai>
2017-05-01 10:30:14 -0700

    Update API's to match automl branch

--- h2o-r/h2o-package/R/automl.R
+++ h2o-r/h2o-package/R/automl.R
@@ -26,6 +26,7 @@
 #' votes_path <- system.file("extdata", "housevotes.csv", package="h2o")
 #' votes_hf <- h2o.uploadFile(path = votes_path, header = TRUE)
 #' aml <- h2o.automl(y = "Class", training_frame = votes_hf, max_runtime_secs = 30)
+#' aml@leaderboard
 #' }
 #' @export
 h2o.automl <- function(x, y, training_frame,
