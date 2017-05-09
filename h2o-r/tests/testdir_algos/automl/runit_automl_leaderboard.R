9cea9f33b31bc76534e1470a845e51ffa3bef3fb
Navdeep Gill <navdeep@h2o.ai>
2017-05-08 19:10:53 -0700

    Minor update to data paths

--- h2o-r/tests/testdir_algos/automl/runit_automl_leaderboard.R
+++ h2o-r/tests/testdir_algos/automl/runit_automl_leaderboard.R
@@ -3,14 +3,14 @@ source("../../../scripts/h2o-r-test-setup.R")
 
 automl.leaderboard.test <- function() {
     #binomial
-    fr = h2o.importFile(locate("smalldata/logreg/prostate.csv"))
+    fr = h2o.uploadFile(locate("smalldata/logreg/prostate.csv"))
     fr["CAPSULE"] = as.factor(fr["CAPSULE"])
     f = h2o.automl(y=2,training_frame = fr, max_runtime_secs = 5)
     f@leaderboard
     expect_equal(names(f@leaderboard),c( "model_id","auc","logloss"))
 
     #regression
-    fr2 = h2o.importFile(locate("smalldata/covtype/covtype.20k.data"))
+    fr2 = h2o.uploadFile(locate("smalldata/covtype/covtype.20k.data"))
     h = h2o.automl(y=55,training_frame = fr2, max_runtime_secs = 5)
     h@leaderboard
     expect_equal(names(h@leaderboard),c("model_id", "mean_residual_deviance","rmse", "mae", "rmsle"))
