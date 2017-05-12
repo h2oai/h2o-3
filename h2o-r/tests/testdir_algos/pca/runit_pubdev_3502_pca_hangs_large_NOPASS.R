ee8cdc01287dd8dc2de28eecda802d4da7fd4bdc
wendycwong <wendycwong@yahoo.com>
2017-05-12 09:19:41 -0700

    PUBDEV-3947: pca slow.  Added runit tests to measure pca run time for all models and R.  Minor change to enable widedataset only when it is necessary. PUBDEV-3947: PCA slow.  Fixed bug when rank deficient dataset is encountered.

--- h2o-r/tests/testdir_algos/pca/runit_pubdev_3502_pca_hangs_large_NOPASS.R
+++ h2o-r/tests/testdir_algos/pca/runit_pubdev_3502_pca_hangs_large_NOPASS.R
@@ -7,26 +7,31 @@ source("../../../scripts/h2o-r-test-setup.R")
 # the purpose of this test, we are only going to use Power.  You can try GramSVD as well.
 test.pca.la1s <- function() {
   run_time_c = c()
-  num_run = 2
+  num_run = 1
 
   browser()
-  dataR = h2o.importFile("bigdata/laptop/jira/la1s.wc.arff.txt.zip",sep=',',destination_frame = "data",header = T, parse=FALSE)
+  dataR = h2o.importFile(locate("bigdata/laptop/jira/la1s.wc.arff.txt.zip"),sep=',',destination_frame = "data",header = T, parse=FALSE)
   data <- h2o.parseRaw(dataR, destination_frame = "bigParse",
                               parse_type = "CSV", header = T) # chunk_size = 124022500 size will make one chunk.
   data$CLASS_LABEL = NULL
 
   for (runIndex in 1:num_run) {
     # k=1939 is very large and this is going to take a long time.
-    mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1938,max_iterations = 300,pca_method = "Randomized")
-    print("PCA run time with car.arff.txt data in ms is ")
+    mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1938,max_iterations = 10,pca_method = "GramSVD")
+    print("PCA (GramSVD) run time with car.arff.txt data in ms is ")
     print(mm@model$run_time)
-    run_time_c = c(run_time_c,mm@model$run_time)
-    print("average run time in ms for data.arff.txt is: ")
-    print(mean(run_time_c))
-    print("maximum run time in ms for data.arff.txt is: ")
-    print(max(run_time_c))
-    print("minimum run time in ms for data.arff.txt is: ")
-    print(min(run_time_c))
+
+    h2o.rm(mm)
+
+    mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1938,max_iterations = 10,pca_method = "Power")
+    print("PCA (Power) run time with car.arff.txt data in ms is ")
+    print(mm@model$run_time)
+
+    h2o.rm(mm)
+
+    mm = h2o.prcomp(data,transform = "STANDARDIZE",k =1938,max_iterations = 10,pca_method = "Randomized")
+    print("PCA (Randomized) run time with car.arff.txt data in ms is ")
+    print(mm@model$run_time)
 
     h2o.rm(mm)
   }
