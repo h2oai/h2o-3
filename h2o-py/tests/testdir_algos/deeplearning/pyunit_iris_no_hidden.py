0c25a3755c77758eb67ae033307f1fd724b0b3f4
Arno Candel <arno.candel@gmail.com>
2017-05-24 09:51:21 -0700

    Use 4 features.

--- h2o-py/tests/testdir_algos/deeplearning/pyunit_iris_no_hidden.py
+++ h2o-py/tests/testdir_algos/deeplearning/pyunit_iris_no_hidden.py
@@ -9,11 +9,11 @@ def deeplearning_no_hidden():
   iris_hex = h2o.import_file(path=pyunit_utils.locate("smalldata/iris/iris.csv"))
 
   hh = H2ODeepLearningEstimator(hidden=[], loss="CrossEntropy", export_weights_and_biases=True)
-  hh.train(x=list(range(3)), y=4, training_frame=iris_hex)
+  hh.train(x=list(range(4)), y=4, training_frame=iris_hex)
   hh.show()
   weights1 = hh.weights(0)
   assert weights1.shape[0] == 3
-  assert weights1.shape[1] == 3
+  assert weights1.shape[1] == 4
 
 if __name__ == "__main__":
   pyunit_utils.standalone_test(deeplearning_no_hidden)
