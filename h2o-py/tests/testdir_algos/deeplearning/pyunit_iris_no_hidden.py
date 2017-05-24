5fb50c7eb7430084baaacdb5929eab06a8bfeaed
Arno Candel <arno.candel@gmail.com>
2017-05-24 09:46:51 -0700

    Improve python unit test.

--- h2o-py/tests/testdir_algos/deeplearning/pyunit_iris_no_hidden.py
+++ h2o-py/tests/testdir_algos/deeplearning/pyunit_iris_no_hidden.py
@@ -6,17 +6,14 @@ from tests import pyunit_utils
 from h2o.estimators.deeplearning import H2ODeepLearningEstimator
 
 def deeplearning_no_hidden():
-
-
-
   iris_hex = h2o.import_file(path=pyunit_utils.locate("smalldata/iris/iris.csv"))
-  hh = H2ODeepLearningEstimator(hidden=None, loss="CrossEntropy")
-  hh.train(x=list(range(3)), y=4, training_frame=iris_hex)
-  hh.show()
 
-  hh = H2ODeepLearningEstimator(hidden=[], loss="CrossEntropy")
+  hh = H2ODeepLearningEstimator(hidden=[], loss="CrossEntropy", export_weights_and_biases=True)
   hh.train(x=list(range(3)), y=4, training_frame=iris_hex)
   hh.show()
+  weights1 = hh.weights(0)
+  assert weights1.shape[0] == 3
+  assert weights1.shape[1] == 3
 
 if __name__ == "__main__":
   pyunit_utils.standalone_test(deeplearning_no_hidden)
