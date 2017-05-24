312dbcd2f45da46534376e34e38226ced4d3b13d
Arno Candel <arno.candel@gmail.com>
2017-05-24 11:11:24 -0700

    Update RUnit.

--- h2o-r/tests/testdir_algos/deeplearning/runit_deeplearning_no_hidden.R
+++ h2o-r/tests/testdir_algos/deeplearning/runit_deeplearning_no_hidden.R
@@ -1,14 +1,12 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../../scripts/h2o-r-test-setup.R")
 
-
-
 check.deeplearning_no_hidden <- function() {
 	iris.hex <- h2o.uploadFile(locate("smalldata/iris/iris.csv"), "iris.hex")
-	hh <- h2o.deeplearning(x=c(1,2,3,4),y=5,hidden=NULL,training_frame=iris.hex)
-	hh <- h2o.deeplearning(x=c(1,2,3,4),y=5,hidden=c(),training_frame=iris.hex)
+	hh <- h2o.deeplearning(x=c(1,2,3,4),y=5,hidden=numeric(),training_frame=iris.hex,export_weights_and_biases=TRUE)
 	print(hh)
-  
+  w3 = h2o.weights(object = hh,1)
+  print(dim(w3))
 }
 
 doTest("Deep Learning Test: Iris", check.deeplearning_no_hidden)
