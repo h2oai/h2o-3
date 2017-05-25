bbe68f190abb094f6cfbe5a240c6d65190c932b2
ledell <erin@h2o.ai>
2017-05-24 23:53:16 -0700

    PUBDEV-4458: Changing h2o.automl R doc example

--- h2o-r/h2o-package/R/automl.R
+++ h2o-r/h2o-package/R/automl.R
@@ -25,10 +25,8 @@
 #' \donttest{
 #' library(h2o)
 #' h2o.init()
-#' 
 #' votes_path <- system.file("extdata", "housevotes.csv", package="h2o")
 #' votes_hf <- h2o.uploadFile(path = votes_path, header = TRUE)
-#' 
 #' aml <- h2o.automl(y = "Class", training_frame = votes_hf, max_runtime_secs = 30)
 #' lb <- aml@leaderboard
 #' lb
