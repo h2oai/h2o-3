3878315dd8525feb0e60fb72f6b51c7f61e735a4
Jakub Hava <jakub.hava@gmail.com>
2017-06-02 22:54:31 +0200

    Add deprecation warning

--- h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/hadoop/h2odriver.java
+++ h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/hadoop/h2odriver.java
@@ -796,6 +796,10 @@ public class h2odriver extends Configured implements Tool {
         internal_secure_connections = true;
       }
       else if (s.equals("-internal_security_conf") || s.equals("-internal_security")) {
+        if(s.equals("-internal_security")){
+          System.out.println("The '-internal_security' configuration is deprecated. " +
+                  "Please use '-internal_security_conf' instead.");
+        }
         i++; if (i >= args.length) { usage(); }
         securityConf = args[i];
       }
