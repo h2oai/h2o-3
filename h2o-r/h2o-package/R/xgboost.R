07c9ef465e5c4eeb8274dc978202dc6d0b3338cb
Jakub Hava <jakub.hava@gmail.com>
2017-06-02 17:03:42 +0200

    [PUBDEV-4538] Make h2o.xgboost.available available in R client

--- h2o-r/h2o-package/R/xgboost.R
+++ h2o-r/h2o-package/R/xgboost.R
@@ -259,7 +259,7 @@ h2o.xgboost <- function(x, y, training_frame,
 #' Ask the H2O server whether a XGBoost model can be built (depends on availability of native backend)
 #' Returns True if a XGBoost model can be built, or False otherwise.
 #' @param h2oRestApiVersion (Optional) Specific version of the REST API to use
-#'
+#' @export
 h2o.xgboost.available <- function(h2oRestApiVersion = .h2o.__REST_API_VERSION) {
 visibility = .h2o.__remoteSend(method = "GET", h2oRestApiVersion = h2oRestApiVersion, .h2o.__MODEL_BUILDERS("xgboost"))$model_builders[["xgboost"]][["visibility"]]
 if (visibility == "Experimental") {
