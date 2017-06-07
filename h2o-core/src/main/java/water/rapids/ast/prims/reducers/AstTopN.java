07bcbc36b02606eec6d84c7901f446781d37ed94
wendycwong <wendycwong@yahoo.com>
2017-06-07 10:11:33 -0700

    PUBDEV-4567: speedup topbottomN.  Added dataframe rebalance, use arrays which are iced per Michal K suggestion.

--- h2o-core/src/main/java/water/rapids/ast/prims/reducers/AstTopN.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/reducers/AstTopN.java
@@ -1,16 +1,15 @@
 package water.rapids.ast.prims.reducers;
 
+import water.DKV;
+import water.H2O;
+import water.Key;
 import water.MRTask;
-import water.fvec.C8Chunk;
-import water.fvec.Chunk;
-import water.fvec.Frame;
-import water.fvec.Vec;
+import water.fvec.*;
 import water.rapids.Env;
 import water.rapids.ast.AstPrimitive;
 import water.rapids.ast.AstRoot;
 import water.rapids.vals.ValFrame;
 
-import java.io.Serializable;
 import java.util.PriorityQueue;
 
 import static java.lang.StrictMath.min;
@@ -47,37 +46,48 @@ public class AstTopN extends AstPrimitive {
 		@Override
 		public ValFrame apply(Env env, Env.StackHelp stk, AstRoot[] asts) { // implementation with PriorityQueue
 				Frame frOriginal = stk.track(asts[1].exec(env)).getFrame(); // get the 2nd argument and convert it to a Frame
+				// break into multiple chunks if needed
+				if ((frOriginal.anyVec().nChunks() == 1) && (frOriginal.numRows() > 1000)) {
+						Key rebalancedKey = Key.make();
+						RebalanceDataSet rb = new RebalanceDataSet(frOriginal, rebalancedKey, 4);
+						H2O.submitTask(rb);
+						rb.join();
+						frOriginal = DKV.get(rebalancedKey).get();
+				}
 				int colIndex = (int) stk.track(asts[2].exec(env)).getNum();     // column index of interest
 				double nPercent = stk.track(asts[3].exec(env)).getNum();        //  top or bottom percentage of row to return
 				int getBottomN = (int) stk.track(asts[4].exec(env)).getNum();   // 0, return top, 1 return bottom percentage
-				int totColumns = frOriginal.numCols();
 				long numRows = Math.round(nPercent * 0.01 * frOriginal.numRows()); // number of rows to return
 
 				String[] finalColumnNames = {"Original_Row_Indices", frOriginal.name(colIndex)}; // set output frame names
-				GrabTopNPQ grabTask = new GrabTopNPQ(finalColumnNames, numRows, (getBottomN == 0));
+				GrabTopNPQ grabTask = new GrabTopNPQ(finalColumnNames, numRows, (getBottomN == 0 ? 1 : -1));
 				grabTask.doAll(frOriginal.vec(colIndex));
+				frOriginal.remove();
 				return new ValFrame(grabTask._sortedOut);
 		}
 
 		public class GrabTopNPQ<E extends Comparable<E>> extends MRTask<GrabTopNPQ<E>> {
 				final String[] _columnName;   // name of column that we are grabbing top N for
 				PriorityQueue _sortQueue;
+				long[] _rowIndices;  // store original row indices of values that are grabbed
+				long[] _lValues;         // store the grabbed values
+				double[] _dValues;  // store grabbed longs
 				Frame _sortedOut;   // store the final result of sorting
 				final int _rowSize;   // number of top or bottom rows to keep
-				final boolean _increasing;  // sort with Top values first if true.
+				final int _flipSign;   // 1 for top values, -1 for bottom values
 				boolean _csLong = false;      // chunk of interest is long
 
-				private GrabTopNPQ(String[] columnName, long rowSize, boolean increasing) {
+				private GrabTopNPQ(String[] columnName, long rowSize, int flipSign) {
 						_columnName = columnName;
 						_rowSize = (int) rowSize;
-						_increasing = increasing;
+						_flipSign = flipSign;
 				}
 
 				@Override
 				public void map(Chunk cs) {
 						_sortQueue = new PriorityQueue<RowValue<E>>(); // instantiate a priority queue
 						_csLong = cs instanceof C8Chunk;
-						Long startRow = cs.start();           // absolute row offset
+						long startRow = cs.start();           // absolute row offset
 
 						for (int rowIndex = 0; rowIndex < cs._len; rowIndex++) {  // stuff our chunks into priorityQueue
 								long absRowIndex = rowIndex + startRow;
@@ -85,30 +95,123 @@ public class AstTopN extends AstPrimitive {
 										addOneValue(cs, rowIndex, absRowIndex, _sortQueue);
 								}
 						}
+
+						// copy the PQ into the corresponding arrays
+						if (_csLong) {
+								_lValues = new long[_sortQueue.size()];
+								copyPQ2ArryL(_sortQueue, _lValues);
+						} else {
+								_dValues = new double[_sortQueue.size()];
+								copyPQ2ArryD(_sortQueue, _dValues);
+						}
+				}
+
+				public void copyPQ2ArryL(PriorityQueue sortQueue, long[] values) {
+						//copy values on PQ into arrays in sorted order
+						int qSize = sortQueue.size();
+						_rowIndices = new long[qSize];
+
+						for (int index = qSize - 1; index >= 0; index--) {
+								RowValue tempPairs = (RowValue) sortQueue.poll();
+								_rowIndices[index] = tempPairs.getRow();
+								values[index] = (long) tempPairs.getValue();
+						}
+				}
+
+				public <T> void copyPQ2ArryD(PriorityQueue sortQueue, double[] values) {
+						//copy values on PQ into arrays in sorted order
+						int qSize = sortQueue.size();
+						_rowIndices = new long[qSize];
+
+						for (int index = qSize - 1; index >= 0; index--) {
+								RowValue tempPairs = (RowValue) sortQueue.poll();
+								_rowIndices[index] = tempPairs.getRow();
+								values[index] = (double) tempPairs.getValue();
+						}
 				}
 
 				@Override
 				public void reduce(GrabTopNPQ<E> other) {
-						this._sortQueue.addAll(other._sortQueue);
+						// do a combine here of two arrays.  Note the value always store values that are increasing
+						if (_csLong)
+								mergeArraysL(other._rowIndices, other._lValues);
+						else
+								mergeArraysD(other._rowIndices, other._dValues);
+				}
 
-						int sizesToReduce = this._sortQueue.size() - _rowSize;
-						if (sizesToReduce > 0) {
-								for (int index = 0; index < sizesToReduce; index++)
-										this._sortQueue.poll();
+				public void mergeArraysL(long[] otherRow, long[] otherValue) {
+						// grab bottom and grab top are slightly different
+						int finalArraySize = min(this._rowSize, this._lValues.length+otherValue.length);
+
+						long[] newRow = new long[finalArraySize];
+						long[] newValues = new long[finalArraySize]; // desired values are at start of array
+						int thisRowIndex = 0;
+						int otherRowIndex = 0;
+						for (int index = 0; index < finalArraySize; index++) {
+								if ((thisRowIndex < this._lValues.length) && (otherRowIndex < otherValue.length)) {
+										if ((this._lValues[thisRowIndex] - otherValue[otherRowIndex]) * this._flipSign >= 0) {
+												newRow[index] = this._rowIndices[thisRowIndex];
+												newValues[index] = this._lValues[thisRowIndex++];
+										} else {
+												newRow[index] = otherRow[otherRowIndex];
+												newValues[index] = otherValue[otherRowIndex++];
+										}
+								} else { // one of the array is done!
+										if (thisRowIndex < this._lValues.length) { // otherArray is done
+												newRow[index] = this._rowIndices[thisRowIndex];
+												newValues[index] = this._lValues[thisRowIndex++];
+										} else { // thisArray is done.  Use the other one
+												newRow[index] = otherRow[otherRowIndex];
+												newValues[index] = otherValue[otherRowIndex++];
+										}
+								}
 						}
+						this._rowIndices = newRow;
+						this._lValues = newValues;
+				}
+
+
+				public void mergeArraysD(long[] otherRow, double[] otherValue) {
+						// grab bottom and grab top are slightly different
+						int finalArraySize = min(this._rowSize, this._rowIndices.length+otherRow.length);
+
+						long[] newRow = new long[finalArraySize];
+						double[] newValues = new double[finalArraySize]; // desired values are at start of array
+						int thisRowIndex = 0;
+						int otherRowIndex = 0;
+						for (int index = 0; index < finalArraySize; index++) {
+								if ((thisRowIndex < this._dValues.length) && (otherRowIndex < otherValue.length)) {
+										if ((this._dValues[thisRowIndex] - otherValue[otherRowIndex]) * this._flipSign >= 0) {
+												newRow[index] = this._rowIndices[thisRowIndex];
+												newValues[index] = this._dValues[thisRowIndex++];
+										} else {
+												newRow[index] = otherRow[otherRowIndex];
+												newValues[index] = otherValue[otherRowIndex++];
+										}
+								} else { // one of the array is done!
+										if (thisRowIndex < this._dValues.length) { // otherArray is done
+												newRow[index] = this._rowIndices[thisRowIndex];
+												newValues[index] = this._dValues[thisRowIndex++];
+										} else { // thisArray is done.  Use the other one
+												newRow[index] = otherRow[otherRowIndex];
+												newValues[index] = otherValue[otherRowIndex++];
+										}
+								}
+						}
+						this._rowIndices = newRow;
+						this._dValues = newValues;
 				}
 
 				@Override
 				public void postGlobal() {  // copy the sorted heap into a vector and make a frame out of it.
 						Vec[] xvecs = new Vec[2];   // final output frame will have two chunks, original row index, top/bottom values
-						long actualRowOutput = min(_rowSize, _sortQueue.size()); // due to NAs, may not have enough rows to return
+						long actualRowOutput = this._rowIndices.length; // due to NAs, may not have enough rows to return
 						for (int index = 0; index < xvecs.length; index++)
 								xvecs[index] = Vec.makeZero(actualRowOutput);
 
 						for (int index = 0; index < actualRowOutput; index++) {
-								RowValue transport = (RowValue) this._sortQueue.poll();
-								xvecs[0].set(index, transport.getRow());
-								xvecs[1].set(index, _csLong ? (Long) transport.getValue() : (Double) transport.getValue());
+								xvecs[0].set(index, this._rowIndices[index]);
+								xvecs[1].set(index, _csLong ? this._lValues[index] : this._dValues[index]);
 						}
 						_sortedOut = new Frame(_columnName, xvecs);
 				}
@@ -120,11 +223,11 @@ public class AstTopN extends AstPrimitive {
 						RowValue currPair = null;
 						if (_csLong) {  // long chunk
 								long a = cs.at8(rowIndex);
-								currPair = new RowValue(absRowIndex, a, _increasing);
+								currPair = new RowValue(absRowIndex, a, _flipSign);
 
 						} else {                      // other numeric chunk
 								double a = cs.atd(rowIndex);
-								currPair = new RowValue(absRowIndex, a, _increasing);
+								currPair = new RowValue(absRowIndex, a, _flipSign);
 						}
 						sortHeap.offer(currPair);   // add pair to PriorityQueue
 						if (sortHeap.size() > _rowSize) {
@@ -138,28 +241,29 @@ public class AstTopN extends AstPrimitive {
 		corresponding value.  Implemented the compareTo function and comparison is performed on
 		the value.
 			*/
-		public class RowValue<E extends Comparable<E>> implements Comparable<RowValue<E>>, Serializable {
-				private Long _rowIndex;
+		public class RowValue<E extends Comparable<E>> implements Comparable<RowValue<E>> {
+				private long _rowIndex;
 				private E _value;
 				boolean _increasing;  // true if grabbing for top N, false for bottom N
+				int _flipSign;        // 1 to grab top and -1 to grab bottom
 
-				public RowValue(Long rowIndex, E value, boolean increasing) {
+				public RowValue(long rowIndex, E value, int flipSign) {
 						this._rowIndex = rowIndex;
 						this._value = value;
-						this._increasing = increasing;
+						this._flipSign = flipSign;
 				}
 
 				public E getValue() {
 						return this._value;
 				}
 
-				public Long getRow() {
+				public long getRow() {
 						return this._rowIndex;
 				}
 
 				@Override
 				public int compareTo(RowValue<E> other) {
-						return (this.getValue().compareTo(other.getValue()) * (this._increasing ? 1 : -1));
+						return (this.getValue().compareTo(other.getValue()) * this._flipSign);
 				}
 		}
 }
