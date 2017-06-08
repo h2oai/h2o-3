9dc8598ef4042b4fcce7ecaba165d38dccb996a2
tomasnykodym <tomas.nykodym@gmail.com>
2017-06-08 15:34:17 -0700

    Added incremental gram computation to GLM. (#1245)
    * PUBDEV-4564 ADDED INCREMENTAL GRAM COMPUTATION TO GLM.
    
    Huge performance impact on gaussian lambda search, noticeable improvement for other families (can save 0 ~ 30% of matrix computation).
    Only active for solver==COD.
    
    Fixed glm test, comparison should be <= instead of < when checking that active coefficients did not exceed user set limit.
    
    * Fixes to Michal's review comments, removed unused method, renamed variable for consistency.
    
    * Fixed glm gram caching - added missing update of cached value, fixed beta comparison.

--- h2o-algos/src/test/java/hex/glm/GLMTest.java
+++ h2o-algos/src/test/java/hex/glm/GLMTest.java
@@ -1732,7 +1732,7 @@ public class GLMTest  extends TestUtil {
       params._max_iterations = 100000;
       params._max_active_predictors = 10000;
       params._alpha = new double[]{1};
-      for(Solver s: new Solver[]{ Solver.IRLSM, Solver.COORDINATE_DESCENT}){//Solver.COORDINATE_DESCENT,}) { // LBFGS lambda-search is too slow now
+      for(Solver s: new Solver[]{Solver.IRLSM, Solver.COORDINATE_DESCENT}){//Solver.COORDINATE_DESCENT,}) { // LBFGS lambda-search is too slow now
         params._solver = s;
         GLM glm = new GLM( params, modelKey);
         glm.trainModel().get();
