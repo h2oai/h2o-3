PUBDEV-4567: top/bottomN speedup. Fixed overflow bug and check column type once.
PUBDEV-4567: top/bottom speedup.  Incorporate Tomas/Michal K codereview.

h2o-py/tests/testdir_munging/pyunit_top_bottomN_large.py
@@ -34,7 +34,7 @@ def h2o_H2OFrame_top_bottomN():
         pyunit_utils.compare_frames(newTopFrame, newTopFrameC, nsample, tol_numeric=tolerance)
 
         # compare one of the return frames with known answer
-        compare_rep_frames(topAnswer, newTopFrame, tolerance, colIndex, -1)
+        compare_rep_frames(topAnswer, newTopFrame, tolerance, colIndex, 1)
     else:
         # test bottomN here
         print("For bottomN: Percentage chosen is {0}.  Column index chosen is {1}".format(nP, colIndex))
@@ -44,14 +44,14 @@ def h2o_H2OFrame_top_bottomN():
         # the two return frames should be the same for this case
         pyunit_utils.compare_frames(newBottomFrame, newBottomFrameC, nsample, tol_numeric=tolerance)
         # compare one of the return frames with known answer
-        compare_rep_frames(bottomAnswer, newBottomFrame, tolerance, colIndex, 1)
+        compare_rep_frames(bottomAnswer, newBottomFrame, tolerance, colIndex, -1)
 
 
-def compare_rep_frames(answerF, repFrame, tolerance,  colIndex, getBottom=-1):
+def compare_rep_frames(answerF, repFrame, tolerance, colIndex, grabTopN=-1):
     # actual answer is in second column of repFrame
     highIndex = int(round(repFrame.nrow/4))
     allIndex = range(answerF.nrow-highIndex, answerF.nrow)
-    if getBottom>0:     # get bottom N percent
+    if grabTopN<0:     # get bottom N percent
         allIndex = range(0, highIndex)
     repIndex = 0
     answerArray = np.transpose(answerF[colIndex].as_data_frame(header=False).values)[0]