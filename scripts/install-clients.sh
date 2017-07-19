1e7c7401b258aaf3d6be09d9840da7ea5b7397d0
ledell <erin@h2o.ai>
2017-07-18 18:32:34 -0700

    PUBDEV-4604: moved install-clients.sh to ./scripts

--- scripts/install-clients.sh
+++ scripts/install-clients.sh
@@ -4,7 +4,6 @@
 # run this script to install the new R and Python clients.
 
 pip uninstall -y h2o
-pip install h2o-py/dist/h2o-*-py2.py3-none-any.whl
+pip install ../h2o-py/dist/h2o-*-py2.py3-none-any.whl
 R CMD REMOVE h2o
-R CMD INSTALL h2o-r/R/src/contrib/h2o*.tar.gz
-
+R CMD INSTALL ../h2o-r/R/src/contrib/h2o*.tar.gz
