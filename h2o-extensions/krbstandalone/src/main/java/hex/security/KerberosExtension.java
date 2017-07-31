8cce1bc7e0635832d5fe2a502e070ecbf9da9b47
michalkurka <michalk@h2o.ai>
2017-07-31 11:36:15 -0700

    WIP

--- h2o-extensions/krbstandalone/src/main/java/hex/security/KerberosExtension.java
+++ h2o-extensions/krbstandalone/src/main/java/hex/security/KerberosExtension.java
@@ -20,16 +20,23 @@ public class KerberosExtension extends AbstractH2OExtension {
   @Override
   public void onLocalNodeStarted() {
     Configuration conf = PersistHdfs.CONF;
-    if ("kerberos".equals(conf.get("hadoop.security.authentication"))) {
-      Log.info("Kerberos enabled in configuration - trying to login using existing keytab");
+    if (conf == null)
+      return; // this is theoretically possible although unlikely
+
+    if (isKerberosEnabled(conf)) {
+      Log.info("Kerberos enabled in Hadoop configuration. Trying to login the (default) user.");
       UserGroupInformation.setConfiguration(conf);
       try {
         UserGroupInformation.loginUserFromSubject(null);
       } catch (IOException e) {
-        throw new RuntimeException("Kerberos initialization FAILED.", e);
+        Log.err("Kerberos initialization FAILED. Kerberos ticket needs to be acquired before starting H2O (run kinit).", e);
       }
     } else
-      Log.info("Kerberos not configured");
+      Log.debug("Kerberos not configured");
+  }
+
+  private static boolean isKerberosEnabled(Configuration conf) {
+    return "kerberos".equals(conf.get("hadoop.security.authentication"));
   }
 
 }
