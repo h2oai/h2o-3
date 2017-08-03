6244ed3c474246a31d239685143c6e8380cec108
michalkurka <michalk@h2o.ai>
2017-08-03 16:47:25 -0700

    wip

--- h2o-extensions/krbstandalone/src/main/java/hex/security/KerberosExtension.java
+++ h2o-extensions/krbstandalone/src/main/java/hex/security/KerberosExtension.java
@@ -8,6 +8,17 @@ import water.util.Log;
 
 import java.io.IOException;
 
+/**
+ * Authenticates the H2O Node to access secured Hadoop cluster in a standalone mode.
+ *
+ * This extension assumes that if Hadoop configuration is present and it has Kerberos enabled
+ * the user will likely want to read data from HDFS even though H2O is running in a standalone mode (not on Hadoop).
+ * The extension attempts to authenticate the user using an existing Kerberos ticket. This means the Kerberos ticket
+ * needs to be manually acquired by the user on each node before the H2O instance is started.
+ *
+ * The extension fails gracefully if the user cannot be authenticated and doesn't stop H2O start-up. The failure
+ * will be logged as an error.
+ */
 public class KerberosExtension extends AbstractH2OExtension {
 
   public static String NAME = "KrbStandalone";
