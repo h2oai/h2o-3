c4a5dbe0ca641ca8622b170ce73538c9637fce57
wendycwong <wendycwong@yahoo.com>
2017-08-10 12:16:53 -0700

    PUBDEV-4702: max runtime.  Pass max_runtime_secs from PCA to SVD. PUBDEV-4702: max runtime.  Added correct method to check max_runtime_secs to kmeans.java

--- h2o-py/tests/testdir_jira/pyunit_pubdev_4702_algo_max_runtime_secs_large.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_4702_algo_max_runtime_secs_large.py
@@ -88,7 +88,7 @@ def algo_max_runtime_secs():
     training1_data = h2o.import_file(path=pyunit_utils.locate("smalldata/gridsearch/pca1000by25.csv"))
     x_indices = list(range(training1_data.ncol))
     model = H2OPCA(k=10, transform="STANDARDIZE", pca_method="Power", compute_metrics=True)
-    grabRuntimeInfo(err_bound*2, 2.0, model, training1_data, x_indices)
+    grabRuntimeInfo(err_bound, 2.0, model, training1_data, x_indices)
     cleanUp([training1_data, model])
 
     # kmeans
