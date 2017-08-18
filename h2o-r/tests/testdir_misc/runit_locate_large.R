3a55139f50b9796b8a51e6c4e6e118258a0e037a
wendycwong <wendycwong@yahoo.com>
2017-08-18 12:59:15 -0700

    PUBDEV-4823: fix runit locate golden: Added more error message to check in runit_locate_large.R PUBDEV-4823: fix runit locate golden.  Fixed runit_km2_4_golden_large.R to allow for message when memory limit is reached.

--- h2o-r/tests/testdir_misc/runit_locate_large.R
+++ h2o-r/tests/testdir_misc/runit_locate_large.R
@@ -10,7 +10,8 @@ test <- function() {
     print(e[[1]])
     geterr1 <- "node stack overflow" %in% e[[1]] # new error message depending on R version
     geterr2 <- "evaluation nested too deeply: infinite recursion / options(expressions=)?" %in% e[[1]]
-    expect_true(geterr1 || geterr2)
+    geterr3 <- all(sapply("too close to the limit", grepl, e[[1]]))
+    expect_true(geterr1 || geterr2 || geterr3)
 }
 
 doTest("test", test)
