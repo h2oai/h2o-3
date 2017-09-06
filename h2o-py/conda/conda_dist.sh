21c111c94e002abd87f9f540189a5bf484445409
abal5 <anmol@h2o.ai>
2017-09-06 13:19:54 -0700

    conda builds

--- h2o-py/conda/conda_dist.sh
+++ h2o-py/conda/conda_dist.sh
@@ -7,10 +7,10 @@
 PROJECT_VERSION=`cat ../../h2o-docs/src/product/project_version`
 
 # Subsitute __version__ to build version
-sed -i '' "s/SUBST_PROJECT_VERSION/${PROJECT_VERSION}/g" ../h2o/__init__.py
+#sed -i '' "s/SUBST_PROJECT_VERSION/${PROJECT_VERSION}/g" ../h2o/__init__.py
 
 # Create conda package
-conda build h2o
+conda build --py 2.7 --py 3.6 h2o
 
 # Convert conda package for other platforms
 conda convert ~/anaconda3/conda-bld/osx-64/h2o-${PROJECT_VERSION}-0.tar.bz2 -p all
@@ -19,9 +19,9 @@ conda convert ~/anaconda3/conda-bld/osx-64/h2o-${PROJECT_VERSION}-0.tar.bz2 -p a
 BUILD_NUMBER=h2o-${PROJECT_VERSION}-0.tar.bz2
 echo $BUILD_NUMBER
 
-anaconda upload linux-32/${BUILD_NUMBER}
-anaconda upload linux-64/${BUILD_NUMBER}
-anaconda upload win-32/${BUILD_NUMBER}
-anaconda upload win-64/${BUILD_NUMBER}
+#anaconda upload linux-32/${BUILD_NUMBER}
+#anaconda upload linux-64/${BUILD_NUMBER}
+#anaconda upload win-32/${BUILD_NUMBER}
+#anaconda upload win-64/${BUILD_NUMBER}
 
 sed -i '' "s/${PROJECT_VERSION}/SUBST_PROJECT_VERSION/g" ../h2o/__init__.py
