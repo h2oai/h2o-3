6523ae1c15fa8b063c3417a2a3a2c253093d538b
angela0xdata <angela@h2o.ai>
2017-09-25 14:39:01 -0700

    additional updates
    Built H2O to generate updated files. This should prevent errors in the
    startup_checks test.

--- h2o-r/h2o-package/R/glm.R
+++ h2o-r/h2o-package/R/glm.R
@@ -28,7 +28,9 @@
 #' @param offset_column Offset column. This will be added to the combination of columns before applying the link function.
 #' @param weights_column Column with observation weights. Giving some observation a weight of zero is equivalent to excluding it from
 #'        the dataset; giving an observation a relative weight of 2 is equivalent to repeating that row twice. Negative
-#'        weights are not allowed.
+#'        weights are not allowed. Note: Weights are per-row observation weights and do not increase the size of the
+#'        data frame. This is typically the number of times a row is repeated, but non-integer values are supported as
+#'        well. During training, rows with higher weights matter more, due to the larger loss function pre-factor.
 #' @param family Family. Use binomial for classification with logistic regression, others are for regression problems. Must be
 #'        one of: "gaussian", "binomial", "quasibinomial", "multinomial", "poisson", "gamma", "tweedie". Defaults to
 #'        gaussian.
