964e8294867280e59e91a7928ebdb4482c97351f
wendycwong <wendycwong@yahoo.com>
2017-09-25 17:01:04 -0700

    PUBDEV-4870: Fixed Java backend bug to properly sort multiple columns; (#1515)
    PUBDEV-4870: Added Runit/Pyunit tests to sort multiple columns;
    PUBDEV-4870: Fixed python and R clients to allow sorting on real columns.
    PUBDEV-4870: Added small shortcut to avoid looping through all 256 bins.
    PUBDEV-4870: Fixed wrong copying in SingleThreadRadixOrder.java
    PUBDEV-4628: sort float bug: moved methods to testUtits and added a junit test for them per Michal K code review suggestions.

--- h2o-py/tests/pyunit_utils/utilsPY.py
+++ h2o-py/tests/pyunit_utils/utilsPY.py
@@ -3000,3 +3000,21 @@ def compare_numeric_frames(f1, f2, prob=0.5, tol=1e-6):
                                                                             abs(temp2[rowInd, colInd]))
                 assert diff<=tol, "Failed frame values check! frame1 value: {0}, frame2 value: " \
                                   "{1}".format(temp1[rowInd, colInd], temp2[rowInd, colInd])
+
+def check_sorted_2_columns(frame1, sorted_column_indices, prob=0.5):
+    for colInd in sorted_column_indices:
+        for rowInd in range(0, frame1.nrow-1):
+            if (random.uniform(0.0,1.0) < prob):
+                if colInd == sorted_column_indices[0]:
+                    if not(math.isnan(frame1[rowInd, colInd])) and not(math.isnan(frame1[rowInd+1,colInd])):
+                        assert frame1[rowInd,colInd] <= frame1[rowInd+1,colInd], "Wrong sort order: value at row {0}: {1}, value at " \
+                                                               "row {2}: {3}".format(rowInd, frame1[rowInd,colInd],
+                                                                                     rowInd+1, frame1[rowInd+1,colInd])
+                else: # for second column
+                    if not(math.isnan(frame1[rowInd, sorted_column_indices[0]])) and not(math.isnan(frame1[rowInd+1,sorted_column_indices[0]])):
+                        if (frame1[rowInd,sorted_column_indices[0]]==frame1[rowInd+1, sorted_column_indices[0]]):  # meaningful to compare row entries then
+                            if not(math.isnan(frame1[rowInd, colInd])) and not(math.isnan(frame1[rowInd+1,colInd])):
+                                assert frame1[rowInd,colInd] <= frame1[rowInd+1,colInd], "Wrong sort order: value at row {0}: {1}, value at " \
+                                                                           "row {2}: {3}".format(rowInd, frame1[rowInd,colInd],
+                                                                                                 rowInd+1, frame1[rowInd+1,colInd])
+
