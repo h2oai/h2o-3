161f52f49113e05b76d0ba7cd8ef18802a7c6e96
wendycwong <wendycwong@yahoo.com>
2017-09-26 17:31:59 -0700

    Wendy sort weierstrass (#1546)
    * Add seed to deeplearning example so that R_cmd_check will not fail due to bad parameter initialization leading to unstable model being returned. (#1535)
    
    * Check to make sure number of models created to be <= rather than == in runit_GBMRandomGrid_airlines_large.R (#1538)
    
    * Fixed pyunit_hexdev_497_import_gzip_airlines_first_header.py file to just compare frame summaries. (#1542)
    
    * PUBDEV-4870: Fixed Java backend bug to properly sort multiple columns; (#1515)
    
    PUBDEV-4870: Added Runit/Pyunit tests to sort multiple columns;
    PUBDEV-4870: Fixed python and R clients to allow sorting on real columns.
    PUBDEV-4870: Added small shortcut to avoid looping through all 256 bins.
    PUBDEV-4870: Fixed wrong copying in SingleThreadRadixOrder.java
    PUBDEV-4628: sort float bug: moved methods to testUtits and added a junit test for them per Michal K code review suggestions.
    
    * Reduce pyunit_pubdev_4870_sort_bug.py run time. (#1544)
    
    * Shorten the tests for runit_pubdev_4870_sort_bug.R. (#1545)

---
+++ h2o-py/tests/testdir_munging/pyunit_pubdev_4870_sort_bug.py
@@ -0,0 +1,17 @@
+from __future__ import print_function
+import sys
+sys.path.insert(1,"../../")
+import h2o
+from tests import pyunit_utils
+
+def sort():
+    df = h2o.import_file(pyunit_utils.locate("smalldata/synthetic/smallIntFloats.csv.zip"))
+    sorted_column_indices = [0,1]
+    df1 = df.sort(sorted_column_indices).asnumeric()
+    pyunit_utils.check_sorted_2_columns(df1, sorted_column_indices, prob=0.001) # check some rows
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(sort)
+else:
+    sort()
+
