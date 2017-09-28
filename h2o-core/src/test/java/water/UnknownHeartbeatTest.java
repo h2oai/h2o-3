ab40a07990ee2996c2e61831ad657dcd6ceded43
Jakub Háva <jakub.hava@gmail.com>
2017-09-28 11:06:47 -0700

    [PUBDEV-4917] Report only relevant clients ( the ones with the same c… (#1529)
    * [PUBDEV-4917] Report only relevant clients ( the ones with the same cloud name )
    (cherry picked from commit 0d441a98f08756193a0aaa36cffd08629cd5410d)

---
+++ h2o-core/src/test/java/water/UnknownHeartbeatTest.java
@@ -0,0 +1,28 @@
+package water;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnknownHeartbeatTest extends TestUtil{
+  @BeforeClass() public static void setup() {
+    stall_till_cloudsize(1);
+  }
+
+  @Test
+  public void testIgnoreUnknownHeartBeat() {
+    HeartBeat hb = new HeartBeat();
+    hb._cloud_name_hash = 777;
+    hb._client = true;
+    hb._jar_md5 = H2O.SELF._heartbeat._jar_md5;
+
+    AutoBuffer ab = new AutoBuffer(H2O.SELF, H2O.MAX_PRIORITY);
+    ab.putUdp(UDP.udp.heartbeat, 65400); // put different port number to simulate heartbeat from fake node
+    hb.write(ab);
+    ab.close();
+
+    // Verify that we don't have a new client
+    assertEquals(0, H2O.getClients().size());
+  }
+}
