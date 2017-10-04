d17eb32cfeb762a2eec02b91c8ecac59e894d0ed
Raymond Peck <rpeck@rpeck.com>
2017-10-04 15:33:33 -0700

    PUBDEV-4978: AutoML: remove GLM s missing_values_handling=Skip temporarily

--- h2o-automl/src/main/java/ai/h2o/automl/AutoML.java
+++ h2o-automl/src/main/java/ai/h2o/automl/AutoML.java
@@ -774,7 +774,8 @@ public final class AutoML extends Lockable<AutoML> implements TimedH2ORunnable {
 
     Map<String, Object[]> searchParams = new HashMap<>();
     glmParameters._alpha = new double[] {0.0, 0.2, 0.4, 0.6, 0.8, 1.0};  // Note: standard GLM parameter is an array; don't use searchParams!
-    searchParams.put("_missing_values_handling", new DeepLearningModel.DeepLearningParameters.MissingValuesHandling[] {DeepLearningModel.DeepLearningParameters.MissingValuesHandling.MeanImputation, DeepLearningModel.DeepLearningParameters.MissingValuesHandling.Skip});
+    // NOTE: removed MissingValuesHandling.Skip for now because it's crashing.  See https://0xdata.atlassian.net/browse/PUBDEV-4974
+    searchParams.put("_missing_values_handling", new DeepLearningModel.DeepLearningParameters.MissingValuesHandling[] {DeepLearningModel.DeepLearningParameters.MissingValuesHandling.MeanImputation /* , DeepLearningModel.DeepLearningParameters.MissingValuesHandling.Skip */});
 
     Job<Grid>glmJob = hyperparameterSearch("GLM", glmParameters, searchParams);
     return glmJob;
