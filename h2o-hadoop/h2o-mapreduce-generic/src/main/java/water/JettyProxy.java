5f6b776c4bc60bd1bcf06a5b287d8d66018069f0
michalkurka <michalk@h2o.ai>
2017-10-06 09:29:49 -0700

    Revert "Fix proxy authorization (we need to pass the actual authenticated username) (#1598)"
    This reverts commit 33abeb4c27112a491889511d7a00bfe642839037.

--- h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/JettyProxy.java
+++ h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/JettyProxy.java
@@ -29,7 +29,7 @@ public class JettyProxy extends AbstractHTTPD {
     ServletHolder proxyServlet = new ServletHolder(Transparent.class);
     proxyServlet.setInitParameter("ProxyTo", _proxyTo);
     proxyServlet.setInitParameter("Prefix", "/");
-    proxyServlet.setInitParameter("Password", _credentials._password);
+    proxyServlet.setInitParameter("BasicAuth", _credentials.toBasicAuth());
     context.addServlet(proxyServlet, "/*");
     handlerWrapper.setHandler(context);
   }
@@ -43,23 +43,17 @@ public class JettyProxy extends AbstractHTTPD {
    * Transparent proxy that automatically adds authentication to each request
    */
   public static class Transparent extends ProxyServlet.Transparent {
-    private String _password;
+    private String _basicAuth;
 
     @Override
     public void init(ServletConfig config) throws ServletException {
       super.init(config);
-      _password = config.getInitParameter("Password");
+      _basicAuth = config.getInitParameter("BasicAuth");
     }
 
     @Override
     protected void customizeExchange(HttpExchange exchange, HttpServletRequest request) {
-      exchange.setRequestHeader("Authorization", getBasicAuth(request));
-    }
-
-    private String getBasicAuth(HttpServletRequest request) {
-      // Note: we need to use the authenticated user and pass it to the H2O node
-      // H2O will be responsible of authorizing the user (it will reject it if the cluster was started for another user)
-      return "Basic " + B64Code.encode(request.getUserPrincipal().getName() + ":" + _password);
+      exchange.setRequestHeader("Authorization", _basicAuth);
     }
   }
 
@@ -75,6 +69,10 @@ public class JettyProxy extends AbstractHTTPD {
     private Credentials(String _user, String _password) {
       this._user = _user;
       this._password = _password;
+    }
+
+    public String toBasicAuth() {
+      return "Basic " + B64Code.encode(_user + ":" + _password);
     }
 
     public String toHashFileEntry() {
