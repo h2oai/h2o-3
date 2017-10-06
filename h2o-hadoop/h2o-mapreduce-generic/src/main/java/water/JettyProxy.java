6c0dccc53800a8e7ba6dd50a6f72fd3d916241cb
michalkurka <michalk@h2o.ai>
2017-10-06 11:14:51 -0700

    HEXDEV-698: Authentication & authorization is fully handled by JettyProxy

--- h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/JettyProxy.java
+++ h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/JettyProxy.java
@@ -1,17 +1,27 @@
 package water;
 
 import org.eclipse.jetty.client.HttpExchange;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.handler.HandlerWrapper;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.servlets.ProxyServlet;
 import org.eclipse.jetty.util.B64Code;
 import org.eclipse.jetty.util.security.Credential;
-import water.network.SecurityUtils;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
+import water.network.SecurityUtils;
 
 public class JettyProxy extends AbstractHTTPD {
 
@@ -26,12 +36,23 @@ public class JettyProxy extends AbstractHTTPD {
 
   @Override
   protected void registerHandlers(HandlerWrapper handlerWrapper, ServletContextHandler context) {
+    // setup authenticating proxy servlet (each request is forwarded with BASIC AUTH)
     ServletHolder proxyServlet = new ServletHolder(Transparent.class);
     proxyServlet.setInitParameter("ProxyTo", _proxyTo);
     proxyServlet.setInitParameter("Prefix", "/");
     proxyServlet.setInitParameter("BasicAuth", _credentials.toBasicAuth());
     context.addServlet(proxyServlet, "/*");
-    handlerWrapper.setHandler(context);
+    // authHandlers assume the user is already authenticated
+    HandlerCollection authHandlers = new HandlerCollection();
+    authHandlers.setHandlers(new Handler[]{
+            new AuthenticationHandler(),
+            context,
+    });
+    // handles requests of login form and delegates the rest to the authHandlers
+    LoginHandler loginHandler = new LoginHandler("/login", "/loginError");
+    loginHandler.setHandler(authHandlers);
+    // login handler is the root handler
+    handlerWrapper.setHandler(loginHandler);
   }
 
   @Override
@@ -91,5 +112,63 @@ public class JettyProxy extends AbstractHTTPD {
       return make(user, SecurityUtils.passwordGenerator(GEN_PASSWORD_LENGTH));
     }
   }
+
+  public class LoginHandler extends HandlerWrapper {
+
+    private final String _loginTarget;
+    private final String _errorTarget;
+    private final byte[] _loginFormData;
+
+    public LoginHandler(String loginTarget, String errorTarget) {
+      _loginTarget = loginTarget;
+      _errorTarget = errorTarget;
+      try {
+        _loginFormData = loadLoginFormResource();
+      } catch (IOException e) {
+        throw new IllegalStateException("Failed to load the login form!", e);
+      }
+    }
+
+    private byte[] loadLoginFormResource() throws IOException {
+      InputStream loginFormStream = LoginHandler.class.getResourceAsStream("/www/login.html");
+      if (loginFormStream == null)
+        throw new IllegalStateException("Login form resource is missing.");
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      IOUtils.copy(loginFormStream, baos);
+      return baos.toByteArray();
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+      if (isLoginTarget(target)) {
+        if (isPageRequest(request))
+          sendLoginForm(response);
+        else
+          sendUnauthorizedResponse(response, "Access denied. Please login.");
+        baseRequest.setHandled(true);
+      } else {
+        // not for us, invoke wrapped handler
+        super.handle(target, baseRequest, request, response);
+      }
+    }
+
+    private void sendLoginForm(HttpServletResponse response) throws IOException {
+      response.setContentType("text/html");
+      response.setContentLength(_loginFormData.length);
+      response.setStatus(HttpServletResponse.SC_OK);
+      IOUtils.write(_loginFormData, response.getOutputStream());
+    }
+
+    private boolean isPageRequest(HttpServletRequest request) {
+      String accept = request.getHeader("Accept");
+      return (accept != null) && accept.contains("text/html");
+    }
+
+    private boolean isLoginTarget(String target) {
+      return target.equals(_loginTarget) || target.equals(_errorTarget);
+    }
+  }
+
 
 }
