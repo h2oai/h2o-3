6db223de85ebeb6c2c65b38ff4843a39f154c6ff
wendycwong <wendycwong@yahoo.com>
2017-10-09 15:14:14 -0700

    PUBDEV-4987: fix any(), all().  (#1616)
    * PUBDEV-4987: fix any(), all().  Added check to make sure we are dealing with boolean columns.  Next added code to count the number of true elements instead of just counting number of non-zeros.
    PUBDEV-4987: fix all, any.  Added pyunit test to verify the fix.
    
    * PUBDEV-4987 fix all, any.  Numerical columns are generated if clients check the condition of a column.

--- h2o-core/src/main/java/water/rapids/ast/prims/reducers/AstAny.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/reducers/AstAny.java
@@ -7,6 +7,8 @@ import water.rapids.vals.ValNum;
 import water.rapids.ast.AstPrimitive;
 import water.rapids.ast.AstRoot;
 
+import static hex.genmodel.utils.ArrayUtils.isBoolColumn;
+
 /**
  * Bulk OR operation on boolean column; NAs count as true.  Returns 0 or 1.
  */
@@ -30,9 +32,15 @@ public class AstAny extends AstPrimitive {
   public ValNum apply(Env env, Env.StackHelp stk, AstRoot asts[]) {
     Val val = stk.track(asts[1].exec(env));
     if (val.isNum()) return new ValNum(val.getNum() == 0 ? 0 : 1);
-    for (Vec vec : val.getFrame().vecs())
-      if (vec.nzCnt() + vec.naCnt() > 0)
+    for (Vec vec : val.getFrame().vecs()) {
+      String[] domainV = vec.domain();
+      if (domainV != null && !isBoolColumn(domainV))  // contain domain that are not true/fale levels
+        return new ValNum(0);         // not a boolean column
+      long trueCount = ((domainV != null) && domainV[0].equalsIgnoreCase("true"))
+              ?(vec.length()-vec.nzCnt()):vec.nzCnt()+vec.naCnt();
+      if (trueCount > 0)
         return new ValNum(1);   // Some nonzeros in there somewhere
+    }
     return new ValNum(0);
   }
 }
