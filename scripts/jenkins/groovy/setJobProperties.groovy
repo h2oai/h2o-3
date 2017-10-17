f13e42784223568b58124c9ddda37cb06e3ae6ee
Michal Raška <michalr@h2o.ai>
2017-10-17 08:23:04 +0200

    [ITA-54] Removed the pythonVersion and rVersion params. (#1638)

--- scripts/jenkins/groovy/setJobProperties.groovy
+++ scripts/jenkins/groovy/setJobProperties.groovy
@@ -1,12 +1,6 @@
 def call() {
   properties(
     [
-        parameters(
-            [
-              choice(name: 'rVersion', description: 'R version used to compile H2O-3', choices: '3.4.1\n3.3.3'),
-              choice(name: 'pythonVersion', description: 'Python version used to compile H2O-3', choices: "3.5\n3.6\n2.7"),
-            ]
-        ),
         buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '25'))
     ]
   )
