d043827338996a54db6d4c4e94ae0f0f4d250443
Michal Raška <michalr@h2o.ai>
2017-10-18 12:04:44 +0200

    [ITA-58] Use commit message as build desc. (#1654)

--- scripts/jenkins/groovy/setJobProperties.groovy
+++ scripts/jenkins/groovy/setJobProperties.groovy
@@ -4,6 +4,7 @@ def call() {
         buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '25'))
     ]
   )
+  currentBuild.description = sh(script: 'cd h2o-3 && git log -1 --pretty=%B', returnStdout: true).trim()
 }
 
 return this
