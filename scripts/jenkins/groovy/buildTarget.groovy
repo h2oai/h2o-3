f9e52189118333c9434920e1beb98d983ff267e5
Michal Raška <michalr@h2o.ai>
2017-10-23 16:24:06 +0200

    [ITA-71] Set correct JAVA_HOME (#1667)
    [ITA-70] Added the noCache param

--- scripts/jenkins/groovy/buildTarget.groovy
+++ scripts/jenkins/groovy/buildTarget.groovy
@@ -38,6 +38,7 @@ def call(body) {
 
 def execMake(target, String h2o3dir) {
   sh """
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
     export USER=jenkins
 
     echo "Activating Python ${env.PYTHON_VERSION}"
