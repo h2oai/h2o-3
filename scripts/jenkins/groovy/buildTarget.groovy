86168379c24c7df12e844c6b000c0bf677408136
Michal Raška <michalr@h2o.ai>
2017-10-23 14:35:11 +0200

    [ITA-69] Removed Webupd8 dependency. (#1666)
    * [ITA-69] Removed Webupd8 dependency.
    
    * [ITA-69] Added sync

--- scripts/jenkins/groovy/buildTarget.groovy
+++ scripts/jenkins/groovy/buildTarget.groovy
@@ -38,7 +38,6 @@ def call(body) {
 
 def execMake(target, String h2o3dir) {
   sh """
-    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
     export USER=jenkins
 
     echo "Activating Python ${env.PYTHON_VERSION}"
