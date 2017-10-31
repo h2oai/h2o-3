49699ab8fa3ef73ea173d66c69a88171d956bdbb
Michal Raška <michalr@h2o.ai>
2017-10-31 12:21:14 +0100

    [ITA-86] Remove all dirs except h2o-3 before starting build. (#1694)

--- scripts/jenkins/groovy/prepareH2O3.groovy
+++ scripts/jenkins/groovy/prepareH2O3.groovy
@@ -1,5 +1,13 @@
 def call(final String mode, final String nodeLabel, final boolean overrideDetectionChange) {
 
+  def findCmd = "find . -maxdepth 1 -not -name 'h2o-3' -not -name h2o-3@tmp -not -name '.'"
+  def deleteCmd = " -exec rm -rf '{}' ';'"
+  def findDeleteCmd = findCmd + deleteCmd
+
+  echo "About to delete these files/folders:"
+  sh findCmd
+  sh findDeleteCmd
+
   // Archive scripts so we don't have to do additional checkouts when changing node
   archiveArtifacts artifacts: "h2o-3/scripts/jenkins/groovy/*", allowEmptyArchive: false
 
