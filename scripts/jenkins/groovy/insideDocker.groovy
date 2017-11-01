250ef0ceef885bd1560f8553a571efcf6b2ff1b0
Michal Raška <michalr@h2o.ai>
2017-11-01 13:37:18 +0100

    [ITA-82] Specify the AWS Credentials explicitly when starting container (#1696)

--- scripts/jenkins/groovy/insideDocker.groovy
+++ scripts/jenkins/groovy/insideDocker.groovy
@@ -13,7 +13,7 @@ def call(customEnv, buildConfig, timeoutValue, timeoutUnit, block) {
       docker.withRegistry("https://${registry}") {
         sh "mkdir -p gradle-user-home"
         withCredentials([file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'), [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: AWS_CREDENTIALS_ID, secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-          docker.image(image).inside('-v /home/0xdiag/smalldata:/home/0xdiag/smalldata -v /home/0xdiag/bigdata:/home/0xdiag/bigdata -v \${WORKSPACE}/gradle-user-home/:\${WORKSPACE}/gradle-user-home') {
+          docker.image(image).inside("-e AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY} -v /home/0xdiag/smalldata:/home/0xdiag/smalldata -v /home/0xdiag/bigdata:/home/0xdiag/bigdata -v \${WORKSPACE}/gradle-user-home/:\${WORKSPACE}/gradle-user-home") {
             sh 'id'
             sh 'printenv'
             block()
