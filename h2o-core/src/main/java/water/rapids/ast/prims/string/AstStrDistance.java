960fa2deba441043588bfced3988095d148afd0f
Jakub Háva <jakub.hava@gmail.com>
2017-11-13 19:27:08 +0000

    [PUBDEV-5038] Add option which enables handling empty strings as NAs … (#1729)

--- h2o-core/src/main/java/water/rapids/ast/prims/string/AstStrDistance.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/string/AstStrDistance.java
@@ -26,8 +26,8 @@ public class AstStrDistance extends AstPrimitive {
 
   @Override
   public int nargs() {
-    return 1 + 3;
-  } // (strDistance x y measure)
+    return 1 + 4;
+  } // (strDistance x y measure compare_empty)
 
   @Override
   public String str() {
@@ -39,7 +39,7 @@ public class AstStrDistance extends AstPrimitive {
     Frame frX = stk.track(asts[1].exec(env)).getFrame();
     Frame frY = stk.track(asts[2].exec(env)).getFrame();
     String measure = asts[3].exec(env).getStr();
-
+    boolean compareEmpty = asts[4].exec(env).getNum() == 1;
     if ((frX.numCols() != frY.numCols()) || (frX.numRows() != frY.numRows()))
       throw new IllegalArgumentException("strDistance() requires the frames to have the same number of columns and rows.");
     for (int i = 0; i < frX.numCols(); i++)
@@ -56,7 +56,7 @@ public class AstStrDistance extends AstPrimitive {
       vecs[i + outputTypes.length] = frY.vec(i);
     }
 
-    Frame distFr = new StringDistanceComparator(measure).doAll(outputTypes, vecs).outputFrame();
+    Frame distFr = new StringDistanceComparator(measure, compareEmpty).doAll(outputTypes, vecs).outputFrame();
     return new ValFrame(distFr);
   }
 
@@ -66,7 +66,11 @@ public class AstStrDistance extends AstPrimitive {
 
   private static class StringDistanceComparator extends MRTask<StringDistanceComparator> {
     private final String _measure;
-    private StringDistanceComparator(String measure) { _measure = measure; }
+    private final boolean _compareEmpty;
+    private StringDistanceComparator(String measure, boolean compareEmpty) {
+      _measure = measure;
+      _compareEmpty = compareEmpty;
+    }
     @Override
     public void map(Chunk[] cs, NewChunk[] nc) {
       BufferedString tmpStr = new BufferedString();
@@ -84,8 +88,12 @@ public class AstStrDistance extends AstPrimitive {
           else {
             String strX = getString(tmpStr, cX, row, domainX);
             String strY = getString(tmpStr, cY, row, domainY);
-            double dist = cmp.compare(strX, strY);
-            nc[i].addNum(dist);
+            if (!_compareEmpty && (strX.isEmpty() || strY.isEmpty())) {
+              nc[i].addNA();
+            } else {
+              double dist = cmp.compare(strX, strY);
+              nc[i].addNum(dist);
+            }
           }
         }
       }
