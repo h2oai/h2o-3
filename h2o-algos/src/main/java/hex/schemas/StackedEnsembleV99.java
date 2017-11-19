2fc47b45a0ac6409440a65471ad820815dd2792d
Erin LeDell <erin@h2o.ai>
2017-11-19 09:55:55 -0800

    PUBDEV-3971: Add cross-validation to Stacked Ensemble (#1745)
    * First attempt at nfolds in Stacked Ensemble
    
    * Add metalearner params to SE parameters class and check if fold_assignment is null
    
    * PUBDEV-3971: Code cleanup
    
    * PUBDEV-3971: Copy xval metrics to ensemble object
    
    * Pass new arguments down to parameters so they are available in APIs
    
    * PUBDEV-3971: Removing metalearner_fold_column from Stacked Ensemble for now
    
    * PUBDEV-3971: Adding runit for nfolds in Stacked Ensemble
    
    * PUBDEV-3971: typo
    
    * PUBDEV-3971: Adding partial pyunit for Stacked Ensemble nfolds
    
    * PUBDEV-3971: Updated estimator_base.py and finished SE nfolds pyunit
    
    * PUBDEV-3971: Updates to code comments and documentation

--- h2o-algos/src/main/java/hex/schemas/StackedEnsembleV99.java
+++ h2o-algos/src/main/java/hex/schemas/StackedEnsembleV99.java
@@ -5,32 +5,50 @@ import hex.StackedEnsembleModel;
 import water.api.API;
 import water.api.schemas3.KeyV3;
 import water.api.schemas3.ModelParametersSchemaV3;
+import hex.Model;
+
 
 
 public class StackedEnsembleV99 extends ModelBuilderSchema<StackedEnsemble,StackedEnsembleV99,StackedEnsembleV99.StackedEnsembleParametersV99> {
   public static final class StackedEnsembleParametersV99 extends ModelParametersSchemaV3<StackedEnsembleModel.StackedEnsembleParameters, StackedEnsembleParametersV99> {
-    static public String[] fields = new String[]{
+    static public String[] fields = new String[] {
       "model_id",
       "training_frame",
       "response_column",
       "validation_frame",
       "base_models",
+      "metalearner_nfolds",
+      "metalearner_fold_assignment",
+      //"metalearner_fold_column",
       "keep_levelone_frame",
-      //"selection_strategy",
     };
 
-    /*
-    @API(help = "Strategy for choosing which models to stack.", values = { "choose_all" }, gridable = false)
-    public StackedEnsembleModel.StackedEnsembleParameters.SelectionStrategy selection_strategy;
-
-    @API(help = "List of model ids which we can stack together.  Which ones are chosen depends on the selection_strategy (currently, all models will be used since selection_strategy can only be set to choose_all).  Models must have been cross-validated using nfolds > 1, fold_assignment equal to Modulo, and keep_cross_validation_folds must be set to True.", required = true)
-    public KeyV3.ModelKeyV3 base_models[];
-    */
 
     @API(help = "List of model ids which we can stack together. Models must have been cross-validated using nfolds > 1, and folds must be identical across models.", required = true)
     public KeyV3.ModelKeyV3 base_models[];
 
     @API(help = "Keep level one frame used for metalearner training.")
     public boolean keep_levelone_frame;
+
+    @API(level = API.Level.critical, direction = API.Direction.INOUT, gridable = true,
+            help = "Number of folds for K-fold cross-validation of the metalearner algorithm (0 to disable or >= 2).")
+    public int metalearner_nfolds;
+
+    // TODO: Add metalearner_fold_column
+    // also requires: import water.api.schemas3.FrameV3;
+    /*
+    @API(level = API.Level.secondary, direction = API.Direction.INOUT, gridable = true,
+            is_member_of_frames = {"training_frame"},
+            is_mutually_exclusive_with = {"ignored_columns", "response_column", "weights_column", "offset_column"},
+            help = "Column with cross-validation fold index assignment per observation.")
+    public FrameV3.ColSpecifierV3 metalearner_fold_column;
+    */
+
+    // For Ensemble cross-validation
+    @API(level = API.Level.secondary, direction = API.Direction.INOUT, gridable = true,
+            values = {"AUTO", "Random", "Modulo", "Stratified"},
+            help = "Cross-validation fold assignment scheme for metalearner cross-validation.  Defaults to AUTO (which is currently set to Random). The 'Stratified' option will " +
+                    "stratify the folds based on the response variable, for classification problems.")
+    public Model.Parameters.FoldAssignmentScheme metalearner_fold_assignment;
   }
 }
