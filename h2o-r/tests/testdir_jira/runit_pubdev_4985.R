e5f0071b8a2c832ad83f751fc2224253bd9c7b26
michalkurka <michalk@h2o.ai>
2017-11-20 13:14:58 -0800

    Use a correct seed for reproducibility

--- h2o-r/tests/testdir_jira/runit_pubdev_4985.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_4985.R
@@ -6,7 +6,7 @@ source("../../scripts/h2o-r-test-setup.R")
 test.pubdev.4985 <- function() {
     expected <- data.frame(a = factor(c("a", NA, NA, NA, NA, NA)), b = 42)
     data <- as.h2o(expected)
-    split <- h2o.splitFrame(data, ratios = c(0.5))
+    split <- h2o.splitFrame(data, ratios = c(0.5), seed = 123)
     actual <- as.data.frame(h2o.rbind(split[[1]], split[[2]]))
 
     expect_equal(expected, actual)
