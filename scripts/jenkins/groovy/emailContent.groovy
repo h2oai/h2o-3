580e3fd41b2f34ad50dbf483478b7378a94eae34
Michal Raška <michal.raska@gmail.com>
2017-11-23 09:55:36 -0800

    [ITA-101] Inline styles

--- scripts/jenkins/groovy/emailContent.groovy
+++ scripts/jenkins/groovy/emailContent.groovy
@@ -3,11 +3,19 @@ import groovy.text.markup.MarkupTemplateEngine
 import groovy.text.markup.TemplateConfiguration
 import groovy.text.Template
 
-def call() {
-    def LOGO_URL = 'https://h2o2016.wpengine.com/wp-content/themes/h2o2016/images/H2O_logo_yellow.svg'
+def call(final String result) {
+    def LOGO_URL = 'https://pbs.twimg.com/profile_images/501572396810129408/DTgFCs-n.png'
     def REPO_URL = 'https://github.com/h2oai/h2o-3'
+    
+    def TR_EVEN_STYLE = 'background: #fff'
+    def TR_ODD_STYLE = 'background: #ccc'
+    def TD_TH_STYLE = 'border: 1px solid #aaa; padding: 0.5em; text-align: left;'
+    def RESULT_INDICATOR_SUCCESS_STYLE = 'width: 10px; background-color: rgb(0, 255, 0);'
+    def RESULT_INDICATOR_FAILURE_STYLE = 'width: 10px; background-color: rgb(255, 0, 0);'
+    
+    def SECTION_HEADER_STYLE = 'font-weight:bold; font-size:16pt;'
+    def SECTION_CONTENT_STYLE = 'margin-left: 15px; padding-bottom: 15px;'
 
-    def result = currentBuild.currentResult
     def build = currentBuild.rawBuild
 
     def changesContent = ''
@@ -17,6 +25,15 @@ def call() {
                 changesContent += "li('<a href=\"${REPO_URL}/commit/${changeSet.getRevision()}\"><strong>${changeSet.getRevision().substring(0, 8)}</strong></a> by <strong>${changeSet.getAuthorEmail()}</strong> - ${changeSet.getMsg()}')"
             }
         }
+    }
+    def changesSection = ''
+    if (changesContent != '') {
+        changesSection = """p('style':'${SECTION_HEADER_STYLE}', 'Changes')
+        div('style':'${SECTION_CONTENT_STYLE}') {
+            ul {
+                ${changesContent}
+            }
+        }"""
     }
 
     testResultsAction = build.getAction(AbstractTestResultAction.class)
@@ -44,12 +61,12 @@ def call() {
         stages << ['name': it.getStartNode().getDisplayName(), 'result': boolToSuccess(it.getError() == null).toLowerCase()]
     }
     stagesContent = ''
-    stages.each { stage ->
+    stages.eachWithIndex { stage, index ->
         stagesContent += """
-            tr('class':'row') {
-                td('class':'result-indicator ${stage['result']}') 
-                td('${stage['name']}') 
-                td('${stage['result'].capitalize()}') 
+            tr('style':'${index % 2 == 0 ? TR_EVEN_STYLE : TR_ODD_STYLE}') {
+                td('style':'${TD_TH_STYLE}${stage['result'] == 'success' ? RESULT_INDICATOR_SUCCESS_STYLE : RESULT_INDICATOR_FAILURE_STYLE}') 
+                td('style':'${TD_TH_STYLE}', '${stage['name']}') 
+                td('style':'${TD_TH_STYLE}', '${stage['result'].capitalize()}') 
             }"""
     }
 
@@ -59,128 +76,37 @@ def call() {
     MarkupTemplateEngine engine = new MarkupTemplateEngine(config)
     String templateString = """
 html {
-    head {
-        style('type':'text/css', '''
-              .summary {
-                  box-shadow: 0px 5px 5px #aaaaaa;
-                  background-color: #424242;
-                  color: white;
-                }
-            
-                .summary-header {
-                  display: table;
-                  overflow: hidden;
-                  height: 150px;
-                }
-            
-                .summary-header-content {
-                  display: table-cell;
-                  vertical-align: middle;
-                  padding-left: 1em;
-                }
-            
-                .summary-result.failure {
-                  color: white;
-                  background-color: #f8433c;
-                }
-            
-                .summary-result.success {
-                  background-color: #84e03e;
-                }
-            
-                .summary-header a {
-                  color: white;
-                  text-decoration: underline;
-                  display: inline-block;
-                  font-size: 20pt;
-                  font-weight: bolder;
-                  line-height: 82px;
-                  vertical-align: middle;
-                  background-image: url(${LOGO_URL});
-                  background-repeat: no-repeat;
-                  padding-left: 92px;
-                }
-            
-                .summary-result {
-                  height: 50px;
-                }
-            
-                .content {
-                  border: 1px solid gray;
-                  padding: 1em 1em 1em 1em;
-                }
-            
-                .section-header {
-                  font-weight: bold;
-                  font-size: 16pt;
-                }
-            
-                .section-content {
-                  margin-left: 15px;
-                  padding-bottom: 15px;
-                }
-            
-                tr:nth-child(even) {
-                  background: #CCC
-                }
-            
-                tr:nth-child(odd) {
-                  background: #FFF
-                }
-            
-                table {
-                  width: 80%;
-                  border-collapse: collapse;
-                }
-            
-                td, th {
-                  border: 1px solid #aaa;
-                  padding: 0.5em;
-                  text-align: left;
-                }
-            
-                .result-indicator {
-                  width: 10px;
-                }
-            
-                .result-indicator.failure {
-                  background-color: rgb(255, 0, 0);
-                }
-            
-                .result-indicator.success {
-                  background-color: rgb(0, 255, 0);
-            ''')
-    }
-        
     body {
-        div('class':'summary') {
-            div('class':'summary-header') {
-                div('class':'summary-header-content') {
-                    a('class':'heading', 'href':'${build.getAbsoluteUrl()}', "Build #${build.number} - ${result}")
+        div('style':'box-shadow: 0px 5px 5px #aaaaaa;background-color: #424242;color: white;') {
+            div('style':'display: table;overflow: hidden;height: 150px;') {
+                div('style':'display: table-cell;vertical-align: middle;padding-left: 1em;') {
+                    div {
+                        img('width':'80', 'height':'80', 'alt':'H2O.ai', 'title':'H2O.ai', 'style':'vertical-align:middle;', 'src':'${LOGO_URL}')
+                        a('style':'vertical-align:middle;color: white;font-size: 20pt;font-weight: bolder;margin-left: 20px;', 'href':'${build.getAbsoluteUrl()}', "${URLDecoder.decode(env.JOB_NAME, "UTF-8")} #${build.number} - ${result}")
+                    }
                 }
             }
-            div('class':'summary-result ${result.toLowerCase()}') {
+            div('style':'height: 15px; ${result.toLowerCase() == 'success' ? 'background-color: #84e03e;': 'color: white;background-color: #f8433c;'}') {
             }
         }
-        div('class':'content') {
-            p('class':'section-header', 'Duration')
-            div('class':'section-content') {
-                p('Started at <strong>${new Date(build.getStartTimeInMillis())}</strong>')
-            }
-            p('class':'section-header', 'Changes')
-            div('class':'section-content') {
+        div('style':'border: 1px solid gray;padding: 1em 1em 1em 1em;') {
+            p('style':'${SECTION_HEADER_STYLE}', 'Details')
+            div('style':'${SECTION_CONTENT_STYLE}') {
                 ul {
-                    ${changesContent}
+                    li('Duration: Started at <strong>${new Date(build.getStartTimeInMillis())}</strong>')
+                    li('Branch: <strong>${env.BRANCH_NAME}</strong>')
+                    li('SHA: <strong>${env.GIT_SHA}</strong>')
                 }
             }
-            p(class:'section-header', 'Stages Overview')
-            div('class':'section-content') {
-                table {
+            ${changesSection}
+            p('style':'${SECTION_HEADER_STYLE}', 'Stages Overview')
+            div('style':'${SECTION_CONTENT_STYLE}') {
+                table('style':'width: 80%;border-collapse: collapse;') {
                     thead {
                         tr {
-                            th('class':'result-indicator')
-                            th('Stage')
-                            th('Result')
+                            th('style':'${TD_TH_STYLE}')
+                            th('style':'${TD_TH_STYLE}', 'Stage')
+                            th('style':'${TD_TH_STYLE}', 'Result')
                         }
                     }
                     tbody {
@@ -188,8 +114,8 @@ html {
                     }
                 }
             }
-            p(class:'section-header', 'Tests Overview')
-            div('class':'section-content') {
+            p('style':'${SECTION_HEADER_STYLE}', 'Tests Overview')
+            div('style':'${SECTION_CONTENT_STYLE}') {
                 ${testsContent}
             }
         }
@@ -208,6 +134,13 @@ def boolToSuccess(final boolean value) {
         return 'SUCCESS'
     }
     return 'FAILURE'
+}
+
+def boolToColor(final boolean value) {
+    if (value) {
+        return ''
+    }
+    return ''
 }
 
 return this
--- scripts/jenkins/groovy/sendEmail.groovy
+++ scripts/jenkins/groovy/sendEmail.groovy
@@ -1,17 +1,16 @@
-def call(emailContent) {
+def call(final String result, emailContent) {
     def FAILURE_RECIPIENTS = 'michalk@h2o.ai'
     def ALWAYS_RECIPIENTS = 'michalr@h2o.ai'
 
     def recipients = ALWAYS_RECIPIENTS
-    def result = currentBuild.currentResult
-//    if (result != 'SUCCESS') {
-//        recipients = "${ALWAYS_RECIPIENTS},${FAILURE_RECIPIENTS}"
-//    }
+    if (result.toLowerCase() != 'success') {
+        recipients = "${ALWAYS_RECIPIENTS},${FAILURE_RECIPIENTS}"
+    }
     def subject = "${env.JOB_NAME.split('/')[0]}: ${result}"
 
     emailext (
         subject: subject,
-        body: emailContent(),
+        body: emailContent(result),
         to: recipients
     )
 }
