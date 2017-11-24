995251adfe27efc816e76644801835376868cb1c
Michal Malohlava <michal.malohlava@gmail.com>
2017-11-24 09:46:10 -0800

    [:fire:] Remove debug print

--- h2o-core/src/main/java/water/init/NetworkInit.java
+++ h2o-core/src/main/java/water/init/NetworkInit.java
@@ -98,7 +98,6 @@ public class NetworkInit {
         break;
 
       } catch (Exception e) {
-        e.printStackTrace();
         for (Throwable ee = e; ee != null; ee = ee.getCause()) {
           if (ee instanceof GeneralSecurityException)
             throw new RuntimeException("Jetty Server initialization failed (check keystore password)", e);
