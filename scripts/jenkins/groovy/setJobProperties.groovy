f64ecbe891b433c859ec6500a1395864917c02b3
Michal Raška <michalr@h2o.ai>
2017-12-11 11:28:32 +0100

    [ITA-140] Use Multibranch for nightly builds of H2O-3. (#1829)

--- scripts/jenkins/groovy/setJobProperties.groovy
+++ scripts/jenkins/groovy/setJobProperties.groovy
@@ -1,14 +1,18 @@
-def call(buildConfig) {
-  properties(
-    [
-      parameters(
-        [
-          booleanParam(defaultValue: buildConfig.getDefaultOverrideRerun(), description: 'If checked, execute all stages regardless of the commit message content. If not checked and the message contains !rerun, only stages failed in previous build will be executed.', name: 'overrideRerun')
-        ]
-      ),
-      buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '25'))
+def call(buildConfig, customProperties=null) {
+    def jobProperties = [
+            parameters(
+                    [
+                            booleanParam(defaultValue: buildConfig.getDefaultOverrideRerun(), description: 'If checked, execute all stages regardless of the commit message content. If not checked and the message contains !rerun, only stages failed in previous build will be executed.', name: 'overrideRerun')
+                    ]
+            ),
+            buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '25'))
     ]
-  )
+    if (customProperties != null) {
+        jobProperties += customProperties
+    }
+    properties(
+        jobProperties
+    )
 }
 
 return this
