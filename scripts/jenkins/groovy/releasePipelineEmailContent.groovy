5c897e700fba091c69afa493e13e448e48ea038c
Michal Raška <michalr@h2o.ai>
2017-12-29 21:42:10 +0100

    [ITA-157] Fix NPE in release pipeline email content (#1882)

--- scripts/jenkins/groovy/releasePipelineEmailContent.groovy
+++ scripts/jenkins/groovy/releasePipelineEmailContent.groovy
@@ -11,9 +11,10 @@ def call(final String result, helpers) {
         ul {
             li('Version: <strong>${env.PROJECT_VERSION}</strong>')
             li('Release Node: <strong>${env.BUILT_ON_NODE}</strong>')
-            li('Nexus Upload: <strong>${strBoolToYesNo(env.UPLOAD_NEXUS)}</strong>')
-            li('PyPI Upload: <strong>${strBoolToYesNo(env.UPLOAD_PYPI)}</strong>')
-            li('Conda Upload: <strong>${strBoolToYesNo(env.UPLOAD_CONDA)}</strong>')
+            li('Test Release: <strong>${params.TEST_RELEASE}</strong>')
+            li('Nexus Upload: <strong>${params.UPLOAD_NEXUS}</strong>')
+            li('PyPI Upload: <strong>${params.UPLOAD_PYPI}</strong>')
+            li('Conda Upload: <strong>${params.UPLOAD_CONDA}</strong>')
         }
     """
 
@@ -28,10 +29,6 @@ div('style':'border: 1px solid gray;padding: 1em 1em 1em 1em;') {
 
     Template template = engine.createTemplate(templateString)
     return template.make().toString()
-}
-
-def strBoolToYesNo(final String value) {
-    return value.toLowerCase() == 'true' ? 'Yes' : 'No'
 }
 
 return this
