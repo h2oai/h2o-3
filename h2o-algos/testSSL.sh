505fc3d09e0e4e60febbea418d626875f6be31a2
Michal Raška <michalr@h2o.ai>
2018-01-15 10:16:41 +0100

    [ITA-191] All JUnit test scripts should now use $JVM_CLASSPATH passed from Gradle, instead of defining the classpath by themselves. (#1935)

--- h2o-algos/testSSL.sh
+++ h2o-algos/testSSL.sh
@@ -88,7 +88,7 @@ else
   fi
 fi
 
-JVM="nice $JAVA_CMD -ea -Xmx3g -Xms3g -cp build/libs/h2o-algos-test.jar${SEP}build/libs/h2o-algos.jar${SEP}../h2o-core/build/libs/h2o-core-test.jar${SEP}../h2o-core/build/libs/h2o-core.jar${SEP}../h2o-genmodel/build/libs/h2o-genmodel.jar${SEP}../lib/*"
+JVM="nice $JAVA_CMD -ea -Xmx3g -Xms3g -cp ${JVM_CLASSPATH}"
 echo "$JVM" > $OUTDIR/jvm_cmd.txt
 
 SSL=""
