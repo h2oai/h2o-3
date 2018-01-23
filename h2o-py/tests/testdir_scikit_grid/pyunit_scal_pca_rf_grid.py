e2f233f12df6e7ddfb15a3a5327adaea7bc02be0
angela0xdata <angela@h2o.ai>
2018-01-23 13:44:21 -0800

    Un-reverting changes for scikit learn tests
    Using h2o.estimators.pca import H2OPrincipalComponentAnalysisEstimator
    in scikit learn examples no longer  gives an error when running fit().

--- h2o-py/tests/testdir_scikit_grid/pyunit_scal_pca_rf_grid.py
+++ h2o-py/tests/testdir_scikit_grid/pyunit_scal_pca_rf_grid.py
@@ -9,7 +9,7 @@ from tests import pyunit_utils
 def scale_pca_rf_pipe():
 
   from h2o.transforms.preprocessing import H2OScaler
-  from h2o.transforms.decomposition import H2OPCA
+  from h2o.estimators.pca import H2OPrincipalComponentAnalysisEstimator
   from h2o.estimators.random_forest import H2ORandomForestEstimator
   from sklearn.pipeline import Pipeline
   from sklearn.grid_search import RandomizedSearchCV
@@ -23,7 +23,7 @@ def scale_pca_rf_pipe():
 
   # build  transformation pipeline using sklearn's Pipeline and H2O transforms
   pipe = Pipeline([("standardize", H2OScaler()),
-                   ("pca", H2OPCA()),
+                   ("pca", H2OPrincipalComponentAnalysisEstimator()),
                    ("rf", H2ORandomForestEstimator())])
 
   params = {"standardize__center":    [True, False],             # Parameters to test
