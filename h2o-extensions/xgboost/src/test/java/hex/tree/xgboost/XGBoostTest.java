5a026af5f979ff4d9a40b839e3e36daaa559a8b2
Raymond Peck <rpeck@rpeck.com>
2018-01-24 14:15:22 -0800

    PUBDEV-5222: fix the test which had incorrectly indicated that there was a bug

--- h2o-extensions/xgboost/src/test/java/hex/tree/xgboost/XGBoostTest.java
+++ h2o-extensions/xgboost/src/test/java/hex/tree/xgboost/XGBoostTest.java
@@ -60,24 +60,24 @@ public class XGBoostTest extends TestUtil {
 
       if (vecs.length != fm.vecs.length) {
         Log.warn("Training frame vec count has changed from: " +
-                fm.vecs.length + " to: " + fm.vecs.length);
+                vecs.length + " to: " + fm.vecs.length);
         error = true;
       }
       if (fm.names.length != fm.names.length) {
         Log.warn("Training frame vec count has changed from: " +
-                fm.names.length + " to: " + fm.names.length);
+                names.length + " to: " + fm.names.length);
         error = true;
       }
 
       for (int i = 0; i < fm.vecs.length; i++) {
         if (!fm.vecs[i].equals(fm.vecs[i])) {
           Log.warn("Training frame vec number " + i + " has changed keys.  Was: " +
-                  fm.vecs[i] + " , now: " + fm.vecs[i]);
+                  vecs[i] + " , now: " + fm.vecs[i]);
           error = true;
         }
         if (!fm.names[i].equals(fm.names[i])) {
           Log.warn("Training frame vec number " + i + " has changed names.  Was: " +
-                  fm.names[i] + " , now: " + fm.names[i]);
+                  names[i] + " , now: " + fm.names[i]);
           error = true;
         }
         if (checksums[i] != fm.vecs[i].checksum()) {
@@ -334,7 +334,6 @@ public class XGBoostTest extends TestUtil {
     try {
       // Parse frame into H2O
       tfr = parse_test_file("./smalldata/junit/weather.csv");
-      FrameMetadata metadataBefore = new FrameMetadata(tfr);
       // define special columns
       String response = "RainTomorrow";
 //      String weight = null;
@@ -343,6 +342,7 @@ public class XGBoostTest extends TestUtil {
       // remove columns correlated with the response
       tfr.remove("RISK_MM").remove();
       tfr.remove("EvapMM").remove();
+      FrameMetadata metadataBefore = new FrameMetadata(tfr);  // make sure it's after removing those columns!
       DKV.put(tfr);
 
       // split into train/test
@@ -363,8 +363,7 @@ public class XGBoostTest extends TestUtil {
       Log.info(model);
 
       FrameMetadata metadataAfter = new FrameMetadata(tfr);
-      // TODO: this fails; put back: Assert.assertEquals(metadataBefore, metadataAfter);
-      // See: https://0xdata.atlassian.net/browse/PUBDEV-5222
+      Assert.assertEquals(metadataBefore, metadataAfter);
 
       preds = model.score(testFrame);
       Assert.assertTrue(model.testJavaScoring(testFrame, preds, 1e-6));
@@ -396,7 +395,6 @@ public class XGBoostTest extends TestUtil {
       Scope.enter();
       // Parse frame into H2O
       tfr = parse_test_file("./smalldata/junit/weather.csv");
-      FrameMetadata metadataBefore = new FrameMetadata(tfr);
       // define special columns
       String response = "RainTomorrow";
 //      String weight = null;
@@ -405,6 +403,7 @@ public class XGBoostTest extends TestUtil {
       // remove columns correlated with the response
       tfr.remove("RISK_MM").remove();
       tfr.remove("EvapMM").remove();
+      FrameMetadata metadataBefore = new FrameMetadata(tfr);  // make sure it's after removing those columns!
       DKV.put(tfr);
 
       // split into train/test
@@ -427,8 +426,7 @@ public class XGBoostTest extends TestUtil {
       Log.info(model);
 
       FrameMetadata metadataAfter = new FrameMetadata(tfr);
-      // TODO: this fails; put back: Assert.assertEquals(metadataBefore, metadataAfter);
-      // See: https://0xdata.atlassian.net/browse/PUBDEV-5222
+      Assert.assertEquals(metadataBefore, metadataAfter);
 
       preds = model.score(testFrame);
       Assert.assertTrue(model.testJavaScoring(testFrame, preds, 1e-6));
