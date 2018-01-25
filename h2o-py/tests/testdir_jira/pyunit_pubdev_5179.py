616ec67ff3a610269cdcfb04bc87908291dbade8
Jakub Hava <jakub.hava@gmail.com>
2018-01-25 17:57:49 +0100

    Additional assert

--- h2o-py/tests/testdir_jira/pyunit_pubdev_5179.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_5179.py
@@ -4,13 +4,14 @@ from tests import pyunit_utils
 import numpy
 
 def pubdev_5179():
-    
+
     data = [numpy.arange(0, 999).tolist() for x in numpy.arange(0, 99).tolist()]
     fr = h2o.H2OFrame(data)
     light = H2OFrame.get_frame(fr.frame_id, light=True)
 
     # verify that light frame have all columns
     assert len(light.columns) == 999
+    assert len(light.types) == 999
 
 
 if __name__ == "__main__":
