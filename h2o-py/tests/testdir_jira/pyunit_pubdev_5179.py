138113709aa18f015872523ae70e85b237ce50d7
Jakub Hava <jakub.hava@gmail.com>
2018-01-25 17:54:03 +0100

    Write test

---
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_5179.py
@@ -0,0 +1,19 @@
+import h2o
+from h2o.frame import H2OFrame
+from tests import pyunit_utils
+import numpy
+
+def pubdev_5179():
+    
+    data = [numpy.arange(0, 999).tolist() for x in numpy.arange(0, 99).tolist()]
+    fr = h2o.H2OFrame(data)
+    light = H2OFrame.get_frame(fr.frame_id, light=True)
+
+    # verify that light frame have all columns
+    assert len(light.columns) == 999
+
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(pubdev_5179)
+else:
+    pubdev_5179()
