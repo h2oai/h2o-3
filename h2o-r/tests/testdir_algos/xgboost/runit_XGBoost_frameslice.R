219fd16f158f7281df90aebd7cdc092a28f32a99
Raymond Peck <rpeck@rpeck.com>
2018-02-02 13:42:31 -0800

    PUBDEV-5249: XGBoost: add more runit and pyunit tests

---
+++ h2o-r/tests/testdir_algos/xgboost/runit_XGBoost_frameslice.R
@@ -0,0 +1,17 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+
+
+test.XGBoost.frameslice <- function() {
+  Log.info("Importing prostate data...\n")
+  pros.hex <- h2o.importFile(path = locate("smalldata/logreg/prostate.csv"))
+
+  Log.info("Running XGBoost on a sliced data frame...\n")
+  pros.hex[,2] = as.factor(pros.hex[,2])
+  pros.xgboost <- h2o.xgboost(x = 2:8, y = 1, training_frame = pros.hex[, 2:9], distribution = "bernoulli")
+
+  
+}
+
+doTest("XGBoost Test: Model building on sliced h2o frame", test.XGBoost.frameslice)
