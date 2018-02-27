4277212110c32c83f283b8a85b457d1f3abd8338
Michal Raška <michalr@h2o.ai>
2018-02-27 08:39:30 +0100

    [PUBDEV-5343] Fix CustomHttpFilterTest (#2074)

--- h2o-core/src/main/java/water/JettyHTTPD.java
+++ h2o-core/src/main/java/water/JettyHTTPD.java
@@ -145,7 +145,12 @@ public class JettyHTTPD extends AbstractHTTPD {
         try { Thread.sleep(100); }
         catch (Exception ignore) {}
       }
-      setCommonResponseHttpHeaders(response, isXhrRequest(baseRequest));
+
+      boolean isXhrRequest = false;
+      if (request != null) {
+        isXhrRequest = isXhrRequest(request);
+      }
+      setCommonResponseHttpHeaders(response, isXhrRequest);
     }
   }
 
@@ -313,7 +318,7 @@ public class JettyHTTPD extends AbstractHTTPD {
     }
   }
 
-  private static boolean isXhrRequest(final Request request) {
+  private static boolean isXhrRequest(final HttpServletRequest request) {
     final String requestedWithHeader = request.getHeader("X-Requested-With");
     return "XMLHttpRequest".equals(requestedWithHeader);
   }
