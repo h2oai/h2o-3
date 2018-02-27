4e26ccf271cf4cf35925004628199c06707d7e88
Megan Kurka <megankurka@Megans-MacBook-Pro.local>
2018-02-26 16:59:01 -0800

    fixing R documentation issue

--- h2o-r/h2o-package/R/frame.R
+++ h2o-r/h2o-package/R/frame.R
@@ -4505,11 +4505,13 @@ h2o.stringdist <- function(x, y, method = c("lv", "lcs", "qgram", "jaccard", "jw
 #' data <- h2o.importFile(
 #' path = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/demos/bank-additional-full.csv",
 #' destination_frame = "data")
-#' mapping_age <- h2o.target_encode_create(data = data, x = list(c("job"), c("job", "marital")), y = "age")
+#' mapping_age <- h2o.target_encode_create(data = data, x = list(c("job"), c("job", "marital")), 
+#' y = "age")
 #' head(mapping_age)
 #' 
 #' # Get Target Encoding Map on bank-additional-full data with binary response
-#' mapping_y <- h2o.target_encode_create(data = data, x = list(c("job"), c("job", "marital")), y = "y")
+#' mapping_y <- h2o.target_encode_create(data = data, x = list(c("job"), c("job", "marital")), 
+#' y = "y")
 #' head(mapping_y)
 #' 
 #' }
