b32f06004bf8187bdc9a91e024686d27a09302be
michalkurka <michalk@h2o.ai>
2018-03-02 10:32:42 -0800

    GLM Ordinal: fix test + revert changes in Frame

--- h2o-core/src/main/java/water/fvec/Frame.java
+++ h2o-core/src/main/java/water/fvec/Frame.java
@@ -471,9 +471,8 @@ public class Frame extends Lockable<Frame> {
   public String[][] domains() {
     Vec[] vecs = vecs();
     String ds[][] = new String[vecs.length][];
-    for( int i=0; i<vecs.length; i++ ) {
-        ds[i] = vecs[i].domain();
-    }
+    for(int i=0; i<vecs.length; i++)
+      ds[i] = vecs[i].domain();
     return ds;
   }
 
