97dfe2d1fad6861097c848cc9eb909d241b55ff9
nikhil shekhar <nikhilshekhar2000@gmail.com>
2018-03-20 02:52:22 -0700

    Add the generated file - aggregator.R

--- h2o-r/h2o-package/R/aggregator.R
+++ h2o-r/h2o-package/R/aggregator.R
@@ -20,6 +20,20 @@
 #' @param save_mapping_frame \code{Logical}. Whether to export the mapping of the aggregated frame Defaults to FALSE.
 #' @param num_iteration_without_new_exemplar The number of iterations to run before aggregator exits if the number of exemplars collected didn't change
 #'        Defaults to 500.
+#' @examples
+#' \donttest{
+#' library(h2o)
+#' h2o.init()
+#' df <- h2o.createFrame(rows=100, cols=5, categorical_fraction=0.6, integer_fraction=0,
+#' binary_fraction=0, real_range=100, integer_range=100, missing_fraction=0)
+#' target_num_exemplars=1000
+#' rel_tol_num_exemplars=0.5
+#' encoding="Eigen"
+#' agg <- h2o.aggregator(training_frame=df,
+#' target_num_exemplars=target_num_exemplars,
+#' rel_tol_num_exemplars=rel_tol_num_exemplars,
+#' categorical_encoding=encoding)
+#' }
 #' @export
 h2o.aggregator <- function(training_frame, x,
                            model_id = NULL,
