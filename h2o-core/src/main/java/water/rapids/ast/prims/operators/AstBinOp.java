90b895c1888c7e80df85e98cf23a01cfa004aa0c
Pavel Pscheidl <pavel@h2o.ai>
2018-03-23 08:05:01 +0100

    Pavel pubdev 5336 rel wolpert (#2192)
    * PUBDEV-5336 - h2o python: get wrong ans when run a conditional statement in h2o
    
    (cherry picked from commit 447484d)
    
    * Rename variable with transformed indices
    
    (cherry picked from commit 8ef2130)
    
    * Whitespaces deleted
    
    (cherry picked from commit 935de15)
    
    * Avoided allocation of additional memory by passing arguments in right order beforehand
    
    (cherry picked from commit 75faba1)
    
    * An attempt to quick fix/verify vec type problem
    
    (cherry picked from commit 4e976bd)
    
    * NaN handling. Pre-calculated categorical column conditions.
    
    (cherry picked from commit f6c3cea)
    
    * Testing NAs
    
    (cherry picked from commit e0aeacb)
    
    * NAs in test
    
    (cherry picked from commit f746312)
    
    (cherry picked from commit ba9d1b6)

--- h2o-core/src/main/java/water/rapids/ast/prims/operators/AstBinOp.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/operators/AstBinOp.java
@@ -3,6 +3,7 @@ package water.rapids.ast.prims.operators;
 import water.Futures;
 import water.H2O;
 import water.MRTask;
+import water.MemoryManager;
 import water.fvec.Chunk;
 import water.fvec.Frame;
 import water.fvec.NewChunk;
@@ -16,7 +17,7 @@ import water.rapids.vals.ValNum;
 import water.rapids.vals.ValRow;
 import water.util.ArrayUtils;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Binary operator.
@@ -309,12 +310,32 @@ abstract public class AstBinOp extends AstPrimitive {
       } else
         throw new IllegalArgumentException("Frames must have same rows, found " + lf.numRows() + " rows and " + rt.numRows() + " rows.");
     }
+
+
+
     if (lf.numCols() == 0) return new ValFrame(lf);
     if (rt.numCols() == 0) return new ValFrame(rt);
     if (lf.numCols() == 1 && rt.numCols() > 1) return vec_op_frame(lf.vecs()[0], rt);
     if (rt.numCols() == 1 && lf.numCols() > 1) return frame_op_vec(lf, rt.vecs()[0]);
     if (lf.numCols() != rt.numCols())
       throw new IllegalArgumentException("Frames must have same columns, found " + lf.numCols() + " columns and " + rt.numCols() + " columns.");
+
+    final int[][] alignedCategoricals = new int[lf.numCols()][];
+    final boolean[] categorical = new boolean[lf.numCols()];
+    final boolean[] rtDomainNotBigger = new boolean[lf.numCols()];
+    for (int c = 0; c < lf.numCols(); c++) {
+      // Store to read during iteration over lines
+      categorical[c] = categoricalOK() && lf.vec(c).isCategorical() && rt.vec(c).isCategorical();
+      if (categorical[c]) {
+        // Store to read during iteration over lines
+        rtDomainNotBigger[c] = lf.vec(c).domain().length >= rt.vec(c).domain().length;
+        if (rtDomainNotBigger[c]) {
+          alignedCategoricals[c] = alignCategoricals(lf.vec(c).domain(), rt.vec(c).domain());
+        } else {
+          alignedCategoricals[c] = alignCategoricals(rt.vec(c).domain(), lf.vec(c).domain());
+        }
+      }
+    }
 
     Frame res = new MRTask() {
       @Override
@@ -329,13 +350,56 @@ abstract public class AstBinOp extends AstPrimitive {
           if (clf.vec().isString())
             for (int i = 0; i < clf._len; i++)
               cres.addNum(str_op(clf.atStr(lfstr, i), crt.atStr(rtstr, i)));
-          else
+          else if (categorical[c]) {
+            // The vec with longer domain is iterated over due to categorical mapping
+            if (rtDomainNotBigger[c]) {
+              for (int i = 0; i < clf._len; i++) {
+                double crtAtdValue = crt.atd(i);
+                if (crt.isNA(i)) {
+                  cres.addNum(op(clf.atd(i), crtAtdValue));
+                } else {
+                  cres.addNum(op(clf.atd(i), alignedCategoricals[c][(int) crtAtdValue]));
+                }
+              }
+            } else {
+              for (int i = 0; i < clf._len; i++) {
+                double clfAtdValue = clf.atd(i);
+                if (clf.isNA(i)) {
+                  cres.addNum(op(clfAtdValue, crt.atd(i)));
+                } else {
+                  cres.addNum(op(alignedCategoricals[c][(int) clfAtdValue], crt.atd(i)));
+                }
+              }
+            }
+          } else {
             for (int i = 0; i < clf._len; i++)
               cres.addNum(op(clf.atd(i), crt.atd(i)));
+          }
         }
       }
     }.doAll(lf.numCols(), Vec.T_NUM, new Frame(lf).add(rt)).outputFrame(lf._names, null);
     return cleanCategorical(lf, res); // Cleanup categorical misuse
+  }
+
+  /**
+   * Produces a mapping array with indexes of the smaller pointing to the larger domain.
+   *
+   * @param longerDomain Domain to originally map from
+   * @param shorterDomain Domain to originally map to
+   * @return Cross-domain mapping as an array of primitive integers
+   */
+  private int[] alignCategoricals(String[] longerDomain, String[] shorterDomain) {
+    String[] sortedLongerDomain = Arrays.copyOf(longerDomain, longerDomain.length);
+    //Sort to make sure binary search is possible
+    Arrays.sort(sortedLongerDomain);
+
+    int[] transformedIndices = MemoryManager.malloc4(shorterDomain.length);
+
+    for (int i = 0; i < shorterDomain.length; i++) {
+      transformedIndices[i] = Arrays.binarySearch(sortedLongerDomain, shorterDomain[i]);
+    }
+
+    return transformedIndices;
   }
 
   private ValFrame frame_op_row(Frame lf, Frame row) {
@@ -407,7 +471,11 @@ abstract public class AstBinOp extends AstPrimitive {
     return cleanCategorical(fr, res); // Cleanup categorical misuse
   }
 
-  // Make sense to run this OP on an enm?
+  /**
+   * Does it make sense to run this operation on a categorical variable ?
+   *
+   * @return True if the operation may be applied on a categorical variable, otherwise false.
+   */
   public boolean categoricalOK() {
     return false;
   }
