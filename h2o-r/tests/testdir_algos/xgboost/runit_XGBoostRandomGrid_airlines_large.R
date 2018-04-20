964186294f9db092ed99ca224d8ed7a2532d7208
Pavel Pscheidl <pavel@h2o.ai>
2018-04-20 13:03:54 +0200

    XGBooost airlines large test (random grid) split into two parts, each with different grow policy. Not all grow policies are compatible with all tree splitting modes.

--- h2o-r/tests/testdir_algos/xgboost/runit_XGBoostRandomGrid_airlines_large.R
+++ h2o-r/tests/testdir_algos/xgboost/runit_XGBoostRandomGrid_airlines_large.R
@@ -6,9 +6,9 @@ source("../../../scripts/h2o-r-test-setup.R")
 xgboost.random.grid.test <- function() {
     air.hex <- h2o.uploadFile(locate("smalldata/airlines/allyears2k_headers.zip"), destination_frame="air.hex")
     print(summary(air.hex))
-    myX <- c("Year","Month","CRSDepTime","UniqueCarrier","Origin","Dest")
+    xVars <- c("Year","Month","CRSDepTime","UniqueCarrier","Origin","Dest")
 
-    hyper_params = list( 
+    hyper_params_depthwise = list(
       ## restrict the search to the range of max_depth established above
       max_depth = seq(1,10,1),                                      
 
@@ -41,7 +41,7 @@ xgboost.random.grid.test <- function() {
 
       ## Some XGBoost-specific parameters:
       tree_method = c("auto", "exact", "hist", "approx"),
-      grow_policy = c("depthwise", "lossguide"),
+      grow_policy = c("depthwise"),
       booster = c("gbtree")
         
     )
@@ -65,25 +65,84 @@ xgboost.random.grid.test <- function() {
       stopping_tolerance = 1e-3
     )
 
-    air.grid <- h2o.grid("xgboost", y = "IsDepDelayed", x = myX,
+    air.grid <- h2o.grid("xgboost", y = "IsDepDelayed", x = xVars,
                          distribution="bernoulli",
                          seed=1234,
                          training_frame = air.hex,
-                         hyper_params = hyper_params,
+                         hyper_params = hyper_params_depthwise,
                          search_criteria = search_criteria,
                          nfolds = 5, fold_assignment = 'Modulo',
                          keep_cross_validation_predictions = TRUE)
     print(air.grid)
     expect_that(length(air.grid@model_ids) <= 5, is_true())
 
-    stacker <- h2o.stackedEnsemble(x = myX, y = "IsDepDelayed", 
+    stacker_depthwise <- h2o.stackedEnsemble(x = xVars, y = "IsDepDelayed",
                                    training_frame = air.hex,
                                    model_id = "my_ensemble",
                                    base_models = air.grid@model_ids)
 
-    predictions = h2o.predict(stacker, air.hex)  # training data
-    print("predictions for ensemble are in: ")
-    print(h2o.getId(predictions))
+    predictions_depthwise = h2o.predict(stacker_depthwise, air.hex)  # training data
+    print("predictions for ensemble with depthwise grow policy are in: ")
+    print(h2o.getId(predictions_depthwise))
+
+    hyper_params_lossguide = list(
+    ## restrict the search to the range of max_depth established above
+    max_depth = seq(1,10,1),
+
+    ## search a large space of row sampling rates per tree
+    sample_rate = seq(0.2,1,0.01),
+
+    ## search a large space of column sampling rates per split
+    col_sample_rate = seq(0.2,1,0.01),
+
+    ## search a large space of column sampling rates per tree
+    col_sample_rate_per_tree = seq(0.2,1,0.01),
+
+    ## search a large space of how column sampling per split should change as a function of the depth of the split
+    ## Not available in Xgboost: col_sample_rate_change_per_level = seq(0.9,1.1,0.01),
+
+    ## search a large space of the number of min rows in a terminal node
+    min_rows = 2^seq(0,log2(nrow(air.hex))-1,1),
+
+    ## search a large space of the number of bins for split-finding for continuous and integer columns
+    ## Not available in Xgboost: nbins = 2^seq(4,10,1),
+
+    ## search a large space of the number of bins for split-finding for categorical columns
+    ## Not available in Xgboost: nbins_cats = 2^seq(4,12,1),
+
+    ## search a few minimum required relative error improvement thresholds for a split to happen
+    min_split_improvement = c(0,1e-8,1e-6,1e-4),
+
+    ## try all histogram types (QuantilesGlobal and RoundRobin are good for numeric columns with outliers)
+    ## Not available in XGBoost:      histogram_type = c("UniformAdaptive","QuantilesGlobal","RoundRobin")
+
+    ## Some XGBoost-specific parameters:
+    tree_method = c("auto", "hist"),
+    grow_policy = c("lossguide"),
+    booster = c("gbtree")
+    )
+
+    air.grid <- h2o.grid("xgboost", y = "IsDepDelayed", x = xVars,
+    distribution="bernoulli",
+    seed=1234,
+    training_frame = air.hex,
+    hyper_params = hyper_params_lossguide,
+    search_criteria = search_criteria,
+    nfolds = 5, fold_assignment = 'Modulo',
+    keep_cross_validation_predictions = TRUE)
+    print(air.grid)
+    expect_that(length(air.grid@model_ids) <= 5, is_true())
+
+    stacker_lossguide <- h2o.stackedEnsemble(x = xVars, y = "IsDepDelayed",
+    training_frame = air.hex,
+    model_id = "my_ensemble",
+    base_models = air.grid@model_ids)
+
+    predictions_lossguide = h2o.predict(stacker_lossguide, air.hex)  # training data
+    print("predictions for ensemble with lossguide growth policy are in: ")
+    print(h2o.getId(predictions_lossguide))
+
+
 }
 
 doTest("XGBoost Grid Test: Airlines Smalldata", xgboost.random.grid.test)
