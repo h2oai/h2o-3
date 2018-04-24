ecdf1b469d44b618853424e38db73c94de361bda
Pavel Pscheidl <pavel@h2o.ai>
2018-04-24 11:17:24 +0200

    Python UnitTest classes may now be executed in pyunit_utils - the util class searches for methods starting with 'test_' prefix

--- h2o-py/tests/testdir_algos/xgboost/pyunit_prostate_gamma_wrong_response_small.py
+++ h2o-py/tests/testdir_algos/xgboost/pyunit_prostate_gamma_wrong_response_small.py
@@ -5,7 +5,7 @@ import unittest
 
 
 class TestGammaWrongResponseType(unittest.TestCase):
-    def test_response_error(self):
+    def test_numeric_response_error(self):
         assert H2OXGBoostEstimator.available()
 
         prostate_frame = h2o.import_file(path=pyunit_utils.locate("smalldata/prostate/prostate_complete.csv.zip"))
@@ -20,16 +20,12 @@ class TestGammaWrongResponseType(unittest.TestCase):
         with self.assertRaises(h2o.exceptions.H2OResponseError) as outcome:
             model.train(x=x, y=y, training_frame=prostate_frame)
 
-        assert str(outcome.exception).__contains__(
-            "ERRR on field: _distribution: Gamma requires the response to be numeric.")
-
-def xgboost_prostate_gamma_wrong_response_small():
-    TestGammaWrongResponseType().test_response_error();
+            assert str(outcome.exception).__contains__(
+                "ERRR on field: _distribution: Gamma requires the response to be numeric.")
 
 
 if __name__ == "__main__":
-    test = TestGammaWrongResponseType()
-    pyunit_utils.standalone_test(test.test_response_error)
+    pyunit_utils.standalone_test(unittest.main)
 else:
-    test = TestGammaWrongResponseType()
-    test.test_response_error()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestGammaWrongResponseType)
+    unittest.TextTestRunner().run(suite)
