4b8e769acd762e65c4d8c61f94dd7bd13d006ca9
wendycwong <wendycwong@yahoo.com>
2018-05-04 18:09:03 -0700

    Making python unit test.

--- h2o-py/tests/testdir_jira/pyunit_pubdev_5049_stopping_method_large.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_5049_stopping_method_large.py
@@ -4,14 +4,20 @@ sys.path.insert(1,"../../")
 import h2o
 from tests import pyunit_utils
 from h2o.estimators.deeplearning import H2ODeepLearningEstimator
-from h2o.estimators.gbm import H2OGradientBoostingEstimator
-from h2o.estimators.glm import H2OGeneralizedLinearEstimator
-from h2o.estimators.glrm import H2OGeneralizedLowRankEstimator
-from h2o.estimators.kmeans import H2OKMeansEstimator
-from h2o.estimators.pca import H2OPrincipalComponentAnalysisEstimator as H2OPCA
-from h2o.estimators.random_forest import H2ORandomForestEstimator
-from h2o.estimators.word2vec import H2OWord2vecEstimator
-from h2o.estimators.deepwater import H2ODeepWaterEstimator
+
+'''
+    This function will test if the stopping_method has been implemented correctly for the model type what was
+    sent.  In particular, it will perform the following tests:
+    1. Check to make sure stopping_method is checked and errors be thrown when
+        a. setting it to valid when a validation set if not provided;
+        b. setting it to xval when no cross validation is enabled;
+    2. when stopping_method is set to xval, the scoring history should be the same as if not stopping_method is
+    chosen by cross-validation was enabled.  Compare the two and make sure they are the same
+    3. when the stopping_method is set to valid, the scoring history should be the same as if not stopping_method is
+    chosen, cross-validation is disabled, a validation is provided.  Compare the two and make sure they are the same
+    4. when the stopping_method is set to train, the scoring history should be the same as if not stopping_method is
+    chosen, no validation is provided.  Compare the two and make sure they are the same
+'''
 
 model_runtime = []  # store actual model runtime in seconds
 model_maxRuntime = []   # store given maximum runtime restrictions placed on building models for different algos
@@ -21,7 +27,7 @@ model_runtime_overrun = []  # % by which the model runtime exceeds the maximum r
 model_within_max_runtime = []
 err_bound = 0.5              # fractor by which we allow the model runtime over-run to be
 
-def algo_max_runtime_secs():
+def test_deeplearning_stopping_method():
     '''
     This pyunit test is written to ensure that the max_runtime_secs can restrict the model training time for all
     h2o algos.  See PUBDEV-4702.
@@ -35,39 +41,44 @@ def algo_max_runtime_secs():
     validation_data = h2o.import_file(path=pyunit_utils.locate("smalldata/gridsearch/gaussian_training2_set.csv"))
     y_index = training1_data.ncol-1
     x_indices = list(range(y_index))
-    model = H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[10, 10, 10], stopping_method="valid",
-                                     stopping_rounds=3, stopping_tolerance=0.01, nfolds=3)
-    grabRuntimeInfo(err_bound, 2.0, model, training1_data, x_indices, y_index, validation_data)
+    model_auto_CV = H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_rounds=3,
+                                             stopping_tolerance=0.01, nfolds=3)
+    model_auto_noCV = H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_rounds=3,
+                                               stopping_tolerance=0.01)
+    model_noCV_valid = H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_method="valid",
+                                     stopping_rounds=3, stopping_tolerance=0.01)
+    model_CV = H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_method="valid",
+                                          stopping_rounds=3, stopping_tolerance=0.01, nfolds=3)
+    testStoppingMethod(0, training1_data, x_indices, y_index, validation_data)
 
 
 
-def grabRuntimeInfo(err_bound, reduction_factor, model, training_data, x_indices, y_index, validation_data):
+def testStoppingMethod(model_index, training_data, x_indices, y_index, validation_data):
     '''
-    This function will train the passed model, extract the model runtime.  Next it train the model again
-    with the max_runtime_secs set to the original model runtime divide by half.  At the end of both runs, it
-    will perform several tasks:
-    1. it will extract the new model runtime, calculate the runtime overrun factor as
-        (new model runtime - max_runtime_secs)/max_runtime_secs.
-    2. for iterative algorithms, it will calculate number of iterations/epochs dropped between the two models;
-    3. determine if the timing test passes/fails based on the runtime overrun factor and the iterations/epochs drop.
-        - test passed if runtime overrun factor < err_bound or if iterations/epochs drop > 0
-    4. it will print out all the related information regarding the timing test.
+    This function will test if the stopping_method has been implemented correctly for the model type what was
+    sent.  In particular, it will perform the following tests:
+    1. Check to make sure stopping_method is checked and errors be thrown when
+        a. setting it to valid when a validation set if not provided;
+        b. setting it to xval when no cross validation is enabled;
+    2. when stopping_method is set to xval, the scoring history should be the same as if not stopping_method is
+    chosen by cross-validation was enabled.  Compare the two and make sure they are the same
+    3. when the stopping_method is set to valid, the scoring history should be the same as if not stopping_method is
+    chosen, cross-validation is disabled, a validation is provided.  Compare the two and make sure they are the same
+    4. when the stopping_method is set to train, the scoring history should be the same as if not stopping_method is
+    chosen, no validation is provided.  Compare the two and make sure they are the same
 
-    :param err_bound: runtime overrun factor used to determine if test passed/failed
-    :param reduction_factor: how much run time to cut in order to set the max_runtime_secs
-    :param model: model to be evaluated
-    :param training_data: H2OFrame containing training dataset
-    :param x_indices: prediction input indices to model.train
-    :param y_index: response index to model.train
-    :return: None
+    :param model_index: choose model to be tested, 0 for deeplearning, 1 for GBM or 2 for RF
+    :param training_data: training data frame
+    :param x_indices: predictor column lists
+    :param y_index: response column
+    :param validation_data: validation dataset
+    :return: pass or fail the test
     '''
-    global model_runtime
-    global model_maxRuntime
-    global algo_names
-    global actual_model_runtime
-    global model_runtime_overrun
-    global model_within_max_runtime
-
+    models_valid = ["H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_method='valid', stopping_rounds=3, stopping_tolerance=0.01, nfolds=3)"]
+    models_train = ["H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_method='train', stopping_rounds=3, stopping_tolerance=0.01, nfolds=3)"]
+    models_xval = ["H2ODeepLearningEstimator(distribution='gaussian', seed=seed, hidden=[3], stopping_method='xval', stopping_rounds=3, stopping_tolerance=0.01, nfolds=3)"]
+    models_auto_CV = ["H2ODeepLearningEstimator(distribution='gaussian', seed=123456, hidden=[3], stopping_rounds=3, stopping_tolerance=0.01, nfolds=3)"]
+    models_auto_noCV = ["H2ODeepLearningEstimator(distribution='gaussian', seed=123456, hidden=[3], stopping_rounds=3, stopping_tolerance=0.01)"]
 
     model.train(x=x_indices, y=y_index, training_frame=training_data, validation_frame=validation_data)
 
@@ -79,6 +90,6 @@ def grabRuntimeInfo(err_bound, reduction_factor, model, training_data, x_indices
 
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(algo_max_runtime_secs)
+    pyunit_utils.standalone_test(test_deeplearning_stopping_method)
 else:
-    algo_max_runtime_secs()
+    test_deeplearning_stopping_method()
