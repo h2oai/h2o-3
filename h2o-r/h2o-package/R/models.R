92423b9a0e31fb68bf2adc5140e1197c926ef2ca
wendycwong <wendycwong@yahoo.com>
2018-05-08 09:27:57 -0700

    remove typo.

--- h2o-r/h2o-package/R/models.R
+++ h2o-r/h2o-package/R/models.R
@@ -2772,7 +2772,7 @@ h2o.sdev <- function(object) {
 #' \donttest{
 #' library(h2o)
 #' h2o.init()
-#' df <- as.h2o(iris)frame
+#' df <- as.h2o(iris)
 #' tab <- h2o.tabulate(data = df, x = "Sepal.Length", y = "Petal.Width",
 #'              weights_column = NULL, nbins_x = 10, nbins_y = 10)
 #' plot(tab)              
