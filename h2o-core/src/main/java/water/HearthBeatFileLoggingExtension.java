fe7dc893bb768fba461bb804bc59d0a1aa3254c2
stexyz <pacinda@seznam.cz>
2018-05-09 19:19:36 +0200

    Multithreaded buffered appending to the log file.

--- h2o-core/src/main/java/water/HearthBeatFileLoggingExtension.java
+++ h2o-core/src/main/java/water/HearthBeatFileLoggingExtension.java
@@ -1,9 +1,13 @@
 package water;
 import water.util.Log;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class HearthBeatFileLoggingExtension implements H2OTelemetryExtension {
     //sampling period in seconds
-    private int samplingTimeout = -1;
+    private int samplingTimeout = 10;
+    private BlockingQueue<HeartBeat> heartBeats = new LinkedBlockingQueue<>();
 
     @Override
     public String getName() {
@@ -12,10 +16,38 @@ public class HearthBeatFileLoggingExtension implements H2OTelemetryExtension {
 
     @Override
     public void init() {
+        new TelemetryThread().start();
     }
 
     @Override
     public void report(HeartBeat data) {
-        Log.telemetry(data.toJsonString());
+        try {
+            heartBeats.put(data);
+        } catch (InterruptedException ie) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    private class TelemetryThread extends Thread {
+        public TelemetryThread(){
+            super("Telemetry Thread HB Consumer");
+            setDaemon(true);
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                while (!heartBeats.isEmpty()) {
+                    try {
+                        HeartBeat hb = heartBeats.poll(samplingTimeout, TimeUnit.SECONDS);
+                        Log.telemetry(hb.toJsonString());
+
+                    } catch (InterruptedException ignore) {
+                        Thread.currentThread().interrupt();
+                        return;
+                    }
+                }
+            }
+        }
     }
 }
