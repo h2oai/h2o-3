e466a4aae6afcd47db071ad54976037b9ac101da
Pavel Pscheidl <pavel@h2o.ai>
2018-05-31 12:47:39 +0200

    XGBoost tested on large airlines_all dataset

---
+++ h2o-py/tests/testdir_algos/xgboost/pyunit_airlines_all_multinode_large.py
@@ -0,0 +1,21 @@
+from h2o.estimators.xgboost import *
+from tests import pyunit_utils
+
+
+def multinode():
+    assert H2OXGBoostEstimator.available() is True
+
+    frame = h2o.import_file(path=pyunit_utils.locate("bigdata/server/airlines_all.csv"))
+
+    # Force factor variables, even if recognized correctly
+        # The ntrees parameters in H2O translates to max_depth param
+    h2o_model = H2OXGBoostEstimator(training_frame=frame, learn_rate = 0.7,
+                                booster='gbtree', seed=1, ntrees=5)
+    h2o_model.train(x=['Origin', 'Dest'], y='IsDepDelayed', training_frame=frame)
+
+    h2o_model.summary()
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(multinode)
+else:
+    multinode()
