2930e964911bbfab11bbc81169e624685dcacdc0
Pavel Pscheidl <pavel@h2o.ai>
2018-06-02 10:46:43 +0200

    XGBoost Airlines_all sparse matrix training test

--- h2o-py/tests/testdir_algos/xgboost/pyunit_airlines_all_multinode_large.py
+++ h2o-py/tests/testdir_algos/xgboost/pyunit_airlines_all_multinode_large.py
@@ -1,21 +1,27 @@
+import pandas as pd
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+
 from h2o.estimators.xgboost import *
 from tests import pyunit_utils
 
 
-def multinode():
+def xgboost_airlines_all_multinode_large():
     assert H2OXGBoostEstimator.available() is True
 
     frame = h2o.import_file(path=pyunit_utils.locate("bigdata/server/airlines_all.csv"))
 
-    # Force factor variables, even if recognized correctly
-        # The ntrees parameters in H2O translates to max_depth param
+    # The ntrees parameters in H2O translates to max_depth param
     h2o_model = H2OXGBoostEstimator(training_frame=frame, learn_rate = 0.7,
-                                booster='gbtree', seed=1, ntrees=5)
-    h2o_model.train(x=['Origin', 'Dest'], y='IsDepDelayed', training_frame=frame)
-
-    h2o_model.summary()
+                                    booster='gbtree', seed=1, ntrees=5, dmatrix_type='sparse')
+    # Categorical variables only - sparse dataset
+    h2o_model.train(x=['Year','Month', 'DayofMonth', 'DayOfWeek', 'DepTime', 'CRSDepTime', 'ArrTime',
+                       'CRSArrTime','UniqueCarrier', 'FlightNum', 'TailNum','ActualElapsedTime', 'CRSElapsedTime', 'AirTime',
+                       'ArrDelay', 'DepDelay', 'Distance', 'TaxiIn', 'TaxiOut', 'Cancelled','CancellationCode','Diverted',
+                       'CarrierDelay', 'WeatherDelay', 'NASDelay', 'SecurityDelay', 'LateAircraftDelay', 'IsArrDelayed',
+                       'Origin', 'Dest'], y='IsDepDelayed', training_frame=frame)
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(multinode)
+    pyunit_utils.standalone_test(xgboost_airlines_all_multinode_large)
 else:
-    multinode()
+    xgboost_airlines_all_multinode_large()
