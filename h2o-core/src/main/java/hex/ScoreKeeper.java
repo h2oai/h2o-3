b789073e45b3881d2eb8c3eb72e455a2caf3a620
wendycwong <wendycwong@yahoo.com>
2018-06-05 17:42:20 -0700

    Remove r2 from stopping_metric enum.

--- h2o-core/src/main/java/hex/ScoreKeeper.java
+++ h2o-core/src/main/java/hex/ScoreKeeper.java
@@ -108,7 +108,7 @@ public class ScoreKeeper extends Iced {
     _custom_metric =  m._custom_metric.value;
   }
 
-  public enum StoppingMetric { AUTO, deviance, logloss, MSE, RMSE,MAE,RMSLE, AUC, lift_top_group, misclassification, mean_per_class_error, custom, r2}
+  public enum StoppingMetric { AUTO, deviance, logloss, MSE, RMSE,MAE,RMSLE, AUC, lift_top_group, misclassification, mean_per_class_error, custom} //, r2}
   public static boolean moreIsBetter(StoppingMetric criterion) {
     return (criterion == StoppingMetric.AUC || criterion == StoppingMetric.lift_top_group || criterion == StoppingMetric.r2);
   }
