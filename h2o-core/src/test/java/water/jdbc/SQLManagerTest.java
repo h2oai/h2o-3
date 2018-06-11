117d7e0a28f3ede22bd17846e66c0050ee7a577e
Pavel Pscheidl <pavel@h2o.ai>
2018-06-11 17:20:26 +0200

    Removed direct manipulation of static final with via reflection from SQLManagerTest

--- h2o-core/src/test/java/water/jdbc/SQLManagerTest.java
+++ h2o-core/src/test/java/water/jdbc/SQLManagerTest.java
@@ -8,6 +8,7 @@ import org.junit.contrib.java.lang.system.ProvideSystemProperty;
 import org.junit.contrib.java.lang.system.RestoreSystemProperties;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
 import water.H2O;
 import water.H2ONode;
 import water.Paxos;
@@ -21,37 +22,28 @@ public class SQLManagerTest {
   public final ProvideSystemProperty provideSystemProperty =
       new ProvideSystemProperty(H2O.OptArgs.SYSTEM_PROP_PREFIX + "sql.connections.max", "7");
 
-
   @Rule
   public final RestoreSystemProperties restoreSystemProperties = new RestoreSystemProperties();
 
-  private Field cloudSizeField;
-  private Field currentRuntimeField;
+  private Runtime runtime;
 
   @Before
   public void setUp() throws Exception {
     Paxos._commonKnowledge = true;
-    cloudSizeField = H2O.class.getField("_memary");
-    cloudSizeField.setAccessible(true);
 
     Field modifiersField = Field.class.getDeclaredField("modifiers");
     modifiersField.setAccessible(true);
-    modifiersField.setInt(cloudSizeField, cloudSizeField.getModifiers() & ~Modifier.FINAL);
-
-    currentRuntimeField = Runtime.class.getDeclaredField("currentRuntime");
-    currentRuntimeField.setAccessible(true);
-    modifiersField.setInt(currentRuntimeField, currentRuntimeField.getModifiers() & ~Modifier.FINAL);
-
+    runtime = PowerMockito.spy(Runtime.getRuntime());
+    PowerMockito.field(Runtime.class, "currentRuntime").set(runtime, runtime);
   }
 
   @Test
+  @PrepareForTest(Runtime.class)
   public void testConnectionPoolSize() throws Exception {
     // Cloud size 1
-    cloudSizeField.set(H2O.CLOUD, new H2ONode[1]);
-    Runtime runtime = PowerMockito.spy(Runtime.getRuntime());
-    // This ensures unified behavior of such test across various environments
+    PowerMockito.field(H2O.class, "_memary").set(H2O.CLOUD, new H2ONode[1]);
     Mockito.when(runtime.availableProcessors()).thenReturn(100);
-    currentRuntimeField.set(null, runtime);
+    // This ensures unified behavior of such test across various environments
     // Verify there are truly 100 processors returned available
     Assert.assertEquals(100, Runtime.getRuntime().availableProcessors());
 
@@ -68,11 +60,9 @@ public class SQLManagerTest {
   @Test
   public void testConnectionPoolSizeOneProcessor() throws Exception {
     // Cloud size 1
-    cloudSizeField.set(H2O.CLOUD, new H2ONode[1]);
-    Runtime runtime = PowerMockito.spy(Runtime.getRuntime());
+    PowerMockito.field(H2O.class, "_memary").set(H2O.CLOUD, new H2ONode[1]);
     // This ensures unified behavior of such test across various environments
     Mockito.when(runtime.availableProcessors()).thenReturn(1);
-    currentRuntimeField.set(null, runtime);
     Assert.assertEquals(1, Runtime.getRuntime().availableProcessors());
 
     SQLManager.SqlTableToH2OFrame frame = new SQLManager.SqlTableToH2OFrame("", "", false,
@@ -91,11 +81,9 @@ public class SQLManagerTest {
   @Test
   public void testConnectionPoolSizeZeroProcessors() throws Exception {
     // Cloud size 1
-    cloudSizeField.set(H2O.CLOUD, new H2ONode[1]);
-    Runtime runtime = PowerMockito.spy(Runtime.getRuntime());
+    PowerMockito.field(H2O.class, "_memary").set(H2O.CLOUD, new H2ONode[1]);
     // This ensures unified behavior of such test across various environments
     Mockito.when(runtime.availableProcessors()).thenReturn(-1);
-    currentRuntimeField.set(null, runtime);
     Assert.assertEquals(-1, Runtime.getRuntime().availableProcessors());
 
     SQLManager.SqlTableToH2OFrame frame = new SQLManager.SqlTableToH2OFrame("", "", false,
@@ -108,10 +96,8 @@ public class SQLManagerTest {
 
   @Test
   public void testConnectionPoolSizeTwoNodes() throws Exception {
-    cloudSizeField.set(H2O.CLOUD, new H2ONode[2]);
-    Runtime runtime = PowerMockito.spy(Runtime.getRuntime());
+    PowerMockito.field(H2O.class, "_memary").set(H2O.CLOUD, new H2ONode[2]);
     Mockito.when(runtime.availableProcessors()).thenReturn(10);
-    currentRuntimeField.set(null, runtime);
     Assert.assertEquals(10, Runtime.getRuntime().availableProcessors());
 
     SQLManager.SqlTableToH2OFrame frame = new SQLManager.SqlTableToH2OFrame("", "", false,
