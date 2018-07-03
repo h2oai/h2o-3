c08d6094136a1fb5f6c2ee39d553b43474ba0b1b
Michal Raška <michalr@h2o.ai>
2018-07-03 09:52:39 +0200

    [ITA-316] Add S3 creds to hadoop smoke images. (#2568)

--- scripts/jenkins/groovy/insideDocker.groovy
+++ scripts/jenkins/groovy/insideDocker.groovy
@@ -1,6 +1,5 @@
 def call(customEnv, image, registry, buildConfig, timeoutValue, timeoutUnit, customArgs='', block) {
 
-  def AWS_CREDENTIALS_ID = 'AWS S3 Credentials'
   def DEFAULT_DNS = '172.16.0.200'
 
   if (customArgs == null) {
@@ -34,7 +33,7 @@ def call(customEnv, image, registry, buildConfig, timeoutValue, timeoutUnit, cus
   withEnv(customEnv) {
     timeout(time: timeoutValue, unit: timeoutUnit) {
       docker.withRegistry("https://${registry}") {
-        withCredentials([file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'), [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: AWS_CREDENTIALS_ID, secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+        withCredentials([file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'), [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS S3 Credentials', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
           docker.image(image).inside("--init --dns ${DEFAULT_DNS} -e AWS_ACCESS_KEY_ID=\${AWS_ACCESS_KEY_ID} -e AWS_SECRET_ACCESS_KEY=\${AWS_SECRET_ACCESS_KEY} -v /home/0xdiag:/home/0xdiag ${customArgs}") {
             sh 'id'
             sh 'printenv'
