4a04c206e05114d426508af5e92d0fa46d8d004f
wendycwong <wendycwong@yahoo.com>
2018-07-10 15:39:33 -0700

    PUBDEV-5723: fixed intermittent due to not calling adaptrFr in backend and added R unit test.

--- h2o-r/tests/testdir_javapredict/runit_PUBDEV_5058_PUBDEV_5723_glrm_mojo_large.R
+++ h2o-r/tests/testdir_javapredict/runit_PUBDEV_5058_PUBDEV_5723_glrm_mojo_large.R
@@ -9,13 +9,18 @@ test.Glrm.mojo <-
     # Run the test
     #----------------------------------------------------------------------
     e <- tryCatch({
+      browser()
       numTest = 1000 # set test dataset to contain 1000 rows
       params_prob_data <- setParmsData(numTest) # generate model parameters, random dataset
       modelAndDir<-buildModelSaveMojoGLRM(params_prob_data$params) # build the model and save mojo
       filename = sprintf("%s/in.csv", modelAndDir$dirName) # save the test dataset into a in.csv file.
       h2o.downloadCSV(params_prob_data$tDataset, filename)
+      loadedModel = h2o.loadModel(paste(modelAndDir$dirName, modelAndDir$model@model_id, sep='/'))
+      lpredict = h2o.predict(loadedModel, params_prob_data$tDataset)
       twoFrames<-mojoH2Opredict(modelAndDir$model, modelAndDir$dirName, filename, glrmReconstruct=TRUE) # perform H2O and mojo prediction and return frames
       compareFrames(twoFrames$h2oPredict,twoFrames$mojoPredict, prob=1, tolerance = 1e-6)
+      print("Comparing mojo predict and loaded model predict....")
+      compareFrames(twoFrames$h2oPredict, lpredict, prob=1, tolerance=1e-6)
       twoFrames<-mojoH2Opredict(modelAndDir$model, modelAndDir$dirName, filename)
       xFactorTest <- h2o.getFrame(paste("GLRMLoading", twoFrames$frameId, sep="_"))
       compareFrames(xFactorTest, twoFrames$mojoPredict, prob=1, tolerance =1e-6)
