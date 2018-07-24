af5fb629fedb1b8307c8a182a71f64f5f8f4b327
michalkurka <michalk@h2o.ai>
2018-07-24 09:47:47 -0700

    XGB Fix: Create a new instance of MB for each Map task

--- h2o-extensions/xgboost/src/main/java/ml/dmlc/xgboost4j/java/XGBoostScoreTask.java
+++ h2o-extensions/xgboost/src/main/java/ml/dmlc/xgboost4j/java/XGBoostScoreTask.java
@@ -2,7 +2,6 @@ package ml.dmlc.xgboost4j.java;
 
 import com.google.common.collect.ObjectArrays;
 import hex.*;
-import hex.genmodel.utils.DistributionFamily;
 import hex.tree.xgboost.*;
 import hex.tree.xgboost.XGBoost;
 import water.*;
@@ -23,12 +22,12 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
     private final XGBoostOutput _output;
     private final XGBoostModel.XGBoostParameters _parms;
     private final BoosterParms _boosterParms;
-    private final ModelMetrics.MetricBuilder _metricBuilder;
     private final boolean _computeMetrics;
     private final int _weightsChunkId;
     private final Model _model;
+    private final double _threshold;
 
-
+    private ModelMetrics.MetricBuilder _metricBuilder;
     private byte[] rawBooster;
 
     public static class XGBoostScoreTaskResult {
@@ -55,13 +54,11 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
                 data.find(parms._weights_column),
                 m).doAll(outputTypes(output), data);
 
-        String[] names = ObjectArrays.concat(Model.makeScoringNames(output), new String[] {"label"}, String.class);
-        Frame preds = task.outputFrame(destinationKey, names, makeDomains(output, names));
+        final String[] names = Model.makeScoringNames(output);
+        final Frame preds = task.outputFrame(destinationKey, names, makeDomains(output, names));
 
         XGBoostScoreTaskResult res = new XGBoostScoreTaskResult();
 
-        Vec resp = preds.lastVec();
-        preds.remove(preds.vecs().length - 1);
         if (output.nclasses() == 1) {
             Vec pred = preds.vec(0);
             if (computeMetrics) {
@@ -70,12 +67,10 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
         } else if (output.nclasses() == 2) {
             Vec p1 = preds.vec(2);
             if (computeMetrics) {
-                resp.setDomain(output.classNames());
                 res.mm = task._metricBuilder.makeModelMetrics(m, originalData, data, new Frame(p1));
             }
         } else {
             if (computeMetrics) {
-                resp.setDomain(output.classNames());
                 Frame pp = new Frame(preds);
                 pp.remove(0);
                 Scope.enter();
@@ -93,11 +88,11 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
     private static byte[] outputTypes(XGBoostOutput output) {
         // Last output is the response, which eventually will be removed before returning the preds Frame but is needed to build metrics
         if(output.nclasses() == 1) {
-            return new byte[]{T_NUM, T_NUM};
+            return new byte[]{T_NUM};
         } else if(output.nclasses() == 2) {
-            return new byte[]{T_CAT, T_NUM, T_NUM, T_NUM};
+            return new byte[]{T_CAT, T_NUM, T_NUM};
         } else{
-            byte[] types = new byte[output.nclasses() + 2];
+            byte[] types = new byte[output.nclasses() + 1];
             Arrays.fill(types, T_NUM);
             return types;
         }
@@ -107,9 +102,8 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
         if(output.nclasses() == 1) {
             return null;
         } else if(output.nclasses() == 2) {
-            String[][] domains = new String[4][];
+            String[][] domains = new String[3][];
             domains[0] = new String[]{"N", "Y"};
-            domains[3] = new String[]{"N", "Y"};
             return domains;
         } else{
             String[][] domains = new String[names.length][];
@@ -132,9 +126,9 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
         _boosterParms = boosterParms;
         this.rawBooster = XGBoost.getRawArray(booster);
         _computeMetrics = computeMetrics;
-        _metricBuilder = _computeMetrics ? createMetricsBuilder(_output.nclasses(), _output.classNames()) : null;
         _weightsChunkId = weightsChunkId;
         _model = model;
+        _threshold = Model.defaultThreshold(_output);
     }
 
     /**
@@ -160,6 +154,7 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
     public void map(Chunk[] cs, NewChunk[] ncs) {
         DMatrix data = null;
         Booster booster = null;
+        _metricBuilder = _computeMetrics ? createMetricsBuilder(_output.nclasses(), _output.classNames()) : null;
         try {
             Map<String, String> rabitEnv = new HashMap<>();
             // Rabit has to be initialized as parts of booster.predict() are using Rabit
@@ -202,43 +197,38 @@ public class XGBoostScoreTask extends MRTask<XGBoostScoreTask> {
                 }
                 for (int i = 0; i < cs[0]._len; ++i) {
                     ncs[0].addNum(dpreds[i]);
-                    ncs[1].addNum(labels[i]);
                 }
             } else if (_output.nclasses() == 2) {
-                double[] dpreds = new double[preds.length];
-                for (int j = 0; j < dpreds.length; ++j) {
-                    dpreds[j] = preds[j][0];
-                }
-
+                double[] row = new double[3];
+                float[] yact = new float[1];
                 for (int i = 0; i < cs[0]._len; ++i) {
-                    final double p = dpreds[i];
-                    double[] row = new double[]{0, 1.0D - p, p};
-                    double predLab = hex.genmodel.GenModel.getPrediction(row, _output._priorClassDist, null, Model.defaultThreshold(_output));
+                    final double p = preds[i][0];
+                    row[1] = 1 - p;
+                    row[2] = p;
+                    row[0] = hex.genmodel.GenModel.getPrediction(row, _output._priorClassDist, null, _threshold);
 
-                    ncs[0].addNum(predLab);
+                    ncs[0].addNum(row[0]);
                     ncs[1].addNum(row[1]);
-                    ncs[2].addNum(p);
-                    ncs[3].addNum(labels[i]);
+                    ncs[2].addNum(row[2]);
 
                     if (_computeMetrics) {
-                        double[] metricPreds = new double[]{predLab, p, row[1]};
                         double weight = _weightsChunkId != -1 ? cs[_weightsChunkId].atd(i) : 1; // If there is no chunk with weights, the weight is considered to be 1
-                        _metricBuilder.perRow(metricPreds, new float[]{labels[i]}, weight, 0, _model);
+                        yact[0] = labels[i];
+                        _metricBuilder.perRow(row, yact, weight, 0, _model);
                     }
                 }
             } else {
                 for (int i = 0; i < cs[0]._len; ++i) {
-                    double[] row = new double[ncs.length - 1];
+                    double[] row = new double[ncs.length];
                     for (int j = 1; j < row.length; ++j) {
                         double val = preds[i][j - 1];
                         ncs[j].addNum(val);
                         row[j] = val;
                     }
                     ncs[0].addNum(hex.genmodel.GenModel.getPrediction(row, _output._priorClassDist, null, Model.defaultThreshold(_output)));
-                    ncs[ncs.length - 1].addNum(labels[i]);
                     if (_computeMetrics) {
                         double weight = _weightsChunkId != -1 ? cs[_weightsChunkId].atd(i) : 1; // If there is no chunk with weights, the weight is considered to be 1
-                        if (_computeMetrics) _metricBuilder.perRow(row, new float[]{labels[i]}, weight, 0, _model);
+                        _metricBuilder.perRow(row, new float[]{labels[i]}, weight, 0, _model);
                     }
                 }
             }
