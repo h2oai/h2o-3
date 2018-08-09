3a6d41cc3a6133eadd58e761f826f955fd743149
Pavel Pscheidl <pavel.junior@pscheidl.cz>
2018-08-08 18:35:14 -0700

    Tree class and tree number index description changed to describe the numbering better

--- h2o-algos/src/main/java/hex/schemas/TreeV3.java
+++ h2o-algos/src/main/java/hex/schemas/TreeV3.java
@@ -9,10 +9,10 @@ public class TreeV3 extends SchemaV3<Iced, TreeV3> {
     @API(required = true, direction = API.Direction.INPUT, help = "Key of the model the desired tree belongs to",level = API.Level.critical)
     public KeyV3.ModelKeyV3 model;
 
-    @API(required = true, direction = API.Direction.INPUT, help = "Number of the tree in the model", level = API.Level.critical)
+    @API(required = true, direction = API.Direction.INPUT, help = "Index of the tree in the model.", level = API.Level.critical)
     public int tree_number;
 
-    @API(direction = API.Direction.INPUT, help = "Tree class (if applicable)", level = API.Level.critical)
+    @API(direction = API.Direction.INPUT, help = "Tree class index. Equals to 1 for regression/binomial. For multinomial, ranges from 0 to response column cardinality - 1.", level = API.Level.critical)
     public int tree_class;
 
     @API(direction = API.Direction.OUTPUT, help = "Left child nodes in the tree")
