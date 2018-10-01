5900c278a6888c875db1ff19700b4618f8b2fcf8
wendycwong <wendycwong@yahoo.com>
2018-10-01 09:41:38 -0700

    Added pyunit.

--- h2o-py/tests/testdir_algos/glrm/pyunit_PUBDEV_5944_svd_bug.py
+++ h2o-py/tests/testdir_algos/glrm/pyunit_PUBDEV_5944_svd_bug.py
@@ -10,17 +10,14 @@ from h2o.estimators.glrm import H2OGeneralizedLowRankEstimator
 def glrm_svd_init_bug():
     trainData = h2o.import_file(pyunit_utils.locate("smalldata/airlines/AirlinesTrain.csv.zip"))
     testData = h2o.import_file(pyunit_utils.locate("smalldata/airlines/AirlinesTest.csv.zip"))
-    glrmModel = H2OGeneralizedLowRankEstimator(k=4, init="svd", svd_method="gram_s_v_d", recover_svd=True)
+    glrmModel = H2OGeneralizedLowRankEstimator(k=4, loss="huber", init="svd", recover_svd=True)
     glrmModel.train(x=trainData.names, training_frame=trainData)
-    predV = glrmModel.predict(testData)
+    #predV = glrmModel.predict(testData)
 
-    glrmModel2 = H2OGeneralizedLowRankEstimator(k=4, init="svd", svd_method="power", recover_svd=True)
-    glrmModel2.train(x=trainData.names, training_frame=trainData)
-    predV2 = glrmModel2.predict(testData)
-
-    glrmModel3 = H2OGeneralizedLowRankEstimator(k=4, init="svd", svd_method="randomized", recover_svd=True)
-    glrmModel3.train(x=trainData.names, training_frame=trainData)
-    predV3 = glrmModel3.predict(testData)
+    y = glrmModel.archetypes()
+    print(y)
+    x = h2o.get_frame(glrmModel._model_json["output"]["representation_name"])
+    print(x)
 
 
 if __name__ == "__main__":
