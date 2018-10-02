bef1932016e0436a6b55a7e53bfb3749150dabce
Michal Raška <michalr@h2o.ai>
2018-10-02 12:12:45 -0700

    [ITA-344] Build Python package under h2o-py/build/. (#2881)
    Build the Python package inside `h2o-py/build` directory

--- scripts/install-clients.sh
+++ scripts/install-clients.sh
@@ -4,6 +4,6 @@
 # run this script to install the new R and Python clients.
 
 pip uninstall -y h2o
-pip install ../h2o-py/dist/h2o-*-py2.py3-none-any.whl
+pip install ../h2o-py/build/dist/h2o-*-py2.py3-none-any.whl
 R CMD REMOVE h2o
 R CMD INSTALL ../h2o-r/R/src/contrib/h2o*.tar.gz
