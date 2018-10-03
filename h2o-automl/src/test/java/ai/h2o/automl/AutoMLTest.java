02790fc2167001518ef62c814aa2af077adab0d7
Andrey Spiridonov <andrey@h2o.ai>
2018-10-03 14:15:29 +0200

    Fixing keys leakage in KeepCrossValidationFoldAssignment test

--- h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/AutoMLTest.java
@@ -1,9 +1,11 @@
 package ai.h2o.automl;
 
 import org.junit.Assert;
+import hex.Model;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import water.DKV;
 import water.Key;
 import water.fvec.Frame;
 
@@ -141,24 +143,30 @@ public class AutoMLTest extends water.TestUtil {
 
   @Ignore //reenable in PUBDEV-5956
   @Test public void KeepCrossValidationFoldAssignmentTest() {
-    AutoML aml=null;
-    Frame fr=null;
+    AutoML aml = null;
+    Frame fr = null;
+    Model leader = null;
     try {
       AutoMLBuildSpec autoMLBuildSpec = new AutoMLBuildSpec();
       fr = parse_test_file("./smalldata/logreg/prostate_train.csv");
       autoMLBuildSpec.input_spec.training_frame = fr._key;
       autoMLBuildSpec.input_spec.response_column = "CAPSULE";
-      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(5);
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_models(1);
+      autoMLBuildSpec.build_control.stopping_criteria.set_max_runtime_secs(30);
       autoMLBuildSpec.build_control.keep_cross_validation_fold_assignment = true;
 
       aml = AutoML.makeAutoML(Key.<AutoML>make(), new Date(), autoMLBuildSpec);
       AutoML.startAutoML(aml);
       aml.get();
 
-      assertTrue(aml.leader() !=null && aml.leader()._parms._keep_cross_validation_fold_assignment);
-      assertTrue(aml.leader() !=null && aml.leader()._output._cross_validation_fold_assignment_frame_id != null);
+      leader = aml.leader();
+
+      assertTrue(leader !=null && leader._parms._keep_cross_validation_fold_assignment);
+      assertTrue(leader !=null && leader._output._cross_validation_fold_assignment_frame_id != null);
+
     } finally {
-      // cleanup
+      // Since user asked to keep cv fold assignments( we set parameter `keep_cross_validation_fold_assignment` to true) we need to remove this key manually
+      DKV.remove(leader._output._cross_validation_fold_assignment_frame_id);
       if(aml!=null) aml.deleteWithChildren();
       if(fr != null) fr.delete();
     }
