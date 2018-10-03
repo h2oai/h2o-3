a0ce2caf01d413f803f078b5da5e5b6f6815bb90
wendycwong <wendycwong@yahoo.com>
2018-10-03 15:39:10 -0700

    PUBDEV-5856 GLM multinomial COD implementation check (#2826)
    * PUBDEV-5856: Fixed multinomial dataset generation.
    PUBDEV-5856: Removed redundant gram.getXX() action.
    PUBDEV-5856: separate fitCOD for multinomial from fitIRLSM.
    PUBDEV-5856: incorporate michalk code review comments.
    
    * Added correct class when invoking GLMMultinomialGradientTask.

--- h2o-algos/src/test/java/hex/glm/GLMBasicTestMultinomial.java
+++ h2o-algos/src/test/java/hex/glm/GLMBasicTestMultinomial.java
@@ -1,6 +1,7 @@
 package hex.glm;
 
 import hex.CreateFrame;
+import hex.DataInfo;
 import hex.FrameSplitter;
 import hex.ModelMetricsBinomialGLM.ModelMetricsMultinomialGLM;
 import hex.SplitFrame;
@@ -8,6 +9,7 @@ import hex.deeplearning.DeepLearningModel;
 import hex.glm.GLMModel.GLMParameters;
 import hex.glm.GLMModel.GLMParameters.Family;
 import hex.glm.GLMModel.GLMParameters.Solver;
+import hex.optimization.ADMM;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 import water.*;
@@ -206,6 +208,131 @@ public class GLMBasicTestMultinomial extends TestUtil {
       if(preds != null)preds.delete();
     }
   }
+
+  /*
+  I have manually derived the coefficient updates for COD and they are more accurate than what is currently
+  implemented because I update all the probabilities after a coefficient has been changed.  In reality, this will
+  be very slow and an approximation may be more appropriate.  The coefficients generated here is the golden standard.
+   */
+  @Test
+  public void testCODGradients(){
+    Scope.enter();
+    Frame train;
+    GLMParameters params = new GLMParameters(Family.multinomial);
+    GLMModel model = null;
+    double[] oldGLMCoeffs = new double[] {0.059094274726151426, 0.013361781886804975, -0.00798977427248744,
+            0.007467359562151555, 0.06737827548293934, -1.002393430927568, -0.04066511294457045, -0.018960901996125427,
+            0.07330281133353159, -0.02285669809606731, 0.002805290931441751, -1.1394632268347782, 0.021976767313534512,
+            0.01013967640490087, -0.03999288928633559, 0.012385348397898913, -0.0017922461738315199,
+            -1.159667420372168};
+    try {
+      train = parse_test_file("smalldata/glm_test/multinomial_3_class.csv");
+      Scope.track(train);
+      params._response_column = "response";
+      params._train = train._key;
+      params._lambda = new double[]{4.881e-05};
+      params._alpha = new double[]{0.5};
+      params._objective_epsilon = 1e-6;
+      params._beta_epsilon = 1e-4;
+      params._max_iterations = 1; // one iteration
+      params._seed = 12345; // don't think this one matters but set it anyway
+      Solver s = Solver.COORDINATE_DESCENT;
+      System.out.println("solver = " + s);
+      params._solver = s;
+      model = new GLM(params).trainModel().get();
+      Scope.track_generic(model);
+      DataInfo tinfo = new DataInfo(train.clone(), null, 0, true, DataInfo.TransformType.STANDARDIZE,
+              DataInfo.TransformType.NONE, false, false, false,
+              /* weights */ false, /* offset */ false, /* fold */ false);
+      double[] manualCoeff = getCODCoeff(train, params._alpha[0], params._lambda[0], model._ymu, tinfo);
+      Scope.track_generic(tinfo);
+
+      compareGLMCoeffs(manualCoeff, model._output._submodels[0].beta, 2e-2);  // compare two sets of coeffs
+      compareGLMCoeffs(model._output._submodels[0].beta, oldGLMCoeffs, 1e-10);  // compare to original GLM
+
+    } finally{
+      Scope.exit();
+    }
+  }
+
+  public void compareGLMCoeffs(double[] coeff1, double[] coeff2, double tol) {
+
+    assertTrue(coeff1.length==coeff2.length); // assert coefficients having the same length first
+    for (int index=0; index < coeff1.length; index++) {
+      assert Math.abs(coeff1[index]-coeff2[index]) < tol :
+              "coefficient difference "+Math.abs(coeff1[index]-coeff2[index])+" exceeded tolerance of "+tol;
+    }
+  }
+
+  public double[] getCODCoeff(Frame train, double alpha, double lambda, double[] ymu, DataInfo tinfo) {
+    int numClass = train.vec("response").domain().length;
+    int numPred = train.numCols() - 1;
+    int numRow = (int) train.numRows();
+    double[] beta = new double[numClass * (numPred + 1)];
+    double reg = 1.0/train.numRows();
+
+    // initialize beta
+    for (int index = 0; index < numClass; index++) {
+      beta[(index + 1) * (numPred + 1) - 1] = Math.log(ymu[index]);
+    }
+
+    for (int iter = 0; iter < 3; iter++) {
+      // update beta
+      for (int cindex = 0; cindex < numClass; cindex++) {
+        for (int pindex = 0; pindex < numPred; pindex++) {
+          double grad = 0;
+          double hess = 0;
+
+          for (int rindex = 0; rindex < numRow; rindex++) {
+            int resp = (int) train.vec("response").at(rindex);
+            double predProb = calProb(train, rindex, beta, numClass, numPred, cindex, tinfo);
+            double entry = (train.vec(pindex).at(rindex) - tinfo._numMeans[pindex]) * tinfo._normMul[pindex];
+            grad -= entry * ((resp == cindex ? 1 : 0) - predProb);
+            hess += entry * entry * (predProb - predProb * predProb); // hess calculation is correct
+          }
+          grad = grad * reg + lambda * (1 - alpha) * beta[cindex * (numPred + 1) + pindex]; // add l2 penalty
+          hess = hess * reg + lambda * (1 - alpha);
+          beta[cindex * (numPred + 1) + pindex] -= ADMM.shrinkage(grad, lambda * alpha) / hess;
+        }
+
+        double grad = 0;
+        double hess = 0;
+        // change the intercept term here
+        for (int rindex = 0; rindex < numRow; rindex++) {
+          int resp = (int) train.vec("response").at(rindex);
+
+
+          double predProb = calProb(train, rindex, beta, numClass, numPred, cindex, tinfo);
+          grad -= ((resp == cindex ? 1 : 0) - predProb);
+          hess += (predProb - predProb * predProb);
+
+        }
+        grad *= reg;
+        hess *= reg;
+        beta[(cindex + 1) * (numPred + 1) - 1] -= grad / hess;
+      }
+    }
+    return beta;
+  }
+
+  public double calProb(Frame train, int rowIndex, double[] beta, int numClass, int numPred, int classNo, DataInfo tinfo) {
+    double prob = 0.0;
+    double sum = 0.0;
+    for (int cindex = 0; cindex < numClass; cindex++) {
+      double temp = 0;
+      for (int pindex = 0; pindex < numPred; pindex++) {
+        double entry = (train.vec(pindex).at(rowIndex)-tinfo._numMeans[pindex])*tinfo._normMul[pindex];
+        temp += entry*beta[cindex*(numPred+1)+pindex];
+      }
+      temp+= beta[(cindex+1)*(numPred+1)-1];
+      if (classNo == cindex) {
+        prob = Math.exp(temp);
+      }
+      sum+= Math.exp(temp);
+    }
+    return (prob/sum);
+  }
+
 
   @Test
   public void testCovtypeMinActivePredictors(){
