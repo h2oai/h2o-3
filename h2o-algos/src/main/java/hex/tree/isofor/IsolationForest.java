6ec549fa6c7699006dfa73fd3438314241af2450
michalkurka <michalk@h2o.ai>
2018-10-12 09:49:12 -0700

    wip

--- h2o-algos/src/main/java/hex/tree/isofor/IsolationForest.java
+++ h2o-algos/src/main/java/hex/tree/isofor/IsolationForest.java
@@ -11,6 +11,9 @@ import water.Key;
 import water.MRTask;
 import water.fvec.Chunk;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import static water.util.RandomUtils.getRNG;
@@ -81,6 +84,33 @@ public class IsolationForest extends SharedTree<IsolationForestModel, IsolationF
   @Override
   protected DTree.DecidedNode makeDecided(DTree.UndecidedNode udn, DHistogram hs[]) {
     return new IFDecidedNode(udn, hs);
+  }
+
+  private class IFDecidedNode extends DTree.DecidedNode {
+
+    private IFDecidedNode(DTree.UndecidedNode n, DHistogram[] hs) {
+      super(n, hs);
+    }
+
+    @Override
+    public DTree.Split bestCol(DTree.UndecidedNode u, DHistogram hs[]) {
+      if( hs == null ) return null;
+      final int maxCols = u._scoreCols == null /* all cols */ ? hs.length : u._scoreCols.length;
+      List<FindSplits> findSplits = new ArrayList<>();
+      for (int i=0; i<maxCols; i++) {
+        int col = u._scoreCols == null ? i : u._scoreCols[i];
+        if( hs[col]==null || hs[col].nbins() <= 1 ) continue;
+        findSplits.add(new FindSplits(hs, col, u));
+      }
+      Collections.shuffle(findSplits, _rand);
+      for (FindSplits fs : findSplits) {
+        DTree.Split s = fs.computeSplit();
+        if (s != null) {
+          return s;
+        }
+      }
+      return null;
+    }
   }
 
   // ----------------------
---
+++ h2o-r/tests/testdir_algos/isofor/runit_isofor_accuracy.R
@@ -0,0 +1,33 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+
+
+test.IsolationForest.accuracy <- function() {
+    set.seed(1234)
+    N = 1e6
+    random_data <- data.frame(
+        x = c(rnorm(N, 0, 0.5), rnorm(N*0.05, -2, 1)),
+        y = c(rnorm(N, 0, 0.5), rnorm(N*0.05,  2, 1)),
+        outlier = c(rep("NO", N), rep("YES", (0.05*N)))
+    )
+    random_data.hex <- as.h2o(random_data)
+
+    h2o_isolation_forest <- h2o.isolationForest(x = c("x", "y"), training_frame = random_data.hex[, c("x", "y")],
+                                                ntrees = 100, seed = 1234, max_depth = 16,
+                                                min_rows = ceiling(nrow(random_data.hex) * 0.01))
+
+    anomaly_score <- h2o.predict(h2o_isolation_forest, random_data.hex)
+
+    glm_train <- anomaly_score
+    glm_train$outlier <- random_data.hex$outlier
+
+    glm_model <- h2o.glm(training_frame = glm_train, x = "predict", y = "outlier", family = "binomial")
+
+    # we should get a good AUC
+    expect_equal(h2o.auc(glm_model), 0.9738122, tolerance = .02, scale = 1)
+    # rate of outlier misclassification should be low
+    expect_equal(h2o.confusionMatrix(glm_model)["YES", "Error"], 0.341120, tolerance = .05, scale = 1)
+}
+
+doTest("IsolationForest: Test Accuracy", test.IsolationForest.accuracy)
