f8be16af920b7c20d87bf416d0b4726fab40bd11
michalkurka <michalk@h2o.ai>
2018-10-14 16:53:58 -0700

    wip

--- h2o-py/tests/testdir_algos/isofor/pyunit_isofor_smoke.py
+++ h2o-py/tests/testdir_algos/isofor/pyunit_isofor_smoke.py
@@ -12,11 +12,15 @@ def isolation_forest():
     train = h2o.import_file(pyunit_utils.locate("smalldata/anomaly/ecg_discord_train.csv"))
     test = h2o.import_file(pyunit_utils.locate("smalldata/anomaly/ecg_discord_test.csv"))
 
-    if_model = H2OIsolationforestEstimator(ntrees=7)
+    if_model = H2OIsolationforestEstimator(ntrees=7, seed=12)
     if_model.train(training_frame=train)
 
-    test_pred = if_model.predict(test)
+    perf = if_model.model_performance()
+    print(perf)
+    assert perf.mean_score() > 0
+    assert perf.mean_normalized_score() > 0
 
+    test_pred = if_model.predict(test)
     assert test_pred.nrow == 23
 
 if __name__ == "__main__":
