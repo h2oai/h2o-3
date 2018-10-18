0f237f08fb0adb525df8b4684e25e309c8f0caa6
Petr Kozelka <pkozelka@gmail.com>
2018-10-18 18:45:14 +0200

    PUBDEV-5820: module h2o-ext-authsupport reintroduced as modules h2o-jetty-8 and h2o-jetty-9; dependencies on these are explicit from every single assembly

---
+++ h2o-jetty-8/src/main/java/ai/h2o/org/eclipse/jetty/plus/jaas/spi/LdapLoginModule.java
@@ -0,0 +1,8 @@
+package ai.h2o.org.eclipse.jetty.plus.jaas.spi;
+
+/**
+ * LdapLoginModule is relocated in Sparkling Water to package ai.h2o.org.eclipse.jetty.plus.jaas.spi
+ * This class lets user define login module that will work both for H2O and SW
+ * (user needs to put "ai.h2o.org.eclipse.jetty.plus.jaas.spi.LdapLoginModule required" in the login conf)
+ */
+public class LdapLoginModule extends org.eclipse.jetty.plus.jaas.spi.LdapLoginModule { /* empty */ }
