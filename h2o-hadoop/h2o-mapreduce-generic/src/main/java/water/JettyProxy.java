1d62c6d7e024facc38f6a53eecae8d4d80e73c8e
Petr Kozelka <pkozelka@gmail.com>
2018-10-19 19:21:42 +0200

    PUBDEV-5820: extracted class Credentials out of JettyProxy - it will be shared among multiple versions

--- h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/JettyProxy.java
+++ h2o-hadoop/h2o-mapreduce-generic/src/main/java/water/JettyProxy.java
@@ -8,9 +8,7 @@ import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.handler.HandlerWrapper;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.B64Code;
-import org.eclipse.jetty.util.security.Credential;
-import water.network.SecurityUtils;
+import water.server.Credentials;
 import water.server.jetty.AbstractHTTPD;
 
 import javax.servlet.ServletConfig;
@@ -75,41 +73,6 @@ public class JettyProxy extends AbstractHTTPD {
     protected void addProxyHeaders(HttpServletRequest clientRequest, org.eclipse.jetty.client.api.Request proxyRequest) {
       proxyRequest.header("Authorization", _basicAuth);
       super.addProxyHeaders(clientRequest, proxyRequest);
-    }
-  }
-
-  /**
-   * Representation of the User-Password pair
-   */
-  public static class Credentials {
-    private static final int GEN_PASSWORD_LENGTH = 16;
-
-    private final String _user;
-    private final String _password;
-
-    private Credentials(String _user, String _password) {
-      this._user = _user;
-      this._password = _password;
-    }
-
-    public String toBasicAuth() {
-      return "Basic " + B64Code.encode(_user + ":" + _password);
-    }
-
-    public String toHashFileEntry() {
-      return _user + ": " + Credential.MD5.digest(_password) + "\n";
-    }
-
-    public String toDebugString() {
-      return "Credentials[_user='" + _user + "', _password='" + _password + "']";
-    }
-
-    public static Credentials make(String user, String password) {
-      return new Credentials(user, password);
-    }
-
-    public static Credentials make(String user) {
-      return make(user, SecurityUtils.passwordGenerator(GEN_PASSWORD_LENGTH));
     }
   }
 
