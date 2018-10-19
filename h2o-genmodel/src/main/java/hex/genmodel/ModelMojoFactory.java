e78ababc3709191757af6fc3895e171c08d2d6c8
michalkurka <michalk@h2o.ai>
2018-10-19 13:24:55 -0700

    PUBDEV-5775: Combine two models into one mojo

--- h2o-genmodel/src/main/java/hex/genmodel/ModelMojoFactory.java
+++ h2o-genmodel/src/main/java/hex/genmodel/ModelMojoFactory.java
@@ -7,6 +7,7 @@ import hex.genmodel.algos.gbm.GbmMojoReader;
 import hex.genmodel.algos.glm.GlmMojoReader;
 import hex.genmodel.algos.glrm.GlrmMojoReader;
 import hex.genmodel.algos.kmeans.KMeansMojoReader;
+import hex.genmodel.algos.pipeline.MojoPipelineReader;
 import hex.genmodel.algos.svm.SvmMojoReader;
 import hex.genmodel.algos.word2vec.Word2VecMojoReader;
 import hex.genmodel.algos.ensemble.StackedEnsembleMojoReader;
@@ -83,6 +84,9 @@ public class ModelMojoFactory {
       case "StackedEnsemble":
       case "Stacked Ensemble":
         return new StackedEnsembleMojoReader();
+
+      case "MOJO Pipeline":
+        return new MojoPipelineReader();
 
       default:
         // Try to load MOJO reader via service
