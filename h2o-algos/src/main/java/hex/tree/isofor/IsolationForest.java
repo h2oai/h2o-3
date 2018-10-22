f6ad1ec69e83c677d14ebbba3380313d92170631
michalkurka <michalk@h2o.ai>
2018-10-22 11:19:13 -0700

    [HOTFIX] DTree constructor changed, fixing the usage in Isolation Forest

--- h2o-algos/src/main/java/hex/tree/isofor/IsolationForest.java
+++ h2o-algos/src/main/java/hex/tree/isofor/IsolationForest.java
@@ -157,7 +157,7 @@ public class IsolationForest extends SharedTree<IsolationForestModel, IsolationF
       randomResp(_parms._seed, _model._output._ntrees);
 
       final long rseed = _rand.nextLong();
-      final DTree tree = new DTree(_train, _ncols, (char)_nclass, _mtry, _mtry_per_tree, rseed, _parms);
+      final DTree tree = new DTree(_train, _ncols, _mtry, _mtry_per_tree, rseed, _parms);
       final DTree[] ktrees = {tree};
 
       new Sample(tree, _parms._sample_rate, null)
