d7796d06d4c3a44eb38275858282862978c75d65
Pavel Pscheidl <pavel.junior@pscheidl.cz>
2018-10-24 11:20:37 +0200

    PUBDEV-5955 StackedEnsemble Mojo categorical index out of range & PUBDEV-6004 - copying of scored data

--- h2o-genmodel/src/main/java/hex/genmodel/algos/ensemble/StackedEnsembleMojoReader.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/ensemble/StackedEnsembleMojoReader.java
@@ -17,11 +17,51 @@ public class StackedEnsembleMojoReader extends MultiModelMojoReader<StackedEnsem
         int baseModelNum = readkv("base_models_num", 0);
         _model._baseModelNum = baseModelNum;
         _model._metaLearner = getModel((String) readkv("metalearner"));
-        _model._baseModels = new MojoModel[baseModelNum];
+        _model._baseModels = new StackedEnsembleMojoModel.StackedEnsembleMojoSubModel[baseModelNum];
+        final String[] columnNames = readkv("[columns]");
         for (int i = 0; i < baseModelNum; i++) {
             String modelKey = readkv("base_model" + i);
-            _model._baseModels[i] = getModel(modelKey);
+            final MojoModel model = getModel(modelKey);
+            _model._baseModels[i] = new StackedEnsembleMojoModel.StackedEnsembleMojoSubModel(model,
+                    createMapping(model, columnNames, modelKey));
         }
+    }
+
+    /**
+     * Creates an array of integers with mapping of referential column name space into model-specific column name space.
+     *
+     * @param model     Model to create column mapping for
+     * @param reference Column mapping servig as a reference
+     * @param modelName Name of the model for various error reports
+     * @return An array of integers with mapping. Null of no mapping is necessary.
+     */
+    private static int[] createMapping(final MojoModel model, final String[] reference, final String modelName) {
+        int[] mapping = new int[reference.length];
+        if (model._names.length != reference.length) {
+            throw new IllegalStateException(String.format("Model '%s' is expected to have has non-standard number of columns.",
+                    modelName));
+        }
+        boolean foundDifference = false;
+        for (int i = 0; i < reference.length; i++) {
+            final int pos = findColumnIndex(model._names, reference[i]);
+            if (pos == -1) {
+                throw new IllegalStateException(String.format("Model '%s' does not have input column '%s'",
+                        modelName, reference[i]));
+            }
+            if (pos != i) foundDifference = true;
+            mapping[i] = pos;
+        }
+
+        if (foundDifference) {
+            return mapping;
+        } else return null;
+    }
+
+    private static int findColumnIndex(String[] arr, String searchedColname) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(searchedColname)) return i;
+        }
+        return -1;
     }
 
     @Override
