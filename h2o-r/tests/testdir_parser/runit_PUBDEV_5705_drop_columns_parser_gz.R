711f9d2bb92b5e0c773e8a1eaf359cf12affbe3a
wendycwong <wendycwong@yahoo.com>
2018-11-05 13:24:46 -0800

    Reduce R unit test time to avoid timeout.

--- h2o-r/tests/testdir_parser/runit_PUBDEV_5705_drop_columns_parser_gz.R
+++ h2o-r/tests/testdir_parser/runit_PUBDEV_5705_drop_columns_parser_gz.R
@@ -15,7 +15,7 @@ test.parseSkippedColumnsgz<- function() {
   set.seed <- 12345
   onePermute <- sample(h2o.ncol(f1))
   skipall <- onePermute
-  skip90Per <- onePermute[1:floor(h2o.ncol(f1) * 0.9)]
+  skip99Per <- onePermute[1:floor(h2o.ncol(f1) * 0.99)]
 
   # test skipall for h2o.importFile
   e <-
@@ -35,9 +35,9 @@ test.parseSkippedColumnsgz<- function() {
   print(e2)
 
   # skip 90% of the columns randomly
-  print("Testing skipping 90% of columns")
-  assertCorrectSkipColumns(fileName, fullFrameR, skip90Per, TRUE, h2o.getTypes(f1)) # test importFile
-  assertCorrectSkipColumns(fileName, fullFrameR, skip90Per, FALSE, h2o.getTypes(f1)) # test uploadFile
+  print("Testing skipping 99% of columns")
+  assertCorrectSkipColumns(fileName, fullFrameR, skip99Per, TRUE, h2o.getTypes(f1)) # test importFile
+  assertCorrectSkipColumns(fileName, fullFrameR, skip99Per, FALSE, h2o.getTypes(f1)) # test uploadFile
 }
 
 doTest("Test gz parser with skipped columns", test.parseSkippedColumnsgz)
