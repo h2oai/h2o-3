2d4dfdb5db608d081aec98fb15a084e61ff75091
Pavel Pscheidl <pavel.junior@pscheidl.cz>
2018-11-08 16:04:49 +0100

    Change test name to pyunit_pubdev_5352

--- h2o-py/tests/testdir_jira/pyunit_pubdev_5352.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_5352.py
@@ -2,8 +2,7 @@ import h2o
 from h2o.estimators.random_forest import H2ORandomForestEstimator
 from tests import pyunit_utils
 
-def pubdev_5179():
-    h2o.init()
+def pubdev_5352():
 
     mnist_original = h2o.import_file(pyunit_utils.locate("smalldata/flow_examples/mnist/test.csv.gz"));
     mnist_original['C785'] = mnist_original['C785'].asfactor()
@@ -12,11 +11,11 @@ def pubdev_5179():
     train, valid, new_data = mnist_original.split_frame(ratios=[.5, .2], seed=1234)
     drf = H2ORandomForestEstimator(model_id='drf', ntrees=3, seed=1234)
     drf.train(x=predictors, y=target, training_frame=train, validation_frame=valid)
-    drf_checkpoint = H2ORandomForestEstimator(model_id='drf_checkpoint',checkpoint=drf, ntrees=10, seed=1234)
+    drf_checkpoint = H2ORandomForestEstimator(model_id='drf_checkpoint',checkpoint=drf, ntrees=4, seed=1234)
     drf_checkpoint.train(x=predictors, y=target, training_frame=new_data, validation_frame=valid)
 
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(pubdev_5179)
+    pyunit_utils.standalone_test(pubdev_5352)
 else:
-    pubdev_5179()
+    pubdev_5352()
