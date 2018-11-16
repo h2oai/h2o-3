6c189748128aae2e1cc1ec8994ee9c66199c65ac
Sebastien Poirier <sebastien@h2o.ai>
2018-11-16 15:28:17 +0100

    fixed exception

--- h2o-automl/src/test/java/ai/h2o/automl/LeaderboardTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/LeaderboardTest.java
@@ -3,7 +3,6 @@ package ai.h2o.automl;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import water.DKV;
 import water.fvec.Frame;
 import water.util.TwoDimTable;
 
@@ -11,33 +10,40 @@ public class LeaderboardTest extends water.TestUtil {
 
   // TODO: plenty of tests to write for this class
 
-  @BeforeClass public static void setup() { stall_till_cloudsize(1); }
+  @BeforeClass
+  public static void setup() {
+    stall_till_cloudsize(1);
+  }
 
-  @Test public void test_toTwoDimTable_with_empty_models_and_without_sort_metric() {
-    AutoML aml = new AutoML();
+  @Test
+  public void test_toTwoDimTable_with_empty_models_and_without_sort_metric() {
     Leaderboard lb = null;
+    UserFeedback ufb = new UserFeedback(new AutoML());
     try {
-      lb = Leaderboard.getOrMakeLeaderboard("dummy_lb_no_sort_metric", new UserFeedback(aml), new Frame(), null);
+      lb = Leaderboard.getOrMakeLeaderboard("dummy_lb_no_sort_metric", ufb, new Frame(), null);
 
       TwoDimTable table = lb.toTwoDimTable();
       Assert.assertNotNull("empty leaderboard should also produce a TwoDimTable", table);
       Assert.assertEquals("no models in this leaderboard", table.getTableDescription());
     } finally {
       if (lb != null) lb.deleteWithChildren();
+      ufb.delete();
     }
   }
 
-  @Test public void test_toTwoDimTable_with_empty_models_and_with_sort_metric() {
-    AutoML aml = new AutoML();
+  @Test
+  public void test_toTwoDimTable_with_empty_models_and_with_sort_metric() {
     Leaderboard lb = null;
+    UserFeedback ufb = new UserFeedback(new AutoML());
     try {
-      lb = Leaderboard.getOrMakeLeaderboard("dummy_lb_logloss_sort_metric", new UserFeedback(aml), new Frame(), "logloss");
+      lb = Leaderboard.getOrMakeLeaderboard("dummy_lb_logloss_sort_metric", ufb, new Frame(), "logloss");
 
       TwoDimTable table = lb.toTwoDimTable();
       Assert.assertNotNull("empty leaderboard should also produce a TwoDimTable", table);
       Assert.assertEquals("no models in this leaderboard", table.getTableDescription());
     } finally {
       if (lb != null) lb.deleteWithChildren();
+      ufb.delete();
     }
   }
 
