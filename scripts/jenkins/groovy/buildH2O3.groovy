1e0b319b608961b08fb4d53fe8cbc66834964bcf
Michal Raška <michal.raska@gmail.com>
2018-11-20 15:24:53 +0100

    [PUBDEV-6068] Jenkinsfile and buildH2O3.

--- scripts/jenkins/groovy/buildH2O3.groovy
+++ scripts/jenkins/groovy/buildH2O3.groovy
@@ -10,93 +10,85 @@ def call(final pipelineContext) {
 
     def stageName = 'Build H2O-3'
     def buildClosure = {
-        pipelineContext.getBuildSummary().addStageSummary(this, stageName, 'h2o-3')
-        pipelineContext.getBuildSummary().setStageDetails(this, stageName, env.NODE_NAME, env.WORKSPACE)
-        try {
+        buildSummary.stageWithSummary(stageName, 'h2o-3') {
             // Launch docker container, build h2o-3, create test packages and archive artifacts
             def buildEnv = pipelineContext.getBuildConfig().getBuildEnv() + "PYTHON_VERSION=${PYTHON_VERSION}" + "R_VERSION=${R_VERSION}" + "JAVA_VERSION=${JAVA_VERSION}"
             def timeoutMinutes = pipelineContext.getBuildConfig().getBuildHadoop() ? 50 : 15
-            stage(stageName) {
-                insideDocker(buildEnv, pipelineContext.getBuildConfig().getDefaultImage(), pipelineContext.getBuildConfig().DOCKER_REGISTRY, pipelineContext.getBuildConfig(), timeoutMinutes, 'MINUTES') {
-                    try {
-                        makeTarget(pipelineContext) {
-                            target = 'build-h2o-3'
-                            hasJUnit = false
-                            archiveFiles = false
-                            makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
-                            activatePythonEnv = true
-                        }
-                        findAutoMLTests(pipelineContext, pipelineContext.getBuildConfig().COMPONENT_PY)
-                        makeTarget(pipelineContext) {
-                            target = 'test-package-py'
-                            hasJUnit = false
-                            archiveFiles = false
-                            makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
-                            activatePythonEnv = true
-                        }
-                        findAutoMLTests(pipelineContext, pipelineContext.getBuildConfig().COMPONENT_R)
-                        makeTarget(pipelineContext) {
-                            target = 'test-package-r'
-                            hasJUnit = false
-                            archiveFiles = false
-                            makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
-                            activatePythonEnv = true
-                        }
-                        if (pipelineContext.getBuildConfig().getBuildHadoop()) {
-                            makeTarget(pipelineContext) {
-                                target = 'test-package-hadoop'
-                                hasJUnit = false
-                                archiveFiles = false
-                                makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
-                                activatePythonEnv = true
-                            }
-                        }
-                        if (pipelineContext.getBuildConfig().componentChanged(pipelineContext.getBuildConfig().COMPONENT_JS)) {
-                            makeTarget(pipelineContext) {
-                                target = 'test-package-js'
-                                hasJUnit = false
-                                archiveFiles = false
-                                makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
-                                activatePythonEnv = true
-                            }
-                        }
-                        if (pipelineContext.getBuildConfig().componentChanged(pipelineContext.getBuildConfig().COMPONENT_JAVA)) {
-                            makeTarget(pipelineContext) {
-                                target = 'test-package-java'
-                                hasJUnit = false
-                                archiveFiles = false
-                                makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
-                                activatePythonEnv = true
-                            }
-                        }
-                    } finally {
-                        archiveArtifacts "**/*.log, **/out.*, **/*py.out.txt, **/java*out.txt, **/status.*"
-                        pipelineContext.getBuildConfig().TEST_PACKAGES_COMPONENTS.each { component ->
-                            if (pipelineContext.getBuildConfig().stashComponent(component)) {
-                                echo "********* Stash ${component} *********"
-                                pipelineContext.getUtils().stashFiles(
-                                        this,
-                                        pipelineContext.getBuildConfig().getStashNameForTestPackage(component),
-                                        "h2o-3/test-package-${component}.zip",
-                                        true
-                                )
-                                if (component == pipelineContext.getBuildConfig().COMPONENT_PY) {
-                                    pipelineContext.getUtils().stashXGBoostWheels(this, pipelineContext.getBuildConfig().getCurrentXGBVersion())
-                                }
-                            }
-                        }
-                        pipelineContext.getUtils().stashFiles(
-                                this,
-                                pipelineContext.getBuildConfig().H2O_JAR_STASH_NAME,
-                                "h2o-3/build/h2o.jar"
-                        )
+            insideDocker(buildEnv, pipelineContext.getBuildConfig().getDefaultImage(), pipelineContext.getBuildConfig().DOCKER_REGISTRY, pipelineContext.getBuildConfig(), timeoutMinutes, 'MINUTES') {
+                try {
+                    makeTarget(pipelineContext) {
+                        target = 'build-h2o-3'
+                        hasJUnit = false
+                        archiveFiles = false
+                        makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                        activatePythonEnv = true
                     }
+                    findAutoMLTests(pipelineContext, pipelineContext.getBuildConfig().COMPONENT_PY)
+                    makeTarget(pipelineContext) {
+                        target = 'test-package-py'
+                        hasJUnit = false
+                        archiveFiles = false
+                        makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                        activatePythonEnv = true
+                    }
+                    findAutoMLTests(pipelineContext, pipelineContext.getBuildConfig().COMPONENT_R)
+                    makeTarget(pipelineContext) {
+                        target = 'test-package-r'
+                        hasJUnit = false
+                        archiveFiles = false
+                        makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                        activatePythonEnv = true
+                    }
+                    if (pipelineContext.getBuildConfig().getBuildHadoop()) {
+                        makeTarget(pipelineContext) {
+                            target = 'test-package-hadoop'
+                            hasJUnit = false
+                            archiveFiles = false
+                            makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                            activatePythonEnv = true
+                        }
+                    }
+                    if (pipelineContext.getBuildConfig().componentChanged(pipelineContext.getBuildConfig().COMPONENT_JS)) {
+                        makeTarget(pipelineContext) {
+                            target = 'test-package-js'
+                            hasJUnit = false
+                            archiveFiles = false
+                            makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                            activatePythonEnv = true
+                        }
+                    }
+                    if (pipelineContext.getBuildConfig().componentChanged(pipelineContext.getBuildConfig().COMPONENT_JAVA)) {
+                        makeTarget(pipelineContext) {
+                            target = 'test-package-java'
+                            hasJUnit = false
+                            archiveFiles = false
+                            makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                            activatePythonEnv = true
+                        }
+                    }
+                } finally {
+                    archiveArtifacts "**/*.log, **/out.*, **/*py.out.txt, **/java*out.txt, **/status.*"
+                    pipelineContext.getBuildConfig().TEST_PACKAGES_COMPONENTS.each { component ->
+                        if (pipelineContext.getBuildConfig().stashComponent(component)) {
+                            echo "********* Stash ${component} *********"
+                            pipelineContext.getUtils().stashFiles(
+                                    this,
+                                    pipelineContext.getBuildConfig().getStashNameForTestPackage(component),
+                                    "h2o-3/test-package-${component}.zip",
+                                    true
+                            )
+                            if (component == pipelineContext.getBuildConfig().COMPONENT_PY) {
+                                pipelineContext.getUtils().stashXGBoostWheels(this, pipelineContext.getBuildConfig().getCurrentXGBVersion())
+                            }
+                        }
+                    }
+                    pipelineContext.getUtils().stashFiles(
+                            this,
+                            pipelineContext.getBuildConfig().H2O_JAR_STASH_NAME,
+                            "h2o-3/build/h2o.jar"
+                    )
                 }
             }
-            pipelineContext.getBuildSummary().markStageSuccessful(this, stageName)
-        } catch (Exception e) {
-            pipelineContext.getBuildSummary().markStageFailed(this, stageName)
-            throw e
         }
     }
     if (env.BUILDING_FORK) {
