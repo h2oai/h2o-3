36147d288c39b3f50f947964127176c06217af81
Veronika Maurerová <veronikamaurerova@MacBook-Pro-3.local>
2018-11-20 13:43:43 +0100

    PUBDEV-5236 add the missing transform method to the pca estimator in python client

--- h2o-py/tests/testdir_pipeline/pyunit_scale_pca_rf.py
+++ h2o-py/tests/testdir_pipeline/pyunit_scale_pca_rf.py
@@ -4,13 +4,10 @@ import h2o
 from tests import pyunit_utils
 
 
-
 def scale_pca_rf_pipe():
 
   from h2o.transforms.preprocessing import H2OScaler
   from h2o.transforms.decomposition import H2OPCA
-  # this should work below, but it's not yet: https://0xdata.atlassian.net/browse/PUBDEV-5236
-  #from h2o.estimators.pca import H2OPrincipalComponentAnalysisEstimator as H2OPCA
   from h2o.estimators.random_forest import H2ORandomForestEstimator
   from sklearn.pipeline import Pipeline
 
@@ -23,8 +20,25 @@ def scale_pca_rf_pipe():
   pipe.fit(iris[:4],iris[4])
 
 
+def scale_pca_rf_pipe_new_import():
+
+  from h2o.transforms.preprocessing import H2OScaler
+  from h2o.estimators.pca import H2OPrincipalComponentAnalysisEstimator as H2OPCA
+  from h2o.estimators.random_forest import H2ORandomForestEstimator
+  from sklearn.pipeline import Pipeline
+
+  iris = h2o.import_file(path=pyunit_utils.locate("smalldata/iris/iris_wheader.csv"))
+
+  # build transformation pipeline using sklearn's Pipeline and H2O transforms
+  pipe = Pipeline([("standardize", H2OScaler()),
+                     ("pca", H2OPCA(k=2)),
+                     ("rf", H2ORandomForestEstimator(seed=42,ntrees=50))])
+  pipe.fit(iris[:4],iris[4])
+
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(scale_pca_rf_pipe)
+  pyunit_utils.standalone_test(scale_pca_rf_pipe)
+  pyunit_utils.standalone_test(scale_pca_rf_pipe_new_import)
 else:
-    scale_pca_rf_pipe()
+  scale_pca_rf_pipe()
+  scale_pca_rf_pipe_new_import()
