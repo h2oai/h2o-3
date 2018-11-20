126ab792227f0e9047327d2f9442a59240d1dbc7
Michal Raška <michal.raska@gmail.com>
2018-11-20 15:49:28 +0100

    [PUBDEV-6068] Details.

--- scripts/jenkins/groovy/initPipelineContext.groovy
+++ scripts/jenkins/groovy/initPipelineContext.groovy
@@ -1,32 +1,62 @@
+import ai.h2o.ci.buildsummary.StagesSummary
+import ai.h2o.ci.buildsummary.DetailsSummary
+
 def call(final scmEnv, final String mode, final boolean ignoreChanges) {
-  return call(scmEnv, mode, ignoreChanges, null)
+    return call(scmEnv, mode, ignoreChanges, null)
 }
 
 def call(final scmEnv, final String mode, final boolean ignoreChanges, final List<String> gradleOpts) {
 
-  clearStageDirs()
+    clearStageDirs()
 
-  def pipelineContextFactory = load('h2o-3/scripts/jenkins/groovy/pipelineContext.groovy')
-  def final pipelineContext = pipelineContextFactory('h2o-3', mode, scmEnv, ignoreChanges, gradleOpts)
+    def pipelineContextFactory = load('h2o-3/scripts/jenkins/groovy/pipelineContext.groovy')
+    def final pipelineContext = pipelineContextFactory('h2o-3', mode, scmEnv, ignoreChanges, gradleOpts)
 
-  pipelineContext.getBuildSummary().addDetailsSection(this, mode)
-  pipelineContext.getBuildSummary().addChangesSectionIfNecessary(this)
-  pipelineContext.getBuildSummary().addFailedTestsSection(this)
+    buildSummary('https://github.com/h2oai/h2o-3', true)
+    buildSummary.get().addStagesSummary(this, new StagesSummary())
 
-  // Archive scripts so we don't have to do additional checkouts when changing node
-  pipelineContext.getUtils().stashScripts(this)
 
-  return pipelineContext
+    DetailsSummary detailsSummary = new DetailsSummary()
+    buildSummary.get().addDetailsSummary(this, detailsSummary)
+
+    if (mode) {
+        detailsSummary.setEntry(this, 'Mode', mode)
+    }
+    detailsSummary.setEntry(this, 'Commit Message', env.COMMIT_MESSAGE)
+    detailsSummary.setEntry(this, 'Branch', env.BRANCH_NAME)
+    detailsSummary.setEntry(this, 'Git SHA', env.GIT_SHA)
+
+    if (env.BUILDING_FORK) {
+        detailsSummary.setEntry(this, 'Building Fork', "true")
+    }
+
+
+    if (mode == 'MODE_SINGLE_TEST') {
+        detailsSummary.setEntry(this, 'Python', params.singleTestPyVersion)
+        detailsSummary.setEntry(this, 'R', params.singleTestRVersion)
+        detailsSummary.setEntry(this, 'Test', "${params.testComponent} - ${context.params.testPath}")
+        detailsSummary.setEntry(this, 'java.xmx', params.singleTestXmx)
+        detailsSummary.setEntry(this, '# H2O Nodes', params.singleTestNumNodes)
+        detailsSummary.setEntry(this, '# Runs:</strong', params.singleTestNumRuns)
+    }
+
+    pipelineContext.getBuildSummary().addChangesSectionIfNecessary(this)
+    pipelineContext.getBuildSummary().addFailedTestsSection(this)
+
+    // Archive scripts so we don't have to do additional checkouts when changing node
+    pipelineContext.getUtils().stashScripts(this)
+
+    return pipelineContext
 }
 
 void clearStageDirs() {
-  def findCmd = "find . -maxdepth 1 -not -name 'h2o-3' -not -name h2o-3@tmp -not -name '.'"
-  def deleteCmd = " -exec rm -rf '{}' ';'"
-  def findDeleteCmd = findCmd + deleteCmd
+    def findCmd = "find . -maxdepth 1 -not -name 'h2o-3' -not -name h2o-3@tmp -not -name '.'"
+    def deleteCmd = " -exec rm -rf '{}' ';'"
+    def findDeleteCmd = findCmd + deleteCmd
 
-  echo "About to delete these files/folders:"
-  sh findCmd
-  sh findDeleteCmd
+    echo "About to delete these files/folders:"
+    sh findCmd
+    sh findDeleteCmd
 }
 
 return this
