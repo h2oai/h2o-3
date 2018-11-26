111cabce2698436c8b005ff8057c06192b7d5e66
michalkurka <michalk@h2o.ai>
2018-11-26 13:10:06 -0800

    Add a tests that makes sure implicitly generated fold assignment is kept
    After the model building finishes

--- h2o-algos/src/test/java/hex/CrossValidFoldAssignmentsTest.java
+++ h2o-algos/src/test/java/hex/CrossValidFoldAssignmentsTest.java
@@ -6,10 +6,13 @@ import hex.tree.gbm.GBMModel;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import water.DKV;
+import water.MRTask;
 import water.TestUtil;
+import water.fvec.Chunk;
 import water.fvec.Frame;
 import water.fvec.Vec;
 import water.rapids.ast.prims.advmath.AstKFold;
+import water.util.ArrayUtils;
 
 import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.assertEquals;
@@ -98,4 +101,62 @@ public class CrossValidFoldAssignmentsTest extends TestUtil {
       if (cvFoldAssignmentFrame != null) cvFoldAssignmentFrame.delete();
     }
   }
+
+  // Checks that implicitly generated fold column is preserved after model is built
+  @Test public void checkImplicitFoldAssignmentsAreKeptWithoutMakeCopy() {
+    final int nfolds = 3;
+    Frame tfr = null;
+    Frame cvFoldAssignmentFrame = null;
+    GBMModel gbm = null;
+    try {
+      tfr = parse_test_file("smalldata/iris/iris_wheader.csv");
+
+      GBMModel.GBMParameters parms = new GBMModel.GBMParameters();
+      parms._train = tfr._key;
+      parms._response_column = "class";
+      parms._ntrees = 1;
+      parms._max_depth = 1;
+      parms._nfolds = nfolds;
+      parms._distribution = DistributionFamily.multinomial;
+      parms._keep_cross_validation_predictions=false;
+      parms._keep_cross_validation_fold_assignment=true;
+      GBM job = new GBM(parms);
+      gbm = job.trainModel().get();
+
+      assertNotNull(gbm._output._cross_validation_fold_assignment_frame_id);
+      cvFoldAssignmentFrame = DKV.getGet(gbm._output._cross_validation_fold_assignment_frame_id);
+      assertNotNull(cvFoldAssignmentFrame);
+      assertEquals(tfr.numRows(), cvFoldAssignmentFrame.numRows());
+      assertEquals(tfr.numRows(), ArrayUtils.sum(new CheckFoldTask(nfolds).doAll(cvFoldAssignmentFrame)._foldCnt));
+    } finally {
+      if (tfr != null) tfr.remove();
+      if (gbm != null) {
+        gbm.delete();
+        gbm.deleteCrossValidationModels();
+      }
+      if (cvFoldAssignmentFrame != null) cvFoldAssignmentFrame.delete();
+    }
+  }
+
+  private static class CheckFoldTask extends MRTask<CheckFoldTask> {
+    private long[] _foldCnt;
+    private CheckFoldTask(int nfolds) {
+      _foldCnt = new long[nfolds];
+    }
+    @Override
+    public void map(Chunk c) {
+      for (int i = 0; i < c._len; i++) {
+        double val = c.atd(i);
+        if ((int) val != val || val < 0 || val > _foldCnt.length) {
+          throw new IllegalStateException("Unexpected value: " + val);
+        }
+        _foldCnt[(int) val]++;
+      }
+    }
+    @Override
+    public void reduce(CheckFoldTask mrt) {
+      _foldCnt = ArrayUtils.add(_foldCnt, mrt._foldCnt);
+    }
+  }
+
 }
