21da68c4c74f36e8642453a5ff34c14ad17d8a3f
Veronika Maurerová <veronikamaurerova@MacBook-Pro-3.local>
2018-11-27 10:21:45 +0100

    PUBDEV-3847 add new test defined in issue

---
+++ h2o-py/tests/testdir_algos/gbm/pyunit_gbm_PUBDEV_3847.py
@@ -0,0 +1,38 @@
+from __future__ import print_function
+import sys
+sys.path.insert(1,"../../../")
+import h2o
+from tests import pyunit_utils
+from h2o.estimators.gbm import H2OGradientBoostingEstimator
+
+# Turn the PUBDEV-3847 issue into the test and check if it fails
+def test_pubdev_3847():
+    train = h2o.import_file(path=pyunit_utils.locate("smalldata/gbm_test/pubdev-3847.zip"), destination_frame="train")
+    train.describe()
+
+    ntrees = 100
+    max_depth = 6
+    min_rows = 5
+    learn_rate = 0.1
+    sample_rate = 0.8
+    col_sample_rate_per_tree = 0.6
+    nfolds = 2
+    min_split_improvement = 1e-04
+    response = "class"
+    features = train.col_names.remove(response)
+
+    my_gbm = H2OGradientBoostingEstimator(ntrees=ntrees,
+                                          max_depth=max_depth,
+                                          min_rows=min_rows,
+                                          learn_rate=learn_rate,
+                                          sample_rate=sample_rate,
+                                          col_sample_rate_per_tree=col_sample_rate_per_tree,
+                                          nfolds=nfolds,
+                                          min_split_improvement=min_split_improvement)
+    my_gbm.train(x=features, y=response, training_frame=train, validation_frame=train)
+
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(test_pubdev_3847)
+else:
+    test_pubdev_3847()
---
+++ h2o-r/tests/testdir_jira/runit_pubdev_3847.R
@@ -0,0 +1,28 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../scripts/h2o-r-test-setup.R")
+######################################################################################
+# Turn the PUBDEV-3847 issue into the test and check if it fails
+######################################################################################
+pubdev.3847.test <-
+  function() {
+      file <- locate("smalldata/gbm_test/pubdev-3847.zip")
+      data <- h2o.importFile(file, destination_frame = "pubdev3847.data", parse = FALSE)
+      response <- "class"
+      features <- setdiff(names(data), response)
+
+      ntrees <- 100
+      max_depth <- 6
+      min_rows <- 5
+      learn_rate <- 0.1
+      sample_rate <- 0.8
+      col_sample_rate_per_tree <- 0.6
+      nfolds <- 2
+      min_split_improvement <- 1e-04
+
+      model <- h2o.gbm(x = features,y = response,training_frame = data,model_id ="amodel",ntrees = ntrees,
+                        max_depth =max_depth ,min_rows = min_rows,learn_rate = learn_rate,
+                        sample_rate =sample_rate ,col_sample_rate_per_tree =col_sample_rate_per_tree ,
+                        nfolds = nfolds,min_split_improvement = min_split_improvement)
+  }
+
+doTest("PUBDEV-3847", pubdev.3847.test)
