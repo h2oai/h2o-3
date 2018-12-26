338579410528a961597e71825d12fb2cbe3b52b2
michalkurka <michalk@h2o.ai>
2018-12-26 14:07:47 -0800

    Stacked Ensembles: Adds a failing test to demonstrate issue PUBDEV-6157

---
+++ h2o-algos/src/test/java/hex/GLMHelper.java
@@ -0,0 +1,26 @@
+package hex;
+
+import hex.glm.GLMModel;
+import water.Job;
+import water.Scope;
+import water.fvec.Frame;
+
+// to be able to access package-private methods in GLM 
+public class GLMHelper {
+  
+  public static void runBigScore(GLMModel model,
+                                 Frame fr, boolean computeMetrics,
+                                 boolean makePrediction, Job j) {
+    String[] names = model.makeScoringNames();
+    String[][] domains = model.makeScoringDomains(fr, computeMetrics, names);
+
+    Frame adaptedFrame = new Frame(fr);
+    model.adaptTestForTrain(adaptedFrame, true, computeMetrics);
+    Scope.track(adaptedFrame);
+
+    model
+            .makeBigScoreTask(domains, names, adaptedFrame, computeMetrics, makePrediction, j, null)
+            .doAll(adaptedFrame);
+  } 
+  
+}
