be504fd2a3503db2e819dc17a5c59e03fbfe5089
Jordan Bentley <jbentley@evergage.com>
2018-12-27 16:57:05 -0500

    adding missing file

---
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/XGLeafAssignMojo.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2010-2018 Evergage, Inc.
+ * All rights reserved.
+ */
+
+package hex.genmodel.algos;
+
+/**
+ * TODO: Add brief description.
+ */
+public interface XGLeafAssignMojo {
+
+    float[] getLeafNodeAssignments(double[] rawData);
+    
+}
