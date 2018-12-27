8752f2bf079a82fb5f32ae1da52ea930bbc3c18c
Jordan Bentley <jbentley@evergage.com>
2018-12-27 17:01:21 -0500

    removing EVG copyright header

--- h2o-genmodel/src/main/java/hex/genmodel/algos/XGLeafAssignMojo.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/XGLeafAssignMojo.java
@@ -1,13 +1,6 @@
-/*
- * Copyright (C) 2010-2018 Evergage, Inc.
- * All rights reserved.
- */
 
 package hex.genmodel.algos;
 
-/**
- * TODO: Add brief description.
- */
 public interface XGLeafAssignMojo {
 
     float[] getLeafNodeAssignments(double[] rawData);
