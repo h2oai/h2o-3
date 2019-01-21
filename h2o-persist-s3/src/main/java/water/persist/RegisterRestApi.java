439abbfd8ecd0f88c2ca669c0c0844e38d9e9468
Pavel Pscheidl <pavel@h2o.ai>
2019-01-21 15:15:07 +0100

    Persist S3 REST API registration fix.

--- h2o-persist-s3/src/main/java/water/persist/RegisterRestApi.java
+++ h2o-persist-s3/src/main/java/water/persist/RegisterRestApi.java
@@ -1,11 +1,17 @@
 package water.persist;
 
+import water.api.AbstractRegister;
 import water.api.AlgoAbstractRegister;
 import water.api.RestApiContext;
 
-public class RegisterRestApi extends AlgoAbstractRegister {
+public class RegisterRestApi extends AbstractRegister {
     @Override
     public void registerEndPoints(RestApiContext context) {
         context.registerEndpoint("set_s3_credentials", "GET /3/Persist", PersistS3Handler.class, "setS3Credentials", "Set Amazon S3 credentials (Secret Key ID, Secret Access Key)");
+    }
+
+    @Override
+    public String getName() {
+        return "Amazon S3";
     }
 }
