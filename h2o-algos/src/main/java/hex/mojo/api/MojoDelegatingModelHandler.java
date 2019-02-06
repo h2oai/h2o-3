ee2954b5c44cc8c6366128db86030a4817666e44
Pavel Pscheidl <pavel@h2o.ai>
2019-02-06 12:27:17 -0800

    MojoDelegatingModelHandler formatting

--- h2o-algos/src/main/java/hex/mojo/api/MojoDelegatingModelHandler.java
+++ h2o-algos/src/main/java/hex/mojo/api/MojoDelegatingModelHandler.java
@@ -28,21 +28,26 @@ public class MojoDelegatingModelHandler extends Handler {
 
     /**
      * Retrieves pre-uploaded MOJO archive and performs basic verifications, if present.
+     *
      * @param key Key to MOJO bytes in DKV
      * @return An instance of {@link ByteVec} containing the bytes of an uploaded MOJO, if present. Or exception. Never returns null.
      * @throws IllegalArgumentException In case the supplied key is invalid (MOJO missing, empty key etc.)
      */
-    private final ByteVec getUploadedMojo(final String key) throws IllegalArgumentException{
+    private final ByteVec getUploadedMojo(final String key) throws IllegalArgumentException {
         final Value value = DKV.get(key);
-        if (value == null) throw new IllegalArgumentException(String.format("Given MOJO file key '%s' is not to be found in H2O.", value));
-        if(!value.isFrame()) throw new IllegalArgumentException(String.format("Given MOJO file key should a one-columnar Frame, however the following type has been found: '%d'", value.type()));
+        if (value == null)
+            throw new IllegalArgumentException(String.format("Given MOJO file key '%s' is not to be found in H2O.", value));
+        if (!value.isFrame())
+            throw new IllegalArgumentException(String.format("Given MOJO file key should a one-columnar Frame, however the following type has been found: '%d'", value.type()));
 
         Frame mojoFrame = value.get();
-        if(mojoFrame.numCols() > 1) throw new IllegalArgumentException(String.format("Given MOJO frame with key '%s' should contain only 1 column with MOJO bytes. More columns found. Incorrect key provided ?", key));
+        if (mojoFrame.numCols() > 1)
+            throw new IllegalArgumentException(String.format("Given MOJO frame with key '%s' should contain only 1 column with MOJO bytes. More columns found. Incorrect key provided ?", key));
         ByteVec mojoData = (ByteVec) mojoFrame.anyVec();
-        
-        if(mojoData.length() < 1) throw new IllegalArgumentException(String.format("Given MOJO frame with key '%s' is empty (0 bytes). Please provide a non-empty MOJO file.", key) );
-        
+
+        if (mojoData.length() < 1)
+            throw new IllegalArgumentException(String.format("Given MOJO frame with key '%s' is empty (0 bytes). Please provide a non-empty MOJO file.", key));
+
         return mojoData;
     }
 
