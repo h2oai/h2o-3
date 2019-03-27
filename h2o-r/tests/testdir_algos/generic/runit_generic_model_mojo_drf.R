b18e4411b1aa5f29bca3c2dbcac2db714ff4c565
Pavel Pscheidl <pavel@h2o.ai>
2019-03-27 07:44:41 +0100

    Changed names of Generic API variables - removed dots

--- h2o-r/tests/testdir_algos/generic/runit_generic_model_mojo_drf.R
+++ h2o-r/tests/testdir_algos/generic/runit_generic_model_mojo_drf.R
@@ -6,17 +6,17 @@ source("../../../scripts/h2o-r-test-setup.R")
 test.model.generic.drf <- function() {
     data <- h2o.importFile(path = locate('smalldata/testng/airlines_train.csv'))
     cols <- c("Origin", "Distance")
-    model.original <- h2o.randomForest(x=cols, y = "IsDepDelayed", training_frame = data)
+    original_model <- h2o.randomForest(x=cols, y = "IsDepDelayed", training_frame = data)
 
-    mojo.original.name <- h2o.download_mojo(model = model.original, path = tempdir())
-    mojo.original.path <- paste0(tempdir(),"/",mojo.original.name)
+    mojo_original_name <- h2o.download_mojo(model = original_model, path = tempdir())
+    mojo_original_path <- paste0(tempdir(),"/",mojo_original_name)
     
-    model.generic <- h2o.genericModel(mojo.original.path)
-    model.generic.preds  <- h2o.predict(model.generic, data)
-    expect_equal(length(model.generic.preds), 3)
-    expect_equal(h2o.nrow(model.generic.preds), 24421)
-    model.generic.path <- h2o.download_mojo(model = model.generic, path = tempdir())
-    expect_equal(file.size(paste0(tempdir(),"/",model.generic.path)), file.size(mojo.original.path))
+    generic_model <- h2o.genericModel(mojo_original_path)
+    generic_model_preds  <- h2o.predict(generic_model, data)
+    expect_equal(length(generic_model_preds), 3)
+    expect_equal(h2o.nrow(generic_model_preds), 24421)
+    generic_model_path <- h2o.download_mojo(model = generic_model, path = tempdir())
+    expect_equal(file.size(paste0(tempdir(),"/",generic_model_path)), file.size(mojo_original_path))
     
 }
 
