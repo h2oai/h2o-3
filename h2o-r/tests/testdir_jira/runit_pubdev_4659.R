27463d8109fdd069cd7b154e69fa268ad233b77c
maurever <veronika@maurerova.cz>
2019-03-29 15:57:29 +0100

    PUBDEV-4659 edit test, log print

--- h2o-r/tests/testdir_jira/runit_pubdev_4659.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_4659.R
@@ -2,43 +2,41 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
 
 
-test.4659 <- function() {
-    prostate.hex = read.csv(locate("smalldata/logreg/prostate.csv"))
-    prostate.hex$our_factor<-as.factor(paste0("Q",c(rep(c(1:380),1))))
+test_4659 <- function() {
+    prostate = read.csv(locate("smalldata/logreg/prostate.csv"))
+    prostate$our_factor<-as.factor(paste0("Q",c(rep(c(1:380),1))))
 
-    prostate.hex<-as.h2o(prostate.hex)
-    prostate.hex$weight<-1
+    prostate<-as.h2o(prostate)
+    prostate$weight<-1
     
-    prostateTrain<-prostate.hex[1:300,]
-    prostateTest<-prostate.hex[301:380,]
+    prostate_train<-prostate[1:300,]
+    prostate_test<-prostate[301:380,]
 
     # delete response variable from test data
-    prostateTest<-prostateTest[,-3]
+    prostate_test<-prostate_test[,-3]
     
     print("GLM 1")
-    model<-h2o.glm(y = "AGE", x = c("our_factor"),
-    training_frame = prostateTrain,offset_column="weight")
-    predict(model,newdata=prostateTrain)
-    predict(model,newdata=prostateTest)
+    model<-h2o.glm(y = "AGE", x = c("our_factor"), training_frame = prostate_train,offset_column="weight")
+    predict(model,newdata=prostate_train)
+    predict(model,newdata=prostate_test)
 
     # Failed when response column was missing and there is only one feature 
     print("GLM 2")
-    model<-h2o.glm(y = "AGE", x = c("our_factor"),
-    training_frame = prostateTrain)
-    predict(model,newdata=prostateTrain)
-    predict(model,newdata=prostateTest)
+    model<-h2o.glm(y = "AGE", x = c("our_factor"), training_frame = prostate_train)
+    predict(model,newdata=prostate_train)
+    predict(model,newdata=prostate_test)
     
     # Failed when original columns names does not fit with edited frame columns with dummy NA column for response column
     print("GBM 1")
-    model<-h2o.gbm(y = "AGE", x = c("our_factor"), training_frame = prostateTrain,categorical_encoding = "OneHotExplicit")
-    predictionResponse <- predict(model,newdata=prostateTrain)
-    predict(model,newdata=prostateTest)
+    model<-h2o.gbm(y = "AGE", x = c("our_factor"), training_frame = prostate_train,categorical_encoding = "OneHotExplicit")
+    prediction_response <- predict(model,newdata=prostate_train)
+    predict(model,newdata=prostate_test)
 
     print("Prediction check")
     # Delete response variable from train data to compare result predictions
-    predictionWithoutResponse <- predict(model, newdata=prostateTrain[,-3])
+    prediction_without_response <- predict(model, newdata=prostate_train[,-3])
     # Result predictions should be the same 
-    expect_equal(predictionResponse, predictionWithoutResponse)
+    expect_equal(prediction_response, prediction_without_response)
 }
 
-doTest("PUBDEV-4659", test.4659)
+doTest("PUBDEV-4659 test predict on data without response column", test_4659)
