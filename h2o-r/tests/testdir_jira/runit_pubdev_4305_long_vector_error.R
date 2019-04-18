d01f4cd0c9006f47c5e1b7f38f72d4bfbb0b7293
maurever <veronika@maurerova.cz>
2019-04-18 11:52:39 +0200

    PUBDEV-4305 edit test

--- h2o-r/tests/testdir_jira/runit_pubdev_4305_long_vector_error.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_4305_long_vector_error.R
@@ -1,17 +1,18 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
+
+
 ######################################################################################
 # PUBDEV-4305 Test long vector conversion
 ######################################################################################
-pubdev.4305.test <-
+pubdev_4305_test <-
 function() {
     file <- locate("smalldata/wa_cannabis/raw/Dashboard_Usable_Sales_w_Weight_Daily.csv")
-    data <- h2o.importFile(file, destination_frame = "pubdev4305.data")
-
-    dataH2o = as.h2o(data)
-    dim(dataH2o)
-    dataDF = as.data.frame(dataH2o)
-    #expect_true(all(data == dataDF))
+    bigdata <- read.csv(file, header = FALSE)
+    h2oframe = as.h2o(bigdata)
+    bigdataframe = as.data.frame(h2oframe)
+    
+    expect_true(all.equal(bigdata,bigdataframe))
 }
 
-doTest("PUBDEV-4305", pubdev.4305.test)
+doTest("PUBDEV-4305", pubdev_4305_test)
