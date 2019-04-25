0e26bbdac47f658a749c52070827dd762bcd5532
Pavel Pscheidl <pavel@h2o.ai>
2019-04-25 14:48:55 +0200

    MOJO import convenience methods (R)

---
+++ h2o-r/tests/testdir_algos/generic/runit_generic_mojo_convenience.R
@@ -0,0 +1,27 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+
+
+test.model.generic.mojo.convenience<- function() {
+  data <- as.h2o(iris)
+  features <- c("Sepal.Length", "Sepal.Length", "Sepal.Width", "Petal.Length", "Petal.Width")
+  original_model <- h2o.gbm(x=features, y = "Species", training_frame = data)
+  mojo_original_name <- h2o.download_mojo(model = original_model, path = tempdir())
+  mojo_original_path <- paste0(tempdir(),"/",mojo_original_name)
+  
+  # Mojo Import
+  mojo_model <- h2o.importMojo(mojo_original_path)
+  
+  predictions  <- h2o.predict(mojo_model, data)
+  expect_equal(length(predictions), 4)
+  expect_equal(h2o.nrow(predictions), 150)
+  
+  # MOJO Upload
+  mojo_model <- h2o.uploadMojo(mojo_original_path)
+  predictions  <- h2o.predict(mojo_model, data)
+  expect_equal(length(predictions), 4)
+  expect_equal(h2o.nrow(predictions), 150)
+}
+
+doTest("Generic model from GBM MOJO", test.model.generic.mojo.convenience)
