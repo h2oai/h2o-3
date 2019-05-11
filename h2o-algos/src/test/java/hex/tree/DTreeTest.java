5132dff60cf0b713d95a7789cd8b71e24f882285
michalkurka <michalk@h2o.ai>
2019-05-10 17:54:28 -0700

    Shows that GBM doesn't sometimes include SE of NAs into total node SE
    This makes the algo abandon the split even if it would lead to an actual
    improvement in SE

--- h2o-algos/src/test/java/hex/tree/DTreeTest.java
+++ h2o-algos/src/test/java/hex/tree/DTreeTest.java
@@ -8,6 +8,40 @@ import static org.junit.Assert.*;
 
 public class DTreeTest {
 
+  @Test
+  public void testFindBestSplitPoint_pubdev6495() {
+
+    double[] ws = new double[]{10,         10,         100, 1  };
+    double[] cs = new double[]{Double.NaN, Double.NaN, 0.5, 1.5};
+    double[] ys = new double[]{0,          1,          0,   1  };
+    int[] rows  = new int[]   {0,          1,          2,   3  };
+
+    DHistogram hs = new DHistogram("test_hs", 2, 0, (byte) 0, 0, 2, 0.01,
+            SharedTreeModel.SharedTreeParameters.HistogramType.UniformAdaptive, 123, null, null);
+    hs.init();
+    hs.updateHisto(ws, null, cs, ys,rows, rows.length, 0);
+
+    // 1. min_rows = #NAs
+    DTree.Split s1 = DTree.findBestSplitPoint(hs, 0, 20, 0, Double.NaN, Double.NaN, false);
+    assertNotNull(s1);
+    final double se = s1._se; 
+
+    // 2. min_rows = #NAs + 1
+    DTree.Split s2 = DTree.findBestSplitPoint(hs, 0, 21, 0, Double.NaN, Double.NaN, false);
+    assertNull(s2); // not enough improvement => no split
+
+    // 3. allow negative (!!!) split improvement, min_rows = #NAs + 1
+    DHistogram hsN = new DHistogram("test_hs", 2, 0, (byte) 0, 0, 2, -9,
+            SharedTreeModel.SharedTreeParameters.HistogramType.UniformAdaptive, 123, null, null);
+    hsN.init();
+    hsN.updateHisto(ws, null, cs, ys,rows, rows.length, 0);
+    DTree.Split s3 = DTree.findBestSplitPoint(hsN, 0, 21, 0, Double.NaN, Double.NaN, false);
+    assertNotNull(s3); // split was made thanks to the negative split improvement
+    assertEquals(s3._se, seNonNA(ws, cs, ys), 0); // SE is actually the non-NA SE
+    assertTrue(s3._se < s1._se); // SE is different because the SE of NAs is not accounted for
+    // case (2) shows that we would abandon the split if it was not for the negative minimum split improvement 
+  }
+
   @Test // check that bounds constrained "NA vs REST" splits have correct Square Errors
   public void testFindBestSplitPoint_bounds_NAvsREST() {
     final double min_pred = 0.3;
@@ -77,26 +111,26 @@ public class DTreeTest {
       return new ExpectedSplitInfo(NASplitDir.NAvsREST, se, se_max_pred, se_min_pred);
     } else {
       // ignore actual SE and use non-NA SE
-      double nna_se = seNonNA(cs, ys); 
+      double nna_se = seNonNA(ws, cs, ys); 
       return new ExpectedSplitInfo(NASplitDir.NALeft, nna_se, se_min_pred, se_max_pred);
     }
   }
   
-  private static double seNonNA(double[] cs, double[] ys) {
+  private static double seNonNA(double ws[], double[] cs, double[] ys) {
     double nna_y_sum = 0;
-    int nna_y_cnt = 0;
+    double nna_y_weight = 0;
     for (int i = 0; i < ys.length; i++) {
       if (Double.isNaN(cs[i]))
         continue;
-      nna_y_cnt++;
-      nna_y_sum += ys[i];
+      nna_y_weight += ws[i];
+      nna_y_sum += ws[i] * ys[i];
     }
-    double nna_y_mean = nna_y_sum / nna_y_cnt;
+    double nna_y_mean = nna_y_sum / nna_y_weight;
     double nna_se = 0;
     for (int i = 0; i < ys.length; i++) {
       if (Double.isNaN(cs[i]))
         continue;
-      nna_se += (ys[i] - nna_y_mean) * (ys[i] - nna_y_mean);
+      nna_se += ws[i] * (ys[i] - nna_y_mean) * (ys[i] - nna_y_mean);
     }
     return nna_se;
   }
