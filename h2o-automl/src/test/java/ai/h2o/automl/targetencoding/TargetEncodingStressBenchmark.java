8dad51229734a8935e2f50db703f7a7261c30034
Andrey Spiridonov <andrey@h2o.ai>
2019-05-14 15:43:51 +0200

    PUBDEV-6481: Removed test that was failing on purpose. Increased number of rows to 5m.

--- h2o-automl/src/test/java/ai/h2o/automl/targetencoding/TargetEncodingStressBenchmark.java
+++ h2o-automl/src/test/java/ai/h2o/automl/targetencoding/TargetEncodingStressBenchmark.java
@@ -2,14 +2,12 @@ package ai.h2o.automl.targetencoding;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 import water.Scope;
 import water.TestUtil;
 import water.fvec.Frame;
 import water.fvec.TestFrameBuilder;
 import water.fvec.Vec;
-import water.parser.Categorical;
 
 import java.util.Map;
 import java.util.Random;
@@ -24,14 +22,14 @@ public class TargetEncodingStressBenchmark extends TestUtil {
   }
 
   @Test
-  public void syntheticBigDataTest() {
+  public void syntheticBigDataWithHighCardinalityCategoricalColumnTest() {
 
     Map<String, Frame> encodingMap = null;
 
     Scope.enter();
     long seed = new Random().nextLong();
     try {
-      int size = 1000;
+      int size = 5000000;
       String[] arr = new String[size];
       for (int a = 0; a < size; a++) {
         arr[a] = Integer.toString(new Random().nextInt(size / 2));
@@ -42,15 +40,17 @@ public class TargetEncodingStressBenchmark extends TestUtil {
               .withVecTypes(Vec.T_STR, Vec.T_NUM)
               .withDataForCol(0, arr)
               .withRandomIntDataForCol(1, size, 0, 2, seed)
+              .withChunkLayout(1000000, 1000000, 1000000, 1000000, 1000000)
               .build();
 
       asFactor(fr, "ColA");
       asFactor(fr, "y");
 
       int cardinality = fr.vec("ColA").cardinality();
+      System.out.println("Cardinality of the categorical column which is about to be encoded is: " + cardinality);
 //      Assert.assertTrue(cardinality >= Categorical.MAX_CATEGORICAL_COUNT * 0.5);
 
-      printOutFrameAsTable(fr, false, 100);
+      printOutFrameAsTable(fr, false, 10);
       String foldColumnName = "fold";
       addKFoldColumn(fr, foldColumnName, 5, seed);
 
@@ -78,11 +78,6 @@ public class TargetEncodingStressBenchmark extends TestUtil {
       if (encodingMap != null) encodingMapCleanUp(encodingMap);
       Scope.exit();
     }
-  }
-
-  @Test
-  public void failsForSureTest() {
-    Assert.fail();
   }
 
   private void encodingMapCleanUp(Map<String, Frame> encodingMap) {
