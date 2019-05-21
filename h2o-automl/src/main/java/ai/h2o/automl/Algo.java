751220b7c0a0ef0209dd0e72a04f5413da6d3670
Sebastien Poirier <sebastien@h2o.ai>
2019-05-21 17:17:50 +0200

    ensure that Java UTs take into consideration the fact that algos are enabled or not

--- h2o-automl/src/main/java/ai/h2o/automl/Algo.java
+++ h2o-automl/src/main/java/ai/h2o/automl/Algo.java
@@ -1,5 +1,9 @@
 package ai.h2o.automl;
 
+import water.ExtensionManager;
+import water.H2O;
+
+
 // if we need to make the Algo list dynamic, we should just turn this enum into a class...
 // implementation of AutoML.algo can be safely removed once we get rid of this interface: current purpose
 // is to keep backward compatibility with {@link AutoML.algo}
@@ -9,10 +13,22 @@ public enum Algo {
   GBM,
   DeepLearning,
   StackedEnsemble,
-  XGBoost,
+  XGBoost() {
+    private static final String DISTRIBUTED_XGBOOST_ENABLED = H2O.OptArgs.SYSTEM_PROP_PREFIX + "automl.xgboost.multinode.enabled";
+
+    @Override
+    boolean enabled() {
+      return ExtensionManager.getInstance().isCoreExtensionEnabled(this.name())
+              && !(H2O.CLOUD.size() > 1 && !Boolean.parseBoolean(System.getProperty(DISTRIBUTED_XGBOOST_ENABLED, "false")));
+    }
+  },
   ;
 
   String urlName() {
     return this.name().toLowerCase();
+  }
+
+  boolean enabled() {
+    return true;
   }
 }
