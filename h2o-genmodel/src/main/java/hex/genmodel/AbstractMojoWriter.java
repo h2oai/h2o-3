c667762ba5f511b1c572ca875f91d913068065fb
Pavel Pscheidl <pavel@h2o.ai>
2019-06-05 20:16:43 +0200

    Removed unused import in AbstractMojoWriter

--- h2o-genmodel/src/main/java/hex/genmodel/AbstractMojoWriter.java
+++ h2o-genmodel/src/main/java/hex/genmodel/AbstractMojoWriter.java
@@ -8,7 +8,6 @@ import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.regex.Pattern;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
