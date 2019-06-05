5c04860f7efd836886796dc59d06673769d1f4c6
Pavel Pscheidl <pavel@h2o.ai>
2019-06-05 07:46:28 +0200

    Improved PrintMojo documentation

--- h2o-genmodel/src/main/java/hex/genmodel/tools/PrintMojo.java
+++ h2o-genmodel/src/main/java/hex/genmodel/tools/PrintMojo.java
@@ -71,9 +71,9 @@ public class PrintMojo {
     System.out.println("");
     System.out.println("    --fontsize | -f    Set font sizes of strings.");
     System.out.println("");
-    System.out.println("    --raw    Print raw graph representation to standard output.");
+    System.out.println("    --raw    Print raw, human-readable graph representation to standard output. Together with graphviz representation.");
     System.out.println("");
-    System.out.println("    --internal    Internal H2O representation of the graph (splits etc.) is used for generating the GRAPHVIZ format.");
+    System.out.println("    --internal    Internal H2O representation of the decision tree (splits etc.) is used for generating the GRAPHVIZ format.");
     System.out.println("");
     System.out.println("");
     System.out.println("Example:");
