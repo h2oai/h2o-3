bc3d5316cd90ff61eff19169e11efd280dbb2cd9
wendycwong <wendycwong@yahoo.com>
2019-06-11 13:32:26 -0700

    PUBDEV-6438: Finished adding to Java backend to support 2d PDP. PUBDEV-6438: Add junit tests for 2D pdp. PUBDEV-6438: added pyunit test. PUBDEV-6438: added R client support and Runit test. PUBDEV-6438: 3-D plot in Python. PUBDEV-6438: R 3D plot version0. PUBDEV-6438: incorporate Honza comments. PUDBEV-6438: Incorporate Navdeep suggestions. PUBDEV-6438: moved pdp to its own class per requests from both Navdeep and Honza.  Okay, boys, this one is for you! PUBDEV-6438: Fixed error in Junit test caught by Michalk.

--- h2o-r/h2o-package/R/models.R
+++ h2o-r/h2o-package/R/models.R
@@ -3181,6 +3181,9 @@ h2o.cross_validation_predictions <- function(object) {
 #' @param user_splits A two-level nested list containing user defined split points for pdp plots for each column.
 #' If there are two columns using user defined split points, there should be two lists in the nested list.
 #' Inside each list, the first element is the column name followed by values defined by the user.
+#' @param col_pairs_2dpdp A two-level nested list like this: col_pairs_2dpdp = list(c("col1_name", "col2_name"),
+#'   c("col1_name","col3_name"), ...,) where a 2D partial plots will be generated for col1_name, col2_name pair, for
+#'   col1_name, col3_name pair and whatever other pairs that are specified in the nested list.    
 #' @param save_to Fully qualified prefix of the image files the resulting plots should be saved to, e.g. '/home/user/pdp'.
 #'  Plots for each feature are saved separately in PNG format, each file receives a suffix equal to the corresponding feature name, e.g. `/home/user/pdp_AGE.png`.
 #'  If the files already exists, they will be overridden. Files are only saves if plot = TRUE (default).
@@ -3205,7 +3208,8 @@ h2o.cross_validation_predictions <- function(object) {
 #' @export
 
 h2o.partialPlot <- function(object, data, cols, destination_key, nbins=20, plot = TRUE, plot_stddev = TRUE,
-                            weight_column=-1, include_na=FALSE, user_splits=NULL, save_to=NULL, row_index=-1) {
+                            weight_column=-1, include_na=FALSE, user_splits=NULL, col_pairs_2dpdp=NULL, save_to=NULL,
+row_index=-1) {
   if(!is(object, "H2OModel")) stop("object must be an H2Omodel")
   if( is(object, "H2OMultinomialModel")) stop("object must be a regression model or binary classfier")
   if( is(object, "H2OOrdinalModel")) stop("object must be a regression model or binary classfier")
@@ -3214,11 +3218,26 @@ h2o.partialPlot <- function(object, data, cols, destination_key, nbins=20, plot 
   if(!is.logical(plot)) stop("plot must be a logical value")
   if(!is.logical(plot_stddev)) stop("plot must be a logical value")
   if(!is.logical(include_na)) stop("add_missing_NA must be a logical value")
-  if(missing(cols)) cols =  object@parameters$x
+  noPairs = missing(col_pairs_2dpdp)
+  noCols = missing(cols)
+  if(noCols && noPairs) cols =  object@parameters$x # set to default only if both are missing
 
   y = object@parameters$y
-  x = cols
-  args <- .verify_dataxy(data, x, y)
+  numCols = 0
+  numColPairs = 0    
+  if (!missing(cols)) { # check valid cols in cols for 1d pdp
+    x <- cols
+    args <- .verify_dataxy(data, x, y)
+  }
+  cpairs <- NULL
+  if (!missing(col_pairs_2dpdp))   { # verify valid cols for 2d pdp
+    for (onePair in col_pairs_2dpdp) {
+      pargs <- .verify_dataxy(data, onePair, y)
+      cpairs <-
+        c(cpairs, paste0("[", paste (pargs$x, collapse = ','), "]"))
+    }
+    numColPairs = length(cpairs)
+  }
 
   if (is.numeric(weight_column) && (weight_column != -1)) {
       stop("weight_column should be a column name of your data frame.")
@@ -3234,7 +3253,14 @@ h2o.partialPlot <- function(object, data, cols, destination_key, nbins=20, plot 
   }
   
   parms = list()
-  parms$cols <- paste0("[", paste (args$x, collapse = ','), "]")
+  if (!missing(col_pairs_2dpdp)) {
+    parms$col_pairs_2dpdp <- paste0("[", paste (cpairs, collapse = ','), "]")
+  }
+  if (!missing(cols)) {
+    parms$cols <- paste0("[", paste (args$x, collapse = ','), "]")
+    numCols = length(cols)
+  }
+  noCols = missing(cols)
   parms$model_id  <- attr(object, "model_id")
   parms$frame_id <- attr(data, "id")
   parms$nbins <- nbins
@@ -3300,10 +3326,27 @@ h2o.partialPlot <- function(object, data, cols, destination_key, nbins=20, plot 
 
   ## Change feature names to the original supplied, the following is okay because order is preserved
   pps <- res$partial_dependence_data
-  for(i in 1:length(pps)) if(!all(is.na( pps[[i]])) ) names(pps[[i]]) <- c(cols[i], "mean_response", "stddev_response", "std_error_mean_response")
-
+  for (i in 1:length(pps)) {
+    if (!all(is.na(pps[[i]]))) {
+      if (i <=  numCols) {
+      names(pps[[i]]) <-
+        c(cols[i],
+          "mean_response",
+          "stddev_response",
+          "std_error_mean_response")
+      } else {
+        names(pps[[i]]) <-
+          c(col_pairs_2dpdp[[i-numCols]][1],
+            col_pairs_2dpdp[[i-numCols]][2],
+            "mean_response",
+            "stddev_response",
+            "std_error_mean_response")
+      }
+    }
+  }
   col_types = unlist(h2o.getTypes(data))
   col_names = names(data)
+    
   pp.plot <- function(pp) {
     if(!all(is.na(pp))) {
       type = col_types[which(col_names == names(pp)[1])]
@@ -3327,7 +3370,61 @@ h2o.partialPlot <- function(object, data, cols, destination_key, nbins=20, plot 
       print("Partial Dependence not calculated--make sure nbins is as high as the level count")
     }
   }
-
+  pp.plot2 <- function(pp, nBins=nbins, user_cols=NULL, user_num_splits=NULL) {
+    xtickMarks <- NULL
+    ytickMarks <- NULL
+    if (!all(is.na(pp))) {
+      if (col_types[which(col_names == names(pp)[1])] == "enum") {
+        x <- replaceEnumLevel(pp[,1], unique(pp[,1]))
+        xtickMarks <- unique(pp[,1])
+      } else {
+        x <- pp[,1]
+      }
+      if (col_types[which(col_names == names(pp)[2])] == "enum") {
+        y <- replaceEnumLevel(pp[,2], unique(pp[,2]))
+        ytickMarks <- unique(pp[,2])
+      } else {
+        y <- pp[,2]
+      }
+      allMetric <- reShape(x, y, pp[, 3], names(pp)[1], names(pp)[2], nBins, user_cols, user_num_splits)
+      XX <- allMetric[[1]]
+      YY <- allMetric[[2]]
+      ZZ <- allMetric[[3]]
+      tTitle <- ""
+      if (!is.null(xtickMarks)) {
+        xc <- c(1:length(xtickMarks))
+        tTitle <- paste0("X axis tick marks: ", paste(xc, xtickMarks, sep=":", collapse=", "))
+      }
+      if (!is.null(ytickMarks)) {
+        yc <- c(1:length(ytickMarks))
+        temp <- paste0("Y axis tick marks: ", paste(yc, ytickMarks, sep=":", collapse=", "))
+        tTitle <- paste0(tTitle, temp)
+      }
+      ## Plot one standard deviation above and below the mean
+      if (plot_stddev) {
+        ## Added upper and lower std dev confidence bound
+        upper = pp[, 3] + pp[, 4]
+        lower = pp[, 3] - pp[, 4]
+        Zupper = matrix(upper, ncol=dim(XX)[2], byrow=F)
+        Zlower = matrix(lower, ncol=dim(XX)[2], byrow=F)
+        open3d()
+        persp3Drgl(XX, YY, ZZ,theta=30, phi=15, axes=TRUE,scale=2, box=TRUE, nticks=5, 
+                ticktype="detailed",xlab=names(pp)[1], ylab=names(pp)[2], zlab="2D partial plots", 
+                main=tTitle, border='black', alpha=0.5)
+        persp3Drgl(XX, YY, Zupper,alpha=0.2,lwd=2, add=TRUE, border='yellow')   
+        persp3Drgl(XX, YY, Zlower,alpha=0.2,lwd=2, add=TRUE, border='green')  
+        grid3d(c("x", "y", "z"))
+      } else {
+        persp3D(XX, YY, ZZ,theta=30, phi=50, axes=TRUE,scale=2, box=TRUE, nticks=5, 
+                ticktype="detailed",xlab=names(pp)[1], ylab=names(pp)[2], zlab="2D partial plots", 
+                main=tTitle, border='black', alpha=0.5)
+        grid3d(c("x", "y", "z"))
+      }
+    } else {
+      print("2D Partial Dependence not calculated--make sure nbins is as high as the level count")
+    }
+  }
+  
   pp.plot.save <- function(pp) {
     # If user accidentally provides one of the most common suffixes in R, it is removed.
     save_to <- gsub(replacement = "",pattern = "(\\.png)|(\\.jpg)|(\\.pdf)", x = save_to)
@@ -3337,17 +3434,99 @@ h2o.partialPlot <- function(object, data, cols, destination_key, nbins=20, plot 
     dev.off()
   }
 
-  if(plot) lapply(pps, pp.plot)
+  pp.plot.save2 <- function(pp, nBins=nbins, user_cols=NULL, user_num_splits=NULL) {
+    # If user accidentally provides one of the most common suffixes in R, it is removed.
+    save_to <- gsub(replacement = "",pattern = "(\\.png)|(\\.jpg)|(\\.pdf)", x = save_to)
+    colnames = paste0(names(pp)[1], "_", names(pp)[2])
+    destination_file <- paste0(save_to,"_",colnames,'.png')
+    pp.plot2(pp, nbins, user_cols, user_num_splits)
+    snapshot3d(destination_file)
+    dev.off()
+}
 
-  if(plot && !is.null(save_to)){
-    lapply(pps, pp.plot.save)
+  if(plot && !noCols) lapply(pps[1:numCols], pp.plot) # plot 1d pdp here
+  if(plot && !noCols && !is.null(save_to)){  # save 1d pdp here
+    lapply(pps[1:numCols], pp.plot.save)
+  }
+  
+  if (!noPairs &&
+      ("plot3Drgl" %in% rownames(installed.packages())) &&
+      ("rgl" %in% rownames(installed.packages()))) {
+    library(plot3Drgl)
+    library(rgl)
+    if (plot && !is.null(save_to)) {
+      # plot and save to file
+      if (is.null(user_splits)) {
+        sapply(
+          pps[(numCols + 1):(numCols + numColPairs)],
+          pp.plot.save2,
+          nBins = nbins,
+          user_cols = NULL,
+          user_num_splits = NULL
+        )
+      } else {
+        sapply(
+          pps[(numCols + 1):(numCols + numColPairs)],
+          pp.plot.save2,
+          nBins = nbins,
+          user_cols = user_cols,
+          user_num_splits = user_num_splits
+        )
+      }
+    } else {
+      # only plot
+      if (is.null(user_splits)) {
+        sapply(
+          pps[(numCols + 1):(numCols + numColPairs)],
+          pp.plot2,
+          nBins = nbins,
+          user_cols = NULL,
+          user_num_splits = NULL
+        )
+      } else {
+        sapply(
+          pps[(numCols + 1):(numCols + numColPairs)],
+          pp.plot2,
+          nBins = nbins,
+          user_cols = user_cols,
+          user_num_splits = user_num_splits
+        )
+      }
+    }
+  } else {
+    if (plot && !noPairs)
+      print("Cannot generate 2D partial plots.  Libraries plot3Drgl and rgl are missing")
   }
 
-  if(length( pps) == 1) {
+
+  if(length(pps) == 1) {
     return(pps[[1]])
   } else {
     return(pps)
   }
+}
+
+replaceEnumLevel <- function(originalV, vlevels) {
+  x <- rep(1, length(originalV))
+  for (ind in c(1:length(originalV))) {
+    x[ind] <- which(originalV[ind] == vlevels)
+  }
+  x
+}
+
+reShape<- function(x, y, z, xname, yname, nbin, user_cols, user_num_splits) {
+  ybin <- nbin
+  if(!is.null(user_cols)) {
+    if (yname %in% user_cols) {
+      ybin <- user_num_splits[which(yname==user_cols)]
+    }
+  }
+
+  xbin <- floor(length(x)/ybin)
+  X<-matrix(x, nrow=ybin, ncol=xbin,byrow=F)
+  Y <- matrix(y, nrow=ybin, ncol=xbin, byrow=F)
+  Z <- matrix(z, nrow=ybin, ncol=xbin, byrow=F)
+  list(X,Y,Z)
 }
 
 #' Feature Generation via H2O Deep Learning or DeepWater Model
