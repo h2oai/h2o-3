9b3bd019ce572fac220ba1d4006d9ed46fad2067
michalkurka <michalk@h2o.ai>
2019-06-14 16:13:49 -0700

    SE: Demonstrate an issue with a failing test

--- h2o-algos/src/test/java/hex/ensemble/StackedEnsembleTest.java
+++ h2o-algos/src/test/java/hex/ensemble/StackedEnsembleTest.java
@@ -1035,5 +1035,57 @@ public class StackedEnsembleTest extends TestUtil {
       }
     }
   }
-  
+
+  @Test
+  public void testInvalidFoldColumn_trainingFrame() {
+    GBMModel gbmModel = null;
+    try {
+      Scope.enter();
+
+      final Frame trainingFrame = TestUtil.parse_test_file("./smalldata/iris/iris_wheader.csv");
+      Scope.track(trainingFrame);
+
+      GBMModel.GBMParameters parameters = new GBMModel.GBMParameters();
+      parameters._train = trainingFrame._key;
+      parameters._fold_column = "class";
+      parameters._seed = 0xFEED;
+      parameters._response_column = "petal_len";
+      parameters._ntrees = 1;
+      parameters._keep_cross_validation_predictions = true;
+
+      GBM gbm = new GBM(parameters);
+      gbmModel = gbm.trainModel().get();
+      assertNotNull(gbmModel);
+
+      final Frame seTrain = new Frame(Key.<Frame>make(), trainingFrame.names(), trainingFrame.vecs());
+      Vec foldVec = seTrain.remove("class");
+      seTrain.add("class", foldVec.toStringVec());
+      DKV.put(seTrain);
+      Scope.track(seTrain);
+
+      final StackedEnsembleParameters seParams = new StackedEnsembleParameters();
+      seParams._train = seTrain._key;
+      seParams._response_column = "petal_len";
+      seParams._metalearner_algorithm = Algorithm.AUTO;
+      seParams._base_models = new Key[]{gbmModel._key};
+      seParams._seed = 0x5EED;
+      seParams._metalearner_fold_column = "class";
+
+      expectedException.expect(IllegalArgumentException.class);
+      expectedException.expectMessage("Specified fold column 'class' not found in the data frame: '"
+              + seTrain._key.toString() + "'. Available column names are: [sepal_len, sepal_wid, petal_len, petal_wid]");
+
+      new StackedEnsemble(seParams);
+      fail("Expected the Stack Ensemble Model never to be initialized successfully.");
+    } finally {
+      Scope.exit();
+      if(gbmModel != null){
+        gbmModel.deleteCrossValidationModels();
+        gbmModel.deleteCrossValidationPreds();
+        gbmModel.remove();
+      }
+    }
+  }
+
+
 }
