69777a4e315c1dead5ce2109cece4b837a947d5f
Jakub Háva <jakub.hava@gmail.com>
2019-07-11 09:03:52 +0200

    [PUBDEV-6657] H2O.CLOUD._memary(idx).getTimestamp returns 0 even though it should return valid timestamp of remote node (#3666)

--- h2o-core/src/test/java/water/H2OTest.java
+++ h2o-core/src/test/java/water/H2OTest.java
@@ -5,35 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class H2OTest {
-
-  @Test
-  public void decodeClientInfoNotClient(){
-    short timestamp = H2O.calculateNodeTimestamp(1540375717281L, false);
-    assertEquals(timestamp, 9633);
-    assertFalse(H2O.decodeIsClient(timestamp));
-  }
-
-  @Test
-  public void decodeClientInfoClient(){
-    short timestamp = H2O.calculateNodeTimestamp(1540375717281L, true);
-    assertEquals(timestamp, -9633);
-    assertTrue(H2O.decodeIsClient(timestamp));
-  }
-
-  @Test
-  public void decodeNotClientZeroTimestamp(){
-    short timestamp = H2O.calculateNodeTimestamp(0L, false);
-    assertEquals(timestamp, 1);
-    assertFalse(H2O.decodeIsClient(timestamp));
-  }
-
-  @Test
-  public void decodeClientZeroTimestamp(){
-    short timestamp = H2O.calculateNodeTimestamp(0L, true);
-    assertEquals(timestamp, -1);
-    assertTrue(H2O.decodeIsClient(timestamp));
-  }
-
   @Test
   public void testGetSysProperty() {
     assertEquals("default1", H2O.getSysProperty("test.testGetSysProperty", "default1"));
