e63f1b9bb0a8ec55eb2532ee231a277dc11c7a0a
Pavel Pscheidl <pavel@h2o.ai>
2019-07-12 12:45:18 +0200

    Variable importance calculation - variable names extracted directly from the serialized table. GBM regression Python test.

--- h2o-py/tests/testdir_generic_model/pyunit_generic_model_mojo_gbm.py
+++ h2o-py/tests/testdir_generic_model/pyunit_generic_model_mojo_gbm.py
@@ -124,11 +124,63 @@ def mojo_model_test_binomial():
     generic_mojo_filename = tempfile.mkdtemp("zip", "genericMojo");
     generic_mojo_filename = model.download_mojo(path=generic_mojo_filename)
     assert os.path.getsize(generic_mojo_filename) == os.path.getsize(original_model_filename)
+
+def compare_regression_output(original, generic):
+    original = original[original.find("'Model Summary: '"):].replace('ModelMetricsRegression: gbm','').strip()
+    generic = generic[generic.find("'Model Summary: '"):].replace('ModelMetricsRegression: generic', '').strip()
+    assert generic == original
     
-    
+def mojo_model_test_regression():
+    # GBM
+    airlines = h2o.import_file(path=pyunit_utils.locate("smalldata/testng/airlines_train.csv"))
+    gbm = H2OGradientBoostingEstimator(ntrees=1)
+    gbm.train(x=["Origin", "IsDepDelayed"], y="Distance", training_frame=airlines)
+    print(gbm)
+    with Capturing() as original_output:
+        gbm.show()
+
+    original_model_filename = tempfile.mkdtemp()
+    original_model_filename = gbm.download_mojo(original_model_filename)
+
+    key = h2o.lazy_import(original_model_filename)
+    fr = h2o.get_frame(key[0])
+    model = H2OGenericEstimator(model_key=fr)
+    model.train()
+    print(model)
+    with Capturing() as generic_output:
+        model.show()
+
+    compare_regression_output(str(original_output), str(generic_output))
+
+    predictions = model.predict(airlines)
+    assert predictions is not None
+    assert predictions.nrows == 24421
+    assert model._model_json["output"]["variable_importances"] is not None
+    assert len(model._model_json["output"]["variable_importances"]._cell_values) > 0
+    assert model._model_json["output"]["model_summary"] is not None
+    assert len(model._model_json["output"]["model_summary"]._cell_values) > 0
+
+    # Test constructor generating the model from existing MOJO file
+    model = H2OGenericEstimator.from_file(original_model_filename)
+    assert model is not None
+    predictions = model.predict(airlines)
+    assert predictions is not None
+    assert predictions.nrows == 24421
+    assert model._model_json["output"]["variable_importances"] is not None
+    assert len(model._model_json["output"]["variable_importances"]._cell_values) > 0
+    assert model._model_json["output"]["model_summary"] is not None
+    assert len(model._model_json["output"]["model_summary"]._cell_values) > 0
+
+    generic_mojo_filename = tempfile.mkdtemp("zip", "genericMojo");
+    generic_mojo_filename = model.download_mojo(path=generic_mojo_filename)
+    assert os.path.getsize(generic_mojo_filename) == os.path.getsize(original_model_filename)
+
+
 if __name__ == "__main__":
     pyunit_utils.standalone_test(mojo_model_test_multinomial)
     pyunit_utils.standalone_test(mojo_model_test_binomial)
+    pyunit_utils.standalone_test(mojo_model_test_regression)
 else:
     mojo_model_test_multinomial()
     mojo_model_test_binomial()
+    mojo_model_test_regression()
