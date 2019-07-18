3cd09c4641cc7d5d8ba7671761bf5c115abd50cc
Sebastien Poirier <sebastien@h2o.ai>
2019-07-18 17:25:52 +0200

    generic wrapper working for AutoML

--- h2o-py/h2o/sklearn/__init__.py
+++ h2o-py/h2o/sklearn/__init__.py
@@ -5,18 +5,19 @@ from .. import estimators
 from .pipeline import H2OPipeline, H2OClassifierPipeline, H2ORegressorPipeline
 from .wrapper import estimator, expect_h2o_frames
 
-# H2OAutoMLEstimator = estimator(automl.H2OAutoML, is_generic=True)
-H2OAutoMLClassifier = estimator(automl.H2OAutoML, name="H2OAutoMLClassifier", mixins=(ClassifierMixin,))
-# H2OAutoMLRegressor = estimator(automl.H2OAutoML, name="H2OAutoMLRegressor", mixins=(RegressorMixin,))
+module = __name__
+H2OAutoMLEstimator = estimator(automl.H2OAutoML, is_generic=True)
+H2OAutoMLClassifier = estimator(automl.H2OAutoML, name=module+".H2OAutoMLClassifier", mixins=(ClassifierMixin,))
+H2OAutoMLRegressor = estimator(automl.H2OAutoML, name=module+".H2OAutoMLRegressor", mixins=(RegressorMixin,))
 
 # H2OGradientBoostingEstimator = estimator(estimators.H2OGradientBoostingEstimator)
 # H2OXGBoostEstimator = estimator(estimators.H2OXGBoostEstimator)
 
 __all__ = (
     expect_h2o_frames,
-    # H2OAutoMLEstimator,
+    H2OAutoMLEstimator,
     H2OAutoMLClassifier,
-    # H2OAutoMLRegressor,
+    H2OAutoMLRegressor,
     # H2OPipeline,
     # H2OClassifierPipeline,
     # H2ORegressorPipeline,
--- h2o-py/h2o/sklearn/pipeline.py
+++ h2o-py/h2o/sklearn/pipeline.py
@@ -1,8 +1,8 @@
-from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin, MetaEstimatorMixin, TransformerMixin
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.utils.metaestimators import _BaseComposition
 
-from .wrapper import H2OClusterMixin
+from .wrapper import BaseEstimatorMixin, H2OClusterMixin
 
 
 class H2OPipeline(Pipeline, H2OClusterMixin):
@@ -13,7 +13,7 @@ class H2OPipeline(Pipeline, H2OClusterMixin):
         self.init_cluster(**(cluster_args if cluster_args else {}))
 
 
-class H2OClassifierPipeline(H2OPipeline, ClassifierMixin, MetaEstimatorMixin):
+class H2OClassifierPipeline(H2OPipeline, ClassifierMixin, BaseEstimatorMixin):
 
     def __init__(self, steps, memory=None, cluster_args=None):
         super(H2OClassifierPipeline, self).__init__(steps,
@@ -22,7 +22,7 @@ class H2OClassifierPipeline(H2OPipeline, ClassifierMixin, MetaEstimatorMixin):
                                                     cluster_args=cluster_args)
 
 
-class H2ORegressorPipeline(H2OPipeline, RegressorMixin, MetaEstimatorMixin):
+class H2ORegressorPipeline(H2OPipeline, RegressorMixin, BaseEstimatorMixin):
 
     def __init__(self, steps, memory=None, cluster_args=None):
         super(H2ORegressorPipeline, self).__init__(steps,
