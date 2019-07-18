a30f3c2d75844cda466c9b7066e63ea7a74eeada
Sebastien Poirier <sebastien@h2o.ai>
2019-07-18 14:11:34 +0200

    provide generic wrappers for estimators

--- h2o-py/h2o/estimators/estimator_base.py
+++ h2o-py/h2o/estimators/estimator_base.py
@@ -354,6 +354,7 @@ class H2OEstimator(ModelBase):
 
 
     #------ Scikit-learn Interface Methods -------
+
     def fit(self, X, y=None, **params):
         """
         Fit an H2O model as part of a scikit-learn pipeline or grid search.
