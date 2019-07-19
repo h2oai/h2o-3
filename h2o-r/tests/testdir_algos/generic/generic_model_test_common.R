31b2a743142b1af8ef0623b6364bead5272bf942
Pavel Pscheidl <pavel@h2o.ai>
2019-07-19 13:35:23 +0200

    DRF R Tests. Extracted common output validation logic into a separate file.

---
+++ h2o-r/tests/testdir_algos/generic/generic_model_test_common.R
@@ -0,0 +1,33 @@
+compare_output <- function(original_output, generic_output, skipped_original, skipped_generic){
+  
+  removed_original <- c()
+  for (i in 1:length(original_output)) {
+    line <- original_output[i]
+    
+    for (name in skipped_original) {
+      if(grepl(name, line)){
+        removed_original <- append(removed_original, i)
+      }
+    }
+  }
+  if(length(removed_original) > 0){
+    original_output <- original_output[-removed_original]        
+  }
+  
+  removed_generic <- c()
+  for (i in 1:length(generic_output)) {
+    line <- generic_output[i]
+    for (name in skipped_generic) {
+      if(grepl(name, line)){
+        removed_generic <- append(removed_generic, i)
+      }
+    }
+
+  }
+  if(length(removed_generic) > 0){
+    generic_output <- generic_output[-removed_generic]
+  }
+  print(original_output)
+  print(generic_output)
+  expect_equal(TRUE, all.equal(original_output, generic_output))
+}
