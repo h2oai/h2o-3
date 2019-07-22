f1a3548430403a33050759a9888bfbb1ed668a85
Pavel Pscheidl <pavel@h2o.ai>
2019-07-22 10:16:33 +0200

    PUBDEV-6682 - DRF MOJO output checking (#3716)
    * PUBDEV-6682 - DRF Mojo output Python test. Extracted test logic common with GBM into a separate file.
    
    * DRF R Tests. Extracted common output validation logic into a separate file.
    
    * Python test imports fix.
    
    * Fixed bad NaN handling in ModelJsonReader when reading table.

--- h2o-py/tests/testdir_generic_model/pyunit_generic_model_mojo_gbm.py
+++ h2o-py/tests/testdir_generic_model/pyunit_generic_model_mojo_gbm.py
@@ -1,41 +1,12 @@
 import h2o
 import tempfile
 import os
+
 from h2o.estimators import H2OGradientBoostingEstimator, H2OGenericEstimator
 from tests import pyunit_utils
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
-import sys
+from tests.testdir_generic_model import Capturing, compare_regression_output, compare_binomial_output, \
+    compare_multinomial_output
 
-# Required to work both in Py 2 & 3
-class Capturing(list):
-    def __enter__(self):
-        self._stdout = sys.stdout
-        sys.stdout = self._stringio = StringIO()
-        return self
-    def __exit__(self, *args):
-        self.extend(self._stringio.getvalue().splitlines())
-        del self._stringio    # free up some memory
-        sys.stdout = self._stdout
-        
-def compare_multinomial_output(original, generic):
-    original = original[original.find("'Model Summary: '"):].replace('ModelMetricsMultinomial: gbm','').strip()
-    generic = generic[generic.find("'Model Summary: '"):].replace('ModelMetricsMultinomialGeneric: generic', '').strip()
-    assert generic == original
-
-
-def compare_binomial_output(original, generic):
-    original = original[original.find("'Model Summary: '"):].replace('ModelMetricsBinomial: gbm','').strip()
-    generic = generic[generic.find("'Model Summary: '"):].replace('ModelMetricsBinomialGeneric: generic', '').strip()
-    assert generic == original
-    
-
-def compare_regression_output(original, generic):
-    original = original[original.find("'Model Summary: '"):].replace('ModelMetricsRegression: gbm','').strip()
-    generic = generic[generic.find("'Model Summary: '"):].replace('ModelMetricsRegression: generic', '').strip()
-    assert generic == original
 
 def test(x, y, output_test):
     airlines = h2o.import_file(path=pyunit_utils.locate("smalldata/testng/airlines_train.csv"))
@@ -56,7 +27,7 @@ def test(x, y, output_test):
     with Capturing() as generic_output:
         model.show()
 
-    output_test(str(original_output), str(generic_output))
+    output_test(str(original_output), str(generic_output), 'gbm')
     
     predictions = model.predict(airlines)
     assert predictions is not None
