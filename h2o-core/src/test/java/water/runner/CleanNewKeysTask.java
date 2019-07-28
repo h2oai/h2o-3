df02d190fd8b6e6a483f95d376df562a99bea4f5
Pavel Pscheidl <pavel@h2o.ai>
2019-07-28 21:27:46 +0200

    Use DKVManager to clean newly added keys.

--- h2o-core/src/test/java/water/runner/CleanNewKeysTask.java
+++ h2o-core/src/test/java/water/runner/CleanNewKeysTask.java
@@ -10,21 +10,7 @@ public class CleanNewKeysTask extends KeysMRTask<CleanNewKeysTask> {
 
     @Override
     protected void setupLocal() {
-        final Set<Key> initKeys = LocalTestRuntime.initKeys;
-        final Set<Key> actualKeys = H2O.localKeySet();
-        for (Key actualKey : actualKeys){
-
-            final Value value = Value.STORE_get(actualKey);
-            if (initKeys.contains(actualKey) || isIgnorableKeyLeak(actualKey, value)) continue;
-            if (!(value.get() instanceof Keyed)) {
-                // Keyed objects might override remove_impl to excerscise their own removal strategy
-                // Non-keyed objects should just be removed from the DKV
-                DKV.remove(actualKey);
-            } else {
-                actualKey.remove();
-            }
-        }
-        
+        DKVManager.retain(LocalTestRuntime.initKeys.toArray(new Key[0]));
     }
 
 }
