6b5dca88173d4e4ab5ca1f064f8de133f8288897
maurever <veronika@maurerova.cz>
2019-07-29 16:28:59 +0200

    PUBDEV-6725 add init_for_pipeline method, add pipeline tests, refactor pca test

--- h2o-py/h2o/estimators/svd.py
+++ h2o-py/h2o/estimators/svd.py
@@ -260,4 +260,16 @@ class H2OSingularValueDecompositionEstimator(H2OEstimator):
         assert_is_type(export_checkpoints_dir, None, str)
         self._parms["export_checkpoints_dir"] = export_checkpoints_dir
 
+    def init_for_pipeline(self):
+        """
+        Returns H2OSVD object which implements fit and transform method to be used in sklearn.Pipeline properly.
+        All parameters defined in self.__params, should be input parameters in H2OSVD.__init__ method.
 
+        :returns: H2OSVD object
+        """
+        import inspect
+        from h2o.transforms.decomposition import H2OSVD
+        # check which parameters can be passed to H2OSVD init
+        var_names = list(dict(inspect.getmembers(H2OSVD.__init__.__code__))['co_varnames'])
+        parameters = {k: v for k, v in self._parms.items() if k in var_names}
+        return H2OSVD(**parameters)
