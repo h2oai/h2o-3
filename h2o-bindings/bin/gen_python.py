1facfd4fde23ddfc4adf797a0dbeff7b432a9241
Veronika Maurerová <maurever@users.noreply.github.com>
2019-08-07 16:47:22 +0000

    PUBDEV-6725 fix svd transform pipeline (#3758)
    Add init_for_pipeline method, add pipeline tests, refactor pca test

--- h2o-bindings/bin/gen_python.py
+++ h2o-bindings/bin/gen_python.py
@@ -605,6 +605,22 @@ def class_extra_for(algo):
             parameters = {k: v for k, v in self._parms.items() if k in var_names}
             return H2OPCA(**parameters)
         """
+    if algo == "svd":
+        return """
+        def init_for_pipeline(self):
+            \"\"\"
+            Returns H2OSVD object which implements fit and transform method to be used in sklearn.Pipeline properly.
+            All parameters defined in self.__params, should be input parameters in H2OSVD.__init__ method.
+
+            :returns: H2OSVD object
+            \"\"\"
+            import inspect
+            from h2o.transforms.decomposition import H2OSVD
+            # check which parameters can be passed to H2OSVD init
+            var_names = list(dict(inspect.getmembers(H2OSVD.__init__.__code__))['co_varnames'])
+            parameters = {k: v for k, v in self._parms.items() if k in var_names}
+            return H2OSVD(**parameters)
+        """
     elif algo == "coxph":
         return """
         def _additional_used_columns(self, parms):
