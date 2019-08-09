f4cbb7233a20db29463f6973daeede81d632645c
Michal Raška <michal.raska@gmail.com>
2019-08-09 16:29:30 +0200

    Build mojocompat image. Bump Java 12 links.

--- scripts/jenkins/groovy/buildConfig.groovy
+++ scripts/jenkins/groovy/buildConfig.groovy
@@ -14,7 +14,7 @@ class BuildConfig {
   private static final String DEFAULT_HADOOP_IMAGE_NAME_PREFIX = 'dev-build-hadoop-gradle'
   private static final String DEFAULT_RELEASE_IMAGE_NAME_PREFIX = 'dev-release-gradle'
 
-  public static final int DEFAULT_IMAGE_VERSION_TAG = 14
+  public static final int DEFAULT_IMAGE_VERSION_TAG = 16
   public static final String AWSCLI_IMAGE = DOCKER_REGISTRY + '/opsh2oai/awscli'
   public static final String S3CMD_IMAGE = DOCKER_REGISTRY + '/opsh2oai/s3cmd'
 
