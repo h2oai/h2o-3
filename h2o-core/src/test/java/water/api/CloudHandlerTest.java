29125ff70dd8cefa3fe84765ff61185335294f86
Jakub Hava <jakub.hava@gmail.com>
2019-08-26 12:33:42 +0200

    Add test

---
+++ h2o-core/src/test/java/water/api/CloudHandlerTest.java
@@ -0,0 +1,24 @@
+package water.api;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import water.TestUtil;
+import water.api.schemas3.CloudV3;
+
+import static org.junit.Assert.assertNotNull;
+
+public class CloudHandlerTest extends TestUtil {
+
+  @BeforeClass
+  public static void beforeClass() {
+    TestUtil.stall_till_cloudsize(1);
+  }
+
+  @Test
+  public void status() {
+    final CloudHandler cloudHandler = new CloudHandler();
+    final CloudV3 status = cloudHandler.status(3, new CloudV3());
+    assertNotNull(status);
+    assertNotNull(status.leader);
+  }
+}
