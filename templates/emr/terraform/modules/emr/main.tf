613d13707c9e73f4b539bf4728da990562a5cfc0
michalkurka <michalk@h2o.ai>
2019-08-27 10:06:44 -0700

    Fix a typo

--- templates/emr/terraform/modules/emr/main.tf
+++ templates/emr/terraform/modules/emr/main.tf
@@ -39,7 +39,7 @@ cd /home/hadoop/h2o
 wget http://h2o-release.s3.amazonaws.com/h2o/rel-${var.h2o_codename}/${var.h2o_fix_version}/h2o-${var.h2o_main_version}.${var.h2o_fix_version}-hdp2.6.zip
 unzip -o h2o*.zip 1> /dev/null & wait
 aws s3 cp ${format("s3://%s/realm.properties", aws_s3_bucket.h2o_bucket.bucket)} realm.properties
-keytool -genkey -keyalg RSA -keystore h2o.jks -keypass h2oh2o -storepass h2oh2o -keysize 2048 -dname "CN=Jonh Smith, OU=H2O, O=H2O.ai, L=Mountain View, S=California, C=US"
+keytool -genkey -keyalg RSA -keystore h2o.jks -keypass h2oh2o -storepass h2oh2o -keysize 2048 -dname "CN=John Smith, OU=H2O, O=H2O.ai, L=Mountain View, S=California, C=US"
 
 EOF
 }
