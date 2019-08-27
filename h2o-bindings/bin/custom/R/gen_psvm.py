891edd47aaa7c1510a87c1c6eeadfcd4ea1b3e44
Sebastien Poirier <sebastien@h2o.ai>
2019-08-27 15:47:30 +0200

    fixed psvm R doc after rebase

--- h2o-bindings/bin/custom/R/gen_psvm.py
+++ h2o-bindings/bin/custom/R/gen_psvm.py
@@ -13,4 +13,10 @@ Trains a Support Vector Machine model on an H2O dataset
 
 Alpha version. Supports only binomial classification problems. 
 """,
+    params=dict(
+        y="""
+The name or column index of the response variable in the data. The response must be either a binary
+categorical/factor variable or a numeric variable with values -1/1 (for compatibility with SVMlight format).
+"""
+    ),
 )
