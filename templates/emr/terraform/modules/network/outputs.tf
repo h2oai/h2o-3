71cd8628ee6f0b7e5c6f9b4c256c4ac001c7ffe5
michalkurka <michalk@h2o.ai>
2019-08-29 11:30:04 -0700

    EMR Terraform: Setup EMR on a private subnet expose Flow using load balancer

--- templates/emr/terraform/modules/network/outputs.tf
+++ templates/emr/terraform/modules/network/outputs.tf
@@ -10,6 +10,14 @@ output "aws_subnet_id" {
   value = "${aws_subnet.main.id}"
 }
 
+output "aws_subnet_public_id" {
+  value = "${aws_subnet.public.id}"
+}
+
+output "aws_subnet_public2_id" {
+  value = "${aws_subnet.public2.id}"
+}
+
 output "aws_vpc_cidr_block" {
   value = "${aws_vpc.main.cidr_block}"
 }
