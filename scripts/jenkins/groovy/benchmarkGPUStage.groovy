2ac8a6bc6e8904fa39f1904dccf9e3489adfc318
Jan Sterba <jan.sterba@h2o.ai>
2019-09-11 14:09:49 +0200

    install r package

--- scripts/jenkins/groovy/benchmarkGPUStage.groovy
+++ scripts/jenkins/groovy/benchmarkGPUStage.groovy
@@ -30,7 +30,6 @@ def call(final pipelineContext, final stageConfig) {
         
         locale
         env
-        
         cd ${WORKSPACE}/${h2o3dir}
         
         pwd
@@ -51,6 +50,9 @@ def call(final pipelineContext, final stageConfig) {
         export BENCHMARK_ALGORITHM=${stageConfig.customData.algorithm}
         export BUILD_ID=${env.BUILD_ID}
         export H2O_JAR_PATH=${H2O_ROOT}/build/h2o.jar
+        
+        echo "Installing R package"
+        R CMD INSTALL ${h2o3dir}/h2o-r/R/src/contrib/h2o*.tar.gz
         
         echo "Running Make"
         
