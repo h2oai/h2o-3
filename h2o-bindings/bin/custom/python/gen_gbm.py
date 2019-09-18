03b343f9a64f6e66b67e76328dbb49bc25dace8c
Angela Bartz <angela@h2o.ai>
2019-09-17 17:40:21 -0700

    adding nfolds example

--- h2o-bindings/bin/custom/python/gen_gbm.py
+++ h2o-bindings/bin/custom/python/gen_gbm.py
@@ -41,6 +41,19 @@ examples = dict(
 ...                validation_frame = valid)
 >>> cars_gbm.auc(valid=True)
 """,
+    nfolds="""
+>>> cars = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/junit/cars_20mpg.csv")
+>>> cars["economy_20mpg"] = cars["economy_20mpg"].asfactor()
+>>> predictors = ["displacement","power","weight","acceleration","year"]
+>>> response = "economy_20mpg"
+>>> folds = 5
+>>> cars_gbm = H2OGradientBoostingEstimator(nfolds=folds,
+...                                         seed = 1234
+>>> cars_gbm.train(x=predictors,
+...                y=response,
+...                training_frame=cars)
+>>> cars_gbm.auc()
+""",
     keep_cross_validation_models="""
 >>> cars = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/junit/cars_20mpg.csv")
 >>> cars["economy_20mpg"] = cars["economy_20mpg"].asfactor()
