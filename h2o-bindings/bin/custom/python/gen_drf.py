2c7372155d614af001671079eeaddd4e35386c7d
Angela Bartz <angela@h2o.ai>
2019-09-18 13:15:40 -0700

    updated description

--- h2o-bindings/bin/custom/python/gen_drf.py
+++ h2o-bindings/bin/custom/python/gen_drf.py
@@ -2,14 +2,8 @@ deprecated = ['offset_column', 'distribution']
 
 doc = dict(
     __class__="""
-Builds a Distributed Random Forest (DRF) on a parsed dataset, for regression or classification.
-DRF is a powerful classification and regression tool. When given a set of data,
-DRF generates a forest of classification or regression trees, rather than a single classification or regression tree.
-Each of these trees is a weak learner built on a subset of rows and columns.
-More trees will reduce the variance. Both classification and regression take the average prediction over all of their trees to make a final prediction,
-whether predicting for a class or numeric value. (Note: For a categorical response column,
-DRF maps factors (e.g. ‘dog’, ‘cat’, ’mouse) in lexicographic order to a name lookup array with integer indices
-(e.g. ’cat -> 0, ‘dog’ -> 1, ‘mouse’ -> 2.)
+Builds a Distributed Random Forest (DRF) on a parsed dataset, for regression or 
+classification. 
 """
 )
 
