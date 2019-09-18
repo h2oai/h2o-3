5a5492d6ba5d097683f8bd8b539e84ce784681d3
Angela Bartz <angela@h2o.ai>
2019-09-17 17:34:03 -0700

    one more test

--- h2o-bindings/bin/custom/python/gen_gbm.py
+++ h2o-bindings/bin/custom/python/gen_gbm.py
@@ -41,18 +41,20 @@ examples = dict(
 ...                validation_frame = valid)
 >>> cars_gbm.auc(valid=True)
 """,
-    nfolds="""
+    keep_cross_validation_models="""
 >>> cars = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/junit/cars_20mpg.csv")
 >>> cars["economy_20mpg"] = cars["economy_20mpg"].asfactor()
 >>> predictors = ["displacement","power","weight","acceleration","year"]
 >>> response = "economy_20mpg"
->>> train, valid = cars.split_frame(ratios = [.8], seed = 1234)
 >>> folds = 5
->>> cars_gbm = H2OGradientBoostingEstimator(nfolds = folds, seed = 1234)
->>> cars_gbm.train(x = predictors,
-...                y = response,
-...                training_frame = train,
-...                validation_frame = valid)
+>>> train, valid = cars.split_frame(ratios = [.8], seed = 1234)
+>>> cars_gbm = H2OGradientBoostingEstimator(keep_cross_validation_models = True,
+...                                         nfolds=folds,
+...                                         seed=1234)
+>>> cars_gbm.train(x=predictors,
+...                y=response,
+...                training_frame=train,
+...                validation_frame=valid)
 >>> cars_gbm.auc()
 """
 )
