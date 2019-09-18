a27064fa23ffb198f3e72363d9ec6c602dd5978f
Hannah Tillman <hannah.tillman@h2o.ai>
2019-09-18 11:50:28 -0700

    PUBDEV-6675: testing

--- h2o-bindings/bin/custom/python/gen_drf.py
+++ h2o-bindings/bin/custom/python/gen_drf.py
@@ -1,14 +1,18 @@
 deprecated = ['offset_column', 'distribution']
 
-doc = dict(
-    __class__="""
-Builds a Distributed Random Forest (DRF) on a parsed dataset, for regression or classification.
-DRF is a powerful classification and regression tool. When given a set of data,
-DRF generates a forest of classification or regression trees, rather than a single classification or regression tree.
-Each of these trees is a weak learner built on a subset of rows and columns.
-More trees will reduce the variance. Both classification and regression take the average prediction over all of their trees to make a final prediction,
-whether predicting for a class or numeric value. (Note: For a categorical response column,
-DRF maps factors (e.g. ‘dog’, ‘cat’, ’mouse) in lexicographic order to a name lookup array with integer indices
-(e.g. ’cat -> 0, ‘dog’ -> 1, ‘mouse’ -> 2.)
+examples = dict(
+    training_frame="""
+>>> cars = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/junit/cars_20mpg.csv")
+>>> cars["economy_20mpg"] = cars["economy_20mpg"].asfactor()
+>>> predictors = ["displacement","power","weight","acceleration","year"]
+>>> response = "economy_20mpg"
+>>> train, valid = cars.split_frame(ratios = [.8],
+...                                 seed = 1234)
+>>> cars_drf = H2ORandomForestEstimator(seed = 1234)
+>>> cars_drf.train(x=predictors,
+...                y=response,
+...                training_frame=train,
+...                validation_frame=valid)
+>>> cars_drf.auc(valid=True)
 """
 )
