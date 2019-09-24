554340a14cf91bb034f0ad4cd2b7c30e10cc578a
Jan Sterba <jan.sterba@h2o.ai>
2019-09-24 11:56:16 +0200

    gblinear and cleanup

--- h2o-extensions/xgboost/src/main/java/hex/tree/xgboost/XGBoostPojoWriter.java
+++ h2o-extensions/xgboost/src/main/java/hex/tree/xgboost/XGBoostPojoWriter.java
@@ -2,6 +2,7 @@ package hex.tree.xgboost;
 
 import biz.k11i.xgboost.Predictor;
 import biz.k11i.xgboost.gbm.Dart;
+import biz.k11i.xgboost.gbm.GBLinear;
 import biz.k11i.xgboost.gbm.GBTree;
 import biz.k11i.xgboost.tree.RegTree;
 import biz.k11i.xgboost.tree.RegTreeNode;
@@ -26,14 +27,14 @@ public abstract class XGBoostPojoWriter {
         if (p.getBooster() instanceof GBTree) {
             return new XGBoostPojoTreeWriter(p, namePrefix, output, defaultThreshold);
         } else {
-            throw new UnsupportedOperationException("GBLiear not supported.");
+            return new XGBoostPojoLinearWriter(p, namePrefix, output, defaultThreshold);
         }
     }
 
     protected final Predictor _p;
     protected final String _namePrefix;
     protected final XGBoostOutput _output;
-    protected final double _defaultThreshold;
+    private final double _defaultThreshold;
 
     protected XGBoostPojoWriter(Predictor p, String namePrefix, XGBoostOutput output, double defaultThreshold) {
         _p = p;
@@ -41,73 +42,17 @@ public abstract class XGBoostPojoWriter {
         _output = output;
         _defaultThreshold = defaultThreshold;
     }
-
-    private void renderTree(JCodeSB sb, RegTree tree, int nidx) {
-        RegTreeNode node = tree.getNodes()[nidx];
-        if (node.isLeaf()) {
-            sb.ip("").pj(node.getLeafValue());
+    
+    protected String getFeatureAccessor(int idx) {
+        if (idx >= _output._catOffsets[_output._cats]) {
+            int colIdx = idx - _output._catOffsets[_output._cats] + _output._cats;
+            return "data[" + colIdx + "]";
         } else {
-            String accessor;
-            if (node.getSplitIndex() >= _output._catOffsets[_output._cats]) {
-                int colIdx = node.getSplitIndex() - _output._catOffsets[_output._cats] + _output._cats;
-                accessor = "data[" + colIdx + "]";
-            } else {
-                int colIdx = 0;
-                while (node.getSplitIndex() >= _output._catOffsets[colIdx + 1]) colIdx++;
-                int colValue = node.getSplitIndex() - _output._catOffsets[colIdx];
-                accessor = "(data[" + colIdx + "] == " + colValue + " ? 1 : " + (_output._sparse ? "NaN" : "0") + ")";
-            }
-            String operator;
-            int trueChild;
-            int falseChild;
-            if (node.default_left()) {
-                operator = " < ";
-                trueChild = node.getLeftChildIndex();
-                falseChild = node.getRightChildIndex();
-            } else {
-                operator = " >= ";
-                trueChild = node.getRightChildIndex();
-                falseChild = node.getLeftChildIndex();
-            }
-            sb.ip("((Double.isNaN(").p(accessor).p(") || ").p(accessor).p(operator).pj(node.getSplitCondition()).p(") ?").nl();
-            sb.ii(1);
-            renderTree(sb, tree, trueChild);
-            sb.nl().ip(":").nl();
-            renderTree(sb, tree, falseChild);
-            sb.di(1);
-            sb.nl().ip(")");
+            int colIdx = 0;
+            while (idx >= _output._catOffsets[colIdx + 1]) colIdx++;
+            int colValue = idx - _output._catOffsets[colIdx];
+            return "(data[" + colIdx + "] == " + colValue + " ? 1 : " + (_output._sparse ? "NaN" : "0") + ")";
         }
-    }
-
-    protected String renderTreeClass(
-        RegTree[][] trees,
-        final int gidx,
-        final int tidx,
-        final Dart dart,
-        CodeGeneratorPipeline fileCtx
-    ) {
-        final RegTree tree = trees[gidx][tidx];
-        final String className = _namePrefix + "_Tree_g_" + gidx + "_t_" + tidx;
-        fileCtx.add(new CodeGenerator() {
-            @Override
-            public void generate(JCodeSB sb) {
-                sb.nl().p("class ").p(className).p(" {").nl();
-                sb.ii(1);
-                sb.ip("static float score0(double[] data) {").nl();
-                sb.ii(1);
-                sb.ip("return ");
-                if (dart != null) {
-                    sb.pj(dart.weight(tidx)).p(" * ");
-                }
-                renderTree(sb, tree, 0);
-                sb.p(";").nl();
-                sb.di(1);
-                sb.ip("}").nl();
-                sb.di(1);
-                sb.ip("}").nl();
-            }
-        });
-        return className;
     }
 
     private void renderPredTransformViaLinkFunction(LinkFunctionType type, SBPrintStream sb) {
@@ -128,7 +73,7 @@ public abstract class XGBoostPojoWriter {
         sb.ip("}").nl();
     }
 
-    protected void renderPredTransform(SBPrintStream sb) {
+    private void renderPredTransform(SBPrintStream sb) {
         String objFunction = _p.getObjName();
         if (ObjectiveType.REG_GAMMA.getId().equals(objFunction) ||
             ObjectiveType.REG_TWEEDIE.getId().equals(objFunction) ||
@@ -145,36 +90,8 @@ public abstract class XGBoostPojoWriter {
             throw new IllegalArgumentException("Unexpected objFunction " + objFunction);
         }
     }
-
-    public abstract void renderJavaPredictBody(SBPrintStream sb, CodeGeneratorPipeline fileCtx);
-
-}
-
-class XGBoostPojoTreeWriter extends XGBoostPojoWriter {
-
-    protected XGBoostPojoTreeWriter(Predictor p, String namePrefix, XGBoostOutput output, double defaultThreshold) {
-        super(p, namePrefix, output, defaultThreshold);
-    }
-
-    @Override
-    public void renderJavaPredictBody(SBPrintStream sb, CodeGeneratorPipeline fileCtx) {
-        GBTree booster = (GBTree) _p.getBooster();
-        Dart dartBooster = null;
-        if (booster instanceof Dart) {
-            dartBooster = (Dart) booster;
-        }
-        RegTree[][] trees = booster.getGroupedTrees();
-        for (int gidx = 0; gidx < trees.length; gidx++) {
-            sb.ip("preds[").p(gidx).p("] = ").nl();
-            sb.ii(1);
-            for (int tidx = 0; tidx < trees[gidx].length; tidx++) {
-                String treeClassName = renderTreeClass(trees, gidx, tidx, dartBooster, fileCtx);
-                sb.ip(treeClassName).p(".score0(data)").p(" + ").nl();
-            }
-            sb.ip("").pj(_p.getBaseScore()).p(";").nl();
-            sb.di(1);
-        }
-        renderPredTransform(sb);
+    
+    private void renderPredPostProcess(SBPrintStream sb) {
         if (_output.nclasses() > 2) {
             sb.ip("for (int i = preds.length-2; i >= 0; i--)").nl();
             sb.ip("  preds[1 + i] = preds[i];").nl();
@@ -185,5 +102,123 @@ class XGBoostPojoTreeWriter extends XGBoostPojoWriter {
             sb.ip("preds[0] = GenModel.getPrediction(preds, PRIOR_CLASS_DISTRIB, data, ").pj(_defaultThreshold).p(");").nl();
         }
     }
-    
+
+    public void renderJavaPredictBody(SBPrintStream sb, CodeGeneratorPipeline fileCtx) {
+        renderComputePredict(sb, fileCtx);
+        renderPredTransform(sb);
+        renderPredPostProcess(sb);
+    }
+
+    protected abstract void renderComputePredict(SBPrintStream sb, CodeGeneratorPipeline fileCtx);
+
+    static class XGBoostPojoTreeWriter extends XGBoostPojoWriter {
+
+        protected XGBoostPojoTreeWriter(Predictor p, String namePrefix, XGBoostOutput output, double defaultThreshold) {
+            super(p, namePrefix, output, defaultThreshold);
+        }
+
+        @Override
+        public void renderComputePredict(SBPrintStream sb, CodeGeneratorPipeline fileCtx) {
+            GBTree booster = (GBTree) _p.getBooster();
+            Dart dartBooster = null;
+            if (booster instanceof Dart) {
+                dartBooster = (Dart) booster;
+            }
+            RegTree[][] trees = booster.getGroupedTrees();
+            for (int gidx = 0; gidx < trees.length; gidx++) {
+                sb.ip("preds[").p(gidx).p("] =").nl();
+                sb.ii(1);
+                for (int tidx = 0; tidx < trees[gidx].length; tidx++) {
+                    String treeClassName = renderTreeClass(trees, gidx, tidx, dartBooster, fileCtx);
+                    sb.ip(treeClassName).p(".score0(data)").p(" + ").nl();
+                }
+                sb.ip("").pj(_p.getBaseScore()).p(";").nl();
+                sb.di(1);
+            }
+        }
+
+        private String renderTreeClass(
+            RegTree[][] trees,
+            final int gidx,
+            final int tidx,
+            final Dart dart,
+            CodeGeneratorPipeline fileCtx
+        ) {
+            final RegTree tree = trees[gidx][tidx];
+            final String className = _namePrefix + "_Tree_g_" + gidx + "_t_" + tidx;
+            fileCtx.add(new CodeGenerator() {
+                @Override
+                public void generate(JCodeSB sb) {
+                    sb.nl().p("class ").p(className).p(" {").nl();
+                    sb.ii(1);
+                    sb.ip("static float score0(double[] data) {").nl();
+                    sb.ii(1);
+                    sb.ip("return ");
+                    if (dart != null) {
+                        sb.pj(dart.weight(tidx)).p(" * ");
+                    }
+                    renderTree(sb, tree, 0);
+                    sb.p(";").nl();
+                    sb.di(1);
+                    sb.ip("}").nl();
+                    sb.di(1);
+                    sb.ip("}").nl();
+                }
+            });
+            return className;
+        }
+
+        private void renderTree(JCodeSB sb, RegTree tree, int nidx) {
+            RegTreeNode node = tree.getNodes()[nidx];
+            if (node.isLeaf()) {
+                sb.ip("").pj(node.getLeafValue());
+            } else {
+                String accessor = getFeatureAccessor(node.getSplitIndex());
+                String operator;
+                int trueChild;
+                int falseChild;
+                if (node.default_left()) {
+                    operator = " < ";
+                    trueChild = node.getLeftChildIndex();
+                    falseChild = node.getRightChildIndex();
+                } else {
+                    operator = " >= ";
+                    trueChild = node.getRightChildIndex();
+                    falseChild = node.getLeftChildIndex();
+                }
+                sb.ip("((Double.isNaN(").p(accessor).p(") || ").p(accessor).p(operator).pj(node.getSplitCondition()).p(") ?").nl();
+                sb.ii(1);
+                renderTree(sb, tree, trueChild);
+                sb.nl().ip(":").nl();
+                renderTree(sb, tree, falseChild);
+                sb.di(1);
+                sb.nl().ip(")");
+            }
+        }
+
+    }
+
+    static class XGBoostPojoLinearWriter extends XGBoostPojoWriter {
+
+        protected XGBoostPojoLinearWriter(Predictor p, String namePrefix, XGBoostOutput output, double defaultThreshold) {
+            super(p, namePrefix, output, defaultThreshold);
+        }
+
+        @Override
+        public void renderComputePredict(SBPrintStream sb, CodeGeneratorPipeline fileCtx) {
+            GBLinear booster = (GBLinear) _p.getBooster();
+            for (int gidx = 0; gidx < booster.getNumOutputGroup(); gidx++) {
+                sb.ip("preds[").p(gidx).p("] =").nl();
+                sb.ii(1);
+                for (int fid = 0; fid < booster.getNumFeature(); fid++) {
+                    String accessor = getFeatureAccessor(fid);
+                    sb.ip("(Double.isNaN(").p(accessor).p(") ? 0 : (").pj(booster.weight(fid, gidx)).p(" * ").p(accessor).p(")) + ").nl();
+                }
+                sb.ip("").pj(booster.bias(gidx)).p(" +").nl();
+                sb.ip("").pj(_p.getBaseScore()).p(";").nl();
+                sb.di(1);
+            }
+        }
+    }
+
 }
--- h2o-py/tests/testdir_algos/xgboost/pyunit_xgboost_pojo.py
+++ h2o-py/tests/testdir_algos/xgboost/pyunit_xgboost_pojo.py
@@ -5,7 +5,7 @@ from h2o.estimators.xgboost import *
 from tests import pyunit_utils
 
 
-def compare_preds(train, test, x, y, booster, ntrees, max_depth):
+def compare_preds(train, test, x, y, booster, ntrees, max_depth, max_error):
     model = H2OXGBoostEstimator(
         booster=booster, seed=1, 
         ntrees=ntrees, max_depth=max_depth
@@ -22,10 +22,10 @@ def compare_preds(train, test, x, y, booster, ntrees, max_depth):
     h2o.download_csv(pred_h2o, os.path.join(tmp_dir, "out_h2o.csv"))
     pred_pojo = pyunit_utils.pojo_predict(model, tmp_dir, mojo_name)
     print("%s: Comparing pojo %s predict and h2o predict..." % (model._id, booster))
-    pyunit_utils.compare_frames_local(pred_h2o, pred_pojo, 1, tol=1e-10)
+    pyunit_utils.compare_frames_local(pred_h2o, pred_pojo, 1, tol=max_error)
 
 
-def test_booster(name):
+def test_booster(booster, max_error=1e-10):
     assert H2OXGBoostEstimator.available()
 
     # prostate - regression without categoricals
@@ -33,18 +33,36 @@ def test_booster(name):
     prostate_train, prostate_test = prostate_data.split_frame(ratios=[.8], seed=1)
     prostate_x = ['AGE', 'RACE', 'DPROS', 'DCAPS']
     prostate_y = 'CAPSULE'
-    compare_preds(prostate_train, prostate_test, prostate_x, prostate_y, name, ntrees=20, max_depth=3)
-    compare_preds(prostate_train, prostate_test, prostate_x, prostate_y, name, ntrees=1, max_depth=10)
-    compare_preds(prostate_train, prostate_test, prostate_x, prostate_y, name, ntrees=12, max_depth=12)
+    compare_preds(
+        prostate_train, prostate_test, prostate_x, prostate_y, 
+        booster, ntrees=20, max_depth=3, max_error=max_error
+    )
+    compare_preds(
+        prostate_train, prostate_test, prostate_x, prostate_y, 
+        booster, ntrees=1, max_depth=10, max_error=max_error
+    )
+    compare_preds(
+        prostate_train, prostate_test, prostate_x, prostate_y, 
+        booster, ntrees=12, max_depth=12, max_error=max_error
+    )
 
     # insurance - regression with categoricals
     insurance_train = h2o.import_file(pyunit_utils.locate("smalldata/testng/insurance_train1.csv"))
     insurance_test = h2o.import_file(pyunit_utils.locate("smalldata/testng/insurance_validation1.csv"))
     insurance_x = ['Age', 'District', 'Group', 'Holders']
     insurance_y = 'Claims'
-    compare_preds(insurance_train, insurance_test, insurance_x, insurance_y, name, ntrees=20, max_depth=3)
-    compare_preds(insurance_train, insurance_test, insurance_x, insurance_y, name, ntrees=1, max_depth=10)
-    compare_preds(insurance_train, insurance_test, insurance_x, insurance_y, name, ntrees=12, max_depth=12)
+    compare_preds(
+        insurance_train, insurance_test, insurance_x, insurance_y, 
+        booster, ntrees=20, max_depth=3, max_error=max_error
+    )
+    compare_preds(
+        insurance_train, insurance_test, insurance_x, insurance_y, 
+        booster, ntrees=1, max_depth=10, max_error=max_error
+    )
+    compare_preds(
+        insurance_train, insurance_test, insurance_x, insurance_y, 
+        booster, ntrees=12, max_depth=12, max_error=max_error
+    )
 
     # cars classification (binomial)
     cars_data = h2o.import_file(path=pyunit_utils.locate("smalldata/junit/cars_20mpg.csv"))
@@ -53,25 +71,37 @@ def test_booster(name):
     cars_data[cars_data[cars_y1].isna(), cars_y1] = 0
     cars_data[cars_y1] = cars_data[cars_y1].asfactor()
     cars_train, cars_test = cars_data.split_frame(ratios=[.8], seed=1)
-    compare_preds(cars_train, cars_test, cars_x, cars_y1, name, ntrees=1, max_depth=12)
-    compare_preds(cars_train, cars_test, cars_x, cars_y1, name, ntrees=12, max_depth=6)
+    compare_preds(
+        cars_train, cars_test, cars_x, cars_y1, 
+        booster, ntrees=1, max_depth=12, max_error=max_error
+    )
+    compare_preds(
+        cars_train, cars_test, cars_x, cars_y1, 
+        booster, ntrees=12, max_depth=6, max_error=max_error
+    )
 
     # cars classification (binomial)
     cars_y2 = "cylinders"
     cars_data[cars_data[cars_y2].isna(), cars_y2] = 0
     cars_data[cars_y2] = cars_data[cars_y2].asfactor()
     cars_train, cars_test = cars_data.split_frame(ratios=[.8], seed=1)
-    compare_preds(cars_train, cars_test, cars_x, cars_y2, name, ntrees=1, max_depth=12)
-    compare_preds(cars_train, cars_test, cars_x, cars_y2, name, ntrees=12, max_depth=6)
+    compare_preds(
+        cars_train, cars_test, cars_x, cars_y2, 
+        booster, ntrees=1, max_depth=12, max_error=max_error
+    )
+    compare_preds(
+        cars_train, cars_test, cars_x, cars_y2, 
+        booster, ntrees=12, max_depth=6, max_error=max_error
+    )
 
 
-def test_xgboost_mojo_pojo():
+def test_xgboost_pojo():
     test_booster("gbtree")
     test_booster("dart")
-    #test_booster("gblinear")
+    test_booster("gblinear", 1e-5)
 
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(test_xgboost_mojo_pojo)
+    pyunit_utils.standalone_test(test_xgboost_pojo)
 else:
-    test_xgboost_mojo_pojo()
+    test_xgboost_pojo()
