0b3c7f828dc2fd81c4f7e4f556586db5fcceee65
Pavel Pscheidl <pavel@h2o.ai>
2019-10-09 15:53:55 +0200

    ColumnSpecifier required column name not to be null in all cases.

--- h2o-genmodel/src/main/java/hex/genmodel/attributes/parameters/ColumnSpecifier.java
+++ h2o-genmodel/src/main/java/hex/genmodel/attributes/parameters/ColumnSpecifier.java
@@ -1,6 +1,7 @@
 package hex.genmodel.attributes.parameters;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ColumnSpecifier implements Serializable {
 
@@ -8,6 +9,7 @@ public class ColumnSpecifier implements Serializable {
   private final String[] is_member_of_frames;
 
   public ColumnSpecifier(String columnName, String[] is_member_of_frames) {
+    Objects.requireNonNull(columnName);
     this.columnName = columnName;
     this.is_member_of_frames = is_member_of_frames;
   }
