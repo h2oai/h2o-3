d48456cd5d53ba6d9b1c0a4011c26126949c2f0d
Hannah Tillman <hannah.tillman@h2o.ai>
2019-10-18 10:25:04 -0500

    PUBDEV-6986: added examples

--- h2o-bindings/bin/custom/python/gen_targetencoder.py
+++ h2o-bindings/bin/custom/python/gen_targetencoder.py
@@ -15,8 +15,22 @@ def class_extensions():
         :param int seed: a random seed used to generate draws from the uniform distribution for random noise. Defaults to -1.
 
         :example:
-        >>> targetEncoder = TargetEncoder(encoded_columns=te_columns, target_column=responseColumnName, blended_avg=True, inflection_point=10, smoothing=20)
-                            >>> encodedTrain = targetEncoder.transform(frame=trainFrame, data_leakage_handling="None", seed=1234, is_train_or_valid=True)
+        >>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+        >>> predictors = ["home.dest", "cabin", "embarked"]
+        >>> response = "survived"
+        >>> titanic["survived"] = titanic["survived"].asfactor()
+        >>> fold_col = "kfold_column"
+        >>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+        >>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+        ...                                        f = 0.3,
+        ...                                        data_leakage_handling = "None",
+        ...                                        blending = True)
+        >>> titanic_te.train(x = predictors,
+        ...                  y = response,
+        ...                  training_frame = titanic)
+        >>> transformed = titanic_te.transform(frame = titanic,
+        ...                                    data_leakage_handling="None",
+        ...                                    seed = 1234)
         """
         output = h2o.api("GET /3/TargetEncoderTransform", data={'model': self.model_id, 'frame': frame.key,
                                                                 'data_leakage_handling': data_leakage_handling,
@@ -28,4 +42,135 @@ def class_extensions():
 extensions = dict(
     __imports__="""import h2o""",
     __class__=class_extensions,
+)
+
+examples = dict(
+    blending="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling= "None",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    data_leakage_handling="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling= "k_fold",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    f="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling = "None",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    fold_column="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling = "None",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    ignored_columns="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> ignored = ["fare","age"]
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling = "None",
+...                                        ignored_columns = ignored)
+>>> titanic_te.train(y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    k="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling = "None",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    response_column="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response_column = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling = "None",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response_column,
+...                  training_frame = titanic)
+>>> titanic_te
+""",
+    training_frame="""
+>>> titanic = h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/gbm_test/titanic.csv")
+>>> predictors = ["home.dest", "cabin", "embarked"]
+>>> response = "survived"
+>>> titanic["survived"] = titanic["survived"].asfactor()
+>>> fold_col = "kfold_column"
+>>> titanic[fold_col] = titanic.kfold_column(n_folds=5, seed=1234)
+>>> titanic_te = H2OTargetEncoderEstimator(k = 0.7,
+...                                        f = 0.3,
+...                                        data_leakage_handling = "None",
+...                                        blending = True)
+>>> titanic_te.train(x = predictors,
+...                  y = response,
+...                  training_frame = titanic)
+>>> titanic_te
+"""
 )
