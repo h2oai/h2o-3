97cc9fc25a8afefc73722cb3d1060de89028ac70
Hannah Tillman <hannah.tillman@h2o.ai>
2019-10-24 07:55:18 -0500

    PUBDEV-7004: added examples

--- h2o-bindings/bin/custom/python/gen_pca.py
+++ h2o-bindings/bin/custom/python/gen_pca.py
@@ -17,3 +17,59 @@ def class_extensions():
 extensions = dict(
     __class__=class_extensions,
 )
+
+examples = dict(
+    compute_metrics="""
+>>> prostate = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip")
+>>> prostate['CAPSULE'] = prostate['CAPSULE'].asfactor()
+>>> prostate['RACE'] = prostate['RACE'].asfactor()
+>>> prostate['DCAPS'] = prostate['DCAPS'].asfactor()
+>>> prostate['DPROS'] = prostate['DPROS'].asfactor()
+>>> pros_pca = H2OPrincipalComponentAnalysisEstimator(compute_metrics=False)
+>>> pros_pca.train(x=prostate.names, training_frame=prostate)
+>>> pros_pca.show()
+""",
+    export_checkpoints_dir="""
+>>> import tempfile
+>>> from os import listdir
+>>> prostate = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip")
+>>> prostate['CAPSULE'] = prostate['CAPSULE'].asfactor()
+>>> prostate['RACE'] = prostate['RACE'].asfactor()
+>>> prostate['DCAPS'] = prostate['DCAPS'].asfactor()
+>>> prostate['DPROS'] = prostate['DPROS'].asfactor()
+>>> checkpoints_dir = tempfile.mkdtemp()
+>>> pros_pca = H2OPrincipalComponentAnalysisEstimator()
+>>> pros_pca.train(x=prostate.names, training_frame=prostate)
+>>> len(listdir(checkpoints_dir))
+""",
+    ignore_const_cols="""
+>>> prostate = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip")
+>>> prostate['CAPSULE'] = prostate['CAPSULE'].asfactor()
+>>> prostate['RACE'] = prostate['RACE'].asfactor()
+>>> prostate['DCAPS'] = prostate['DCAPS'].asfactor()
+>>> prostate['DPROS'] = prostate['DPROS'].asfactor()
+>>> pros_pca = H2OPrincipalComponentAnalysisEstimator(ignore_const_cols=False)
+>>> pros_pca.train(x=prostate.names, training_frame=prostate)
+>>> pros_pca.show()
+""",
+    impute_missing="""
+>>> prostate = h2o.import_file("http://s3.amazonaws.com/h2o-public-test-data/smalldata/prostate/prostate.csv.zip")
+>>> prostate['CAPSULE'] = prostate['CAPSULE'].asfactor()
+>>> prostate['RACE'] = prostate['RACE'].asfactor()
+>>> prostate['DCAPS'] = prostate['DCAPS'].asfactor()
+>>> prostate['DPROS'] = prostate['DPROS'].asfactor()
+>>> pros_pca = H2OPrincipalComponentAnalysisEstimator(impute_missing=True)
+>>> pros_pca.train(x=prostate.names, training_frame=prostate)
+>>> pros_pca.show()
+""",
+    init_for_pipeline="""
+>>> from sklearn.pipeline import Pipeline
+>>> from h2o.transforms.preprocessing import H2OScaler
+>>> from h2o.estimators import H2ORandomForestEstimator
+>>> iris = h2o.import_file("http://h2o-public-test-data.s3.amazonaws.com/smalldata/iris/iris_wheader.csv")
+>>> pipe = Pipeline([("standardize", H2OScaler()),
+...                  ("pca", H2OPrincipalComponentAnalysisEstimator(k=2).init_for_pipeline()),
+...                  ("rf", H2ORandomForestEstimator(seed=42,ntrees=5))])
+>>> pipe.fit(iris[:4], iris[4])
+""",
+)
