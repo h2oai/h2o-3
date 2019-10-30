4e13a15f5e6f06ce28a60cd460f26557eed2e780
Hannah Tillman <hannah.tillman@h2o.ai>
2019-10-30 11:56:11 -0500

    PUBDEV-7009: added examples

--- h2o-bindings/bin/custom/python/gen_word2vec.py
+++ h2o-bindings/bin/custom/python/gen_word2vec.py
@@ -13,6 +13,19 @@ def class_extensions():
         
         :param external: H2OFrame with an external model
         :return: H2OWord2vecEstimator instance representing the external model
+
+        :examples:
+
+        >>> words = h2o.create_frame(rows=10, cols=1,
+        ...                          string_fraction=1.0,
+        ...                          missing_fraction=0.0)
+        >>> embeddings = h2o.create_frame(rows=10, cols=100,
+        ...                               real_fraction=1.0,
+        ...                               missing_fraction=0.0)
+        >>> word_embeddings = words.cbind(embeddings)
+        >>> w2v_model = H2OWord2vecEstimator.from_external(external=word_embeddings)
+        >>> model_id = w2v_model.model_id
+        >>> model = h2o.get_model(model_id)
         """
         w2v_model = H2OWord2vecEstimator(pre_trained=external)
         w2v_model.train()
@@ -68,4 +81,80 @@ examples = dict(
 >>> w2v_model.train(training_frame=train)
 >>> len(listdir(checkpoints_dir))
 """,
-    
+    init_learning_rate="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, init_learning_rate=0.05)
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    max_runtime_secs="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, max_runtime_secs=10)
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    min_word_freq="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, max_runtime_secs=10)
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    norm_model="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, norm_model="hsm")
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    pre_trained="""
+>>> words = h2o.create_frame(rows=1000,cols=1,
+...                          string_fraction=1.0,
+...                          missing_fraction=0.0)
+>>> embeddings = h2o.create_frame(rows=1000,cols=100,
+...                               real_fraction=1.0,
+...                               missing_fraction=0.0)
+>>> word_embeddings = words.cbind(embeddings)
+>>> w2v_model = H2OWord2vecEstimator(pre_trained=word_embeddings)
+>>> w2v_model.train(training_frame=word_embeddings)
+>>> model_id = w2v_model.model_id
+>>> model = h2o.get_model(model_id)
+""",
+    sent_sample_rate="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, sent_sample_rate=0.01)
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    training_frame="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator()
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    vec_size="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, vec_size=50)
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    window_size="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, window_size=2)
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+""",
+    word_model="""
+>>> train = h2o.import_file(("https://s3.amazonaws.com/h2o-public-test-data/bigdata/laptop/text8.gz"), header=1, col_types=["string"])
+>>> w2v_model = H2OWord2vecEstimator(epochs=1, word_model="skip_gram")
+>>> w2v_model.train(training_frame=train)
+>>> synonyms = w2v_model.find_synonyms("war", 3)
+>>> print(synonyms)
+"""
+)
