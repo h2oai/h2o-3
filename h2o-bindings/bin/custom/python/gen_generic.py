cb575b3dd6f739af6112670e1c7ff91b8fdba7aa
Hannah Tillman <hannah.tillman@h2o.ai>
2019-11-01 11:44:54 -0500

    PUBDEV-7022: fixed tempfile lines

--- h2o-bindings/bin/custom/python/gen_generic.py
+++ h2o-bindings/bin/custom/python/gen_generic.py
@@ -17,7 +17,7 @@ def class_extensions():
         :examples:
 
         >>> from h2o.estimators import H2OIsolationForestEstimator, H2OGenericEstimator
-        >>> from tempfile
+        >>> import tempfile
         >>> airlines= h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/testng/airlines_train.csv")
         >>> ifr = H2OIsolationForestEstimator(ntrees=1)
         >>> ifr.train(x=["Origin","Dest"], y="Distance", training_frame=airlines)
@@ -59,7 +59,7 @@ examples = dict(
 """,
     path="""
 >>> from h2o.estimators import H2OIsolationForestEstimator, H2OGenericEstimator
->>> from tempfile
+>>> import tempfile
 >>> airlines= h2o.import_file("https://s3.amazonaws.com/h2o-public-test-data/smalldata/testng/airlines_train.csv")
 >>> ifr = H2OIsolationForestEstimator(ntrees=1)
 >>> ifr.train(x=["Origin","Dest"], y="Distance", training_frame=airlines)
