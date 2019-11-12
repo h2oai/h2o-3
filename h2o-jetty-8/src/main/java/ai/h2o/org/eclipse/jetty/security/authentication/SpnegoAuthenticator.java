6c5b11b436d47c901d44d0e70053cd4276750b41
Jan Sterba <jan.sterba@h2o.ai>
2019-11-12 13:59:43 +0100

    add original licences

--- h2o-jetty-8/src/main/java/ai/h2o/org/eclipse/jetty/security/authentication/SpnegoAuthenticator.java
+++ h2o-jetty-8/src/main/java/ai/h2o/org/eclipse/jetty/security/authentication/SpnegoAuthenticator.java
@@ -1,3 +1,20 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2019 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
 package ai.h2o.org.eclipse.jetty.security.authentication;
 
 import java.io.IOException;
