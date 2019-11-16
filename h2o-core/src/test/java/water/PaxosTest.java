07ab66c803b1f69e03bfb45f784f182b2f6ecfb3
michalkurka <michalk@h2o.ai>
2019-11-15 19:09:17 -0500

    Fix tests: there can already exist clients introduced by other tests

--- h2o-core/src/test/java/water/PaxosTest.java
+++ h2o-core/src/test/java/water/PaxosTest.java
@@ -1,19 +1,28 @@
 package water;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class PaxosTest extends TestUtil {
 
-  @Before
-  public void setUp() {
+  @BeforeClass
+  public static void setUp() {
     stall_till_cloudsize(1);
+  }
+
+  private H2ONode[] oldClients;
+
+  @Before
+  public void saveOldClients() {
+    oldClients = H2O.getClients();
   }
 
   @Test
@@ -31,7 +40,7 @@ public class PaxosTest extends TestUtil {
     clientNode.setHeartBeat(clientHeartBeat);
 
     // the node is recognized by H2O as a client at this point
-    assertArrayEquals(new H2ONode[]{clientNode}, H2O.getClients());
+    assertArrayEquals(new H2ONode[]{clientNode}, getNewClients());
 
     Paxos.doHeartbeat(clientNode);
 
@@ -40,6 +49,24 @@ public class PaxosTest extends TestUtil {
     assertArrayEquals(new Object[]{clientNode}, events.get(0));
 
     // the node was removed from cloud
-    assertArrayEquals(new H2ONode[]{}, H2O.getClients());
+    assertArrayEquals(new H2ONode[]{}, getNewClients());
   }
+
+  private H2ONode[] getNewClients() {
+    List<H2ONode> newClients = new ArrayList<>();
+    for (H2ONode client : H2O.getClients()) {
+      if (! isOldClient(client))
+        newClients.add(client);
+    }
+    return newClients.toArray(new H2ONode[0]);
+  }
+
+  private boolean isOldClient(H2ONode client) {
+    for (H2ONode oldClient : oldClients) {
+      if (oldClient == client)
+        return true;
+    }
+    return false;
+  }
+  
 }
