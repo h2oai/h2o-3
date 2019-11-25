e6a4c9c7d19add17a1abaedb936656b62e055c6d
Jakub Hava <jakub.hava@gmail.com>
2019-11-26 00:02:41 +0100

    stop the case in case we are not leader

--- h2o-core/src/main/java/water/RestApiPingCheckThread.java
+++ h2o-core/src/main/java/water/RestApiPingCheckThread.java
@@ -17,6 +17,9 @@ public class RestApiPingCheckThread extends Thread {
           Log.fatal("Stopping H2O cluster since we haven't received any REST api request on 3/Ping!");
           H2O.shutdown(-1);
         }
+      } else if (H2O.CLOUD._memary.length != 0 && Paxos._cloudLocked) {
+        // Cloud is locked, but we are not leader, we can stop the thread
+        Thread.currentThread().interrupt();
       }
       try {
         Thread.sleep(H2O.ARGS.rest_api_ping_timeout);
