3baa4b94eb4a80d9cee2002e1d873381d4113462
Hannah Tillman <hannah.tillman@h2o.ai>
2019-12-10 11:10:57 -0600

    PUBDEV-7075: test

--- h2o-docs/src/product/conf.py
+++ h2o-docs/src/product/conf.py
@@ -36,7 +36,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.osexample',
     'sphinx.ext.mathjax',
-    'sphinx.ext.todo'
+    'sphinx.ext.todo',
+    'sphinx_tabs.tabs'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
