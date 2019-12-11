c40120bb933d4db9c64355102e4e75193423d1c9
Jakub Háva <jakub.hava@gmail.com>
2019-12-11 22:46:29 +0100

    [SW-7134] Provide a reason for cloud locking (#4136)

--- h2o-core/src/main/java/water/api/CloudLockHandler.java
+++ h2o-core/src/main/java/water/api/CloudLockHandler.java
@@ -6,8 +6,12 @@ import water.api.schemas3.CloudLockV3;
 class CloudLockHandler extends Handler {
   
   @SuppressWarnings("unused") // called through reflection by RequestServer
-  public CloudLockV3 lock(int version, CloudLockV3 cloud) {
-    Paxos.lockCloud("requested via REST api");
-    return cloud;
+  public CloudLockV3 lock(int version, CloudLockV3 cloudLock) {
+    StringBuilder builder = new StringBuilder("requested via REST api.");
+    if (cloudLock.reason != null) {
+      builder.append(" Reason: ").append(cloudLock.reason);
+    }
+    Paxos.lockCloud(builder.toString());
+    return cloudLock;
   }
 }
