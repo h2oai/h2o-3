10fb946d662d86a02ce6fb99febaa521f4c65715
Sebastien Poirier <sebastien@h2o.ai>
2019-12-12 15:51:33 +0100

    disabling XGBoost in multinode and improved warning message when user is trying to use it in cluster environment

--- h2o-automl/src/main/java/ai/h2o/automl/AutoML.java
+++ h2o-automl/src/main/java/ai/h2o/automl/AutoML.java
@@ -320,7 +320,11 @@ public final class AutoML extends Lockable<AutoML> implements TimedH2ORunnable {
 
     for (Algo algo : Algo.values()) {
       if (!skippedAlgos.contains(algo) && !algo.enabled()) {
-        _eventLog.warn(Stage.ModelTraining, "AutoML: "+algo.name()+" is not available; skipping it.");
+        boolean isMultinode = H2O.CLOUD.size() > 1;
+        _eventLog.warn(Stage.ModelTraining,
+                isMultinode ? "AutoML: "+algo.name()+" is not available in multi-node cluster; skipping it." :
+                        "AutoML: "+algo.name()+" is not available; skipping it."
+        );
         skippedAlgos.add(algo);
       }
     }
