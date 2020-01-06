13cdaa9e97159214adf2b3b8feec67f13ec44f47
michalkurka <michalk@h2o.ai>
2020-01-06 17:52:16 -0500

    Fix GCS test

--- h2o-hadoop-common/tests/python/pyunit_gcs_import.py
+++ h2o-hadoop-common/tests/python/pyunit_gcs_import.py
@@ -6,10 +6,11 @@ sys.path.insert(1, os.path.join("../../../h2o-py"))
 from tests import pyunit_utils
 import h2o
 
+
 def gcs_import():
     # Just test the import works - no class clashes, no exception
     fr = h2o.import_file("gs://gcp-public-data-nexrad-l2/2018/01/01/KABR/NWS_NEXRAD_NXL2DPBL_KABR_20180101050000_20180101055959.tar", parse=False)
-    assert fr.nrow > 0
+    assert len(fr) == 1
 
 
 if __name__ == "__main__":
