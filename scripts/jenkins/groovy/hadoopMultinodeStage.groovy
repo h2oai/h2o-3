441468b29992b85b8e3debde67b15ba97dc22963
Jan Sterba <jan.sterba@h2o.ai>
2020-01-14 17:45:54 +0100

    fix script

--- scripts/jenkins/groovy/hadoopMultinodeStage.groovy
+++ scripts/jenkins/groovy/hadoopMultinodeStage.groovy
@@ -71,7 +71,7 @@ private GString downloadConfigsScript(Map config) {
         """
     }
     def extraInit = ""
-    if (config.nameNode == "") {
+    if (config.nameNode == "mr-0xg5") {
         extraInit = """
             export HDP_VERSION=2.4.2.0-258
         """
