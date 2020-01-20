7e7894cf2638fc73e26ba945e8e6c2e61fdfdb2c
Marek Novotný <marek.novotny@h2o.ai>
2020-01-20 17:07:26 +0100

    Validations of request parameters

--- h2o-core/src/main/java/water/api/ChunkServlet.java
+++ h2o-core/src/main/java/water/api/ChunkServlet.java
@@ -1,6 +1,9 @@
 package water.api;
 
 import water.ChunkAutoBufferWriter;
+import water.DKV;
+import water.ExternalFrameUtils;
+import water.fvec.Frame;
 import water.server.ServletUtils;
 
 import javax.servlet.http.HttpServlet;
@@ -46,29 +49,112 @@ public final class ChunkServlet extends HttpServlet {
     return result;
   }
   
-  private RequestParameters extractRequestParameters(final HttpServletRequest request) {
+  private RequestParameters parseRequestParameters(final HttpServletRequest request) {
     final String frameName = getParameterAsString(request, "frame_name");
     
     final String chunkIdString = getParameterAsString(request, "chunk_id");
-    int chunkId = Integer.parseInt(chunkIdString);
+    final int chunkId = Integer.parseInt(chunkIdString);
     
     final String expectedTypesString = getParameterAsString(request, "expected_types");
-    byte[] expectedTypes = Base64.getDecoder().decode(expectedTypesString);
+    final byte[] expectedTypes = Base64.getDecoder().decode(expectedTypesString);
 
     final String selectedColumnsString = getParameterAsString(request, "selected_columns");
-    byte[] selectedColumnsBytes = Base64.getDecoder().decode(selectedColumnsString);
+    final byte[] selectedColumnsBytes = Base64.getDecoder().decode(selectedColumnsString);
     final IntBuffer buffer = ByteBuffer.wrap(selectedColumnsBytes).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
-    int[] selectedColumnIndices = new int[buffer.remaining()];
+    final int[] selectedColumnIndices = new int[buffer.remaining()];
     buffer.get(selectedColumnIndices);
             
     return new RequestParameters(frameName, chunkId, expectedTypes, selectedColumnIndices); 
+  }
+  
+  private void validateRequestParameters(final RequestParameters parameters) {
+    final Frame frame = DKV.getGet(parameters.frameName);
+    if (frame == null) {
+      throw new RuntimeException(String.format("A frame with name '%s' doesn't exist.", parameters.frameName));
+    }
+
+    validateChunkId(parameters, frame);
+    validateSelectedColumns(parameters, frame);
+    validateExpectedTypes(parameters, frame);
+  }
+  
+  private void validateChunkId(final RequestParameters parameters, final Frame frame) {
+    if (parameters.chunkId < 0) {
+      throw new RuntimeException(String.format("chunk_id can't be negative. Current value: %d", parameters.chunkId));
+    }
+    
+    final int numberOfChunks = frame.anyVec().nChunks();
+    if (parameters.chunkId >= numberOfChunks) {
+      final String message = String.format(
+        "chunk_id '%d' is out of range. The frame '%s' has %d chunks.",
+        parameters.chunkId,
+        parameters.frameName,
+        numberOfChunks);
+      throw new RuntimeException(message);
+    }
+  }
+
+  private void validateSelectedColumns(final RequestParameters parameters, final Frame frame) {
+    for (int i = 0; i < parameters.selectedColumnIndices.length; i++) {
+      if (parameters.selectedColumnIndices[i] < 0) {
+        final String message = String.format(
+          "Selected column index ('selected_columns') at position %d with the value '%d' is negative.",
+          i,
+          parameters.selectedColumnIndices[i]);
+        throw new RuntimeException(message);
+      }
+      if (parameters.selectedColumnIndices[i] >= frame.numCols()) {
+        final String message = String.format(
+          "Selected column index ('selected_columns') at position %d with the value '%d' is out of range. Frame '%s' has %d columns.",
+          i,
+          parameters.selectedColumnIndices[i],
+          parameters.frameName,
+          frame.numCols());
+        throw new RuntimeException(message);
+      }
+    }    
+  }
+
+  private void validateExpectedTypes(final RequestParameters parameters, final Frame frame) {
+    for (int i = 0; i < parameters.expectedTypes.length; i++) {
+      if (parameters.expectedTypes[i] < ExternalFrameUtils.EXPECTED_BOOL ||
+          parameters.expectedTypes[i] > ExternalFrameUtils.EXPECTED_VECTOR) {
+        final String message = String.format(
+          "Expected Type ('expected_types') at position %d with the value '%d' is invalid.",
+          i,
+          parameters.expectedTypes[i]);
+        throw new RuntimeException(message);
+      }
+    }
+    
+    if (parameters.selectedColumnIndices.length != parameters.expectedTypes.length) {
+      final String message = String.format(
+        "The number of expected_types '%d' is not the same as the number of selected_columns '%d'",
+        parameters.expectedTypes.length,
+        parameters.selectedColumnIndices.length);
+      throw new RuntimeException(message);
+    }
+    
+    final byte[] expectedVecTypes = new byte[parameters.expectedTypes.length];
+    final byte[] frameTypes = frame.types();
+    for (int i = 0; i < parameters.expectedTypes.length; i++) {
+      final int frameTypePosition = parameters.selectedColumnIndices[i];  
+      if (expectedVecTypes[i] != frameTypes[frameTypePosition]) {
+        final String message = String.format(
+          "Expected Type ('expected_types') at position %d doesn't correspond to the frame column type at position %d.",
+          i,
+          frameTypePosition);
+        throw new RuntimeException(message);
+      }
+    }
   }
   
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
     final String uri = ServletUtils.getDecodedUri(request);
     try {
-      final RequestParameters parameters = extractRequestParameters(request);
+      final RequestParameters parameters = parseRequestParameters(request);
+      validateRequestParameters(parameters);
       response.setContentType("application/octet-stream");
       ServletUtils.setResponseStatus(response, HttpServletResponse.SC_OK);
       try (OutputStream outputStream = response.getOutputStream();
