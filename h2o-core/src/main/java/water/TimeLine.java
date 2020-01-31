b50fc50de8a91595589374661f16d3825bdc0fa5
Jakub Háva <jakub.hava@gmail.com>
2020-01-31 14:10:26 +1300

    Fix NPE on Timeline

--- h2o-core/src/main/java/water/TimeLine.java
+++ h2o-core/src/main/java/water/TimeLine.java
@@ -184,7 +184,7 @@ public class TimeLine extends UDP {
   // possible to the same point in time.
   static long[][] SNAPSHOT;
   static long TIME_LAST_SNAPSHOT = 1;
-  static private H2O CLOUD;      // Cloud instance being snapshotted
+  static private H2O CLOUD = H2O.CLOUD;      // Cloud instance being snapshotted
   public static H2O getCLOUD(){return CLOUD;}
   static public long[][] system_snapshot() {
     // Now spin-wait until we see all snapshots check in.
