0bec57a3e1ae943fe729a8edb26a4eae7d9dc543
maurever <veronika@maurerova.cz>
2020-02-05 14:55:19 +0100

    PUBDEV-6447 remove tmp file line

--- h2o-algos/src/test/java/hex/kmeans/KmeansConstrainedTest.java
+++ h2o-algos/src/test/java/hex/kmeans/KmeansConstrainedTest.java
@@ -267,7 +267,6 @@ public class KmeansConstrainedTest extends TestUtil {
         try {
             Scope.enter();
             fr = Scope.track(parse_test_file("bigdata/laptop/mnist/train.csv.gz"));
-            //fr = Scope.track(parse_test_file("/home/mori/Documents/h2o/code/h2o-3/bigdata/laptop/mnist/train.csv.gz"));
 
             KMeansModel.KMeansParameters parms = new KMeansModel.KMeansParameters();
             parms._train = fr._key;
