32d708f749f259a8e94ff2d24b1940c5eb8c57d7
Adam Valenta <valenad1@fit.cvut.cz>
2020-02-17 03:19:41 +0100

    PUBDEV-7138 - create extended isolation forest class with required dependencies

---
+++ h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForestOutput.java
@@ -0,0 +1,11 @@
+package hex.tree.isoforextended;
+
+import hex.tree.SharedTree;
+import hex.tree.SharedTreeModel;
+
+public class ExtendedIsolationForestOutput extends SharedTreeModel.SharedTreeOutput {
+    
+    public ExtendedIsolationForestOutput(SharedTree b) {
+        super(b);
+    }
+}
