c91b0e70002791e493d1e199b8e012fce04b595b
Jan Sterba <jan.sterba@h2o.ai>
2020-02-17 18:12:50 +0100

    fix test

--- h2o-extensions/xgboost/src/test/java/hex/tree/xgboost/XGBoostModelMaxThreadsTest.java
+++ h2o-extensions/xgboost/src/test/java/hex/tree/xgboost/XGBoostModelMaxThreadsTest.java
@@ -3,9 +3,9 @@ package hex.tree.xgboost;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.contrib.java.lang.system.ClearSystemProperties;
-import water.H2O;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class XGBoostModelMaxThreadsTest {
 
@@ -13,9 +13,9 @@ public class XGBoostModelMaxThreadsTest {
     public final ClearSystemProperties clearPropertyRule = new ClearSystemProperties("sys.ai.h2o.xgboost.nthreadMax");
 
     @Test
-    public void shouldGetCorrectMaxThread() {
-        int cloudSize = Math.max(1, H2O.CLOUD.size());
-        assertEquals(Runtime.getRuntime().availableProcessors() / cloudSize, XGBoostModel.getMaxNThread());
+    public void shouldRespectPropertyOverride() {
+        int original = XGBoostModel.getMaxNThread();
+        assertNotEquals(original, 2); // just make sure the property will actually change things
         System.setProperty("sys.ai.h2o.xgboost.nthreadMax", "2");
         assertEquals(2, XGBoostModel.getMaxNThread());
     }
