f63d72204e47533d7609da4c5ccd197738ce6434
michalkurka <michalk@h2o.ai>
2020-02-18 13:51:09 -0500

    PUBDEV-7308: XGBoost ContributionsPredictor is not Serializable

--- h2o-genmodel-extensions/xgboost/src/test/java/hex/genmodel/algos/xgboost/XGBoostJavaMojoModelTest.java
+++ h2o-genmodel-extensions/xgboost/src/test/java/hex/genmodel/algos/xgboost/XGBoostJavaMojoModelTest.java
@@ -1,6 +1,12 @@
 package hex.genmodel.algos.xgboost;
 
+import hex.genmodel.MojoModel;
+import hex.genmodel.MojoReaderBackend;
+import hex.genmodel.MojoReaderBackendFactory;
+import hex.genmodel.PredictContributions;
 import org.junit.Test;
+
+import java.io.*;
 
 import static org.junit.Assert.*;
 
@@ -15,5 +21,32 @@ public class XGBoostJavaMojoModelTest {
       assertNotNull(XGBoostJavaMojoModel.getObjFunction(type.getId()));
     }
   }
+
+  @Test
+  public void testPredictContributionsSerialization() throws Exception {
+    MojoReaderBackend readerBackend = MojoReaderBackendFactory.createReaderBackend(
+            XGBoostJavaMojoModelTest.class.getResource("xgboost_java.zip"),
+            MojoReaderBackendFactory.CachingStrategy.MEMORY);
+    XGBoostJavaMojoModel mojo = (XGBoostJavaMojoModel) MojoModel.load(readerBackend);
+    PredictContributions pc = mojo.makeContributionsPredictor();
+    assertNotNull(pc);
+    assertTrue(deserialize(serialize(pc)) instanceof PredictContributions);
+  }
+
+  private static byte[] serialize(Object o) throws Exception {
+    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    try (ObjectOutput out = new ObjectOutputStream(bos)) {
+      out.writeObject(o);
+    }
+    return bos.toByteArray();
+  }
+
+  private static Object deserialize(byte[] bs) throws Exception {
+    try (ByteArrayInputStream bis = new ByteArrayInputStream(bs)) {
+      ObjectInput in = new ObjectInputStream(bis);
+      return in.readObject();
+    }
+  }
+
 
 }
