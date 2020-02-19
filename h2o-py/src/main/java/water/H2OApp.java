b2d7aca62ffecc21cc14823c883af416b49909fa
michalkurka <michalk@h2o.ai>
2020-02-19 11:47:25 -0500

    HEXDEV-744: Provide a Python wheel that can't be used to start a local H2O

---
+++ h2o-py/src/main/java/water/H2OApp.java
@@ -0,0 +1,9 @@
+package water;
+
+public class H2OApp {
+
+  public static void main(String[] args) {
+    throw new IllegalStateException("Client version of the library cannot be used to start a local H2O instance. Use h2o.connect() instead.");
+  }
+
+}
