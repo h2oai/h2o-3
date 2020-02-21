adae5a5fd892afef94c8364d03787ae383d3c801
Pavel Pscheidl <pavel@h2o.ai>
2020-02-21 16:54:55 +0100

    Revert "PUBDEV-6603 - Test of frame deletion in Python is failing on assertion (#4342)" (#4345)
    This reverts commit 27bb955b

--- h2o-py/tests/testdir_jira/pyunit_pubdev_6603.py
+++ h2o-py/tests/testdir_jira/pyunit_pubdev_6603.py
@@ -10,11 +10,8 @@ import pandas as pd
 def pubdev_6603():
     hf = h2o.H2OFrame(pd.DataFrame([{'a': 1, 'b': 2}, {'a': 3, 'b': 4}]))
     s1, s2 = hf.split_frame(ratios=[0.5], seed=1)
-    frame_ids = [s1.frame_id, s2.frame_id, hf.frame_id]
     h2o.remove([hf, s1, s2])
-    
-    for id in frame_ids:
-        assert h2o.get_frame(id) is None
+    assert len(h2o.ls()) == 0
 if __name__ == "__main__":
     pyunit_utils.standalone_test(pubdev_6603)
 else:
