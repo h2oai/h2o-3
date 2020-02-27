7ddb7cabe33030e17d2760259878a906a28eac53
Adam Valenta <valenad1@fit.cvut.cz>
2020-02-27 03:28:30 +0100

    PUBDEV-7138 - create 256 rows long sub-sample of train frame and apply first split. Better method for Frame split(filter) needs to me implemented.

--- h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForest.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForest.java
@@ -1,12 +1,17 @@
 package hex.tree.isoforextended;
 
+import hex.FrameSplitter;
 import hex.ModelBuilder;
 import hex.ModelCategory;
+import hex.SplitFrame;
 import hex.psvm.psvm.MatrixUtils;
 import hex.tree.SharedTree;
+import water.DKV;
+import water.Key;
 import water.MRTask;
 import water.fvec.Chunk;
 import water.fvec.Frame;
+import water.fvec.NewChunk;
 import water.fvec.Vec;
 import water.util.ArrayUtils;
 import water.util.FrameUtils;
@@ -51,60 +56,93 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
         return false;
     }
 
+    private static class PrintRowsMRTask extends MRTask<PrintRowsMRTask> {
+
+        @Override
+        public void map(Chunk[] cs) {
+            System.out.println(this);
+            for (int i = 0; i < cs.length; i++) {
+                for (int j = 0; j < cs[i]._len; j++) {
+                    System.out.println(cs[i].atd(j));
+                }
+            }
+        }
+
+        @Override
+        public void reduce(PrintRowsMRTask mrt) {
+            // nothing to do
+        }
+    }
+
     private class ExtendedIsolationForestDriver extends ModelBuilder<ExtendedIsolationForestModel,
             ExtendedIsolationForestParameters,
             ExtendedIsolationForestOutput>.Driver {
 
         @Override
         public void computeImpl() {
-            Vec p = VecUtils.uniformDistrFromFrameMR(_train, _parms._seed);
-            Vec n = VecUtils.makeGaussianVec(_train.numCols(), _train.numCols() - _parms.extensionLevel - 1, _parms._seed);
+            
+            // create subsamble viz. EIF/IF paper algorithm 1
+            double ratio = 256.0/_train.numRows();
+            SplitFrame sf = new SplitFrame(_train, new double[]{ratio, 1 - ratio}, new Key[] {Key.make("train.hex"), Key.make("test.hex")});
+            sf.exec().get();
+            Key[] ksplits = sf._destination_frames;
+            Frame tr = DKV.get(ksplits[0]).get();
+
+            System.out.println("a");
+            
+            // create first split
+            Vec p = VecUtils.uniformDistrFromFrameMR(tr, _parms._seed);
+            Vec n = VecUtils.makeGaussianVec(tr.numCols(), tr.numCols() - _parms.extensionLevel - 1, _parms._seed);
             System.out.println(Arrays.toString(FrameUtils.asDoubles(n)));
             System.out.println(n.length());
-            Frame res = MatrixUtils.subtractionMtv(_train, p);
+            Frame res = MatrixUtils.subtractionMtv(tr, p);
             Vec mul = MatrixUtils.productMtv2(res, n);
             System.out.println(Arrays.toString(FrameUtils.asDoubles(mul)));
-            SplitFrameMRTask task = new SplitFrameMRTask(mul).doAll(_train);
-            System.out.println("task.left.length = " + task.left.length);
-            for (double [] row : task.left) {
-                System.out.println(Arrays.toString(row));
-            }
-            System.out.println("task.right.length = " + task.right.length);
-            for (double [] row : task.right) {
-                System.out.println(Arrays.toString(row));
-            }
+            Frame left = new FilterFrameLeftTask(mul).doAll(tr.types(), tr).outputFrame();
+            Frame right = new FilterFrameRightTask(mul).doAll(tr.types(), tr).outputFrame();
+            System.out.println("a");
         }
     }
 
-    private class SplitFrameMRTask extends MRTask<SplitFrameMRTask> {
+    private class FilterFrameLeftTask extends MRTask<FilterFrameLeftTask> {
         private Vec mul;
 
-        private double [][] left;
-        private double [][] right;
-
-        public SplitFrameMRTask(Vec mul) {
+        public FilterFrameLeftTask(Vec mul) {
             super();
             this.mul = mul;
         }
 
         @Override
-        public void map(Chunk[] cs) {
+        public void map(Chunk[] cs, NewChunk[] ncs) {
             for (int row = 0; row < cs[0]._len; row++) {
                 double num = mul.at(cs[0].start() + row);
-                if (num < 0) {
+                if (num <= 0) {
                     // left
-                    double [] rowValues = new double[cs.length];
                     for (int column = 0; column < cs.length; column++) {
-                        rowValues[column] = cs[column].atd(row);
+                        ncs[column].addNum(cs[column].atd(row));
                     }
-                    left = ArrayUtils.append(left, rowValues);
-                } else {
+                }
+            }
+        }
+    }
+
+    private class FilterFrameRightTask extends MRTask<FilterFrameRightTask> {
+        private Vec mul;
+
+        public FilterFrameRightTask(Vec mul) {
+            super();
+            this.mul = mul;
+        }
+
+        @Override
+        public void map(Chunk[] cs, NewChunk[] ncs) {
+            for (int row = 0; row < cs[0]._len; row++) {
+                double num = mul.at(cs[0].start() + row);
+                if (num > 0) {
                     // right
-                    double [] rowValues = new double[cs.length];
                     for (int column = 0; column < cs.length; column++) {
-                        rowValues[column] = cs[column].atd(row);
+                        ncs[column].addNum(cs[column].atd(row));
                     }
-                    right = ArrayUtils.append(right, rowValues);
                 }
             }
         }
