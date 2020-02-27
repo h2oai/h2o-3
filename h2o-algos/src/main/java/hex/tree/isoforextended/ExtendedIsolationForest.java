be970163d72d81a729cdf64940814c66a0385bb2
Adam Valenta <valenad1@fit.cvut.cz>
2020-02-27 18:29:52 +0100

    PUBDEV-7138 - add recoursive iTree implementation

--- h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForest.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForest.java
@@ -1,6 +1,5 @@
 package hex.tree.isoforextended;
 
-import hex.FrameSplitter;
 import hex.ModelBuilder;
 import hex.ModelCategory;
 import hex.SplitFrame;
@@ -13,7 +12,6 @@ import water.fvec.Chunk;
 import water.fvec.Frame;
 import water.fvec.NewChunk;
 import water.fvec.Vec;
-import water.util.ArrayUtils;
 import water.util.FrameUtils;
 import water.util.VecUtils;
 
@@ -28,6 +26,8 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
         ExtendedIsolationForestParameters,
         ExtendedIsolationForestOutput> {
 
+
+    public static final double SUB_SAMPLING_SIZE = 256.0;
 
     public ExtendedIsolationForest(ExtendedIsolationForestParameters parms) {
         super(parms);
@@ -82,41 +82,92 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
         public void computeImpl() {
             
             // create subsamble viz. EIF/IF paper algorithm 1
-            double ratio = 256.0/_train.numRows();
+            double ratio = SUB_SAMPLING_SIZE /_train.numRows();
             SplitFrame sf = new SplitFrame(_train, new double[]{ratio, 1 - ratio}, new Key[] {Key.make("train.hex"), Key.make("test.hex")});
             sf.exec().get();
             Key[] ksplits = sf._destination_frames;
-            Frame tr = DKV.get(ksplits[0]).get();
-
-            System.out.println("a");
+            Frame subSample = DKV.get(ksplits[0]).get();
             
-            // create first split
-            Vec p = VecUtils.uniformDistrFromFrameMR(tr, _parms._seed);
-            Vec n = VecUtils.makeGaussianVec(tr.numCols(), tr.numCols() - _parms.extensionLevel - 1, _parms._seed);
-            System.out.println(Arrays.toString(FrameUtils.asDoubles(n)));
-            System.out.println(n.length());
-            Frame res = MatrixUtils.subtractionMtv(tr, p);
-            Vec mul = MatrixUtils.productMtv2(res, n);
-            System.out.println(Arrays.toString(FrameUtils.asDoubles(mul)));
-            Frame left = new FilterFrameLeftTask(mul).doAll(tr.types(), tr).outputFrame();
-            Frame right = new FilterFrameRightTask(mul).doAll(tr.types(), tr).outputFrame();
-            System.out.println("a");
+            int heightLimit = (int) Math.ceil(customLog(2, SUB_SAMPLING_SIZE));
+            ITree iTree = new ITree(subSample, 0, heightLimit);
         }
     }
 
-    private class FilterFrameLeftTask extends MRTask<FilterFrameLeftTask> {
+    private static double customLog(double base, double logNumber) {
+        return Math.log(logNumber) / Math.log(base);
+    }
+    
+    private class ITree {
+        private Frame frame;
+        private Vec n;
+        private Vec p;
+        private ITree leftNode;
+        private ITree rightNode;
+
+        private int currentHeight;
+        private boolean external = false;
+        private long size;
+
+        public ITree(Frame frame, int currentHeight, int heightLimit) {
+            this.frame = frame;
+            this.currentHeight = currentHeight;
+            
+            if (currentHeight >= heightLimit || frame.numRows() <= 1) {
+                external = true;
+                size = frame.numRows();
+            } else {
+                p = VecUtils.uniformDistrFromFrameMR(frame, _parms._seed);
+                n = VecUtils.makeGaussianVec(frame.numCols(), frame.numCols() - _parms.extensionLevel - 1, _parms._seed);
+                Frame sub = MatrixUtils.subtractionMtv(frame, p);
+                Vec mul = MatrixUtils.productMtv2(sub, n);
+                Frame left = new FilterLteTask(mul, 0).doAll(frame.types(), frame).outputFrame();
+                Frame right = new FilterGtRightTask(mul, 0).doAll(frame.types(), frame).outputFrame();
+
+                leftNode = new ITree(left, currentHeight + 1, heightLimit);
+                rightNode = new ITree(right, currentHeight + 1, heightLimit);
+            }
+        }
+
+        public Frame getFrame() {
+            return frame;
+        }
+
+        public Vec getN() {
+            return n;
+        }
+
+        public Vec getP() {
+            return p;
+        }
+
+        public ITree getLeftNode() {
+            return leftNode;
+        }
+
+        public ITree getRightNode() {
+            return rightNode;
+        }
+        
+        private void growTree() {
+            
+        }
+    }
+
+    private class FilterLteTask extends MRTask<FilterLteTask> {
+        
+        private double value;
         private Vec mul;
 
-        public FilterFrameLeftTask(Vec mul) {
-            super();
-            this.mul = mul;
+        public FilterLteTask(Vec vec, double value) {
+            this.mul = vec;
+            this.value = value;
         }
 
         @Override
         public void map(Chunk[] cs, NewChunk[] ncs) {
             for (int row = 0; row < cs[0]._len; row++) {
                 double num = mul.at(cs[0].start() + row);
-                if (num <= 0) {
+                if (num <= value) {
                     // left
                     for (int column = 0; column < cs.length; column++) {
                         ncs[column].addNum(cs[column].atd(row));
@@ -126,19 +177,21 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
         }
     }
 
-    private class FilterFrameRightTask extends MRTask<FilterFrameRightTask> {
+    private class FilterGtRightTask extends MRTask<FilterGtRightTask> {
+        
+        private double value;
         private Vec mul;
 
-        public FilterFrameRightTask(Vec mul) {
-            super();
+        public FilterGtRightTask(Vec mul, double value) {
             this.mul = mul;
+            this.value = value;
         }
 
         @Override
         public void map(Chunk[] cs, NewChunk[] ncs) {
             for (int row = 0; row < cs[0]._len; row++) {
                 double num = mul.at(cs[0].start() + row);
-                if (num > 0) {
+                if (num > value) {
                     // right
                     for (int column = 0; column < cs.length; column++) {
                         ncs[column].addNum(cs[column].atd(row));
