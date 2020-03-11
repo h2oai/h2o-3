bcdab1e4c0cb81d2449855cf3b134441e6d2c51c
Adam Valenta <valenad1@fit.cvut.cz>
2020-03-11 13:25:46 +0100

    PUBDEV-7138 - implement example of implementation raising StackOverflowError

--- h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForestModel.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForestModel.java
@@ -1,6 +1,8 @@
 package hex.tree.isoforextended;
 
+import hex.ModelMetrics;
 import hex.tree.SharedTreeModel;
+import hex.tree.isofor.ModelMetricsAnomaly;
 import water.Key;
 import water.util.SBPrintStream;
 
@@ -11,14 +13,20 @@ import water.util.SBPrintStream;
 public class ExtendedIsolationForestModel extends SharedTreeModel<ExtendedIsolationForestModel,
                                                                     ExtendedIsolationForestParameters,
                                                                     ExtendedIsolationForestOutput> {
-
+    
     public ExtendedIsolationForestModel(Key<ExtendedIsolationForestModel> selfKey, ExtendedIsolationForestParameters parms,
                                         ExtendedIsolationForestOutput output) {
         super(selfKey, parms, output);
+//        iTrees = new ExtendedIsolationForest.ITree[_parms._ntrees];
     }
 
     @Override
     protected void toJavaUnifyPreds(SBPrintStream body) {
         // avalenta - TODO
+    }
+
+    @Override
+    public ModelMetrics.MetricBuilder makeMetricBuilder(String[] domain) {
+        return new ModelMetricsAnomaly.MetricBuilderAnomaly("Isolation Forest Metrics");
     }
 }
--- h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForestOutput.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForestOutput.java
@@ -1,11 +1,21 @@
 package hex.tree.isoforextended;
 
+import hex.ModelCategory;
 import hex.tree.SharedTree;
 import hex.tree.SharedTreeModel;
+import hex.tree.isofor.IsolationForest;
+import water.util.TwoDimTable;
 
 public class ExtendedIsolationForestOutput extends SharedTreeModel.SharedTreeOutput {
+
+    public ExtendedIsolationForest.Test test;
     
     public ExtendedIsolationForestOutput(SharedTree b) {
         super(b);
+    }
+
+    @Override
+    public ModelCategory getModelCategory() {
+        return ModelCategory.AnomalyDetection;
     }
 }
