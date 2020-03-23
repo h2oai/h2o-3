d22b566165a36f0a0b18621bb2ad2ee644ab5c71
wendycwong <wendy@h2o.ai>
2020-03-23 08:54:50 -0700

    Update h2o-genmodel/src/main/java/hex/genmodel/GenModel.java
    Co-Authored-By: Pavel Pscheidl <pavel@h2o.ai>

--- h2o-genmodel/src/main/java/hex/genmodel/GenModel.java
+++ h2o-genmodel/src/main/java/hex/genmodel/GenModel.java
@@ -209,7 +209,7 @@ public abstract class GenModel implements IGenModel, IGeneratedModel, Serializab
 
   /** Returns the expected size of preds array which is passed to `predict(double[], double[])` function. */
   @Override public int getPredsSize() { // fractional binomial has numerical response
-    return isClassifier()?(1 + getNumResponseClasses()): 2;
+    return isClassifier() ? (1 + getNumResponseClasses()) : 2;
   }
 
   public int getPredsSize(ModelCategory mc) {
