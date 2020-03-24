0e027d4ec2eb83f460798daacfae3af1a9da9f26
Pavel Pscheidl <pavel@h2o.ai>
2020-03-24 13:51:01 +0100

    Explicitly pass gam_x in R test.

--- h2o-r/tests/testdir_algos/gam/runit_gam.R
+++ h2o-r/tests/testdir_algos/gam/runit_gam.R
@@ -4,7 +4,7 @@ source("../../../scripts/h2o-r-test-setup.R")
 test.model.gam <- function() {
     data <- h2o.importFile(path = locate('smalldata/testng/airlines_train.csv'))
     cols <- c("Origin", "Distance", "IsDepDelayed")
-    original_model <- h2o.gam(x=cols, y = "IsDepDelayed", training_frame = data, family = "binomial")
+    original_model <- h2o.gam(x=cols, y = "IsDepDelayed", gam_X = cols, training_frame = data, family = "binomial")
     print(original_model)
     expect_false(is.null(original_model))
 }
