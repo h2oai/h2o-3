5938a65522a74eff1b156632004bf3f1c7ffbd23
Jan Sterba <info@jansterba.com>
2020-04-17 22:50:57 +0200

    [PUBDEV-7465] add option to run xgboost on secured cluster

--- h2o-extensions/xgboost/src/main/java/hex/tree/xgboost/XGBoost.java
+++ h2o-extensions/xgboost/src/main/java/hex/tree/xgboost/XGBoost.java
@@ -100,7 +100,7 @@ public class XGBoost extends ModelBuilder<XGBoostModel,XGBoostModel.XGBoostParam
   @Override public void init(boolean expensive) {
     super.init(expensive);
     if (H2O.CLOUD.size() > 1) {
-      if(H2O.SELF.getSecurityManager().securityEnabled) {
+      if(H2O.SELF.getSecurityManager().securityEnabled && !H2O.ARGS.allow_insecure_xgboost) {
         throw new H2OIllegalArgumentException("Cannot run XGBoost on an SSL enabled cluster larger than 1 node. XGBoost does not support SSL encryption.");
       }
     }
