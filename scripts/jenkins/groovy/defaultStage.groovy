f8357936542a1d0d34e42190719b1877a60e155d
Pavel Pscheidl <pavel@h2o.ai>
2020-04-17 07:25:25 +0200

    Removed forgotten print

--- scripts/jenkins/groovy/defaultStage.groovy
+++ scripts/jenkins/groovy/defaultStage.groovy
@@ -5,7 +5,6 @@ def call(final pipelineContext, final stageConfig) {
     def buildEnv = pipelineContext.getBuildConfig().getBuildEnv() + ["PYTHON_VERSION=${stageConfig.pythonVersion}", "R_VERSION=${stageConfig.rVersion}", "JAVA_VERSION=${stageConfig.javaVersion}"]
 
     echo "###### Changes for ${stageConfig.component} detected, starting ${stageConfig.stageName} ######"
-    print(stageConfig.addToDockerGroup)
     insideDocker(buildEnv, stageConfig.image, pipelineContext.getBuildConfig().DOCKER_REGISTRY, pipelineContext.getBuildConfig(), stageConfig.timeoutValue, 'MINUTES', stageConfig.customDockerArgs.join(' '), stageConfig.addToDockerGroup) {
         def h2oFolder = stageConfig.stageDir + '/h2o-3'
 
