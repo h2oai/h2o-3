2b83d7e02bfc3f2b43dcccafda7d9ae7664cef22
Ján Jendrušák <jendrjan@fit.cvut.cz>
2020-04-29 18:57:51 +0200

    TF-IDF bigdata test
    Add bigdatat test for TF-IDF.

--- h2o-core/src/test/java/water/rapids/ast/prims/advmath/AstTfIdfTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/advmath/AstTfIdfTest.java
@@ -1,9 +1,6 @@
 package water.rapids.ast.prims.advmath;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ExpectedException;
 import water.Scope;
 import water.TestUtil;
@@ -14,12 +11,21 @@ import water.rapids.Rapids;
 import water.rapids.Session;
 import water.rapids.Val;
 import water.rapids.vals.ValFrame;
+import water.util.FileUtils;
 import water.util.Pair;
 
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.zip.GZIPInputStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 
 public class AstTfIdfTest extends TestUtil {
 
+    private static final String BIGDATA_TESTS_ENV_VAR_NAME = "testTfIdfBigdata";
     private static final double DELTA = 0.0001;
 
     @Rule
@@ -47,7 +53,6 @@ public class AstTfIdfTest extends TestUtil {
             Val resVal = Rapids.exec("(tf-idf " + frameName + ")", sess);
             Assert.assertTrue(resVal instanceof ValFrame);
             Frame resFrame = resVal.getFrame();
-            System.out.println(resFrame.toTwoDimTable().toString());
             Scope.track(resFrame);
 
             assertEquals(expectedOutputFrame.numCols(), resFrame.numCols());
@@ -64,6 +69,100 @@ public class AstTfIdfTest extends TestUtil {
         } finally {
             Scope.exit();
         }
+    }
+    
+    @Test
+    public void testDocumentFrequenciesBigData() throws IOException {
+        Assume.assumeThat("DF bigdata tests are enabled", System.getProperty(BIGDATA_TESTS_ENV_VAR_NAME), is(notNullValue()));
+
+        System.out.println("Loading data...");
+        File testFile = FileUtils.getFile("bigdata/laptop/text8.gz");
+        List<String> tokens = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(testFile))))
+                .lines().collect(Collectors.toList());
+        int tokensCnt = tokens.size();
+        System.out.println(tokensCnt + " words have been loaded.");
+
+        int docSplitIndex = tokensCnt / 2;
+        List<String> doc1Tokens = tokens.subList(0, docSplitIndex);
+        List<String> doc2Tokens = tokens.subList(docSplitIndex, tokensCnt);
+
+        String[] documents = new String[] {
+                String.join(" ", doc1Tokens),
+                String.join(" ", doc2Tokens)
+        };
+        long[] docIds = new long[] { 0L, 1L };
+
+        Set<String> doc1UniqueTokens = new HashSet<>(doc1Tokens);
+        Set<String> doc2UniqueTokens = new HashSet<>(doc2Tokens);
+
+        Map<String, Long> doc1Counts = tokens.subList(0, docSplitIndex)
+                .stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        Map<String, Long> doc2Counts = tokens.subList(docSplitIndex, tokensCnt)
+                .stream().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        Map[] expectedTFs = new Map[]{ doc1Counts, doc2Counts };
+
+        Map<String, Integer> expectedDFs = doc1UniqueTokens.stream().collect(Collectors.toMap(token -> token, token -> 1));
+        doc2UniqueTokens.forEach(token -> expectedDFs.merge(token, 1, Integer::sum));
+        Map<String, Double> expectedIDFs = expectedDFs.entrySet().stream()
+                       .collect(Collectors.toMap(Map.Entry::getKey, entry -> idf(entry.getValue(), documents.length)));
+
+        Scope.enter();
+        try {
+            Session sess = new Session();
+            String frameName = "testFrame";
+            
+            Frame inputFrame = new TestFrameBuilder()
+                                    .withName(frameName, sess)
+                                    .withColNames("Document ID", "Document")
+                                    .withVecTypes(Vec.T_NUM, Vec.T_STR)
+                                    .withDataForCol(0, docIds)
+                                    .withDataForCol(1, documents)
+                                    .withChunkLayout(1, 1)
+                                    .build();
+            Scope.track(inputFrame);
+
+            Val resVal = Rapids.exec("(tf-idf " + frameName + ")", sess);
+            Assert.assertTrue(resVal instanceof ValFrame);
+            Frame resFrame = resVal.getFrame();
+            Scope.track(resFrame);
+            
+            int expectedNumCols = 5;
+            assertEquals(expectedNumCols, resFrame.numCols());
+            assertEquals(doc1UniqueTokens.size() + doc2UniqueTokens.size(), resFrame.numRows());
+            
+            Vec outDocIds = resFrame.vec(0);
+            Vec outTokens = resFrame.vec(1);
+            Vec outTF = resFrame.vec(2);
+            Vec outIDF = resFrame.vec(3);
+            Vec outTFIDF = resFrame.vec(4);
+
+            for (int row = 0; row < resFrame.numRows(); row++) {
+                long docId = outDocIds.at8(row);
+                String token = outTokens.stringAt(row);
+                long tf = outTF.at8(row);
+                double idf = outIDF.at(row);
+                double tfIdf = outTFIDF.at(row);
+
+                Map<String, Long> docTfValues = expectedTFs[(int) docId];
+
+                Assert.assertTrue(docTfValues.containsKey(token));
+                long expectedTf = docTfValues.get(token);
+                Assert.assertEquals(expectedTf, tf);
+
+                Assert.assertTrue(expectedIDFs.containsKey(token));
+                double expectedIdf = expectedIDFs.get(token);
+                Assert.assertEquals(expectedIdf, idf, DELTA);
+
+                Assert.assertTrue(expectedIDFs.containsKey(token));
+                Assert.assertEquals(expectedTf * expectedIdf, tfIdf, DELTA);
+            }
+        } finally {
+            Scope.exit();
+        }
+    }
+    
+    private static double idf(long df, long documentsCnt) {
+        return Math.log(((double)(documentsCnt + 1)) / (df + 1));
     }
 
     private Pair<Frame, Frame> getSimpleTestFrames(final String frameName, final Session session) {
