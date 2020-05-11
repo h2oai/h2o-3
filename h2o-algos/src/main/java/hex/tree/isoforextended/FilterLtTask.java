afafb3079ff1d48dd84df64c7b5b602faff5b826
Adam Valenta <valenad1@fit.cvut.cz>
2020-05-12 01:29:09 +0200

    PUBDEV-7138 - add documentation to Extended Isoaltion Forest filter methods and create tests for them

--- h2o-algos/src/main/java/hex/tree/isoforextended/FilterLtTask.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/FilterLtTask.java
@@ -5,20 +5,30 @@ import water.fvec.Chunk;
 import water.fvec.NewChunk;
 import water.fvec.Vec;
 
+/**
+ * Filter given frame by value. Given frame will be filtered by values in given vector.
+ *
+ * @author Adam Valenta
+ */
 public class FilterLtTask extends MRTask<FilterLtTask> {
 
     private double value;
-    private Vec mul;
+    private Vec vec;
 
+    /**
+     * In the output will be all rows from frame where vec.at(rownumber) < value
+     * @param vec Vector used for filtering. Length of vector must be the same as number of rows in given Frame
+     * @param value value to compare
+     */
     public FilterLtTask(Vec vec, double value) {
-        this.mul = vec;
+        this.vec = vec;
         this.value = value;
     }
 
     @Override
     public void map(Chunk[] cs, NewChunk[] ncs) {
         for (int row = 0; row < cs[0]._len; row++) {
-            double num = mul.at(cs[0].start() + row);
+            double num = vec.at(cs[0].start() + row);
             if (num < value) {
                 for (int column = 0; column < cs.length; column++) {
                     ncs[column].addNum(cs[column].atd(row));
