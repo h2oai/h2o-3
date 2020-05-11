2424857efe59afe06d580b18654a464ce5c07c5e
Adam Valenta <valenad1@fit.cvut.cz>
2020-05-12 01:29:09 +0200

    PUBDEV-7138 - add documentation to Extended Isoaltion Forest filter methods and create tests for them

--- h2o-algos/src/main/java/hex/tree/isoforextended/FilterGteTask.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/FilterGteTask.java
@@ -5,20 +5,30 @@ import water.fvec.Chunk;
 import water.fvec.NewChunk;
 import water.fvec.Vec;
 
-public class FilterGteRightTask extends MRTask<FilterGteRightTask> {
+/**
+ * Filter given frame by value. Given frame will be filtered by values in given vector.
+ * 
+ * @author Adam Valenta
+ */
+public class FilterGteTask extends MRTask<FilterGteTask> {
 
     private double value;
-    private Vec mul;
+    private Vec vec;
 
-    public FilterGteRightTask(Vec mul, double value) {
-        this.mul = mul;
+    /**
+     * In the output will be all rows from frame where vec.at(rownumber) >= value
+     * @param vec Vector used for filtering. Length of vector must be the same as number of rows in given Frame
+     * @param value value to compare
+     */
+    public FilterGteTask(Vec vec, double value) {
+        this.vec = vec;
         this.value = value;
     }
 
     @Override
     public void map(Chunk[] cs, NewChunk[] ncs) {
         for (int row = 0; row < cs[0]._len; row++) {
-            double num = mul.at(cs[0].start() + row);
+            double num = vec.at(cs[0].start() + row);
             if (num >= value) {
                 for (int column = 0; column < cs.length; column++) {
                     ncs[column].addNum(cs[column].atd(row));
--- h2o-algos/src/test/java/hex/tree/isoforextended/ExtendedIsolationForestTest.java
+++ h2o-algos/src/test/java/hex/tree/isoforextended/ExtendedIsolationForestTest.java
@@ -88,4 +88,94 @@ public class ExtendedIsolationForestTest extends TestUtil {
         assertEquals(0, ExtendedIsolationForest.IsolationTree.averagePathLengthOfUnsuccesfullSearch(0), 1e-5);
         assertEquals(0, ExtendedIsolationForest.IsolationTree.averagePathLengthOfUnsuccesfullSearch(-1), 1e-5);
     }
+
+    @Test
+    public void FilterLtTaskTest() {
+        try {
+            Scope.enter();
+            Frame m = new TestFrameBuilder()
+                    .withVecTypes(Vec.T_NUM, Vec.T_NUM, Vec.T_NUM, Vec.T_NUM)
+                    .withDataForCol(0, ard(1.0, 1.0))
+                    .withDataForCol(1, ard(1.0, 1.0))
+                    .withDataForCol(2, ard(1.0, 1.0))
+                    .withDataForCol(3, ard(1.0, 1.0))
+                    .build();
+            Scope.track(m);
+            Vec v = Vec.makeVec(ard(2.0, -1), Vec.newKey());
+
+            Frame resVec = new FilterLtTask(v, 0).doAll(m.types(), m).outputFrame(Key.make(), null, null);
+            assertEquals(1, resVec.numRows());
+            assertEquals(4, resVec.numCols());
+            DKV.remove(resVec._key);
+        } finally {
+            Scope.exit();
+        }
+    }
+
+    @Test
+    public void FilterLtTaskCategoricalDataTest() {
+        try {
+            Scope.enter();
+            Frame m = new TestFrameBuilder()
+                    .withVecTypes(Vec.T_NUM, Vec.T_CAT, Vec.T_NUM)
+                    .withDataForCol(0, ard(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0))
+                    .withDataForCol(1, ar("B", "C", "D", "E", "B", "C", "D", "E", "A", "B"))
+                    .withDataForCol(2, ard(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0))
+                    .build();
+            Scope.track(m);
+            Vec v = Vec.makeVec(ard(2.0, 2.0, 0.0, -1.2, 5.5, -5.5, 6.5, 5.5, -5.5, 6.5), Vec.newKey());
+
+            Frame resVec = new FilterLtTask(v, 0).doAll(m.types(), m).outputFrame(Key.make(), null, null);
+            assertEquals(3, resVec.numRows());
+            assertEquals(3, resVec.numCols());
+            DKV.remove(resVec._key);
+        } finally {
+            Scope.exit();
+        }
+    }
+
+    @Test
+    public void FilterGteTaskTest() {
+        try {
+            Scope.enter();
+            Frame m = new TestFrameBuilder()
+                    .withVecTypes(Vec.T_NUM, Vec.T_NUM, Vec.T_NUM, Vec.T_NUM)
+                    .withDataForCol(0, ard(1.0, 1.0))
+                    .withDataForCol(1, ard(1.0, 1.0))
+                    .withDataForCol(2, ard(1.0, 1.0))
+                    .withDataForCol(3, ard(1.0, 1.0))
+                    .build();
+            Scope.track(m);
+            Vec v = Vec.makeVec(ard(2.0, -1.0), Vec.newKey());
+
+            Frame resVec = new FilterGteTask(v, 0).doAll(m.types(), m).outputFrame(Key.make(), null, null);
+            assertEquals(1, resVec.numRows());
+            assertEquals(4, resVec.numCols());
+            DKV.remove(resVec._key);
+        } finally {
+            Scope.exit();
+        }
+    }
+
+    @Test
+    public void FilterGteTaskCategoricalDataTest() {
+        try {
+            Scope.enter();
+            Frame m = new TestFrameBuilder()
+                    .withVecTypes(Vec.T_NUM, Vec.T_CAT, Vec.T_NUM)
+                    .withDataForCol(0, ard(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0))
+                    .withDataForCol(1, ar("B", "C", "D", "E", "B", "C", "D", "E", "A", "B"))
+                    .withDataForCol(2, ard(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0))
+                    .build();
+            Scope.track(m);
+            Vec v = Vec.makeVec(ard(2.0, 2.0, 0.0, -1.2, 5.5, -5.5, 6.5, 5.5, -5.5), Vec.newKey());
+
+            Frame resVec = new FilterGteTask(v, 0).doAll(m.types(), m).outputFrame(Key.make(), null, null);
+            assertEquals(7, resVec.numRows());
+            assertEquals(3, resVec.numCols());
+            DKV.remove(resVec._key);
+        } finally {
+            Scope.exit();
+        }
+    }
 }
