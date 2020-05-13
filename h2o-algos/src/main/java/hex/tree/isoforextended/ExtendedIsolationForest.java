7f0844b10be85d604cc9ee3dcb7f7c02de428469
Adam Valenta <valenad1@fit.cvut.cz>
2020-05-13 22:58:11 +0200

    PUBDEV-7138 - make model parameters with underscore - project convencion. Improve code quality a documentation of Extended Isoaltion Forest subclasses IsolationTree and Node

--- h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForest.java
+++ h2o-algos/src/main/java/hex/tree/isoforextended/ExtendedIsolationForest.java
@@ -13,7 +13,7 @@ import water.util.MathUtils;
 import water.util.VecUtils;
 
 /**
- * Extended isolation forest implementation by https://arxiv.org/pdf/1811.02141.pdf paper.
+ * Extended isolation forest implementation. Algorithm comes from https://arxiv.org/pdf/1811.02141.pdf paper.
  *
  * @author Adam Valenta
  */
@@ -22,6 +22,7 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
         ExtendedIsolationForestModel.ExtendedIsolationForestOutput> {
 
     transient IsolationTree[] _iTrees;
+    public int _k = 0; // track number of trees generated in buildNextKTrees method
 
     // Called from an http request
     public ExtendedIsolationForest(ExtendedIsolationForestModel.ExtendedIsolationForestParameters parms) {
@@ -104,12 +105,10 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
 
     @Override
     protected void addCustomInfo(ExtendedIsolationForestModel.ExtendedIsolationForestOutput out) {
-        if (iTrees != null) {
-            out.iTrees = iTrees;
+        if (_iTrees != null) {
+            out.iTrees = _iTrees;
         }
     }
-
-    public int _k = 0;
 
     private class ExtendedIsolationForestDriver extends Driver {
 
@@ -141,67 +140,80 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
 //            iTree.clean();
 //            iTree.print();
 //            iTree.printHeight();
-            iTrees[k] = iTree;
-            k++;
+            _iTrees[_k] = iTree;
+            _k++;
             return false;
         }
 
         @Override
         protected void initializeModelSpecifics() {
-            iTrees = new IsolationTree[_parms._ntrees];
+            _iTrees = new IsolationTree[_parms._ntrees];
         }
 
     }
 
+    /**
+     * IsolationTree class implements Algorithm 2 (iTree) and Algorithm 3 (pathLength) 
+     * from the Extended Isolation Forest paper.
+     * 
+     * The binary tree recursive implementation is replaced by array implementation.
+     * It is the most simple array implementation of binary tree.
+     */
     public static class IsolationTree extends Iced<IsolationTree> {
-        private Node[] nodes;
+        private Node[] _nodes;
 
         private Key<Frame> _frameKey;
         private int _heightLimit;
         private long _seed;
         private int _extensionLevel;
-        private int heightLimit;
-        private long seed;
-        private int extensionLevel;
 
-        public IsolationTree(Key<Frame> frame, int heightLimit, long seed, int extensionLevel) {
-            this.frameKey = frame;
-            this.heightLimit = heightLimit;
-            this.seed = seed;
-            this.extensionLevel = extensionLevel;
+        public IsolationTree(Key<Frame> frame, int _heightLimit, long _seed, int _extensionLevel) {
+            this._frameKey = frame;
+            this._heightLimit = _heightLimit;
+            this._seed = _seed;
+            this._extensionLevel = _extensionLevel;
 
-            this.nodes = new Node[(int) Math.pow(2, heightLimit) - 1];
+            int maxNumNodesInTree = (int) Math.pow(2, _heightLimit) - 1;
+            this._nodes = new Node[maxNumNodesInTree]; 
         }
 
+        /**
+         * Implementation of Algorithm 2 (iTree) from paper.
+         * Algorithm not need exact row vectors in scoring (evaluation) stage but just number of rows in node.
+         * Therefore nodeFrames are removed from DKV after filtering.
+         */
         public void buildTree() {
-            Frame frame = DKV.get(frameKey).get();
-            nodes[0] = new Node(frame._key, frame.numRows(), 0);
-            for (int i = 0; i < nodes.length; i++) {
-                Node node = nodes[i];
+            Frame frame = DKV.get(_frameKey).get();
+            _nodes[0] = new Node(frame._key, frame.numRows(), 0);
+            for (int i = 0; i < _nodes.length; i++) {
+                Node node = _nodes[i];
                 if (node == null || node.external) {
                     continue;
                 }
                 Frame nodeFrame = node.getFrame();
                 int currentHeight = node.height;
-                if (node.height >= heightLimit || nodeFrame.numRows() <= 1) {
+                if (node.height >= _heightLimit || nodeFrame.numRows() <= 1) {
                     node.external = true;
-                    node.size = nodeFrame.numRows();
+                    node.numRows = nodeFrame.numRows();
                     node.height = currentHeight;
                     DKV.remove(node.frameKey);
                 } else {
                     currentHeight++;
 
-                    node.p = VecUtils.uniformDistrFromFrame(nodeFrame, seed + i);
-                    node.n = ArrayUtils.gaussianVector(nodeFrame.numCols(), seed + i, nodeFrame.numCols() - extensionLevel - 1);
+                    node.p = VecUtils.uniformDistrFromFrame(nodeFrame, _seed + i);
+                    node.n = ArrayUtils.gaussianVector(
+                            nodeFrame.numCols(), _seed + i, nodeFrame.numCols() - _extensionLevel - 1);
                     Frame sub = MatrixUtils.subtractionMtv(nodeFrame, node.p);
                     Vec mul = MatrixUtils.productMtvMath(sub, node.n);
-                    Frame left = new FilterLtTask(mul, 0).doAll(nodeFrame.types(), nodeFrame).outputFrame(Key.make(), null, null);
-                    Frame right = new FilterGteTask(mul, 0).doAll(nodeFrame.types(), nodeFrame).outputFrame(Key.make(), null, null);
+                    Frame left = new FilterLtTask(mul, 0)
+                            .doAll(nodeFrame.types(), nodeFrame).outputFrame(Key.make(), null, null);
+                    Frame right = new FilterGteTask(mul, 0)
+                            .doAll(nodeFrame.types(), nodeFrame).outputFrame(Key.make(), null, null);
                     DKV.remove(nodeFrame._key);
 
-                    if ((2 * i + 2) < nodes.length) {
-                        nodes[2 * i + 1] = new Node(left._key, left.numRows(), currentHeight);
-                        nodes[2 * i + 2] = new Node(right._key, right.numRows(), currentHeight);
+                    if (rightChildIndex(i) < _nodes.length) {
+                        _nodes[leftChildIndex(i)] = new Node(left._key, left.numRows(), currentHeight);
+                        _nodes[rightChildIndex(i)] = new Node(right._key, right.numRows(), currentHeight);
                     } else {
                         DKV.remove(left._key);
                         DKV.remove(right._key);
@@ -209,48 +221,70 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
                 }
             }
         }
+        
+        private int leftChildIndex(int i) {
+            return 2 * i + 1;
+        }
 
+        private int rightChildIndex(int i) {
+            return 2 * i + 2;
+        }        
+
+        /**
+         * Helper method. Print nodes size of the tree.
+         */
         public void print() {
-            for (int i = 0; i < nodes.length; i++) {
-                if (nodes[i] == null)
+            for (int i = 0; i < _nodes.length; i++) {
+                if (_nodes[i] == null)
                     System.out.print(". ");
                 else
-                    System.out.print(nodes[i].size + " ");
+                    System.out.print(_nodes[i].numRows + " ");
             }
             System.out.println("");
         }
 
+        /**
+         * Helper method. Print height of each node in trees. Root is 0.
+         */
         public void printHeight() {
-            for (int i = 0; i < nodes.length; i++) {
-                if (nodes[i] == null)
+            for (int i = 0; i < _nodes.length; i++) {
+                if (_nodes[i] == null)
                     System.out.print(". ");
                 else
-                    System.out.print(nodes[i].height + " ");
+                    System.out.print(_nodes[i].height + " ");
             }
             System.out.println("");
         }
 
+        /**
+         * Implementation of Algorithm 3 (pathLength) from paper.
+         */
         public double computePathLength(double[] row) {
             int position = 0;
-            Node node = nodes[0];
+            Node node = _nodes[0];
             double score = 0;
             while (!node.external) {
                 double[] sub = ArrayUtils.subtract(row, node.p);
                 double mul = ArrayUtils.innerProduct(sub, node.n);
                 if (mul <= 0) {
-                    position = 2 * position + 1;
+                    position = leftChildIndex(position);
                 } else {
-                    position = 2 * position + 2;
+                    position = rightChildIndex(position);
                 }
-                if (position < nodes.length)
-                    node = nodes[position];
+                if (position < _nodes.length)
+                    node = _nodes[position];
                 else
                     break;
             }
-            score += node.height + averagePathLengthOfUnsuccesfullSearch(node.size);
+            score += node.height + averagePathLengthOfUnsuccesfullSearch(node.numRows);
             return score;
         }
 
+        /**
+         * IsolationTree Node. Naming convetion comes from Algorithm 2 (iTree) in paper.
+         * frameKey should be always empty after buildTree() method because only number of rows in Frame is needed for
+         * scoring (evaluation) stage.
+         */
         private static class Node extends Iced<Node> {
             private Key<Frame> frameKey;
             private double[] n;
@@ -258,12 +292,12 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
 
             int height;
             boolean external = false;
-            long size;
+            long numRows;
 
-            public Node(Key<Frame> frameKey, long size, int currentHeight) {
+            public Node(Key<Frame> frameKey, long numRows, int currentHeight) {
                 this.frameKey = frameKey;
                 this.height = currentHeight;
-                this.size = size;
+                this.numRows = numRows;
             }
 
             Frame getFrame() {
@@ -272,7 +306,8 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
         }
 
         /**
-         * Gives the average path length of unsuccessful search in BST
+         * Gives the average path length of unsuccessful search in BST.
+         * Comes from Algorithm 3 (pathLength) and Equation 2 in paper
          *
          * @param n number of elements
          */
@@ -282,19 +317,6 @@ public class ExtendedIsolationForest extends SharedTree<ExtendedIsolationForestM
             if (n == 2)
                 return 1;
             return 2 * MathUtils.harmonicNumberEstimation(n - 1) - (2.0 * (n - 1.0)) / n;
-        }
-    }
-
-    private static class PrintRowsMRTask extends MRTask<PrintRowsMRTask> {
-
-        @Override
-        public void map(Chunk[] cs) {
-            for (int row = 0; row < cs[0]._len; row++) {
-                for (int column = 0; column < cs.length; column++) {
-                    System.out.print(cs[column].atd(row) + " ");
-                }
-                System.out.println();
-            }
         }
     }
 
