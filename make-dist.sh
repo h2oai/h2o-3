75933d15f2e589b83b07e3577ecd6190b12137ea
Honza Sterba <jan.sterba@h2o.ai>
2020-05-13 18:37:42 +0200

    [build] fix releasing (#4612)

--- make-dist.sh
+++ make-dist.sh
@@ -68,6 +68,7 @@ if [ -n "$DO_RELEASE" ]; then
 fi
 
 # Run some required gradle tasks to produce final build output.
+./gradlew booklets
 ./gradlew $DO_RELEASE -Prelease.branch=${BRANCH_NAME} -Prelease.useAutomaticVersion=true -Prelease.releaseVersion=${PROJECT_VERSION} -Prelease.newVersion=${CURRENT_VERSION} publish
 
 # Create target dir, which is uploaded to s3.
