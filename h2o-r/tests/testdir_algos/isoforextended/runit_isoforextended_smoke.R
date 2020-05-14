870ec6b6d22d38cf3d9e9ceae1581a7430c8e504
Adam Valenta <valenad1@fit.cvut.cz>
2020-05-15 00:09:19 +0200

    PUBDEV-7138 - improve R smoke test for Extended Isolation Forest

--- h2o-r/tests/testdir_algos/isoforextended/runit_isoforextended_smoke.R
+++ h2o-r/tests/testdir_algos/isoforextended/runit_isoforextended_smoke.R
@@ -13,6 +13,11 @@ test.ExtendedIsolationForest.smoke <- function() {
     random_data.hex <- as.h2o(random_data)
     
     exisofor.model <- h2o.extendedIsolationForest(training_frame = random_data.hex)
+    score <- h2o.predict(exisofor.model, random_data.hex)
+    result_pred <- as.data.frame(score)$anomaly_score
+
+    result_expected <- c(0.4311024, 0.4023118, 0.4286952, 0.6100855, 0.4194065, 0.4420759)
+    expect_equal(head(result_pred), result_expected, tolerance = 0.1, scale = 1)
     expect_equal(as.character(class(exisofor.model)), "H2OAnomalyDetectionModel")
 }
 
