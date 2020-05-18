600d7d9994a0218c4fc5c6b09e9da83bfa50c3aa
ardKelmendi <kelmeard@fit.cvut.cz>
2020-05-18 08:39:22 +0200

    Calculating feature importance of one feature

--- h2o-core/src/main/java/water/rapids/FeatureImportance4BBM.java
+++ h2o-core/src/main/java/water/rapids/FeatureImportance4BBM.java
@@ -2,36 +2,73 @@ package water.rapids;
 
 import hex.Model;
 //import hex.VarImp;
-import sun.java2d.jules.TileWorker;
 import water.fvec.Frame;
+import water.fvec.Vec;
+import water.util.FrameUtils;
+import water.util.MathUtils;
 import water.util.TwoDimTable;
+import water.util.VecUtils;
 
 
 public class FeatureImportance4BBM /* extends VarImp */ {
     
     Model _model;
-    public Frame _ouput; //public for now
-    TwoDimTable _orignal_dataset; // original dataset used to train model
+    public Frame _ouput; 
+    Frame _train_fr; // original dataset used to train model
+    Frame _validate_dataset; // original dataset used to train model
+    Frame _predcited_frame; 
     
-    TwoDimTable permutation_dataset; // original dataset used to train model
-    String algo_name; // what algorithm is it being called upon 
-
-    TwoDimTable _scaled_FI;
+    boolean _data_in_holdOut_form; // do I have the data split in train and validation, if not spliting will happen
+    
+    String algo_name; 
     
     public FeatureImportance4BBM(Model model) {_model = model;}
     
-    //TODO
-    public void getWokringdataset() {}
+    public FeatureImportance4BBM(Model model, Frame data_set, Frame prediction_frame) {
+        _model = model;
+        _train_fr = data_set;
+        _data_in_holdOut_form = false;
+        _validate_dataset = null;
+        _predcited_frame = prediction_frame;
+    }
+
+    public FeatureImportance4BBM(Model model, Frame train_data_set, Frame validate_data_set, Frame prediction_frame) {
+        _model = model;
+        _train_fr = train_data_set;
+        _validate_dataset = validate_data_set;
+        _data_in_holdOut_form = true;
+        _predcited_frame = prediction_frame;   
+    }
     
-    //TODO
-    public void permutateColumns(TwoDimTable working_df) {}
-    
-    //TODO
-    public void calculateValues() {}
-    
-    // dummy output for testing purposes
-    public TwoDimTable Foo(){
-        return _model._output._scoring_history;
+    public void getFeatureImportance() {
+        if(!_data_in_holdOut_form){
+            //TODO split the data if needed
+        }
+        
+        double sq_err = new MathUtils.SquareError().doAll(_train_fr.vecs()[1],_predcited_frame.vecs()[0])._sum;
+        double init_mse = sq_err/_predcited_frame.numRows();
+        sq_err = 0;
+        
+        double [] mse_all = new double [_train_fr.vecs().length];
+
+        String [] features = _train_fr.names();
+//        for (int f = 1; f < features.length; f++)//f = 0 is predicted col
+//        {
+            Vec shuffledColFr = Vec.makeVec(new VecUtils.shuffleVecVol2().doAll(_train_fr.vec(features[0]))._result, Vec.newKey());
+
+            // score the model again and compute diff
+        
+            Frame new_score = _model.score(_train_fr);
+
+            sq_err = new MathUtils.SquareError().doAll(_train_fr.vecs()[1],_predcited_frame.vecs()[0])._sum;
+            mse_all[0] = init_mse - sq_err/new_score.numRows();
+            
+            //TODO revert  frame 
+        
+        // store a copy of the vector before shuffleing then append it, swap/remove with shuffled col
+//        }
+
     }
     
 }
+
