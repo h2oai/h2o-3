3a0edb7e19c0f79e04de1558738ea26b95215882
ardKelmendi <kelmeard@fit.cvut.cz>
2020-05-20 06:48:42 +0200

    Method now uses newly created MRtask from ShuffleVec class

--- h2o-core/src/main/java/water/rapids/FeatureImportance4BBM.java
+++ h2o-core/src/main/java/water/rapids/FeatureImportance4BBM.java
@@ -2,12 +2,17 @@ package water.rapids;
 
 import hex.Model;
 //import hex.VarImp;
+import water.DKV;
+import water.Key;
+import water.Scope;
 import water.fvec.Frame;
 import water.fvec.Vec;
 import water.util.FrameUtils;
 import water.util.MathUtils;
 import water.util.TwoDimTable;
 import water.util.VecUtils;
+
+import java.util.Arrays;
 
 
 public class FeatureImportance4BBM /* extends VarImp */ {
@@ -40,33 +45,74 @@ public class FeatureImportance4BBM /* extends VarImp */ {
         _predcited_frame = prediction_frame;   
     }
     
+    
+    /* maybe afeter creating the fld put it in the DKV */
     public void getFeatureImportance() {
         if(!_data_in_holdOut_form){
             //TODO split the data if needed
         }
+//            Frame deepCopy(String keyName)
+        String [] features = _train_fr.names();
         
+        // initial criterion
         double sq_err = new MathUtils.SquareError().doAll(_train_fr.vecs()[1],_predcited_frame.vecs()[0])._sum;
         double init_mse = sq_err/_predcited_frame.numRows();
-        sq_err = 0;
-        
-        double [] mse_all = new double [_train_fr.vecs().length];
 
-        String [] features = _train_fr.names();
-//        for (int f = 1; f < features.length; f++)//f = 0 is predicted col
-//        {
-            Vec shuffledColFr = Vec.makeVec(new VecUtils.shuffleVecVol2().doAll(_train_fr.vec(features[0]))._result, Vec.newKey());
+        // create a copy of original training data
+        Frame _shuffled_train_fr = _train_fr.deepCopy(Key.make().toString());
+        DKV.put(_shuffled_train_fr);
+        _shuffled_train_fr.vec(0).setNA(1);
+
+        double [] mse_all = new double [_train_fr.vecs().length];
+//        ShuffleVec shf = new ShuffleVec(_shuffled_train_fr);          I see it as usefull for later
+
+        for (int f = 1; f < _train_fr.numCols(); f++)//f = 0 is predicted col
+        {
+            //permuate vector
+            ShuffleVec.ShuffleTask.shuffle(_shuffled_train_fr.vec(features[f]));
 
             // score the model again and compute diff
-        
-            Frame new_score = _model.score(_train_fr);
+            Frame new_score = _model.score(_shuffled_train_fr);
 
-            sq_err = new MathUtils.SquareError().doAll(_train_fr.vecs()[1],_predcited_frame.vecs()[0])._sum;
-            mse_all[0] = init_mse - sq_err/new_score.numRows();
+            // calculate criterion usnig MSE for now TODO calculate critorion based on choice
+            sq_err = new MathUtils.SquareError().doAll(_shuffled_train_fr.vecs()[1],_predcited_frame.vecs()[0])._sum;
             
-            //TODO revert  frame 
-        
-        // store a copy of the vector before shuffleing then append it, swap/remove with shuffled col
-//        }
+            // store results in array TODO Use hashmap string (feature) to double (calculation)
+            mse_all[f] = init_mse - sq_err/_predcited_frame.numRows();
+
+            /* TODO create a swaping function */
+//            swap_vecs(_train_fr.vec(f), _shuffled_train_fr.vec(f), features[f]);
+            
+            // lazy add/remove
+            _shuffled_train_fr.remove(features[f]);
+            _shuffled_train_fr.add(features[f], _train_fr.vec(features[f]));
+
+
+        }
+    }
+    public void swap_vecs(Vec src, Vec dst, String feature_name){
+
+        if( !src.group().equals(dst.group()) && !Arrays.equals(src.espc(),dst.espc()) )
+            throw new IllegalArgumentException("Vector groups differs - swaping vec'"+ feature_name);
+        if( _train_fr.numRows() != dst.length() )
+            throw new IllegalArgumentException("Vector lengths differ - swaping vec '"+feature_name);
+        System.arraycopy(src,0,dst,1, (int) _train_fr.numRows()); // careful typecasting long to int!!!! FIXME
+
+/*
+        System.arraycopy(_names,0,names,0,i);
+        System.arraycopy(_vecs,0,vecs,0,i);
+        System.arraycopy(_keys,0,keys,0,i);
+        names[i] = name;
+        vecs[i] = vec;
+        keys[i] = vec._key;
+        System.arraycopy(_names,i,names,i+1,_names.length-i);
+        System.arraycopy(_vecs,i,vecs,i+1,_vecs.length-i);
+        System.arraycopy(_keys,i,keys,i+1,_keys.length-i);
+        _vecs = vecs;
+        setNames(names);
+        _keys = keys;
+*/
+
 
     }
     
