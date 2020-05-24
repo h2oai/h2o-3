89fea311673ecc933b166e78bfb19e97ef78646e
ardKelmendi <kelmeard@fit.cvut.cz>
2020-05-24 02:02:39 +0200

    Created Ast class for calculating FI

--- h2o-core/src/main/java/water/rapids/ast/prims/models/AstFeatureImportance4BBM.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/models/AstFeatureImportance4BBM.java
@@ -2,29 +2,47 @@ package water.rapids.ast.prims.models;
 
 
 import hex.Model;
+import water.Key;
+import water.fvec.Frame;
+import water.fvec.Vec;
 import water.rapids.Env;
 import water.rapids.FeatureImportance4BBM;
 import water.rapids.ast.AstPrimitive;
 import water.rapids.ast.AstRoot;
 import water.rapids.vals.ValFrame;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import java.util.HashMap;
+
 public class AstFeatureImportance4BBM extends AstPrimitive {
 
     @Override
-    public int nargs() {return 1 + 1;}
+    public int nargs() {return 1 + 2;} // Perm_feature_importance + Modle + Frame
 
     @Override
-    public String[] args() {return new String [] {"Model"};} // 
+    public String[] args() {return new String [] {"model", "frame"};} 
     
     @Override
-    public String str() {return "FI4BBM";} // uncertain
+    public String str() {return "Perm_Feature_importance";} // or call it model_reliance 
     
     @Override
     public ValFrame apply(Env env, Env.StackHelp stk, AstRoot asts[]){
         Model model = stk.track(asts[1].exec(env)).getModel();
-        FeatureImportance4BBM fi = new FeatureImportance4BBM(model);
-        return new ValFrame(fi._ouput);
+        Frame fr = stk.track(asts[2].exec(env)).getFrame();
+        
+        FeatureImportance4BBM fi = new FeatureImportance4BBM(model, fr , null, model._parms._response_column);
+        HashMap<String, Double> FI = fi.getFeatureImportance(); // might be able to use AstPerfectAuc for calculation of AUC 
 
-//        return new ValFrame(models.toFrame());
+        // Frame contains one row and n features 
+        Frame outfr = new Frame(Key.make("feature_imp")); 
+        
+        for (Map.Entry<String, Double> entry : FI.entrySet()) {
+            outfr.add( entry.getKey(),Vec.makeVec(new double [] {entry.getValue()}, Vec.newKey()));
+            System.out.println(entry.getKey() + "/" + entry.getValue());
+        }
+        
+        return new ValFrame(outfr);            
     }
 }
