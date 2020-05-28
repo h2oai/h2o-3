0b8b78c34f5818cf5580bc32ceb998c4a8f65004
Pavel Pscheidl <pavelpscheidl@gmail.com>
2020-05-28 11:25:22 +0200

    Moved ColumnIndicesParser separately and tested validations in a separate test.

--- h2o-core/src/main/java/water/rapids/ast/prims/filters/dropduplicates/AstDropDuplicates.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/filters/dropduplicates/AstDropDuplicates.java
@@ -1,7 +1,6 @@
 package water.rapids.ast.prims.filters.dropduplicates;
 
 import water.fvec.Frame;
-import water.fvec.Vec;
 import water.rapids.Env;
 import water.rapids.Val;
 import water.rapids.ast.AstPrimitive;
@@ -28,7 +27,8 @@ public class AstDropDuplicates extends AstPrimitive<AstDropDuplicates> {
   @Override
   public Val apply(Env env, Env.StackHelp stk, AstRoot[] asts) {
     final Frame deduplicatedFrame = stk.track(asts[1].exec(env)).getFrame();
-    final int[] comparedColumnsIndices = parseAndCheckComparedColumnIndices(deduplicatedFrame, stk.track(asts[2].exec(env)));
+    final int[] comparedColumnsIndices = ColumnIndicesParser.parseAndCheckComparedColumnIndices(deduplicatedFrame,
+            stk.track(asts[2].exec(env)));
     final String dropOrderString = asts[3].str();
     final KeepOrder keepOrder = EnumUtils.valueOfIgnoreCase(KeepOrder.class, dropOrderString)
             .orElseThrow(() -> new IllegalArgumentException(String.format("Unknown drop order: '%s'. Known types: %s",
@@ -39,51 +39,6 @@ public class AstDropDuplicates extends AstPrimitive<AstDropDuplicates> {
     return new ValFrame(outputFrame);
   }
 
-  private int[] parseAndCheckComparedColumnIndices(final Frame deduplicatedFrame, final Val comparedColumns) {
-    final int[] columnRange;
-
-    if (comparedColumns.isStr()) {
-      final String columnName = comparedColumns.getStr();
-      final int columnIndex = deduplicatedFrame.find(columnName);
-      if (columnIndex == -1) {
-        throw new IllegalArgumentException(String.format("Unknown column name: '%s'", columnName));
-      }
-      columnRange = new int[]{columnIndex};
-    } else if (comparedColumns.isStrs()) {
-      final String[] columnNames = comparedColumns.getStrs();
-      columnRange = new int[columnNames.length];
-
-      for (int i = 0; i < columnNames.length; i++) {
-        final String columnName = columnNames[i];
-        final int columnIndex = deduplicatedFrame.find(columnName);
-        if (columnIndex == -1) {
-          throw new IllegalArgumentException(String.format("Unknown column name: '%s'", columnName));
-        } else if (deduplicatedFrame.types()[columnIndex] == Vec.T_STR || deduplicatedFrame.types()[columnIndex] == Vec.T_BAD
-                || deduplicatedFrame.types()[columnIndex] == Vec.T_UUID) {
-          throw new IllegalArgumentException(String.format("Column '%s' is of unsupported type %s for row de-duplication.", columnName, Vec.TYPE_STR[deduplicatedFrame.types()[columnIndex]]));
-        }
-        columnRange[i] = columnIndex;
-
-      }
-
-    } else if (comparedColumns.isNums()) {
-      final double[] columnRangeDouble = comparedColumns.getNums();
-      columnRange = new int[columnRangeDouble.length];
-      for (int i = 0; i < columnRangeDouble.length; i++) {
-        columnRange[i] = (int) columnRangeDouble[i];
-        if (columnRange[i] < 0 || columnRange[i] > deduplicatedFrame.numCols() - 1) {
-          throw new IllegalArgumentException(String.format("No such column index: '%d', frame has %d columns," +
-                          "maximum index is %d.",
-                  columnRange[i], deduplicatedFrame.numCols(), deduplicatedFrame.numCols() - 1));
-        }
-      }
-    } else {
-      throw new IllegalArgumentException(String.format("Column range for deduplication must either be a set of columns, or a " +
-              "column range. Given type: %s", comparedColumns.type()));
-    }
-
-    return columnRange;
-  }
 
   @Override
   public String str() {
