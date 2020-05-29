b0bbe1625681cb47f7aff6e7ec96ff100f93f0c6
ardKelmendi <kelmeard@fit.cvut.cz>
2020-05-29 05:01:01 +0200

    Trying to create tests to AST class

--- h2o-core/src/test/java/water/rapids/ast/prims/PermutationFeatureImportanceTest.java
+++ h2o-core/src/test/java/water/rapids/ast/prims/PermutationFeatureImportanceTest.java
@@ -1,6 +1,7 @@
 package water.rapids.ast.prims;
-
 import hex.Model;
+import hex.ModelMetrics;
+import hex.ScoringInfo;
 import hex.createframe.CreateFramePostprocessStep;
 import hex.createframe.postprocess.ShuffleOneColumnCfps;
 import hex.tree.gbm.GBMModel;
@@ -27,7 +28,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
-public class FeatureImportance4BBMTest extends TestUtil {
+public class PermutationFeatureImportanceTest extends TestUtil {
 
     @BeforeClass
     public static void stall() {
@@ -143,7 +144,7 @@ public class FeatureImportance4BBMTest extends TestUtil {
             fr2 = gbm.score(fr);
             //job.response() can be used in place of fr.vecs()[1] but it has been rebalanced
 
-            FeatureImportance4BBM Fi = new FeatureImportance4BBM(gbm, fr, fr2, parms._response_column);
+            PermutationFeatureImportance Fi = new PermutationFeatureImportance(gbm, fr, fr2);
             Fi.getFeatureImportance();
             
         } finally {
@@ -155,7 +156,7 @@ public class FeatureImportance4BBMTest extends TestUtil {
         
     }
 
-    //testing if model is getting the GBMmodel on my class (FeatureImportance4BBM.java)
+    //testing if model is getting the GBMmodel on my class (PermutationFeatureImportanceM.java)
     @Test
     public void Classification() throws Exception {
         try {
@@ -195,22 +196,25 @@ public class FeatureImportance4BBMTest extends TestUtil {
                 GBM job = new GBM(parms);
                 GBMModel gbm = job.trainModel().get();
                 Scope.track_generic(gbm);
-
                 
+                                
                 // Done building model; produce a score column with predictions
                 Frame scored = Scope.track(gbm.score(fr));
 
                 // Build a POJO & MOJO, validate same results
                 Assert.assertTrue(gbm.testJavaScoring(fr, scored, 1e-15));
 
-                FeatureImportance4BBM Fi = new FeatureImportance4BBM(gbm, fr, scored, parms._response_column);
-                Fi.getFeatureImportance("AUC");
+                PermutationFeatureImportance Fi = new PermutationFeatureImportance(gbm, fr, scored);
+                Fi.getFeatureImportance();
+                System.out.print(Fi.getTable());
+            
             }
         } finally {
             Scope.exit();
         }
     }
 
+    // Test will crash as to not passing the model correctly
     @Test public void testFI4SmallDataAst(){
         Frame frame = null;
         Frame fi_frame = null;
@@ -264,6 +268,7 @@ public class FeatureImportance4BBMTest extends TestUtil {
         }
 
     }
+    // similar as the previous test this one crashes
     @Test
     public void SimpleGenDataSet(){
         Scope.enter();
@@ -320,48 +325,4 @@ public class FeatureImportance4BBMTest extends TestUtil {
             Scope.exit();
         }
     }
-/*
-    @Test
-    public void testFoo2() {
-        GBMModel gbm = null;
-        Frame fr = null, fr2 = null;
-        try {
-//            String tmp = System.getProperty("user.dir");
-            fr = parse_test_file("./smalldata/gbm_test/Mfgdata_gaussian_GBM_testing.csv");
-            GBMModel.GBMParameters parms = new GBMModel.GBMParameters();
-            parms._train = fr._key;
-            parms._distribution = gaussian;
-            parms._response_column = fr._names[1]; // Row in col 0, dependent in col 1, predictor in col 2
-            parms._ntrees = 1;
-            parms._max_depth = 1;
-            parms._min_rows = 1;
-            parms._nbins = 20;
-            // Drop ColV2 0 (row), keep 1 (response), keep col 2 (only predictor), drop remaining cols
-            String[] xcols = parms._ignored_columns = new String[fr.numCols() - 2];
-            xcols[0] = fr._names[0];
-            System.arraycopy(fr._names, 3, xcols, 1, fr.numCols() - 3);
-            parms._learn_rate = 1.0f;
-            parms._score_each_iteration = true;
-
-            GBM job = new GBM(parms);
-            gbm = job.trainModel().get();
-            Assert.assertTrue(job.isStopped()); //HEX-1817
-
-            // Done building model; produce a score column with predictions
-            fr2 = gbm.score(fr);
-            //job.response() can be used in place of fr.vecs()[1] but it has been rebalanced
-            double sq_err = new MathUtils.SquareError().doAll(fr.vecs()[1], fr2.vecs()[0])._sum;
-            double mse = sq_err / fr2.numRows();
-            assertEquals(79152.12337641386, mse, 0.1);
-            assertEquals(79152.12337641386, gbm._output._scored_train[1]._mse, 0.1);
-            assertEquals(79152.12337641386, gbm._output._scored_train[1]._mean_residual_deviance, 0.1);
-        } finally {
-            if (fr != null) fr.remove();
-            if (fr2 != null) fr2.remove();
-            if (gbm != null) gbm.remove();
-            //           FeatureImportance4BBM Fi = new FeatureImportance4BBM(gbm);
-        }
- 
-    }
- */
 }
