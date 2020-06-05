8821cac36f43661f87cfc77254fec596161956e5
Pavel Pscheidl <pavel@h2o.ai>
2020-06-05 14:43:34 +0200

    Change logger in PersistS3 class to Log4J (#4682)

--- h2o-persist-s3/src/main/java/water/persist/PersistS3.java
+++ h2o-persist-s3/src/main/java/water/persist/PersistS3.java
@@ -11,19 +11,17 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.S3ClientOptions;
 import com.amazonaws.services.s3.model.*;
+import org.apache.log4j.Logger;
 import water.*;
 import water.fvec.FileVec;
 import water.fvec.S3FileVec;
 import water.fvec.Vec;
 import water.util.ByteStreams;
-import water.util.Log;
 import water.util.RIStream;
 
-import java.io.EOFException;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.SocketTimeoutException;
 import java.net.URI;
 import java.util.*;
 
@@ -31,7 +29,7 @@ import static water.H2O.OptArgs.SYSTEM_PROP_PREFIX;
 
 /** Persistence backend for S3 */
 public final class PersistS3 extends Persist {
-  private static final String HELP = "You can specify a credentials properties file with the -aws_credentials command line switch.";
+  private static final Logger LOG = Logger.getLogger(PersistS3.class);
 
   private static final String KEY_PREFIX = "s3://";
   private static final int KEY_PREFIX_LEN = KEY_PREFIX.length();
@@ -124,7 +122,7 @@ public final class PersistS3 extends Persist {
       try {
         return new PropertiesCredentials(credentials);
       } catch (IOException e) {
-        Log.debug(
+        LOG.debug(
             "Unable to load AWS credentials from file " + credentials + 
                 "; exists? " + credentials.exists() + ", canRead? " + credentials.canRead() + 
                 ", size=" + credentials.length() + "; problem: " + e.getMessage());
@@ -201,7 +199,7 @@ public final class PersistS3 extends Persist {
     }
   }
   public void importFiles(String path, String pattern, ArrayList<String> files, ArrayList<String> keys, ArrayList<String> fails, ArrayList<String> dels) {
-    Log.info("ImportS3 processing (" + path + ")");
+    LOG.info("ImportS3 processing (" + path + ")");
     // List of processed files
     AmazonS3 s3 = getClient();
     String [] parts = decodePath(path);
@@ -245,12 +243,8 @@ public final class PersistS3 extends Persist {
         return b;
         // Explicitly ignore the following exceptions but
         // fail on the rest IOExceptions
-      } catch( EOFException e ) {
-        ignoreAndWait(e, false);
-      } catch( SocketTimeoutException e ) {
-        ignoreAndWait(e, false);
       } catch( IOException e ) {
-        ignoreAndWait(e, true);
+        ignoreAndWait(e);
       } finally {
         try {
           if( s != null ) s.close();
@@ -259,8 +253,8 @@ public final class PersistS3 extends Persist {
     }
   }
 
-  private static void ignoreAndWait(final Exception e, boolean printException) {
-    Log.ignore(e, "Hit the S3 reset problem, waiting and retrying...", printException);
+  private static void ignoreAndWait(final Exception e) {
+    LOG.debug("Hit the S3 reset problem, waiting and retrying...", e);
     try {
       Thread.sleep(500);
     } catch( InterruptedException ie ) {}
@@ -388,17 +382,17 @@ public final class PersistS3 extends Persist {
   static  AmazonS3Client configureClient(AmazonS3Client s3Client) {
     if (System.getProperty(S3_REGION) != null) {
       String region = System.getProperty(S3_REGION);
-      Log.debug("S3 region specified: ", region);
+      LOG.debug(String.format("S3 region specified: %s", region) );
       s3Client.setRegion(RegionUtils.getRegion(region));
     }
     // Region overrides end-point settings
     if (System.getProperty(S3_END_POINT) != null) {
       String endPoint = System.getProperty(S3_END_POINT);
-      Log.debug("S3 endpoint specified: ", endPoint);
+      LOG.debug(String.format("S3 endpoint specified: %s", endPoint));
       s3Client.setEndpoint(endPoint);
     }
     if (System.getProperty(S3_ENABLE_PATH_STYLE) != null && Boolean.valueOf(System.getProperty(S3_ENABLE_PATH_STYLE))) {
-      Log.debug("S3 path style access enabled");
+      LOG.debug("S3 path style access enabled");
       S3ClientOptions sco = new S3ClientOptions();
       sco.setPathStyleAccess(true);
       s3Client.setS3ClientOptions(sco);
@@ -423,7 +417,7 @@ public final class PersistS3 extends Persist {
       if (e.getErrorCode().contains("404")) {
         throw new IOException(e);
       } else {
-        Log.err("AWS failed for " + Arrays.toString(parts) + ": " + e.getMessage());
+        LOG.error("AWS failed for " + Arrays.toString(parts) + ": " + e.getMessage());
         throw e;
       }
     }
@@ -439,7 +433,7 @@ public final class PersistS3 extends Persist {
 
     public boolean containsKey(String k) { return Arrays.binarySearch(_cache,k) >= 0;}
     protected String [] update(){
-      Log.debug("Renewing S3 bucket cache.");
+      LOG.debug("Renewing S3 bucket cache.");
       List<Bucket> l = getClient().listBuckets();
       String [] cache = new String[l.size()];
       int i = 0;
@@ -477,7 +471,7 @@ public final class PersistS3 extends Persist {
 
     @Override
     protected String [] update(){
-      Log.debug("Renewing S3 cache.");
+      LOG.debug("Renewing S3 cache.");
       AmazonS3 s3 = getClient();
       ObjectListing currentList = s3.listObjects(_bucket,"");
       ArrayList<String> res = new ArrayList<>();
