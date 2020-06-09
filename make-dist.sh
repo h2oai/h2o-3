976bcea31594090485eaf4f1172a8523bceaf381
michalkurka <michalk@h2o.ai>
2020-06-09 14:33:56 -0400

    add ALL documentation to the h2odriver zip

--- make-dist.sh
+++ make-dist.sh
@@ -52,6 +52,8 @@ function make_hadoop_zip {
   IMAGEDIR=${TOPDIR}/h2o-dist/tmp/${PROJECT_BASE}
 
   mkdir -p $IMAGEDIR
+  mkdir $IMAGEDIR/docs
+  cp -r target/docs-website/* $IMAGEDIR/docs/ 
   cp h2o-hadoop-*/h2o-${HADOOP_VERSION}-assembly/build/libs/h2odriver.jar $IMAGEDIR
   cat h2o-dist/hadoop/README.txt | sed -e "s/SUBST_BRANCH_NAME/${BRANCH_NAME}/g" | sed -e "s/SUBST_BUILD_NUMBER/${BUILD_NUMBER}/g" > ${IMAGEDIR}/README.txt
   cat h2o-dist/hadoop/distribution.info | sed -e "s/H2O_VERSION/${PROJECT_VERSION}/g" | sed -e "s/HADOOP_VERSION/${HADOOP_VERSION}/g" > ${IMAGEDIR}/distribution.info
@@ -83,6 +85,29 @@ make -f scripts/jenkins/Makefile.jenkins r-generate-docs
 # Create target dir, which is uploaded to s3.
 mkdir target
 echo ${PROJECT_VERSION} > target/project_version
+
+# Add documentation to target.
+mkdir target/docs-website
+mkdir target/docs-website/h2o-docs
+mkdir target/docs-website/h2o-docs/booklets
+mkdir target/docs-website/h2o-r
+mkdir target/docs-website/h2o-py
+mkdir target/docs-website/h2o-core
+mkdir target/docs-website/h2o-algos
+mkdir target/docs-website/h2o-genmodel
+mkdir target/docs-website/h2o-scala_2.10
+mkdir target/docs-website/h2o-scala_2.11
+cp -rp h2o-docs/src/product/_build/html/* target/docs-website/h2o-docs
+cp -rp h2o-docs/web/* target/docs-website/h2o-docs
+cp -p h2o-docs/src/booklets/v2_2015/source/*.pdf target/docs-website/h2o-docs/booklets
+cp -p h2o-r/R/h2o_package.pdf target/docs-website/h2o-r
+cp -rp h2o-py/docs/docs target/docs-website/h2o-py
+cp -rp h2o-r/h2o-package/docs target/docs-website/h2o-r
+cp -rp h2o-core/build/docs/javadoc target/docs-website/h2o-core
+cp -rp h2o-algos/build/docs/javadoc target/docs-website/h2o-algos
+cp -rp h2o-genmodel/build/docs/javadoc target/docs-website/h2o-genmodel
+cp -rp h2o-scala/build/h2o-scala_2.10/docs/scaladoc target/docs-website/h2o-scala_2.10
+cp -rp h2o-scala/build/h2o-scala_2.11/docs/scaladoc target/docs-website/h2o-scala_2.11
 
 # Create zip files and add them to target.
 make_zip
@@ -138,29 +163,6 @@ cp -rp build/repo target/maven
 
 # Generate SHA256 from zip file
 (cd target && sha256sum h2o-*.zip > sha256.txt)
-
-# Add documentation to target.
-mkdir target/docs-website
-mkdir target/docs-website/h2o-docs
-mkdir target/docs-website/h2o-docs/booklets
-mkdir target/docs-website/h2o-r
-mkdir target/docs-website/h2o-py
-mkdir target/docs-website/h2o-core
-mkdir target/docs-website/h2o-algos
-mkdir target/docs-website/h2o-genmodel
-mkdir target/docs-website/h2o-scala_2.10
-mkdir target/docs-website/h2o-scala_2.11
-cp -rp h2o-docs/src/product/_build/html/* target/docs-website/h2o-docs
-cp -rp h2o-docs/web/* target/docs-website/h2o-docs
-cp -p h2o-docs/src/booklets/v2_2015/source/*.pdf target/docs-website/h2o-docs/booklets
-cp -p h2o-r/R/h2o_package.pdf target/docs-website/h2o-r
-cp -rp h2o-py/docs/docs target/docs-website/h2o-py
-cp -rp h2o-r/h2o-package/docs target/docs-website/h2o-r
-cp -rp h2o-core/build/docs/javadoc target/docs-website/h2o-core
-cp -rp h2o-algos/build/docs/javadoc target/docs-website/h2o-algos
-cp -rp h2o-genmodel/build/docs/javadoc target/docs-website/h2o-genmodel
-cp -rp h2o-scala/build/h2o-scala_2.10/docs/scaladoc target/docs-website/h2o-scala_2.10
-cp -rp h2o-scala/build/h2o-scala_2.11/docs/scaladoc target/docs-website/h2o-scala_2.11
 
 # Copy content of distribution site
 cp h2o-dist/* target/ 2>/dev/null || true
