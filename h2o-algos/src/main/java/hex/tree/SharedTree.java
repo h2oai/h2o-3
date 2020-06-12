ad1f2de058f47711ca1ea94355b6d4190a7db3c3
michalkurka <michalk@h2o.ai>
2020-06-12 14:28:18 -0400

    Use ignored verbose logging parameter

--- h2o-algos/src/main/java/hex/tree/SharedTree.java
+++ h2o-algos/src/main/java/hex/tree/SharedTree.java
@@ -240,9 +240,9 @@ public abstract class SharedTree<
             boolean verboseSampling = Boolean.getBoolean(H2O.OptArgs.SYSTEM_PROP_PREFIX + "debug.sharedTree.sampleFrameStratified.verbose");
             Frame stratified;
             if(isQuasibinomial) {
-              stratified = water.util.MRUtils.sampleFrameStratified(_train, _train.lastVec(), _train.vec(_model._output.weightsName()), trainSamplingFactors, (long) (_parms._max_after_balance_size * _train.numRows()), _parms._seed, true, false, domain);
+              stratified = water.util.MRUtils.sampleFrameStratified(_train, _train.lastVec(), _train.vec(_model._output.weightsName()), trainSamplingFactors, (long) (_parms._max_after_balance_size * _train.numRows()), _parms._seed, true, verboseSampling, domain);
             } else {
-              stratified = water.util.MRUtils.sampleFrameStratified(_train, _train.lastVec(), _train.vec(_model._output.weightsName()), trainSamplingFactors, (long) (_parms._max_after_balance_size * _train.numRows()), _parms._seed, true, false, null);
+              stratified = water.util.MRUtils.sampleFrameStratified(_train, _train.lastVec(), _train.vec(_model._output.weightsName()), trainSamplingFactors, (long) (_parms._max_after_balance_size * _train.numRows()), _parms._seed, true, verboseSampling, null);
             }
             if (stratified != _train) {
               _train = stratified;
