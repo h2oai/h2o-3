afdddb6053669dedb390d6d1c387dd85f79828ad
Jan Sterba <info@jansterba.com>
2020-06-18 09:20:01 +0200

    fix imports

--- h2o-core/src/test/java/water/webserver/EchoServletProvider.java
+++ h2o-core/src/test/java/water/webserver/EchoServletProvider.java
@@ -9,14 +9,9 @@ import water.webserver.iface.WebsocketConnection;
 import water.webserver.iface.WebsocketHandler;
 import water.webserver.iface.WebsocketServlet;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
