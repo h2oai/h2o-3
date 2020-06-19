3afd486db0ae5c8f7b33a1912a2caf770bd21bb2
michalkurka <michalk@h2o.ai>
2020-06-19 11:24:23 -0400

    Fix: _bits cannot be transient, duh!

--- h2o-core/src/main/java/water/LightKeyed.java
+++ h2o-core/src/main/java/water/LightKeyed.java
@@ -1,12 +1,16 @@
 package water;
 
-public abstract class LightKeyed<T extends LightKeyed> extends Keyed<T> {
+public abstract class LightKeyed<T extends LightKeyed<T>> extends Keyed<T> {
 
     public LightKeyed( Key<T> key ) {
         super(key);
     }
 
-    public abstract T reloadFromBytes(Key k, byte [] ary);
+    public abstract T reloadFromBytes(Key<T> k, byte[] ary);
     public abstract byte[] asBytes();
 
+    @Override
+    public T reloadFromBytes(byte[] ary) {
+        throw new IllegalStateException("Reloading from bytes is not supported for LightKeyed objects");
+    }
 }
