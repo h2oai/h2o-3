451708a0717bdf7384606da9e8473a16dcef3d6b
Jan Sterba <info@jansterba.com>
2020-06-30 16:13:55 +0200

    [R] remove links to sparkling-water managed demo files

--- h2o-r/h2o-package/R/word2vec.R
+++ h2o-r/h2o-package/R/word2vec.R
@@ -25,8 +25,7 @@
 #' h2o.init()
 #' 
 #' # Import the CraigslistJobTitles dataset
-#' f <- "https://raw.githubusercontent.com/h2oai/sparkling-water/rel-1.6/examples/smalldata/"
-#' job_titles <- h2o.importFile(paste0(f, "craigslistJobTitles.csv"), 
+#' job_titles <- h2o.importFile("https://s3.amazonaws.com/h2o-public-test-data/smalldata/craigslistJobTitles.csv", 
 #'                              col.names = c("category", "jobtitle"),
 #'                              col.types = c("String", "String"), header = TRUE)
 #' 
