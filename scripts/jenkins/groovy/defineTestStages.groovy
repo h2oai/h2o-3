2bec3992520496fba727f2eb06b2e305e6849202
michalkurka <michalk@h2o.ai>
2020-08-12 15:36:31 -0400

    Increase open files limit for XGBoost stress test to 150
    100 seems too low after recent changes; no sign of a leak

--- scripts/jenkins/groovy/defineTestStages.groovy
+++ scripts/jenkins/groovy/defineTestStages.groovy
@@ -399,7 +399,7 @@ def call(final pipelineContext) {
     ],
     [ // These run with reduced number of file descriptors for early detection of FD leaks
       stageName: 'XGBoost Stress tests', target: 'test-pyunit-xgboost-stress', pythonVersion: '3.5', timeoutValue: 40,
-      component: pipelineContext.getBuildConfig().COMPONENT_PY, customDockerArgs: [ '--ulimit nofile=100:100' ]
+      component: pipelineContext.getBuildConfig().COMPONENT_PY, customDockerArgs: [ '--ulimit nofile=150:150' ]
     ],
     [
       stageName: 'Kubernetes', target: 'test-h2o-k8s', timeoutValue: 20,
