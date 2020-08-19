6ad2eb87900d2ae75d6ffc9029d976a1e112418d
ardKelmendi <kelmeard@fit.cvut.cz>
2020-08-20 00:40:09 +0200

    Randomly Shuffle MRTask Current version of ShuffleVec class. This is being moved to VecUtils and one generalised map task. Also taking into account that this version doesn't work for bigger data.

--- h2o-core/src/main/java/water/rapids/ShuffleVec.java
+++ h2o-core/src/main/java/water/rapids/ShuffleVec.java
@@ -2,8 +2,10 @@ package water.rapids;
 
 import water.MRTask;
 import water.fvec.Chunk;
+import water.fvec.NewChunk;
 import water.fvec.Vec;
 import water.fvec.Frame;
+import water.parser.BufferedString;
 import water.util.FrameUtils;
 import water.util.RandomUtils;
 
@@ -11,67 +13,65 @@ import java.util.Random;
 
 import static water.util.RandomUtils.getRNG;
 
-public class ShuffleVec  {
-    
-    Frame _fr;
-    String _col_name;
-    
-    
-    
-    public ShuffleVec(Frame fr) { 
-        _fr = fr;    
-    }
-    
-    /* create a frame with shuffled columns */
-    public /*Frame*/ void ShuffleFrameVecs(int features_no, Frame out){
-        Vec v_shuffled [] = new Vec[features_no];
-        for (int f_no = 0 ; f_no < features_no ; f_no++){
-            v_shuffled [f_no] = ShuffleTask.shuffle(_fr.vec(f_no));
-            
-        }
-    }
-    
+public class ShuffleVec {
 
-    public Vec getVecInArray(String col_names ) {
-//        d_vals = new FrameUtils.Vec2ArryTsk((int) _fr.numRows()).doAll(_fr.vec(_col_name)).res;
-        return ShuffleTask.shuffle(_fr.vec(_col_name));
-//        long d = v_shuffled.length();
-
-//        RandomUtils.getRNG()
+    public static long seed(int cidx) {
+        return (0xe031e74f321f7e29L + ((long) cidx << 32L));
     }
-    public static long seed(int cidx) { return (0xe031e74f321f7e29L + ((long)cidx << 32L)); }
 
     public static class ShuffleTask extends MRTask<ShuffleTask> {
 
-        @Override public void map(Chunk ic, Chunk oc) {
-            if (ic._len==0) return;
-            
+        @Override
+        public void map(Chunk ic, Chunk oc) {
+            if (ic._len == 0) return;
+
             Random rand = new Random();
             long rseed = rand.nextLong();
             Random rng = getRNG(rseed);
-            for (int row=1; row<ic._len; row++) {
-                int j = rng.nextInt(row+1); // inclusive upper bound <0,row>
-                if (j!=row) oc.set(row, oc.atd(j));
+            for (int row = 1; row < ic._len; row++) {
+                int j = rng.nextInt(row + 1); // inclusive upper bound <0,row>
+                if (j != row) oc.set(row, oc.atd(j));
                 oc.set(j, ic.atd(row));
             }
         }
 
-/* 
-    dont forget to check !
-    
-    for (int i = 0; i < n; ++i)
-      assert(ArrayUtils.contains(the array, a[i]));
-    return result;
-     
-*/
-
-    
         public static Vec shuffle(Vec ivec) {
-        Vec ovec = ivec.makeZero();
-        new ShuffleTask().doAll(ivec, ovec).outputFrame();
-        return ovec;
+            Vec ovec = Vec.makeZero(ivec.length(), ivec.get_type());
+            switch (ivec.get_type()) {
+                case Vec.T_STR:
+                    new ShuffleTaskString().doAll(ivec, ovec).outputFrame();
+                    break;
+                case Vec.T_NUM:
+                    new ShuffleTask().doAll(ivec, ovec).outputFrame();
+                    break;
+                default:
+                    try{
+//                    new ShuffleTask().doAll(ivec, ovec).outputFrame();
+                        new ShuffleTask().doAll(ivec).outputFrame();
+                    } catch (Exception e){
+                        throw new IllegalArgumentException("vec type not supported yet");
+                    }
+            }
+            return ovec;
         }
-        
-    }
-}
 
+        public static class ShuffleTaskString extends MRTask<ShuffleTaskString> {
+
+            @Override
+            public void map(Chunk ic, NewChunk oc) { //consider NewChunk oc
+//                if (cs[i] instanceof CStrChunk)  // <- try dis w/ NewChunk
+                Random rand = new Random();
+                long rseed = rand.nextLong();
+                Random rng = getRNG(rseed);
+                BufferedString tmpStr = new BufferedString();
+                for (int row = 1; row < ic._len; row++) {
+                    int j = rng.nextInt(row + 1); // inclusive upper bound <0,row>
+                    String _tmpStr = ic.stringAt(j);
+                    if (j != row) oc.setAny(row, _tmpStr);
+                    oc.setAny(j, ic.stringAt(row));
+                }
+            }
+        }
+    }
+
+}
