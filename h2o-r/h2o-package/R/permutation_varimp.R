00d25df3f8c2ece9de347125c9d204e6c698e248
ardKelmendi <kelmeard@fit.cvut.cz>
2020-08-20 02:41:02 +0200

    Fix R errors
    +Typo
    "permutation_varim" -> permutation_varimp

--- h2o-r/h2o-package/R/permutation_varimp.R
+++ h2o-r/h2o-package/R/permutation_varimp.R
@@ -2,12 +2,13 @@
 #' Calculate Permutation Feature Importance 
 #'
 
-h2o.permutation_varim() <- function(model, validation_frame){
+h2o.permutation_varimp <- function(model, validation_frame){
     if (is.H2OFrame(validation_frame))
          tryCatch(permutation_varim_table <- .newExpr('Perm_Feature_importance', model, validation_frame), 
                 error = function(err) {
                     message(err)
-                    stop("argument " validation_frame" is a valid H2OFrame, newExpr didnt work")
+                    message("validation frame is a valid H2OFrame, newExpr didnt work")
+                    stop("argument  is a valid H2OFrame, newExpr didnt work")
                 },
                 warning = function(err){
                     message("warning message:")
@@ -17,6 +18,6 @@ h2o.permutation_varim() <- function(model, validation_frame){
                     message("Loading permutation_varim")
                     return(permutation_varim_table)
                 })
-    else stop("Input frame" validation_frame "is not H2OFrame") # find out how to check for model
+    else stop("Input frame is not H2OFrame") # find out how to check for model
         
 }
