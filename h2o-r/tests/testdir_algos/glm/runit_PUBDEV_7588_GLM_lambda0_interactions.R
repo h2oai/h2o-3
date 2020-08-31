47fa60d99bd44958f5563f13d579ccb63f1ecc1a
wendycwong <wendycwong@yahoo.com>
2020-08-31 16:10:48 -0700

    Switch from public to private for datasets

--- h2o-r/tests/testdir_algos/glm/runit_PUBDEV_7588_GLM_lambda0_interactions.R
+++ h2o-r/tests/testdir_algos/glm/runit_PUBDEV_7588_GLM_lambda0_interactions.R
@@ -4,18 +4,18 @@ source("../../../scripts/h2o-r-test-setup.R")
 # This test is mainly written to compare R glm with H2O glm when we have interaction columns.  The coefficients
 # should be very close between the two methods.
 test.glm.interactions.lambda0 <- function() {
-   data <- h2o.importFile("https://raw.githubusercontent.com/guru99-edu/R-Programming/master/adult.csv")
+   data <- h2o.importFile(locate("smalldata/census_income/adult_data.csv"))
    temp <- data["hours-per-week"] / data["age"]
    names(temp) <- "y"
    dataCombined <- h2o.cbind(data, temp)
    dataR <- as.data.frame(dataCombined)
 
-   glmH2O <- h2o.glm(x=c("income", "gender"), y="y", training_frame=dataCombined, interactions=c("income", "gender"), lambda=0)
-   glmR <- lm(y ~income*gender+income+gender, data=dataR)
+   glmH2O <- h2o.glm(x=c("income", "sex"), y="y", training_frame=dataCombined, interactions=c("income", "sex"), lambda=0)
+   glmR <- lm(y ~income*sex+income+sex, data=dataR)
    coeffR <- glmR$coefficients
    coeffH2O <- glmH2O@model$coefficients
-   rcoeffNames <- c("(Intercept)", "income>50K", "genderMale", "income>50K:genderMale")
-   h2ocoeffNames <- c("Intercept", "income.>50K", "gender.Male", "income_gender.>50K_Male")
+   rcoeffNames <- c("(Intercept)", "income>50K", "sexMale", "income>50K:sexMale")
+   h2ocoeffNames <- c("Intercept", "income.>50K", "sex.Male", "income_sex.>50K_Male")
    
    # compare coefficient between R and H2O glm model.  They should be close
    for (ind in seq_len(length(rcoeffNames))) {
