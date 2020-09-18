8fa28e9b6fd6eb081e7a49725d1a52396fc7a158
Jan Sterba <info@jansterba.com>
2020-09-18 19:31:42 +0200

    increase xgb smoke max memory

--- h2o-extensions/xgboost/testMultiNode.sh
+++ h2o-extensions/xgboost/testMultiNode.sh
@@ -58,7 +58,7 @@ fi
 #   build/classes/test - Test h2o core classes
 #   build/resources/main - Main resources (e.g. page.html)
 
-MAX_MEM=${H2O_JVM_XMX:-2500m}
+MAX_MEM=${H2O_JVM_XMX:-4g}
 
 # Check if coverage should be run
 if [ $JACOCO_ENABLED = true ]
