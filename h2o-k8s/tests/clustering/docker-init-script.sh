0d205948a3505a75fd44826fc89c7a9a660f2d5b
Pavel Pscheidl <pavel@h2o.ai>
2020-10-01 19:18:53 +0200

    Customizable command

--- h2o-k8s/tests/clustering/docker-init-script.sh
+++ h2o-k8s/tests/clustering/docker-init-script.sh
@@ -1,6 +1,7 @@
 #! /bin/bash -x
 
 pwd
+ls
 export H2O_BASE=$(pwd)
 if [[ $string == *"@"* ]]; then
   echo "H2O base path contains at sign. Unable to create K3S cluster."
@@ -15,8 +16,9 @@ kubectl cluster-info
 sleep 15 # Making sure the default namespace is initialized. The --wait flag does not guarantee this.
 kubectl get namespaces
 envsubst < testvalues-template.yaml >> testvalues.yaml
-helm install -f testvalues.yaml h2o $H2O_BASE/h2o-helm/
+helm install -f testvalues.yaml h2o $H2O_BASE/h2o-helm --kubeconfig $KUBECONFIG
 helm test h2o
+kubectl get ingresses
 kubectl describe pods
 timeout 120s bash h2o-cluster-check.sh
 export EXIT_STATUS=$?
