9b776d53f8c8d61e12e78c0e6bff539e6781dabd
ardKelmendi <kelmeard@fit.cvut.cz>
2020-10-19 17:56:39 +0200

    Ast class supporting passing the metric + Shuffle MRTask tests

--- h2o-core/src/main/java/water/rapids/ast/prims/models/AstPermutationFeatureImportance.java
+++ h2o-core/src/main/java/water/rapids/ast/prims/models/AstPermutationFeatureImportance.java
@@ -7,7 +7,6 @@ import water.Scope;
 import water.fvec.Frame;
 import water.fvec.Vec;
 import water.rapids.Env;
-import water.rapids.PermutationFeatureImportance;
 import water.rapids.ast.AstPrimitive;
 import water.rapids.ast.AstRoot;
 import water.rapids.vals.ValFrame;
@@ -16,19 +15,20 @@ import water.util.TwoDimTable;
 public class AstPermutationFeatureImportance extends AstPrimitive{
 
     @Override
-    public int nargs() {return 1 + 2;} // Perm_feature_importance + Frame + Model
+    public int nargs() {return 1 + 3;} // Perm_feature_importance + Frame + Model
 
     @Override
-    public String[] args() {return new String [] {"frame", "model"};} 
+    public String[] args() {return new String [] {"frame", "model", "metric"};} 
     
     @Override
-    public String str() {return "Perm_Feature_importance";} // or call it model_reliance 
+    public String str() {return "PermutationVarImp";} 
     
     @Override
     public ValFrame apply(Env env, Env.StackHelp stk, AstRoot asts[]){
-        Frame in_fr = stk.track(asts[1].exec(env)).getFrame();
-        Model model = stk.track(asts[2].exec(env)).getModel();
-
+        Model model = stk.track(asts[1].exec(env)).getModel();
+        Frame in_fr = stk.track(asts[2].exec(env)).getFrame();
+        String metric = stk.track(asts[3].exec(env)).getStr();
+        
         Scope.enter();
         Frame pred_fr = null;
         Frame FiFrame = null;
@@ -36,10 +36,7 @@ public class AstPermutationFeatureImportance extends AstPrimitive{
             pred_fr = model.score(in_fr); // prediction on original dataset
             Scope.track(pred_fr);
 
-            PermutationFeatureImportance fi = new PermutationFeatureImportance(model, in_fr, pred_fr);
-//            HashMap<String, Double> FI = fi.getFeatureImportance(); // might be able to use AstPerfectAuc for calculation of AUC 
-
-            TwoDimTable varImp_t = model.getPermVarImpTable_oat(in_fr, pred_fr);
+            TwoDimTable varImp_t = model.getPermVarImpTable(in_fr, metric);
             
             // Frame contains one row and n features 
             FiFrame = new Frame(Key.make(model._key + "_feature_imp"));
