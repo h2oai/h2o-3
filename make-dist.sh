h2oai/h2o-3

commit 087e771366a766390527ff9c62404a4b0cfc5c23

Gradle 6.6 and build Javadoc only for release (#5047)
* Gradle 6.6 and build Javadoc only for release

* Dev image version 36/ hadoop version 83

gradle/publish.gradle
@@ -23,6 +23,16 @@ artifacts {
     toPublish sourcesJar
 }
 
+if (project.ext.isRelease) {
+    java {
+        withJavadocJar()
+    }
+
+    javadoc {
+        options.encoding = 'UTF-8'
+    }
+}
+
 //
 // Record all generated POM files for given publishing task
 //

gradle/java.gradle
@@ -10,14 +10,6 @@ compileJava {
   options.encoding = "UTF-8"
 }
 
-java {
-    withJavadocJar()
-}
-
-javadoc {
-    options.encoding = 'UTF-8'
-}
-
 task testJar(type: Jar, dependsOn: testClasses, group: "build") {
     classifier = 'test'
     from sourceSets.test.output

gradle/wrapper/gradle-wrapper.properties
@@ -1,6 +1,5 @@
-#Fri May 01 15:10:34 CEST 2020
-distributionUrl=https\://services.gradle.org/distributions/gradle-6.3-all.zip
 distributionBase=GRADLE_USER_HOME
 distributionPath=wrapper/dists
-zipStorePath=wrapper/dists
+distributionUrl=https\://services.gradle.org/distributions/gradle-6.6.1-bin.zip
 zipStoreBase=GRADLE_USER_HOME
+zipStorePath=wrapper/dists

gradlew
@@ -154,19 +154,19 @@ if [ "$cygwin" = "true" -o "$msys" = "true" ] ; then
         else
             eval `echo args$i`="\"$arg\""
         fi
-        i=$((i+1))
+        i=`expr $i + 1`
     done
     case $i in
-        (0) set -- ;;
-        (1) set -- "$args0" ;;
-        (2) set -- "$args0" "$args1" ;;
-        (3) set -- "$args0" "$args1" "$args2" ;;
-        (4) set -- "$args0" "$args1" "$args2" "$args3" ;;
-        (5) set -- "$args0" "$args1" "$args2" "$args3" "$args4" ;;
-        (6) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" ;;
-        (7) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" ;;
-        (8) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" "$args7" ;;
-        (9) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" "$args7" "$args8" ;;
+        0) set -- ;;
+        1) set -- "$args0" ;;
+        2) set -- "$args0" "$args1" ;;
+        3) set -- "$args0" "$args1" "$args2" ;;
+        4) set -- "$args0" "$args1" "$args2" "$args3" ;;
+        5) set -- "$args0" "$args1" "$args2" "$args3" "$args4" ;;
+        6) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" ;;
+        7) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" ;;
+        8) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" "$args7" ;;
+        9) set -- "$args0" "$args1" "$args2" "$args3" "$args4" "$args5" "$args6" "$args7" "$args8" ;;
     esac
 fi
 
@@ -175,14 +175,9 @@ save () {
     for i do printf %s\\n "$i" | sed "s/'/'\\\\''/g;1s/^/'/;\$s/\$/' \\\\/" ; done
     echo " "
 }
-APP_ARGS=$(save "$@")
+APP_ARGS=`save "$@"`
 
 # Collect all arguments for the java command, following the shell quoting and substitution rules
 eval set -- $DEFAULT_JVM_OPTS $JAVA_OPTS $GRADLE_OPTS "\"-Dorg.gradle.appname=$APP_BASE_NAME\"" -classpath "\"$CLASSPATH\"" org.gradle.wrapper.GradleWrapperMain "$APP_ARGS"
-
-# by default we should be in the correct project dir, but when run from Finder on Mac, the cwd is wrong
-if [ "$(uname)" = "Darwin" ] && [ "$HOME" = "$PWD" ]; then
-  cd "$(dirname "$0")"
-fi
 
 exec "$JAVACMD" "$@"

gradlew.bat
@@ -29,6 +29,9 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 

scripts/jenkins/groovy/buildConfig.groovy
@@ -14,12 +14,12 @@ class BuildConfig {
   private static final String DEFAULT_HADOOP_IMAGE_NAME_PREFIX = 'dev-build-hadoop-gradle'
   private static final String DEFAULT_RELEASE_IMAGE_NAME_PREFIX = 'dev-release-gradle'
 
-  public static final int DEFAULT_IMAGE_VERSION_TAG = 35
+  public static final int DEFAULT_IMAGE_VERSION_TAG = 36
   public static final String AWSCLI_IMAGE = DOCKER_REGISTRY + '/opsh2oai/awscli'
   public static final String S3CMD_IMAGE = DOCKER_REGISTRY + '/opsh2oai/s3cmd'
 
   private static final String HADOOP_IMAGE_NAME_PREFIX = 'h2o-3-hadoop'
-  private static final String HADOOP_IMAGE_VERSION_TAG = '82'
+  private static final String HADOOP_IMAGE_VERSION_TAG = '83'
 
   private static final String K8S_TEST_IMAGE_VERSION_TAG = '3' // Change the new Docker image in the K8S YAML template as well
 

commit 94d2a397e5f27afdf81c94871e37816b43d8f5ce

fix failing build - release at CI needs to make javadoc
caused by 087e771366a766390527ff9c62404a4b0cfc5c23

Javadoc was not created when nexus upload was not enabled by jenkins parameters. Then `cp: cannot stat 'h2o-core/build/docs/javadoc': No such file or directory` appeared.

build.gradle
@@ -196,6 +196,7 @@ allprojects {
 
     ext {
         isRelease = rootProject.hasProperty("doRelease")
+        isMakeJavadoc = rootProject.hasProperty("makeJavadoc")
         isCi = isCi
     }
 }

gradle/publish.gradle
@@ -23,7 +23,7 @@ artifacts {
     toPublish sourcesJar
 }
 
-if (project.ext.isRelease) {
+if (project.ext.isMakeJavadoc) {
     java {
         withJavadocJar()
     }

make-dist.sh
@@ -69,9 +69,13 @@ if [ -n "$DO_RELEASE" ]; then
   DO_RELEASE="-PdoRelease"
 fi
 
+if [ -n "$MAKE_JAVADOC" ]; then
+  MAKE_JAVADOC="-PmakeJavadoc"
+fi
+
 # Run some required gradle tasks to produce final build output.
 ./gradlew booklets
-./gradlew $DO_RELEASE publish
+./gradlew $DO_RELEASE $MAKE_JAVADOC publish
 
 # Generate Py Docs
 (cd h2o-py && sphinx-build -b html docs/ docs/docs/)

scripts/jenkins/jenkinsfiles/Jenkinsfile-Release
@@ -659,6 +659,9 @@ private setReleaseJobProperties(final pipelineContext) {
             env.DO_RELEASE = true
         }
     }
+
+    env.MAKE_JAVADOC = true
+
     sh "printenv | sort"
 }
 