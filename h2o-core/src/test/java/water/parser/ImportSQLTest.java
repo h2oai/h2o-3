ea39b067420c4b245382eb439949caac908939a0
Ondra Nekola <ondrej.nekola@h2o.ai>
2020-10-22 17:02:28 +0200

    wip

--- h2o-core/src/test/java/water/parser/ImportSQLTest.java
+++ h2o-core/src/test/java/water/parser/ImportSQLTest.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 import java.util.Arrays;
 
 @RunWith(H2ORunner.class)
-@CloudSize(5)
+@CloudSize(2)
 public class ImportSQLTest {
   public static final int ROW_COUNT = 20_000_000;
   //MySQL
