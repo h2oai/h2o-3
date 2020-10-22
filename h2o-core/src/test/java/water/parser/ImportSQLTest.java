e75e0593c4a0f6afa74faff98c3ab496904a52e4
Ondra Nekola <ondrej.nekola@h2o.ai>
2020-10-22 16:36:49 +0200

    WIP add h2 support to tests

--- h2o-core/src/test/java/water/parser/ImportSQLTest.java
+++ h2o-core/src/test/java/water/parser/ImportSQLTest.java
@@ -1,21 +1,36 @@
 package water.parser;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import water.TestUtil;
 import water.fvec.Frame;
 import water.jdbc.SQLManager;
 import water.jdbc.SqlFetchMode;
+import water.runner.CloudSize;
+import water.runner.H2ORunner;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class ImportSQLTest extends TestUtil{
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+@RunWith(H2ORunner.class)
+@CloudSize(5)
+public class ImportSQLTest {
+  public static final int ROW_COUNT = 20_000_000;
   //MySQL
-  private String conUrl = "jdbc:mysql://172.16.2.178:3306/ingestSQL?&useSSL=false";
-  String user = "root";
-  String password = "0xdata";
+  static private String conUrl;
+  static private Server server;
+  String user = "sa";
+  String password = "";
   
   //postgresql
 //  String conUrl = "jdbc:postgresql://mr-0xf2/ingestSQL";
@@ -26,18 +41,69 @@ public class ImportSQLTest extends TestUtil{
   String columns = "*";
   
   @BeforeClass
-  static public void setup() {stall_till_cloudsize(1);}
+  static public void setup() throws SQLException {
+    startDB();
+    fillDB();
+  }
 
-  @Ignore @Test
+  private static void startDB() throws SQLException {
+    server = Server.createTcpServer("-tcpAllowOthers", "-ifNotExists").start();
+    server.start();
+    String serverURL = server.getURL();
+    System.out.println(serverURL);
+    
+    conUrl = "jdbc:h2:" + serverURL + "/~/test.t";
+  }
+
+  private static void fillDB() throws SQLException {
+    Connection connection = DriverManager.
+            getConnection(conUrl, "sa", "");
+    // add application code here
+
+    connection.createStatement().executeUpdate("CREATE TABLE foo (id INTEGER not NULL, i INTEGER, s VARCHAR(256));");
+    
+    connection.setAutoCommit(false);
+    
+    final String query = "INSERT INTO foo (id, i, s) VALUES (?, ?, ?)";
+    
+    PreparedStatement ps = connection.prepareStatement(query);
+    for (int i = 0; i < ROW_COUNT; i++) {
+      ps.setInt(1, i);
+      ps.setInt(2, i);
+      ps.setString(3, "__" + i + "__");
+      ps.addBatch();
+    }
+    ps.executeBatch();
+    connection.commit();
+
+    connection.close();
+  }
+
+
+    @AfterClass
+  static public void shutdown() throws SQLException {
+    Connection connection = DriverManager.getConnection(conUrl, "sa", "");
+    connection.createStatement().executeUpdate("DROP TABLE foo;");
+    server.shutdown();
+  }
+
+  @Test
   public void citibike20k() {
     String table = "citibike20k";
-    Frame sql_f = SQLManager.importSqlTable(conUrl, table, select_query, user, password, columns, null, null, SqlFetchMode.DISTRIBUTED, null).get();
-    assertTrue(sql_f.numRows() == 2e4);
-    assertTrue(sql_f.numCols() == 15);
-    sql_f.delete();
-    sql_f = SQLManager.importSqlTable(conUrl, table, select_query, user, password, "bikeid, starttime", null, null, SqlFetchMode.DISTRIBUTED, null).get();
-    assertTrue(sql_f.numRows() == 2e4);
-    assertTrue(sql_f.numCols() == 2);
+    Frame sql_f = SQLManager.importSqlTable(conUrl, "foo", select_query, user, password, columns, null, null, SqlFetchMode.DISTRIBUTED, 5).get();
+    assertTrue(sql_f.numRows() == ROW_COUNT);
+    System.out.println(Arrays.toString(sql_f.names()));
+    double min = sql_f.vec("I").min();
+    System.out.println(min);
+    assertTrue(min == 0);
+    double max = sql_f.vec("I").max();
+    System.out.println(max);
+    assertTrue(max == (20_000_000.0 - 1));
+//    assertTrue(sql_f.numCols() == 15);
+//    sql_f.delete();
+//    sql_f = SQLManager.importSqlTable(conUrl, table, select_query, user, password, "bikeid, starttime", null, null, SqlFetchMode.DISTRIBUTED, null).get();
+//    assertTrue(sql_f.numRows() == 2e4);
+//    assertTrue(sql_f.numCols() == 2);
     sql_f.delete();
   }
   
