2833511266bacb43d2747cc7d7be2e885f276f65
ardKelmendi <kelmeard@fit.cvut.cz>
2020-11-09 16:53:46 +0100

    AstOneAtaTimeMorris.java
    Final version of OAT Ast

---
+++ h2o-core/src/main/java/water/rapids/ast/prims/models/AstOneAtaTimeMorris.java
@@ -0,0 +1,75 @@
+package water.rapids.ast.prims.models;
+
+import hex.Model;
+import water.Key;
+import water.Scope;
+import water.fvec.Frame;
+import water.fvec.Vec;
+import water.rapids.Env;
+import water.rapids.PermutationVarImp;
+import water.rapids.ast.AstPrimitive;
+import water.rapids.ast.AstRoot;
+import water.rapids.vals.ValFrame;
+import water.util.TwoDimTable;
+
+public class AstOneAtaTimeMorris extends AstPrimitive {
+
+    @Override
+    public int nargs() {return 1 + 2;} // Perm_feature_importance + Frame + Model
+
+    @Override
+    public String[] args() {return new String [] {"model", "frame"};}
+
+    @Override
+    public String str() {return "PermutationVarImpOat";} 
+
+    @Override
+    public ValFrame apply(Env env, Env.StackHelp stk, AstRoot asts[]){
+        Model model = stk.track(asts[1].exec(env)).getModel();
+        Frame in_fr = stk.track(asts[2].exec(env)).getFrame();
+
+        Scope.enter();
+        Frame pviFr = null;
+        try {
+            PermutationVarImp fi = new PermutationVarImp(model, in_fr); 
+            TwoDimTable pvi_table = fi.getPermutationVarImp();
+            TwoDimTable oatTable = fi.oat(); // might be able to use AstPerfectAuc for calculation of AUC 
+            
+            System.out.println(oatTable);
+            // Frame contains one row and n features 
+            pviFr = new Frame(Key.make(model._key + "Oat_pfi"));
+            pviFr.add(headerToStrings(oatTable.getRowHeaders()), rowsToVecs(oatTable));
+            Scope.track(pviFr);
+            
+            Scope.track(pviFr);
+        } finally {
+            Key[] keysToKeep = pviFr != null ? pviFr.keys() : new Key[]{};
+            Scope.exit(keysToKeep);
+        }
+        return new ValFrame(pviFr);
+    }
+
+    // Rows of TwoDimTable to a Vec [] 
+    Vec[] rowsToVecs(TwoDimTable varImp_t) {
+        Vec[] vecs = new Vec[varImp_t.getRowDim() + 1];
+        // Relative, scaled, and percentage importance
+        vecs[0] = Vec.makeVec(varImp_t.getColHeaders(), Vec.newKey());
+        double[] tmp_row = new double[varImp_t.getColDim()];
+        for (int i = 0; i < varImp_t.getRowDim(); i++) {
+            for (int j = 0; j < varImp_t.getColDim(); j++) {
+                tmp_row[j] = (double) varImp_t.get(i, j);
+            }
+            vecs[i + 1] = Vec.makeVec(tmp_row, Vec.newKey());
+        }
+        return vecs;
+    }
+
+    // Features to String []
+    String[] headerToStrings(String[] table_names) {
+        String[] varNames = new String[table_names.length + 1];
+        varNames[0] = "indices";
+        System.arraycopy(table_names, 0, varNames, 1, table_names.length);
+        // add column containing Strings: Relative importance, Scaled importance, percentage 
+        return varNames;
+    }
+}
