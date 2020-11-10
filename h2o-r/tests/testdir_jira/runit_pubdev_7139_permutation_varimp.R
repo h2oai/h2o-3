8194ea56df8c4d09ba16d963109b2ed28cce9089
Ard <56032881+ardKelmendi@users.noreply.github.com>
2020-11-10 02:06:27 +0100

    Update runit_pubdev_7139_permutation_varimp.R
    removing redundant code

--- h2o-r/tests/testdir_jira/runit_pubdev_7139_permutation_varimp.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_7139_permutation_varimp.R
@@ -2,7 +2,7 @@ setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../scripts/h2o-r-test-setup.R")
 
 test.permutation.varimp <- function(){
-    Log.info("Making glm without validation_frame...")
+    Log.info("Training glm...")
     pros.train <- h2o.uploadFile(locate("smalldata/prostate/prostate.csv.zip"))
 
     pros.glm <- h2o.glm(x = 3:9, y = 2, training_frame = pros.train, family = "binomial")
@@ -14,4 +14,3 @@ test.permutation.varimp <- function(){
 }
 
 doTest("Testing Permutation Feature Importance", test.permutation.varimp)
-
