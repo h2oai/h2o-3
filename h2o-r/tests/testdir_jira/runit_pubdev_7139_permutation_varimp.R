c9b7e1b1ef2af937d7bc856488f30a5e2e8411af
Ard <56032881+ardKelmendi@users.noreply.github.com>
2020-11-10 02:07:05 +0100

    Update runit_pubdev_7139_permutation_varimp.R
    typo

--- h2o-r/tests/testdir_jira/runit_pubdev_7139_permutation_varimp.R
+++ h2o-r/tests/testdir_jira/runit_pubdev_7139_permutation_varimp.R
@@ -11,5 +11,5 @@ prostate.hex <- h2o.uploadFile(path = prosPath, destination_frame = "prostate.he
 prostate.gbm <- h2o.gbm(x = setdiff(colnames(prostate.hex), "CAPSULE"), y = "CAPSULE", training_frame = prostate.hex, ntrees = 10, max_depth = 5, learn_rate = 0.1, distribution = "bernoulli")
 
 Log.info("Calculating Permutation Variable Importance...")
-permutation_varimp <- h2o.permutation_varimp(iris.gbm, prostate.hex)
+permutation_varimp <- h2o.permutation_varimp(prostate.gbm, prostate.hex)
 Log.info(paste0("Permutation Variable importance: ",permutation_varimp))
