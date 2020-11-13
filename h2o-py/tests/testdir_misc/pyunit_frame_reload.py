9fddcacc5d5a8d57e4ac83ceb3d570c4d956e9a6
Honza Sterba <jan.sterba@h2o.ai>
2020-11-13 08:38:04 +0100

    [PUBDEV-7859] fix frame loading (#5127)
    - need to create a VecGroup so other vec creation does not overwrite Frame's vecs

--- h2o-py/tests/testdir_misc/pyunit_frame_reload.py
+++ h2o-py/tests/testdir_misc/pyunit_frame_reload.py
@@ -2,6 +2,9 @@ import h2o
 from h2o.exceptions import H2OResponseError
 from tests import pyunit_utils
 import tempfile
+from collections import OrderedDict
+from h2o.grid.grid_search import H2OGridSearch
+from h2o.estimators.gbm import H2OGradientBoostingEstimator
 
 
 def test_frame_reload():
@@ -25,6 +28,18 @@ def test_frame_reload():
     df_pd_loaded = df_loaded.as_data_frame()
     assert df_pd_orig.equals(df_pd_loaded_force)
     assert df_pd_orig.equals(df_pd_loaded)
+    
+    # try running grid search on the frame
+    h2o.remove_all()
+    df_loaded = h2o.load_frame(df_key, work_dir)
+    hyper_parameters = OrderedDict()
+    hyper_parameters["ntrees"] = [5, 10, 20, 30]
+    grid_small = H2OGridSearch(
+        H2OGradientBoostingEstimator,
+        hyper_params=hyper_parameters
+    )
+    grid_small.train(x=list(range(4)), y=4, training_frame=df_loaded)
+    assert len(grid_small.models) == 4
 
 
 if __name__ == "__main__":
