d76084a44cf49b0155f75edc3cc776d017edf737
Pavel Pscheidl <pavel@h2o.ai>
2020-11-27 14:21:02 +0100

    Migrate R API to use quotechar

--- h2o-r/h2o-package/R/parse.R
+++ h2o-r/h2o-package/R/parse.R
@@ -38,13 +38,14 @@
 h2o.parseRaw <- function(data, pattern="", destination_frame = "", header=NA, sep = "", col.names=NULL,
                          col.types=NULL, na.strings=NULL, blocking=FALSE, parse_type = NULL, chunk_size = NULL,
                          decrypt_tool = NULL, skipped_columns = NULL, custom_non_data_line_markers = NULL, partition_by=NULL,
-                         single_quotes = FALSE) {
+                         quotechar = NULL) {
+  single_qouotes <- if (is.null(quotechar) || quotechar != "'") FALSE else TRUE
   # Check and parse col.types in case col.types is supplied col.name = col.type vec
   if( length(names(col.types)) > 0 & typeof(col.types) != "list" ) {
     parse.params <- h2o.parseSetup(data, pattern="", destination_frame, header, sep, col.names, col.types = NULL,
                                    na.strings = na.strings, parse_type = parse_type, chunk_size = chunk_size,
                                    decrypt_tool = decrypt_tool, skipped_columns=skipped_columns,
-                                   custom_non_data_line_markers = custom_non_data_line_markers, partition_by = partition_by, single_quotes=single_quotes)
+                                   custom_non_data_line_markers = custom_non_data_line_markers, partition_by = partition_by, single_quotes = single_qouotes)
     idx = match(names(col.types), parse.params$column_names)
     parse.params$column_types[idx] = as.character(col.types)
   } else {
@@ -52,7 +53,7 @@ h2o.parseRaw <- function(data, pattern="", destination_frame = "", header=NA, se
                                    na.strings = na.strings, parse_type = parse_type, chunk_size = chunk_size,
                                    decrypt_tool = decrypt_tool, skipped_columns=skipped_columns,
                                    custom_non_data_line_markers = custom_non_data_line_markers,
-                                   partition_by = partition_by,single_quotes = single_quotes)
+                                   partition_by = partition_by, single_quotes = single_qouotes)
   }
   for(w in parse.params$warnings){
     cat('WARNING:',w,'\n')
