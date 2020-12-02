5343a5d8593188c4a256afa5d82cdf4a95c861ca
wendycwong <wendy@h2o.ai>
2020-12-02 14:16:53 -0800

    Add GAM synthetic generation code in Python. (#5109)
    Make sure response is balanced among all the classes for classification.

--- h2o-py/h2o/estimators/gam.py
+++ h2o-py/h2o/estimators/gam.py
@@ -36,11 +36,12 @@ class H2OGeneralizedAdditiveEstimator(H2OEstimator):
                    "solver", "alpha", "lambda_", "lambda_search", "early_stopping", "nlambdas", "standardize",
                    "missing_values_handling", "plug_values", "compute_p_values", "remove_collinear_columns",
                    "intercept", "non_negative", "max_iterations", "objective_epsilon", "beta_epsilon",
-                   "gradient_epsilon", "link", "prior", "cold_start", "lambda_min_ratio", "beta_constraints",
-                   "max_active_predictors", "interactions", "interaction_pairs", "obj_reg", "export_checkpoints_dir",
-                   "stopping_rounds", "stopping_metric", "stopping_tolerance", "balance_classes",
-                   "class_sampling_factors", "max_after_balance_size", "max_confusion_matrix_size", "max_runtime_secs",
-                   "custom_metric_func", "num_knots", "knot_ids", "gam_columns", "bs", "scale", "keep_gam_cols"}
+                   "gradient_epsilon", "link", "startval", "prior", "cold_start", "lambda_min_ratio",
+                   "beta_constraints", "max_active_predictors", "interactions", "interaction_pairs", "obj_reg",
+                   "export_checkpoints_dir", "stopping_rounds", "stopping_metric", "stopping_tolerance",
+                   "balance_classes", "class_sampling_factors", "max_after_balance_size", "max_confusion_matrix_size",
+                   "max_runtime_secs", "custom_metric_func", "num_knots", "knot_ids", "gam_columns", "bs", "scale",
+                   "keep_gam_cols"}
 
     def __init__(self, **kwargs):
         super(H2OGeneralizedAdditiveEstimator, self).__init__()
@@ -627,6 +628,21 @@ class H2OGeneralizedAdditiveEstimator(H2OEstimator):
     def link(self, link):
         assert_is_type(link, None, Enum("family_default", "identity", "logit", "log", "inverse", "tweedie", "ologit"))
         self._parms["link"] = link
+
+
+    @property
+    def startval(self):
+        """
+        double array to initialize coefficients for GAM.
+
+        Type: ``List[float]``.
+        """
+        return self._parms.get("startval")
+
+    @startval.setter
+    def startval(self, startval):
+        assert_is_type(startval, None, [numeric])
+        self._parms["startval"] = startval
 
 
     @property
