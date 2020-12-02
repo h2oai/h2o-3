89e665439b0c60f9dc5d4321d3b03a51b183c579
Hemen Kapadia <hemen.kapadia@gmail.com>
2020-12-01 23:31:54 -0800

    refactoring h2ocluster service account name

--- templates/gcp/network/modules/service-accounts/main.tf
+++ templates/gcp/network/modules/service-accounts/main.tf
@@ -18,10 +18,20 @@ resource "google_project_iam_member" "workspace-vm-sa-serviceAccountUser-binding
   member = "serviceAccount:${google_service_account.workspace-vm-sa.email}"
 }
 
+
+
+
 # Service Account to be assigned to VM instances forming the h2o cluster
-resource "google_service_account" "h2o-cluster-vm-sa" {
+resource "google_service_account" "h2ocluster-vm-sa" {
   project = var.gcp_project_id
-  account_id = "h2o-cluster-vm-sa"
-  display_name = "h2o-cluster-vm-sa"
+  account_id = "h2ocluster-vm-sa"
+  display_name = "h2ocluster-vm-sa"
   description = "Service account assigned to the VMs that form the H2O cluster"
+}
+# add IAM bindings for this SA. 
+# Only 1 role per block is allowed. Add additional blocks for additional roles
+resource "google_project_iam_member" "h2ocluster-vm-sa-serviceAccountUser-binding" {
+  project = var.gcp_project_id
+  role = "roles/iam.serviceAccountUser"
+  member = "serviceAccount:${google_service_account.h2ocluster-vm-sa.email}"
 }
--- templates/gcp/network/modules/service-accounts/outputs.tf
+++ templates/gcp/network/modules/service-accounts/outputs.tf
@@ -1,7 +1,7 @@
 output "workspace-vm-sa-email" {
   value = google_service_account.workspace-vm-sa.email 
 }
-output "h2o-cluster-vm-sa-email" {
-  value = google_service_account.h2o-cluster-vm-sa.email
+output "h2ocluster-vm-sa-email" {
+  value = google_service_account.h2ocluster-vm-sa.email
 }
 
