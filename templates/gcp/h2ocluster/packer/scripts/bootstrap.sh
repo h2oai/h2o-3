5de6908dc0d093c4ac0807d836eed53daa305151
Hemen Kapadia <hemen.kapadia@gmail.com>
2020-12-13 16:36:12 -0800

    need sudo to install

--- templates/gcp/h2ocluster/packer/scripts/bootstrap.sh
+++ templates/gcp/h2ocluster/packer/scripts/bootstrap.sh
@@ -2,8 +2,8 @@
 set -o xtrace
 
 # install dependencies
-unzip -v || { echo "Installing Unzip"; yum install -y unzip; }
-java -version || { echo "Installing Java JDK"; yum install -y java-1.8.0-openjdk-devel; }
+unzip -v || { echo "Installing Unzip"; sudo yum install -y unzip; }
+java -version || { echo "Installing Java JDK"; sudo yum install -y java-1.8.0-openjdk-devel; }
 
 H2O_URL="${1}"
 echo "${H2O_URL}" > /tmp/h2o_url
@@ -16,10 +16,10 @@ H2O_HOME_DIR="/opt/h2oai/${H2O_VERSION}"
 echo "${H2O_HOME_DIR}" > /tmp/h2o_home_dir
 
 # Start installing H2O
-mkdir -p /opt/h2oai
+sudo mkdir -p /opt/h2oai
 # if flag file h2o_installed is not found install H2O
 if [[ ! -f /opt/h2oai/h2o_installed ]]; then
     curl "${H2O_URL}" -o "/tmp/${H2O_ZIP_FILE}"
-    unzip -d /opt/h2oai "/tmp/${H2O_ZIP_FILE}"
-    touch /opt/h2oai/h2o_installed 
+    sudo unzip -d /opt/h2oai "/tmp/${H2O_ZIP_FILE}"
+    sudo touch /opt/h2oai/h2o_installed 
 fi
