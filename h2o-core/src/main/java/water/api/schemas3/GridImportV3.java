b67c11ab2e6db23e546f515fce9b9c775b200fd6
Jan Sterba <info@jansterba.com>
2020-12-15 17:39:04 +0100

    fix doc

--- h2o-core/src/main/java/water/api/schemas3/GridImportV3.java
+++ h2o-core/src/main/java/water/api/schemas3/GridImportV3.java
@@ -10,7 +10,7 @@ public class GridImportV3 extends SchemaV3<Iced, GridImportV3> {
       required = true, direction = API.Direction.INPUT, level = API.Level.critical)
   public String grid_path;
 
-  @API(help = "If true will also load saved objects referenced by params. Will fail with an error if grid was saved without frames.",
+  @API(help = "If true will also load saved objects referenced by params. Will fail with an error if grid was saved without objects referenced by params.",
       direction = API.Direction.INPUT)
   public boolean load_params_references = false;
 
