65e6fb8cdc0fd2c49042c28e34ad557be0388026
Sebastien Poirier <sebastien@h2o.ai>
2021-01-08 00:12:58 +0100

    improve interactions: keep only useful domain

--- h2o-extensions/target-encoder/src/test/java/ai/h2o/targetencoding/InteractionsEncoderTest.java
+++ h2o-extensions/target-encoder/src/test/java/ai/h2o/targetencoding/InteractionsEncoderTest.java
@@ -21,28 +21,24 @@ public class InteractionsEncoderTest {
                 new String[] {"A", "B", "C"},
         };
         InteractionsEncoder enc = new InteractionsEncoder(domains, true);
-        assertEquals((2+1)*(3+1), enc._interactionDomain.length);
-        assertArrayEquals(
-                new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"},
-                enc._interactionDomain
-        );
+        
         String[] input = new String[]{"b", "C"};
-        int encVal = enc.encodeStr(input);
+        long encVal = enc.encodeStr(input);
         assertEquals(1+(3*2), encVal);  //b -> 1, C -> (len(a, b)+1) * 2
         assertArrayEquals(input, enc.decodeStr(encVal));
         
         String[] withNA = new String[] {null, "B"};
-        int withNAEnc = enc.encodeStr(withNA);
+        long withNAEnc = enc.encodeStr(withNA);
         assertEquals(2+(3*1), withNAEnc);
         assertArrayEquals(withNA, enc.decodeStr(withNAEnc));
 
         String[] allNAs = new String[] {null, null};
-        int allNAsEnc = enc.encodeStr(allNAs);
+        long allNAsEnc = enc.encodeStr(allNAs);
         assertEquals(2+(3*3), allNAsEnc);
         assertArrayEquals(allNAs, enc.decodeStr(allNAsEnc));
 
         String[] withUnseen = new String[] {"a", "F"};
-        int withUnseenEnc = enc.encodeStr(withUnseen);
+        long withUnseenEnc = enc.encodeStr(withUnseen);
         assertEquals(0+(3*3), withUnseenEnc);
         assertArrayEquals(new String[] {"a", null}, enc.decodeStr(withUnseenEnc));
     }
@@ -57,13 +53,9 @@ public class InteractionsEncoderTest {
            new String[] {"aaa", "bbb", "ccc", "ddd", "eee"},
         };
         InteractionsEncoder enc = new InteractionsEncoder(domains, true);
-        assertEquals((2+1)*(3+1)*(2+1)*(4+1)*(5+1), enc._interactionDomain.length);
-        assertArrayEquals(
-                IntStream.range(0, 3*4*3*5*6).mapToObj(Integer::toString).toArray(String[]::new),
-                enc._interactionDomain
-        );
+        
         String[] input = new String[]{"b", "C", "aa", "BB", "eee"};
-        int encVal = enc.encodeStr(input);
+        long encVal = enc.encodeStr(input);
         assertArrayEquals(input, enc.decodeStr(encVal));
 
         String[] withNAsAndUnseen = new String[]{"b", null, "aa", "BBD", "eee"};
