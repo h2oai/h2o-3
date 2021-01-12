d915a82fcf2c903e373a04c202e31caf415c90d3
maurever <veronika@maurerova.cz>
2021-01-12 15:11:39 +0100

    PUBDEV-4904 implement uplift into h2o structure

--- h2o-r/h2o-package/R/gbm.R
+++ h2o-r/h2o-package/R/gbm.R
@@ -102,6 +102,8 @@
 #' @param gainslift_bins Gains/Lift table number of bins. 0 means disabled.. Default value -1 means automatic binning. Defaults to -1.
 #' @param auc_type Set default multinomial AUC type. Must be one of: "AUTO", "NONE", "MACRO_OVR", "WEIGHTED_OVR", "MACRO_OVO",
 #'        "WEIGHTED_OVO". Defaults to AUTO.
+#' @param uplift_column Define column which will be use for computing uplift gain to select best split for a tree. The column has to
+#'        devide dataset into treatment (value 1) and control (value 0) group.
 #' @param verbose \code{Logical}. Print scoring history to the console (Metrics per tree). Defaults to FALSE.
 #' @seealso \code{\link{predict.H2OModel}} for prediction
 #' @examples
@@ -177,6 +179,7 @@ h2o.gbm <- function(x,
                     check_constant_response = TRUE,
                     gainslift_bins = -1,
                     auc_type = c("AUTO", "NONE", "MACRO_OVR", "WEIGHTED_OVR", "MACRO_OVO", "WEIGHTED_OVO"),
+                    uplift_column = NULL,
                     verbose = FALSE)
 {
   # Validate required training_frame first and other frame args: should be a valid key or an H2OFrame object
@@ -318,6 +321,8 @@ h2o.gbm <- function(x,
     parms$gainslift_bins <- gainslift_bins
   if (!missing(auc_type))
     parms$auc_type <- auc_type
+  if (!missing(uplift_column))
+    parms$uplift_column <- uplift_column
 
   # Error check and build model
   model <- .h2o.modelJob('gbm', parms, h2oRestApiVersion=3, verbose=verbose)
@@ -380,6 +385,7 @@ h2o.gbm <- function(x,
                                     check_constant_response = TRUE,
                                     gainslift_bins = -1,
                                     auc_type = c("AUTO", "NONE", "MACRO_OVR", "WEIGHTED_OVR", "MACRO_OVO", "WEIGHTED_OVO"),
+                                    uplift_column = NULL,
                                     segment_columns = NULL,
                                     segment_models_id = NULL,
                                     parallelism = 1)
@@ -525,6 +531,8 @@ h2o.gbm <- function(x,
     parms$gainslift_bins <- gainslift_bins
   if (!missing(auc_type))
     parms$auc_type <- auc_type
+  if (!missing(uplift_column))
+    parms$uplift_column <- uplift_column
 
   # Build segment-models specific parameters
   segment_parms <- list()
