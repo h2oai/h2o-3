175053e65a39842f738cbe2e8d5229a23c3f6f79
Adam Valenta <adam.valenta@h2o.ai>
2021-01-18 20:57:36 +0100

    PUBDEV-7937 - add test for "Expose most important reason codes" for R randomForest

---
+++ h2o-r/tests/testdir_algos/randomforest/runit_RF_predict_contribs_sort.R
@@ -0,0 +1,47 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+
+
+test.RF.contribs_sorted_smoke <- function() {
+    prostate_hex <- h2o.importFile(locate("smalldata/prostate/prostate.csv"))
+    first_row <- prostate_hex[1,]
+
+    rf_model <- h2o.randomForest(training_frame = prostate_hex[2:9], y = "CAPSULE", max_depth=100, ntrees=50, keep_cross_validation_models=TRUE, seed=1234)
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, prostate_hex, top_n=0, top_bottom_n=0, abs=FALSE))
+    expect_equal(dim(contributions), c(380,8), info="Output frame has wrong dimmension")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, first_row, top_n=2, top_bottom_n=0, abs=FALSE))
+    expect_equal("VOL", contributions[1,1], info="Not sorted correctly")
+    expect_equal("RACE", contributions[1,3], info="Not sorted correctly")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, first_row, top_n=0, top_bottom_n=2, abs=FALSE))
+    expect_equal("PSA", contributions[1,1], info="Not sorted correctly")
+    expect_equal("GLEASON", contributions[1,3], info="Not sorted correctly")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, first_row, top_n=2, top_bottom_n=2, abs=FALSE))
+    expect_equal("VOL", contributions[1,1], info="Not sorted correctly")
+    expect_equal("RACE", contributions[1,3], info="Not sorted correctly")
+    expect_equal("PSA", contributions[1,5], info="Not sorted correctly")
+    expect_equal("GLEASON", contributions[1,7], info="Not sorted correctly")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, prostate_hex, top_n=0, top_bottom_n=0, abs=TRUE))
+    expect_equal(dim(contributions), c(380,1), info="Output frame has wrong dimmension")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, first_row, top_n=2, top_bottom_n=0, abs=TRUE))
+    expect_equal("PSA", contributions[1,1], info="Not sorted correctly")
+    expect_equal("GLEASON", contributions[1,3], info="Not sorted correctly")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, first_row, top_n=0, top_bottom_n=2, abs=TRUE))
+    expect_equal("DCAPS", contributions[1,1], info="Not sorted correctly")
+    expect_equal("RACE", contributions[1,3], info="Not sorted correctly")
+
+    contributions <- as.data.frame(h2o.predict_contributions(rf_model, first_row, top_n=2, top_bottom_n=2, abs=TRUE))
+    expect_equal("PSA", contributions[1,1], info="Not sorted correctly")
+    expect_equal("GLEASON", contributions[1,3], info="Not sorted correctly")
+    expect_equal("DCAPS", contributions[1,5], info="Not sorted correctly")
+    expect_equal("RACE", contributions[1,7], info="Not sorted correctly")
+}
+
+doTest("RF Test: Make sure RF contributions are sorted", test.RF.contribs_sorted_smoke)
