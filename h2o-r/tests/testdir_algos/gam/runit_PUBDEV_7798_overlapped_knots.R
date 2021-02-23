6498a31807a0dedc916a7bfd3af7dd96d3037322
km2018 <karthik.murthy123@gmail.com>
2021-02-23 12:46:18 -0600

    Add R unit test for GAM overlapped knots

---
+++ h2o-r/tests/testdir_algos/gam/runit_PUBDEV_7798_overlapped_knots.R
@@ -0,0 +1,37 @@
+library(h2o)
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+test.model.gam.overlapped.knots <- function() {
+    data <- data.frame(C1=sample(8, size=1000, replace = TRUE), target=sample(0:1, size=1000, replace = TRUE))
+    data$target <- as.factor(data$target)
+    
+    splits <- h2o.splitFrame(data = data, ratios = 0.8)
+    train <- splits[[1]]
+    test <- splits[[2]]
+    
+    # Set the predictor and response columns
+    predictors <- c("C1")
+    response <- 'target'
+    
+    
+    
+    
+    
+    tryCatch({
+      gam_model <- h2o.gam(x = predictors,
+                           y = response,
+                           training_frame = train,
+                           family = 'binomial',
+                           gam_columns = predictors,
+                           scale = c(1),
+                           num_knots = c(10))
+    }, error=function(cond) {
+        print("Test passed.")
+    }, finally={
+        stop_cluster("saver")
+    })
+    
+}
+
+doTest("General Additive Model test for overlapped knots", test.model.gam.overlapped.knots)
