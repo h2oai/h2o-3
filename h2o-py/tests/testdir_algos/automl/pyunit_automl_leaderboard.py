393ef32663434047d3cfd02fa778efc12a45c4e2
Tomáš Frýda <61695433+tomasfryda@users.noreply.github.com>
2021-03-12 15:56:13 +0100

    PUBDEV-7820: Add a `topbasemodel` attribute to AutoML (#5259)
    * Initial add a topbasemodel attribute to automl implementation
    
    * Make python best_models a function and in R preload the models during init
    
    * Incorporate suggestions from meeting 2021/02/01
    
    * Fix docstrings and match.arg in R for algorithm
    
    * Incorporate Seb's suggestions
    
    * Make algos and criterions case insensitive, improve tests
    
    * Automatically infer what extra cols should be retrieved from criterion
    
    * Incorporate more Seb's suggestions
    
    * Update docstrings
    
    * Replace grep+regex with conditions
    
    * Add list of options to R's criterion parameter
    
    * Change abbrev. metrics to upper case and add deprecation warning when criterion = 'deviance'
    
    * Remove warning about deprecating deviance and prioritize it over mean_residual_deviance
    
    * Make lower-case metrics in python docstring

--- h2o-py/tests/testdir_algos/automl/pyunit_automl_leaderboard.py
+++ h2o-py/tests/testdir_algos/automl/pyunit_automl_leaderboard.py
@@ -1,5 +1,8 @@
 from __future__ import print_function
 import sys, os
+
+from h2o.exceptions import H2OValueError
+
 sys.path.insert(1, os.path.join("..","..",".."))
 import h2o
 import random
@@ -304,16 +307,77 @@ def test_custom_leaderboard():
     assert aml.leaderboard.names == std_columns
     assert get_leaderboard(aml).names == std_columns
     assert get_leaderboard(aml, extra_columns=[]).names == std_columns
-    assert get_leaderboard(aml, extra_columns='ALL').names == std_columns + ["training_time_ms", "predict_time_per_row_ms"]
+    assert get_leaderboard(aml, extra_columns='ALL').names == std_columns + ["training_time_ms", "predict_time_per_row_ms", "algo"]
     assert get_leaderboard(aml, extra_columns="unknown").names == std_columns
     assert get_leaderboard(aml, extra_columns=["training_time_ms"]).names == std_columns + ["training_time_ms"]
     assert get_leaderboard(aml, extra_columns=["predict_time_per_row_ms", "training_time_ms"]).names == std_columns + ["predict_time_per_row_ms", "training_time_ms"]
     assert get_leaderboard(aml, extra_columns=["unknown", "training_time_ms"]).names == std_columns + ["training_time_ms"]
     lb_ext = get_leaderboard(aml, extra_columns='ALL')
     print(lb_ext)
-    assert all(lb_ext[:, 1:].isnumeric()), "metrics and extension columns should all be numeric"
+    assert all(lb_ext[:, [c for c in lb_ext.columns if c not in ("model_id", "algo")]].isnumeric()), "metrics and extension columns should all be numeric"
     assert (lb_ext["training_time_ms"].as_data_frame().values >= 0).all()
     assert (lb_ext["predict_time_per_row_ms"].as_data_frame().values > 0).all()
+    assert (lb_ext["algo"].as_data_frame().isin(["DRF", "DeepLearning", "GBM",
+                                                 "GLM", "StackedEnsemble", "XGBoost"]).all().all())
+
+
+def test_get_best_model_per_family():
+    ds = prepare_data('binomial')
+    aml = H2OAutoML(project_name="py_aml_custom_lb_test",
+                    max_models=11,
+                    seed=automl_seed)
+    aml.train(y=ds.target, training_frame=ds.train)
+
+    def _check_best_models(model_ids, criterion):
+        # test case insensitivity in algo specification
+        top_models = [aml.get_best_model(mtype, criterion) for mtype in ["deeplearning", "drf", "gbm", "GLM",
+                                                                         "STaCKeDEnsEmblE", "xgboost"]]
+        nones = [v is None for v in top_models]
+        assert sum(nones) <= 1 and len(nones) >= 6
+        seen = set()
+        top_model_ids = [m.model_id for m in top_models if m is not None]
+        for model_id in model_ids:
+            model_type = model_id.split("_")[0]
+            if model_type not in seen:
+                assert model_id in top_model_ids
+                if model_type in ("DRF", "XRT"):
+                    seen.add("DRT")
+                    seen.add("XRT")
+                else:
+                    seen.add(model_type)
+    # Check default criterion
+    model_ids = aml.leaderboard.as_data_frame()["model_id"]
+    _check_best_models(model_ids, None)
+
+    # Check AUC criterion (the higher the better) and check case insensitivity
+    model_ids = aml.leaderboard.sort(by="auc", ascending=False).as_data_frame()["model_id"]
+    _check_best_models(model_ids, "AUC")
+
+    # Check it works for custom criterion (MSE)
+    model_ids = aml.leaderboard.sort(by="mse").as_data_frame()["model_id"]
+    _check_best_models(model_ids, "mse")
+
+    # Check it works for without specifying a model type
+    assert aml.get_best_model().model_id == aml.leaderboard[0, "model_id"]
+
+    # Check it works with just criterion
+    assert aml.get_best_model(criterion="mse").model_id == aml.leaderboard.sort(by="mse")[0, "model_id"]
+
+    # Check it works with extra_cols
+    top_model = h2o.automl.get_leaderboard(aml, extra_columns=["training_time_ms"]).sort(by="training_time_ms")[0, "model_id"]
+    assert aml.get_best_model(criterion="training_time_ms").model_id == top_model
+
+    # Check validation works
+    try:
+        aml.get_best_model(algorithm="GXboost")
+        assert False, "Algorithm validation does not work!"
+    except H2OValueError:
+        pass
+    try:
+        aml.get_best_model(criterion="lorem_ipsum_dolor_sit_amet")
+        assert False, "Criterion validation does not work!"
+    except H2OValueError:
+        pass
 
 
 pyunit_utils.run_tests([
@@ -331,4 +395,5 @@ pyunit_utils.run_tests([
     test_AUTO_stopping_metric_with_auc_sorting_metric,
     test_AUTO_stopping_metric_with_custom_sorting_metric,
     test_custom_leaderboard,
+    test_get_best_model_per_family,
 ])
