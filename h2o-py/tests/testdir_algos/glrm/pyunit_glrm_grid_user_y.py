72e87acd45b25fe11ddb31755b0509fe9dad7418
wendycwong <wendy@h2o.ai>
2021-04-02 11:21:19 -0700

    Fix pyunit_glrm_grid_user_y.py failure (#5416)
    * Compare all archetypes instead of just the first one
    
    * Update h2o-py/tests/testdir_algos/glrm/pyunit_glrm_grid_user_y.py
    
    Co-authored-by: Seb <sebastien@h2o.ai>
    
    * Incorporate seb comments
    
    * Extract model based on parameters used to build model and not model order.
    
    Co-authored-by: Seb <sebastien@h2o.ai>

--- h2o-py/tests/testdir_algos/glrm/pyunit_glrm_grid_user_y.py
+++ h2o-py/tests/testdir_algos/glrm/pyunit_glrm_grid_user_y.py
@@ -15,13 +15,15 @@ def glrm_grid_user_y():
     train = h2o.H2OFrame(train_data.tolist(), destination_frame="glrm_train")
     initial_y_data = np.random.rand(10, 100)
     initial_y_h2o = h2o.H2OFrame(initial_y_data.tolist(), destination_frame="glrm_initial_y")
+    numArchetypes = 10
     params = {
-        "k": 10,
+        "k": numArchetypes,
         "init": "User",
         "user_y": initial_y_h2o,
         "loss": "Quadratic",
         "regularization_x": "OneSparse",
-        "regularization_y": "NonNegative"
+        "regularization_y": "NonNegative",
+        "seed":12345
     }
     hyper_params = {
         "transform": ["NONE", "STANDARDIZE"],
@@ -37,8 +39,12 @@ def glrm_grid_user_y():
     print("first grid")
     print(grid)
     assert len(grid.model_ids) == 2
-    archetypes1 = grid.models[0].archetypes()
-    archetypes2 = grid.models[1].archetypes()
+    if (grid.models[0].actual_params['transform']=='STANDARDIZE'):
+        archetypes0p1Standardize = grid.models[0].archetypes()
+        archetypes0p1None = grid.models[1].archetypes()
+    else:
+        archetypes0p1Standardize = grid.models[1].archetypes()
+        archetypes0p1None = grid.models[0].archetypes()
     grid_path = h2o.save_grid(export_dir, grid.grid_id)
     h2o.remove_all()
     
@@ -52,11 +58,22 @@ def glrm_grid_user_y():
     print(grid)
     assert len(grid.model_ids) == 4
     # check actual training occurred and results are different
-    assert grid.models[0].archetypes() == archetypes1
-    assert grid.models[1].archetypes() == archetypes2
-    # instead of using python compare, I am just check and make first the first arrays are not equal.
-    assert not(pyunit_utils.equal_two_arrays(grid.models[1].archetypes()[0], grid.models[2].archetypes()[0], throw_error=False))
-    assert not(pyunit_utils.equal_two_arrays(grid.models[2].archetypes()[0], grid.models[3].archetypes()[0], throw_error=False))
+    for oneGridModel in grid.models:
+        if (oneGridModel.actual_params['gamma_x'] == 0.1) and (oneGridModel.actual_params['transform']=='STANDARDIZE'):
+            assert oneGridModel.archetypes() == archetypes0p1Standardize
+        if (oneGridModel.actual_params['gamma_x'] == 0.1) and (oneGridModel.actual_params['transform']=='NONE'):
+            assert oneGridModel.archetypes() == archetypes0p1None
+        if (oneGridModel.actual_params['gamma_x'] == 1) and (oneGridModel.actual_params['transform']=='STANDARDIZE'):
+            archetypes1None = oneGridModel.archetypes()
+        if (oneGridModel.actual_params['gamma_x'] == 1) and (oneGridModel.actual_params['transform']=='NONE'):
+            archetypes1Standardize = oneGridModel.archetypes()
+  
+    archetypesNotEqual12 = not all([pyunit_utils.equal_two_arrays(archetypes1None[i], archetypes0p1None[i], 
+                                                                  throw_error=False) for i in range(numArchetypes)])
+    assert archetypesNotEqual12
+    archetypesNotEqual23 = not all([pyunit_utils.equal_two_arrays(archetypes1Standardize[i], archetypes0p1Standardize[i], 
+                                                                  throw_error=False) for i in range(numArchetypes)])
+    assert archetypesNotEqual23
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(glrm_grid_user_y)
