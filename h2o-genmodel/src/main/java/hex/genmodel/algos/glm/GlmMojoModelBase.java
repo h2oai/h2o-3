147d87bef0395260e9edc6446a2c2e1e55ff8811
km2018 <karthik.murthy123@gmail.com>
2021-04-02 08:17:43 -0500

    Add impute interactions to GLM mojo model base

--- h2o-genmodel/src/main/java/hex/genmodel/algos/glm/GlmMojoModelBase.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/glm/GlmMojoModelBase.java
@@ -3,10 +3,14 @@ package hex.genmodel.algos.glm;
 import com.sun.corba.se.spi.orb.StringPair;
 import hex.genmodel.MojoModel;
 
+import java.util.HashMap;
+import java.util.stream.IntStream;
+
 abstract class GlmMojoModelBase extends MojoModel {
 
   boolean _useAllFactorLevels;
   public StringPair[] _interaction_pairs;
+  HashMap<String, Integer[]> _interaction_mapping;
 
   int _cats;
   int[] _catModes;
@@ -22,6 +26,7 @@ abstract class GlmMojoModelBase extends MojoModel {
 
   GlmMojoModelBase(String[] columns, String[][] domains, String responseColumn) {
     super(columns, domains, responseColumn);
+    mapInteractions();
   }
 
   void init() { /* do nothing by default */ }
@@ -37,12 +42,62 @@ abstract class GlmMojoModelBase extends MojoModel {
   abstract double[] glmScore0(double[] data, double[] preds);
 
   private void imputeMissingWithMeans(double[] data) {
-    for (int i = 0; i < _cats; ++i)
-      if (Double.isNaN(data[i])) data[i] = _catModes[i];
-    for (int i = 0; i < _nums; ++i)
-      if (Double.isNaN(data[i + _cats])) data[i + _cats] = _numMeans[i];
+    for (int i = 0; i < _cats; ++i) {
+      if (Double.isNaN(data[i])) {
+        if (isInteraction(_names[i])) {
+          imputeInteraction(data, i);
+        } else {
+          data[i] = _catModes[i];
+        }
+      }
+    }
+    for (int i = 0; i < _nums; ++i) {
+      if (Double.isNaN(data[i + _cats])) {
+        if (isInteraction(_names[i + _cats])) {
+          imputeInteraction(data, i + _cats);
+        } else {
+          data[i + _cats] = _numMeans[i];  
+        }
+      } 
+    }
   }
 
+  private void mapInteractions() {
+    if (_interaction_pairs == null) {
+      _interaction_mapping = null;
+      return;
+    }
+    
+    _interaction_mapping = new HashMap<String, Integer[]>();
+    for (StringPair pair: _interaction_pairs) {
+      int index1 = IntStream.range(0, _names.length)
+              .filter(i -> pair.getFirst() == _names[i])
+              .findFirst()
+              .orElse(-1);
+      int index2 = IntStream.range(0, _names.length)
+              .filter(i -> pair.getSecond() == _names[i])
+              .findFirst()
+              .orElse(-1);
+      _interaction_mapping.put((pair.getFirst() + "_" + pair.getSecond()), new Integer[]{index1, index2});
+    }
+  }
+  
+  private boolean isInteraction(String name) {
+    if(_interaction_pairs == null) {return false;}
+    return _interaction_mapping.containsKey(name);
+  }
+  
+  private void imputeInteraction(double[] data, int index) {
+    Integer[] pairs = _interaction_mapping.get(_names[index]);
+    if (_domains[pairs[0]] != null && _domains[pairs[1]] != null) {
+      data[index] = (_domains[pairs[0]].length * data[pairs[0]]) + data[pairs[1]] - 1;
+    } else if (_domains[pairs[0]] == null && _domains[pairs[1]] == null) {
+      data[index] = data[pairs[0]] * data[pairs[1]];
+    } else {
+      data[index] = _domains[pairs[0]] == null ? data[pairs[0]] : data[pairs[1]];  
+    }
+  }
+  
   @Override
   public String[] getOutputNames() {
     // special handling of binomial case where response domain is not represented
