e5f7ea4f9c33f89f16a46c1e71b2f7652e0edc59
michalkurka <michalk@h2o.ai>
2021-04-19 12:54:41 -0400

    Refactor: use _global_beta_multinomial directly when needed

--- h2o-algos/src/main/java/hex/glm/GLMModel.java
+++ h2o-algos/src/main/java/hex/glm/GLMModel.java
@@ -1913,32 +1913,31 @@ public class GLMModel extends Model<GLMModel,GLMModel.GLMParameters,GLMModel.GLM
     return new GLMMojoWriter(this);
   }
 
-  private boolean isFeatureUsedInPredict(int featureIdx, int offset){
+  private boolean isFeatureUsedInPredict(int featureIdx, double[] beta){
     if (featureIdx < _output._dinfo._catOffsets.length - 1 && _output._column_types[featureIdx].equals("Enum")) {
       for (int i = _output._dinfo._catOffsets[featureIdx];
            i < _output._dinfo._catOffsets[featureIdx + 1];
            i++) {
-        if (beta()[i + offset] != 0) return true;
+        if (beta[i] != 0) return true;
       }
       return false;
     } else {
       featureIdx += _output._dinfo._numOffsets[0] - _output._dinfo._catOffsets.length + 1;
     }
-    return beta()[featureIdx + offset] != 0;
+    return beta[featureIdx] != 0;
 }
 
   @Override
   protected boolean isFeatureUsedInPredict(int featureIdx) {
     if (_parms._interactions != null) return true;
     if (_output.isMultinomialClassifier()) {
-      int outputClasses =  beta().length / _output.coefficientNames().length;
-      for (int i = 0; i < outputClasses; i++) {
-        if (isFeatureUsedInPredict(featureIdx, i * _output.coefficientNames().length))
+      for (double[] classBeta : _output._global_beta_multinomial) {
+        if (isFeatureUsedInPredict(featureIdx, classBeta))
           return true;
       }
       return false;
     } else {
-      return isFeatureUsedInPredict(featureIdx, 0);
+      return isFeatureUsedInPredict(featureIdx, _output._global_beta);
     }
   }
 }
