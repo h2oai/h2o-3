ce24c65876b0e2ff7fc247c6f41661fac9553478
Adam Valenta <adam.valenta@h2o.ai>
2021-04-27 18:58:12 +0200

    PUBDEV-7138 - create MAX_VEC_NUM_ROWS_FOR_ARRAY_EXPORT constant

--- h2o-core/src/main/java/water/util/FrameUtils.java
+++ h2o-core/src/main/java/water/util/FrameUtils.java
@@ -25,6 +25,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class FrameUtils {
 
+  public static final int MAX_VEC_NUM_ROWS_FOR_ARRAY_EXPORT = 100_000;
+
   /** Parse given file(s) into the form of single frame represented by the given key.
    *
    * @param okey  destination key for parsed frame
@@ -180,12 +182,12 @@ public class FrameUtils {
   }
 
   public static double [] asDoubles(Vec v){
-    if(v.length() > 100_000) throw new IllegalArgumentException("Vec is too big to be extracted into array");
+    if(v.length() > MAX_VEC_NUM_ROWS_FOR_ARRAY_EXPORT) throw new IllegalArgumentException("Vec is too big to be extracted into array");
     return new Vec2ArryTsk((int)v.length()).doAll(v).res;
   }
 
   public static double [][] asDoubles(Frame frame){
-    if (frame.numRows() > 100_000)
+    if (frame.numRows() > MAX_VEC_NUM_ROWS_FOR_ARRAY_EXPORT)
       throw new IllegalArgumentException("Frame is too big to be extracted into array");
 
     double [][] frameArray = new double[frame.numCols()][(int) frame.numRows()];
