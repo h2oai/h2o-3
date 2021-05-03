aa9337f553a80f53794888eb082dedd62b44d58e
michalkurka <michalk@h2o.ai>
2021-05-03 13:48:30 -0400

    Revert "Revert "Remove inits, remove NaN from GLM large mojo interaction test""
    This reverts commit 2d562e4c42bb37e57369f475ace89c44e9cc5821.

--- h2o-py/tests/testdir_javapredict/pyunit_pubdev_8027_glm_mojo_all_interaction_large.py
+++ h2o-py/tests/testdir_javapredict/pyunit_pubdev_8027_glm_mojo_all_interaction_large.py
@@ -7,9 +7,9 @@ import subprocess
 from subprocess import STDOUT,PIPE
 def glm_mojo_all_interaction_test_large():
     seed = 12345
-    bigCat = pyunit_utils.random_dataset_enums_only(10000, 1, factorL=30, misFrac=0.01, randSeed=seed)
-    bitCat2 = pyunit_utils.random_dataset_enums_only(10000, 1, factorL=20, misFrac=0.01, randSeed=seed)
-    smallCats = pyunit_utils.random_dataset_enums_only(10000, 4, factorL=5, misFrac=0.01, randSeed=seed)
+    bigCat = pyunit_utils.random_dataset_enums_only(10000, 1, factorL=30, misFrac=0, randSeed=seed)
+    bitCat2 = pyunit_utils.random_dataset_enums_only(10000, 1, factorL=20, misFrac=0, randSeed=seed)
+    smallCats = pyunit_utils.random_dataset_enums_only(10000, 4, factorL=5, misFrac=0, randSeed=seed)
     numerics = pyunit_utils.random_dataset_real_only(10000, 5, realR=100, misFrac=0.01, randSeed=seed)
     dataframe = numerics.cbind(smallCats.cbind(bitCat2.cbind(bigCat)))
     dataframe.set_names(["response","n1","n2","n3","n4","c1","c2","c3","c4","c5","c6"])
@@ -19,7 +19,7 @@ def glm_mojo_all_interaction_test_large():
     TMPDIR = tempfile.mkdtemp()
     glmGaussianModel = pyunit_utils.build_save_model_generic(params, xcols, dataframe, "response", "glm", TMPDIR) # build and save mojo model
     MOJONAME = pyunit_utils.getMojoName(glmGaussianModel._id)
-    splitFrame = dataframe.split_frame(ratios=[0.001], seed=seed)
+    splitFrame = dataframe.split_frame(ratios=[0.1], seed=seed)
     pred_h2o = glmGaussianModel.predict(splitFrame[0])
     h2o.download_csv(splitFrame[0], os.path.join(TMPDIR, 'in.csv'))
     pred_mojo = mojo_predict(TMPDIR, MOJONAME)  # load model and perform predict
@@ -50,8 +50,6 @@ def mojo_predict(tmpdir, mojoname):
     pred_mojo = h2o.import_file(outfile, header=1)  # load mojo prediction in 
     return pred_mojo
 if __name__ == "__main__":
-    h2o.init(ip='192.168.0.141', port=54321, strict_version_check=False)
     pyunit_utils.standalone_test(glm_mojo_all_interaction_test_large)
 else:
-    h2o.init(ip='192.168.0.141', port=54321, strict_version_check=False)
     glm_mojo_all_interaction_test_large()
