af024ededbf684d895671fd213b2b59337a75754
michalkurka <michalk@h2o.ai>
2021-05-03 13:48:29 -0400

    Revert "Revert "Add support for categorical numerical interaction, missing interaction values in GLM mojo""
    This reverts commit be7c8b2b2bad994aa6047bf7b74bca11802092eb.

--- h2o-genmodel/src/main/java/hex/genmodel/algos/glm/GlmMojoModelBase.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/glm/GlmMojoModelBase.java
@@ -11,6 +11,7 @@ abstract class GlmMojoModelBase extends MojoModel {
   boolean _useAllFactorLevels;
   public StringPair[] _interaction_pairs;
   HashMap<String, Integer[]> _interaction_mapping;
+  HashMap<String, Integer> _column_mapping;
 
   int _cats;
   int[] _catModes;
@@ -20,13 +21,17 @@ abstract class GlmMojoModelBase extends MojoModel {
   double[] _numMeans;
   boolean _meanImputation;
 
+  int[] _catNumOffsets;
+  
   double[] _beta;
 
   String _family;
 
   GlmMojoModelBase(String[] columns, String[][] domains, String responseColumn) {
     super(columns, domains, responseColumn);
+    mapColumns();
     mapInteractions();
+    addCatNumOffsets();
   }
 
   void init() { /* do nothing by default */ }
@@ -62,6 +67,13 @@ abstract class GlmMojoModelBase extends MojoModel {
     }
   }
 
+  private void mapColumns() {
+    _column_mapping = new HashMap<String, Integer>();
+    for (int i = 0; i < _names.length; i++) {
+      _column_mapping.put(_names[i], i);
+    }
+  }
+  
   private void mapInteractions() {
     if (_interaction_pairs == null) {
       _interaction_mapping = null;
@@ -70,15 +82,22 @@ abstract class GlmMojoModelBase extends MojoModel {
     
     _interaction_mapping = new HashMap<String, Integer[]>();
     for (StringPair pair: _interaction_pairs) {
-      int index1 = IntStream.range(0, _names.length)
-              .filter(i -> pair.getFirst() == _names[i])
-              .findFirst()
-              .orElse(-1);
-      int index2 = IntStream.range(0, _names.length)
-              .filter(i -> pair.getSecond() == _names[i])
-              .findFirst()
-              .orElse(-1);
+      int index1 = _column_mapping.get(pair.getFirst());
+      int index2 = _column_mapping.get(pair.getSecond());
       _interaction_mapping.put((pair.getFirst() + "_" + pair.getSecond()), new Integer[]{index1, index2});
+    }
+  }
+  
+  private void addCatNumOffsets() {
+    int catNumInteractions = 0;
+    for (int i = _cats; i < _domains.length - 1; i++) {
+      if (_domains[i] == null) {break;}
+      catNumInteractions++;
+    }
+    _catNumOffsets = new int[catNumInteractions + 1];
+    _catNumOffsets[0] = 0;
+    for (int i = 0; i < catNumInteractions; i++) {
+      _catNumOffsets[i + 1] = _catNumOffsets[i] + _domains[_cats + i].length;
     }
   }
   
@@ -88,13 +107,29 @@ abstract class GlmMojoModelBase extends MojoModel {
   }
   
   private void imputeInteraction(double[] data, int index) {
-    Integer[] pairs = _interaction_mapping.get(_names[index]);
-    if (_domains[pairs[0]] != null && _domains[pairs[1]] != null) {
-      data[index] = (_domains[pairs[0]].length * data[pairs[0]]) + data[pairs[1]] - 1;
-    } else if (_domains[pairs[0]] == null && _domains[pairs[1]] == null) {
-      data[index] = data[pairs[0]] * data[pairs[1]];
+    Integer[] pair = _interaction_mapping.get(_names[index]);
+    if (_domains[pair[0]] != null && _domains[pair[1]] != null) {
+      if(Double.isNaN(data[pair[0]]) || Double.isNaN(data[pair[1]])) {
+        data[index] = _catModes[index];
+      } else {
+        data[index] = (_domains[pair[0]].length * data[pair[0]]) + data[pair[1]] - 1;  
+      }
+    } else if (_domains[pair[0]] == null && _domains[pair[1]] == null) {
+      if(Double.isNaN(data[pair[0]]) || Double.isNaN(data[pair[1]])) {
+        int meanOffset = _catNumOffsets[_catNumOffsets.length - 1];
+        data[index] = _numMeans[meanOffset + (index - _cats - (_catNumOffsets.length - 1))];
+      } else {
+        data[index] = data[pair[0]] * data[pair[1]];  
+      }
     } else {
-      data[index] = _domains[pairs[0]] == null ? data[pairs[0]] : data[pairs[1]];  
+      int cat_index = _domains[pair[0]] == null ? 1 : 0;
+      int num_index = _domains[pair[0]] == null ? 0 : 1;
+      if (Double.isNaN(data[num_index])) {
+        int meanOffset = _catNumOffsets[index - _cats];
+        data[index] = _numMeans[meanOffset + (int)data[cat_index]];
+      } else {
+        data[index] = data[num_index];
+      }
     }
   }
   
