79f84f9f791b0ab55f7a55bf8152ab97baff72e0
Tomáš Frýda <61695433+tomasfryda@users.noreply.github.com>
2021-06-29 12:27:57 +0200

    Fix failing scale_pca_rf test (#5550)

--- h2o-py/tests/testdir_pipeline/pyunit_scale_pca_rf.py
+++ h2o-py/tests/testdir_pipeline/pyunit_scale_pca_rf.py
@@ -18,7 +18,7 @@ def scale_pca_rf_pipe():
                    ("pca", H2OPCA(k=2)),
                    ("rf", H2ORandomForestEstimator(seed=42,ntrees=50))])
   pipe.fit(iris[:4],iris[4])
-  pca = pipe['pca']
+  pca = pipe.named_steps['pca']
   assert pca.model_id == pca._delegate.model_id
   assert pca._model_json == pca._delegate._model_json
   pca.download_pojo()
