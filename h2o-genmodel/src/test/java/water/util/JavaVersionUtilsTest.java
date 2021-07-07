f2c3a1b67f73d1ff534e55236da61b7e55c59de7
michalkurka <michalk@h2o.ai>
2021-07-07 10:59:31 -0400

    Refactor - make specifying verbose GC flag less prone to coding errors

--- h2o-genmodel/src/test/java/water/util/JavaVersionUtilsTest.java
+++ h2o-genmodel/src/test/java/water/util/JavaVersionUtilsTest.java
@@ -14,23 +14,38 @@ public class JavaVersionUtilsTest {
     @Test
     public void testPreJava10Parsing() {
         // OpenJDK 7
-        assertEquals(7, JAVA_VERSION.parseMajor("1.7.0_75"));
+        assertEquals(7, JavaVersionUtils.parseMajor("1.7.0_75"));
         // Oracle JDK 8
-        assertEquals(8, JAVA_VERSION.parseMajor("1.8.0_151"));
+        assertEquals(8, JavaVersionUtils.parseMajor("1.8.0_151"));
     }
 
     @Test
     public void testJava10AndLaterParsing() {
         // OpenJDK 9
-        assertEquals(9, JAVA_VERSION.parseMajor("9"));
+        assertEquals(9, JavaVersionUtils.parseMajor("9"));
         // Oracle JDK 10
-        assertEquals(10, JAVA_VERSION.parseMajor("10.0.2"));
+        assertEquals(10, JavaVersionUtils.parseMajor("10.0.2"));
     }
 
     @Test
     public void testNegative() {
-        assertEquals(JavaVersionUtils.UNKNOWN, JAVA_VERSION.parseMajor(null));
-        assertEquals(JavaVersionUtils.UNKNOWN, JAVA_VERSION.parseMajor(""));
-        assertEquals(JavaVersionUtils.UNKNOWN, JAVA_VERSION.parseMajor("x"));
+        assertEquals(JavaVersionUtils.UNKNOWN, JavaVersionUtils.parseMajor(null));
+        assertEquals(JavaVersionUtils.UNKNOWN, JavaVersionUtils.parseMajor(""));
+        assertEquals(JavaVersionUtils.UNKNOWN, JavaVersionUtils.parseMajor("x"));
     }
+
+    @Test
+    public void testGetVerboseGCFlag() {
+        // Java 8
+        assertEquals("-verbose:gc", JavaVersionUtils.JAVA_8.getVerboseGCFlag());
+        // Java 9 and newer
+        assertEquals("-Xlog:gc=info", JavaVersionUtils.JAVA_9.getVerboseGCFlag());
+        if (JAVA_VERSION.useUnifiedLogging()) {
+            assertEquals(JavaVersionUtils.JAVA_9.getVerboseGCFlag(), JAVA_VERSION.getVerboseGCFlag());
+        } else {
+            assertEquals(JavaVersionUtils.JAVA_8.getVerboseGCFlag(), JAVA_VERSION.getVerboseGCFlag());
+        }
+    }
+
+
 }
