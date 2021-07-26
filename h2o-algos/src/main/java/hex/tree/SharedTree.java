6b8c692e95dc87e35c03251bdc49df963375affc
michalkurka <michalk@h2o.ai>
2021-07-26 14:09:21 -0400

    PUBDEV-8250: Monotone constraints in GBM can cause issues with reproducing the model

--- h2o-algos/src/main/java/hex/tree/SharedTree.java
+++ h2o-algos/src/main/java/hex/tree/SharedTree.java
@@ -604,8 +604,8 @@ public abstract class SharedTree<
       // Pass 2: Build new summary DHistograms on the new child Nodes every row
       // got assigned into.  Collect counts, mean, variance, min, max per bin,
       // per column.
-//      new ScoreBuildHistogram(this,_k, _st._ncols, _nbins, _nbins_cats, _tree, _leafOffsets[_k], _hcs[_k], _family, _weightIdx, _workIdx, _nidIdx).dfork2(null,_fr2,_build_tree_one_node);
-      new ScoreBuildHistogram2(this,_k, _st._ncols, _nbins, _nbins_cats, _tree, _leafOffsets[_k], _hcs[_k], _family, 
+      int treeNum = ((SharedTreeModel.SharedTreeOutput) _st._model._output)._ntrees;
+      new ScoreBuildHistogram2(this, treeNum, _k, _st._ncols, _nbins, _nbins_cats, _tree, _leafOffsets[_k], _hcs[_k], _family,
               _respIdx, _weightIdx, _predsIdx, _workIdx, _nidIdx).dfork2(null,_fr2,_build_tree_one_node);
     }
     @Override public void onCompletion(CountedCompleter caller) {
