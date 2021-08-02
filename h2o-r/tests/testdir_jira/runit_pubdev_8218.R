76b73de27139d6c9a212f90991d3a7c940313414
Zuzana Olajcova <zuzana.olajcova@h2o.ai>
2021-08-02 19:05:11 +0200

    PUBDEV-8218: fix h2o interaction outcomes (#5581)

---
+++ h2o-r/tests/testdir_jira/runit_pubdev_8218.R
@@ -0,0 +1,55 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../scripts/h2o-r-test-setup.R")
+
+test.pubdev_8218 = function(){
+    # 1. check that interaction term does not create incorrect values anymore:
+    df = data.frame(
+    v1 = c('Y', 'Y', 'Y', 'N', 'N'),
+    v2 = c('S', 'S', 'S', 'A', 'A'),
+    v3 = c('E1', 'E1', 'E1', 'B1', 'B1')
+    )
+    df.hex = as.h2o(df, 'dfhex')
+    interaction = h2o.interaction(
+    data = df.hex,
+    destination_frame = 'interaction',
+    factors =  c('v2','v3', 'v1'),
+    pairwise = F,
+    max_factors = 1000,
+    min_occurrence = 1
+    )
+    combined = h2o.cbind(df.hex, interaction)
+    print(combined)
+    expect_true(interaction[1,1] == "S_E1_Y")
+    expect_true(interaction[2,1] == "S_E1_Y")
+    expect_true(interaction[3,1] == "S_E1_Y")
+    expect_true(interaction[4,1] == "A_B1_N")
+    expect_true(interaction[5,1] == "A_B1_N")
+    
+    
+    # 2. check that there is no error during interaction creation for data with more that 5 rows anymore:
+    df = data.frame(
+    v1 = c('Y', 'Y', 'Y', 'N', 'N', 'Y'),
+    v2 = c('S', 'S', 'S', 'A', 'A', 'N'),
+    v3 = c('E1', 'E1', 'E1', 'B1', 'B1', 'B1')
+    )
+    df.hex = as.h2o(df, 'dfhex')
+    interaction = h2o.interaction(
+    data = df.hex,
+    destination_frame = 'interaction',
+    factors =  c('v2','v3', 'v1'),
+    pairwise = F,
+    max_factors = 1000,
+    min_occurrence = 1
+    )
+    combined = h2o.cbind(df.hex, interaction)
+    print(combined)
+    expect_true(interaction[1,1] == "S_E1_Y")
+    expect_true(interaction[2,1] == "S_E1_Y")
+    expect_true(interaction[3,1] == "S_E1_Y")
+    expect_true(interaction[4,1] == "A_B1_N")
+    expect_true(interaction[5,1] == "A_B1_N")
+    expect_true(interaction[6,1] == "N_B1_Y")
+    
+}
+
+doTest("h2o.interaction() does not output the desired outcomes", test.pubdev_8218)
