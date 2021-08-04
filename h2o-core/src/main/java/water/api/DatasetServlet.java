72023c19135d8992146c89269fdbede781ca43dc
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-08-04 12:13:28 +0200

    PUBDEV-8230: use request.getServletPath instead of uri

--- h2o-core/src/main/java/water/api/DatasetServlet.java
+++ h2o-core/src/main/java/water/api/DatasetServlet.java
@@ -31,7 +31,7 @@ public class DatasetServlet extends HttpServlet {
           .setHexString(Boolean.parseBoolean(hex_string))
           .setEscapeQuotes(Boolean.parseBoolean(escape_quotes_string));
       InputStream is = dataset.toCSV(parms);
-      if (uri.equals("/3/DownloadDataset")) {
+      if (request.getServletPath().equals("/3/DownloadDataset")) {
         response.setContentType("text/csv");
       } else {
         response.setContentType("application/octet-stream");
