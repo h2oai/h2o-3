f11838df3d0b89cc797f8c62ac67c43b87ed4574
maurever <veronika@maurerova.cz>
2021-08-05 17:47:08 +0200

    PUBDEV-4940 correct divergence metrics, add doc

--- h2o-algos/src/main/java/hex/tree/uplift/Divergence.java
+++ h2o-algos/src/main/java/hex/tree/uplift/Divergence.java
@@ -2,73 +2,51 @@ package hex.tree.uplift;
 
 import water.Iced;
 
+/**
+ * Divergence class used to calculate gain to split the node in Uplift trees algorithms. 
+ * Currently only UpliftRandomForest uses this class.
+ * Source: https://link.springer.com/content/pdf/10.1007/s10115-011-0434-0.pdf page 308
+ * 
+ */
 public abstract class Divergence extends Iced<Divergence> {
 
     /**
-     * 
+     * Calculate distance divergence metric between two probabilities.
      * @param prCT1
      * @param prCT0
-     * @return
+     * @return distance divergence metric
      */
-    public abstract double node(
-        double prCT1,
-        double prCT0
-    );
+    public abstract double metric(double prCT1, double prCT0);
 
     /**
-     * 
-     * @param prL
-     * @param prLY1CT1
-     * @param prLY1CT0
-     * @param prR
-     * @param prRY1CT1
-     * @param prRY1CT0
-     * @return
+     * Calculate distance metric between two probabilities in the node.
+     * @param prCT1 probability of treatment group
+     * @param prCT0 probability of control group
+     * @return distance divergence metric in the node
      */
-    public double split(
-        double prL, double prLY1CT1, double prLY1CT0,
-        double prR, double prRY1CT1, double prRY1CT0
-    ) {
+    public double node(double prCT1, double prCT0) {
+        return metric(prCT1, prCT0) + metric(1 - prCT1, 1 - prCT0);
+    }
+
+    /**
+     * Calculate gain after split
+     * @param prL probability of response in left node
+     * @param prLY1CT1 probability of response = 1 in treatment group in left node
+     * @param prLY1CT0 probability of response = 1 in control group in left node
+     * @param prR probability of response in right node
+     * @param prRY1CT1 probability of response = 1 in treatment group in right node
+     * @param prRY1CT0 probability of response = 1 in control group in right node
+     * @return gain after split
+     */
+    public double split( double prL, double prLY1CT1, double prLY1CT0, 
+                         double prR, double prRY1CT1, double prRY1CT0) {
         double klL = node(prLY1CT1, prLY1CT0);
         double klR = node(prRY1CT1, prRY1CT0);
         return prL * klL + prR * klR;
     }
 
     /**
-     * 
-     * @param prY1CT1
-     * @param prY1CT0
-     * @param prL
-     * @param prLY1CT1
-     * @param prLY1CT0
-     * @param prR
-     * @param prRY1CT1
-     * @param prRY1CT0
-     * @return
-     */
-    public double gain(
-        double prY1CT1, double prY1CT0, 
-        double prL, double prLY1CT1, double prLY1CT0,
-        double prR, double prRY1CT1, double prRY1CT0
-    ) {
-        return split(prL, prLY1CT1, prLY1CT0, prR, prRY1CT1, prRY1CT0) - node(prY1CT1, prY1CT0);
-    }
-
-    /**
-     * 
-     * @param prCT1
-     * @param prCT0
-     * @param prLCT1
-     * @param prLCT0
-     * @return
-     */
-    public abstract double norm(
-        double prCT1, double prCT0,
-        double prLCT1, double prLCT0
-    );
-
-    /**
-     * Calculate normalized gain
+     * Calculate overall gain as divergence between split gain and node gain.
      * @param prY1CT1 probability of response = 1 in treatment group before split
      * @param prY1CT0 probability of response = 1 in control group before 
      * @param prL probability of response in left node
@@ -77,22 +55,43 @@ public abstract class Divergence extends Iced<Divergence> {
      * @param prR probability of response in right node
      * @param prRY1CT1 probability of response = 1 in treatment group in right node
      * @param prRY1CT0 probability of response = 1 in control group in right node
-     * @param prCT1 probability in treatment group
-     * @param prCT0 probability in control group
-     * @param prLCT1 probability in treatment group in left node
-     * @param prLCT0 probability in control group in left node
-     * @return
+     * @return overall gain
      */
-    public double value(
-        double prY1CT1, double prY1CT0,
-        double prL, double prLY1CT1, double prLY1CT0,
-        double prR, double prRY1CT1, double prRY1CT0,
-        double prCT1, double prCT0,
-        double prLCT1, double prLCT0
-    ) {
-        return
-            gain(prY1CT1, prY1CT0, prL,  prLY1CT1, prLY1CT0, prR, prRY1CT1, prRY1CT0)
-                /
-            norm(prCT1, prCT0, prLCT1, prLCT0);
+    public double gain(double prY1CT1, double prY1CT0, double prL, double prLY1CT1, double prLY1CT0, 
+                       double prR, double prRY1CT1, double prRY1CT0) {
+        return split(prL, prLY1CT1, prLY1CT0, prR, prRY1CT1, prRY1CT0) - node(prY1CT1, prY1CT0);
+    }
+
+    /**
+     * Calculate normalization factor to normalize gain.
+     * @param prCT1 probability of treatment group
+     * @param prCT0 probability of control group
+     * @param prLCT1 probability of treatment group in left node
+     * @param prLCT0 probability of control group in left node
+     * @return normalization factor
+     */
+    public abstract double norm(double prCT1, double prCT0, double prLCT1, double prLCT0);
+
+    /**
+     * Calculate normalized gain as result value to select best split.
+     * @param prY1CT1 probability of response = 1 in treatment group before split
+     * @param prY1CT0 probability of response = 1 in control group before 
+     * @param prL probability of response in left node
+     * @param prLY1CT1 probability of response = 1 in treatment group in left node
+     * @param prLY1CT0 probability of response = 1 in control group in left node
+     * @param prR probability of response in right node
+     * @param prRY1CT1 probability of response = 1 in treatment group in right node
+     * @param prRY1CT0 probability of response = 1 in control group in right node
+     * @param prCT1 probability of treatment group
+     * @param prCT0 probability of control group
+     * @param prLCT1 probability of treatment group in left node
+     * @param prLCT0 probability of control group in left node
+     * @return normalized gain
+     */
+    public double value(double prY1CT1, double prY1CT0, double prL, double prLY1CT1, double prLY1CT0, 
+                        double prR, double prRY1CT1, double prRY1CT0, double prCT1, double prCT0, 
+                        double prLCT1, double prLCT0) {
+        return gain(prY1CT1, prY1CT0, prL,  prLY1CT1, prLY1CT0, prR, prRY1CT1, prRY1CT0) / 
+                norm(prCT1, prCT0, prLCT1, prLCT0);
     }
 }
--- h2o-algos/src/main/java/hex/tree/uplift/KLDivergence.java
+++ h2o-algos/src/main/java/hex/tree/uplift/KLDivergence.java
@@ -4,13 +4,9 @@ import static water.util.MathUtils.log2;
 
 public class KLDivergence extends Divergence {
     
+    @Override
     public double metric(double prCT1, double prCT0) {
         return prCT1 * log2(prCT1 / prCT0);
-    }
-    
-    @Override
-    public double node(double prCT1, double prCT0) {
-        return metric(prCT1, prCT0) + metric(1 - prCT1, 1 - prCT0);
     }
     
     @Override
@@ -22,7 +18,7 @@ public class KLDivergence extends Divergence {
         double entCT =  -(prCT1 * log2(prCT1) + prCT0 * log2(prCT0));
         double entCT1 = -(prLCT1 * log2(prLCT1) + (1 - prLCT1) * log2((1 - prLCT1)));
         double entCT0 = -(prLCT0 * log2(prLCT0) + (1 - prLCT0) * log2((1 - prLCT0)));
-        return klCT * entCT + entCT1 * prCT1 + entCT0 * prCT0 + 0.5;
+        return klCT * entCT + prCT1 * entCT1 + prCT0 * entCT0 + 0.5;
     }
     
 }
