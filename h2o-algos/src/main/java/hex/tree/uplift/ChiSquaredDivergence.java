e01017df697b61a2455f500a1b6fe4a52354e0a1
maurever <veronika@maurerova.cz>
2021-08-09 09:55:43 +0200

    PUBDEV-4940 fix dividing by zero

--- h2o-algos/src/main/java/hex/tree/uplift/ChiSquaredDivergence.java
+++ h2o-algos/src/main/java/hex/tree/uplift/ChiSquaredDivergence.java
@@ -4,7 +4,7 @@ public class ChiSquaredDivergence extends EuclideanDistance {
 
     @Override
     public double metric(double prCT1, double prCT0) {
-        return ((prCT1 - prCT0) * (prCT1 - prCT0)) / prCT0;
+        return ((prCT1 - prCT0) * (prCT1 - prCT0)) / prCT0 == 0 ? ZERO_TO_DIVIDE : prCT0;
     }
 
 }
