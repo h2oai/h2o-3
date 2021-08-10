885aebcd56dc0df761e81326bef54a7263017eb6
Adam Valenta <adam.valenta@h2o.ai>
2021-08-10 21:04:30 +0200

    PUBDEV-4940 - run causalml test only for python >= 3.6

--- h2o-py/tests/testdir_algos/uplift/pyunit_uplift_causalml_make_uplift_classification_smoke.py
+++ h2o-py/tests/testdir_algos/uplift/pyunit_uplift_causalml_make_uplift_classification_smoke.py
@@ -1,29 +1,28 @@
 from __future__ import print_function
-import sys, os
-
-sys.path.insert(1, os.path.join("..", "..", ".."))
-from tests import pyunit_utils
+import sys
+import unittest
 from causalml.dataset import make_uplift_classification
 import causalml
 
 
-def causalml_make_uplift_classification_smoke():
-    print("Causalml library smoke test")
-    print("Version of causalml: {0}".format(causalml.__version__))
-    n_samples = 500
-    seed = 12345
+class TestCausalmlMakeUpliftClassificationIsWorking(unittest.TestCase):
 
-    train, x_names = make_uplift_classification(n_samples=n_samples,
-                                                treatment_name=['control', 'treatment'],
-                                                n_classification_features=10,
-                                                n_classification_informative=10,
-                                                random_seed=seed
-                                                )
-    assert not train.empty
-    assert x_names
+    @unittest.skipIf(sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] <= 5), "Tested only on >3.5, causalml is not supported on lower python version")
+    def test_causalml_make_uplift_classification_smoke(self):
+        print("Causalml library smoke test")
+        print("Version of causalml: {0}".format(causalml.__version__))
+        n_samples = 500
+        seed = 12345
+
+        train, x_names = make_uplift_classification(n_samples=n_samples,
+                                                    treatment_name=['control', 'treatment'],
+                                                    n_classification_features=10,
+                                                    n_classification_informative=10,
+                                                    random_seed=seed
+                                                    )
+        assert not train.empty
+        assert x_names
 
 
-if __name__ == "__main__":
-    pyunit_utils.standalone_test(causalml_make_uplift_classification_smoke)
-else:
-    causalml_make_uplift_classification_smoke()
+suite = unittest.TestLoader().loadTestsFromTestCase(TestCausalmlMakeUpliftClassificationIsWorking)
+unittest.TextTestRunner().run(suite)
