d839281534c28732094462c2aebc1fbd7994d8fb
Adam Valenta <adam.valenta@h2o.ai>
2021-08-11 14:31:40 +0200

    PUBDEV-4940 - import causalml inside the tests

--- h2o-py/tests/testdir_algos/uplift/pyunit_uplift_causalml_smoke.py
+++ h2o-py/tests/testdir_algos/uplift/pyunit_uplift_causalml_smoke.py
@@ -1,15 +1,16 @@
 from __future__ import print_function
 import sys
 import unittest
-from causalml.inference.tree import UpliftRandomForestClassifier
-from causalml.dataset import make_uplift_classification
-import causalml
 
 
 class TestCausalmlIsWorking(unittest.TestCase):
 
     @unittest.skipIf(sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] <= 5), "Tested only on >3.5, causalml is not supported on lower python version")
     def test_causalml_smoke(self):
+        from causalml.inference.tree import UpliftRandomForestClassifier
+        from causalml.dataset import make_uplift_classification
+        import causalml
+
         print("Causalml library smoke test")
         print("Version of causalml: {0}".format(causalml.__version__))
         n_samples = 500
