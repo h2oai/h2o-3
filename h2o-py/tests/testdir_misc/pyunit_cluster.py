11d29a53c3368685eaeb7be96bf028259939704e
Sebastien Poirier <sebastien@h2o.ai>
2021-08-16 18:52:59 +0200

    added test for cluster and error formats  as it seems like it was missing

---
+++ h2o-py/tests/testdir_misc/pyunit_cluster.py
@@ -0,0 +1,32 @@
+import sys
+
+sys.path.insert(1,"../../")
+import h2o
+from tests import pyunit_utils as pu
+
+
+def test_cluster_status():
+    h2o.cluster().show_status(True)
+
+
+def test_cluster_properties():
+    cl = h2o.cluster()
+    assert len(cl._schema_attrs_) == 24 
+    for k in cl._schema_attrs_.keys():
+        assert getattr(cl, k) is not None
+    
+
+def test_exception_on_unknown_cluster_property():
+    cl = h2o.cluster()
+    try:
+        assert cl.unknown_prop is not None, "should have failed before the assertion"
+    except AttributeError as e:
+        assert "Unknown attribute `unknown_prop` on object of type `H2OCluster`, this property is not available for this H2O backend" in str(e)
+
+
+pu.run_tests([
+    test_cluster_status,
+    test_cluster_properties,
+    test_exception_on_unknown_cluster_property
+])
+
