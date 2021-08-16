28a79096176d915112c48af0f16fdbca5a04a0b3
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-08-16 16:31:25 +0200

    PUBDEV-8293: Use log4j2 as a logger implementation in H2O standalone

--- h2o-core/src/test/java/water/util/LogTest.java
+++ h2o-core/src/test/java/water/util/LogTest.java
@@ -18,7 +18,8 @@ public class LogTest {
 
     private static final int ONE_MEGABYTE_IN_BYTES = 1048576;
     private static final String LOGGED_MESSAGE = "In Flanders fields the poppies blow, Between the crosses, row on row.";
-
+    private static final String CONTENT_STRING = "08-23 17:29:21.219 192.168.0.102:54321   47986        main  INFO water.default: In Flanders fields the poppies blow, Between the crosses, row on row.\n";
+    
     @Rule
     public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
@@ -36,11 +37,12 @@ public class LogTest {
             
             long previousSize = -1;
             while (infoLoggingFile.length() > previousSize) {
-                assertTrue(infoLoggingFile.length() < ONE_MEGABYTE_IN_BYTES);
                 previousSize = infoLoggingFile.length();
                 Log.info(LOGGED_MESSAGE);
             }
-            assertEquals(0, infoLoggingFile.length()); // Freshly rotated file
+            final File archivedInfoLoggingFile = new File(Log.getLogFilePath("info") + ".1");
+            assertTrue(archivedInfoLoggingFile.length() <= ONE_MEGABYTE_IN_BYTES + CONTENT_STRING.getBytes().length);
+            assertTrue(infoLoggingFile.length() <= CONTENT_STRING.getBytes().length);
         } finally {
             H2O.ARGS.log_dir = originalLogDir;
             Log.init(H2O.ARGS.log_level, H2O.ARGS.quiet, H2O.ARGS.max_log_file_size);
--- h2o-logging/logback-log4j2/src/main/java/water/util/LoggerBackend.java
+++ h2o-logging/logback-log4j2/src/main/java/water/util/LoggerBackend.java
@@ -2,12 +2,29 @@ package water.util;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.*;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 public class LoggerBackend {
 
     public static final Level[] L4J_LVLS = { Level.FATAL, Level.ERROR, Level.WARN, Level.INFO, Level.DEBUG, Level.TRACE };
+    public static final org.apache.logging.log4j.Level[] L4J_LOGGING_LVLS = {
+            org.apache.logging.log4j.Level.FATAL,
+            org.apache.logging.log4j.Level.ERROR,
+            org.apache.logging.log4j.Level.WARN,
+            org.apache.logging.log4j.Level.INFO,
+            org.apache.logging.log4j.Level.DEBUG,
+            org.apache.logging.log4j.Level.TRACE
+    };
 
     public int _level;
     public String _prefix;
@@ -17,7 +34,137 @@ public class LoggerBackend {
     public Function<String, String> _getLogFilePath;
 
     public Logger createLog4j() {
+        String h2oLog4jConfiguration = System.getProperty("h2o.log4j.configuration");
+
+        if (h2oLog4jConfiguration != null) {
+            // Try to configure via a file on local filesystem
+            File file = new File(h2oLog4jConfiguration);
+            if (file.exists()) {
+                Configurator.reconfigure(file.toURI());
+            } else {
+                // Try to load file via classloader resource (e.g., from classpath)
+                URL confUrl = LoggerBackend.class.getClassLoader().getResource(h2oLog4jConfiguration);
+                if (confUrl != null) {
+                    try {
+                        Configurator.reconfigure(confUrl.toURI());
+                    } catch (URISyntaxException e) {
+                        System.err.println("ERROR: failed in createLog4j, exiting now.");
+                        e.printStackTrace();
+                        return null;
+                    }
+                }
+            }
+        } else {
+            try {
+                reconfigureLog4J();
+            } catch (Exception e) {
+                System.err.println("ERROR: failed in createLog4j, exiting now.");
+                e.printStackTrace();
+                return null;
+            }
+
+            // TODO: hadoop and sparkling water cases
+        }
         return Logger.getLogger("water.default");
     }
 
+    public void reconfigureLog4J() {
+        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+        builder.setStatusLevel(L4J_LOGGING_LVLS[_level]);
+        builder.setConfigurationName("H2OLogConfiguration");
+        
+        // configure appenders:
+        String patternTail = _prefix + " %10.10t %5.5p %c: %m%n";
+        String pattern = "%d{MM-dd HH:mm:ss.SSS} " + patternTail;
+
+        LayoutComponentBuilder layoutComponentBuilder = builder.newLayout("PatternLayout").addAttribute("pattern", pattern);
+
+        builder.add(builder.newAppender("Console", "Console")
+                .addAttribute("target", "SYSTEM_OUT")
+                .add(layoutComponentBuilder));
+
+        builder.add(newRollingFileAppenderComponent(builder, "R1", "1MB", _getLogFilePath.apply("trace"), pattern, Level.TRACE));
+        builder.add(newRollingFileAppenderComponent(builder, "R2", _maxLogFileSize, _getLogFilePath.apply("debug"), pattern, Level.DEBUG));
+        builder.add(newRollingFileAppenderComponent(builder, "R3", _maxLogFileSize, _getLogFilePath.apply("info"), pattern, Level.INFO));
+        builder.add(newRollingFileAppenderComponent(builder, "R4", "256KB", _getLogFilePath.apply("warn"), pattern, Level.WARN));
+        builder.add(newRollingFileAppenderComponent(builder, "R5", "256KB", _getLogFilePath.apply("error"), pattern, Level.ERROR));
+        builder.add(newRollingFileAppenderComponent(builder, "R6", "256KB", _getLogFilePath.apply("fatal"), pattern, Level.FATAL));
+        builder.add(newRollingFileAppenderComponent(builder, "HTTPD", "1MB", _getLogFilePath.apply("httpd"), "%d{ISO8601} " + patternTail, Level.TRACE));
+
+        AppenderRefComponentBuilder consoleAppenderRef = builder.newAppenderRef("Console");
+        
+        // configure loggers:
+        List<AppenderRefComponentBuilder> appenderReferences = new ArrayList();
+        appenderReferences.add(builder.newAppenderRef("R1"));
+        appenderReferences.add(builder.newAppenderRef("R2"));
+        appenderReferences.add(builder.newAppenderRef("R3"));
+        appenderReferences.add(builder.newAppenderRef("R4"));
+        appenderReferences.add(builder.newAppenderRef("R5"));
+        appenderReferences.add(builder.newAppenderRef("R6"));
+        appenderReferences.add(consoleAppenderRef);
+        
+        builder.add(newLoggerComponent(builder, "hex", appenderReferences));
+        builder.add(newLoggerComponent(builder, "water", appenderReferences));
+        builder.add(newLoggerComponent(builder, "ai.h2o", appenderReferences));
+        builder.add(builder.newRootLogger(String.valueOf(L4J_LVLS[_level])).add(consoleAppenderRef));
+        
+        // HTTPD logging
+        appenderReferences = new ArrayList();
+        appenderReferences.add(builder.newAppenderRef("HTTPD"));
+        builder.add(newLoggerComponent(builder, "water.api.RequestServer", appenderReferences));
+
+        // Turn down the logging for some class hierarchies.
+        appenderReferences = new ArrayList();
+        appenderReferences.add(consoleAppenderRef);
+        builder.add(newLoggerComponent(builder, "org.apache.http", appenderReferences, "WARN"));
+        builder.add(newLoggerComponent(builder, "com.amazonaws", appenderReferences, "WARN"));
+        builder.add(newLoggerComponent(builder, "org.apache.hadoop", appenderReferences, "WARN"));
+        builder.add(newLoggerComponent(builder, "org.jets3t.service", appenderReferences, "WARN"));
+        builder.add(newLoggerComponent(builder, "org.reflections.Reflections", appenderReferences, "ERROR"));
+        builder.add(newLoggerComponent(builder, "com.brsanthu.googleanalytics", appenderReferences, "ERROR"));
+        // Turn down the logging for external libraries that Orc parser depends on-->
+        builder.add(newLoggerComponent(builder, "org.apache.hadoop.util.NativeCodeLoader", appenderReferences, "ERROR"));
+        
+
+        Configurator.reconfigure(builder.build());
+    }
+
+    AppenderComponentBuilder newRollingFileAppenderComponent(ConfigurationBuilder builder, String name, String sizeBasedTriggeringPolicyValue,  String fileNameValue, String filePatternValue, Level thresholdFilterLevel) {
+        ComponentBuilder triggeringPolicy = builder.newComponent("Policies")
+                .addComponent(builder.newComponent("SizeBasedTriggeringPolicy").addAttribute("size", sizeBasedTriggeringPolicyValue));
+        LayoutComponentBuilder layoutBuilder = builder.newLayout("PatternLayout")
+                .addAttribute("pattern", filePatternValue);
+        FilterComponentBuilder thresholdFilter = builder.newFilter("ThresholdFilter", Filter.Result.ACCEPT, Filter.Result.DENY)
+                .addAttribute("level", thresholdFilterLevel.toString());
+        ComponentBuilder rolloverStrategy = builder.newComponent("DefaultRolloverStrategy").addAttribute("max", 3);
+
+        AppenderComponentBuilder appenderBuilder = builder.newAppender(name, "RollingFile")
+                .addAttribute("fileName", fileNameValue)
+                .addAttribute("filePattern", fileNameValue.concat(".%i"))
+                .add(thresholdFilter)
+                .addComponent(triggeringPolicy)
+                .addComponent(layoutBuilder)
+                .addComponent(rolloverStrategy);
+
+        return appenderBuilder;
+    }
+
+    LoggerComponentBuilder newLoggerComponent(ConfigurationBuilder builder, String name, List<AppenderRefComponentBuilder> appenderReferences) {
+        LoggerComponentBuilder loggerComponentBuilder = builder.newLogger(name);
+        for (AppenderRefComponentBuilder reference : appenderReferences) {
+            loggerComponentBuilder.add(reference);
+        }
+        loggerComponentBuilder.addAttribute("additivity", false);
+        return loggerComponentBuilder;
+    }
+
+    LoggerComponentBuilder newLoggerComponent(ConfigurationBuilder builder, String name, List<AppenderRefComponentBuilder> appenderReferences, String level) {
+        LoggerComponentBuilder loggerComponentBuilder = builder.newLogger(name);
+        for (AppenderRefComponentBuilder reference : appenderReferences) {
+            loggerComponentBuilder.add(reference);
+        }
+        loggerComponentBuilder.addAttribute("additivity", false);
+        loggerComponentBuilder.addAttribute("level", level);
+        return loggerComponentBuilder;
+    }
 }
