54e943dda92c7b358991807faf896115aadbb1e3
Adam Valenta <adam.valenta@h2o.ai>
2021-08-31 17:45:29 +0200

    Rename current Isolation Forest grid search test

--- h2o-r/tests/testdir_algos/isofor/runit_isofor_grid_supervised.R
+++ h2o-r/tests/testdir_algos/isofor/runit_isofor_grid_supervised.R
@@ -21,7 +21,6 @@ test.grid.resume <- function() {
       x = 1:4,
       y = 5,
       training_frame = iris.hex,
-      is_supervised = TRUE,
       hyper_params = hyper_parameters
     )
   grid_id <- baseline_grid@grid_id
@@ -53,7 +52,6 @@ test.grid.resume <- function() {
       y = 5,
       training_frame = train,
       validation_frame = test,
-      is_supervised = TRUE,
       hyper_params = hyper_parameters,
       validation_response_column = "label"
     )
