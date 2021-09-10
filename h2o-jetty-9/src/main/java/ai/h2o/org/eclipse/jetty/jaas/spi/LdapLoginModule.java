851daa1da87f1f9246da804297d45765cafe7d14
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-10 23:31:17 +0800

    Delete h2o-jetty-9 directory

--- h2o-jetty-9/src/main/java/ai/h2o/org/eclipse/jetty/jaas/spi/LdapLoginModule.java
+++
@@ -1,8 +0,0 @@
-package ai.h2o.org.eclipse.jetty.jaas.spi;
-
-/**
- * LdapLoginModule is relocated in Sparkling Water to the package ai.h2o.org.eclipse.jetty.jaas.spi
- * of Jetty 9. This class lets user define login module that will work both for H2O and SW
- * (user needs to put "org.eclipse.jetty.jaas.spi.LdapLoginModule required" in the login conf)
- */
-public class LdapLoginModule extends org.eclipse.jetty.jaas.spi.LdapLoginModule { /* empty */ }
--- h2o-jetty-9/src/main/java/ai/h2o/org/eclipse/jetty/plus/jaas/spi/LdapLoginModule.java
+++
@@ -1,8 +0,0 @@
-package ai.h2o.org.eclipse.jetty.plus.jaas.spi;
-
-/**
- * LdapLoginModule is relocated in Sparkling Water to the package ai.h2o.org.eclipse.jetty.jaas.spi
- * of Jetty 9. External backend workers on Hadoop 2 utilize Jetty 8 and thus the module 
- * org.eclipse.jetty.plus.jaas.spi. This class enables to use only one package name for both cases.
- */
-public class LdapLoginModule extends org.eclipse.jetty.jaas.spi.LdapLoginModule { /* empty */ }
