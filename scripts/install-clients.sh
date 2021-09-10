aa6ac13f82320b0ee37928f99537262cb9c2db4e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-11 00:00:29 +0800

    Delete scripts directory

--- scripts/install-clients.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# After building H2O with ./gradlew build
-# run this script to install the new R and Python clients.
-
-pip uninstall -y h2o
-pip install ../h2o-py/build/dist/h2o-*-py2.py3-none-any.whl
-R CMD REMOVE h2o
-R CMD INSTALL ../h2o-r/R/src/contrib/h2o*.tar.gz
--- scripts/jenkins/groovy/coverageStage.groovy
+++
@@ -1,9 +0,0 @@
-def call(final pipelineContext, final stageConfig) {
-
-  def defaultStage = load('h2o-3/scripts/jenkins/groovy/defaultStage.groovy')
-  defaultStage(pipelineContext, stageConfig)
-
-  jacoco(execPattern: "${stageConfig.stageDir}/h2o-3/build/reports/jacoco/report.exec")
-}
-
-return this
