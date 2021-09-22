d2b5ef0028d106409fc06294e5daab5d0f14c7eb
maurever <veronika@maurerova.cz>
2021-09-22 14:15:39 +0200

    PUBDEV-8297 add model_id parameter

--- h2o-py/h2o/estimators/generic.py
+++ h2o-py/h2o/estimators/generic.py
@@ -102,12 +102,13 @@ class H2OGenericEstimator(H2OEstimator):
 
 
     @staticmethod
-    def from_file(file=str):
+    def from_file(file=str, model_id=None):
         """
         Creates new Generic model by loading existing embedded model into library, e.g. from H2O MOJO.
         The imported model must be supported by H2O.
 
         :param file: A string containing path to the file to create the model from
+        :param model_id: Model ID, default None
         :return: H2OGenericEstimator instance representing the generic model
 
         :examples:
@@ -122,7 +123,7 @@ class H2OGenericEstimator(H2OEstimator):
         >>> model = H2OGenericEstimator.from_file(original_model_filename)
         >>> model.model_performance()
         """
-        model = H2OGenericEstimator(path = file)
+        model = H2OGenericEstimator(path=file, model_id=model_id)
         model.train()
 
         return model
