b31314a70432f1b90326247d9ab0d9e84aabc636
Sebastien Poirier <sebastien@h2o.ai>
2021-09-23 21:12:47 +0200

    add smoke tests + stage to run only those with XGB disabled

--- h2o-py/tests/testdir_algos/automl/pyunit_SMOKE_automl.py
+++ h2o-py/tests/testdir_algos/automl/pyunit_SMOKE_automl.py
@@ -1,28 +1,30 @@
 from __future__ import print_function
 
-import contextlib
 import os
 import sys
 
 sys.path.insert(1, os.path.join("..","..",".."))
 import h2o
 from tests import pyunit_utils as pu
-from h2o.automl import H2OAutoML
+from h2o.automl import H2OAutoML, get_leaderboard
 from h2o.model import ModelBase
 
 
-def test_smoke_with_xgboost_disabled():
+def test_smoke_automl():
     fr = h2o.import_file(path=pu.locate("smalldata/prostate/prostate.csv"))
     target = "CAPSULE"
     fr[target] = fr[target].asfactor()
+    nmodels = 20  # enough models to run every step (all base models, all grids, all SEs…)
     
-    aml = H2OAutoML(max_models=3, project_name="py_aml_xgb_disabled", seed=42, verbosity='debug')
+    aml = H2OAutoML(max_models=nmodels, project_name="py_aml_smoke", seed=42, verbosity='debug')
     model = aml.train(y=target, training_frame=fr)
     
     assert isinstance(model, ModelBase)
-    print(aml.get_leaderboard(['provider', 'step', 'group']))
+    lb = get_leaderboard(aml, ['algos', 'provider', 'step', 'group'])
+    print(lb)
+    assert lb.nrows > nmodels
 
 
 pu.run_tests([
-    test_smoke_with_xgboost_disabled
-], init_options=dict(jvm_custom_args=["-Dsys.ai.h2o.ext.core.toggle.XGBoost=false"]))
+    test_smoke_automl
+])
