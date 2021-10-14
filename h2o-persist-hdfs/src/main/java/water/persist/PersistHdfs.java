d97f543eb4777d7209ecf6c850dd23f640f84d7c
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-10-14 16:43:23 +0200

    fix npe

--- h2o-persist-hdfs/src/main/java/water/persist/PersistHdfs.java
+++ h2o-persist-hdfs/src/main/java/water/persist/PersistHdfs.java
@@ -301,7 +301,7 @@ public final class PersistHdfs extends Persist {
   }
 
   public static void startDelegationTokenRefresher(Path p) throws IOException {
-    if (lastSavedHadoopConfiguration != null && !lastSavedHadoopConfiguration.getBoolean(H2O_DYNAMIC_AUTH_S3A_TOKEN_REFRESHER_ENABLED, false)) {
+    if (lastSavedHadoopConfiguration == null || !lastSavedHadoopConfiguration.getBoolean(H2O_DYNAMIC_AUTH_S3A_TOKEN_REFRESHER_ENABLED, false)) {
       return;
     }
     if (isInBucketWithAlreadyExistingToken(p.toUri())) {
