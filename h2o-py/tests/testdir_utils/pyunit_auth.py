f2c20b55c7e827272ef0d1614b1cf160b853a6ef
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-10-21 14:55:07 +0200

    HEXDEV-777: refactor + add basic cases

--- h2o-py/tests/testdir_utils/pyunit_auth.py
+++ h2o-py/tests/testdir_utils/pyunit_auth.py
@@ -6,18 +6,23 @@ from h2o.auth import SpnegoAuth
 
 
 def test_is_spnego_response():
-    invalid1 = "Digest realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    valid1 = "Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    valid2 = "Basic, Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    invalid2 = "Basic Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    invalid3 = "Basic, Negotiatesds realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    valid3 = "Basic realm='http-auth@example.org', Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    invalid4 = "Basic, asdfNegotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    invalid5 = "Basic realm='http-auth@example.org', qop='auth, auth-int', algorithm=Negotiate, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
-    invalid6 = "Custom realm='apps', negotiate='true'"
-
     edgeCaseNotSupported = "Basic realm='apps', title='Foo, Negotiate, Bar'"
-    
+    invalid = {
+        "Digest realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+        "Basic Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+        "Basic, Negotiatesds realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+        "Basic, asdfNegotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+        "Basic realm='http-auth@example.org', qop='auth, auth-int', algorithm=Negotiate, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+        "Custom realm='apps', negotiate='true'"        
+    }
+    valid = {"Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+             "Basic, Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+             "Basic realm='http-auth@example.org', Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'",
+             "Negotiate", 
+             "Basic, Negotiate", 
+             "Negotiate, Basic",
+             edgeCaseNotSupported
+    }
     
     spnego = SpnegoAuth("dummyPrincipal")
 
@@ -25,38 +30,14 @@ def test_is_spnego_response():
     response.status_code = 200
     response._content = b'{ "key" : "a" }'
     
-    response.headers['www-authenticate'] = invalid1
-    assert spnego._is_spnego_response(response) is False
+    for header in invalid:
+        response.headers['www-authenticate'] = header
+        assert spnego._is_spnego_response(response) is False
 
-    response.headers['www-authenticate'] = valid1
-    assert spnego._is_spnego_response(response) is True
+    for header in valid:
+        response.headers['www-authenticate'] = header
+        assert spnego._is_spnego_response(response) is True
 
-    response.headers['www-authenticate'] = valid2
-    assert spnego._is_spnego_response(response) is True
-
-    response.headers['www-authenticate'] = invalid2
-    assert spnego._is_spnego_response(response) is False
-
-    response.headers['www-authenticate'] = invalid3
-    assert spnego._is_spnego_response(response) is False
-
-    response.headers['www-authenticate'] = valid3
-    assert spnego._is_spnego_response(response) is True
-
-    response.headers['www-authenticate'] = invalid4
-    assert spnego._is_spnego_response(response) is False
-
-    response.headers['www-authenticate'] = invalid5
-    assert spnego._is_spnego_response(response) is False
-
-    response.headers['www-authenticate'] = edgeCaseNotSupported
-    assert spnego._is_spnego_response(response) is True
-
-    response.headers['www-authenticate'] = invalid6
-    assert spnego._is_spnego_response(response) is False
-
-
-    
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(test_is_spnego_response)
