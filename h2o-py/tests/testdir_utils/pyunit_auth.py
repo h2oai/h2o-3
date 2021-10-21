c31dc3b4416aa07656524e0c26dcc895846bed9a
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-10-21 13:02:46 +0200

    HEXDEV-777: convert edgeCaseSupported to invalid6

--- h2o-py/tests/testdir_utils/pyunit_auth.py
+++ h2o-py/tests/testdir_utils/pyunit_auth.py
@@ -14,9 +14,9 @@ def test_is_spnego_response():
     valid3 = "Basic realm='http-auth@example.org', Negotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
     invalid4 = "Basic, asdfNegotiate realm='http-auth@example.org', qop='auth, auth-int', algorithm=MD5, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
     invalid5 = "Basic realm='http-auth@example.org', qop='auth, auth-int', algorithm=Negotiate, nonce='7ypf/xlj9XXwfDPEoM4URrv/xwf94BcCAzFZH4GiTo0v', opaque='FQhe/qaU925kfnzjCev0ciny7QMkPqMAFRtzCUYo5tdS'"
+    invalid6 = "Custom realm='apps', negotiate='true'"
 
     edgeCaseNotSupported = "Basic realm='apps', title='Foo, Negotiate, Bar'"
-    edgeCaseSupported = "Custom realm='apps', negotiate='true'"
     
     
     spnego = SpnegoAuth("dummyPrincipal")
@@ -52,7 +52,7 @@ def test_is_spnego_response():
     response.headers['www-authenticate'] = edgeCaseNotSupported
     assert spnego._is_spnego_response(response) is True
 
-    response.headers['www-authenticate'] = edgeCaseSupported
+    response.headers['www-authenticate'] = invalid6
     assert spnego._is_spnego_response(response) is False
 
 
