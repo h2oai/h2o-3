7daca9ef279f67b6ecc158b666f64893548c0c49
Michal Kurka <michalk@h2o.ai>
2021-10-22 12:25:09 -0400

    Add a smoke test for minimal assembly

--- scripts/jenkins/groovy/buildConfig.groovy
+++ scripts/jenkins/groovy/buildConfig.groovy
@@ -36,7 +36,8 @@ class BuildConfig {
   // always run
   public static final String COMPONENT_ANY = 'any'
   public static final String COMPONENT_HADOOP = 'hadoop'
-  public static final List<String> TEST_PACKAGES_COMPONENTS = [COMPONENT_PY, COMPONENT_R, COMPONENT_JS, COMPONENT_JAVA, COMPONENT_HADOOP]
+  public static final String COMPONENT_MINIMAL = 'minimal' 
+  public static final List<String> TEST_PACKAGES_COMPONENTS = [COMPONENT_PY, COMPONENT_R, COMPONENT_JS, COMPONENT_JAVA, COMPONENT_HADOOP, COMPONENT_MINIMAL]
 
   public static final String H2O_JAR_STASH_NAME = 'h2o-3-stash-jar'
   private static final String TEST_PACKAGE_STASH_NAME_PREFIX = 'h2o-3-stash'
@@ -54,7 +55,7 @@ class BuildConfig {
   public static final String MAKEFILE_PATH = 'scripts/jenkins/Makefile.jenkins'
   public static final String BENCHMARK_MAKEFILE_PATH = 'ml-benchmark/jenkins/Makefile.jenkins'
 
-  private static final List<String> STASH_ALWAYS_COMPONENTS = [COMPONENT_R]
+  private static final List<String> STASH_ALWAYS_COMPONENTS = [COMPONENT_R, COMPONENT_MINIMAL]
 
   private static final String JACOCO_GRADLE_OPT = 'jacocoCoverage'
 
--- scripts/jenkins/groovy/buildH2O3.groovy
+++ scripts/jenkins/groovy/buildH2O3.groovy
@@ -67,6 +67,13 @@ def call(final pipelineContext) {
                                 makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
                                 activatePythonEnv = true
                             }
+                            makeTarget(pipelineContext) {
+                                target = 'test-package-minimal'
+                                hasJUnit = false
+                                archiveFiles = false
+                                makefilePath = pipelineContext.getBuildConfig().MAKEFILE_PATH
+                                activatePythonEnv = true
+                            }
                         } else {
                             makeTarget(pipelineContext) {
                                 target = 'test-package-gradle'
