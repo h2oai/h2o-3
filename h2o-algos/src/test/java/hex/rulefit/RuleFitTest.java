f1b67785bc9467570ce618b8bcffd855e7f883aa
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-11-02 08:55:38 +0100

    PUBDEV-8133: commit seed to reproduce scoring bug

--- h2o-algos/src/test/java/hex/rulefit/RuleFitTest.java
+++ h2o-algos/src/test/java/hex/rulefit/RuleFitTest.java
@@ -1014,7 +1014,7 @@ public class RuleFitTest extends TestUtil {
             parms._model_type = RuleFitModel.ModelType.RULES;
             parms._min_rule_length = 1;
             parms._max_rule_length = 10;
-            parms._seed = 1234;
+            parms._seed = -2348835740834922574L;
 
             RuleFitModel rfit = new RuleFit(parms).trainModel().get();
             Scope.track_generic(rfit);
