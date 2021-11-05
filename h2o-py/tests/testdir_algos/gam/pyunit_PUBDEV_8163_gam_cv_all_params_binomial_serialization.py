6499603678a9f774695fc34d2c393c3f8d6f9f74
wendycwong <wendycwong@yahoo.com>
2021-11-05 13:38:14 -0700

    Reformat new test per tomf suggestion

--- h2o-py/tests/testdir_algos/gam/pyunit_PUBDEV_8163_gam_cv_all_params_binomial_serialization.py
+++ h2o-py/tests/testdir_algos/gam/pyunit_PUBDEV_8163_gam_cv_all_params_binomial_serialization.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from __future__ import print_function
 import sys
+
 sys.path.insert(1, "../../../")
 import h2o
 from tests import pyunit_utils
@@ -8,6 +9,7 @@ from h2o.estimators.gam import H2OGeneralizedAdditiveEstimator
 import random
 import tempfile
 import os
+
 
 # test to make sure serlization works with saved models, frames from cv run
 def test_gam_model_predict():
@@ -27,24 +29,24 @@ def test_gam_model_predict():
     myY = "C21"
     h2o_data["C21"] = h2o_data["C21"].asfactor()
 
-    nfold = random.randint(3,8)
-    h2o_model = H2OGeneralizedAdditiveEstimator(family = 'binomial', gam_columns = ["C11"],  scale = [0.0001],
-                                                nfolds = nfold,
-                                                keep_cross_validation_models = True,
-                                                keep_cross_validation_predictions = True,
-                                                keep_cross_validation_fold_assignment = True,
-                                                fold_assignment = "random")
-    h2o_model.train(x=list(range(0,20)), y=myY, training_frame=h2o_data)
+    nfold = random.randint(3, 8)
+    h2o_model = H2OGeneralizedAdditiveEstimator(family='binomial', gam_columns=["C11"], scale=[0.0001],
+                                                nfolds=nfold,
+                                                keep_cross_validation_models=True,
+                                                keep_cross_validation_predictions=True,
+                                                keep_cross_validation_fold_assignment=True,
+                                                fold_assignment="random")
+    h2o_model.train(x=list(range(0, 20)), y=myY, training_frame=h2o_data)
     tmpdir = tempfile.mkdtemp()
     model_path = h2o.save_model(h2o_model, tmpdir)
     xval_predictions = h2o_model.cross_validation_holdout_predictions()
     xval_filename = os.path.join(tmpdir, "xval_predictions.csv")
     h2o.download_csv(xval_predictions, xval_filename)
- 
+
     xval_fold_assignments = h2o_model.cross_validation_fold_assignment()
     xval_fold_filename = os.path.join(tmpdir, "xval_fold_assignments.csv")
     h2o.download_csv(xval_fold_assignments, xval_fold_filename)
-    
+
     h2o.remove_all()
     loaded_model = h2o.load_model(model_path)
 
@@ -52,7 +54,7 @@ def test_gam_model_predict():
     xval_predictions_loaded["predict"] = xval_predictions_loaded["predict"].asnumeric()
     xval_pred_original = h2o.import_file(xval_filename)
     pyunit_utils.compare_frames_local(xval_pred_original, xval_predictions_loaded, prob=1.0)
-    
+
     xval_fold_loaded = loaded_model.cross_validation_fold_assignment()
     xval_fold_original = h2o.import_file(xval_fold_filename)
     pyunit_utils.compare_frames_local(xval_fold_loaded, xval_fold_original, prob=1.0)
@@ -74,7 +76,7 @@ def test_gam_model_predict():
     h2o_data["C21"] = h2o_data["C21"].asfactor()
     pred_frame = xval_models[0].predict(h2o_data)
     assert pred_frame.nrow == h2o_data.nrow, "Expected number of rows {0}, actual number of rows " \
-                                         "{1}".format(pred_frame.nrow, h2o_data.nrow)
+                                             "{1}".format(pred_frame.nrow, h2o_data.nrow)
     print("Test complete.")
 
 
