7f8ecc6b4206c72a9467f88911b0d15e0edc41d2
zuzanaolajcova <zuzana.olajcova@h2o.ai>
2021-11-05 10:37:31 +0100

    PUBDEV-8385: test decorate_plot_result()

--- h2o-py/tests/testdir_algos/glm/pyunit_PUBDEV-8385_add_saving_parameters_to_plot_functions.py
+++ h2o-py/tests/testdir_algos/glm/pyunit_PUBDEV-8385_add_saving_parameters_to_plot_functions.py
@@ -1,10 +1,14 @@
 import sys
 sys.path.insert(1,"../../../")
 import h2o
+import tempfile
 from tests import pyunit_utils, test_plot_result_saving
 from h2o.estimators.glm import H2OGeneralizedLinearEstimator as glm
-import tempfile
+from h2o.plot import decorate_plot_result
 
+def tests():
+    binomial_plot_test()
+    test_decorate_plot_result()
 
 def binomial_plot_test():
     benign = h2o.import_file(pyunit_utils.locate("smalldata/logreg/benign.csv"))
@@ -19,6 +23,51 @@ def binomial_plot_test():
     path2 = "{}/plot2.png".format(tmpdir)
     test_plot_result_saving(model.plot(timestep="AUTO", metric="objective", server=True), path2,
                             model.plot(timestep="AUTO", metric="objective", server=True, save_plot_path=path1), path1)
+    
+def test_decorate_plot_result():
+    figure = "let's pretend I'm a figure"
+
+    res = decorate_plot_result(figure=figure)
+    assert res.figure == figure
+
+    res = decorate_plot_result((3, 5), figure=figure)
+    assert isinstance(res, tuple)
+    a, b = res
+    assert a == 3
+    assert b == 5
+    assert res.figure == figure
+
+    res = decorate_plot_result([3, 5, 6], figure=figure)
+    assert isinstance(res, list)
+    assert res == [3, 5, 6]
+    assert res.figure == figure
+
+    somedict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+
+    res = decorate_plot_result({"brand": "Ford", "model": "Mustang", "year": 1964}, figure=figure)
+    assert isinstance(res, dict)
+    assert res == {"brand": "Ford", "model": "Mustang", "year": 1964}
+    assert res.figure == figure
+
+    res = decorate_plot_result("Hi", figure=figure)
+    assert isinstance(res, str)
+    assert res == "Hi"
+    assert res.figure == figure
+
+    class Foo(object):
+        def __init__(self, bar=None):
+            self.bar=bar
+    
+    res = decorate_plot_result(Foo(bar="baz"), figure=figure)
+    assert isinstance(res, Foo)
+    assert res.bar == "baz"
+    assert res.figure == figure
+    
+    
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(binomial_plot_test)
