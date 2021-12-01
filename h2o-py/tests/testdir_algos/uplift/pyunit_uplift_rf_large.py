878088915e2eb432828ad8e954d22c7e35acda71
maurever <veronika@maurerova.cz>
2021-12-01 19:52:48 +0100

    PUBDEV-4940 fix tests

--- h2o-py/tests/testdir_algos/uplift/pyunit_uplift_rf_large.py
+++ h2o-py/tests/testdir_algos/uplift/pyunit_uplift_rf_large.py
@@ -1,56 +1,61 @@
 from __future__ import print_function
 import sys, os
+import unittest
 
 sys.path.insert(1, os.path.join("..", "..", ".."))
 import h2o
-from tests import pyunit_utils
 from h2o.estimators import H2OUpliftRandomForestEstimator
 from causalml.dataset import make_uplift_classification
 
 
-def uplift_random_forest_smoke():
-    n_samples = 10_000
-    seed = 12345
+class UpliftDrfLargeSmoke(unittest.TestCase):
 
-    # Data generation
-    # Generate dataset with 50 features, treatment/control flag feature
-    # and outcome feature (In this case "conversion")
-    train, x_names = make_uplift_classification(n_samples=n_samples,  # n_samples*2 rows will be generated
-                                                                      # (#n_samples for each treatment_name)
-                                                treatment_name=['control', 'treatment'],
-                                                n_classification_features=50,
-                                                # Dataset contains only features valid for modeling
-                                                # Do not confuse model with irrelevant or redundant features
-                                                n_classification_informative=50,
-                                                random_seed=seed
-                                                )
+    @unittest.skipIf(sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] <= 5),
+                     "Tested only on >3.5, causalml is not supported on lower python version")
+    def test_uplift_random_forest_smoke(self):
+        n_samples = 10_000
+        seed = 12345
 
-    treatment_column = "treatment_group_key"
-    response_column = "conversion"
+        h2o.init(strict_version_check=False)
+        # Data generation
+        # Generate dataset with 50 features, treatment/control flag feature
+        # and outcome feature (In this case "conversion")
+        train, x_names = make_uplift_classification(n_samples=n_samples,  # n_samples*2 rows will be generated
+                                                                          # (#n_samples for each treatment_name)
+                                                    treatment_name=['control', 'treatment'],
+                                                    n_classification_features=50,
+                                                    # Dataset contains only features valid for modeling
+                                                    # Do not confuse model with irrelevant or redundant features
+                                                    n_classification_informative=50,
+                                                    random_seed=seed
+                                                    )
 
-    train_h2o = h2o.H2OFrame(train)
-    train_h2o[treatment_column] = train_h2o[treatment_column].asfactor()
-    train_h2o[response_column] = train_h2o[response_column].asfactor()
+        treatment_column = "treatment_group_key"
+        response_column = "conversion"
 
-    uplift_predict_kl = uplift_train_predict("KL", x_names, treatment_column, response_column, train_h2o, seed)
-    uplift_predict_euc = uplift_train_predict("euclidean", x_names, treatment_column, response_column, train_h2o, seed)
-    uplift_predict_chi = uplift_train_predict("chi_squared", x_names, treatment_column, response_column, train_h2o, seed)
+        train_h2o = h2o.H2OFrame(train)
+        train_h2o[treatment_column] = train_h2o[treatment_column].asfactor()
+        train_h2o[response_column] = train_h2o[response_column].asfactor()
 
-    print("KL:" + str(uplift_predict_kl.mean().as_data_frame()["uplift_predict"][0]))
-    print("EUC:" + str(uplift_predict_euc.mean().as_data_frame()["uplift_predict"][0]))
-    print("CHI:" + str(uplift_predict_chi.mean().as_data_frame()["uplift_predict"][0]))
+        uplift_predict_kl = uplift_train_predict("KL", x_names, treatment_column, response_column, train_h2o, seed)
+        uplift_predict_euc = uplift_train_predict("euclidean", x_names, treatment_column, response_column, train_h2o, seed)
+        uplift_predict_chi = uplift_train_predict("chi_squared", x_names, treatment_column, response_column, train_h2o, seed)
 
-    assert 0.007 < uplift_predict_kl.mean().as_data_frame()["uplift_predict"][0] < 0.008, \
-        "Not expected output: Mean uplift is suspiciously different. " \
-        + str(uplift_predict_kl.mean().as_data_frame()["uplift_predict"][0])
+        print("KL:" + str(uplift_predict_kl.mean().as_data_frame()["uplift_predict"][0]))
+        print("EUC:" + str(uplift_predict_euc.mean().as_data_frame()["uplift_predict"][0]))
+        print("CHI:" + str(uplift_predict_chi.mean().as_data_frame()["uplift_predict"][0]))
 
-    assert 0.0075 < uplift_predict_euc.mean().as_data_frame()["uplift_predict"][0] < 0.0085, \
-        "Not expected output: Mean uplift is suspiciously different." \
-        + str(uplift_predict_euc.mean().as_data_frame()["uplift_predict"][0].mean())
+        assert 0.007 < uplift_predict_kl.mean().as_data_frame()["uplift_predict"][0] < 0.008, \
+            "Not expected output: Mean uplift is suspiciously different. " \
+            + str(uplift_predict_kl.mean().as_data_frame()["uplift_predict"][0])
 
-    assert 0.01 < uplift_predict_chi.mean().as_data_frame()["uplift_predict"][0] < 0.02, \
-        "Not expected output: Mean uplift is suspiciously different." \
-        + str(uplift_predict_chi.mean().as_data_frame()["uplift_predict"][0].mean())
+        assert 0.0075 < uplift_predict_euc.mean().as_data_frame()["uplift_predict"][0] < 0.0085, \
+            "Not expected output: Mean uplift is suspiciously different." \
+            + str(uplift_predict_euc.mean().as_data_frame()["uplift_predict"][0].mean())
+
+        assert 0.01 < uplift_predict_chi.mean().as_data_frame()["uplift_predict"][0] < 0.02, \
+            "Not expected output: Mean uplift is suspiciously different." \
+            + str(uplift_predict_chi.mean().as_data_frame()["uplift_predict"][0].mean())
 
 
 def uplift_train_predict(uplift_metric, x_names, treatment_column, response_column, train_h2o, seed):
@@ -73,7 +78,5 @@ def uplift_train_predict(uplift_metric, x_names, treatment_column, response_colu
     return prediction['uplift_predict']
 
 
-if __name__ == "__main__":
-    pyunit_utils.standalone_test(uplift_random_forest_smoke)
-else:
-    uplift_random_forest_smoke()
+suite = unittest.TestLoader().loadTestsFromTestCase(UpliftDrfLargeSmoke)
+unittest.TextTestRunner().run(suite)
--- h2o-py/tests/testdir_algos/uplift/pyunit_uplift_rf_saveload.py
+++ h2o-py/tests/testdir_algos/uplift/pyunit_uplift_rf_saveload.py
@@ -5,33 +5,21 @@ sys.path.insert(1, os.path.join("..", "..", ".."))
 import h2o
 from tests import pyunit_utils, assert_equals
 from h2o.estimators import H2OUpliftRandomForestEstimator
-from causalml.dataset import make_uplift_classification
 
 
 def uplift_random_forest_save_and_load():
     print("Uplift Distributed Random Forest Save Load Test")
-    n_samples = 500
     seed = 12345
 
-    # Data generation
-    # Generate dataset with 10 features, treatment/control flag feature
-    # and outcome feature (In this case "conversion")
-    train, x_names = make_uplift_classification(n_samples=n_samples,  # n_samples*2 rows will be generated
-                                                                      # (#n_samples for each treatment_name)
-                                                treatment_name=['control', 'treatment'],
-                                                n_classification_features=10,
-                                                # Dataset contains only features valid for modeling
-                                                # Do not confuse model with irrelevant or redundant features
-                                                n_classification_informative=10,
-                                                random_seed=seed
-                                                )
+    treatment_column = "treatment"
+    response_column = "outcome"
+    x_names = ["feature_"+str(x) for x in range(1, 13)]
 
-    treatment_column = "treatment_group_key"
-    response_column = "conversion"
-
-    train_h2o = h2o.H2OFrame(train)
+    train_h2o = h2o.upload_file(pyunit_utils.locate("smalldata/uplift/upliftml_train.csv"))
     train_h2o[treatment_column] = train_h2o[treatment_column].asfactor()
     train_h2o[response_column] = train_h2o[response_column].asfactor()
+
+    n_samples = train_h2o.shape[0]
 
     uplift_model = H2OUpliftRandomForestEstimator(
         ntrees=10,
@@ -47,7 +35,7 @@ def uplift_random_forest_save_and_load():
     )
     uplift_model.train(y=response_column, x=x_names, training_frame=train_h2o)
     prediction = uplift_model.predict(train_h2o)
-    assert_equals(n_samples*2, prediction.shape[0], "Not correct shape")
+    assert_equals(n_samples, prediction.shape[0], "Not correct shape")
     assert_equals(3, prediction.shape[1], "Not correct shape")
     print(prediction)
     uplift_predict = prediction['uplift_predict'].as_data_frame(use_pandas=True)["uplift_predict"]
@@ -60,7 +48,7 @@ def uplift_random_forest_save_and_load():
     assert os.path.isfile(model_path), "Expected load file {0} to exist, but it does not. ".format(model_path)
     reloaded = h2o.load_model(model_path)
     prediction_reloaded = reloaded.predict(train_h2o)
-    assert_equals(n_samples*2, prediction.shape[0], "Not correct shape")
+    assert_equals(n_samples, prediction.shape[0], "Not correct shape")
     assert_equals(3, prediction.shape[1], "Not correct shape")
     print(prediction_reloaded)
     uplift_predict_reloaded = prediction_reloaded['uplift_predict'].as_data_frame(use_pandas=True)["uplift_predict"]
