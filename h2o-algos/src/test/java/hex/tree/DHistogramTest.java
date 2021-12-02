89b73250b15f70066421e56920ab343c828065e2
Michal Kurka <michalk@h2o.ai>
2021-12-02 09:36:23 -0500

    Uplift clean-up: removed method only called from tests

--- h2o-algos/src/test/java/hex/tree/DHistogramTest.java
+++ h2o-algos/src/test/java/hex/tree/DHistogramTest.java
@@ -192,14 +192,14 @@ public class DHistogramTest extends TestUtil {
             SharedTreeModel.SharedTreeParameters.HistogramType.UniformAdaptive, 42L, null, null, false);
     histoOpt.init();
 
-    histoOpt.updateHisto(weights, null, dataInt, ys, null, rows, N, 0);
+    histoOpt.updateHisto(weights, null, dataInt, ys, null, rows, N, 0, null);
 
     // optimization OFF
     DHistogram histo = new DHistogram("intOpt-off", 1000, 1024, (byte) 1, 0, 1000, false, false, -0.001,
             SharedTreeModel.SharedTreeParameters.HistogramType.UniformAdaptive, 42L, null, null, false);
     histo.init();
 
-    histo.updateHisto(weights, null, data, ys, null, rows, N, 0);
+    histo.updateHisto(weights, null, data, ys, null, rows, N, 0, null);
 
     assertEquals(histo._min2, histoOpt._min2, 0);
     assertEquals(histo._maxIn, histoOpt._maxIn, 0);
