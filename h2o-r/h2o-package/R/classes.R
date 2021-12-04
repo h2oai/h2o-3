b8035e8b7534a1bde8bb076ffa86f20e21136132
ledell <erin@h2o.ai>
2021-12-04 10:07:20 -0800

    PUDBEV-8222: remove initialize class in classes.R

--- h2o-r/h2o-package/R/classes.R
+++ h2o-r/h2o-package/R/classes.R
@@ -961,7 +961,6 @@ setClass("H2OInfogram", slots = c(model_id = "character",
                                   admissible_score = "H2OFrame"))
 
 
-setGeneric("initialize")
 
 #' Method on \code{H2OInfogram} object which in this case is to instantiate and initialize it
 #'
