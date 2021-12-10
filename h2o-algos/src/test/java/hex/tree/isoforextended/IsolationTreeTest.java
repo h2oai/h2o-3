d496ebaf75ebaf726f0259957e1f8e0cf8a4ccfa
Adam Valenta <adam.valenta@h2o.ai>
2021-12-10 20:30:46 +0100

    eif - add isolation tree test for statistic values

--- h2o-algos/src/test/java/hex/tree/isoforextended/IsolationTreeTest.java
+++ h2o-algos/src/test/java/hex/tree/isoforextended/IsolationTreeTest.java
@@ -105,4 +105,62 @@ public class IsolationTreeTest extends TestUtil {
         assertEquals("Array should contain two zeros: " + Arrays.toString(a),2, numOfZeros);
         assertArrayEquals("Arrays are different: " + Arrays.toString(a), new double[]{0.866, 0.0, 1.657, -0.166, 0.0}, a, 1e-3);
     }
+
+    @Test
+    public void testIsolationTreeStatsSmokeTreeLimitEqualToDataSize() {
+        double[][] data = new double[][]{
+                {5, 0.25159, 0.98433, 0.96306, -0.99260, -0.26453, -0.20910, 1.61677, -0.83930, -0.63632, 0.63247, 0.42071, -0.06717, -0.23723, -0.19529, 0.92324},
+                {3, 1.49243, 1.24084, -0.93344, 1.54171, 0.32838, -1.12037, -0.46590, 0.74632, 1.13758, 0.93562, 1.17489, 0.42124, 1.42660, -0.11774, 0.46041}
+        };
+        IsolationTree isolationTree = new IsolationTree(4, 1);
+        isolationTree.buildTree(data, 0xF00D, 0);
+        isolationTree.logNodesNumRows(Level.INFO);
+        assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 2, isolationTree.getIsolatedPoints());
+        assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 14, isolationTree.getNotIsolatedPoints());
+        assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 1, isolationTree.getZeroSplits());
+        assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 6, isolationTree.getLeaves());
+        assertEquals("Depth should almost always be equal to height limit", 4, isolationTree.getDepth());
+
+        assertEquals("Some point are lost in the process: ", data[0].length, isolationTree.getIsolatedPoints() + isolationTree.getNotIsolatedPoints());
+    }
+
+    @Test
+    public void testIsolationTreeStatsSmokeTreeLimitGreaterThanDataSize() {
+        try {
+            Scope.enter();
+            Frame train = Scope.track(parseTestFile("smalldata/anomaly/single_blob.csv"));
+            IsolationTree isolationTree = new IsolationTree(9, 1);
+            isolationTree.buildTree(FrameUtils.asDoubles(train), 0xF00D, 0);
+            isolationTree.logNodesNumRows(Level.INFO);
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 11, isolationTree.getIsolatedPoints());
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 489, isolationTree.getNotIsolatedPoints());
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 22, isolationTree.getZeroSplits());
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 51, isolationTree.getLeaves());
+            assertEquals("Depth should almost always be equal to height limit", 9, isolationTree.getDepth());
+
+            assertEquals("Some points are lost in the process: ", train.numRows(), isolationTree.getIsolatedPoints() + isolationTree.getNotIsolatedPoints());
+        } finally {
+            Scope.exit();
+        }
+    }
+
+    @Test
+    public void testIsolationTreeStatsSmokeTreeLimitLessThanDataSize() {
+        try {
+            Scope.enter();
+            Frame train = Scope.track(parseTestFile("smalldata/anomaly/single_blob.csv"));
+            IsolationTree isolationTree = new IsolationTree(7, 1);
+            isolationTree.buildTree(FrameUtils.asDoubles(train), 0xF00D, 0);
+            isolationTree.logNodesNumRows(Level.INFO);
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 2, isolationTree.getIsolatedPoints());
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 498, isolationTree.getNotIsolatedPoints());
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 9, isolationTree.getZeroSplits());
+            assertEquals("Check if isolation tree splitting correctly and adjust the number in case of inner splitting change", 23, isolationTree.getLeaves());
+            assertEquals("Depth should almost always be equal to height limit", 7, isolationTree.getDepth());
+
+            assertEquals("Some points are lost in the process: ", train.numRows(), isolationTree.getIsolatedPoints() + isolationTree.getNotIsolatedPoints());
+        } finally {
+            Scope.exit();
+        }
+    }
 }
