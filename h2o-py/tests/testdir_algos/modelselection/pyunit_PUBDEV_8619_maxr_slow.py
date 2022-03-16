ca5f6ce5dc28f53da8b7046e8481ccd986b7a23a
wendycwong <wendycwong@yahoo.com>
2022-03-16 12:08:20 -0700

    add time info

--- h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
+++ h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
@@ -5,7 +5,7 @@ sys.path.insert(1, "../../../")
 import h2o
 from tests import pyunit_utils
 from h2o.estimators.model_selection import H2OModelSelectionEstimator
-
+import time
 # test to find out why maxr is slow
 def test_maxr_slow():
     train = pyunit_utils.random_dataset_real_only(14200, 189, misFrac=0.0, randSeed=1234)
@@ -15,8 +15,10 @@ def test_maxr_slow():
     response = predictors[0]
     predictors.remove(response)
 
+    start = time.time()
     maxrModel = H2OModelSelectionEstimator(mode="maxr", max_predictor_number=10, intercept=True)
     maxrModel.train(x=predictors, y=response, training_frame=train)
+    print("elapsed time: {0}".format(time.time()-start))
     
 if __name__ == "__main__":
     pyunit_utils.standalone_test(test_maxr_slow)
