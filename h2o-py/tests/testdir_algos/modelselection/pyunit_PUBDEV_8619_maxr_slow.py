f5cd9366e5f8999a1ff986755e9c703669ec8468
wendycwong <wendycwong@yahoo.com>
2022-03-15 17:15:51 -0700

    Added test from Joe-g

---
+++ h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+from __future__ import division
+import sys
+sys.path.insert(1, "../../../")
+import h2o
+from tests import pyunit_utils
+from h2o.estimators.model_selection import H2OModelSelectionEstimator
+
+# test to find out why maxr is slow
+def test_maxr_slow():
+    train = h2o.create_frame(rows=142000, cols=189, seed=12345)
+
+    predictors = train.columns
+    response = "C1"
+    predictors.remove(response)
+
+    maxrModel = H2OModelSelectionEstimator(mode="maxr", max_predictor_number=10, intercept=True)
+    maxrModel.train(x=predictors, y=response, training_frame=train)
+    
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(test_maxr_slow)
+else:
+    test_maxr_slow()
