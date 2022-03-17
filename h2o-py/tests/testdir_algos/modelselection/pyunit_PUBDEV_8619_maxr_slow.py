3415cb5b5b210e87163a1f3938861e8a3dd46814
wendycwong <wendycwong@yahoo.com>
2022-03-16 17:13:19 -0700

    Make sure tests are the same

--- h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
+++ h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
@@ -8,17 +8,18 @@ from h2o.estimators.model_selection import H2OModelSelectionEstimator
 import time
 # test to find out why maxr is slow
 def test_maxr_slow():
-    train = pyunit_utils.random_dataset_real_only(14200, 189, misFrac=0.0, randSeed=1234)
-  #  train = h2o.create_frame(rows=142000, cols=189, seed=12345)
-
+    ncol = 189
+    train = h2o.create_frame("testFrame", 14200, ncol, seed=1234)
     predictors = train.columns
-    response = predictors[0]
+    for index in range(ncol):
+        if train.type(index)=='real':
+            response = predictors[index]
+            break
     predictors.remove(response)
-
     start = time.time()
     maxrModel = H2OModelSelectionEstimator(mode="maxr", max_predictor_number=10, intercept=True)
     maxrModel.train(x=predictors, y=response, training_frame=train)
-    print("elapsed time: {0}".format(time.time()-start))
+    print("Time elapsed with nparallelism=1: {0}".format(time.time() - start))
     
 if __name__ == "__main__":
     pyunit_utils.standalone_test(test_maxr_slow)
