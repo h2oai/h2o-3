28738e66261e37fdb044c498c04327056b9999e7
wendycwong <wendycwong@yahoo.com>
2022-03-16 17:54:24 -0700

    access run_time directly

--- h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
+++ h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
@@ -5,7 +5,7 @@ sys.path.insert(1, "../../../")
 import h2o
 from tests import pyunit_utils
 from h2o.estimators.model_selection import H2OModelSelectionEstimator
-import time
+
 # test to find out why maxr is slow
 def test_maxr_slow():
     ncol = 189
@@ -16,10 +16,9 @@ def test_maxr_slow():
             response = predictors[index]
             break
     predictors.remove(response)
-    start = time.time()
     maxrModel = H2OModelSelectionEstimator(mode="maxr", max_predictor_number=10, intercept=True)
     maxrModel.train(x=predictors, y=response, training_frame=train)
-    print("Time elapsed with nparallelism=1: {0}".format(time.time() - start))
+    print("Time elapsed : {0}".format(maxrModel._model_json["output"]["run_time"]))
     
 if __name__ == "__main__":
     pyunit_utils.standalone_test(test_maxr_slow)
