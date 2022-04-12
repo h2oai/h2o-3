e41a1120f225d1c7d3941f3470d8e54e6c97a4ec
Adam Valenta <adam.valenta@h2o.ai>
2022-04-12 19:21:56 +0200

    Improve current checkpointing test to be sure, that we can delete and load model to the cluster and checkpoint works properly

--- h2o-py/tests/testdir_misc/pyunit_checkpointing.py
+++ h2o-py/tests/testdir_misc/pyunit_checkpointing.py
@@ -20,9 +20,29 @@ def checkpointing_test():
     checkpointed_gbm = H2OGradientBoostingEstimator(ntrees=2, checkpoint=gbm.model_id)
     checkpointed_gbm.train(x=["Origin", "Dest"], y="Distance", training_frame=airlines, validation_frame=airlines)
     assert checkpointed_gbm.checkpoint == gbm.model_id
-    
+
+
+def checkpointing_with_delete_test():
+    airlines = h2o.import_file(path=pyunit_utils.locate("smalldata/testng/airlines_train.csv"))
+    gbm = H2OGradientBoostingEstimator(ntrees=1)
+    gbm.train(x=["Origin", "Dest"], y="Distance", training_frame=airlines, validation_frame=airlines)
+
+    path = pyunit_utils.locate("results")
+
+    gbm_path = h2o.save_model(gbm, path=path, force=True)
+    h2o.remove_all([airlines.frame_id])
+    gbm = h2o.load_model(gbm_path)
+
+    checkpointed_gbm = H2OGradientBoostingEstimator(ntrees=2, checkpoint=gbm)
+    checkpointed_gbm.train(x=["Origin", "Dest"], y="Distance", training_frame=airlines, validation_frame=airlines)
+    assert checkpointed_gbm.checkpoint == gbm
+
+    checkpointed_gbm = H2OGradientBoostingEstimator(ntrees=2, checkpoint=gbm.model_id)
+    checkpointed_gbm.train(x=["Origin", "Dest"], y="Distance", training_frame=airlines, validation_frame=airlines)
+    assert checkpointed_gbm.checkpoint == gbm.model_id
+
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(checkpointing_test)
+    pyunit_utils.run_tests([checkpointing_test, checkpointing_with_delete_test])
 else:
     checkpointing_test()
