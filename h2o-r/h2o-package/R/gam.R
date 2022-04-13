82797f7a479e63690c5a69006655b65908d7e258
hannah-tillman <52463461+hannah-tillman@users.noreply.github.com>
2022-04-12 22:18:08 -0400

    PUBDEV-8654: Added documentation on monotone splines for GAM (#6164)
    * PUBDEV-8654: monotone start
    
    * PUBDEV-8654: monotone addition
    
    * PUBDEV-8654: first draft
    
    * PUBDEV-8654: requested changes
    
    * PUBDEV-8564: requested updates / added note
    
    * PUBDEV-8654: small update in references
    
    * PUBDEV-8654: updated schemas / added example
    
    * PUBDEV-8654: requested example updates
    
    * PUBDEV-8654: added bs & spline_orders
    
    * PUBDEV-8654: b-spline update

--- h2o-r/h2o-package/R/gam.R
+++ h2o-r/h2o-package/R/gam.R
@@ -119,12 +119,13 @@
 #' @param max_runtime_secs Maximum allowed runtime in seconds for model training. Use 0 to disable. Defaults to 0.
 #' @param custom_metric_func Reference to custom evaluation function, format: `language:keyName=funcName`
 #' @param num_knots Number of knots for gam predictors
-#' @param spline_orders Order of I-splines used for gam predictors
+#' @param spline_orders Order of I-splines used for gam predictors. If specified, must be the same size as gam_columns.Values for bs=0
+#'        or 1 will be ignored.
 #' @param knot_ids Array storing frame keys of knots.  One for each gam column set specified in gam_columns
 #' @param standardize_tp_gam_cols \code{Logical}. standardize tp (thin plate) predictor columns Defaults to FALSE.
 #' @param scale_tp_penalty_mat \code{Logical}. Scale penalty matrix for tp (thin plate) smoothers as in R Defaults to FALSE.
-#' @param bs Basis function type for each gam predictors, 0 for cr, 1 for thin plate regression with knots, 2 for thin
-#'        plate regression with SVD.  If specified, must be the same size as gam_columns
+#' @param bs Basis function type for each gam predictors, 0 for cr, 1 for thin plate regression with knots, 2 for monotone
+#'        splines.  If specified, must be the same size as gam_columns
 #' @param scale Smoothing parameter for gam predictors.  If specified, must be of the same length as gam_columns
 #' @param keep_gam_cols \code{Logical}. Save keys of model matrix Defaults to FALSE.
 #' @param auc_type Set default multinomial AUC type. Must be one of: "AUTO", "NONE", "MACRO_OVR", "WEIGHTED_OVR", "MACRO_OVO",
