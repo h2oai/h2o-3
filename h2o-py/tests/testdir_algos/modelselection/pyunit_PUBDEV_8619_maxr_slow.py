91bf48abad834237d08808c20d22c17f92e0c2bf
Michal Kurka <michalk@h2o.ai>
2022-04-29 16:26:32 -0400

    from Wendy

--- h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
+++ h2o-py/tests/testdir_algos/modelselection/pyunit_PUBDEV_8619_maxr_slow.py
@@ -1,29 +1,25 @@
 from __future__ import print_function
 from __future__ import division
 import sys
-sys.path.insert(1, "../../../")
+sys.path.insert(1, “../../../“)
 import h2o
 from tests import pyunit_utils
 from h2o.estimators.model_selection import H2OModelSelectionEstimator
-
 # test to find out why maxr is slow
 def test_maxr_slow():
-    ncol = 189
-    train = h2o.create_frame("testFrame", 14200, ncol, seed=1234)
-    predictors = train.columns
-    npred = 4
-    for index in range(ncol):
-        if train.type(index)=='real':
-            response = predictors[index]
-            break
-    predictors.remove(response)
-    maxrModel = H2OModelSelectionEstimator(mode="maxr", max_predictor_number=npred, intercept=True)
-    maxrModel.train(x=predictors, y=response, training_frame=train)
-    print("Time elapsed : {0}".format(maxrModel._model_json["output"]["run_time"]))
-    resultRun = maxrModel.result()
-    assert resultRun.nrow==npred
-    
-if __name__ == "__main__":
-    pyunit_utils.standalone_test(test_maxr_slow)
+  npred = 10
+  hf = h2o.import_file(pyunit_utils.locate("smalldata/model_selection/maxrglm200Cols50KRows.csv"))
+  hf = hf.cbind(hf2)
+  hf = hf.cbind(hf3)
+  target = ‘response’
+  predictors = hf.names
+  predictors.remove(target)
+  maxrModel = H2OModelSelectionEstimator(mode=“maxr”, max_predictor_number=npred, intercept=True)
+  maxrModel.train(x=predictors, y=target, training_frame=hf)
+  print(“Time elapsed : {0}“.format(maxrModel._model_json[“output”][“run_time”]))
+  resultRun = maxrModel.result()
+  assert resultRun.nrow==npred
+if __name__ == “__main__“:
+  pyunit_utils.standalone_test(test_maxr_slow)
 else:
-    test_maxr_slow()
+  test_maxr_slow()
