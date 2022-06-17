c4194cc713b48c3edafbe0d3474c52d69b4b6f00
Syzon Yuliia <syzonyuliia@gmail.com>
2022-06-17 13:48:51 +0200

    Bugfix - fixing accumulating statistics

--- h2o-algos/src/main/java/hex/tree/sdt/binning/FeatureBins.java
+++ h2o-algos/src/main/java/hex/tree/sdt/binning/FeatureBins.java
@@ -30,14 +30,20 @@ public class FeatureBins {
         BinAccumulatedStatistics tmpAccumulatorRight = new BinAccumulatedStatistics();
         int rightIndex;
         for (int leftIndex = 0; leftIndex < _bins.size(); leftIndex++) {
+            // todo - should I include the last bin - the ratio will be all:0 - maybe we should not use the last bin 
+            //  as a candidate for splitting
             tmpAccumulatorLeft.accumulateLeftStatistics(_bins.get(leftIndex)._count0, _bins.get(leftIndex)._count);
             statistics.get(leftIndex).copyLeftValues(tmpAccumulatorLeft);
             statistics.get(leftIndex)._maxBinValue = _bins.get(leftIndex)._max;
             // accumulate from the right (from the end of bins array)
             rightIndex = _bins.size() - leftIndex - 1;
+            // todo - fix - right statistics should not contain actual bin values 
+            //  (it is excuded for the right count as it is included for the left one)
+            // firstly accumulate with old values, then add the actual bin for the future statistics 
+            // as the values of the actual bin are not included in its right statistics
+            statistics.get(rightIndex).copyRightValues(tmpAccumulatorRight);
             tmpAccumulatorRight.accumulateRightStatistics(_bins.get(rightIndex)._count0,
                     _bins.get(rightIndex)._count);
-            statistics.get(rightIndex).copyRightValues(tmpAccumulatorRight);
         }
         return statistics;
     }
