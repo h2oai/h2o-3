79bf4b70f36db0ee224d8b954d2d4032000b56dc
wendycwong <wendycwong@yahoo.com>
2022-07-20 09:25:57 -0700

    make sure null model can predict

--- h2o-py/tests/testdir_algos/glm/pyunit_pubdev_8775_gamma_null_model.py
+++ h2o-py/tests/testdir_algos/glm/pyunit_pubdev_8775_gamma_null_model.py
@@ -6,6 +6,7 @@ from h2o.estimators.glm import H2OGeneralizedLinearEstimator
 
 def test_gamma_null_model():
     training_data = h2o.import_file("http://h2o-public-test-data.s3.amazonaws.com/smalldata/glm_test/gamma_dispersion_factor_9_10kRows.csv")
+    training_data2 = h2o.import_file("http://h2o-public-test-data.s3.amazonaws.com/smalldata/glm_test/gamma_dispersion_factor_9_10kRows.csv")
     Y = 'resp'
     x = ['abs.C1.', 'abs.C2.', 'abs.C3.', 'abs.C4.', 'abs.C5.']
     model = H2OGeneralizedLinearEstimator(family='gamma', lambda_=0, compute_p_values=True, 
@@ -26,8 +27,15 @@ def test_gamma_null_model():
     assert len(coeffs_null_ml) == 1, "Null model from ml coefficient length: {0} shall be 1.".format(len(coeffs_null_ml))
     assert len(coeffs_null_p) == 1, "Null model from pearson coefficient length: {0} shall be " \
                                           "1.".format(len(coeffs_null_p))
-    assert 'Intercept' in coeffs_null_ml.keys(), "Null model from ml should contain Intercept as its only coefficient but did not."
-    assert 'Intercept' in coeffs_null_p.keys(), "Null model from pearson should contain Intercept as its only coefficient but did not."
+    assert 'Intercept' in coeffs_null_ml.keys(), "Null model from ml should contain Intercept as its only coefficient" \
+                                                 " but did not."
+    assert 'Intercept' in coeffs_null_p.keys(), "Null model from pearson should contain Intercept as its only" \
+                                                " coefficient but did not."
+
+    # make sure it can predict with null model
+    pred_ml = model_null_ml.predict(training_data)
+    pred_p = model_null_p.predict(training_data2)
+    pyunit_utils.compare_frames_local(pred_p[0], pred_ml[0], prob=1)
     
 if __name__ == "__main__":
   pyunit_utils.standalone_test(test_gamma_null_model)
