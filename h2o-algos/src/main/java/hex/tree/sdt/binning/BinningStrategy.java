61980194295f0a33da881aa50a890f4b7a88ab53
Syzon Yuliia <syzonyuliia@gmail.com>
2022-07-26 08:01:12 +0200

    Optimisation - one task for each bin

--- h2o-algos/src/main/java/hex/tree/sdt/binning/BinningStrategy.java
+++ h2o-algos/src/main/java/hex/tree/sdt/binning/BinningStrategy.java
@@ -130,26 +130,26 @@ public enum BinningStrategy {
     abstract List<Bin> createFeatureBins(Frame originData, DataFeaturesLimits featuresLimits, int feature);
     public static List<Bin> calculateBinSamplesCount(Frame data, List<Bin> emptyBins,
                                                      int feature, double[][] featuresLimits) {
-//        // run MR task to compute accumulated statistic for bins - one task for each bin
-//        return emptyBins.stream().peek(bin -> {
-//            CountBinSamplesCountMRTask task =
-//                    new CountBinSamplesCountMRTask(feature, bin._min, bin._max, featuresLimits);
-//            task.doAll(data);
-//            bin._count0 = task._count0;
-//            bin._count = task._count;
-//        }).collect(Collectors.toList());
+        // run MR task to compute accumulated statistic for bins - one task for each bin
+        return emptyBins.stream().peek(bin -> {
+            CountBinSamplesCountMRTask task =
+                    new CountBinSamplesCountMRTask(feature, bin._min, bin._max, featuresLimits);
+            task.doAll(data);
+            bin._count0 = task._count0;
+            bin._count = task._count;
+        }).collect(Collectors.toList());
 
 
-        // run MR task to compute accumulated statistic for bins - one task for one feature
-        double[][] binsArray = 
-                emptyBins.stream().map(bin -> new double[]{bin._min, bin._max, 0, 0}).toArray(double[][]::new);
-        CountBinsSamplesCountsMRTask task = new CountBinsSamplesCountsMRTask(feature, featuresLimits, binsArray);
-        task.doAll(data);
-        for(int i = 0; i < binsArray.length; i ++) {
-            emptyBins.get(i)._count = (int) task._bins[i][COUNT];
-            emptyBins.get(i)._count0 = (int) task._bins[i][COUNT_0];
-        }
-        return emptyBins;
+//        // run MR task to compute accumulated statistic for bins - one task for one feature
+//        double[][] binsArray = 
+//                emptyBins.stream().map(bin -> new double[]{bin._min, bin._max, 0, 0}).toArray(double[][]::new);
+//        CountBinsSamplesCountsMRTask task = new CountBinsSamplesCountsMRTask(feature, featuresLimits, binsArray);
+//        task.doAll(data);
+//        for(int i = 0; i < binsArray.length; i ++) {
+//            emptyBins.get(i)._count = (int) task._bins[i][COUNT];
+//            emptyBins.get(i)._count0 = (int) task._bins[i][COUNT_0];
+//        }
+//        return emptyBins;
     }
 }
     
