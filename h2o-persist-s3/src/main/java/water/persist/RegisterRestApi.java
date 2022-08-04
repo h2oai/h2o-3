ef8946a892542be88f6a209015c86f8b7cee572e
Michal Kurka <michalk@h2o.ai>
2022-08-04 13:27:49 -0400

    Add support for removing user-set AWS credentials from the cluster

--- h2o-persist-s3/src/main/java/water/persist/RegisterRestApi.java
+++ h2o-persist-s3/src/main/java/water/persist/RegisterRestApi.java
@@ -1,13 +1,16 @@
 package water.persist;
 
 import water.api.AbstractRegister;
-import water.api.AlgoAbstractRegister;
 import water.api.RestApiContext;
 
 public class RegisterRestApi extends AbstractRegister {
+
     @Override
     public void registerEndPoints(RestApiContext context) {
-        context.registerEndpoint("set_s3_credentials", "POST /3/PersistS3", PersistS3Handler.class, "setS3Credentials", "Set Amazon S3 credentials (Secret Key ID, Secret Access Key)");
+        context.registerEndpoint("set_s3_credentials", "POST /3/PersistS3", PersistS3Handler.class, 
+                "setS3Credentials", "Set Amazon S3 credentials (Secret Key ID, Secret Access Key)");
+        context.registerEndpoint("remove_s3_credentials", "DELETE /3/PersistS3", PersistS3Handler.class,
+                "removeS3Credentials", "Remove store Amazon S3 credentials");
     }
 
     @Override
