6b0a024efe908a8114c11b33c52535438931a6f1
Michal Kurka <michalk@h2o.ai>
2022-08-08 10:45:39 -0400

    Adapt "Persist S3" tests to respect "H2O_" prefix in AWS credentials

--- h2o-py/tests/testdir_persist/pyunit_validate_set_s3_credentials.py
+++ h2o-py/tests/testdir_persist/pyunit_validate_set_s3_credentials.py
@@ -1,32 +1,10 @@
-import h2o
-import os
-
 from h2o.exceptions import H2OValueError
 from h2o.persist import set_s3_credentials
 
 from tests import pyunit_utils
 
-def s3_access_test():
 
-    access_key_id = os.environ['AWS_ACCESS_KEY_ID']
-    secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-    assert access_key_id is not None
-    assert secret_access_key is not None
-    set_s3_credentials(access_key_id, secret_access_key)
-    
-    file = h2o.import_file("s3://test.0xdata.com/h2o-unit-tests/iris.csv")
-    assert file is not None
-
-    access_key_id = 'abcd'
-    secret_access_key = 'abcd'
-    set_s3_credentials(access_key_id, secret_access_key)
-    try: 
-        h2o.import_file("s3://test.0xdata.com/h2o-unit-tests/iris.csv")
-        assert False
-    except Exception as e:
-        assert type(e) is h2o.exceptions.H2OServerError
-        assert e.args[0].find("Error: The AWS Access Key Id you provided does not exist in our records. (Service: Amazon S3; Status Code: 403; Error Code: InvalidAccessKeyId;") != -1
-
+def test_validate_set_s3_credentials_inputs():
     try:
         set_s3_credentials("", "abcd")
         assert False
@@ -57,6 +35,6 @@ def s3_access_test():
 
 
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(s3_access_test)
+    pyunit_utils.standalone_test(test_validate_set_s3_credentials_inputs)
 else:
-    s3_access_test()
+    test_validate_set_s3_credentials_inputs()
