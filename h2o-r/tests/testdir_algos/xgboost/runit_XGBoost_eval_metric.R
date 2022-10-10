6803101be0856c7883cb66247601893ff3abd148
Michal Kurka <michalk@h2o.ai>
2022-10-10 15:25:58 -0400

    XGBoost: Add tests for eval_metric

--- h2o-r/tests/testdir_algos/xgboost/runit_XGBoost_eval_metric.R
+++ h2o-r/tests/testdir_algos/xgboost/runit_XGBoost_eval_metric.R
@@ -3,7 +3,7 @@ source("../../../scripts/h2o-r-test-setup.R")
 
 
 
-test.XGBoost.ecology <- function() {
+test.XGBoost.eval_metric <- function() {
   expect_true(h2o.xgboost.available())
   
   n.trees <- 10
@@ -11,11 +11,11 @@ test.XGBoost.ecology <- function() {
   min_rows <- 10
   learn_rate <- 0.7
   distribution <- "bernoulli"
+  eval_metric <- "error@0.75"
   
   ecology.hex <- h2o.importFile(locate("smalldata/gbm_test/ecology_model.csv"))
   ecology.hex$Angaus <- as.factor(ecology.hex$Angaus)
 
-  #Train H2O XGBoost Model:
   ecology.h2o <- h2o.xgboost(x = 3:13, 
                              y = "Angaus", 
                              training_frame = ecology.hex,
@@ -23,16 +23,18 @@ test.XGBoost.ecology <- function() {
                              max_depth = max_depth,
                              min_rows = min_rows,
                              learn_rate = learn_rate,
-                             distribution = distribution)
-  model.params <- getParms(ecology.h2o)
-  print(model.params)
-  
-  expect_equal(model.params$ntrees, n.trees)
-  expect_equal(model.params$max_depth, max_depth)
-  expect_equal(model.params$learn_rate, learn_rate)
-  expect_equal(model.params$min_rows, min_rows)
-  expect_equal(model.params$distribution, distribution)
+                             distribution = distribution,
+                             eval_metric = eval_metric)
+  print(ecology.h2o)
+  predicted <- h2o.predict(ecology.h2o, ecology.hex)
+
+  # Evaluation metric is in scoring history as "custom metric"
+  expect_true("training_custom" %in% colnames(h2o.scoreHistory(ecology.h2o)))
+
+  # Extract eval metric from scoring history and compare with calculated value
+  act_error <- tail(h2o.scoreHistory(ecology.h2o)$training_custom, 1)
+  exp_error <- sum((predicted$p1 >= 0.75) != ecology.hex$Angaus) / nrow(predicted)
+  expect_equal(act_error, exp_error)
 }
 
-doTest("XGBoost: Ecology Data", test.XGBoost.ecology)
-
+doTest("XGBoost: Evaluation Metric", test.XGBoost.eval_metric)
