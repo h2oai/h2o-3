13bfac93b9655485ca8c2f207f63380c364b6c8f
syzonyuliia <yuliia.syzon@h2o.ai>
2022-11-17 15:22:38 +0100

    PUBDEV-8843: Fixing R test due to PR comments

--- h2o-r/tests/testdir_algos/glm/runit_PUBDEV_8843_glm_lambda_not_null_regPath.R
+++ h2o-r/tests/testdir_algos/glm/runit_PUBDEV_8843_glm_lambda_not_null_regPath.R
@@ -1,8 +1,8 @@
-library(glmnet)
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../../scripts/h2o-r-test-setup.R")
+library(glmnet)
 
-# Itest that with regularization on, the p-values are computed
+# Test that with regularization on, the p-values are computed
 test.glm_reg_path <- function() {
     d <-  h2o.importFile(path = locate("smalldata/logreg/prostate.csv"))
     alphaArray <- c(0.1,0.5,0.9)
