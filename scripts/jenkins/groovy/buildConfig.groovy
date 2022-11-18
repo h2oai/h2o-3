a7170d2135c52119311f67080d79da755ff06997
Tomáš Frýda <tomas.fryda@h2o.ai>
2022-11-18 08:34:58 +0100

    Skip explain in the initial tests as it takes too much time to complete

--- scripts/jenkins/groovy/buildConfig.groovy
+++ scripts/jenkins/groovy/buildConfig.groovy
@@ -307,7 +307,8 @@ class BuildConfig {
       def change = changeEntry.key
       changeType != "D" && // was not deleted
               change.startsWith('h2o-py/') && change.contains("pyunit_") &&
-              change.lastIndexOf("pyunit_") > change.lastIndexOf("/") // Allow to run "pyunit_*" files inside of "pyunit_*" directory but do not allow e.g. utilsPY.py to run
+              change.lastIndexOf("pyunit_") > change.lastIndexOf("/") && // Allow to run "pyunit_*" files inside of "pyunit_*" directory but do not allow e.g. utilsPY.py to run
+              !change.contains("pyunit_explain") // takes too much time to run
     }.collect {
       it.key.replaceFirst(".*pyunit_", "pyunit_") // Extract only filename from path
     }
