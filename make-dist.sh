accfc2ba035dc0cce9455902d91a85e368623a54
Marek Novotny <marek.novotny@h2o.ai>
2023-01-20 15:54:09 +0100

    [PUBDEV-8926] Register cloud extensions for release

--- make-dist.sh
+++ make-dist.sh
@@ -165,6 +165,7 @@ done
 
 cp h2o-py/build/dist/*whl target/Python
 cp h2o-py/build/client/dist/*whl target/Python
+cp h2o-py-cloud-extensions/build/dist/*whl target/Python
 
 # Add Java bindings Jar to target.
 mkdir -p target/bindings/java
