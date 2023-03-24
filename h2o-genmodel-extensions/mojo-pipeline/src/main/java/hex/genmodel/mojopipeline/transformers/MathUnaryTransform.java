7d9d1b67cc82c6c22c74a6c6608753f9c78b7b42
Marek Novotny <marek.novotny@h2o.ai>
2023-03-24 23:06:14 +0100

    [PUBDEV-8793] String operations

--- h2o-genmodel-extensions/mojo-pipeline/src/main/java/hex/genmodel/mojopipeline/transformers/MathUnaryTransform.java
+++ h2o-genmodel-extensions/mojo-pipeline/src/main/java/hex/genmodel/mojopipeline/transformers/MathUnaryTransform.java
@@ -14,12 +14,10 @@ import java.util.Map;
 public class MathUnaryTransform extends MojoTransform {
 
     MathUnaryFunction _function;
-    Map<String, Object> _params;
 
-    MathUnaryTransform(int[] iindices, int[] oindices, MathUnaryFunction function, Map<String, Object> params) {
+    MathUnaryTransform(int[] iindices, int[] oindices, MathUnaryFunction function) {
         super(iindices, oindices);
         _function = function;
-        _params = params;
     }
 
     @Override
@@ -27,12 +25,13 @@ public class MathUnaryTransform extends MojoTransform {
         double[] a = (double[]) frame.getColumnData(iindices[0]);
         double[] o = (double[]) frame.getColumnData(oindices[0]);
         for (int i = 0, nrows = frame.getNrows(); i < nrows; i++) {
-            o[i] = _function.call(a[i], _params);
+            o[i] = _function.call(a[i]);
         }
     }
 
    interface MathUnaryFunction {
-        double call(double value, Map<String, Object> params);
+        void initialize(Map<String, Object> params);
+        double call(double value);
     }
 
     public static class Factory implements MojoTransformBuilderFactory {
@@ -40,170 +39,247 @@ public class MathUnaryTransform extends MojoTransform {
         private static final HashMap<String,MathUnaryFunction> _supportedFunctions = new HashMap<String,MathUnaryFunction>() {{
             put("abs", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.abs(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.abs(value); }
             });
             put("acos", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.acos(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.acos(value); }
             });
             put("acosh", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return FastMath.acosh(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return FastMath.acosh(value); }
             });
             put("asin", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.asin(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.asin(value); }
             });
             put("asinh", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return FastMath.asinh(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return FastMath.asinh(value); }
             });
             put("atan", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.atan(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.atan(value); }
             });
             put("atanh", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return FastMath.atanh(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return FastMath.atanh(value); }
             });
             put("ceiling", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.ceil(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.ceil(value); }
             });
             put("cos", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.cos(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.cos(value); }
             });
             put("cosh", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.cosh(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.cosh(value); }
             });
             put("cospi", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.cos(Math.PI * value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.cos(Math.PI * value); }
             });
             put("digamma", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Double.isNaN(value) ? Double.NaN : Gamma.digamma(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Double.isNaN(value) ? Double.NaN : Gamma.digamma(value); }
             });
             put("exp", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.exp(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.exp(value); }
             });
             put("expm1", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.expm1(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.expm1(value); }
             });
             put("floor", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.floor(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.floor(value); }
             });
             put("gamma", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Gamma.gamma(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Gamma.gamma(value); }
             });
             put("lgamma", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Gamma.logGamma(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Gamma.logGamma(value); }
             });
             put("log", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.log(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.log(value); }
             });
             put("log1p", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.log1p(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.log1p(value); }
             });
             put("log2", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.log(value) / Math.log(2); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.log(value) / Math.log(2); }
             });
             put("log10", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.log10(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.log10(value); }
             });
             put("none", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return value; }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return value; }
             });
             put("not", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Double.isNaN(value) ? Double.NaN : value == 0 ? 1 : 0; }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Double.isNaN(value) ? Double.NaN : value == 0 ? 1 : 0; }
             });
             put("sign", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.signum(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.signum(value); }
             });
             put("sin", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.sin(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.sin(value); }
             });
             put("sinh", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.sinh(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.sinh(value); }
             });
             put("sinpi", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.sin(Math.PI * value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.sin(Math.PI * value); }
             });
             put("sqrt", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.sqrt(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.sqrt(value); }
             });
             put("tan", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.tan(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.tan(value); }
             });
             put("tanh", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.tanh(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.tanh(value); }
             });
             put("tanpi", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Math.tan(Math.PI * value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Math.tan(Math.PI * value); }
             });
             put("trigamma", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return Double.isNaN(value) ? Double.NaN : Gamma.trigamma(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return Double.isNaN(value) ? Double.NaN : Gamma.trigamma(value); }
             });
             put("trunc", new MathUnaryFunction() {
                 @Override
-                public double call(double value, Map<String, Object> params) { return value >= 0 ? Math.floor(value) : Math.ceil(value); }
+                public void initialize(Map<String, Object> params) {}
+                @Override
+                public double call(double value) { return value >= 0 ? Math.floor(value) : Math.ceil(value); }
             });
             put("round", new MathUnaryFunction() {
+                double _digits = Double.NaN;
+                double _powerOf10 = Double.NaN;
+                
                 @Override
-                public double call(double x, Map<String, Object> params) {
+                public void initialize(Map<String, Object> params) {
                     Object digitsObj = params.get("rightArg");
                     if (digitsObj == null) {
                         throw new IllegalArgumentException("The 'digits' param is not passed to 'round' function!");
                     }
-                    double digits = (Double)digitsObj;
+                    _digits = (Double)digitsObj;
+                    if ((int) _digits != _digits) _digits = Math.round(_digits);
+                    _powerOf10 = (int) Math.pow(10, (int) _digits);
+                }
+                @Override
+                public double call(double x) {
                     if (Double.isNaN(x)) return x;
                     double sgn = x < 0 ? -1 : 1;
                     x = Math.abs(x);
-                    if ((int) digits != digits) digits = Math.round(digits);
-                    double power_of_10 = (int) Math.pow(10, (int) digits);
-                    return sgn * (digits == 0
+                    return sgn * (_digits == 0
                             // go to the even digit
                             ? (x % 1 > 0.5 || (x % 1 == 0.5 && !(Math.floor(x) % 2 == 0)))
                             ? Math.ceil(x)
                             : Math.floor(x)
-                            : Math.floor(x * power_of_10 + 0.5) / power_of_10);
+                            : Math.floor(x * _powerOf10 + 0.5) / _powerOf10);
                 }
             });
             put("signif", new MathUnaryFunction() {
+                double _digits = Double.NaN;
+
                 @Override
-                public double call(double x, Map<String, Object> params) {
+                public void initialize(Map<String, Object> params) {
                     Object digitsObj = params.get("rightArg");
                     if (digitsObj == null) {
                         throw new IllegalArgumentException("The 'digits' param is not passed to 'signif' function!");
                     }
-                    double digits = (Double)digitsObj;
+                    _digits = (Double)digitsObj;
+                    if (_digits < 1) _digits = 1; //mimic R's base::signif
+                    if ((int) _digits != _digits) _digits = Math.round(_digits);
+                }
+                @Override
+                public double call(double x) {
                     if (Double.isNaN(x)) return x;
-                    if (digits < 1) digits = 1; //mimic R's base::signif
-                    if ((int) digits != digits) digits = Math.round(digits);
                     java.math.BigDecimal bd = new java.math.BigDecimal(x);
-                    bd = bd.round(new java.math.MathContext((int) digits, java.math.RoundingMode.HALF_EVEN));
+                    bd = bd.round(new java.math.MathContext((int) _digits, java.math.RoundingMode.HALF_EVEN));
                     return bd.doubleValue();
                 }
             });
@@ -236,7 +312,8 @@ public class MathUnaryTransform extends MojoTransform {
                                            ReaderBackend backend) {
             final String functionName = (String) params.get("function");
             final MathUnaryFunction function = Factory.getFunction(functionName);
-            return new MathUnaryTransform(iindcies, oindices, function, params);
+            function.initialize(params);
+            return new MathUnaryTransform(iindcies, oindices, function);
         }
     }
 }
