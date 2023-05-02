062cd14bdb27f53492c60bdb6a7cae807e8bca19
syzonyuliia <yuliia.syzon@h2o.ai>
2023-05-02 15:41:57 +0200

    HEXDEV-785: generate files with gradlew build

--- h2o-r/h2o-package/R/glm.R
+++ h2o-r/h2o-package/R/glm.R
@@ -93,7 +93,7 @@
 #'        Defaults to family_default.
 #' @param rand_link Link function array for random component in HGLM. Must be one of: "[identity]", "[family_default]".
 #' @param startval double array to initialize fixed and random coefficients for HGLM, coefficients for GLM.
-#' @param calc_like \code{Logical}. if true, will return likelihood function value. Defaults to FALSE.
+#' @param calc_like \code{Logical}. if true, will return likelihood function value for HGLM. Defaults to FALSE.
 #' @param HGLM \code{Logical}. If set to true, will return HGLM model.  Otherwise, normal GLM model will be returned Defaults
 #'        to FALSE.
 #' @param prior Prior probability for y==1. To be used only for logistic regression iff the data has been sampled and the mean
