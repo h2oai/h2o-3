512124a76271da3a8cc6a95eaf7d4136f39da202
wendycwong <wendycwong@yahoo.com>
2023-05-02 16:43:21 -0700

    add Java and R tests.

--- h2o-r/tests/testdir_algos/glm/runit_PUBDEV_8947_GLM_likelihoods_aic.R
+++ h2o-r/tests/testdir_algos/glm/runit_PUBDEV_8947_GLM_likelihoods_aic.R
@@ -1,6 +1,7 @@
 setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
 source("../../../scripts/h2o-r-test-setup.R")
 
+# checking to see that R clients can access aic and likelihood functions
 test.glm.aic.likelihood <- function() {
   # checking for Gaussian
   h2o.data = h2o.uploadFile(locate("smalldata/prostate/prostate_complete.csv.zip"), destination_frame="h2o.data")    
@@ -9,69 +10,26 @@ test.glm.aic.likelihood <- function() {
   myX = c("ID","AGE","RACE","CAPSULE","DCAPS","PSA","VOL","DPROS")
   R.formula = (R.data[,"GLEASON"]~.) 
   model.h2o.gaussian.identity <- h2o.glm(calc_like=TRUE, x=myX, y=myY, training_frame=h2o.data, family="gaussian", link="identity",alpha=0.5, lambda=0, nfolds=0)
-  model.R.gaussian.identity <- glm(formula=R.formula, data=R.data[,2:9], family=gaussian(link=identity), na.action=na.omit)
+  # R toolbox has a bug in calculating AIC and loglikelihood
+  # hand derived correct answer is from R calculation and using the correct formula
+  nobs <- 192512
+  dev <- 157465
+  rRank <- 9
+  loglikeR <- -0.5*dev-0.5*nobs*log(2*pi)
+  aicR <- -2*loglikeR+2*rRank
   perf <- h2o.performance(model.h2o.gaussian.identity)
   print("GLM Gaussian")
   print("H2O AIC")
   print(h2o.aic(perf))
   print("H2O log likelihood")
-  print(h2o.loglikelihood(perf)) # Yuliia: please implement this
+  print(h2o.loglikelihood(perf))
   print("R AIC")
-  print(AIC(model.R.gaussian.identity))
+  print( aicR)
   print("R loglikelihood")
-  print(logLik(model.R.gaussian.identity))
- # browser()
+  print(loglikeR)
+  expect_true(abs(h2o.aic(perf)-aicR) < 1e-1)
+  expect_true(abs(h2o.loglikelihood(perf)-loglikeR)<1e-1)
   
-  # check for Gamma
-  myY = "DPROS"
-  myX = c("ID","AGE","RACE","CAPSULE","DCAPS","PSA","VOL","GLEASON")
-  R.formula = (R.data[,"DPROS"]~.) 
-  model.h2o.gamma.inverse <- h2o.glm(calc_like=TRUE, x=myX, y=myY, training_frame=h2o.data, family="gamma", link="inverse",alpha=0.5, lambda=0, nfolds=0)
-  model.R.gamma.inverse <- glm(formula=R.formula, data=R.data[,c(1:5,7:9)], family=Gamma(link=inverse), na.action=na.omit)
-  perf <- h2o.performance(model.h2o.gamma.inverse)
-  print("GLM Gamma")
-  print("H2O AIC")
-  print(h2o.aic(perf))
-  print("H2O log likelihood")
-  print(h2o.loglikelihood(perf)) # Yuliia: please implement this
-  print("R AIC")
-  print(AIC(model.R.gamma.inverse))
-  print("R loglikelihood")
-  print(logLik(model.R.gamma.inverse))
-  
-  # checking for binomial
-  myY <- "CAPSULE"
-  myX <- c("AGE","RACE","DCAPS","PSA","VOL","DPROS","GLEASON")
-  R.formula <- (R.data[,"CAPSULE"]~.)
-  model.h2o.binomial.logit <- h2o.glm(calc_like=TRUE, x=myX, y=myY, training_frame=h2o.data, family="binomial", link="logit",alpha=0.5, lambda=0, nfolds=0)
-  model.R.binomial.logit <- glm(formula=R.formula, data=R.data[,4:10], family=binomial(link=logit), na.action=na.omit)
-  perf <- h2o.performance(model.h2o.binomial.logit)
-  print("GLM binomial")
-  print("H2O AIC")
-  print(h2o.aic(perf))
-  print("H2O log likelihood")
-  print(h2o.loglikelihood(perf)) # Yuliia: please implement this
-  print("R AIC")
-  print(AIC(model.R.binomial.logit))
-  print("R loglikelihood")
-  print(logLik(model.R.binomial.logit))
-  
-  # checking for Poisson
-  myY = "GLEASON"
-  myX = c("ID","AGE","RACE","CAPSULE","DCAPS","PSA","VOL","DPROS")
-  R.formula = (R.data[,"GLEASON"]~.) 
-  model.h2o.poisson.log <- h2o.glm(calc_like=TRUE, x=myX, y=myY, training_frame=h2o.data, family="poisson", link="log",alpha=0.5, lambda=0, nfolds=0)
-  model.R.poisson.log <- glm(formula=R.formula, data=R.data[,2:9], family=poisson(link=log), na.action=na.omit)
-  perf <- h2o.performance(model.h2o.poisson.log)
-  print("GLM Poisson")
-  print("H2O AIC")
-  print(h2o.aic(perf))
-  print("H2O log likelihood")
-  print(h2o.loglikelihood(perf)) # Yuliia: please implement this
-  print("R AIC")
-  print(AIC(model.R.poisson.log))
-  print("R loglikelihood")
-  print(logLik(model.R.poisson.log))
-}
+ }
 
 doTest("Testing AIC/likelihood for GLM", test.glm.aic.likelihood)
