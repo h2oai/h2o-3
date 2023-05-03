eb06fd5a0b02c5b5cd09eb1ec1fb9ca386fd9f23
syzonyuliia <yuliia.syzon@h2o.ai>
2023-05-03 18:05:33 +0200

    PUBDEV-8691: add comments for features limits

--- h2o-algos/src/main/java/hex/tree/dt/FeatureLimits.java
+++ h2o-algos/src/main/java/hex/tree/dt/FeatureLimits.java
@@ -4,9 +4,8 @@ package hex.tree.dt;
  * Limits for one feature.
  */
 public class FeatureLimits {
-    public double _min;
-    public double _max;
-
+    public double _min; // min value is not included in the range (excluded)
+    public double _max; // max value is included in the range
 
     private double EPSILON = 1e-3; // epsilon for approximate comparing of limits, used in tests only
     public FeatureLimits(final double min, final double max) {
