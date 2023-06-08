94e5302368144761a4d2c860d0bce1146ce6f396
Marek Novotny <marek.novotny@h2o.ai>
2023-06-08 16:48:57 +0200

    Revert with DAI credentials

--- scripts/jenkins/groovy/insideDocker.groovy
+++ scripts/jenkins/groovy/insideDocker.groovy
@@ -22,24 +22,22 @@ def call(customEnv, image, registry, buildConfig, timeoutValue, timeoutUnit, cus
     timeout(time: timeoutValue, unit: timeoutUnit) {
       docker.withRegistry("https://${registry}") {
         withCredentials([file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'), [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: "${awsCredsPrefix}AWS_ACCESS_KEY_ID", credentialsId: 'AWS S3 Credentials', secretKeyVariable: "${awsCredsPrefix}AWS_SECRET_ACCESS_KEY"]]) {
-          withCredentials([string(credentialsId: 'DRIVERLESS_AI_LICENSE_KEY', variable: 'DRIVERLESS_AI_LICENSE_KEY')]) {
-            dockerGroupIdAdd = ""
-            if (addToDockerGroup) {
-              dockerGroupName = "docker"
-              dockerGroupId = sh(script: "awk -F: '/$dockerGroupName/ {print \$3}' /etc/group", returnStdout: true).trim()
-              if (dockerGroupId == "") {
-                error "cannot find gid of $dockerGroupName"
-              } else {
-                dockerGroupIdAdd = "--group-add ${dockerGroupId}"
-              }
+          dockerGroupIdAdd = ""
+          if(addToDockerGroup) {
+            dockerGroupName = "docker"
+            dockerGroupId = sh(script: "awk -F: '/$dockerGroupName/ {print \$3}' /etc/group", returnStdout: true).trim()
+            if (dockerGroupId == "") {
+              error "cannot find gid of $dockerGroupName"
+            } else{
+              dockerGroupIdAdd = "--group-add ${dockerGroupId}"
             }
-            docker.image(image).inside("--init ${dockerGroupIdAdd} -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID} -e ${awsCredsPrefix}AWS_SECRET_ACCESS_KEY=${awsCredsPrefix}\${AWS_SECRET_ACCESS_KEY} -e DRIVERLESS_AI_LICENSE_KEY=${DRIVERLESS_AI_LICENSE_KEY} -v /home/0xdiag:/home/0xdiag -v /home/jenkins/repos:/home/jenkins/repos ${customArgs}") {
-              sh """
+          }
+          docker.image(image).inside("--init ${dockerGroupIdAdd} -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID} -e ${awsCredsPrefix}AWS_SECRET_ACCESS_KEY=${awsCredsPrefix}\${AWS_SECRET_ACCESS_KEY} -v /home/0xdiag:/home/0xdiag -v /home/jenkins/repos:/home/jenkins/repos ${customArgs}") {
+            sh """
               id
               printenv | sort
             """
-              block()
-            }
+            block()
           }
         }
       }
