07522b241d07d442949a15a5afa96c5c123b053a
Marek Novotny <marek.novotny@h2o.ai>
2023-08-14 15:36:56 +0200

    [GH-15687] Extract string comparators from Duke library

---
+++ h2o-genmodel/src/main/java/water/util/comparison/string/StringComparator.java
@@ -0,0 +1,24 @@
+package water.util.comparison.string;
+
+/*
+Copyright 2023 Lars Marius Garshol
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Original code: https://github.com/larsga/Duke/blob/duke-1.2/src/main/java/no/priv/garshol/duke/Comparator.java
+public interface StringComparator {
+    public boolean isTokenized();
+
+    public double compare(String v1, String v2);
+}
