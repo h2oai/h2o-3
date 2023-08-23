2742fbdaa90ab0ac819422b835028a970b321bc1
Veronika Maurerova <veronika.maurerova@h2o.ai>
2023-08-23 11:23:54 +0200

    rebase master

--- h2o-genmodel/src/main/java/hex/genmodel/GenModel.java
+++ h2o-genmodel/src/main/java/hex/genmodel/GenModel.java
@@ -150,7 +150,7 @@ public abstract class GenModel implements IGenModel, IGeneratedModel, Serializab
         outputNames = new String[]{"lp"};
         break;
       case BinomialUplift:
-        outputNames = new String[]{"uplift_predict", "p_y1_ct1", "p_y1_ct0"};
+        outputNames = new String[]{"uplift_predict", "p_y1_with_treatment", "p_y1_without_treatment"};
         break;
       default:
         throw new UnsupportedOperationException("Getting output column names for model category '" + 
