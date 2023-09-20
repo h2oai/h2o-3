7f35991d257ae27f30e5b83487148744729cae50
Veronika Maurerova <veronika.maurerova@h2o.ai>
2023-09-20 15:23:10 +0200

    Move custom threshold logic from make metrics to another PR

--- h2o-core/src/main/java/water/api/ModelMetricsHandler.java
+++ h2o-core/src/main/java/water/api/ModelMetricsHandler.java
@@ -174,7 +174,6 @@ class ModelMetricsHandler extends Handler {
     @API(help = "Set number of bins to calculate AUUC. Must be -1 or higher than 0. Default is -1 which means 1000 (optional, only for uplift binomial classification).", json=false, direction = API.Direction.INPUT)
     public int auuc_nbins;
     
-
     @API(help = "Set custom thresholds to calculate AUUC (optional, only for uplift binomial classification). ", json=false, direction = API.Direction.INPUT)
     public double[] custom_auuc_thresholds;
 
