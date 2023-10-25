537e78554addc189a119bec139873361194b9081
wendycwong <wendy.wong@h2o.ai>
2023-10-24 17:11:38 -0700

    removed formatting change.

--- h2o-py/h2o/utils/shared_utils.py
+++ h2o-py/h2o/utils/shared_utils.py
@@ -126,6 +126,7 @@ def is_module_available(mod):
     import importlib.util
     return importlib.util.find_spec(mod) is not None
 
+
 def can_use_pandas():
     return is_module_available('pandas')
 
