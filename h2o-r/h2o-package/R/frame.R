e6518e85144bbbd78831d24ae5e64eaef4afa434
sebhrusen <sebastien.poirier@h2o.ai>
2023-10-25 23:46:49 +0200

    remove link to deleted blog post that breaks CRAN checks

--- h2o-r/h2o-package/R/frame.R
+++ h2o-r/h2o-package/R/frame.R
@@ -4160,7 +4160,6 @@ as.h2o.H2OFrame <- function(x, destination_frame="", ...) {
 #' @details 
 #' Method \code{as.h2o.data.frame} will use \code{\link[data.table]{fwrite}} if data.table package is installed in required version.
 #' @seealso \code{\link{use.package}}
-#' @references \url{https://h2o.ai/blog/fast-csv-writing-for-r/}
 #' @export
 as.h2o.data.frame <- function(x, destination_frame="", use_datatable=TRUE, ...) {
   if( destination_frame=="" ) {
