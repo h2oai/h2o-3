69e37106bb33585fa0599f5974857c3da3b782d1
wendycwong <wendy.wong@h2o.ai>
2023-12-05 08:59:32 -0800

    capture correct warning message

--- h2o-py/tests/testdir_misc/pyunit_gh_15861_no_datatable.py
+++ h2o-py/tests/testdir_misc/pyunit_gh_15861_no_datatable.py
@@ -10,8 +10,9 @@ def test_datatable_without_datatable():
         with pyunit_utils.catch_warnings() as ws:
             h2oFrame = h2o.import_file(pyunit_utils.locate("bigdata/laptop/jira/PUBDEV_5266_merge_with_string_columns/PUBDEV_5266_f1.csv"))
             new_frame = h2oFrame.as_data_frame() 
-            assert "converting H2O frame to pandas dataframe using single-thread.  For faster conversion using" \
-                   " multi-thread, install datatable, or polars and pyarrow." in str(ws[0].message)
+            assert "converting H2O frame to pandas dataframe using single-thread.  For faster conversion using"
+            " multi-thread, install datatable (for Python 3.9 or lower), or polars and pyarrow "
+            "(for Python 3.10 or above)" in str(ws[0].message)
 
 if __name__ == "__main__":
     pyunit_utils.standalone_test(test_datatable_without_datatable)
