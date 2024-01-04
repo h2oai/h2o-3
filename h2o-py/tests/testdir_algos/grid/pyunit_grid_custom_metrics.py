e2ae4146c566e967e54dd8fb75e6bdcebb868765
Tomáš Frýda <tomas.fryda@h2o.ai>
2024-01-04 11:09:59 +0100

    Add test for custom_increasing

--- h2o-py/tests/testdir_algos/grid/pyunit_grid_custom_metrics.py
+++ h2o-py/tests/testdir_algos/grid/pyunit_grid_custom_metrics.py
@@ -6,7 +6,7 @@ from tests import pyunit_utils
 from collections import OrderedDict
 from h2o.grid.grid_search import H2OGridSearch
 from h2o.estimators.gbm import H2OGradientBoostingEstimator
-from tests.pyunit_utils import CustomMaeFunc, CustomRmseRegressionFunc
+from tests.pyunit_utils import CustomMaeFunc, CustomRmseRegressionFunc, CustomNegativeRmseRegressionFunc
 
 
 def custom_mae_mm():
@@ -15,6 +15,10 @@ def custom_mae_mm():
 
 def custom_rmse_mm():
     return h2o.upload_custom_metric(CustomRmseRegressionFunc, func_name="rmse", func_file="mm_rmse.py")
+
+
+def custom_nrmse_mm():
+    return h2o.upload_custom_metric(CustomNegativeRmseRegressionFunc, func_name="nrmse", func_file="mm_nrmse.py")
 
 
 def grid_custom_metric():
@@ -38,6 +42,31 @@ def grid_custom_metric():
 
     # Should be ok - just one definition of custom metric
     print(gs.get_grid(sort_by="custom"))
+
+
+def grid_custom_increasing_metric():
+    train = h2o.import_file(path=pyunit_utils.locate("smalldata/iris/iris_wheader.csv"))
+    # Run GBM Grid Search using Cross Validation with parallelization enabled
+    ntrees_opts = [1, 3, 5, 10]
+    hyper_parameters = OrderedDict()
+    hyper_parameters["ntrees"] = ntrees_opts
+    hyper_parameters["custom_metric_func"] = custom_nrmse_mm()
+    hyper_parameters["stopping_metric"] = "custom_increasing"
+    print("GBM grid with the following hyper_parameters:", hyper_parameters)
+
+    gs = H2OGridSearch(H2OGradientBoostingEstimator, hyper_params=hyper_parameters,
+                       parallelism=1)
+    gs.train(y=3, training_frame=train, nfolds=3)
+
+    assert len(gs.models) == 4
+    print(gs.get_grid(sort_by="rmse"))
+
+    print(gs.get_grid(sort_by="mae"))
+
+    # Should be ok - just one definition of custom metric
+    # Note that grid search is not smart about metrics (e.g. aucpr will be interpreted as loss) so it's needed to 
+    # specify decreasing=True
+    print(gs.get_grid(sort_by="custom_increasing", decreasing=True))
 
 
 def grid_custom_metrics():
@@ -95,6 +124,6 @@ def grid_custom_metrics_parallel():
 
 
 if __name__ == "__main__":
-    pyunit_utils.run_tests([grid_custom_metrics, grid_custom_metric, grid_custom_metrics_parallel])
+    pyunit_utils.run_tests([grid_custom_increasing_metric, grid_custom_metrics, grid_custom_metric, grid_custom_metrics_parallel])
 else:
     grid_custom_metric()
