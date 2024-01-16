ee38e5fb340b6c58b86b59c1d6996d527230174a
Tomáš Frýda <tomas.fryda@h2o.ai>
2024-01-16 18:23:17 +0100

    Fix runit_pubdev_8683_glm_gamma_dispersion_factor_large.R

--- h2o-r/tests/testdir_algos/glm/runit_pubdev_8683_glm_gamma_dispersion_factor_large.R
+++ h2o-r/tests/testdir_algos/glm/runit_pubdev_8683_glm_gamma_dispersion_factor_large.R
@@ -82,7 +82,7 @@ compareH2ORGLM <-
         family = "gamma",
         lambda = 0,
         nfolds = 0,
-        dispersion_factor_method = "ml",
+        dispersion_parameter_method = "ml",
         compute_p_values = TRUE
       )
     print("Comparing H2O and R GLM model coefficients....")
