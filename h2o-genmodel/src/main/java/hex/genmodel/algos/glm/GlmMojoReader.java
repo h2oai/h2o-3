76e62f612095499e4fa0e558c33f2bf745ad3c93
syzonyuliia <yuliia.syzon@h2o.ai>
2024-02-01 17:46:00 +0100

    GH-15809: fix value

--- h2o-genmodel/src/main/java/hex/genmodel/algos/glm/GlmMojoReader.java
+++ h2o-genmodel/src/main/java/hex/genmodel/algos/glm/GlmMojoReader.java
@@ -29,7 +29,7 @@ public class GlmMojoReader extends ModelMojoReader<GlmMojoModelBase> {
 
     _model._beta = readkv("beta");
     _model._family = readkv("family");
-    _model._dispersion_estimated = readkv("dispersion_estimated", 1);
+    _model._dispersion_estimated = readkv("dispersion_estimated", 0.0);
 
     if (_model instanceof GlmMojoModel) {
       GlmMojoModel m = (GlmMojoModel) _model;
