b71f79d0d9786d8c38479070b11a23cc2ad21f72
Adam Valenta <adam.valenta@h2o.ai>
2024-02-22 17:13:07 +0100

    test 2

--- scripts/jenkins/groovy/defineTestStages.groovy
+++ scripts/jenkins/groovy/defineTestStages.groovy
@@ -60,6 +60,10 @@ def call(final pipelineContext) {
     [
       stageName: 'Java 8 Smoke', target: 'test-junit-smoke-jenkins', javaVersion: 8, timeoutValue: 20,
       component: pipelineContext.getBuildConfig().COMPONENT_JAVA
+    ],
+    [
+      stageName: 'LOGGER initialized properly', target: 'test-logger-initialize-properly', javaVersion: 8, timeoutValue: 20,
+      component: pipelineContext.getBuildConfig().COMPONENT_JAVA
     ]
   ]
 
