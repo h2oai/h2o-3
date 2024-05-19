24eb46fa47bc1d3b7b8643642c6bb7140db630af
Paul <53956863+hutch3232@users.noreply.github.com>
2024-05-19 11:19:56 -0500

    add failing tests for `h2o.ifelse`

--- h2o-r/tests/testdir_misc/runit_ifelse.R
+++ h2o-r/tests/testdir_misc/runit_ifelse.R
@@ -11,6 +11,18 @@ test.ifelse <- function() {
   setosa.hex <- ifelse(iris.hex$Species == "setosa", "N", "Y")
 
   expect_equal(as.data.frame(setosa.hex), data.frame(C1 = setosa))
+
+  Log.info("Test case for all TRUE condition with categorical column")
+  df <- as.h2o(data.frame(a = c("a", "a", "a"), stringsAsFactors = TRUE))
+  result <- h2o.ifelse(df$a == "a", df$a, "d")
+  expected_result <- data.frame(C1 = c("a", "a", "a"), stringsAsFactors = TRUE)
+  expect_equal(as.data.frame(result), expected_result)
+  
+  Log.info("Test case for all FALSE condition with categorical column")
+  df <- as.h2o(data.frame(a = c("a", "b", "c"), stringsAsFactors = TRUE))
+  result <- h2o.ifelse(df$a == "d", "d", df$a)
+  expected_result <- data.frame(C1 = c("a", "b", "c"), stringsAsFactors = TRUE)
+  expect_equal(as.data.frame(result), expected_result)
 }
 
 doTest("R and H2O ifelse Function", test.ifelse)
