6b97ffa896df7feaabf42371d74c57311a9f4ec3
wendycwong <wendy@h2o.ai>
2024-05-29 12:42:25 -0700

    Update h2o-py/tests/pyunit_utils/utils_parser_tests.py
    Co-authored-by: Veronika Maurerová <maurever@users.noreply.github.com>

--- h2o-py/tests/pyunit_utils/utils_parser_tests.py
+++ h2o-py/tests/pyunit_utils/utils_parser_tests.py
@@ -5,41 +5,41 @@ import pandas as pd
 
 def test_frame_conversion(dataset, original_pandas_frame, module):
     # convert frame using datatable or polar/pyarrow
-    h2oFrame = h2o.import_file(pyunit_utils.locate(dataset))
-    test_frames_conversion(h2oFrame, original_pandas_frame, module)
+    h2oframe = h2o.import_file(pyunit_utils.locate(dataset))
+    test_frames_conversion(h2oframe, original_pandas_frame, module)
             
-def test_frames_conversion(h2oFrame, original_pandas_frame, module):
-    startT = time.time()
-    new_pandas_frame = h2oFrame.as_data_frame(use_multi_thread=True)
-    newTime = time.time()-startT
-    print("H2O frame to Pandas frame conversion time with multi-thread using module {1}: {0}".format(newTime, module))
+def test_frames_conversion(h2oframe, original_pandas_frame, module):
+    start_time = time.time()
+    new_pandas_frame = h2oframe.as_data_frame(use_multi_thread=True)
+    new_time = time.time()-start_time
+    print("H2O frame to Pandas frame conversion time with multi-thread using module {1}: {0}".format(new_time, module))
     # compare two frames column types                
     new_types = new_pandas_frame.dtypes
     old_types = original_pandas_frame.dtypes
-    ncol = h2oFrame.ncol
-    colNames = new_pandas_frame.columns
+    ncol = h2oframe.ncol
+    col_names = new_pandas_frame.columns
 
     for ind in list(range(ncol)):
-        assert new_types[colNames[ind]] == old_types[colNames[ind]], "Expected column types: {0}, actual column types: " \
-                                                                     "{1}".format(old_types[colNames[ind]], new_types[colNames[ind]])
-        if new_types[colNames[ind]] == "object":
-            diff = new_pandas_frame[colNames[ind]] == original_pandas_frame[colNames[ind]]
+        assert new_types[col_names[ind]] == old_types[col_names[ind]], "Expected column types: {0}, actual column types: " \
+                                                                     "{1}".format(old_types[col_names[ind]], new_types[col_names[ind]])
+        if new_types[col_names[ind]] == "object":
+            diff = new_pandas_frame[col_names[ind]] == original_pandas_frame[col_names[ind]]
             if not diff.all(): # difference caused by the presence of NAs
-                newSeries = pd.Series(new_pandas_frame[colNames[ind]])
-                newNA = newSeries.isna()
-                oldSeries = pd.Series(original_pandas_frame[colNames[ind]])
-                oldNA = oldSeries.isna()
-                assert (newNA==oldNA).all()
+                new_series = pd.Series(new_pandas_frame[col_names[ind]])
+                new_NA = new_series.isna()
+                old_series = pd.Series(original_pandas_frame[col_names[ind]])
+                old_NA = old_series.isna()
+                assert (new_NA==old_NA).all()
         else:
-            diff = (new_pandas_frame[colNames[ind]] - original_pandas_frame[colNames[ind]]).abs()
+            diff = (new_pandas_frame[col_names[ind]] - original_pandas_frame[col_names[ind]]).abs()
             assert diff.max() < 1e-10    
 
 
 def single_thread_pandas_conversion(dataset):
     print("converting h2o frame to pandas frame using single thread")
-    h2oFrame = h2o.import_file(pyunit_utils.locate(dataset))
-    startT = time.time()
-    h2oframe_panda =  h2oFrame.as_data_frame()
-    newTime = time.time()-startT
+    h2oframe = h2o.import_file(pyunit_utils.locate(dataset))
+    start_time = time.time()
+    h2oframe_panda =  h2oframe.as_data_frame()
+    new_time = time.time()-start_time
     print("H2O frame to Pandas frame conversion time with single thread for dataset {1}: {0}".format(newTime, dataset))
     return h2oframe_panda
