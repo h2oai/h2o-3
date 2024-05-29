b3b813e485ad4fafeef76181b454f6e7b5cd8b1f
wendycwong <wendy@h2o.ai>
2024-05-29 12:41:42 -0700

    GH-15947: fixed skipped_column error in Python (#16164)
    * GH-15947: fixed column length discrepancies when skipped_columns are specified when calling h2o.H2OFrame.
    GH-15947: fixed skipped columns for normal import_file path.
    GH-15947: added same skipped column capability when transforming R data frames to h2O data frames.
    GH-15947: add parameter to avoid R cmd test failure.
    GH-15947: Incorporate Seb code review comments.
    * GH-15947: fixed test conditions when python object is tranforming to H2O Frame and when import_file is loaded into H2O frame.

--- h2o-r/h2o-package/R/parse.R
+++ h2o-r/h2o-package/R/parse.R
@@ -219,8 +219,10 @@ h2o.parseSetup <- function(data, pattern="", destination_frame = "", header = NA
     else
       col.names
     if (!is.null(parseSetup$column_names) &&
-        (length(parseSetup$column_names) != parsedColLength)) {
-      stop("length of col.names must equal to the number of columns in dataset")
+        (length(parseSetup$column_names) != parsedColLength)) { # should equal, if not, need to check skipped_columns
+        if ((!is.null(skipped_columns) && ((length(parseSetup$column_names)-length(skipped_columns)) != parsedColLength))
+            || is.null(skipped_columns)) # if no skipped column, this is an error.  If skipped columns, check length
+            stop("length of col.names (minus length of skipped_columns if present) must equal to the number of columns in dataset")
     }
     # change column names to what the user specified
     if (!is.null(skipped_columns)) {
