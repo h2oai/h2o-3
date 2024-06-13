dfc108f419f6e4a647caf46b3afb43dd616adb3f
wendycwong <wendy.wong@h2o.ai>
2024-06-13 13:43:39 -0700

    add test for integer sort which is where the bug is.

--- h2o-py/tests/testdir_munging/pyunit_gh_6644_sort_integer_bug_large.py
+++ h2o-py/tests/testdir_munging/pyunit_gh_6644_sort_integer_bug_large.py
@@ -3,24 +3,13 @@ import h2o
 sys.path.insert(1,"../../")
 from tests import pyunit_utils
 
-def test_sort_huge_vector_bug():
-    # load frame that have sorting problem from Hutch3232
-    # sorting did okay from 0 to 180000000
-    # I am trying out the last rows because I believe the sorting of long is the problem. Nope, it works
+def test_second_sort_bug():
+    # from user.  The integer sort is buggy form some reason.
     data_frame = h2o.import_file(pyunit_utils.locate("bigdata/laptop/sort_merge_tests/gh_6644_sort_bug_shorter.zstd"))
-    sorted_frame = data_frame.sort([0])
-    # check and make sure real number sort is correct
+    sorted_frame = h2o.import_file(pyunit_utils.locate("bigdata/laptop/sort_merge_tests/gh_6644_after_1st_sort_shorter.zstd"))
+    # check and make sure integer sort is correct
     row_per_subframe = 500000
     nrow = data_frame.nrow
-    num_for_loop = int(nrow/row_per_subframe)
-    offset = 0
-    for ind in range(1, num_for_loop):
-        low_index = (ind-1)*row_per_subframe
-        high_index = ind*row_per_subframe
-        print("checking row {0} : {1}".format(low_index, high_index))
-        sub_frame1 = sorted_frame[low_index:high_index,0]
-        pyunit_utils.check_sorted_1_column_local(sub_frame1, [0], prob=1.0, offset=offset)
-        offset += row_per_subframe        
     resorted_frame = sorted_frame.sort([1])
     offset = 0
     num_for_loop = int(nrow/row_per_subframe)
@@ -44,16 +33,14 @@ def test_sort_huge_vector_bug():
     sub_frame2 = resorted_frame[offset:nrow, 1]
     compare_result_temp = pyunit_utils.compare_frame_local_one_int_column(sub_frame1, sub_frame2, offset)
     if len(compare_result_temp) > 0:
-        compare_result.extend(compare_result_temp)
-    if len(compare_result) > 0 :
         # with open("/Users/wendycwong/temp/badSort.txt", "w") as f:
         #     for s in compare_result:
         #         f.write(str(s)+"\n")
-        print("Lenght of bad sort: {0}".format(len(compare_result)))
-        assert len(compare_result)==0, "Integer sort failed at {0} rows".format(len(compare_result))
+        print("Lenght of bad sort: {0}".format(len(compare_result_temp)))
+        assert len(compare_result_temp)==0, "Integer sort with {0} rows".format(len(compare_result_temp))
     
     
 if __name__ == "__main__":
-    pyunit_utils.standalone_test(test_sort_huge_vector_bug)
+    pyunit_utils.standalone_test(test_second_sort_bug)
 else:
-    test_sort_huge_vector_bug()
+    test_second_sort_bug()
