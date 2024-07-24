66639530e7c839b11056c67c7a649eb21dc2aff9
Adam Valenta <adam.valenta@h2o.ai>
2024-07-24 16:00:11 +0200

    fix R test

--- h2o-r/tests/testdir_algos/dt/runit_dt_multinomial.R
+++ h2o-r/tests/testdir_algos/dt/runit_dt_multinomial.R
@@ -15,10 +15,10 @@ test_dt_multinomial <- function() {
   predictors <- c("C1", "C2", "C3")
   
   # Train the model
-  dt <- h2o.decisionTree(max_depth = 3, x = predictors, y = response_col, training_frame = data)
+  dt <- h2o.decision_tree(max_depth = 3, x = predictors, y = response_col, training_frame = data)
   
   # Print model summary
   print(dt)
 }
 
-doTest("Decision tree: multinomial classification", test.compare.classification.with.r)
+doTest("Decision tree: multinomial classification", test_dt_multinomial)
