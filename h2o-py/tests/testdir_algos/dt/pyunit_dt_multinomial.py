19d8cda2944954c2be8cc88eeda2391cd9deda6e
syzonyuliia <yuliia.syzon@h2o.ai>
2024-07-24 14:24:34 +0200

    GH-6769: add multinomial python test

---
+++ h2o-py/tests/testdir_algos/dt/pyunit_dt_multinomial.py
@@ -0,0 +1,26 @@
+import sys
+import h2o
+from tests import pyunit_utils
+from h2o.estimators import H2ODecisionTreeEstimator
+
+sys.path.insert(1, "../../../")
+
+
+def test_dt_multinomial():
+    data = h2o.import_file(pyunit_utils.locate("smalldata/sdt/sdt_3EnumCols_10kRows_multinomial.csv"))
+    response_col = "response"
+    data[response_col] = data[response_col].asfactor()
+
+    predictors = ["C1", "C2", "C3"]
+
+    # train model
+    dt = H2ODecisionTreeEstimator(max_depth=3)
+    dt.train(x=predictors, y=response_col, training_frame=data)
+
+    dt.show()
+
+
+if __name__ == "main":
+    pyunit_utils.standalone_test(test_dt_multinomial)
+else:
+    test_dt_multinomial()
