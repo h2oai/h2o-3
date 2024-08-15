d16b79cdee55b923a25b04dda6bb04eb9d56d7df
Veronika Maurerova <veronika.maurerova@h2o.ai>
2024-08-15 11:50:06 +0200

    Add possibility to set auc_type

--- h2o-py/tests/testdir_algos/automl/pyunit_automl_multiclass.py
+++ h2o-py/tests/testdir_algos/automl/pyunit_automl_multiclass.py
@@ -17,6 +17,19 @@ def test_default_automl_with_multiclass_task():
     print(aml.leaderboard)
     assert aml.leaderboard.columns == ["model_id", "mean_per_class_error", "logloss", "rmse", "mse"]
 
+    # test setting auc_type
+    auc_type = "WEIGHTED_OVR"
+    aml2 = H2OAutoML(max_models=2,
+                     project_name='aml_multiclass_auc_type',
+                     algo_parameters={"auc_type": auc_type})
+    model = aml2.train(y=ds.target, training_frame=ds.train, validation_frame=ds.valid, leaderboard_frame=ds.test)
+    print(model)
+    print(model.params["auc_type"])
+    assert auc_type == model.params["auc_type"]["input"], "The auc_type parameter should be the same."
+    auc_table = model.model_performance().multinomial_auc_table()
+    print(auc_table)
+    assert auc_table is not None, "The multinomial AUC table should not be None"
+
 
 pu.run_tests([
     test_default_automl_with_multiclass_task
