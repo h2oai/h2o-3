55e730ff0171dc432e20be9f01aded96daafc591
wendycwong <wendy.wong@h2o.ai>
2024-10-14 10:32:28 -0700

    GH-16420: change default values from -1 to actual defaults.  Remove check for setting extra parameters.

--- h2o-py/h2o/estimators/infogram.py
+++ h2o-py/h2o/estimators/infogram.py
@@ -66,10 +66,10 @@ class H2OInfogram(H2OEstimator):
                  algorithm="auto",  # type: Literal["auto", "deeplearning", "drf", "gbm", "glm", "xgboost"]
                  algorithm_params=None,  # type: Optional[dict]
                  protected_columns=None,  # type: Optional[List[str]]
-                 total_information_threshold=-1.0,  # type: float
-                 net_information_threshold=-1.0,  # type: float
-                 relevance_index_threshold=-1.0,  # type: float
-                 safety_index_threshold=-1.0,  # type: float
+                 total_information_threshold=0.1,  # type: float
+                 net_information_threshold=0.1,  # type: float
+                 relevance_index_threshold=0.1,  # type: float
+                 safety_index_threshold=0.1,  # type: float
                  data_fraction=1.0,  # type: float
                  top_n_features=50,  # type: int
                  ):
@@ -700,25 +700,6 @@ class H2OInfogram(H2OEstimator):
         Customized parameters for the machine learning algorithm specified in the algorithm parameter.
 
         Type: ``dict``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> gbm_params = {'ntrees':3}
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, algorithm="gbm", algorithm_params=gbm_params)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.algorithm_params
-        >>> ig.plot()
         """
         if self._parms.get("algorithm_params") != None:
             algorithm_params_dict =  ast.literal_eval(self._parms.get("algorithm_params"))
@@ -764,24 +745,6 @@ class H2OInfogram(H2OEstimator):
         information is the x-axis of the Core Infogram. Default is -1 which gets set to 0.1.
 
         Type: ``float``, defaults to ``-1.0``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, total_information_threshold=0.5)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.total_information_threshold
-        >>> ig.plot()
         """
         return self._parms.get("total_information_threshold")
 
@@ -805,24 +768,6 @@ class H2OInfogram(H2OEstimator):
         the y-axis of the Core Infogram. Default is -1 which gets set to 0.1.
 
         Type: ``float``, defaults to ``-1.0``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, total_information_threshold=0.5)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.total_information_threshold
-        >>> ig.plot()
         """
         return self._parms.get("net_information_threshold")
 
@@ -847,24 +792,6 @@ class H2OInfogram(H2OEstimator):
         which gets set to 0.1.
 
         Type: ``float``, defaults to ``-1.0``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, relevance_index_threshold=0.5)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.relevance_index_threshold
-        >>> ig.plot()
         """
         return self._parms.get("relevance_index_threshold")
 
@@ -889,24 +816,6 @@ class H2OInfogram(H2OEstimator):
         gets set to 0.1.
 
         Type: ``float``, defaults to ``-1.0``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, safety_index_threshold=0.5)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.safety_index_threshold
-        >>> ig.plot()
         """
         return self._parms.get("safety_index_threshold")
 
@@ -928,24 +837,6 @@ class H2OInfogram(H2OEstimator):
         and less than or equal to 1.0 is acceptable.
 
         Type: ``float``, defaults to ``1.0``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, data_fraction=0.7)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.data_fraction
-        >>> ig.plot()
         """
         return self._parms.get("data_fraction")
 
@@ -961,24 +852,6 @@ class H2OInfogram(H2OEstimator):
         importance, and the top N are evaluated.  Defaults to 50.
 
         Type: ``int``, defaults to ``50``.
-
-        :examples:
-
-        >>> import h2o
-        >>> from h2o.estimators.infogram import H2OInfogram
-        >>> h2o.init()
-        >>> f = "https://s3.amazonaws.com/h2o-public-test-data/smalldata/admissibleml_test/taiwan_credit_card_uci.csv"
-        >>> col_types = {'SEX': "enum", 'MARRIAGE': "enum", 'default payment next month': "enum"}
-        >>> df = h2o.import_file(path=f, col_types=col_types)
-        >>> train = df.split_frame(seed=1)[0]
-        >>> y = "default payment next month"
-        >>> x = train.columns
-        >>> x.remove(y)
-        >>> pcols = ["SEX", "MARRIAGE", "AGE"]
-        >>> ig = H2OInfogram(protected_columns=pcols, top_n_features=30)
-        >>> ig.train(y=y, x=x, training_frame=train)
-        >>> ig.top_n_features
-        >>> ig.plot()
         """
         return self._parms.get("top_n_features")
 
@@ -1001,7 +874,7 @@ class H2OInfogram(H2OEstimator):
 
     def plot(self, train=True, valid=False, xval=False, figsize=(10, 10), title="Infogram", legend_on=False, server=False):
         """
-        Plot the infogram.  By default, it will plot the infogram calculated from training dataset. 
+        Plot the infogram.  By default, it will plot the infogram calculated from training dataset.  
         Note that the frame rel_cmi_frame contains the following columns:
         - 0: predictor names
         - 1: admissible 
