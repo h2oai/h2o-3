cd375242e5c8b7c5ef39f1e594bafa75e0e55a19
Adam Valenta <adam.valenta@h2o.ai>
2024-11-01 15:13:10 +0100

    Cleanup - remove unused code

--- h2o-sagemaker/grid/grid_scripts/startup_h2o_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-memTotalKb=`cat /proc/meminfo | grep MemTotal | sed 's/MemTotal:[ \t]*//' | sed 's/ kB//'`
-memTotalMb=$[ $memTotalKb / 1024 ]
-tmp=$[ $memTotalMb * 70 ]
-xmxMb=$[ $tmp / 100 ]
-
-/usr/bin/java -Xmx${xmxMb}m -jar h2o.jar -name sagemakerCloud -flatfile flatfile.txt # -network 10.0.0.0/8
