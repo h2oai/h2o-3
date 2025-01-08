548f0f794a112c3b785eeb2c3cdbd7e4ab591161
Veronika Maurerova <veronika.maurerova@h2o.ai>
2025-01-08 16:07:16 +0100

    Implement KNN API

---
+++ h2o-r/tests/testdir_algos/knn/runit_knn_smoke.R
@@ -0,0 +1,24 @@
+setwd(normalizePath(dirname(R.utils::commandArgs(asValues=TRUE)$"f")))
+source("../../../scripts/h2o-r-test-setup.R")
+
+
+
+knn.smoke <- function() {
+    iris.hex <- h2o.uploadFile( locate("smalldata/iris/iris.csv"))
+    iris.knn <-  h2o.knn(x=1:4, training_frame=iris.hex, k = 3, distance="euclidean", seed = 1234)
+
+    # Score test data with different default auc_type (previous was "NONE", so no AUC calculation)
+    perf <- h2o.performance(iris.knn, test.hex, auc_type="WEIGHTED_OVO")
+
+    # Check default AUC is set correctly
+    auc_table <- h2o.multinomial_auc_table(perf)
+    default_auc <- h2o.auc(perf)
+    weighted_ovo_auc <- auc_table[32, 4] # weighted ovo AUC is the last number in the table
+
+    expect_equal(default_auc, weighted_ovo_auc)
+    
+    distances <- iris.knn@model$distances
+    print(distances)
+}
+
+doTest("KNN Test: Check model is running.", knn.smoke)
