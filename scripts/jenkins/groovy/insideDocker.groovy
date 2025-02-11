5f979dd4e3bcc61cb81f0190a36dd1b0d75e77f6
Adam Valenta <adam.valenta@h2o.ai>
2025-02-11 21:40:33 +0100

    fix docker insight with entrypoint and root user

--- scripts/jenkins/groovy/insideDocker.groovy
+++ scripts/jenkins/groovy/insideDocker.groovy
@@ -17,41 +17,34 @@ def call(customEnv, image, registry, buildConfig, timeoutValue, timeoutUnit, cus
     }
   }
 
-  docker.image(image).inside("--init --entrypoint='' -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID}") {
-    sh """
-      printenv | sort
-    """
+  withEnv(customEnv) {
+    timeout(time: timeoutValue, unit: timeoutUnit) {
+      docker.withRegistry("https://${registry}") {
+        withCredentials([/**file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'),**/ [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: "${awsCredsPrefix}AWS_ACCESS_KEY_ID", credentialsId: 'H2O-AWS_CT-JENKINS-SHARED-SERVICE-PROD', secretKeyVariable: "${awsCredsPrefix}AWS_SECRET_ACCESS_KEY"]]) {
+          withCredentials([string(credentialsId: 'DRIVERLESS_AI_LICENSE_KEY', variable: 'DRIVERLESS_AI_LICENSE_KEY')/**, string(credentialsId: "H2O3_GET_PROJECT_TOKEN", variable:  "H2O3_GET_PROJECT_TOKEN")**/]) {
+            dockerGroupIdAdd = ""
+            if (addToDockerGroup) {
+              dockerGroupName = "docker"
+              dockerGroupId = sh(script: "awk -F: '/$dockerGroupName/ {print \$3}' /etc/group", returnStdout: true).trim()
+              if (dockerGroupId == "") {
+                error "cannot find gid of $dockerGroupName"
+              } else {
+                dockerGroupIdAdd = "--group-add ${dockerGroupId}"
+              }
+            }
+//            // TODO add -e H2O3_GET_PROJECT_TOKEN=${H2O3_GET_PROJECT_TOKEN}
+           docker.image(image).inside("--user=root:root --entrypoint='' --init ${dockerGroupIdAdd} -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID} -e ${awsCredsPrefix}AWS_SECRET_ACCESS_KEY=${awsCredsPrefix}\${AWS_SECRET_ACCESS_KEY} -e DRIVERLESS_AI_LICENSE_KEY=${DRIVERLESS_AI_LICENSE_KEY}  -v /home/jenkins:/home/jenkins/repos ${customArgs}") {
+              sh """
+              id
+              printenv | sort
+            """
+              block()
+            }
+          }
+        }
+      }
+    }
   }
-  
-  
-//  withEnv(customEnv) {
-//    timeout(time: timeoutValue, unit: timeoutUnit) {
-//      docker.withRegistry("https://${registry}") {
-//        withCredentials([/**file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'),**/ [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: "${awsCredsPrefix}AWS_ACCESS_KEY_ID", credentialsId: 'H2O-AWS_CT-JENKINS-SHARED-SERVICE-PROD', secretKeyVariable: "${awsCredsPrefix}AWS_SECRET_ACCESS_KEY"]]) {
-//          withCredentials([string(credentialsId: 'DRIVERLESS_AI_LICENSE_KEY', variable: 'DRIVERLESS_AI_LICENSE_KEY')/**, string(credentialsId: "H2O3_GET_PROJECT_TOKEN", variable:  "H2O3_GET_PROJECT_TOKEN")**/]) {
-//            dockerGroupIdAdd = ""
-//            if (addToDockerGroup) {
-//              dockerGroupName = "docker"
-//              dockerGroupId = sh(script: "awk -F: '/$dockerGroupName/ {print \$3}' /etc/group", returnStdout: true).trim()
-//              if (dockerGroupId == "") {
-//                error "cannot find gid of $dockerGroupName"
-//              } else {
-//                dockerGroupIdAdd = "--group-add ${dockerGroupId}"
-//              }
-//            }
-////            // TODO add -e H2O3_GET_PROJECT_TOKEN=${H2O3_GET_PROJECT_TOKEN}
-//           docker.image(image).inside("--init ${dockerGroupIdAdd} -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID} -e ${awsCredsPrefix}AWS_SECRET_ACCESS_KEY=${awsCredsPrefix}\${AWS_SECRET_ACCESS_KEY} -e DRIVERLESS_AI_LICENSE_KEY=${DRIVERLESS_AI_LICENSE_KEY}  -v /home/jenkins:/home/jenkins/repos ${customArgs}") {
-//              sh """
-//              id
-//              printenv | sort
-//            """
-//              block()
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
 }
 
 return this
