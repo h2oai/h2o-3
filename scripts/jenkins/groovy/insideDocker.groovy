89920222501e6348668172215c64fd4e70138ac9
Adam Valenta <adam.valenta@h2o.ai>
2025-02-13 19:26:31 +0100

    GH-16526 Bring back nightly (#16549)
    * change registry
    
    * fix docker login
    
    * fix docker login
    
    * comment missing credential
    
    * remove unknown token
    
    * Remove health checking
    
    * comment printenv
    
    * Ignore xgboost for now
    
    * [DevOps] Fix Nodel Labels for Jenkins Pipelines (#16521)
    
    * ci: Fix labels
    
    * ci: fix label typo
    
    * ci: fix label constructors
    
    * ci: remove health check labels
    
    * remove opsh2oai from the image reference
    
    * leave only one stage that should work
    
    * build regardless of health check
    
    * try different volumes
    
    * try only docker image in inside docker
    
    * fix docker test in insightDocker
    
    * Remove block()
    
    * Remove id
    
    * fix docker inside
    
    * try image in the main dockerfile
    
    * test
    
    * fix docker insight with entrypoint and root user
    
    * fix :h2o-r:gitbranch
    
    * test
    
    * fix git
    
    * install pupeteer
    
    * try with image where node js is on version 20
    
    * install nodejs
    
    * test with node 20
    
    * try to solve git issue
    
    * Add python 3.7, java 8 and R 3.4.1 tests
    
    * Comment xgboost
    
    * back to standard rebuilded image
    
    * use image version 46
    
    * Fix public build temporarly
    
    ---------
    
    Co-authored-by: Chathrinda Ranasinghe <chathurinda.ranasinghe@h2o.ai>

--- scripts/jenkins/groovy/insideDocker.groovy
+++ scripts/jenkins/groovy/insideDocker.groovy
@@ -8,19 +8,20 @@ def call(customEnv, image, registry, buildConfig, timeoutValue, timeoutUnit, cus
   }
 
   retryWithDelay(3 /* retries */, 120 /* delay in sec */) {
-    withCredentials([usernamePassword(credentialsId: registry, usernameVariable: 'REGISTRY_USERNAME', passwordVariable: 'REGISTRY_PASSWORD')]) {
+    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: "H2O-AWS_CT-JENKINS-H2O-ECR"]]) {
+      H2O_ECR_PWD = sh(script: 'aws ecr get-login-password --region us-east-1', returnStdout: true).trim()
       sh """
-        docker login -u $REGISTRY_USERNAME -p $REGISTRY_PASSWORD ${registry}
-        docker pull ${image}
-      """
+            docker login --username AWS --password $H2O_ECR_PWD ${registry}
+            docker pull ${image}
+        """
     }
   }
 
   withEnv(customEnv) {
     timeout(time: timeoutValue, unit: timeoutUnit) {
       docker.withRegistry("https://${registry}") {
-        withCredentials([file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'), [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: "${awsCredsPrefix}AWS_ACCESS_KEY_ID", credentialsId: 'H2O-AWS_CT-JENKINS-SHARED-SERVICE-PROD', secretKeyVariable: "${awsCredsPrefix}AWS_SECRET_ACCESS_KEY"]]) {
-          withCredentials([string(credentialsId: 'DRIVERLESS_AI_LICENSE_KEY', variable: 'DRIVERLESS_AI_LICENSE_KEY'), string(credentialsId: "H2O3_GET_PROJECT_TOKEN", variable:  "H2O3_GET_PROJECT_TOKEN")]) {
+        withCredentials([/**file(credentialsId: 'c096a055-bb45-4dac-ba5e-10e6e470f37e', variable: 'JUNIT_CORE_SITE_PATH'),**/ [$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: "${awsCredsPrefix}AWS_ACCESS_KEY_ID", credentialsId: 'H2O-AWS_CT-JENKINS-SHARED-SERVICE-PROD', secretKeyVariable: "${awsCredsPrefix}AWS_SECRET_ACCESS_KEY"]]) {
+          withCredentials([string(credentialsId: 'DRIVERLESS_AI_LICENSE_KEY', variable: 'DRIVERLESS_AI_LICENSE_KEY')/**, string(credentialsId: "H2O3_GET_PROJECT_TOKEN", variable:  "H2O3_GET_PROJECT_TOKEN")**/]) {
             dockerGroupIdAdd = ""
             if (addToDockerGroup) {
               dockerGroupName = "docker"
@@ -31,7 +32,8 @@ def call(customEnv, image, registry, buildConfig, timeoutValue, timeoutUnit, cus
                 dockerGroupIdAdd = "--group-add ${dockerGroupId}"
               }
             }
-            docker.image(image).inside("--init ${dockerGroupIdAdd} -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID} -e ${awsCredsPrefix}AWS_SECRET_ACCESS_KEY=${awsCredsPrefix}\${AWS_SECRET_ACCESS_KEY} -e DRIVERLESS_AI_LICENSE_KEY=${DRIVERLESS_AI_LICENSE_KEY} -e H2O3_GET_PROJECT_TOKEN=${H2O3_GET_PROJECT_TOKEN} -v /home/0xdiag:/home/0xdiag -v /home/jenkins/repos:/home/jenkins/repos ${customArgs}") {
+//            // TODO add -e H2O3_GET_PROJECT_TOKEN=${H2O3_GET_PROJECT_TOKEN}
+           docker.image(image).inside("--user=root:root --entrypoint='' --init ${dockerGroupIdAdd} -e AWS_CREDS_PREFIX='${awsCredsPrefix}' -e ${awsCredsPrefix}AWS_ACCESS_KEY_ID=${awsCredsPrefix}\${AWS_ACCESS_KEY_ID} -e ${awsCredsPrefix}AWS_SECRET_ACCESS_KEY=${awsCredsPrefix}\${AWS_SECRET_ACCESS_KEY} -e DRIVERLESS_AI_LICENSE_KEY=${DRIVERLESS_AI_LICENSE_KEY}  -v /home/jenkins:/home/jenkins/repos ${customArgs}") {
               sh """
               id
               printenv | sort
