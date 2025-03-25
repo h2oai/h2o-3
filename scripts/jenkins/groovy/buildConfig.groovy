81ea726e04960b55dff866cb02c0961738f54d4a
Chathurinda <chathurinda.ranasinghe@h2o.ai>
2025-03-25 13:46:07 +0530

    ci: change image used for testing

--- scripts/jenkins/groovy/buildConfig.groovy
+++ scripts/jenkins/groovy/buildConfig.groovy
@@ -370,7 +370,7 @@ class BuildConfig {
     println version
     println suffix
     println "#### GET SMOKE HADOOP IMAGE IMPL ###"
-    return "353750902984.dkr.ecr.us-east-1.amazonaws.com/h2o-3/dev-build-hadoop:46".toString()
+    return "353750902984.dkr.ecr.us-east-1.amazonaws.com/opsh2oai/h2o-3-hadoop-cdh-6.3:84".toString()
   }
 
   static enum NodeLabels {
