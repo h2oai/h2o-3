cbdf8dd8f074553c3dd425abc8199e57d1da7019
silvarohan18 <rohan.silva@h2o.ai>
2025-05-09 13:42:47 +0530

    ci

--- scripts/github/build_h2o3.sh
+++ scripts/github/build_h2o3.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Running H2O-3 build (mock)..."
 # Simulate Gradle/Maven build
-./gradlew clean build -x test
+./gradlew --parallel build -x test -x :h2o-assemblies:minimal:shadowJar -x :h2o-assemblies:steam:shadowJar
