6b71c4ad2694833ef22d8a8f2d8478c6190ff984
chathurinda <chathurinda.ranasinghe@h2o.ai>
2025-05-26 12:44:35 +0530

    ci: add safe directory in gradle

--- scripts/jenkins/groovy/hadoopStage.groovy
+++ scripts/jenkins/groovy/hadoopStage.groovy
@@ -47,7 +47,7 @@ def call(final pipelineContext, final stageConfig) {
             rm -f mykeystore.jks
             keytool -genkey -dname "cn=Mr. Jenkins, ou=H2O-3, o=H2O.ai, c=US" -alias h2o -keystore mykeystore.jks -storepass h2oh2o -keypass h2oh2o -keyalg RSA -keysize 2048
 
-            git config --global --add safe.directory .
+            git config --global --add safe.directory $(pwd)
 
             echo 'Building H2O'
             BUILD_HADOOP=true H2O_TARGET=${stageConfig.customData.distribution}${stageConfig.customData.version} ./gradlew clean build -x test
