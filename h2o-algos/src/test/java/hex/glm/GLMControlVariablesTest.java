e1cc4b60969168dac3fb60ad64e8c7bec46ab14f
Veronika Maurerova <veronika.maurerova@h2o.ai>
2025-05-26 15:11:00 +0200

    Implement scoring history and variable importance

--- h2o-algos/src/test/java/hex/glm/GLMControlVariablesTest.java
+++ h2o-algos/src/test/java/hex/glm/GLMControlVariablesTest.java
@@ -11,16 +11,18 @@ import water.fvec.Frame;
 import water.fvec.Vec;
 import water.runner.CloudSize;
 import water.runner.H2ORunner;
+import water.util.ArrayUtils;
 import water.util.DistributedException;
+import water.util.TwoDimTable;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @RunWith(H2ORunner.class)
 @CloudSize(1)
 public class GLMControlVariablesTest extends TestUtil {
     
     @Test
-    public void testTrainModelWithControlVariables() {
+    public void compareModelWithControlVariablesEnabledAndDisabled() {
         Frame train = null;
         Frame test = null;
         Frame preds = null;
@@ -48,19 +50,23 @@ public class GLMControlVariablesTest extends TestUtil {
 
             test = new Frame(train);
             test.remove(responseColumn);
+            
+            String[] control_variables = new String[]{"C1", "C13", "C20"};
 
             GLMModel.GLMParameters params = new GLMModel.GLMParameters(family);
             params._response_column = responseColumn;
             params._train = train._key;
-            params._control_variables = new String[]{"C1", "C13", "C20"};
+            params._control_variables = control_variables;
+            params._score_each_iteration = true;
 
+            // train model witch control variables enabled
             glm = new GLM(params).trainModel().get();
             Scope.track_generic(glm);
             
             preds = glm.score(test);
             Scope.track_generic(preds);
 
-            // train model without control variables
+            // train model with control variables disabled
             params._control_variables = null;
 
             glm2 = new GLM(params).trainModel().get();
@@ -69,7 +75,7 @@ public class GLMControlVariablesTest extends TestUtil {
             preds2 = glm2.score(test);
             Scope.track_generic(preds2);
 
-            // check metrics are the same
+            // check reuslt training metrics are the same
             double delta = 10e-10;
             assertEquals(glm.auc(), glm2.auc(), delta);
             assertEquals(glm.mse(), glm2.mse(), delta);
@@ -79,6 +85,11 @@ public class GLMControlVariablesTest extends TestUtil {
             double tMse2 = glm2._output._training_metrics._MSE;
             System.out.println(tMse+" "+tMse2);
             assertEquals(tMse, tMse2, delta);
+
+            // check result training metrics are not the same with control val training metrics
+            assertNotEquals(glm2._output._training_metrics.auc_obj()._auc, glm._output._control_val_training_metrics.auc_obj()._auc);
+            assertNotEquals(glm2._output._training_metrics.mse(), glm._output._control_val_training_metrics.mse());
+            assertNotEquals(glm2._output._training_metrics.rmse(), glm._output._control_val_training_metrics.rmse());
             
             // check preds differ
             int differ = 0;
@@ -89,7 +100,44 @@ public class GLMControlVariablesTest extends TestUtil {
                 if(preds.vec(2).at(i) != preds2.vec(2).at(i)) differ++;
             }
             System.out.println(differ + " " + threshold);
-            assert differ > threshold; 
+            assert differ > threshold;
+
+            TwoDimTable glmSH = glm._output._scoring_history;
+            TwoDimTable glm2SH = glm2._output._scoring_history;
+            TwoDimTable glmSHCV = glm._output._control_val_scoring_history;
+            TwoDimTable glm2SHCV = glm2._output._control_val_scoring_history;
+            
+            // check scoring history is the same (instead of timestamp and duration column)
+            assertTwoDimTableEquals(glmSH, glm2SH, new int[]{0,1});
+            
+            // check control val scoring history is not null when control vals is enabled
+            assertNotNull(glmSHCV);
+
+            // check control val scoring history is null when control vals is disabled
+            assertNull(glm2SHCV);
+            
+            //check variable importance
+            TwoDimTable vi = glm._output._variable_importances;
+            String[] viRowHeader = vi.getRowHeaders();
+            
+            int numControlVariables = 0;
+            String suffix = "_control_variable";
+            for (String name : viRowHeader) {
+                if(name.contains(suffix)){
+                    numControlVariables++;
+                    if (name.contains(".")) {
+                        String catName = name.split("\\.")[0];
+                        assert ArrayUtils.find(control_variables, catName) > -1 : 
+                                "Variable "+catName+" should not be marked as _control_variable.";
+                    } else {
+                        String numericName = name.substring(0, name.length() - suffix.length());
+                        assert ArrayUtils.find(control_variables, numericName) > -1 :
+                                "Variable "+numericName+" should not be marked as _control_variable.";
+                    }
+                }
+            }
+            assert numControlVariables >= control_variables.length;
+            
         } finally {
             if(train != null) train.remove();
             if(test != null) test.remove();
@@ -138,7 +186,6 @@ public class GLMControlVariablesTest extends TestUtil {
             test.remove(glm._output.responseName());
             preds = glm.score(test);
             Scope.track_generic(preds);
-
 
             glm.adaptTestForTrain(test, true, false);
             test.remove(test.numCols() - 1); // remove response
