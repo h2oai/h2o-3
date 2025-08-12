a15a07605b3b0dd17d1c3923eb7e24f33f0e2a86
Adam Valenta <adam.valenta@h2o.ai>
2025-08-12 14:51:18 +0200

    Add check for empty value, if its empty, do not adjust anything as there is nothing

--- h2o-parsers/h2o-parquet-parser/src/main/java/water/parser/parquet/FrameParquetExporter.java
+++ h2o-parsers/h2o-parquet-parser/src/main/java/water/parser/parquet/FrameParquetExporter.java
@@ -113,9 +113,11 @@ public class FrameParquetExporter  {
                         switch (currColType) {
                             case (T_UUID):
                             case (T_TIME):
-                                long timestamp = cs[j].at8(i);
-                                long adjustedTimestamp = timestamp - timeStampAdjustment;
-                                group = group.append(currColName, adjustedTimestamp);
+                                if (!cs[j].isNA(i)) {
+                                    long timestamp = cs[j].at8(i);
+                                    long adjustedTimestamp = timestamp - timeStampAdjustment;
+                                    group = group.append(currColName, adjustedTimestamp);
+                                }
                                 break;
                             case (T_STR):
                                 if (!cs[j].isNA(i)) {
