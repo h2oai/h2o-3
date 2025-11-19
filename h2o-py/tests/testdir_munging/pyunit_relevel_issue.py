413539515e5b03e41f83235490becb5f831b0f2b
Tomáš Frýda <tomas.fryda@h2o.ai>
2025-11-19 17:11:21 +0100

    Incorporate Adam's suggestion

--- h2o-py/tests/testdir_munging/pyunit_relevel_issue.py
+++ h2o-py/tests/testdir_munging/pyunit_relevel_issue.py
@@ -4,8 +4,6 @@ import h2o
 import pandas as pd
 from tests import pyunit_utils
 
-def _assert_equal(x, y):
-    assert x == y, f"{x} != {y}"
 
 def test_relevel_issue():
     df = pd.DataFrame({
@@ -13,10 +11,9 @@ def test_relevel_issue():
         "bin_levels": ["$10'0", "$2\"00", "$500", "other"]
     })
     hdf = h2o.H2OFrame(df, column_types=["enum", "enum"])
-    _assert_equal(hdf["bin_levels"].relevel("$500").levels()[0][0], "$500")
-    _assert_equal(hdf["bin_levels"].relevel("$10'0").levels()[0][0], "$10'0")
-    _assert_equal(hdf["bin_levels"].relevel("$2\"00").levels()[0][0], "$2\"00")
-
+    pyunit_utils.assert_equals(hdf["bin_levels"].relevel("$500").levels()[0][0], "$500")
+    pyunit_utils.assert_equals(hdf["bin_levels"].relevel("$10'0").levels()[0][0], "$10'0")
+    pyunit_utils.assert_equals(hdf["bin_levels"].relevel("$2\"00").levels()[0][0], "$2\"00")
 
 
 pyunit_utils.standalone_test(test_relevel_issue)
