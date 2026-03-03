1e248ef38bbd240a99c314f2de84eb30c6fb5b8f
Veronika Maurerova <veronika.maurerova@h2o.ai>
2026-03-03 06:53:57 +0100

    Implement copilot review

--- h2o-py/tests/testdir_algos/glm/pyunit_remove_offset_effects_compare.py
+++ h2o-py/tests/testdir_algos/glm/pyunit_remove_offset_effects_compare.py
@@ -31,7 +31,8 @@ def glm_remove_offset_effects():
 
     # offset_column passed in the train method
     glm_model_remove_offset_effects = H2OGeneralizedLinearEstimator(family="binomial", remove_offset_effects=True)
-    glm_model_remove_offset_effects.train(x=list(range(2,8)),y="economy_20mpg", training_frame=cars, offset_column=offset_col)
+    glm_model_remove_offset_effects.train(x=list(range(2,8)),y="economy_20mpg", training_frame=cars, 
+                                          offset_column=offset_col)
 
     predictions_train_remove_offset_effects = glm_model_remove_offset_effects.predict(cars).as_data_frame()
     print(predictions_train_remove_offset_effects)
@@ -44,20 +45,28 @@ def glm_remove_offset_effects():
     cars[offset_col] = 0
 
     # predict with offset effects removed
-    predictions_train_remove_offset_manualy = glm_model.predict(cars).as_data_frame()
-    print(predictions_train_remove_offset_manualy)
+    predictions_train_remove_offset_manual = glm_model.predict(cars).as_data_frame()
+    print(predictions_train_remove_offset_manual)
 
     # metrics with offset effects removed
-    perf_remove_offset_manualy = glm_model.model_performance(cars)
-    print(perf_remove_offset_manualy)
-    
-    assert perf.mse() != perf_remove_offset_manualy.mse()
-    assert perf_remove_offset_manualy.mse() == perf_remove_offset_effects.mse()
+    perf_remove_offset_manual = glm_model.model_performance(cars)
+    print(perf_remove_offset_manual)
+
+    mse_with_offset = perf.mse()
+    mse_remove_offset_manual = perf_remove_offset_manual.mse()
+    mse_remove_offset_effects = perf_remove_offset_effects.mse()
+    # use tolerance-based comparisons to avoid brittleness with floating point values
+    assert abs(mse_with_offset - mse_remove_offset_manual) > 1e-6, \
+        "MSE with offset should differ from MSE with offset effects manually removed"
+    pyunit_utils.assert_equals(mse_remove_offset_manual, mse_remove_offset_effects, delta=1e-6)
 
     # check predictions are different
     for i in range(predictions_train.shape[0]):
-        pyunit_utils.assert_not_equal(predictions_train.iloc[i, 1], predictions_train_remove_offset_effects.iloc[i, 1], f"Predictions at position {i} should differ but they don't!")
-        pyunit_utils.assert_equals(predictions_train_remove_offset_manualy.iloc[i, 1], predictions_train_remove_offset_effects.iloc[i, 1], f"Predictions at position {i} should equal but they don't!")
+        pyunit_utils.assert_not_equal(predictions_train.iloc[i, 1], predictions_train_remove_offset_effects.iloc[i, 1], 
+                                      f"Predictions at position {i} should differ but they don't!")
+        pyunit_utils.assert_equals(predictions_train_remove_offset_manual.iloc[i, 1], 
+                                   predictions_train_remove_offset_effects.iloc[i, 1], 
+                                   f"Predictions at position {i} should equal but they don't!")
     
 
 if __name__ == "__main__":
