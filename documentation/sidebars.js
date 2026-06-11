c21a0b0c65507e5354b16054e456ba5d9a14fc75
Shaun <shaun.yogeshwaran@h2o.ai>
2026-06-11 12:16:05 +0530

    Strip data-munging migration and converter tooling from foundation PR

--- documentation/sidebars.js
+++ documentation/sidebars.js
@@ -4,28 +4,6 @@ module.exports = {
     {
       "Get started": ["get-started/overview", "installation"],
     },
-    {
-      "Data manipulation": [
-        "data-munging/uploading-data",
-        "data-munging/importing-data",
-        "data-munging/importing-files",
-        "data-munging/downloading-data",
-        "data-munging/change-column-type",
-        "data-munging/combining-columns",
-        "data-munging/combining-rows",
-        "data-munging/fillnas",
-        "data-munging/groupby",
-        "data-munging/imputing-data",
-        "data-munging/merging-data",
-        "data-munging/pivot",
-        "data-munging/replacing-values",
-        "data-munging/slicing-columns",
-        "data-munging/slicing-rows",
-        "data-munging/sortcolumn",
-        "data-munging/splitting-datasets",
-        "data-munging/tokenize",
-      ],
-    },
     "release-notes",
   ],
 };
