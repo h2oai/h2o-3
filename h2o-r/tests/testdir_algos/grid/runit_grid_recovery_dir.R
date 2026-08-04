655dd0b5b2e8e5eb8ee10504e3c651a7a836f8bd
Adam Valenta <adam.valenta@h2o.ai>
2026-08-04 14:46:18 +0200

    GH-16875 - Use dedicated recovery dir in R grid recovery test (#16913)

--- h2o-r/tests/testdir_algos/grid/runit_grid_recovery_dir.R
+++ h2o-r/tests/testdir_algos/grid/runit_grid_recovery_dir.R
@@ -6,7 +6,8 @@ test.grid.resume <- function() {
 
   ntrees_opts <- c(100, 200)
   learn_rate_opts <- c(0.01, 0.02)
-  export_dir <- tempdir()
+  export_dir <- tempfile("grid_recovery")
+  dir.create(export_dir)
 
   hyper_parameters <- list(ntrees = ntrees_opts, learn_rate = learn_rate_opts)
   baseline_grid <- h2o.grid(
