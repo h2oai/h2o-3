95b4e01dc6ff337a94a98586fe7c221f960e7296
Adam Valenta <adam.valenta@h2o.ai>
2026-08-06 16:58:56 +0200

    GH-16919 - Raise timeouts for Medium-large test stages
    R3.5 and R3.4 Medium-large are capped at 210 minutes but have taken
    214-253 minutes in every recent nightly, so Jenkins aborts them and no
    test results are produced. Py3.7 Medium-large in PR_STAGES hit the same
    problem at 415 minutes against a 400 minute cap.

--- scripts/jenkins/groovy/defineTestStages.groovy
+++ scripts/jenkins/groovy/defineTestStages.groovy
@@ -200,7 +200,7 @@ def call(final pipelineContext) {
     ],
     [
       stageName: 'Py3.7 Medium-large', target: 'test-pyunit-medium-large', pythonVersion: '3.7',
-      timeoutValue: 400, component: pipelineContext.getBuildConfig().COMPONENT_PY
+      timeoutValue: 480, component: pipelineContext.getBuildConfig().COMPONENT_PY
     ],
     [
       stageName: 'Py3.7 X-large', target: 'test-pyunit-xlarge', pythonVersion: '3.7',
@@ -208,7 +208,7 @@ def call(final pipelineContext) {
     ],
     [
       stageName: 'R3.5 Medium-large', target: 'test-r-medium-large', rVersion: '3.5.3',
-      timeoutValue: 210, component: pipelineContext.getBuildConfig().COMPONENT_R
+      timeoutValue: 300, component: pipelineContext.getBuildConfig().COMPONENT_R
     ],
     [
       stageName: 'R3.5 Demos Medium-large', target: 'test-r-demos-medium-large', rVersion: '3.5.3',
@@ -463,7 +463,7 @@ def call(final pipelineContext) {
     ],
     [
       stageName: 'R3.4 Medium-large', target: 'test-r-medium-large', rVersion: '3.4.1',
-      timeoutValue: 210, component: pipelineContext.getBuildConfig().COMPONENT_R
+      timeoutValue: 300, component: pipelineContext.getBuildConfig().COMPONENT_R
     ],
     [
       stageName: 'R3.4 Small', target: 'test-r-small', rVersion: '3.4.1',
